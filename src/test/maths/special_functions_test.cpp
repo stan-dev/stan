1d2cd85c76910a58931b97086cca41e1259c6f20
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2011-11-15 22:23:38 +0000

    updated special_functions test in math
    git-svn-id: https://stan.googlecode.com/svn/trunk@171 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/test/maths/special_functions_test.cpp
+++ src/test/maths/special_functions_test.cpp
@@ -69,7 +69,7 @@ TEST(maths_test, inv_cloglog) {
 
 TEST(maths_test, Phi) {
   EXPECT_EQ(0.5 + 0.5 * boost::math::erf(0.0), stan::maths::Phi(0.0));
-  EXPECT_EQ(0.5 + 0.5 * boost::math::erf(0.9/std::sqrt(2.0)), stan::maths::Phi(0.9));
+  EXPECT_FLOAT_EQ(0.5 + 0.5 * boost::math::erf(0.9/std::sqrt(2.0)), stan::maths::Phi(0.9));
   EXPECT_EQ(0.5 + 0.5 * boost::math::erf(-5.0/std::sqrt(2.0)), stan::maths::Phi(-5.0));
 }
 
