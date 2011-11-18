18ace1ef5ac8411a75bf594baaaedf87c626740e
heywhoah <heywhoah@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2011-11-18 17:43:05 +0000

    Simplified (and maybe sped up) the implementations of normal_log().
    git-svn-id: https://stan.googlecode.com/svn/trunk@186 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/stan/prob/ag_distributions.hpp
+++ src/stan/prob/ag_distributions.hpp
@@ -13,62 +13,69 @@ namespace stan {
       std::vector<agrad::vari*> y_;
       agrad::vari* mu_;
       agrad::vari* sigma_;
-      double sigmasq_inv_, log_sigma_;
-      std::vector<double> y_minus_mu_;
+      double mu_adj2_, sigma_adj2_;
+      std::vector<double> y_adj2_;
     public:
-      normal_log_vec_vvv_vari(std::vector<agrad::vari*> y, agrad::vari* mu, agrad::vari* sigma) :
-        agrad::vari(0), y_(y), mu_(mu), sigma_(sigma), 
-        y_minus_mu_(y.size()) {
+      normal_log_vec_vvv_vari(std::vector<agrad::vari*>& y, 
+                              agrad::vari* mu, agrad::vari* sigma) :
+        agrad::vari(0), y_(y), mu_(mu), sigma_(sigma), mu_adj2_(0), 
+        sigma_adj2_(0), y_adj2_(y.size()) {
         double& val = const_cast<double&>(val_);
-        sigmasq_inv_ = 0.5 / (sigma_->val_ * sigma_->val_);
-        log_sigma_ = std::log(sigma_->val_);
-        for (unsigned int i = 0; i < y.size(); i++) {
-          y_minus_mu_[i] = y_[i]->val_ - mu->val_;
-          val -= 0.5 * sigmasq_inv_ * y_minus_mu_[i] * y_minus_mu_[i];
+        double sigma_inv = 1.0 / sigma_->val_;
+        double sigmasq_inv = sigma_inv * sigma_inv;
+        double sigmacubed_inv = sigmasq_inv * sigma_inv;
+        double half_sigmasq_inv = 0.5 * sigmasq_inv;
+        double log_sigma = std::log(sigma_->val_);
+        for (unsigned int i = 0; i < y.size(); ++i) {
+          double y_minus_mu = y_[i]->val_ - mu->val_;
+          double y_minus_mu_sq = y_minus_mu * y_minus_mu;
+          val -= half_sigmasq_inv * y_minus_mu_sq;
+          sigma_adj2_ += sigmacubed_inv * y_minus_mu_sq;
+          mu_adj2_ += sigmasq_inv * y_minus_mu;
+          y_adj2_[i] -= sigmasq_inv * y_minus_mu;
         }
-        val -= y.size() * (log_sigma_ - NEG_LOG_SQRT_TWO_PI);
+        val -= y.size() * (log_sigma - NEG_LOG_SQRT_TWO_PI);
+        sigma_adj2_ -= y.size() * sigma_inv;
       }
 
       void chain() {
-        double sigmacubed_inv2 = 2 * sigmasq_inv_ / sigma_->val_;
-        for (unsigned int i = 0; i < y_.size(); i++) {
-          y_[i]->adj_ -= adj_ * y_minus_mu_[i] * sigmasq_inv_;
-          mu_->adj_ += adj_ * y_minus_mu_[i] * sigmasq_inv_;
-          sigma_->adj_ += adj_ * 0.5 * y_minus_mu_[i] * y_minus_mu_[i] * 
-            sigmacubed_inv2;
-        }
-        sigma_->adj_ -= adj_ * y_.size() * 1.0 / sigma_->val_;
+        for (unsigned int i = 0; i < y_.size(); ++i)
+          y_[i]->adj_ += adj_ * y_adj2_[i];
+        mu_->adj_ += adj_ * mu_adj2_;
+        sigma_->adj_ += adj_ * sigma_adj2_;
       }
     };
-
+    
     class normal_log_vec_dvv_vari : public agrad::vari {
     protected:
       agrad::vari* mu_;
       agrad::vari* sigma_;
-      double sigmasq_inv_, log_sigma_;
-      std::vector<double> y_minus_mu_;
+      double mu_adj2_, sigma_adj2_;
     public:
-      normal_log_vec_dvv_vari(std::vector<double> y, agrad::vari* mu, agrad::vari* sigma) :
-        agrad::vari(0), mu_(mu), sigma_(sigma), 
-        y_minus_mu_(y.size()) {
+      normal_log_vec_dvv_vari(const std::vector<double>& y, agrad::vari* mu,
+                              agrad::vari* sigma) :
+        agrad::vari(0), mu_(mu), sigma_(sigma), mu_adj2_(0), 
+        sigma_adj2_(0) {
         double& val = const_cast<double&>(val_);
-        sigmasq_inv_ = 0.5 / (sigma_->val_ * sigma_->val_);
-        log_sigma_ = std::log(sigma_->val_);
-        for (unsigned int i = 0; i < y.size(); i++) {
-          y_minus_mu_[i] = y[i] - mu->val_;
-          val -= 0.5 * sigmasq_inv_ * y_minus_mu_[i] * y_minus_mu_[i];
+        double sigma_inv = 1.0 / sigma_->val_;
+        double sigmasq_inv = sigma_inv * sigma_inv;
+        double sigmacubed_inv = sigmasq_inv * sigma_inv;
+        double half_sigmasq_inv = 0.5 * sigmasq_inv;
+        double log_sigma = std::log(sigma_->val_);
+        for (unsigned int i = 0; i < y.size(); ++i) {
+          double y_minus_mu = y[i] - mu->val_;
+          double y_minus_mu_sq = y_minus_mu * y_minus_mu;
+          val -= half_sigmasq_inv * y_minus_mu_sq;
+          sigma_adj2_ += sigmacubed_inv * y_minus_mu_sq;
+          mu_adj2_ += sigmasq_inv * y_minus_mu;
         }
-        val -= y.size() * (log_sigma_ - NEG_LOG_SQRT_TWO_PI);
+        val -= y.size() * (log_sigma - NEG_LOG_SQRT_TWO_PI);
+        sigma_adj2_ -= y.size() * sigma_inv;
       }
 
       void chain() {
-        double sigmacubed_inv2 = 2 * sigmasq_inv_ / sigma_->val_;
-        for (unsigned int i = 0; i < y_minus_mu_.size(); i++) {
-          mu_->adj_ += adj_ * y_minus_mu_[i] * sigmasq_inv_;
-          sigma_->adj_ += adj_ * 0.5 * y_minus_mu_[i] * y_minus_mu_[i] * 
-            sigmacubed_inv2;
-        }
-        sigma_->adj_ -= adj_ * y_minus_mu_.size() * 1.0 / sigma_->val_;
+        mu_->adj_ += adj_ * mu_adj2_;
+        sigma_->adj_ += adj_ * sigma_adj2_;
       }
     };
 
@@ -76,20 +83,23 @@ namespace stan {
     protected:
       agrad::vari* mu_;
       agrad::vari* sigma_;
-      double y_minus_mu_, y_minus_mu_sigmasq_inv_;
+      double mu_adj2_, sigma_adj2_;
     public:
       normal_log_dvv_vari(double y, agrad::vari* mu, agrad::vari* sigma) :
-        agrad::vari(0), mu_(mu), sigma_(sigma), 
-        y_minus_mu_(y-mu->val_),
-        y_minus_mu_sigmasq_inv_(y_minus_mu_ / (sigma_->val_ * sigma_->val_)) {
-        const_cast<double&>(val_) = -0.5 * y_minus_mu_sigmasq_inv_ * y_minus_mu_
+        agrad::vari(0), mu_(mu), sigma_(sigma), mu_adj2_(0), sigma_adj2_(0) {
+        double sigma_inv = 1.0 / sigma->val_;
+        double y_minus_mu = y - mu->val_;
+        double sigmasq_inv_y_minus_mu = sigma_inv * sigma_inv * y_minus_mu;
+        double sigmasq_inv_y_minus_mu_sq = sigmasq_inv_y_minus_mu * y_minus_mu;
+        const_cast<double&>(val_) = -0.5 * sigmasq_inv_y_minus_mu_sq
           - log(sigma->val_) - NEG_LOG_SQRT_TWO_PI;
+        mu_adj2_ = sigmasq_inv_y_minus_mu;
+        sigma_adj2_ = (sigmasq_inv_y_minus_mu_sq - 1) * sigma_inv;
       }
       
       void chain() {
-        mu_->adj_ += adj_ * y_minus_mu_sigmasq_inv_;
-        sigma_->adj_ += adj_ * (y_minus_mu_sigmasq_inv_ * y_minus_mu_ / 
-                                sigma_->val_ - 1.0 / sigma_->val_);
+        mu_->adj_ += adj_ * mu_adj2_;
+        sigma_->adj_ += adj_ * sigma_adj2_;
       }
     };
 
