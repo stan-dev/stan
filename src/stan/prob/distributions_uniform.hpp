1e945e7223d058b855598ad1d52e66456d6cab82
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2011-11-29 20:00:15 +0000

    policies for distributions
    git-svn-id: https://stan.googlecode.com/svn/trunk@210 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

---
+++ src/stan/prob/distributions_uniform.hpp
@@ -0,0 +1,167 @@
+#ifndef __STAN__PROB__DISTRIBUTIONS_UNIFORM_HPP__
+#define __STAN__PROB__DISTRIBUTIONS_UNIFORM_HPP__
+
+#include <boost/math/constants/constants.hpp>
+#include <boost/math/special_functions.hpp>
+#include <boost/math/tools/promotion.hpp>
+#include <boost/math/distributions/detail/common_error_handling.hpp>
+#include <boost/math/policies/error_handling.hpp>
+#include <boost/math/policies/policy.hpp>
+
+#include "stan/prob/transform.hpp"
+#include "stan/prob/distributions_error_handling.hpp"
+#include "stan/prob/distributions_constants.hpp"
+
+
+
+namespace stan {
+  namespace prob {
+    using namespace std;
+    using namespace stan::maths;
+
+    // CONTINUOUS, UNIVARIATE DENSITIES
+    /**
+     * The log of a uniform density for the given 
+     * y, lower, and upper bound. 
+     *
+     \f{eqnarray*}{
+     y &\sim& \mbox{\sf{U}}(\alpha, \beta) \\
+     \log (p (y \,|\, \alpha, \beta)) &=& \log \left( \frac{1}{\beta-\alpha} \right) \\
+     &=& \log (1) - \log (\beta - \alpha) \\
+     &=& -\log (\beta - \alpha) \\
+     & & \mathrm{ where } \; y \in [\alpha, \beta], \log(0) \; \mathrm{otherwise}
+     \f}
+     * 
+     * @param y A scalar variable.
+     * @param alpha Lower bound.
+     * @param beta Upper bound.
+     * @throw std::invalid_argument if the lower bound is greater than 
+     *    or equal to the lower bound
+     * @tparam T_y Type of scalar.
+     * @tparam T_low Type of lower bound.
+     * @tparam T_high Type of upper bound.
+     */
+    template <typename T_y, typename T_low, typename T_high, class Policy>
+    inline typename boost::math::tools::promote_args<T_y,T_low,T_high>::type
+    uniform_log(const T_y& y, const T_low& alpha, const T_high& beta, const Policy& /* pol */) {
+      static const char* function = "stan::prob::uniform_log<%1%>(%1%)";
+      
+      typename boost::math::tools::promote_args<T_y,T_low,T_high>::type result;
+      if(false == stan::prob::check_x(function, y, &result, Policy()))
+	return result;
+      if(false == stan::prob::check_bounds(function, alpha, beta, &result, Policy()))
+	return result;
+      
+      if (y < alpha || y > beta)
+	return LOG_ZERO;
+      return -log(beta - alpha);
+    }
+      
+
+    /**
+     * The log of a uniform density for the given 
+     * y, lower, and upper bound. 
+     *
+     \f{eqnarray*}{
+     y &\sim& \mbox{\sf{U}}(\alpha, \beta) \\
+     \log (p (y \,|\, \alpha, \beta)) &=& \log \left( \frac{1}{\beta-\alpha} \right) \\
+     &=& \log (1) - \log (\beta - \alpha) \\
+     &=& -\log (\beta - \alpha) \\
+     & & \mathrm{ where } \; y \in [\alpha, \beta], \log(0) \; \mathrm{otherwise}
+     \f}
+     * 
+     * @param y A scalar variable.
+     * @param alpha Lower bound.
+     * @param beta Upper bound.
+     * @throw std::invalid_argument if the lower bound is greater than 
+     *    or equal to the lower bound
+     * @tparam T_y Type of scalar.
+     * @tparam T_low Type of lower bound.
+     * @tparam T_high Type of upper bound.
+     */
+    template <typename T_y, typename T_low, typename T_high>
+    inline typename boost::math::tools::promote_args<T_y,T_low,T_high>::type
+    uniform_log(const T_y& y, const T_low& alpha, const T_high& beta) {
+      return uniform_log (y, alpha, beta, boost::math::policies::policy<>());
+    }
+
+    /**
+     * The log of a density proportional to a uniform density for the given 
+     * y, lower, and upper bound. 
+     *
+     * @param y A scalar variable.
+     * @param alpha Lower bound.
+     * @param beta Upper bound.
+     * @throw std::invalid_argument if the lower bound is greater than 
+     *    or equal to the lower bound
+     * @tparam T_y Type of scalar.
+     * @tparam T_low Type of lower bound.
+     * @tparam T_high Type of upper bound.
+     */
+    template <typename T_y, typename T_low, typename T_high, class Policy>
+    inline typename boost::math::tools::promote_args<T_y,T_low,T_high>::type
+    uniform_propto_log(const T_y& y, const T_low& alpha, const T_high& beta, const Policy& /* pol */) {
+      return uniform_log (y, alpha, beta, Policy());
+    }
+    /**
+     * The log of a density proportional to a uniform density for the given 
+     * y, lower, and upper bound. 
+     *
+     * @param y A scalar variable.
+     * @param alpha Lower bound.
+     * @param beta Upper bound.
+     * @throw std::invalid_argument if the lower bound is greater than 
+     *    or equal to the lower bound
+     * @tparam T_y Type of scalar.
+     * @tparam T_low Type of lower bound.
+     * @tparam T_high Type of upper bound.
+     */
+    template <typename T_y, typename T_low, typename T_high, class Policy>
+    inline typename boost::math::tools::promote_args<T_y,T_low,T_high>::type
+    uniform_propto_log(const T_y& y, const T_low& alpha, const T_high& beta) {
+      return uniform_log (y, alpha, beta, boost::math::policies::policy<>());
+    }
+
+    /**
+     * The log of a density proportional to a uniform density for the given 
+     * y, lower, and upper bound. 
+     *
+     * @param lp The log probability to increment.
+     * @param y A scalar variable.
+     * @param alpha Lower bound.
+     * @param beta Upper bound.
+     * @throw std::invalid_argument if the lower bound is greater than 
+     *    or equal to the lower bound
+     * @tparam T_y Type of scalar.
+     * @tparam T_low Type of lower bound.
+     * @tparam T_high Type of upper bound.
+     */
+    template <typename T_y, typename T_low, typename T_high, class Policy>
+    inline void
+    uniform_propto_log(stan::agrad::var& lp, const T_y& y, const T_low& alpha, const T_high& beta, const Policy& /* pol */) {
+      lp += uniform_propto_log (y, alpha, beta, Policy());
+    }
+
+    /**
+     * The log of a density proportional to a uniform density for the given 
+     * y, lower, and upper bound. 
+     *
+     * @param lp The log probability to increment.
+     * @param y A scalar variable.
+     * @param alpha Lower bound.
+     * @param beta Upper bound.
+     * @throw std::invalid_argument if the lower bound is greater than 
+     *    or equal to the lower bound
+     * @tparam T_y Type of scalar.
+     * @tparam T_low Type of lower bound.
+     * @tparam T_high Type of upper bound.
+     */
+    template <typename T_y, typename T_low, typename T_high, class Policy>
+    inline void
+    uniform_propto_log(stan::agrad::var& lp, const T_y& y, const T_low& alpha, const T_high& beta) {
+      lp += uniform_propto_log (y, alpha, beta, boost::math::policies::policy<>());
+    }
+
+  }}
+
+#endif
---
+++ src/test/prob/distributions_uniform_test.cpp
@@ -0,0 +1,28 @@
+// These tests should not have reference to stan::agrad::var. Distribution tests 
+// with stan::agrad::var should be placed in src/test/agrad/distributions_test.cpp
+
+#include <cmath>
+#include <gtest/gtest.h>
+#include <Eigen/Dense>
+#include "stan/prob/distributions_uniform.hpp"
+
+TEST(distributions,Uniform) {
+  EXPECT_FLOAT_EQ(1.0, exp(stan::prob::uniform_log(0.2,0.0,1.0)));
+  EXPECT_FLOAT_EQ(2.0, exp(stan::prob::uniform_log(0.2,-0.25,0.25)));
+  EXPECT_FLOAT_EQ(0.1, exp(stan::prob::uniform_log(101.0,100.0,110.0)));
+  // lower boundary
+  EXPECT_FLOAT_EQ(1.0, exp(stan::prob::uniform_log(0.0,0.0,1.0)));
+  EXPECT_FLOAT_EQ(0.0, exp(stan::prob::uniform_log(-1.0,0.0,1.0)));
+  // upper boundary
+  EXPECT_FLOAT_EQ(1.0, exp(stan::prob::uniform_log(1.0,0.0,1.0)));
+  EXPECT_FLOAT_EQ(0.0, exp(stan::prob::uniform_log(2.0,0.0,1.0)));  
+}
+TEST(distributions,UniformDefaultPolicy) {
+  // lower bound higher than the upper bound
+  EXPECT_THROW (stan::prob::uniform_log(0.0,1.0,0.0), std::domain_error);
+  // lower and upper boundary the same
+  EXPECT_THROW (stan::prob::uniform_log(0.0, 0.0, 0.0), std::domain_error);
+  EXPECT_THROW (stan::prob::uniform_log(1.0, 0.0, 0.0), std::domain_error);
+  EXPECT_THROW (stan::prob::uniform_log(-1.0, 0.0, 0.0), std::domain_error);
+}
+
