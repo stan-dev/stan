4d4220b04b5b91e9f6f6189a1ff2593c4c1898fc
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2011-12-06 19:01:49 +0000

    distributions: inv_wishart
    git-svn-id: https://stan.googlecode.com/svn/trunk@252 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

---
+++ src/test/prob/distributions_inv_wishart_test.cpp
@@ -0,0 +1,25 @@
+// These tests should not have reference to stan::agrad::var. Distribution tests 
+// with stan::agrad::var should be placed in src/test/agrad/distributions_test.cpp
+
+#include <gtest/gtest.h>
+#include "stan/prob/distributions_inv_wishart.hpp"
+
+using Eigen::Dynamic;
+using Eigen::Matrix;
+
+TEST(distributions,InvWishart) {
+  Matrix<double,Dynamic,Dynamic> Y(3,3);
+  Y <<  12.147233, -11.9036079, 1.0910458,
+    -11.903608,  16.7585782, 0.8530256,
+    1.091046,   0.8530256, 2.5786609;
+
+  Matrix<double,Dynamic,Dynamic> Sigma(3,3);
+  Sigma << 7.785215,  3.0597878,  1.1071663,
+    3.059788, 10.3515035, -0.1232598,
+    1.107166, -0.1232598,  7.7623386;
+  
+  double dof = 4.0;
+  double log_p = log(2.008407e-08);
+
+  EXPECT_NEAR(log_p, stan::prob::inv_wishart_log(Y,dof,Sigma), 0.01);
+}
--- src/test/prob/distributions_test.cpp
+++ src/test/prob/distributions_test.cpp
@@ -234,20 +234,3 @@ TEST(prob_prob,multinomial) {
   theta << 0.2, 0.3, 0.5;
   EXPECT_FLOAT_EQ(-2.002481, stan::prob::multinomial_log(ns,theta));
 }
-
-TEST(prob_prob,iwishart) {
-  Matrix<double,Dynamic,Dynamic> Y(3,3);
-  Y <<  12.147233, -11.9036079, 1.0910458,
-    -11.903608,  16.7585782, 0.8530256,
-    1.091046,   0.8530256, 2.5786609;
-
-  Matrix<double,Dynamic,Dynamic> Sigma(3,3);
-  Sigma << 7.785215,  3.0597878,  1.1071663,
-    3.059788, 10.3515035, -0.1232598,
-    1.107166, -0.1232598,  7.7623386;
-  
-  double dof = 4.0;
-  double log_p = log(2.008407e-08);
-
-  EXPECT_NEAR(log_p, stan::prob::inv_wishart_log(Y,dof,Sigma), 0.01);
-}
