bc5d5100b09bb69f558105a503f23b1c363aec16
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2011-12-07 18:27:33 +0000

    distributions: cleaned test names
    git-svn-id: https://stan.googlecode.com/svn/trunk@276 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/test/prob/distributions_cauchy_test.cpp
+++ src/test/prob/distributions_cauchy_test.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 #include "stan/prob/distributions_cauchy.hpp"
 
-TEST(prob_prob,cauchy) {
+TEST(distributions,Cauchy) {
   EXPECT_FLOAT_EQ(-1.837877, stan::prob::cauchy_log(1.0, 0.0, 1.0));
   EXPECT_FLOAT_EQ(-2.323385, stan::prob::cauchy_log(-1.5, 0.0, 1.0));
   EXPECT_FLOAT_EQ(-2.323385, stan::prob::cauchy_log(-2.5, -1.0, 1.0));
--- src/test/prob/distributions_neg_binomial_test.cpp
+++ src/test/prob/distributions_neg_binomial_test.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 #include "stan/prob/distributions_neg_binomial.hpp"
 
-TEST(prob_prob,neg_binomial) {
+TEST(distributions,NegBinomial) {
   EXPECT_FLOAT_EQ(-7.786663, stan::prob::neg_binomial_log(10,2.0,1.5));
   EXPECT_FLOAT_EQ(-142.6147, stan::prob::neg_binomial_log(100,3.0,3.5));
 }
