548770acafe573a1b25eab8f24ad4f32e6d71336
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2011-12-09 04:50:37 +0000

    cleaned up test code
    git-svn-id: https://stan.googlecode.com/svn/trunk@302 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/test/prob/distributions_exponential_test.cpp
+++ src/test/prob/distributions_exponential_test.cpp
@@ -1,7 +1,3 @@
-// These tests should not have reference to stan::agrad::var. Distribution tests 
-// with stan::agrad::var should be placed in src/test/agrad/distributions_test.cpp
-
-#include <cmath>
 #include <gtest/gtest.h>
 #include "stan/prob/distributions_exponential.hpp"
 
@@ -10,7 +6,6 @@ TEST(ProbDistributions,Exponential) {
   EXPECT_FLOAT_EQ(-57.13902, stan::prob::exponential_log(15.0,3.9));
 }
 
-
 TEST(ProbDistributionsCumulative,Exponential) {
   // values from R
   EXPECT_FLOAT_EQ(0.95021293, stan::prob::exponential_p(2.0,1.5));
@@ -18,7 +13,6 @@ TEST(ProbDistributionsCumulative,Exponential) {
   EXPECT_FLOAT_EQ(1.0, stan::prob::exponential_p(15.0,3.9));
   EXPECT_FLOAT_EQ(0.62280765, stan::prob::exponential_p(0.25,3.9));
 }
-
 
 TEST(ProbDistributionsTruncated,ExponentialLowHigh) {
   double x, beta, low, high;
@@ -149,11 +143,6 @@ TEST(ProbDistributionsTruncated,ExponentialLowHighDefaultPolicyLowHigh) {
     << "low > high should throw an exception";
   
 }  
-
-
-
-
-
 
 TEST(ProbDistributionsTruncated,ExponentialLow) {
   double x, beta, low;
