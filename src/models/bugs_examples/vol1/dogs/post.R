550cfc6ff460acc46b4fe1a01bb431c9cb5b24db
guojq28@gmail.com <guojq28@gmail.com@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2011-12-12 15:22:38 +0000

    vol 1 example Dogs (works)
    git-svn-id: https://stan.googlecode.com/svn/trunk@307 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

---
+++ src/models/bugs_examples/vol1/dogs/post.R
@@ -0,0 +1,27 @@
+## take a look at the samples and compare with results computed 
+## in other program. 
+
+library(coda) 
+post <- read.csv(file = "samples.csv", header = FALSE)
+
+colnames(post) <- c("alpha", "beta")  
+
+summary(as.mcmc(post)) 
+
+# run in JAGS 
+library(BUGSExamples)
+# BUGSExamples package is installed by 
+# install.packages("BUGSExamples", repos = "http://R-Forge.R-project.org")
+
+if (!is.element("BUGSExamples", installed.packages()[, 1])) {
+    cat("Package BUGSExamples is not installed. It is used to get", 
+        "the results from JAGS.", sep = '\n')
+    quit();
+} 
+
+ex <- list(name = "Dogs", parameters = c("alpha", "beta"), 
+           nSample = 2000, nBurnin = 500, nThin = 1, 
+           nChain = 3)
+
+jagspost <- runExample(ex, engine = 'JAGS') 
+summary(jagspost$coda) 
