721213922b4c0226c8c4154997a6c29c754ba3b9
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2011-12-14 03:20:29 +0000

    FIXED issue #23.
    git-svn-id: https://stan.googlecode.com/svn/trunk@321 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/stan/prob/distributions_chi_square.hpp
+++ src/stan/prob/distributions_chi_square.hpp
@@ -1,20 +1,15 @@
 #ifndef __STAN__PROB__DISTRIBUTIONS_CHI_SQUARE_HPP__
 #define __STAN__PROB__DISTRIBUTIONS_CHI_SQUARE_HPP__
 
-#include <boost/math/constants/constants.hpp>
-#include <boost/math/special_functions.hpp>
-#include <boost/math/tools/promotion.hpp>
-#include <boost/math/distributions/detail/common_error_handling.hpp>
-#include <boost/math/policies/error_handling.hpp>
-#include <boost/math/policies/policy.hpp>
-
-#include "stan/prob/transform.hpp"
 #include "stan/prob/distributions_error_handling.hpp"
 #include "stan/prob/distributions_constants.hpp"
 
+#include <stan/meta/traits.hpp>
 
 namespace stan {
   namespace prob {
+    using boost::math::tools::promote_args;
+    using boost::math::policies::policy;
 
     /**
      * The log of a chi-squared density for y with the specified
@@ -35,84 +30,35 @@ namespace stan {
      * @tparam T_y Type of scalar.
      * @tparam T_dof Type of degrees of freedom.
      */
-    template <typename T_y, typename T_dof, class Policy>
+    template <bool propto = false, 
+	      typename T_y, typename T_dof, 
+	      class Policy = policy<> >
     inline typename boost::math::tools::promote_args<T_y,T_dof>::type
-    chi_square_log(const T_y& y, const T_dof& nu, const Policy& /* pol */) {
+    chi_square_log(const T_y& y, const T_dof& nu, const Policy& = Policy()) {
       static const char* function = "stan::prob::chi_square_log<%1%>(%1%)";
 
       double result;
       if (!stan::prob::check_positive (function, nu, "Degrees of freedom", &result, Policy()))
 	return result;
-      if (!stan::prob::check_nonnegative (function, y, "Random variate y", &result, Policy()))
+      if (!stan::prob::check_x (function, y, &result, Policy()))
 	return result;
-      return - lgamma(0.5 * nu)
-	+ nu * NEG_LOG_TWO_OVER_TWO
-	+ (0.5 * nu - 1.0) * log(y)
-	- 0.5 * y;
+      
+      if (y < 0)
+	return LOG_ZERO;
+      
+      typename promote_args<T_y,T_dof>::type lp(0.0);
+      
+      if (!propto)
+	lp += nu * NEG_LOG_TWO_OVER_TWO - lgamma(0.5 * nu);
+      if (!propto
+	  || !stan::is_constant<T_y>::value
+	  || !stan::is_constant<T_dof>::value)
+	lp += (0.5 * nu - 1.0) * log(y);
+      if (!propto
+	  || !stan::is_constant<T_y>::value)
+	lp -= 0.5 * y;
+      return lp;
     }
-
-    /**
-     * The log of a chi-squared density for y with the specified
-     * degrees of freedom parameter.
-     * The degrees of freedom prarameter must be greater than 0.
-     * y must be greater than or equal to 0.
-     * 
-     \f{eqnarray*}{
-       y &\sim& \chi^2_\nu \\
-       \log (p (y \,|\, \nu)) &=& \log \left( \frac{2^{-\nu / 2}}{\Gamma (\nu / 2)} y^{\nu / 2 - 1} \exp^{- y / 2} \right) \\
-       &=& - \frac{\nu}{2} \log(2) - \log (\Gamma (\nu / 2)) + (\frac{\nu}{2} - 1) \log(y) - \frac{y}{2} \\
-       & & \mathrm{ where } \; y \ge 0
-     \f}
-     * @param y A scalar variable.
-     * @param nu Degrees of freedom.
-     * @throw std::domain_error if nu is not greater than or equal to 0
-     * @throw std::domain_error if y is not greater than or equal to 0.
-     * @tparam T_y Type of scalar.
-     * @tparam T_dof Type of degrees of freedom.
-     */
-    template <typename T_y, typename T_dof>
-    inline typename boost::math::tools::promote_args<T_y,T_dof>::type
-    chi_square_log(const T_y& y, const T_dof& nu) {
-      return chi_square_log (y, nu, boost::math::policies::policy<>());
-    }
-
-    /**
-     * The log of a distribution proportional to a chi-squared density for y with the specified
-     * degrees of freedom parameter.
-     * The degrees of freedom prarameter must be greater than 0.
-     * y must be greater than or equal to 0.
-     *
-     * @param y A scalar variable.
-     * @param nu Degrees of freedom.
-     * @throw std::domain_error if nu is not greater than or equal to 0
-     * @throw std::domain_error if y is not greater than or equal to 0.
-     * @tparam T_y Type of scalar.
-     * @tparam T_dof Type of degrees of freedom.
-     */
-    template <typename T_y, typename T_dof, class Policy>
-    inline typename boost::math::tools::promote_args<T_y,T_dof>::type
-    chi_square_propto_log(const T_y& y, const T_dof& nu, const Policy& /* pol */) {
-      return chi_square_log(y, nu, Policy());
-    } 
-
-    /**
-     * The log of a distribution proportional to a chi-squared density for y with the specified
-     * degrees of freedom parameter.
-     * The degrees of freedom prarameter must be greater than 0.
-     * y must be greater than or equal to 0.
-     *
-     * @param y A scalar variable.
-     * @param nu Degrees of freedom.
-     * @throw std::domain_error if nu is not greater than or equal to 0
-     * @throw std::domain_error if y is not greater than or equal to 0.
-     * @tparam T_y Type of scalar.
-     * @tparam T_dof Type of degrees of freedom.
-     */
-    template <typename T_y, typename T_dof>
-    inline typename boost::math::tools::promote_args<T_y,T_dof>::type
-    chi_square_propto_log(const T_y& y, const T_dof& nu) {
-      return chi_square_propto_log(y, nu, boost::math::policies::policy<>());
-    } 
 
   }
 }
--- src/test/prob/distributions_chi_square_test.cpp
+++ src/test/prob/distributions_chi_square_test.cpp
@@ -1,18 +1,54 @@
 // These tests should not have reference to stan::agrad::var. Distribution tests 
 // with stan::agrad::var should be placed in src/test/agrad/distributions_test.cpp
 
-#include <cmath>
 #include <gtest/gtest.h>
 #include "stan/prob/distributions_chi_square.hpp"
+
+using boost::math::policies::policy;
+using boost::math::policies::evaluation_error;
+using boost::math::policies::domain_error;
+using boost::math::policies::overflow_error;
+using boost::math::policies::domain_error;
+using boost::math::policies::pole_error;
+using boost::math::policies::errno_on_error;
+
+typedef policy<
+  domain_error<errno_on_error>, 
+  pole_error<errno_on_error>,
+  overflow_error<errno_on_error>,
+  evaluation_error<errno_on_error> 
+  > errno_policy;
+
 
 TEST(ProbDistributions,ChiSquare) {
   EXPECT_FLOAT_EQ(-3.835507, stan::prob::chi_square_log(7.9,3.0));
   EXPECT_FLOAT_EQ(-2.8927, stan::prob::chi_square_log(1.9,0.5));
+  EXPECT_FLOAT_EQ(-std::numeric_limits<double>::infinity(), 
+		  stan::prob::chi_square_log(-1.0,0.5));
+}
+TEST(ProbDistributions,ChiSquarePropto) {
+  EXPECT_FLOAT_EQ(0.0, stan::prob::chi_square_log<true>(7.9,3.0));
+  EXPECT_FLOAT_EQ(0.0, stan::prob::chi_square_log<true>(1.9,0.5));
+  EXPECT_FLOAT_EQ(-std::numeric_limits<double>::infinity(), 
+		  stan::prob::chi_square_log<true>(-1.0,0.5));
 }
 TEST(ProbDistributions,ChiSquareDefaultPolicy) {
   double y = 0.0;
   double nu = 0.0;
+  EXPECT_NO_THROW (stan::prob::chi_square_log(1.0, 1.0));
   EXPECT_THROW(stan::prob::chi_square_log(y, nu), std::domain_error);
   EXPECT_THROW(stan::prob::chi_square_log(y, -1), std::domain_error);
   EXPECT_THROW(stan::prob::chi_square_log(-1, nu), std::domain_error);
+}
+TEST(ProbDistributions,ChiSquareErrnoPolicy) {
+  double result;
+  double y = 0.0;
+  double nu = 0.0;
+  
+  result = stan::prob::chi_square_log(y, nu, errno_policy());
+  EXPECT_TRUE (std::isnan(result));
+  result = stan::prob::chi_square_log(y, -1, errno_policy());
+  EXPECT_TRUE (std::isnan(result));
+  result = stan::prob::chi_square_log(-1, nu, errno_policy());  
+  EXPECT_TRUE (std::isnan(result));
 }
