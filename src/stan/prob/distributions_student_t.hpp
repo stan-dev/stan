ff90d8ead3719472bc265a31cd155504c21657c7
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2011-12-16 01:33:04 +0000

    distributions: student t. FIXED issue #29.
    git-svn-id: https://stan.googlecode.com/svn/trunk@333 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/stan/prob/distributions_student_t.hpp
+++ src/stan/prob/distributions_student_t.hpp
@@ -1,21 +1,16 @@
 #ifndef __STAN__PROB__DISTRIBUTIONS_STUDENT_T_HPP__
 #define __STAN__PROB__DISTRIBUTIONS_STUDENT_T_HPP__
 
-#include <boost/math/constants/constants.hpp>
-#include <boost/math/special_functions.hpp>
-#include <boost/math/tools/promotion.hpp>
-#include <boost/math/distributions/detail/common_error_handling.hpp>
-#include <boost/math/policies/error_handling.hpp>
-#include <boost/math/policies/policy.hpp>
-
-#include "stan/prob/transform.hpp"
 #include "stan/prob/distributions_error_handling.hpp"
 #include "stan/prob/distributions_constants.hpp"
 
+#include <stan/meta/traits.hpp>
+
 namespace stan {
   namespace prob {
-    using namespace std;
-    using namespace stan::maths;
+
+    using boost::math::tools::promote_args;
+    using boost::math::policies::policy;
 
     // StudentT(y|nu,mu,sigma)  [nu > 0;   sigma > 0]
     /**
@@ -43,13 +38,14 @@ namespace stan {
      * @tparam T_loc Type of location.
      * @tparam T_scale Type of scale.
      */
-    template <typename T_y, 
+    template <bool propto = false,
+	      typename T_y, 
 	      typename T_dof, 
 	      typename T_loc, 
 	      typename T_scale,
-	      class Policy>
-    inline typename boost::math::tools::promote_args<T_y,T_dof,T_loc,T_scale>::type
-    student_t_log(const T_y& y, const T_dof& nu, const T_loc& mu, const T_scale& sigma, const Policy& /* pol */) {
+	      class Policy = policy<> >
+    inline typename promote_args<T_y,T_dof,T_loc,T_scale>::type
+      student_t_log(const T_y& y, const T_dof& nu, const T_loc& mu, const T_scale& sigma, const Policy& = Policy()) {
       static const char* function = "stan::prob::student_t_log<%1%>(%1%)";
 
       double result;
@@ -62,99 +58,23 @@ namespace stan {
       if(!stan::prob::check_x(function, y, &result, Policy()))
 	return result;
 
-      return lgamma((nu + 1.0) / 2.0)
-	- lgamma(nu / 2.0)
-	- 0.5 * log(nu)
-	+ NEG_LOG_SQRT_PI
-	- log(sigma)
-	- ((nu + 1.0) / 2.0) * log(1.0 + (((y - mu) / sigma) * ((y - mu) / sigma)) / nu);
+      typename promote_args<T_y,T_loc,T_scale>::type lp(0.0);
+      if (!propto)
+	lp += lgamma ( (nu + 1.0) / 2.0) - lgamma (nu / 2.0) + NEG_LOG_SQRT_PI;
+      if (!propto
+	  || !is_constant<T_dof>::value)
+	lp -= 0.5 * log(nu);
+      if (!propto
+	  || !is_constant<T_scale>::value)
+	lp -= log(sigma);
+      if (!propto
+	  || !is_constant<T_y>::value
+	  || !is_constant<T_dof>::value
+	  || !is_constant<T_scale>::value)
+	lp -= ((nu + 1.0) / 2.0) * log(1.0 + (((y - mu) / sigma) * ((y - mu) / sigma)) / nu);
+      return lp;
     }
-
-    /**
-     * The log of the Student-t density for the given y, nu, mean, and
-     * scale parameter.  The scale parameter must be greater
-     * than 0.
-     *
-     * \f{eqnarray*}{
-       y &\sim& t_{\nu} (\mu, \sigma^2) \\
-       \log (p (y \,|\, \nu, \mu, \sigma) ) &=& \log \left( \frac{\Gamma((\nu + 1) /2)}
-           {\Gamma(\nu/2)\sqrt{\nu \pi} \sigma} \left( 1 + \frac{1}{\nu} (\frac{y - \mu}{\sigma})^2 \right)^{-(\nu + 1)/2} \right) \\
-       &=& \log( \Gamma( (\nu+1)/2 )) - \log (\Gamma (\nu/2) - \frac{1}{2} \log(\nu \pi) - \log(\sigma)
-           -\frac{\nu + 1}{2} \log (1 + \frac{1}{\nu} (\frac{y - \mu}{\sigma})^2)
-     \f}
-     * 
-     * @param y A scalar variable.
-     * @param nu Degrees of freedom.
-     * @param mu The mean of the Student-t distribution.
-     * @param sigma The scale parameter of the Student-t distribution.
-     * @return The log of the Student-t density at y.
-     * @throw std::domain_error if sigma is not greater than 0.
-     * @throw std::domain_error if nu is not greater than 0.
-     * @tparam T_y Type of scalar.
-     * @tparam T_dof Type of degrees of freedom.
-     * @tparam T_loc Type of location.
-     * @tparam T_scale Type of scale.
-     */
-    template <typename T_y, 
-	      typename T_dof, 
-	      typename T_loc, 
-	      typename T_scale>
-    inline typename boost::math::tools::promote_args<T_y,T_dof,T_loc,T_scale>::type
-    student_t_log(const T_y& y, const T_dof& nu, const T_loc& mu, const T_scale& sigma) {
-      return student_t_log (y, nu, mu, sigma, boost::math::policies::policy<>());
-    }
-
-    /**
-     * The log of a density proportional to the Student-t density for the given y, nu,
-     * mean, and scale parameter.  The scale parameter must be greater than 0.
-     * 
-     * @param y A scalar variable.
-     * @param nu Degrees of freedom.
-     * @param mu The mean of the Student-t distribution.
-     * @param sigma The scale parameter of the Student-t distribution.
-     * @return The log of the Student-t density at y.
-     * @throw std::domain_error if sigma is not greater than 0.
-     * @throw std::domain_error if nu is not greater than 0.
-     * @tparam T_y Type of scalar.
-     * @tparam T_dof Type of degrees of freedom.
-     * @tparam T_loc Type of location.
-     * @tparam T_scale Type of scale.
-     */
-    template <typename T_y, 
-	      typename T_dof, 
-	      typename T_loc, 
-	      typename T_scale,
-	      class Policy>
-    inline typename boost::math::tools::promote_args<T_y,T_dof,T_loc,T_scale>::type
-    student_t_propto_log(const T_y& y, const T_dof& nu, const T_loc& mu, const T_scale& sigma, const Policy& /* pol */) {
-      return student_t_log (y, nu, mu, sigma, Policy());
-    }
-
-     /**
-     * The log of a density proportional to the Student-t density for the given y, nu,
-     * mean, and scale parameter.  The scale parameter must be greater than 0.
-     * 
-     * @param y A scalar variable.
-     * @param nu Degrees of freedom.
-     * @param mu The mean of the Student-t distribution.
-     * @param sigma The scale parameter of the Student-t distribution.
-     * @return The log of the Student-t density at y.
-     * @throw std::domain_error if sigma is not greater than 0.
-     * @throw std::domain_error if nu is not greater than 0.
-     * @tparam T_y Type of scalar.
-     * @tparam T_dof Type of degrees of freedom.
-     * @tparam T_loc Type of location.
-     * @tparam T_scale Type of scale.
-     */
-    template <typename T_y, 
-	      typename T_dof, 
-	      typename T_loc, 
-	      typename T_scale>
-    inline typename boost::math::tools::promote_args<T_y,T_dof,T_loc,T_scale>::type
-    student_t_propto_log(const T_y& y, const T_dof& nu, const T_loc& mu, const T_scale& sigma) {
-      return student_t_propto_log (y, nu, mu, sigma, boost::math::policies::policy<>());
-    }
-
+    
   }
 }
 #endif
--- src/test/prob/distributions_student_t_test.cpp
+++ src/test/prob/distributions_student_t_test.cpp
@@ -7,3 +7,8 @@ TEST(ProbDistributions,StudentT) {
   EXPECT_FLOAT_EQ(-2.531024, stan::prob::student_t_log(2.0,1.0,0.0,2.0));
   // need test with scale != 1
 }
+TEST(ProbDistributions,StudentTPropto) {
+  EXPECT_FLOAT_EQ(0.0, stan::prob::student_t_log<true>(1.0,1.0,0.0,1.0));
+  EXPECT_FLOAT_EQ(0.0, stan::prob::student_t_log<true>(-3.0,2.0,0.0,1.0));
+  EXPECT_FLOAT_EQ(0.0, stan::prob::student_t_log<true>(2.0,1.0,0.0,2.0));
+}
