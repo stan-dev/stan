c719e45fec27bf29c631ad012a6084db79c05d1d
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2011-12-16 17:27:13 +0000

    distributions: hypergeometric. FIXED issue #46.
    git-svn-id: https://stan.googlecode.com/svn/trunk@351 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/test/prob/distributions_hypergeometric_test.cpp
+++ src/test/prob/distributions_hypergeometric_test.cpp
@@ -1,7 +1,12 @@
+
 #include <gtest/gtest.h>
 #include "stan/prob/distributions_hypergeometric.hpp"
 
 TEST(ProbDistributions,Hypergeometric) {
   EXPECT_FLOAT_EQ(-4.119424, stan::prob::hypergeometric_log(5,15,10,10));
   EXPECT_FLOAT_EQ(-2.302585, stan::prob::hypergeometric_log(0,2,3,2));
+}
+TEST(ProbDistributions,HypergeometricPropto) {
+  EXPECT_FLOAT_EQ(-4.119424, stan::prob::hypergeometric_log<true>(5,15,10,10));
+  EXPECT_FLOAT_EQ(-2.302585, stan::prob::hypergeometric_log<true>(0,2,3,2));
 }
