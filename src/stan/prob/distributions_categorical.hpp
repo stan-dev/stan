5c9dca65e1380c864888bdb39cd4332d2d26f1f0
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2011-12-16 16:41:45 +0000

    distributions: categorical. FIXED issue #41.
    git-svn-id: https://stan.googlecode.com/svn/trunk@346 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/stan/prob/distributions_categorical.hpp
+++ src/stan/prob/distributions_categorical.hpp
@@ -1,54 +1,30 @@
 #ifndef __STAN__PROB__DISTRIBUTIONS_CATEGORICAL_HPP__
 #define __STAN__PROB__DISTRIBUTIONS_CATEGORICAL_HPP__
 
-#include <boost/math/constants/constants.hpp>
-#include <boost/math/special_functions.hpp>
-#include <boost/math/tools/promotion.hpp>
-#include <boost/math/distributions/detail/common_error_handling.hpp>
-#include <boost/math/policies/error_handling.hpp>
-#include <boost/math/policies/policy.hpp>
-
-#include <Eigen/Dense>
-
-#include "stan/prob/transform.hpp"
 #include "stan/prob/distributions_error_handling.hpp"
 #include "stan/prob/distributions_constants.hpp"
 
-
+#include <stan/meta/traits.hpp>
 
 namespace stan {
   namespace prob {
-    using namespace std;
-    using namespace stan::maths;
+    using boost::math::tools::promote_args;
+    using boost::math::policies::policy;
 
     using Eigen::Dynamic;
     using Eigen::Matrix;
 
     // Categorical(n|theta)  [0 <= n < N;   0 <= theta[n] <= 1;  SUM theta = 1]
-    template <typename T_prob, class Policy>
-    inline typename boost::math::tools::promote_args<T_prob>::type
-    categorical_log(const unsigned int n, const Matrix<T_prob,Dynamic,1>& theta, const Policy& /* pol */) {
+    template <bool propto = false, 
+	      typename T_prob, 
+	      class Policy = policy<> >
+    inline typename promote_args<T_prob>::type
+      categorical_log(const unsigned int n, const Matrix<T_prob,Dynamic,1>& theta, const Policy& = Policy()) {
       // FIXME: domain checks
-      return log(theta(n));
-    }
-
-    template <typename T_prob>
-    inline typename boost::math::tools::promote_args<T_prob>::type
-    categorical_log(const unsigned int n, const Matrix<T_prob,Dynamic,1>& theta) {
-      return categorical_log (n, theta, boost::math::policies::policy<>());
-    }
-
-
-    template <typename T_prob, class Policy>
-    inline typename boost::math::tools::promote_args<T_prob>::type
-    categorical_propto_log(const unsigned int n, const Matrix<T_prob,Dynamic,1>& theta, const Policy& /* pol */) {
-      return categorical_log (n, theta, Policy());
-    }
-
-    template <typename T_prob>
-    inline typename boost::math::tools::promote_args<T_prob>::type
-    categorical_propto_log(const unsigned int n, const Matrix<T_prob,Dynamic,1>& theta) {
-      return categorical_propto_log (n, theta, boost::math::policies::policy<>());
+      if (!propto
+	  || !is_constant<T_prob>::value)
+	return log(theta(n));
+      return 0.0;
     }
 
   }
--- src/test/prob/distributions_categorical_test.cpp
+++ src/test/prob/distributions_categorical_test.cpp
@@ -1,5 +1,4 @@
 #include <gtest/gtest.h>
-#include <boost/exception/all.hpp>
 #include "stan/prob/distributions_categorical.hpp"
 
 using Eigen::Dynamic;
@@ -10,4 +9,10 @@ TEST(ProbDistributions,Categorical) {
   theta << 0.3, 0.5, 0.2;
   EXPECT_FLOAT_EQ(-1.203973, stan::prob::categorical_log(0,theta));
   EXPECT_FLOAT_EQ(-0.6931472, stan::prob::categorical_log(1,theta));
+}
+TEST(ProbDistributions,CategoricalPropto) {
+  Matrix<double,Dynamic,1> theta(3,1);
+  theta << 0.3, 0.5, 0.2;
+  EXPECT_FLOAT_EQ(0.0, stan::prob::categorical_log<true>(0,theta));
+  EXPECT_FLOAT_EQ(0.0, stan::prob::categorical_log<true>(1,theta));
 }
