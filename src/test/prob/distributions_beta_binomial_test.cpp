a2d8126d2b4e90cdd417e86b67a8cd795c85f91b
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2011-12-16 17:17:05 +0000

    distributions: beta_binomial. FIXED issue #45.
    git-svn-id: https://stan.googlecode.com/svn/trunk@350 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/test/prob/distributions_beta_binomial_test.cpp
+++ src/test/prob/distributions_beta_binomial_test.cpp
@@ -5,3 +5,7 @@ TEST(ProbDistributions,BetaBinomial) {
   EXPECT_FLOAT_EQ(-1.854007, stan::prob::beta_binomial_log(5,20,10.0,25.0));
   EXPECT_FLOAT_EQ(-4.376696, stan::prob::beta_binomial_log(25,100,30.0,50.0));
 }
+TEST(ProbDistributions,BetaBinomialPropto) {
+  EXPECT_FLOAT_EQ(0.0, stan::prob::beta_binomial_log<true>(5,20,10.0,25.0));
+  EXPECT_FLOAT_EQ(0.0, stan::prob::beta_binomial_log<true>(25,100,30.0,50.0));
+}
