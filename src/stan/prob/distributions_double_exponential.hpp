3ef3f1f9c37e4f37ed21e58ef0214f75f8ded4c9
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2011-12-16 15:03:27 +0000

    distributions: double_exponential. FIXED issue #34.
    git-svn-id: https://stan.googlecode.com/svn/trunk@340 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/stan/prob/distributions_double_exponential.hpp
+++ src/stan/prob/distributions_double_exponential.hpp
@@ -1,53 +1,40 @@
 #ifndef __STAN__PROB__DISTRIBUTIONS_DOUBLE_EXPONENTIAL_HPP__
 #define __STAN__PROB__DISTRIBUTIONS_DOUBLE_EXPONENTIAL_HPP__
 
-#include <boost/math/constants/constants.hpp>
-#include <boost/math/special_functions.hpp>
-#include <boost/math/tools/promotion.hpp>
-#include <boost/math/distributions/detail/common_error_handling.hpp>
-#include <boost/math/policies/error_handling.hpp>
-#include <boost/math/policies/policy.hpp>
-
-#include "stan/prob/transform.hpp"
 #include "stan/prob/distributions_error_handling.hpp"
 #include "stan/prob/distributions_constants.hpp"
 
+#include <stan/meta/traits.hpp>
+
 namespace stan {
   namespace prob {
-    using namespace std;
-    using namespace stan::maths;
+    using boost::math::tools::promote_args;
+    using boost::math::policies::policy;
 
     // DoubleExponential(y|mu,sigma)  [sigma > 0]
-    template <typename T_y, typename T_loc, typename T_scale, class Policy> 
-    inline typename boost::math::tools::promote_args<T_y,T_loc,T_scale>::type
-    double_exponential_log(const T_y& y, const T_loc& mu, const T_scale& sigma, const Policy& /* pol */) {
-      //static const char* function = "stan::prob::double_exponential_log<%1%>(%1%)";
+    template <bool propto = false,
+      typename T_y, typename T_loc, typename T_scale, 
+      class Policy = policy<> > 
+	inline typename promote_args<T_y,T_loc,T_scale>::type
+	double_exponential_log(const T_y& y, const T_loc& mu, const T_scale& sigma, const Policy& = Policy()) {
+	//static const char* function = "stan::prob::double_exponential_log<%1%>(%1%)";
 
       //double result;
       // FIXME: domain checks
-      return NEG_LOG_TWO
-	- log(sigma)
-	- abs(y - mu) / sigma;
+      typename promote_args<T_y,T_loc,T_scale>::type lp(0.0);
+      if (!propto)
+	lp += NEG_LOG_TWO;
+      if (!propto
+	  || !is_constant<T_scale>::value)
+	lp -= log(sigma);
+      if (!propto
+	  || !is_constant<T_y>::value
+	  || !is_constant<T_loc>::value
+	  || !is_constant<T_scale>::value)
+	lp -= abs(y - mu) / sigma;
+      return lp;
     }
     
-    template <typename T_y, typename T_loc, typename T_scale>
-    inline typename boost::math::tools::promote_args<T_y,T_loc,T_scale>::type
-    double_exponential_log(const T_y& y, const T_loc& mu, const T_scale& sigma) {
-      return double_exponential_log (y, mu, sigma, boost::math::policies::policy<>());
-    }
-
-    template <typename T_y, typename T_loc, typename T_scale, class Policy> 
-    inline typename boost::math::tools::promote_args<T_y,T_loc,T_scale>::type
-    double_exponential_propto_log(const T_y& y, const T_loc& mu, const T_scale& sigma, const Policy& /* pol */) {
-      return double_exponential_log (y, mu, sigma, Policy());
-    }
-    
-    template <typename T_y, typename T_loc, typename T_scale>
-    inline typename boost::math::tools::promote_args<T_y,T_loc,T_scale>::type
-    double_exponential_propto_log(const T_y& y, const T_loc& mu, const T_scale& sigma) {
-      return double_exponential_propto_log (y, mu, sigma, boost::math::policies::policy<>());
-    }
-
     
 
   }
--- src/test/prob/distributions_double_exponential_test.cpp
+++ src/test/prob/distributions_double_exponential_test.cpp
@@ -1,3 +1,4 @@
+
 #include <gtest/gtest.h>
 #include "stan/prob/distributions_double_exponential.hpp"
 
@@ -8,4 +9,12 @@ TEST(ProbDistributions,DoubleExponential) {
   EXPECT_FLOAT_EQ(-1.886294, stan::prob::double_exponential_log(1.0,0.0,2.0));
   EXPECT_FLOAT_EQ(-0.8, stan::prob::double_exponential_log(1.9,2.3,0.5));
   EXPECT_FLOAT_EQ(-0.9068528, stan::prob::double_exponential_log(1.9,2.3,0.25));
+}
+TEST(ProbDistributions,DoubleExponentialPropto) {
+  EXPECT_FLOAT_EQ(0.0, stan::prob::double_exponential_log<true>(1.0,1.0,1.0));
+  EXPECT_FLOAT_EQ(0.0, stan::prob::double_exponential_log<true>(2.0,1.0,1.0));
+  EXPECT_FLOAT_EQ(0.0, stan::prob::double_exponential_log<true>(-3.0,2.0,1.0));
+  EXPECT_FLOAT_EQ(0.0, stan::prob::double_exponential_log<true>(1.0,0.0,2.0));
+  EXPECT_FLOAT_EQ(0.0, stan::prob::double_exponential_log<true>(1.9,2.3,0.5));
+  EXPECT_FLOAT_EQ(0.0, stan::prob::double_exponential_log<true>(1.9,2.3,0.25));
 }
