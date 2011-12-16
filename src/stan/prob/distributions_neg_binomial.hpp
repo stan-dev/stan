14ba6fde8967dad9cf587c32dd845251d77f933b
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2011-12-16 17:02:21 +0000

    distributions: neg_binomial. FIXED issue #44.
    git-svn-id: https://stan.googlecode.com/svn/trunk@349 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/stan/prob/distributions_neg_binomial.hpp
+++ src/stan/prob/distributions_neg_binomial.hpp
@@ -1,50 +1,32 @@
 #ifndef __STAN__PROB__DISTRIBUTIONS_NEG_BINOMIAL_HPP__
 #define __STAN__PROB__DISTRIBUTIONS_NEG_BINOMIAL_HPP__
 
-#include <boost/math/constants/constants.hpp>
-#include <boost/math/special_functions.hpp>
-#include <boost/math/tools/promotion.hpp>
-#include <boost/math/distributions/detail/common_error_handling.hpp>
-#include <boost/math/policies/error_handling.hpp>
-#include <boost/math/policies/policy.hpp>
-
-#include "stan/prob/transform.hpp"
 #include "stan/prob/distributions_error_handling.hpp"
 #include "stan/prob/distributions_constants.hpp"
 
+#include <stan/meta/traits.hpp>
+
 namespace stan {
   namespace prob {
-    using namespace std;
-    using namespace stan::maths;
+    using boost::math::tools::promote_args;
+    using boost::math::policies::policy;
 
     // NegBinomial(n|alpha,beta)  [alpha > 0;  beta > 0;  n >= 0]
-    template <typename T_shape, typename T_inv_scale, class Policy>
-    inline typename boost::math::tools::promote_args<T_shape, T_inv_scale>::type
-    neg_binomial_log(const int n, const T_shape& alpha, const T_inv_scale& beta, const Policy& /* pol */) {
-      return maths::binomial_coefficient_log<T_shape>(n + alpha - 1.0, n)
-	+ alpha * log(beta / (beta + 1.0))
-	+ n * -log(beta + 1.0);
+    template <bool propto = false,
+	      typename T_shape, typename T_inv_scale, 
+	      class Policy = policy<> >
+    inline typename promote_args<T_shape, T_inv_scale>::type
+      neg_binomial_log(const int n, const T_shape& alpha, const T_inv_scale& beta, const Policy& = Policy()) {
+      // FIXME: domain checks
+      typename promote_args<T_shape, T_inv_scale>::type lp(0.0);
+      if (!propto)
+	lp += maths::binomial_coefficient_log<T_shape>(n + alpha - 1.0, n);
+      if (!propto
+	  || !is_constant<T_shape>::value
+	  || !is_constant<T_inv_scale>::value)
+	lp += alpha * log(beta) - (alpha + n) * log1p(beta);
+      return lp;
     }
-
-    template <typename T_shape, typename T_inv_scale>
-    inline typename boost::math::tools::promote_args<T_shape, T_inv_scale>::type
-    neg_binomial_log(const int n, const T_shape& alpha, const T_inv_scale& beta) {
-      return neg_binomial_log (n, alpha, beta, boost::math::policies::policy<>());
-    }
-
-    template <typename T_shape, typename T_inv_scale, class Policy>
-    inline typename boost::math::tools::promote_args<T_shape, T_inv_scale>::type
-    neg_binomial_propto_log(const int n, const T_shape& alpha, const T_inv_scale& beta, const Policy& /* pol */) {
-      return neg_binomial_propto_log (n, alpha, beta, Policy());
-    }
-
-    template <typename T_shape, typename T_inv_scale>
-    inline typename boost::math::tools::promote_args<T_shape, T_inv_scale>::type
-    neg_binomial_propto_log(const int n, const T_shape& alpha, const T_inv_scale& beta) {
-      return neg_binomial_propto_log (n, alpha, beta, boost::math::policies::policy<>());
-    }
-
-
 
   }
 }
--- src/test/prob/distributions_neg_binomial_test.cpp
+++ src/test/prob/distributions_neg_binomial_test.cpp
@@ -5,3 +5,7 @@ TEST(ProbDistributions,NegBinomial) {
   EXPECT_FLOAT_EQ(-7.786663, stan::prob::neg_binomial_log(10,2.0,1.5));
   EXPECT_FLOAT_EQ(-142.6147, stan::prob::neg_binomial_log(100,3.0,3.5));
 }
+TEST(ProbDistributions,NegBinomialPropto) {
+  EXPECT_FLOAT_EQ(0.0, stan::prob::neg_binomial_log<true>(10,2.0,1.5));
+  EXPECT_FLOAT_EQ(0.0, stan::prob::neg_binomial_log<true>(100,3.0,3.5));
+}
