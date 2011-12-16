c884c8f9acb14092c208d5793932e9a06625b3b3
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2011-12-16 01:42:44 +0000

    distributions: beta. FIXED issue #30.
    git-svn-id: https://stan.googlecode.com/svn/trunk@334 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/stan/prob/distributions_beta.hpp
+++ src/stan/prob/distributions_beta.hpp
@@ -1,21 +1,15 @@
 #ifndef __STAN__PROB__DISTRIBUTIONS_BETA_HPP__
 #define __STAN__PROB__DISTRIBUTIONS_BETA_HPP__
 
-#include <boost/math/constants/constants.hpp>
-#include <boost/math/special_functions.hpp>
-#include <boost/math/tools/promotion.hpp>
-#include <boost/math/distributions/detail/common_error_handling.hpp>
-#include <boost/math/policies/error_handling.hpp>
-#include <boost/math/policies/policy.hpp>
-
-#include "stan/prob/transform.hpp"
 #include "stan/prob/distributions_error_handling.hpp"
 #include "stan/prob/distributions_constants.hpp"
 
+#include <stan/meta/traits.hpp>
+
 namespace stan {
   namespace prob {
-    using namespace std;
-    using namespace stan::maths;
+    using boost::math::tools::promote_args;
+    using boost::math::policies::policy;
 
     // Beta(y|alpha,beta)  [alpha > 0;  beta > 0;  0 <= y <= 1]
     /**
@@ -40,9 +34,11 @@ namespace stan {
      * @tparam T_alpha Type of prior sample size for alpha.
      * @tparam T_beta Type of prior sample size for beta.
      */
-    template <typename T_y, typename T_alpha, typename T_beta, class Policy>
-    inline typename boost::math::tools::promote_args<T_y,T_alpha,T_beta>::type
-    beta_log(const T_y& y, const T_alpha& alpha, const T_beta& beta, const Policy& /* pol */) {
+    template <bool propto = false,
+	      typename T_y, typename T_alpha, typename T_beta, 
+	      class Policy = policy<> >
+      inline typename promote_args<T_y,T_alpha,T_beta>::type
+      beta_log(const T_y& y, const T_alpha& alpha, const T_beta& beta, const Policy& = Policy()) {
       static const char* function = "stan::prob::beta_log<%1%>(%1%)";
 
       double result;
@@ -53,84 +49,18 @@ namespace stan {
       if(!stan::prob::check_bounded_x(function, y, 0, 1, &result, Policy()))
 	return result;
 
-      return lgamma(alpha + beta)
-	- lgamma(alpha)
-	- lgamma(beta)
-	+ (alpha - 1.0) * log(y)
-	+ (beta - 1.0) * log(1.0 - y);
-    }
-
-    /**
-     * The log of a beta density for y with the specified
-     * prior sample sizes.
-     * Prior sample sizes, alpha and beta, must be greater than 0.
-     * y must be between 0 and 1 inclusive.
-     * 
-     \f{eqnarray*}{
-     y &\sim& \mbox{\sf{Beta}}(\alpha, \beta) \\
-     \log (p (y \,|\, \alpha, \beta) ) &=& \log \left( \frac{\Gamma(\alpha + \beta)}{\Gamma(\alpha) \Gamma(\beta)} y^{\alpha - 1} (1-y)^{\beta - 1} \right) \\
-     &=& \log (\Gamma(\alpha + \beta)) - \log (\Gamma (\alpha) - \log(\Gamma(\beta)) + (\alpha-1) \log(y) + (\beta-1) \log(1 - y) \\
-     & & \mathrm{where} \; y \in [0, 1]
-     \f}
-     * @param y A scalar variable.
-     * @param alpha Prior sample size.
-     * @param beta Prior sample size.
-     * @throw std::domain_error if alpha is not greater than 0.
-     * @throw std::domain_error if beta is not greater than 0.
-     * @throw std::domain_error if y is not greater than or equal to 0.
-     * @tparam T_y Type of scalar.
-     * @tparam T_alpha Type of prior sample size for alpha.
-     * @tparam T_beta Type of prior sample size for beta.
-     */
-    template <typename T_y, typename T_alpha, typename T_beta>
-    inline typename boost::math::tools::promote_args<T_y,T_alpha,T_beta>::type
-    beta_log(const T_y& y, const T_alpha& alpha, const T_beta& beta) {
-      return beta_log (y, alpha, beta, boost::math::policies::policy<>());
-    }
-
-    /**
-     * The log of a distribution proportional to a beta density for y with the specified
-     * prior sample sizes.
-     * Prior sample sizes, alpha and beta, must be greater than 0.
-     * y must be between 0 and 1 inclusive.
-     *
-     * @param y A scalar variable.
-     * @param alpha Prior sample size.
-     * @param beta Prior sample size.
-     * @throw std::domain_error if alpha is not greater than 0.
-     * @throw std::domain_error if beta is not greater than 0.
-     * @throw std::domain_error if y is not greater than or equal to 0.
-     * @tparam T_y Type of scalar.
-     * @tparam T_alpha Type of prior sample size for alpha.
-     * @tparam T_beta Type of prior sample size for beta.
-     */
-    template <typename T_y, typename T_alpha, typename T_beta, class Policy>
-    inline typename boost::math::tools::promote_args<T_y,T_alpha,T_beta>::type
-    beta_propto_log(const T_y& y, const T_alpha& alpha, const T_beta& beta, const Policy& /* pol */) {
-      return beta_log (y, alpha, beta, Policy());
-    }
-    
-    
-    /**
-     * The log of a distribution proportional to a beta density for y with the specified
-     * prior sample sizes.
-     * Prior sample sizes, alpha and beta, must be greater than 0.
-     * y must be between 0 and 1 inclusive.
-     *
-     * @param y A scalar variable.
-     * @param alpha Prior sample size.
-     * @param beta Prior sample size.
-     * @throw std::domain_error if alpha is not greater than 0.
-     * @throw std::domain_error if beta is not greater than 0.
-     * @throw std::domain_error if y is not greater than or equal to 0.
-     * @tparam T_y Type of scalar.
-     * @tparam T_alpha Type of prior sample size for alpha.
-     * @tparam T_beta Type of prior sample size for beta.
-     */
-    template <typename T_y, typename T_alpha, typename T_beta>
-    inline typename boost::math::tools::promote_args<T_y,T_alpha,T_beta>::type
-    beta_propto_log(const T_y& y, const T_alpha& alpha, const T_beta& beta) {
-      return beta_propto_log (y, alpha, beta, boost::math::policies::policy<>());
+      typename promote_args<T_y,T_alpha,T_beta>::type lp(0.0);
+      if (!propto)
+	lp += lgamma(alpha + beta) - lgamma(alpha) - lgamma(beta);
+      if (!propto
+	  || !is_constant<T_y>::value
+	  || !is_constant<T_alpha>::value)
+	lp += (alpha - 1.0) * log(y);
+      if (!propto
+	  || !is_constant<T_y>::value
+	  || !is_constant<T_beta>::value)
+	lp += (beta - 1.0) * log1m(y);
+      return lp;
     }
 
   }
--- src/test/prob/distributions_beta_test.cpp
+++ src/test/prob/distributions_beta_test.cpp
@@ -5,4 +5,7 @@ TEST(ProbDistributions,Beta) {
   EXPECT_FLOAT_EQ(0.0, stan::prob::beta_log(0.2,1.0,1.0));
   EXPECT_FLOAT_EQ(1.628758, stan::prob::beta_log(0.3,12.0,25.0));
 }
-
+TEST(ProbDistributions,BetaPropto) {
+  EXPECT_FLOAT_EQ(0.0, stan::prob::beta_log<true>(0.2,1.0,1.0));
+  EXPECT_FLOAT_EQ(0.0, stan::prob::beta_log<true>(0.3,12.0,25.0));
+}
