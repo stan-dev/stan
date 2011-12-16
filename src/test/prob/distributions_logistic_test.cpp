bee97439f60c9600ff0413207b278950ad506c59
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2011-12-16 15:32:56 +0000

    distributions: logistic. FIXED issue #36.
    git-svn-id: https://stan.googlecode.com/svn/trunk@342 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/test/prob/distributions_logistic_test.cpp
+++ src/test/prob/distributions_logistic_test.cpp
@@ -5,3 +5,7 @@ TEST(ProbDistributions,Logistic) {
   EXPECT_FLOAT_EQ(-2.129645, stan::prob::logistic_log(1.2,0.3,2.0));
   EXPECT_FLOAT_EQ(-3.430098, stan::prob::logistic_log(-1.0,0.2,0.25));
 }
+TEST(ProbDistributions,LogisticPropto) {
+  EXPECT_FLOAT_EQ(0.0, stan::prob::logistic_log<true>(1.2,0.3,2.0));
+  EXPECT_FLOAT_EQ(0.0, stan::prob::logistic_log<true>(-1.0,0.2,0.25));
+}
