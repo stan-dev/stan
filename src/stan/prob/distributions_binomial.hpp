2b10c011cbccfaa4c2bb4110c68ae3b30c327021
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2011-12-16 16:47:39 +0000

    distributions: binomial. FIXED issue #42.
    git-svn-id: https://stan.googlecode.com/svn/trunk@347 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/stan/prob/distributions_binomial.hpp
+++ src/stan/prob/distributions_binomial.hpp
@@ -1,50 +1,33 @@
 #ifndef __STAN__PROB__DISTRIBUTIONS_BINOMIAL_HPP__
 #define __STAN__PROB__DISTRIBUTIONS_BINOMIAL_HPP__
 
-#include <boost/math/constants/constants.hpp>
-#include <boost/math/special_functions.hpp>
-#include <boost/math/tools/promotion.hpp>
-#include <boost/math/distributions/detail/common_error_handling.hpp>
-#include <boost/math/policies/error_handling.hpp>
-#include <boost/math/policies/policy.hpp>
-
-#include "stan/prob/transform.hpp"
 #include "stan/prob/distributions_error_handling.hpp"
 #include "stan/prob/distributions_constants.hpp"
 
+#include <stan/meta/traits.hpp>
+
 namespace stan {
   namespace prob {
-    using namespace std;
-    using namespace stan::maths;
+    using boost::math::tools::promote_args;
+    using boost::math::policies::policy;
 
     // Binomial(n|N,theta)  [N >= 0;  0 <= n <= N;  0 <= theta <= 1]
-    template <typename T_n, typename T_N, typename T_prob, class Policy>
-    inline typename boost::math::tools::promote_args<T_prob>::type
-    binomial_log(const T_n& n, const T_N& N, const T_prob& theta, const Policy& /* pol */) {
-      return maths::binomial_coefficient_log<T_N>(N,n)
-	+ n * log(theta)
-	+ (N - n) * log(1.0 - theta);
+    template <bool propto = false,
+	      typename T_n, typename T_N, typename T_prob, 
+	      class Policy = policy<> >
+    inline typename promote_args<T_prob>::type
+      binomial_log(const T_n& n, const T_N& N, const T_prob& theta, const Policy& = Policy()) {
+      // FIXME: domain checks
+      typename promote_args<T_prob>::type lp(0.0);
+      if (!propto)
+	lp += maths::binomial_coefficient_log<T_N>(N,n);
+      if (!propto
+	  || !is_constant<T_n>::value
+	  || !is_constant<T_N>::value
+	  || !is_constant<T_prob>::value)
+	lp += n * log(theta) + (N-n) * log1m (theta);
+      return lp;
     }
-
-    template <typename T_n, typename T_N, typename T_prob>
-    inline typename boost::math::tools::promote_args<T_prob>::type
-    binomial_log(const T_n& n, const T_N& N, const T_prob& theta) {
-      return binomial_log (n, N, theta, boost::math::policies::policy<>());
-    }
-
-    
-    template <typename T_n, typename T_N, typename T_prob, class Policy>
-    inline typename boost::math::tools::promote_args<T_prob>::type
-    binomial_propto_log(const T_n& n, const T_N& N, const T_prob& theta, const Policy& /* pol */) {
-      return binomial_log (n, N, theta, Policy());
-    }
-
-    template <typename T_n, typename T_N, typename T_prob>
-    inline typename boost::math::tools::promote_args<T_prob>::type
-    binomial_propto_log(const T_n& n, const T_N& N, const T_prob& theta) {
-      return binomial_propto_log (n, N, theta, boost::math::policies::policy<>());
-    }
-
   }
 }
 #endif
--- src/test/prob/distributions_binomial_test.cpp
+++ src/test/prob/distributions_binomial_test.cpp
@@ -5,3 +5,7 @@ TEST(ProbDistributions,Binomial) {
   EXPECT_FLOAT_EQ(-2.144372, stan::prob::binomial_log(10,20,0.4));
   EXPECT_FLOAT_EQ(-16.09438, stan::prob::binomial_log(0,10,0.8));
 }
+TEST(ProbDistributions,BinomialPropto) {
+  EXPECT_FLOAT_EQ(-2.144372, stan::prob::binomial_log(10,20,0.4));
+  EXPECT_FLOAT_EQ(-16.09438, stan::prob::binomial_log(0,10,0.8));
+}
