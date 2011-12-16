576bd521795c81fd0efb237e090e7c000c29b643
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2011-12-16 02:17:07 +0000

    distributions: dirichlet. FIXED issue #31.
    git-svn-id: https://stan.googlecode.com/svn/trunk@335 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/test/prob/distributions_dirichlet_test.cpp
+++ src/test/prob/distributions_dirichlet_test.cpp
@@ -1,5 +1,4 @@
 #include <gtest/gtest.h>
-#include <Eigen/Dense>
 #include "stan/prob/distributions_dirichlet.hpp"
 
 using Eigen::Dynamic;
@@ -17,5 +16,19 @@ TEST(ProbDistributions,Dirichlet) {
   Matrix<double,Dynamic,1> alpha2(4,1);
   alpha2 << 10.5, 11.5, 19.3, 5.1;
   EXPECT_FLOAT_EQ(-43.40045, stan::prob::dirichlet_log(theta2,alpha2));
+}
+
+TEST(ProbDistributions,DirichletPropto) {
+  Matrix<double,Dynamic,1> theta(3,1);
+  theta << 0.2, 0.3, 0.5;
+  Matrix<double,Dynamic,1> alpha(3,1);
+  alpha << 1.0, 1.0, 1.0;
+  EXPECT_FLOAT_EQ(0.0, stan::prob::dirichlet_log<true>(theta,alpha));
+  
+  Matrix<double,Dynamic,1> theta2(4,1);
+  theta2 << 0.01, 0.01, 0.8, 0.18;
+  Matrix<double,Dynamic,1> alpha2(4,1);
+  alpha2 << 10.5, 11.5, 19.3, 5.1;
+  EXPECT_FLOAT_EQ(0.0, stan::prob::dirichlet_log<true>(theta2,alpha2));
 }
 
