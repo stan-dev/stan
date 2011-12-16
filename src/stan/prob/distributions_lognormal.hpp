17d52a083de9af1686f9fa23b0369edeeb76225b
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2011-12-16 15:40:29 +0000

    distributions: lognormal. FIXED issue #37.
    git-svn-id: https://stan.googlecode.com/svn/trunk@343 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/stan/prob/distributions_lognormal.hpp
+++ src/stan/prob/distributions_lognormal.hpp
@@ -1,51 +1,41 @@
 #ifndef __STAN__PROB__DISTRIBUTIONS_LOGNORMAL_HPP__
 #define __STAN__PROB__DISTRIBUTIONS_LOGNORMAL_HPP__
 
-#include <boost/math/constants/constants.hpp>
-#include <boost/math/special_functions.hpp>
-#include <boost/math/tools/promotion.hpp>
-#include <boost/math/distributions/detail/common_error_handling.hpp>
-#include <boost/math/policies/error_handling.hpp>
-#include <boost/math/policies/policy.hpp>
-
-#include "stan/prob/transform.hpp"
 #include "stan/prob/distributions_error_handling.hpp"
 #include "stan/prob/distributions_constants.hpp"
 
+#include <stan/meta/traits.hpp>
+
 namespace stan {
   namespace prob {
-    using namespace std;
-    using namespace stan::maths;
+    using boost::math::tools::promote_args;
+    using boost::math::policies::policy;
 
     // LogNormal(y|mu,sigma)  [y >= 0;  sigma > 0]
-    template <typename T_y, typename T_loc, typename T_scale, class Policy>
-    inline typename boost::math::tools::promote_args<T_y,T_loc,T_scale>::type
-    lognormal_log(const T_y& y, const T_loc& mu, const T_scale& sigma, const Policy& /* pol */) {
-      return NEG_LOG_SQRT_TWO_PI
-	- log(sigma)
-	- log(y)
-	- pow(log(y) - mu,2.0) / (2.0 * sigma * sigma);
+    template <bool propto = false,
+	      typename T_y, typename T_loc, typename T_scale, 
+	      class Policy = policy<> >
+      inline typename promote_args<T_y,T_loc,T_scale>::type
+      lognormal_log(const T_y& y, const T_loc& mu, const T_scale& sigma, const Policy& = Policy()) {
+      //FIXME: bounds checks
+      
+      typename promote_args<T_y,T_loc,T_scale>::type lp(0.0);
+      if (!propto)
+	lp += NEG_LOG_SQRT_TWO_PI;
+      if (!propto
+	  || !is_constant<T_scale>::value)
+	lp -= log(sigma);
+      if (!propto
+	  || !is_constant<T_y>::value)
+	lp -= log (y);
+      if (!propto
+	  || !is_constant<T_y>::value
+	  || !is_constant<T_loc>::value
+	  || !is_constant<T_scale>::value)
+	lp -= pow(log(y) - mu,2.0) / (2.0 * sigma * sigma);
+      return lp;
     }
     
-    template <typename T_y, typename T_loc, typename T_scale>
-    inline typename boost::math::tools::promote_args<T_y,T_loc,T_scale>::type
-    lognormal_log(const T_y& y, const T_loc& mu, const T_scale& sigma) {
-      return lognormal_log (y, mu, sigma, boost::math::policies::policy<>());
-    }
-
-    template <typename T_y, typename T_loc, typename T_scale, class Policy>
-    inline typename boost::math::tools::promote_args<T_y,T_loc,T_scale>::type
-    lognormal_propto_log(const T_y& y, const T_loc& mu, const T_scale& sigma, const Policy& /* pol */) {
-      return lognormal_log (y, mu, sigma, Policy());
-    }
-    
-    template <typename T_y, typename T_loc, typename T_scale>
-    inline typename boost::math::tools::promote_args<T_y,T_loc,T_scale>::type
-    lognormal_propto_log(const T_y& y, const T_loc& mu, const T_scale& sigma) {
-      return lognormal_propto_log (y, mu, sigma, boost::math::policies::policy<>());
-    }
-
-
   }
 }
 #endif
