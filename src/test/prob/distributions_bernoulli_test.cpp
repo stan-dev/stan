08db308f7527b22b33d15677e6fe362e02a1af88
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2011-12-16 15:48:13 +0000

    distributions: bernoulli. FIXED issue #40.
    git-svn-id: https://stan.googlecode.com/svn/trunk@344 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/test/prob/distributions_bernoulli_test.cpp
+++ src/test/prob/distributions_bernoulli_test.cpp
@@ -5,3 +5,7 @@ TEST(ProbDistributions,Bernoulli) {
   EXPECT_FLOAT_EQ(std::log(0.25), stan::prob::bernoulli_log(1,0.25));
   EXPECT_FLOAT_EQ(std::log(1.0 - 0.25), stan::prob::bernoulli_log(0,0.25));
 }
+TEST(ProbDistributions,BernoulliPropto) {
+  EXPECT_FLOAT_EQ(0.0, stan::prob::bernoulli_log<true>(1,0.25));
+  EXPECT_FLOAT_EQ(0.0, stan::prob::bernoulli_log<true>(0,0.25));
+}
