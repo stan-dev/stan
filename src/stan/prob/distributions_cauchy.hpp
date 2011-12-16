439d688ac5c43dd2dee4fed9b7168710066c4e66
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2011-12-16 14:37:04 +0000

    distributions: cauchy. FIXED issue #32.
    git-svn-id: https://stan.googlecode.com/svn/trunk@338 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/stan/prob/distributions_cauchy.hpp
+++ src/stan/prob/distributions_cauchy.hpp
@@ -1,26 +1,22 @@
 #ifndef __STAN__PROB__DISTRIBUTIONS_CAUCHY_HPP__
 #define __STAN__PROB__DISTRIBUTIONS_CAUCHY_HPP__
 
-#include <boost/math/constants/constants.hpp>
-#include <boost/math/special_functions.hpp>
-#include <boost/math/tools/promotion.hpp>
-#include <boost/math/distributions/detail/common_error_handling.hpp>
-#include <boost/math/policies/error_handling.hpp>
-#include <boost/math/policies/policy.hpp>
-
-#include "stan/prob/transform.hpp"
 #include "stan/prob/distributions_error_handling.hpp"
 #include "stan/prob/distributions_constants.hpp"
 
+#include <stan/meta/traits.hpp>
+
 namespace stan {
   namespace prob {
-    using namespace std;
-    using namespace stan::maths;
+    using boost::math::tools::promote_args;
+    using boost::math::policies::policy;
 
     // Cauchy(y|mu,sigma)  [sigma > 0]
-    template <typename T_y, typename T_loc, typename T_scale, class Policy>
-    inline typename boost::math::tools::promote_args<T_y,T_loc,T_scale>::type
-    cauchy_log(const T_y& y, const T_loc& mu, const T_scale& sigma, const Policy& /* pol */) {
+    template <bool propto = false,
+	      typename T_y, typename T_loc, typename T_scale, 
+	      class Policy = policy<> >
+    inline typename promote_args<T_y,T_loc,T_scale>::type
+    cauchy_log(const T_y& y, const T_loc& mu, const T_scale& sigma, const Policy& = Policy()) {
       static const char* function = "stan::prob::cauchy_log<%1%>(%1%)";
 
       double result;
@@ -31,30 +27,19 @@ namespace stan {
       if(!stan::prob::check_x(function, y, &result, Policy()))
 	return result;
 
-      return NEG_LOG_PI
-	- log(sigma)
-	- log(1.0 + (y - mu) * (y - mu) / (sigma * sigma));
+      typename promote_args<T_y,T_loc,T_scale>::type lp(0.0);
+      if (!propto)
+	lp += NEG_LOG_PI;
+      if (!propto
+	  || !is_constant<T_scale>::value)
+	lp -= log(sigma);
+      if (!propto
+	  || !is_constant<T_y>::value
+	  || !is_constant<T_loc>::value
+	  || !is_constant<T_scale>::value)
+	lp -= log(1.0 + (y - mu) * (y - mu) / (sigma * sigma));
+      return lp;
     }
-
-    template <typename T_y, typename T_loc, typename T_scale>
-    inline typename boost::math::tools::promote_args<T_y,T_loc,T_scale>::type
-    cauchy_log(const T_y& y, const T_loc& mu, const T_scale& sigma) {
-      return cauchy_log (y, mu, sigma, boost::math::policies::policy<>());
-    }
-    
-    // Cauchy(y|mu,sigma)  [sigma > 0]
-    template <typename T_y, typename T_loc, typename T_scale, class Policy>
-    inline typename boost::math::tools::promote_args<T_y,T_loc,T_scale>::type
-    cauchy_propto_log(const T_y& y, const T_loc& mu, const T_scale& sigma, const Policy& /* pol */) {
-      return cauchy_log (y, mu, sigma, Policy());
-    }
-
-    template <typename T_y, typename T_loc, typename T_scale>
-    inline typename boost::math::tools::promote_args<T_y,T_loc,T_scale>::type
-    cauchy_propto_log(const T_y& y, const T_loc& mu, const T_scale& sigma) {
-      return cauchy_propto_log (y, mu, sigma, boost::math::policies::policy<>());
-    }
-    
 
   }
 }
--- src/test/prob/distributions_cauchy_test.cpp
+++ src/test/prob/distributions_cauchy_test.cpp
@@ -7,3 +7,10 @@ TEST(ProbDistributions,Cauchy) {
   EXPECT_FLOAT_EQ(-2.323385, stan::prob::cauchy_log(-2.5, -1.0, 1.0));
   // need test with scale != 1
 }
+TEST(ProbDistributions,CauchyPropto) {
+  EXPECT_FLOAT_EQ(0.0, stan::prob::cauchy_log<true>(1.0, 0.0, 1.0));
+  EXPECT_FLOAT_EQ(0.0, stan::prob::cauchy_log<true>(-1.5, 0.0, 1.0));
+  EXPECT_FLOAT_EQ(0.0, stan::prob::cauchy_log<true>(-2.5, -1.0, 1.0));
+  // need test with scale != 1
+}
+
