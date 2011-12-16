d00198a232b41f2084b3d2dce78efa83a8d55766
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2011-12-16 14:54:43 +0000

    distributions: pareto. FIXED issue #33.
    git-svn-id: https://stan.googlecode.com/svn/trunk@339 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/stan/prob/distributions_pareto.hpp
+++ src/stan/prob/distributions_pareto.hpp
@@ -1,50 +1,40 @@
 #ifndef __STAN__PROB__DISTRIBUTIONS_PARETO_HPP__
 #define __STAN__PROB__DISTRIBUTIONS_PARETO_HPP__
 
-#include <boost/math/constants/constants.hpp>
-#include <boost/math/special_functions.hpp>
-#include <boost/math/tools/promotion.hpp>
-#include <boost/math/distributions/detail/common_error_handling.hpp>
-#include <boost/math/policies/error_handling.hpp>
-#include <boost/math/policies/policy.hpp>
-
-#include "stan/prob/transform.hpp"
 #include "stan/prob/distributions_error_handling.hpp"
 #include "stan/prob/distributions_constants.hpp"
 
+#include <stan/meta/traits.hpp>
+
 namespace stan {
   namespace prob {
-    using namespace std;
-    using namespace stan::maths;
+    using boost::math::tools::promote_args;
+    using boost::math::policies::policy;
 
     // Pareto(y|y_m,alpha)  [y > y_m;  y_m > 0;  alpha > 0]
-    template <typename T_y, typename T_scale, typename T_shape, class Policy>
-    inline typename boost::math::tools::promote_args<T_y,T_scale,T_shape>::type
-    pareto_log(const T_y& y, const T_scale& y_min, const T_shape& alpha, const Policy& /* pol */) {
+    template <bool propto = false,
+	      typename T_y, typename T_scale, typename T_shape, 
+	      class Policy = policy<> >
+    inline typename promote_args<T_y,T_scale,T_shape>::type
+      pareto_log(const T_y& y, const T_scale& y_min, const T_shape& alpha, const Policy& = Policy()) {
       // FIXME: bounds checks
-      return log(alpha)
-	+ alpha * log(y_min)
-	- (alpha + 1.0) * log(y);
+      if (y < y_min)
+	return LOG_ZERO;
+	  
+      typename promote_args<T_y,T_scale,T_shape>::type lp(0.0);
+      if (!propto
+	  || !is_constant<T_shape>::value)
+	lp += log(alpha);
+      if (!propto
+	  || !is_constant<T_scale>::value
+	  || !is_constant<T_shape>::value)
+	lp += alpha * log(y_min);
+      if (!propto
+	  || !is_constant<T_y>::value
+	  || !is_constant<T_shape>::value)
+	lp -= (alpha + 1.0) * log(y);
+      return lp;
     }
-
-    template <typename T_y, typename T_scale, typename T_shape>
-    inline typename boost::math::tools::promote_args<T_y,T_scale,T_shape>::type
-    pareto_log(const T_y& y, const T_scale& y_min, const T_shape& alpha) {
-      return pareto_log (y, y_min, alpha, boost::math::policies::policy<>());
-    }
-
-    template <typename T_y, typename T_scale, typename T_shape, class Policy>
-    inline typename boost::math::tools::promote_args<T_y,T_scale,T_shape>::type
-    pareto_propto_log(const T_y& y, const T_scale& y_min, const T_shape& alpha, const Policy& /* pol */) {
-      return pareto_log (y, y_min, alpha, Policy());
-    }
-
-    template <typename T_y, typename T_scale, typename T_shape>
-    inline typename boost::math::tools::promote_args<T_y,T_scale,T_shape>::type
-    pareto_propto_log(const T_y& y, const T_scale& y_min, const T_shape& alpha) {
-      return pareto_propto_log (y, y_min, alpha, boost::math::policies::policy<>());
-    }
-    
 
   }
 }
--- src/test/prob/distributions_pareto_test.cpp
+++ src/test/prob/distributions_pareto_test.cpp
@@ -4,4 +4,10 @@
 TEST(ProbDistributions,Pareto) {
   EXPECT_FLOAT_EQ(-1.909543, stan::prob::pareto_log(1.5,0.5,2.0));
   EXPECT_FLOAT_EQ(-25.69865, stan::prob::pareto_log(19.5,0.15,5.0));
+  EXPECT_FLOAT_EQ(log(0.0), stan::prob::pareto_log(0.0,0.15,5.0));
+}
+TEST(ProbDistributions,ParetoPropto) {
+  EXPECT_FLOAT_EQ(0.0, stan::prob::pareto_log<true>(1.5,0.5,2.0));
+  EXPECT_FLOAT_EQ(0.0, stan::prob::pareto_log<true>(19.5,0.15,5.0));
+  EXPECT_FLOAT_EQ(log(0.0), stan::prob::pareto_log(0.0,0.15,5.0));
 }
