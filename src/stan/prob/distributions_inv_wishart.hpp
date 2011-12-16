9e8f36ba828a43784ab6440550f4338555043ef4
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2011-12-16 01:18:53 +0000

    distributions: inv_wishart. FIXED issue #28.
    git-svn-id: https://stan.googlecode.com/svn/trunk@331 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/stan/prob/distributions_inv_wishart.hpp
+++ src/stan/prob/distributions_inv_wishart.hpp
@@ -1,23 +1,19 @@
 #ifndef __STAN__PROB__DISTRIBUTIONS_INV_WISHART_HPP__
 #define __STAN__PROB__DISTRIBUTIONS_INV_WISHART_HPP__
 
-#include <boost/math/constants/constants.hpp>
-#include <boost/math/special_functions.hpp>
-#include <boost/math/tools/promotion.hpp>
-#include <boost/math/distributions/detail/common_error_handling.hpp>
-#include <boost/math/policies/error_handling.hpp>
-#include <boost/math/policies/policy.hpp>
 
-#include "stan/prob/transform.hpp"
 #include "stan/prob/distributions_error_handling.hpp"
 #include "stan/prob/distributions_constants.hpp"
 
+#include <stan/meta/traits.hpp>
+
 namespace stan {
   namespace prob {
-    using namespace std;
-    using namespace stan::maths;
     using Eigen::Dynamic;
     using Eigen::Matrix;
+
+    using boost::math::tools::promote_args;
+    using boost::math::policies::policy;
 
     // InvWishart(Sigma|n,Omega)  [W, S symmetric, non-neg, definite; 
     //                             W.dims() = S.dims();
@@ -49,12 +45,14 @@ namespace stan {
      * @tparam T_dof Type of degrees of freedom.
      * @tparam T_scale Type of scale.
      */
-    template <typename T_y, typename T_dof, typename T_scale, class Policy>
-    inline typename boost::math::tools::promote_args<T_y,T_dof,T_scale>::type
+    template <bool propto = false, 
+	      typename T_y, typename T_dof, typename T_scale, 
+	      class Policy = policy<> >
+    inline typename promote_args<T_y,T_dof,T_scale>::type
     inv_wishart_log(const Matrix<T_y,Dynamic,Dynamic>& W,
 		    const T_dof& nu,
 		    const Matrix<T_scale,Dynamic,Dynamic>& S,
-		    const Policy& /* pol */) {
+		    const Policy& = Policy()) {
       static const char* function = "stan::prob::wishart_log<%1%>(%1%)";
 
       unsigned int k = S.rows();
@@ -63,104 +61,28 @@ namespace stan {
 	return result;
       // FIXME: domain checks
 
-      return 0.5 * nu * log(S.determinant())
-	- 0.5 * (nu + k + 1.0) * log(W.determinant())
-	- 0.5 * (S * W.inverse()).trace()
-	+  nu * k * NEG_LOG_TWO_OVER_TWO
-	- lmgamma(k, 0.5 * nu);
+      typename promote_args<T_y,T_dof,T_scale>::type lp(0.0);
+      if (!propto)
+	lp -= lmgamma(k, 0.5 * nu);
+      if (!propto
+	  || !is_constant<T_dof>::value
+	  || !is_constant<T_scale>::value)
+	lp += 0.5 * nu * log(S.determinant());
+      if (!propto
+	  || !is_constant<T_y>::value
+	  || !is_constant<T_dof>::value
+	  || !is_constant<T_scale>::value)
+	lp -= 0.5 * (nu + k + 1.0) * log(W.determinant());
+      if (!propto
+	  || !is_constant<T_y>::value
+	  || !is_constant<T_scale>::value)
+	lp -= 0.5 * (S * W.inverse()).trace();
+      if (!propto
+	  || !is_constant<T_dof>::value
+	  || !is_constant<T_scale>::value)
+	lp += nu * k * NEG_LOG_TWO_OVER_TWO;
+      return lp;
     }
-
-        /**
-     * The log of the Inverse-Wishart density for the given W, degrees of freedom, 
-     * and scale matrix. 
-     * 
-     * The scale matrix, S, must be k x k, symmetric, and semi-positive definite.
-     * Dimension, k, is implicit.
-     * nu must be greater than k-1
-     *
-     * \f{eqnarray*}{
-       W &\sim& \mbox{\sf{Inv-Wishart}}_{\nu} (S) \\
-       \log (p (W \,|\, \nu, S) ) &=& \log \left( \left(2^{\nu k/2} \pi^{k (k-1) /4} \prod_{i=1}^k{\Gamma (\frac{\nu + 1 - i}{2})} \right)^{-1} 
-                                                  \times \left| S \right|^{\nu/2} \left| W \right|^{-(\nu + k + 1) / 2}
-						  \times \exp (-\frac{1}{2} \mathsf{tr} (S W^{-1})) \right) \\
-       &=& -\frac{\nu k}{2}\log(2) - \frac{k (k-1)}{4} \log(\pi) - \sum_{i=1}^{k}{\log (\Gamma (\frac{\nu+1-i}{2}))}
-           +\frac{\nu}{2} \log(\det(S)) - \frac{\nu+k+1}{2}\log (\det(W)) - \frac{1}{2} \mathsf{tr}(S W^{-1})
-     \f}
-     * 
-     * @param W A scalar matrix
-     * @param nu Degrees of freedom
-     * @param S The scale matrix
-     * @return The log of the Inverse-Wishart density at W given nu and S.
-     * @throw std::domain_error if nu is not greater than k-1
-     * @throw std::domain_error if S is not square, not symmetric, or not semi-positive definite.
-     * @tparam T_y Type of scalar.
-     * @tparam T_dof Type of degrees of freedom.
-     * @tparam T_scale Type of scale.
-     */
-    template <typename T_y, typename T_dof, typename T_scale>
-    inline typename boost::math::tools::promote_args<T_y,T_dof,T_scale>::type
-    inv_wishart_log(const Matrix<T_y,Dynamic,Dynamic>& W,
-		    const T_dof& nu,
-		    const Matrix<T_scale,Dynamic,Dynamic>& S) {
-      return inv_wishart_log (W, nu, S, boost::math::policies::policy<>());
-    }
-
-
-    /**
-     * The log of a density proportional to the Inverse-Wishart density for 
-     * the given W, degrees of freedom, and scale matrix. 
-     * 
-     * The scale matrix, S, must be k x k, symmetric, and semi-positive definite.
-     * Dimension, k, is implicit.
-     * nu must be greater than k-1
-     *
-     * @param W A scalar matrix
-     * @param nu Degrees of freedom
-     * @param S The scale matrix
-     * @return The log of the Inverse-Wishart density at W given nu and S.
-     * @throw std::domain_error if nu is not greater than k-1
-     * @throw std::domain_error if S is not square, not symmetric, or not semi-positive definite.
-     * @tparam T_y Type of scalar.
-     * @tparam T_dof Type of degrees of freedom.
-     * @tparam T_scale Type of scale.
-     */
-    template <typename T_y, typename T_dof, typename T_scale, class Policy>
-    inline typename boost::math::tools::promote_args<T_y,T_dof,T_scale>::type
-    inv_wishart_propto_log(const Matrix<T_y,Dynamic,Dynamic>& W,
-			   const T_dof& nu,
-			   const Matrix<T_scale,Dynamic,Dynamic>& S,
-			   const Policy& /* pol */) {
-      return inv_wishart_log (W, nu, S, Policy());
-    }
-    
-        /**
-     * The log of a density proportional to the Inverse-Wishart density for 
-     * the given W, degrees of freedom, and scale matrix. 
-     * 
-     * The scale matrix, S, must be k x k, symmetric, and semi-positive definite.
-     * Dimension, k, is implicit.
-     * nu must be greater than k-1
-     *
-     * @param W A scalar matrix
-     * @param nu Degrees of freedom
-     * @param S The scale matrix
-     * @return The log of the Inverse-Wishart density at W given nu and S.
-     * @throw std::domain_error if nu is not greater than k-1
-     * @throw std::domain_error if S is not square, not symmetric, or not semi-positive definite.
-     * @tparam T_y Type of scalar.
-     * @tparam T_dof Type of degrees of freedom.
-     * @tparam T_scale Type of scale.
-     */
-    template <typename T_y, typename T_dof, typename T_scale>
-    inline typename boost::math::tools::promote_args<T_y,T_dof,T_scale>::type
-    inv_wishart_propto_log(const Matrix<T_y,Dynamic,Dynamic>& W,
-			   const T_dof& nu,
-			   const Matrix<T_scale,Dynamic,Dynamic>& S) {
-      return inv_wishart_propto_log (W, nu, S, boost::math::policies::policy<>());
-    }
-
-
-
   }
 }
 #endif
--- src/test/prob/distributions_inv_wishart_test.cpp
+++ src/test/prob/distributions_inv_wishart_test.cpp
@@ -23,3 +23,18 @@ TEST(ProbDistributions,InvWishart) {
 
   EXPECT_NEAR(log_p, stan::prob::inv_wishart_log(Y,dof,Sigma), 0.01);
 }
+TEST(ProbDistributions,InvWishartPropto) {
+  Matrix<double,Dynamic,Dynamic> Y(3,3);
+  Y <<  12.147233, -11.9036079, 1.0910458,
+    -11.903608,  16.7585782, 0.8530256,
+    1.091046,   0.8530256, 2.5786609;
+
+  Matrix<double,Dynamic,Dynamic> Sigma(3,3);
+  Sigma << 7.785215,  3.0597878,  1.1071663,
+    3.059788, 10.3515035, -0.1232598,
+    1.107166, -0.1232598,  7.7623386;
+  
+  double dof = 4.0;
+  
+  EXPECT_FLOAT_EQ(0.0, stan::prob::inv_wishart_log<true>(Y,dof,Sigma));
+}
