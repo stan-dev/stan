9c68bd364c7ea8ab1bf96e15ab39f2e2cb636442
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2011-12-16 15:25:04 +0000

    distributions: weibull. FIXED issue #35.
    git-svn-id: https://stan.googlecode.com/svn/trunk@341 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/stan/prob/distributions_weibull.hpp
+++ src/stan/prob/distributions_weibull.hpp
@@ -1,58 +1,54 @@
 #ifndef __STAN__PROB__DISTRIBUTIONS_WEIBULL_HPP__
 #define __STAN__PROB__DISTRIBUTIONS_WEIBULL_HPP__
 
-#include <boost/math/constants/constants.hpp>
-#include <boost/math/special_functions.hpp>
-#include <boost/math/tools/promotion.hpp>
-#include <boost/math/distributions/detail/common_error_handling.hpp>
-#include <boost/math/policies/error_handling.hpp>
-#include <boost/math/policies/policy.hpp>
-
-#include "stan/prob/transform.hpp"
 #include "stan/prob/distributions_error_handling.hpp"
 #include "stan/prob/distributions_constants.hpp"
 
+#include <stan/meta/traits.hpp>
+
 namespace stan {
   namespace prob {
-    using namespace std;
-    using namespace stan::maths;
+    using boost::math::tools::promote_args;
+    using boost::math::policies::policy;
 
     // Weibull(y|sigma,alpha)     [y >= 0;  sigma > 0;  alpha > 0]
-    template <typename T_y, typename T_shape, typename T_scale, class Policy>
-    inline typename boost::math::tools::promote_args<T_y,T_shape,T_scale>::type
-    weibull_log(const T_y& y, const T_shape& alpha, const T_scale& sigma, const Policy& /* pol */) {
+    template <bool propto = false,
+	      typename T_y, typename T_shape, typename T_scale, 
+	      class Policy = policy<> >
+    inline typename promote_args<T_y,T_shape,T_scale>::type
+      weibull_log(const T_y& y, const T_shape& alpha, const T_scale& sigma, const Policy& = Policy()) {
       //static const char* function = "stan::prob::weibull_log<%1%>(%1%)";
 
       //double result;
       // FIXME: domain checks
-
-      return log(alpha)
-	- log(sigma)
-	+ (alpha - 1.0) * (log(y) - log(sigma))
-	- pow(y / sigma, alpha);
+      if (y < 0)
+	return LOG_ZERO;
+      
+      typename promote_args<T_y,T_shape,T_scale>::type lp(0.0);
+      if (!propto
+	  || !is_constant<T_shape>::value)
+	lp += log(alpha);
+      if (!propto
+	  || !is_constant<T_y>::value
+	  || !is_constant<T_shape>::value)
+	lp += (alpha - 1.0) * log (y);
+      if (!propto
+	  || !is_constant<T_y>::value
+	  || !is_constant<T_scale>::value)
+	lp -= alpha * log(sigma);
+      if (!propto
+	  || !is_constant<T_y>::value
+	  || !is_constant<T_scale>::value)
+	lp -= pow(y / sigma, alpha);
+	
+      return lp;
     }
 
-    template <typename T_y, typename T_shape, typename T_scale>
+    template <bool propto = false,
+	      typename T_y, typename T_shape, typename T_scale, 
+	      class Policy = policy<> >
     inline typename boost::math::tools::promote_args<T_y,T_shape,T_scale>::type
-    weibull_log(const T_y& y, const T_shape& alpha, const T_scale& sigma) {
-      return weibull_log (y, alpha, sigma, boost::math::policies::policy<>());
-    }
-
-    template <typename T_y, typename T_shape, typename T_scale, class Policy>
-    inline typename boost::math::tools::promote_args<T_y,T_shape,T_scale>::type
-    weibull_propto_log(const T_y& y, const T_shape& alpha, const T_scale& sigma, const Policy& /* pol */) {
-      return weibull_log (y, alpha, sigma, Policy());
-    }
-    
-    template <typename T_y, typename T_shape, typename T_scale>
-    inline typename boost::math::tools::promote_args<T_y,T_shape,T_scale>::type
-    weibull_propto_log(const T_y& y, const T_shape& alpha, const T_scale& sigma) {
-      return weibull_propto_log (y, alpha, sigma, boost::math::policies::policy<>());
-    }
-
-    template <typename T_y, typename T_shape, typename T_scale, class Policy>
-    inline typename boost::math::tools::promote_args<T_y,T_shape,T_scale>::type
-    weibull_p(const T_y& y, const T_shape& alpha, const T_scale& sigma, const Policy& /* pol */) {
+      weibull_p(const T_y& y, const T_shape& alpha, const T_scale& sigma, const Policy& = Policy()) {
       //static const char* function = "stan::prob::weibull_p<%1%>(%1%)";
 
       //double result;
@@ -60,16 +56,14 @@ namespace stan {
       
       if (y < 0)
 	return 0;
-      return 1.0 - exp (- pow (y / sigma, alpha));
+      if (!propto
+	  || !is_constant<T_y>::value
+	  || !is_constant<T_shape>::value
+	  || !is_constant<T_scale>::value)
+	return 1.0 - exp (- pow (y / sigma, alpha));
+      return 1;
     }
 
-    template <typename T_y, typename T_shape, typename T_scale>
-    inline typename boost::math::tools::promote_args<T_y,T_shape,T_scale>::type
-    weibull_p(const T_y& y, const T_shape& alpha, const T_scale& sigma) {
-      return weibull_p (y, alpha, sigma, boost::math::policies::policy<>());
-    }
-
-    
   }
 }
 #endif
--- src/test/prob/distributions_weibull_test.cpp
+++ src/test/prob/distributions_weibull_test.cpp
@@ -1,18 +1,24 @@
-// These tests should not have reference to stan::agrad::var. Distribution tests 
-// with stan::agrad::var should be placed in src/test/agrad/distributions_test.cpp
-
 #include <gtest/gtest.h>
-#include "stan/prob/distributions.hpp"
+#include "stan/prob/distributions_weibull.hpp"
 
 TEST(ProbDistributions,Weibull) {
   EXPECT_FLOAT_EQ(-2.0, stan::prob::weibull_log(2.0,1.0,1.0));
   EXPECT_FLOAT_EQ(-3.277094, stan::prob::weibull_log(0.25,2.9,1.8));
   EXPECT_FLOAT_EQ(-102.8962, stan::prob::weibull_log(3.9,1.7,0.25));
 }
+TEST(ProbDistributions,WeibullPropto) {
+  EXPECT_FLOAT_EQ(0.0, stan::prob::weibull_log<true>(2.0,1.0,1.0));
+  EXPECT_FLOAT_EQ(0.0, stan::prob::weibull_log<true>(0.25,2.9,1.8));
+  EXPECT_FLOAT_EQ(0.0, stan::prob::weibull_log<true>(3.9,1.7,0.25));
+}
 
 TEST(ProbDistributionsCumulative,Weibull) {
-  // values from R
   EXPECT_FLOAT_EQ(0.86466472, stan::prob::weibull_p(2.0,1.0,1.0));
   EXPECT_FLOAT_EQ(0.0032585711, stan::prob::weibull_p(0.25,2.9,1.8));
   EXPECT_FLOAT_EQ(1.0, stan::prob::weibull_p(3.9,1.7,0.25));
+}
+TEST(ProbDistributionsCumulative,WeibullPropto) {
+  EXPECT_FLOAT_EQ(1.0, stan::prob::weibull_p<true>(2.0,1.0,1.0));
+  EXPECT_FLOAT_EQ(1.0, stan::prob::weibull_p<true>(0.25,2.9,1.8));
+  EXPECT_FLOAT_EQ(1.0, stan::prob::weibull_p<true>(3.9,1.7,0.25));
 }
