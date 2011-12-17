2d3da92ddab8d33f939cb0522fa12b7b2d415ed4
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2011-12-17 04:29:03 +0000

    cleanup
    git-svn-id: https://stan.googlecode.com/svn/trunk@355 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/test/prob/distributions_multinomial_test.cpp
+++ src/test/prob/distributions_multinomial_test.cpp
@@ -1,10 +1,8 @@
 #include <gtest/gtest.h>
 #include "stan/prob/distributions_multinomial.hpp"
-#include <Eigen/Dense>
 
 using Eigen::Matrix;
 using Eigen::Dynamic;
-
 
 TEST(ProbDistributions,Multinomial) {
   std::vector<int> ns;
