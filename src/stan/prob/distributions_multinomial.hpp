d5e526591a65ab088df1da269b28a9ea560e9ec8
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2011-12-17 04:18:46 +0000

    distributions: multinomial. FIXED issue #47.
    git-svn-id: https://stan.googlecode.com/svn/trunk@354 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/stan/prob/distributions_multinomial.hpp
+++ src/stan/prob/distributions_multinomial.hpp
@@ -1,67 +1,45 @@
 #ifndef __STAN__PROB__DISTRIBUTIONS_MULTINOMIAL_HPP__
 #define __STAN__PROB__DISTRIBUTIONS_MULTINOMIAL_HPP__
 
-#include <boost/math/constants/constants.hpp>
-#include <boost/math/special_functions.hpp>
-#include <boost/math/tools/promotion.hpp>
-#include <boost/math/distributions/detail/common_error_handling.hpp>
-#include <boost/math/policies/error_handling.hpp>
-#include <boost/math/policies/policy.hpp>
-#include <Eigen/Dense>
-
-#include "stan/prob/transform.hpp"
 #include "stan/prob/distributions_error_handling.hpp"
 #include "stan/prob/distributions_constants.hpp"
 
+#include <stan/meta/traits.hpp>
+
 namespace stan {
+
   namespace prob {
-    using namespace std;
-    using namespace stan::maths;
+    using boost::math::tools::promote_args;
+    using boost::math::policies::policy;
 
     using Eigen::Matrix;
     using Eigen::Dynamic;
 
     // Multinomial(ns|N,theta)   [0 <= n <= N;  SUM ns = N;   
     //                            0 <= theta[n] <= 1;  SUM theta = 1]
-    template <typename T_prob, class Policy>
-    inline typename boost::math::tools::promote_args<T_prob>::type
+    template <bool propto = false,
+	      typename T_prob, class Policy = policy<> >
+    inline typename promote_args<T_prob>::type
     multinomial_log(const std::vector<int>& ns,
 		    const Matrix<T_prob,Dynamic,1>& theta, 
-		    const Policy& /* pol */) {
-      unsigned int len = ns.size();
-      double sum = 1.0;
-      for (unsigned int i = 0; i < len; ++i) 
-	sum += ns[i];
-      typename boost::math::tools::promote_args<T_prob>::type log_p
-	= lgamma(sum);
-      for (unsigned int i = 0; i < len; ++i)
-	log_p -= lgamma(ns[i] + 1.0);
-      for (unsigned int i = 0; i < len; ++i)
-	log_p += ns[i] * log(theta[i]);
-      return log_p;
+		    const Policy& = Policy()) {
+      // FIXME: domain checks
+      typename promote_args<T_prob>::type lp(0.0);
+      if (!propto) {	
+	double sum = 1.0;
+	for (unsigned int i = 0; i < ns.size(); ++i) 
+	  sum += ns[i];
+	lp += lgamma(sum);
+	for (unsigned int i = 0; i < ns.size(); ++i)
+	lp -= lgamma(ns[i] + 1.0);
+      }
+      if (!propto
+	  || !is_constant<T_prob>::value)
+	for (unsigned int i = 0; i < ns.size(); ++i)
+	  lp += ns[i] * log(theta[i]);
+      return lp;
     }
 
-    template <typename T_prob>
-    inline typename boost::math::tools::promote_args<T_prob>::type
-    multinomial_log(const std::vector<int>& ns,
-		    const Matrix<T_prob,Dynamic,1>& theta) {
-      return multinomial_log (ns, theta, boost::math::policies::policy<>());
-    }
-    
-    template <typename T_prob, class Policy>
-    inline typename boost::math::tools::promote_args<T_prob>::type
-    multinomial_propto_log(const std::vector<int>& ns,
-			   const Matrix<T_prob,Dynamic,1>& theta, 
-			   const Policy& /* pol */) {
-      return multinomial_log (ns, theta, Policy());
-    }
-
-    template <typename T_prob>
-    inline typename boost::math::tools::promote_args<T_prob>::type
-    multinomial_propto_log(const std::vector<int>& ns,
-			   const Matrix<T_prob,Dynamic,1>& theta) {
-      return multinomial_propto_log (ns, theta, boost::math::policies::policy<>());
-    }
 
   }
 }
--- src/test/prob/distributions_multinomial_test.cpp
+++ src/test/prob/distributions_multinomial_test.cpp
@@ -15,3 +15,12 @@ TEST(ProbDistributions,Multinomial) {
   theta << 0.2, 0.3, 0.5;
   EXPECT_FLOAT_EQ(-2.002481, stan::prob::multinomial_log(ns,theta));
 }
+TEST(ProbDistributions,MultinomialPropto) {
+  std::vector<int> ns;
+  ns.push_back(1);
+  ns.push_back(2);
+  ns.push_back(3);
+  Matrix<double,Dynamic,1> theta(3,1);
+  theta << 0.2, 0.3, 0.5;
+  EXPECT_FLOAT_EQ(0.0, stan::prob::multinomial_log<true>(ns,theta));
+}
