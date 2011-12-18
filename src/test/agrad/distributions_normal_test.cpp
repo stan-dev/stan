81d247448e4fdffa8ae1dfb48034a3ff1c4e6dd3
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2011-12-18 23:18:08 +0000

    agrad distributions test: normal
    git-svn-id: https://stan.googlecode.com/svn/trunk@362 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/test/agrad/distributions_normal_test.cpp
+++ src/test/agrad/distributions_normal_test.cpp
@@ -1,134 +1,45 @@
-#include <gtest/gtest.h>
 #include "stan/prob/distributions_normal.hpp"
 #include "stan/agrad/agrad.hpp"
+#include <gtest/gtest.h>
 
 using stan::agrad::var;
-TEST(AgradDistributions,Normal) {
-  var y;
+
+class AgradDistributionsPropto : public testing::Test {
+  protected:
+  virtual void SetUp() {
+    y1 = 0.3;
+    y2 = 0.6;
+    mu = 0.5;
+    sigma = 0.5;
+    
+    expected = stan::prob::normal_log<false>(y1,mu,sigma) 
+      - stan::prob::normal_log<false>(y2,mu,sigma);
+  }
+  var y1;
+  var y2;
   var mu;
   var sigma;
-  
-  y = 0.0;
-  mu = 0.0;
-  sigma = 1.0;
-  EXPECT_FLOAT_EQ(-0.9189385, stan::prob::normal_log(y,mu,sigma).val());
-  
-  y = 1.0;
-  mu = 0.0;
-  sigma = 1.0;
-  EXPECT_FLOAT_EQ(-1.418939,  stan::prob::normal_log(y,mu,sigma).val());
-  
-  y = -2.0;
-  mu = 0.0;
-  sigma = 1.0;
-  EXPECT_FLOAT_EQ(-2.918939,  stan::prob::normal_log(y,mu,sigma).val());
- 
-  y = -3.5;
-  mu = 1.9;
-  sigma = 7.2;
-  EXPECT_FLOAT_EQ(-3.174270,  stan::prob::normal_log(y,mu,sigma).val());
+  var expected;
+  var result;
+};
+
+TEST_F(AgradDistributionsPropto,Normal) {
+  result = stan::prob::normal_log<true>(y1,mu,sigma)
+    - stan::prob::normal_log<true>(y2,mu,sigma);
+  EXPECT_FLOAT_EQ(expected.val(),result.val());
 }
-TEST(AgradDistributionsPropto,Normal) {
-  double LOG_SQRT_TWO_PI = 0.9189385;
-  var y;
-  var mu;
-  var sigma;
-  
-  y = 0.0;
-  mu = 0.0;
-  sigma = 1.0;
-  EXPECT_FLOAT_EQ(-0.9189385+LOG_SQRT_TWO_PI, stan::prob::normal_log<true>(y,mu,sigma).val());
-  
-  y = 1.0;
-  mu = 0.0;
-  sigma = 1.0;
-  EXPECT_FLOAT_EQ(-1.41893853+LOG_SQRT_TWO_PI,  stan::prob::normal_log<true>(y,mu,sigma).val());
-  
-  y = -2.0;
-  mu = 0.0;
-  sigma = 1.0;
-  EXPECT_FLOAT_EQ(-2.918939+LOG_SQRT_TWO_PI,  stan::prob::normal_log<true>(y,mu,sigma).val());
- 
-  y = -3.5;
-  mu = 1.9;
-  sigma = 7.2;
-  EXPECT_FLOAT_EQ(-3.174270+LOG_SQRT_TWO_PI,  stan::prob::normal_log<true>(y,mu,sigma).val());
+TEST_F(AgradDistributionsPropto,NormalY) {
+  result = stan::prob::normal_log<true>(y1.val(),mu,sigma)
+    - stan::prob::normal_log<true>(y2.val(),mu,sigma);
+  EXPECT_FLOAT_EQ(expected.val(),result.val());
 }
-TEST(AgradDistributionsPropto,NormalY) {
-  double LOG_SQRT_TWO_PI = 0.9189385;
-  double y;
-  var mu;
-  var sigma;
-  
-  y = 0.0;
-  mu = 0.0;
-  sigma = 1.0;
-  EXPECT_FLOAT_EQ(-0.9189385+LOG_SQRT_TWO_PI, stan::prob::normal_log<true>(y,mu,sigma).val());
-  
-  y = 1.0;
-  mu = 0.0;
-  sigma = 1.0;
-  EXPECT_FLOAT_EQ(-1.41893853+LOG_SQRT_TWO_PI,  stan::prob::normal_log<true>(y,mu,sigma).val());
-  
-  y = -2.0;
-  mu = 0.0;
-  sigma = 1.0;
-  EXPECT_FLOAT_EQ(-2.918939+LOG_SQRT_TWO_PI,  stan::prob::normal_log<true>(y,mu,sigma).val());
- 
-  y = -3.5;
-  mu = 1.9;
-  sigma = 7.2;
-  EXPECT_FLOAT_EQ(-3.174270+LOG_SQRT_TWO_PI,  stan::prob::normal_log<true>(y,mu,sigma).val());
+TEST_F(AgradDistributionsPropto,NormalMu) {
+  result = stan::prob::normal_log<true>(y1,mu.val(),sigma)
+    - stan::prob::normal_log<true>(y2,mu.val(),sigma);
+  EXPECT_FLOAT_EQ(expected.val(),result.val());
 }
-TEST(AgradDistributionsPropto,NormalMu) {
-  double LOG_SQRT_TWO_PI = 0.9189385;
-  var y;
-  double mu;
-  var sigma;
-  
-  y = 0.0;
-  mu = 0.0;
-  sigma = 1.0;
-  EXPECT_FLOAT_EQ(-0.9189385+LOG_SQRT_TWO_PI, stan::prob::normal_log<true>(y,mu,sigma).val());
-  
-  y = 1.0;
-  mu = 0.0;
-  sigma = 1.0;
-  EXPECT_FLOAT_EQ(-1.41893853+LOG_SQRT_TWO_PI,  stan::prob::normal_log<true>(y,mu,sigma).val());
-  
-  y = -2.0;
-  mu = 0.0;
-  sigma = 1.0;
-  EXPECT_FLOAT_EQ(-2.918939+LOG_SQRT_TWO_PI,  stan::prob::normal_log<true>(y,mu,sigma).val());
- 
-  y = -3.5;
-  mu = 1.9;
-  sigma = 7.2;
-  EXPECT_FLOAT_EQ(-3.174270+LOG_SQRT_TWO_PI,  stan::prob::normal_log<true>(y,mu,sigma).val());
-}
-TEST(AgradDistributionsPropto,NormalSigma) {
-  double LOG_SQRT_TWO_PI = 0.9189385;
-  var y;
-  var mu;
-  double sigma;
-  
-  y = 0.0;
-  mu = 0.0;
-  sigma = 1.0;
-  EXPECT_FLOAT_EQ(-0.9189385+LOG_SQRT_TWO_PI+log(sigma), stan::prob::normal_log<true>(y,mu,sigma).val());
-  
-  y = 1.0;
-  mu = 0.0;
-  sigma = 1.0;
-  EXPECT_FLOAT_EQ(-1.41893853+LOG_SQRT_TWO_PI+log(sigma),  stan::prob::normal_log<true>(y,mu,sigma).val());
-  
-  y = -2.0;
-  mu = 0.0;
-  sigma = 1.0;
-  EXPECT_FLOAT_EQ(-2.918939+LOG_SQRT_TWO_PI+log(sigma),  stan::prob::normal_log<true>(y,mu,sigma).val());
- 
-  y = -3.5;
-  mu = 1.9;
-  sigma = 7.2;
-  EXPECT_NEAR(-3.1742756+LOG_SQRT_TWO_PI+log(sigma),  stan::prob::normal_log<true>(y,mu,sigma).val(),0.00001);
+TEST_F(AgradDistributionsPropto,NormalSigma) {
+  result = stan::prob::normal_log<true>(y1,mu,sigma.val())
+    - stan::prob::normal_log<true>(y2,mu,sigma.val());
+  EXPECT_FLOAT_EQ(expected.val(),result.val());
 }
