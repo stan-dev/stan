4b45a9de7bf0fdbc40ccef1c0d794dac0c7eb81e
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2012-01-05 17:06:03 +0000

    adding binomial agrad test
    git-svn-id: https://stan.googlecode.com/svn/trunk@467 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/stan/prob/distributions/binomial.hpp
+++ src/stan/prob/distributions/binomial.hpp
@@ -4,6 +4,7 @@
 #include <stan/prob/traits.hpp>
 #include <stan/prob/error_handling.hpp>
 #include <stan/prob/constants.hpp>
+#include <stan/maths/special_functions.hpp>
 
 namespace stan {
   namespace prob {
@@ -14,14 +15,21 @@ namespace stan {
     template <bool propto = false,
 	      typename T_n, typename T_N, typename T_prob, 
 	      class Policy = policy<> >
-    inline typename promote_args<T_prob>::type
+    inline typename promote_args<T_prob,T_n,T_N>::type
       binomial_log(const T_n& n, const T_N& N, const T_prob& theta, const Policy& = Policy()) {
       // FIXME: domain checks
-      typename promote_args<T_prob>::type lp(0.0);
-      if (include_summand<propto>::value)
-	lp += maths::binomial_coefficient_log<T_N>(N,n);
+      
+      using stan::maths::multiply_log;
+      using stan::maths::binomial_coefficient_log;
+      using stan::maths::log1m;
+
+      typename promote_args<T_prob,T_n,T_N>::type lp(0.0);
+      if (include_summand<propto,T_n,T_N>::value)
+	lp += binomial_coefficient_log<T_N,T_n>(N,n);
+      if (include_summand<propto,T_n,T_prob>::value)
+      	lp += multiply_log(n,theta);
       if (include_summand<propto,T_n,T_N,T_prob>::value)
-	lp += n * log(theta) + (N-n) * log1m (theta);
+	lp += (N-n) * log1m(theta);
       return lp;
     }
   }
---
+++ src/test/agrad/distributions/binomial_test.cpp
@@ -0,0 +1,80 @@
+#include <gtest/gtest.h>
+#include <test/agrad/distributions/expect_eq_diffs.hpp>
+#include <stan/agrad/agrad.hpp>
+#include <stan/agrad/special_functions.hpp>
+#include <stan/meta/traits.hpp>
+#include <stan/prob/distributions/binomial.hpp>
+
+template <typename T_n, typename T_N, typename T_prob>
+void expect_propto(T_n n1, T_N N1, T_prob theta1, 
+		   T_n n2, T_N N2, T_prob theta2, 
+		   std::string message) {
+  expect_eq_diffs(stan::prob::binomial_log<false>(n1, N1, theta1),
+		  stan::prob::binomial_log<false>(n2, N2, theta2),
+		  stan::prob::binomial_log<true>(n1, N1, theta1),
+		  stan::prob::binomial_log<true>(n2, N2, theta2),
+		  message);
+}
+
+using stan::agrad::var;
+
+TEST(AgradDistributionsBinomial,Propto) {
+  expect_propto<var, var, var>(2, 10, 0.3,
+			       3, 11, 0.75,
+			       "all var: n, N, and prob");
+}
+TEST(AgradDistributionsBinomial,ProptoN1) {
+  double N;
+  double theta;
+  
+  N = 12;
+  theta = 0.4;
+  expect_propto<var,double,double>(2, N, theta,
+				   3, N, theta,
+				   "var: n");
+}
+TEST(AgradDistributionsBinomial,ProptoN1N2) {
+  double theta;
+  
+  theta = 0.4;
+  expect_propto<var,var,double>(2, 4, theta,
+				3, 15, theta,
+				"var: n and N");
+}
+TEST(AgradDistributionsBinomial,ProptoN1Theta) {
+  double N;
+  
+  N = 15;
+  expect_propto<var,double,var>(2, N, 0.14,
+				3, N, 0.65,
+				"var: n and theta");
+  
+}
+TEST(AgradDistributionsBinomial,ProptoN2) {
+  double n;
+  double theta;
+  
+  n = 15;
+  theta = 0.345;
+  expect_propto<var,double,var>(n, 43, theta,
+				n, 112, theta,
+				"var: N");
+  
+}
+TEST(AgradDistributionsBinomial,ProptoN2Theta) {
+  double n;
+  
+  n = 15;
+  expect_propto<var,double,var>(n, 43, 0.14,
+				n, 112, 0.65,
+				"var: n and theta");
+}
+TEST(AgradDistributionsBinomial,ProptoTheta) {
+  double theta;
+  
+  theta = 0.95;
+  expect_propto<var,double,var>(40, 43, theta,
+				2, 112, theta,
+				"var: theta");
+}
+
