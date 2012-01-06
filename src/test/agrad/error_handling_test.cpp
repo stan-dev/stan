f2849a46ef2aefa7d36d4a469e0594ba7e90893b
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2012-01-06 21:03:46 +0000

    adding error handling tests
    git-svn-id: https://stan.googlecode.com/svn/trunk@477 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/test/agrad/error_handling_test.cpp
+++ src/test/agrad/error_handling_test.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 #include <stan/prob/error_handling.hpp>
 #include <stan/agrad/agrad.hpp>
-#include <limits>
+//#include <limits>
 #include <stan/agrad/matrix.hpp>
 
 using Eigen::Dynamic;
@@ -18,6 +18,64 @@ typedef boost::math::policies::policy<> default_policy;
 
 using namespace stan::prob;
 using stan::agrad::var;
+
+//---------- check_not_nan tests ----------
+TEST(AgradDistributionsErrorHandling,CheckNotNanDefaultPolicy) {
+  const char* function = "check_not_nan(%1%)";
+  var x = 0;
+  double x_d = 0;
+  var result;
+ 
+  EXPECT_TRUE(check_not_nan(function, x, "x", &result, default_policy())) << "check_not_nan should be true with finite x: " << x;
+  EXPECT_TRUE(check_not_nan(function, x_d, "x", &result, default_policy())) << "check_not_nan should be true with finite x: " << x_d;
+  
+  x = std::numeric_limits<var>::infinity();
+  x_d = std::numeric_limits<double>::infinity();
+  EXPECT_TRUE(check_not_nan(function, x, "x", &result, default_policy())) << "check_not_nan should be true with x = Inf: " << x;
+  EXPECT_TRUE(check_not_nan(function, x_d, "x", &result, default_policy())) << "check_not_nan should be true with x = Inf: " << x_d;
+
+  x = -std::numeric_limits<var>::infinity();
+  x_d = -std::numeric_limits<double>::infinity();
+  EXPECT_TRUE(check_not_nan(function, x, "x", &result, default_policy())) << "check_not_nan should be true with x = -Inf: " << x;
+  EXPECT_TRUE(check_not_nan(function, x_d, "x", &result, default_policy())) << "check_not_nan should be true with x = -Inf: " << x_d;
+
+  x = std::numeric_limits<var>::quiet_NaN();
+  x_d = std::numeric_limits<double>::quiet_NaN();
+  EXPECT_THROW(check_not_nan(function, x, "x", &result, default_policy()), std::domain_error) << "check_not_nan should throw exception on NaN: " << x;
+  EXPECT_THROW(check_not_nan(function, x_d, "x", &result, default_policy()), std::domain_error) << "check_not_nan should throw exception on NaN: " << x_d;
+}
+
+TEST(AgradDistributionsErrorHandling,CheckNotNanErrnoPolicy) {
+  const char* function = "check_not_nan(%1%)";
+  var x = 0;
+  double x_d = 0;
+  var result;
+ 
+  EXPECT_TRUE(check_not_nan(function, x, "x", &result, errno_policy())) << "check_not_nan should be true with finite x: " << x;
+  EXPECT_TRUE(check_not_nan(function, x_d, "x", &result, errno_policy())) << "check_not_nan should be true with finite x: " << x;
+  
+  x = std::numeric_limits<var>::infinity();
+  EXPECT_TRUE(check_not_nan(function, x, "x", &result, errno_policy())) << "check_not_nan should be true with x = Inf: " << x;
+  EXPECT_FALSE(std::isnan (result)) << "check_not_nan should not have returned nan: " << x;
+  x_d = std::numeric_limits<double>::infinity();
+  EXPECT_TRUE(check_not_nan(function, x_d, "x", &result, errno_policy())) << "check_not_nan should be true with x = Inf: " << x_d;
+  EXPECT_FALSE(std::isnan (result)) << "check_not_nan should not have returned nan: " << x_d;
+
+  x = -std::numeric_limits<var>::infinity();
+  EXPECT_TRUE(check_not_nan (function, x, "x", &result, errno_policy())) << "check_not_nan should be true with x = -Inf: " << x;
+  EXPECT_FALSE(std::isnan (result)) << "check_not_nan should have returned nan: " << x;
+  x_d = -std::numeric_limits<double>::infinity();
+  EXPECT_TRUE(check_not_nan (function, x_d, "x", &result, errno_policy())) << "check_not_nan should be true with x = -Inf: " << x_d;
+  EXPECT_FALSE(std::isnan (result)) << "check_not_nan should have returned nan: " << x_d;
+ 
+  x = std::numeric_limits<double>::quiet_NaN();
+  EXPECT_FALSE(check_not_nan (function, x, "x", &result, errno_policy())) << "check_not_nan should return FALSE on nan: " << x;
+  EXPECT_TRUE(std::isnan (result)) << "check_not_nan should have returned nan: " << x;
+  x_d = std::numeric_limits<double>::quiet_NaN();
+  EXPECT_FALSE(check_not_nan (function, x_d, "x", &result, errno_policy())) << "check_not_nan should return FALSE on nan: " << x_d;
+  EXPECT_TRUE(std::isnan (result)) << "check_not_nan should have returned nan: " << x_d;
+}
+
 
 //---------- check_x tests ----------
 TEST(AgradDistributionsErrorHandling,CheckXDefaultPolicy) {
