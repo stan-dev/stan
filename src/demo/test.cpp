4aa60ff5af263876ae0900b4cc38e20712987205
carp@alias-i.com <carp@alias-i.com@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2012-01-15 02:40:24 +0000

    test.cpp now has working var version of multi norm
    git-svn-id: https://stan.googlecode.com/svn/trunk@532 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/demo/test.cpp
+++ src/demo/test.cpp
@@ -1,24 +1,67 @@
 #include <iostream>
 #include <vector>
-#include <stan/agrad/agrad.hpp>
+#include <stan/maths/matrix.hpp>
+#include <stan/agrad/matrix.hpp>
 #include <stan/agrad/special_functions.hpp>
 
-int main() {
-  using stan::agrad::var;
-  using stan::agrad::vari;
-  using stan::agrad::log_sum_exp;
-  unsigned int I = 3;
-  std::vector<var> vs(I);
-  for (unsigned int i = 0; i < I; ++i)
-    vs[i] = i;
+using Eigen::Dynamic;
+using Eigen::Matrix;
+using stan::agrad::var;
+using stan::maths::subtract;
+using boost::math::tools::promote_args;
 
-  var lse = log_sum_exp(vs);
 
-  std::cout << "sizeof(var)=" << sizeof(var) << std::endl;
-  std::cout << "sizeof(vari)=" << sizeof(vari) << std::endl;
-  std::cout << "sizeof(vector<vari*>)=" << sizeof(std::vector<vari*>) << std::endl;
+var 
+multivar_norm(Matrix<var,Dynamic,1>& y,
+              Matrix<var,Dynamic,1>& mu,
+              Matrix<var,Dynamic,Dynamic>& L) {
 
-  std::cout << "DONE" << std::endl;
+  var lp(0.0);
 
+  // if #1
+  lp += 2.0 * y.rows();
+  
+  // if #2
+  for (unsigned int m = 0; m < L.rows(); ++m)
+    lp += log(L(m,m));
+  
+  // if #3
+  Eigen::Matrix<var,Dynamic,1> diff 
+    = subtract(y,mu);
+
+  Eigen::Matrix<var,Dynamic,Dynamic> L_inv
+    = L.triangularView<Eigen::Lower>().solve(Matrix<var,Dynamic,Dynamic>::Identity(L.rows(),L.rows()));
+
+  std::cout << "L_inv=" << L_inv << std::endl;
+
+  Eigen::Matrix<var,Dynamic,1> half 
+    = multiply(L_inv,diff);
+
+  lp -= 0.5 * half.dot(half);
+
+  return lp;
 
 }
+
+int main() {
+
+  Eigen::Matrix<var,Dynamic,1> y(3);
+  y << -1, -2, -3;
+
+  Eigen::Matrix<var,Dynamic,1> mu(3);
+  mu << 0.0, -1.0, 1.0;
+
+  // only care about lower-part of argument A, but create it all
+  Eigen::Matrix<var,Dynamic,Dynamic> A(3,3);
+  A << 1, 2, 3,
+       2, 4, 8, 
+       5, 20, 50;
+
+  // L now is just the lower-triangular part of A
+  Eigen::Matrix<var,Dynamic,Dynamic> L
+    = A.triangularView<Eigen::Lower>();
+
+  var lp = multivar_norm(y,mu,L);
+  std::cout << "lp=" << lp << std::endl;
+}
+
