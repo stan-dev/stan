360e75bcd5344ec86ec9e63cc7cd2a53de217e4e
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2012-01-18 19:44:36 +0000

    added cauchy tests.
    git-svn-id: https://stan.googlecode.com/svn/trunk@555 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/stan/prob/distributions/cauchy.hpp
+++ src/stan/prob/distributions/cauchy.hpp
@@ -14,28 +14,28 @@ namespace stan {
 
     // Cauchy(y|mu,sigma)  [sigma > 0]
     template <bool propto = false,
-	      typename T_y, typename T_loc, typename T_scale, 
-	      class Policy = policy<> >
+              typename T_y, typename T_loc, typename T_scale, 
+              class Policy = policy<> >
     inline typename promote_args<T_y,T_loc,T_scale>::type
     cauchy_log(const T_y& y, const T_loc& mu, const T_scale& sigma, const Policy& = Policy()) {
       static const char* function = "stan::prob::cauchy_log<%1%>(%1%)";
 
       typename promote_args<T_y,T_loc,T_scale>::type lp(0.0);
-      if(!stan::prob::check_scale(function, sigma, &lp, Policy()))
-	return lp;
-      if(!stan::prob::check_location(function, mu, &lp, Policy()))
-	return lp;
-      if(!stan::prob::check_x(function, y, &lp, Policy()))
-	return lp;
+      if(!stan::prob::check_positive(function, sigma, "Scale parameter, sigma,", &lp, Policy()))
+        return lp;
+      if(!stan::prob::check_finite(function, mu, "Location parameter, mu,", &lp, Policy()))
+        return lp;
+      if(!stan::prob::check_not_nan(function, y, "Random variate, y,", &lp, Policy()))
+        return lp;
 
       using stan::maths::log1p;
       
       if (include_summand<propto>::value)
-	lp += NEG_LOG_PI;
+        lp += NEG_LOG_PI;
       if (include_summand<propto,T_scale>::value)
-	lp -= log(sigma);
+        lp -= log(sigma);
       if (include_summand<propto,T_y,T_loc,T_scale>::value)
-	lp -= log1p(square ((y - mu) / sigma));
+        lp -= log1p(square ((y - mu) / sigma));
       return lp;
     }
 
--- src/test/prob/distributions/cauchy_test.cpp
+++ src/test/prob/distributions/cauchy_test.cpp
@@ -1,16 +1,100 @@
 #include <gtest/gtest.h>
 #include <stan/prob/distributions/cauchy.hpp>
 
-TEST(ProbDistributions,Cauchy) {
+using boost::math::policies::policy;
+using boost::math::policies::evaluation_error;
+using boost::math::policies::domain_error;
+using boost::math::policies::overflow_error;
+using boost::math::policies::domain_error;
+using boost::math::policies::pole_error;
+using boost::math::policies::errno_on_error;
+
+typedef policy<
+  domain_error<errno_on_error>, 
+  pole_error<errno_on_error>,
+  overflow_error<errno_on_error>,
+  evaluation_error<errno_on_error> 
+  > errno_policy;
+
+using stan::prob::cauchy_log;
+
+TEST(ProbDistributionsCauchy,Cauchy) {
   EXPECT_FLOAT_EQ(-1.837877, stan::prob::cauchy_log(1.0, 0.0, 1.0));
   EXPECT_FLOAT_EQ(-2.323385, stan::prob::cauchy_log(-1.5, 0.0, 1.0));
   EXPECT_FLOAT_EQ(-2.323385, stan::prob::cauchy_log(-2.5, -1.0, 1.0));
   // need test with scale != 1
 }
-TEST(ProbDistributions,CauchyPropto) {
+TEST(ProbDistributionsCauchy,Propto) {
   EXPECT_FLOAT_EQ(0.0, stan::prob::cauchy_log<true>(1.0, 0.0, 1.0));
   EXPECT_FLOAT_EQ(0.0, stan::prob::cauchy_log<true>(-1.5, 0.0, 1.0));
   EXPECT_FLOAT_EQ(0.0, stan::prob::cauchy_log<true>(-2.5, -1.0, 1.0));
   // need test with scale != 1
 }
+TEST(ProbDistributionsCauchy,DefaultPolicy) {
+  double nan = std::numeric_limits<double>::quiet_NaN();
+  double inf = std::numeric_limits<double>::infinity();
 
+  double y = 0.5;
+  double mu = 2.0;
+  double sigma = 3.0;
+  
+  EXPECT_NO_THROW(cauchy_log(y, mu, sigma));
+  EXPECT_NO_THROW(cauchy_log(inf, mu, sigma));
+  EXPECT_NO_THROW(cauchy_log(-inf, mu, sigma));
+  EXPECT_NO_THROW(cauchy_log(y, 0.0, sigma));
+  EXPECT_NO_THROW(cauchy_log(y, -1.0, sigma));
+  
+  EXPECT_THROW(cauchy_log(nan, mu, sigma), std::domain_error);
+
+  EXPECT_THROW(cauchy_log(y, -inf, sigma), std::domain_error);
+  EXPECT_THROW(cauchy_log(y, nan, sigma), std::domain_error);
+  EXPECT_THROW(cauchy_log(y, inf, sigma), std::domain_error);
+
+  EXPECT_THROW(cauchy_log(y, mu, 0.0), std::domain_error);
+  EXPECT_THROW(cauchy_log(y, mu, -1.0), std::domain_error);
+  EXPECT_THROW(cauchy_log(y, mu, -inf), std::domain_error);
+  EXPECT_THROW(cauchy_log(y, mu, nan), std::domain_error);
+  EXPECT_THROW(cauchy_log(y, mu, inf), std::domain_error);
+}
+TEST(ProbDistributionsCauchy,ErrnoPolicy) {
+  double nan = std::numeric_limits<double>::quiet_NaN();
+  double inf = std::numeric_limits<double>::infinity();
+  
+  double result;
+  double y = 0.5;
+  double mu = 2.0;
+  double sigma = 3.0;
+  
+  result = cauchy_log(y, mu, sigma, errno_policy());
+  EXPECT_FALSE(std::isnan(result));
+  result = cauchy_log(inf, mu, sigma, errno_policy());
+  EXPECT_FALSE(std::isnan(result));
+  result = cauchy_log(-inf, mu, sigma, errno_policy());
+  EXPECT_FALSE(std::isnan(result));
+  result = cauchy_log(y, 0.0, sigma, errno_policy()); 
+  EXPECT_FALSE(std::isnan(result));
+  result = cauchy_log(y, -1.0, sigma, errno_policy()); 
+  EXPECT_FALSE(std::isnan(result));
+    
+
+  result = cauchy_log(nan, mu, sigma, errno_policy()); 
+  EXPECT_TRUE(std::isnan(result));
+
+  result = cauchy_log(y, -inf, sigma, errno_policy()); 
+  EXPECT_TRUE(std::isnan(result));
+  result = cauchy_log(y, nan, sigma, errno_policy()); 
+  EXPECT_TRUE(std::isnan(result));
+  result = cauchy_log(y, inf, sigma, errno_policy()); 
+  EXPECT_TRUE(std::isnan(result));
+
+  result = cauchy_log(y, mu, 0.0, errno_policy()); 
+  EXPECT_TRUE(std::isnan(result));
+  result = cauchy_log(y, mu, -1.0, errno_policy()); 
+  EXPECT_TRUE(std::isnan(result));
+  result = cauchy_log(y, mu, -inf, errno_policy()); 
+  EXPECT_TRUE(std::isnan(result));
+  result = cauchy_log(y, mu, nan, errno_policy()); 
+  EXPECT_TRUE(std::isnan(result));
+  result = cauchy_log(y, mu, inf, errno_policy()); 
+  EXPECT_TRUE(std::isnan(result));
+}
