f189b9fdbea4cac6e6bf7999abb5ef81eb69d0de
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2012-01-27 05:58:23 +0000

    updating distributions error handling: beta
    git-svn-id: https://stan.googlecode.com/svn/trunk@606 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/stan/prob/distributions/beta.hpp
+++ src/stan/prob/distributions/beta.hpp
@@ -2,8 +2,9 @@
 #define __STAN__PROB__DISTRIBUTIONS__BETA_HPP__
 
 #include <stan/prob/traits.hpp>
+#include <stan/maths/error_handling.hpp>
 #include <stan/prob/constants.hpp>
-#include <stan/prob/error_handling.hpp>
+
 
 namespace stan {
   namespace prob {
@@ -27,29 +28,35 @@ namespace stan {
      */
     template <bool propto = false,
               typename T_y, typename T_scale_succ, typename T_scale_fail,
-              class Policy = boost::math::policies::policy<> >
-      inline typename boost::math::tools::promote_args<T_y,T_scale_succ,T_scale_fail>::type
-      beta_log(const T_y& y, const T_scale_succ& alpha, const T_scale_fail& beta, 
-               const Policy& = Policy()) {
+              class Policy = stan::maths::default_policy>
+    inline typename boost::math::tools::promote_args<T_y,T_scale_succ,T_scale_fail>::type
+    beta_log(const T_y& y, const T_scale_succ& alpha, const T_scale_fail& beta, 
+             const Policy& = Policy()) {
       static const char* function = "stan::prob::beta_log<%1%>(%1%)";
-
-      typename boost::math::tools::promote_args<T_y,T_scale_succ,T_scale_fail>::type lp(0.0);
-      if (!stan::prob::check_positive(function, alpha, 
-                                     "Prior success sample size plus 1, alpha,",
-                                     &lp, Policy()))
+      
+      using stan::maths::check_positive;
+      using stan::maths::check_not_nan;
+      using boost::math::tools::promote_args;
+      
+      typename promote_args<T_y,T_scale_succ,T_scale_fail>::type lp;
+      if (!check_positive(function, alpha, 
+                          "Prior success sample size plus 1, alpha,",
+                          &lp, Policy()))
         return lp;
-      if (!stan::prob::check_positive(function, beta, 
-                                     "Prior failure sample size plus 1, beta,",
-                                     &lp, Policy()))
+      if (!check_positive(function, beta, 
+                          "Prior failure sample size plus 1, beta,",
+                          &lp, Policy()))
         return lp;
-      if (!stan::prob::check_not_nan(function, y, "Random variate, y,", &lp, Policy()))
+      if (!check_not_nan(function, y, "Random variate, y,", &lp, Policy()))
         return lp;
       
       if (y < 0.0 || y > 1.0)
         return LOG_ZERO;
-
+      
       using stan::maths::multiply_log;
       using stan::maths::log1m;
+
+      lp = 0.0;
       if (include_summand<propto,T_scale_succ,T_scale_fail>::value)
         lp += lgamma(alpha + beta);
       if (include_summand<propto,T_scale_succ>::value)
