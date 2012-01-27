81c6e60555347785719419669b9ec0b24bb30689
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2012-01-27 06:06:08 +0000

    updating distributions error handling: pareto
    git-svn-id: https://stan.googlecode.com/svn/trunk@608 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/stan/prob/distributions/pareto.hpp
+++ src/stan/prob/distributions/pareto.hpp
@@ -1,9 +1,9 @@
 #ifndef __STAN__PROB__DISTRIBUTIONS__PARETO_HPP__
 #define __STAN__PROB__DISTRIBUTIONS__PARETO_HPP__
 
-#include <stan/prob/traits.hpp>
-#include <stan/prob/error_handling.hpp>
 #include <stan/prob/constants.hpp>
+#include <stan/maths/error_handling.hpp>
+#include <stan/prob/traits.hpp>
 
 namespace stan {
   namespace prob {
@@ -11,12 +11,16 @@ namespace stan {
     // Pareto(y|y_m,alpha)  [y > y_m;  y_m > 0;  alpha > 0]
     template <bool propto = false,
               typename T_y, typename T_scale, typename T_shape, 
-              class Policy = boost::math::policies::policy<> >
+              class Policy = stan::maths::default_policy>
     inline typename boost::math::tools::promote_args<T_y,T_scale,T_shape>::type
     pareto_log(const T_y& y, const T_scale& y_min, const T_shape& alpha, const Policy& = Policy()) {
       static const char* function = "stan::prob::pareto_log<%1%>(%1%)";
       
-      typename boost::math::tools::promote_args<T_y,T_scale,T_shape>::type lp(0.0);
+      using stan::maths::check_positive;
+      using stan::maths::check_not_nan;
+      using boost::math::tools::promote_args;
+      
+      typename promote_args<T_y,T_scale,T_shape>::type lp;
       if (!check_positive(function, y_min, "Scale parameter, y_min,", &lp, Policy()))
         return lp;
       if (!check_positive(function, alpha, "Shape parameter, alpha,", &lp, Policy()))
@@ -29,6 +33,7 @@ namespace stan {
           
       using stan::maths::multiply_log;
 
+      lp = 0.0;
       if (include_summand<propto,T_shape>::value)
         lp += log(alpha);
       if (include_summand<propto,T_scale,T_shape>::value)
