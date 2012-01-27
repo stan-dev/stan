8f7e303637552af301663037e007f67d034a7647
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2012-01-27 06:09:31 +0000

    updating distributions error handling: double_exponential
    git-svn-id: https://stan.googlecode.com/svn/trunk@609 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/stan/prob/distributions/double_exponential.hpp
+++ src/stan/prob/distributions/double_exponential.hpp
@@ -1,11 +1,9 @@
 #ifndef __STAN__PROB__DISTRIBUTIONS__DOUBLE_EXPONENTIAL_HPP__
 #define __STAN__PROB__DISTRIBUTIONS__DOUBLE_EXPONENTIAL_HPP__
 
-#include <stan/prob/traits.hpp>
-#include <stan/prob/error_handling.hpp>
 #include <stan/prob/constants.hpp>
-
-
+#include <stan/maths/error_handling.hpp>
+#include <stan/prob/traits.hpp>
 
 namespace stan {
   namespace prob {
@@ -13,17 +11,21 @@ namespace stan {
     // DoubleExponential(y|mu,sigma)  [sigma > 0]
     template <bool propto = false,
               typename T_y, typename T_loc, typename T_scale, 
-              class Policy = boost::math::policies::policy<> > 
+              class Policy = stan::maths::default_policy> 
     inline typename boost::math::tools::promote_args<T_y,T_loc,T_scale>::type
     double_exponential_log(const T_y& y, const T_loc& mu, const T_scale& sigma, const Policy& = Policy()) {
       static const char* function = "stan::prob::double_exponential_log<%1%>(%1%)";
       
-      typename boost::math::tools::promote_args<T_y,T_loc,T_scale>::type lp(0.0);
-      if(!stan::prob::check_finite(function, y, "Random variate, y,", &lp, Policy()))
+      using stan::maths::check_finite;
+      using stan::maths::check_positive;
+      using boost::math::tools::promote_args;
+
+      typename promote_args<T_y,T_loc,T_scale>::type lp(0.0);
+      if(!check_finite(function, y, "Random variate, y,", &lp, Policy()))
         return lp;
-      if(!stan::prob::check_finite(function, mu, "Location parameter, mu,", &lp, Policy()))
+      if(!check_finite(function, mu, "Location parameter, mu,", &lp, Policy()))
         return lp;
-      if(!stan::prob::check_positive(function, sigma, "Scale parameter, sigma,", &lp, Policy()))
+      if(!check_positive(function, sigma, "Scale parameter, sigma,", &lp, Policy()))
         return lp;
 
       if (include_summand<propto>::value)
