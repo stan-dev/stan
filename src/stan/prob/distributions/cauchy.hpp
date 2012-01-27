720f1ce3e49f46afb4a91aa5b5661682914f481e
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2012-01-27 06:03:05 +0000

    updating distributions error handling: cauchy
    git-svn-id: https://stan.googlecode.com/svn/trunk@607 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/stan/prob/distributions/cauchy.hpp
+++ src/stan/prob/distributions/cauchy.hpp
@@ -2,7 +2,7 @@
 #define __STAN__PROB__DISTRIBUTIONS__CAUCHY_HPP__
 
 #include <stan/prob/traits.hpp>
-#include <stan/prob/error_handling.hpp>
+#include <stan/maths/error_handling.hpp>
 #include <stan/prob/constants.hpp>
 
 namespace stan {
@@ -10,17 +10,22 @@ namespace stan {
     // Cauchy(y|mu,sigma)  [sigma > 0]
     template <bool propto = false,
               typename T_y, typename T_loc, typename T_scale, 
-              class Policy = boost::math::policies::policy<> >
+              class Policy = stan::maths::default_policy>
     inline typename boost::math::tools::promote_args<T_y,T_loc,T_scale>::type
     cauchy_log(const T_y& y, const T_loc& mu, const T_scale& sigma, const Policy& = Policy()) {
       static const char* function = "stan::prob::cauchy_log<%1%>(%1%)";
-
-      typename boost::math::tools::promote_args<T_y,T_loc,T_scale>::type lp(0.0);
-      if(!stan::prob::check_positive(function, sigma, "Scale parameter, sigma,", &lp, Policy()))
+      
+      using stan::maths::check_positive;
+      using stan::maths::check_finite;
+      using stan::maths::check_not_nan;
+      using boost::math::tools::promote_args;
+      
+      typename promote_args<T_y,T_loc,T_scale>::type lp(0.0);
+      if(!check_positive(function, sigma, "Scale parameter, sigma,", &lp, Policy()))
         return lp;
-      if(!stan::prob::check_finite(function, mu, "Location parameter, mu,", &lp, Policy()))
+      if(!check_finite(function, mu, "Location parameter, mu,", &lp, Policy()))
         return lp;
-      if(!stan::prob::check_not_nan(function, y, "Random variate, y,", &lp, Policy()))
+      if(!check_not_nan(function, y, "Random variate, y,", &lp, Policy()))
         return lp;
 
       using stan::maths::log1p;
