79df65e9a304e692a4f32ca148bc46c44467323a
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2012-01-27 06:42:19 +0000

    updating distributions error handling: beta_binomial
    git-svn-id: https://stan.googlecode.com/svn/trunk@617 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/stan/prob/distributions/beta_binomial.hpp
+++ src/stan/prob/distributions/beta_binomial.hpp
@@ -1,23 +1,26 @@
 #ifndef __STAN__PROB__DISTRIBUTIONS__BETA_BINOMIAL_HPP__
 #define __STAN__PROB__DISTRIBUTIONS__BETA_BINOMIAL_HPP__
 
-#include <stan/prob/traits.hpp>
-#include <stan/prob/error_handling.hpp>
 #include <stan/prob/constants.hpp>
-#include <stan/maths/special_functions.hpp>
+#include <stan/maths/error_handling.hpp>
+#include <stan/prob/traits.hpp>
 
 namespace stan {
   namespace prob {
     // BetaBinomial(n|alpha,beta) [alpha > 0;  beta > 0;  n >= 0]
     template <bool propto = false,
               typename T_size, 
-              class Policy = boost::math::policies::policy<> >
+              class Policy = stan::maths::default_policy>
     inline typename boost::math::tools::promote_args<T_size>::type
-    beta_binomial_log(const int n, const int N, const T_size& alpha, const T_size& beta,
+    beta_binomial_log(const int n, const int N, const T_size& alpha, const T_size& beta, 
                       const Policy& = Policy()) {
       static const char* function = "stan::prob::beta_binomial_log<%1%>(%1%)";
 
-      typename boost::math::tools::promote_args<T_size>::type lp(0.0);
+      using stan::maths::check_nonnegative;
+      using stan::maths::check_positive;
+      using boost::math::tools::promote_args;
+
+      typename promote_args<T_size>::type lp;
       if (!check_nonnegative(function, N, "Sample size, N,", &lp, Policy()))
         return lp;
       if (!check_positive(function, alpha, "Prior sample size, alpha,", &lp, Policy()))
@@ -31,6 +34,7 @@ namespace stan {
       using stan::maths::lbeta;
       using stan::maths::binomial_coefficient_log;
 
+      lp = 0.0;
       if (include_summand<propto>::value)
         lp += binomial_coefficient_log(N,n);
       if (include_summand<propto,T_size>::value)
