8cb924634698d388cc8141f15f8bc7e54f4161cc
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2012-01-27 06:36:38 +0000

    updating distributions error handling: poisson
    git-svn-id: https://stan.googlecode.com/svn/trunk@615 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/stan/prob/distributions/poisson.hpp
+++ src/stan/prob/distributions/poisson.hpp
@@ -1,31 +1,35 @@
 #ifndef __STAN__PROB__DISTRIBUTIONS__POISSON_HPP__
 #define __STAN__PROB__DISTRIBUTIONS__POISSON_HPP__
 
-#include <stan/prob/traits.hpp>
 #include <stan/prob/constants.hpp>
-#include <stan/prob/error_handling.hpp>
+#include <stan/maths/error_handling.hpp>
+#include <stan/prob/traits.hpp>
 
 namespace stan {
   namespace prob {
     // Poisson(n|lambda)  [lambda > 0;  n >= 0]
     template <bool propto = false,
               typename T_rate, 
-              class Policy = boost::math::policies::policy<> >
+              class Policy = stan::maths::default_policy>
     inline typename boost::math::tools::promote_args<T_rate>::type
     poisson_log(const unsigned int n, const T_rate& lambda, const Policy& = Policy()) {
       static const char* function = "stan::prob::poisson_log<%1%>(%1%)";
       
-      typename boost::math::tools::promote_args<T_rate>::type lp(0.0);
-      if(!stan::prob::check_nonnegative(function, lambda, "Rate parameter, lambda,", &lp, Policy()))
+      using stan::maths::check_nonnegative;
+      using boost::math::tools::promote_args;
+
+      typename promote_args<T_rate>::type lp;
+      if(!check_nonnegative(function, lambda, "Rate parameter, lambda,", &lp, Policy()))
         return lp;
-      if(!stan::prob::check_nonnegative(function, n, "Number n", &lp, Policy()))
+      if(!check_nonnegative(function, n, "Number n", &lp, Policy()))
         return lp;
       
       if (lambda == 0)
         return LOG_ZERO;
-
+      
       using stan::maths::multiply_log;
-
+      
+      lp = 0.0;
       if (include_summand<propto>::value)
         lp -= lgamma(n + 1.0);
       if (include_summand<propto,T_rate>::value)
