07639521f7216d1f8127b3dc56fe341bfba39cb8
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2012-01-29 02:52:55 +0000

    added folders for distributions
    git-svn-id: https://stan.googlecode.com/svn/trunk@631 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/stan/prob/distributions/multivariate.hpp
+++ src/stan/prob/distributions/multivariate.hpp
@@ -1,7 +1,7 @@
 #ifndef __STAN__PROB__DISTRIBUTIONS__MULTIVARIATE_HPP__
 #define __STAN__PROB__DISTRIBUTIONS__MULTIVARIATE_HPP__
 
-#include <stan/prob/distributions/multivariate/scalar.hpp>
-#include <stan/prob/distributions/multivariate/int.hpp>
+#include <stan/prob/distributions/multivariate/continuous.hpp>
+#include <stan/prob/distributions/multivariate/discrete.hpp>
 
 #endif
--- src/stan/prob/distributions/univariate.hpp
+++ src/stan/prob/distributions/univariate.hpp
@@ -1,7 +1,7 @@
 #ifndef __STAN__PROB__DISTRIBUTIONS__UNIVARIATE_HPP__
 #define __STAN__PROB__DISTRIBUTIONS__UNIVARIATE_HPP__
 
-#include <stan/prob/distributions/univariate/scalar.hpp>
-#include <stan/prob/distributions/univariate/int.hpp>
+#include <stan/prob/distributions/univariate/continuous.hpp>
+#include <stan/prob/distributions/univariate/discrete.hpp>
 
 #endif
