9e7f1a9f678a541b48dc5a3d67fe2f5ef6854d43
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2012-01-30 22:23:27 +0000

    split matrix parts of stan/maths/error_handling to stan/maths/matrix_error_handling
    git-svn-id: https://stan.googlecode.com/svn/trunk@652 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/stan/prob/distributions/multivariate/discrete.hpp
+++ src/stan/prob/distributions/multivariate/discrete.hpp
@@ -1,6 +1,6 @@
 #ifndef __STAN__PROB__DISTRIBUTIONS__MULTIVARIATE__DISCRETE_HPP__
 #define __STAN__PROB__DISTRIBUTIONS__MULTIVARIATE__DISCRETE_HPP__
 
-#include <stan/prob/distributions/univariate/discrete/categorical.hpp>
+#include <stan/prob/distributions/multivariate/discrete/categorical.hpp>
 
 #endif
