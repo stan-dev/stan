cc650cce6237a7e1a4e13faae52152d6a2f6ed7c
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2012-01-30 22:14:37 +0000

    split matrix parts of stan/maths/error_handling to stan/maths/matrix_error_handling
    git-svn-id: https://stan.googlecode.com/svn/trunk@651 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/stan/prob/distributions/univariate/discrete.hpp
+++ src/stan/prob/distributions/univariate/discrete.hpp
@@ -2,7 +2,6 @@
 #define __STAN__PROB__DISTRIBUTIONS__UNIVARIATE__DISCRETE_HPP__
 
 #include <stan/prob/distributions/univariate/discrete/bernoulli.hpp>
-#include <stan/prob/distributions/univariate/discrete/categorical.hpp>
 #include <stan/prob/distributions/univariate/discrete/binomial.hpp>
 #include <stan/prob/distributions/univariate/discrete/poisson.hpp>
 #include <stan/prob/distributions/univariate/discrete/neg_binomial.hpp>
