33ab3633cecab30c4b69b6b2aebb7524d2705931
carp@alias-i.com <carp@alias-i.com@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2012-02-10 07:44:00 +0000

    removed unneeded chi sq test of wishart
    git-svn-id: https://stan.googlecode.com/svn/trunk@729 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

--- src/test/prob/distributions/multivariate/continuous/wishart_test.cpp
+++ src/test/prob/distributions/multivariate/continuous/wishart_test.cpp
@@ -166,19 +166,3 @@ TEST(ProbDistributionsWishart,ErrnoPolicy) {
   result = wishart_log(Y, nu, Sigma, errno_policy());
   EXPECT_TRUE(std::isnan(result));
 }
-
-TEST(ProbDistroWishart,chiSqEquiv) {
-  double s = 1.9;
-  double w = 2.5;
-  double nu = 2.0;
-
-  Matrix<double,Dynamic,Dynamic> Sigma(1,1);
-  Sigma(0,0) = s;
-  Matrix<double,Dynamic,Dynamic> W(1,1);
-  W(0,0) = w;
-
-  
-
-  EXPECT_FLOAT_EQ(stan::prob::chi_square_log<false>(w, nu/2.0, 1.0/(2.0 * w)),
-                  stan::prob::wishart_log(W,nu,Sigma));
-}
