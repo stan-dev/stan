2beeddadd35d560dfca2a20f2ecbb3f0e8524896
bearlee@alum.mit.edu <bearlee@alum.mit.edu@9a304bd9-dce1-f7c0-8d5c-bb1642157d4e>
2012-02-16 03:57:57 +0000

    created src/stan/model/model_header.hpp for use in generating models.
    git-svn-id: https://stan.googlecode.com/svn/trunk@778 9a304bd9-dce1-f7c0-8d5c-bb1642157d4e

---
+++ src/stan/model/model_header.hpp
@@ -0,0 +1,21 @@
+#ifndef STAN__MODEL__MODEL__HEADER_HPP__
+#define STAN__MODEL__MODEL__HEADER_HPP__
+
+#include <stan/math/matrix.hpp>
+#include <stan/agrad/agrad.hpp>
+#include <stan/agrad/special_functions.hpp>
+#include <stan/agrad/matrix.hpp>
+#include <stan/gm/command.hpp>
+#include <stan/io/cmd_line.hpp>
+#include <stan/io/dump.hpp>
+#include <stan/io/reader.hpp>
+#include <stan/io/writer.hpp>
+#include <stan/io/csv_writer.hpp>
+#include <stan/math/matrix.hpp>
+#include <stan/math/special_functions.hpp>
+#include <stan/mcmc/hmc.hpp>
+#include <stan/mcmc/sampler.hpp>
+#include <stan/model/prob_grad_ad.hpp>
+#include <stan/prob/distributions.hpp>
+
+#endif
