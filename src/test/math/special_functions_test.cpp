0136759982449ea6c7fc30a25161fcdcfe8ab5c6
Daniel Lee <bearlee@alum.mit.edu>
2012-02-21 12:47:22 -0500

    removed unsigned int warnings for test/math/special_functions

--- src/test/math/special_functions_test.cpp
+++ src/test/math/special_functions_test.cpp
@@ -4,10 +4,10 @@
 #include "stan/math/special_functions.hpp"
 
 TEST(MathsSpecialFunctions, int_step) {
-  EXPECT_EQ(0, stan::math::int_step(-1.0));
-  EXPECT_EQ(0, stan::math::int_step(0.0));
-  EXPECT_EQ(1, stan::math::int_step(0.00000000001));
-  EXPECT_EQ(1, stan::math::int_step(100.0));
+  EXPECT_EQ(0U, stan::math::int_step(-1.0));
+  EXPECT_EQ(0U, stan::math::int_step(0.0));
+  EXPECT_EQ(1U, stan::math::int_step(0.00000000001));
+  EXPECT_EQ(1U, stan::math::int_step(100.0));
 }
 
 TEST(MathsSpecialFunctions, binomial_coefficient_log) {
