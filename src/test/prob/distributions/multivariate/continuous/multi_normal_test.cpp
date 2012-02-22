af01cf116694225318a5989896c9ebf485831796
Daniel Lee <bearlee@alum.mit.edu>
2012-02-22 17:39:53 -0500

    removed warning for multi_normal_test

--- src/test/prob/distributions/multivariate/continuous/multi_normal_test.cpp
+++ src/test/prob/distributions/multivariate/continuous/multi_normal_test.cpp
@@ -55,11 +55,12 @@ TEST(ProbDistributionsMultiNormal,ErrnoPolicySigma) {
   Sigma << 9.0, -3.0, -3.0, 4.0;
   
   double result;
-  EXPECT_NO_THROW (result=stan::prob::multi_normal_log(y, mu, Sigma, errno_policy()));
+  EXPECT_NO_THROW(result=stan::prob::multi_normal_log(y, mu, Sigma, errno_policy()));
+  EXPECT_FALSE(std::isnan(result));
 
   // non-symmetric
   Sigma(0, 1) = -2.5;
-  EXPECT_NO_THROW (result=stan::prob::multi_normal_log(y, mu, Sigma, errno_policy()));
+  EXPECT_NO_THROW(result=stan::prob::multi_normal_log(y, mu, Sigma, errno_policy()));
   EXPECT_TRUE(std::isnan(result)) << "non-symmetric Sigma should return nan.";
 }
 TEST(ProbDistributionsMultiNormal,DefaultPolicyMu) {
@@ -92,7 +93,7 @@ TEST(ProbDistributionsMultiNormal,ErrnoPolicyMu) {
   
   double result;
   EXPECT_NO_THROW (result=stan::prob::multi_normal_log(y, mu, Sigma, errno_policy()));
-
+  EXPECT_FALSE(std::isnan(result));
 
   mu(0) = std::numeric_limits<double>::infinity();
   EXPECT_NO_THROW (result=stan::prob::multi_normal_log(y, mu, Sigma, errno_policy()));
