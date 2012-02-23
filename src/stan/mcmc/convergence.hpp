5c69f1e603d968bfaf8eb382dbea2a912fab0202
Daniel Lee <bearlee@alum.mit.edu>
2012-02-23 17:43:13 -0500

    placeholders for convergence diagnostics

---
+++ src/stan/mcmc/convergence.hpp
@@ -0,0 +1,14 @@
+#ifndef __STAN__MCMC__CONVERGENCE_HPP__
+#define __STAN__MCMC__CONVERGENCE_HPP__
+
+namespace stan {
+
+  namespace mcmc {
+
+    
+  }
+  
+}
+
+
+#endif
---
+++ src/test/mcmc/convergence_test.cpp
@@ -0,0 +1,5 @@
+#include <stan/mcmc/convergence.hpp>
+#include <gtest/gtest.h>
+
+TEST(stanMcmc, convergence) {
+}
