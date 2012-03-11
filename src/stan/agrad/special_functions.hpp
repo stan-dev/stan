a0099e98fb93754c81f73672bdb80f0295c89bae
Ben Goodrich <bg2382@columbia.edu>
2012-03-11 00:41:31 -0500

    Revert "attempt to enable mixed type operations"
    This reverts commit 3d22db52e5dab25c6b038f02b17f652c7237b573.
    
    That patch was not intended to be applied to the master branch yet.

--- src/stan/agrad/special_functions.hpp
+++ src/stan/agrad/special_functions.hpp
@@ -1288,9 +1288,7 @@ namespace stan {
       return var(new multiply_log_dv_vari(a,b.vi_));
     }
 
-    inline const var& conj(const var& x) {return x;}
-    inline const var& real(const var& x) {return x;}
-    inline const var  imag(const var&  ) {return 0.0;}
+
 
   }
 }
