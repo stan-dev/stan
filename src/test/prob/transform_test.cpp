71b31692d729ceac2871f285593627489a98f158
Bob Carpenter <carp@alias-i.com>
2012-04-24 15:08:35 -0400

    swapped in Cholesky-based covar transform

--- src/test/prob/transform_test.cpp
+++ src/test/prob/transform_test.cpp
@@ -315,6 +315,37 @@ TEST(prob_transform,corr_matrix_free_exception) {
   y << 0, 0, 0, 0;
   EXPECT_THROW(stan::prob::corr_matrix_free(y), std::runtime_error);
 }
+
+
+TEST(prob_transform,lkj_cov_matrix_rt) {
+  unsigned int K = 4;
+  unsigned int K_choose_2 = 6; 
+  Matrix<double,Dynamic,1> x(K_choose_2 + K);
+  x << -1.0, 2.0, 0.0, 1.0, 3.0, -1.5,
+    1.0, 2.0, -1.5, 2.5;
+  Matrix<double,Dynamic,Dynamic> y = stan::prob::cov_matrix_constrain_lkj(x,K);
+  Matrix<double,Dynamic,1> xrt = stan::prob::cov_matrix_free_lkj(y);
+  EXPECT_EQ(x.size(), xrt.size());
+  for (Matrix<double,Dynamic,1>::size_type i = 0; i < x.size(); ++i) {
+    EXPECT_FLOAT_EQ(x[i], xrt[i]);
+  }
+}
+TEST(prob_transform,lkj_cov_matrix_free_exception) {
+  Matrix<double,Dynamic,Dynamic> y(0,0);
+  
+  EXPECT_THROW(stan::prob::cov_matrix_free_lkj(y), std::domain_error);
+  y.resize(0,10);
+  EXPECT_THROW(stan::prob::cov_matrix_free_lkj(y), std::domain_error);
+  y.resize(10,0);
+  EXPECT_THROW(stan::prob::cov_matrix_free_lkj(y), std::domain_error);
+  y.resize(1,2);
+  EXPECT_THROW(stan::prob::cov_matrix_free_lkj(y), std::domain_error);
+
+  y.resize(2,2);
+  y << 0, 0, 0, 0;
+  EXPECT_THROW(stan::prob::cov_matrix_free_lkj(y), std::runtime_error);
+}
+
 TEST(prob_transform,cov_matrix_rt) {
   unsigned int K = 4;
   unsigned int K_choose_2 = 6; 
@@ -328,8 +359,13 @@ TEST(prob_transform,cov_matrix_rt) {
     EXPECT_FLOAT_EQ(x[i], xrt[i]);
   }
 }
+TEST(prob_transform,cov_matrix_constrain_exception) {
+  Matrix<double,Dynamic,1> x(7);
+  int K = 12;
+  EXPECT_THROW(stan::prob::cov_matrix_constrain(x,K), std::domain_error);
+}
 TEST(prob_transform,cov_matrix_free_exception) {
-  Matrix<double,Dynamic,Dynamic> y;
+  Matrix<double,Dynamic,Dynamic> y(0,0);
   
   EXPECT_THROW(stan::prob::cov_matrix_free(y), std::domain_error);
   y.resize(0,10);
@@ -341,7 +377,7 @@ TEST(prob_transform,cov_matrix_free_exception) {
 
   y.resize(2,2);
   y << 0, 0, 0, 0;
-  EXPECT_THROW(stan::prob::cov_matrix_free(y), std::runtime_error);
+  EXPECT_THROW(stan::prob::cov_matrix_free(y), std::domain_error);
 }
 
 TEST(prob_transform,simplex_rt0) {
