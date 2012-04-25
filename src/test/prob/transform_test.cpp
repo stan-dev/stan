bf373ecbf5383304548bdf2368f365d343c2cf13
Bob Carpenter <carp@alias-i.com>
2012-04-25 12:18:08 -0400

    added Jacobian test for covar inverse transform

--- src/test/prob/transform_test.cpp
+++ src/test/prob/transform_test.cpp
@@ -379,6 +379,38 @@ TEST(prob_transform,cov_matrix_free_exception) {
   y << 0, 0, 0, 0;
   EXPECT_THROW(stan::prob::cov_matrix_free(y), std::domain_error);
 }
+TEST(prob_transform,cov_matrix_jacobian) {
+  using stan::agrad::var;
+  using stan::math::determinant;
+  using std::log;
+  using std::fabs;
+
+  unsigned int K = 4;
+  unsigned int K_choose_2 = 6;
+  Matrix<var,Dynamic,1> X(K_choose_2 + K);
+  X << 1.0, 2.0, -3.0, 1.7, 9.8, 
+    -12.2, 0.4, 0.2, 1.2, 2.7;
+  std::vector<var> x;
+  for (int i = 0; i < X.size(); ++i)
+    x.push_back(X(i));
+  var lp = 0.0;
+  Matrix<var,Dynamic,Dynamic> Sigma = stan::prob::cov_matrix_constrain(X,K,lp);
+  std::vector<var> y;
+  for (int m = 0; m < K; ++m)
+    for (int n = 0; n <= m; ++n)
+      y.push_back(Sigma(m,n));
+
+  std::vector<std::vector<double> > j;
+  stan::agrad::jacobian(y,x,j);
+
+  Matrix<double,Dynamic,Dynamic> J(10,10);
+  for (int m = 0; m < 10; ++m)
+    for (int n = 0; n < 10; ++n)
+      J(m,n) = j[m][n];
+
+  double log_abs_jacobian_det = log(fabs(determinant(J)));
+  EXPECT_FLOAT_EQ(log_abs_jacobian_det,lp.val());
+}
 
 TEST(prob_transform,simplex_rt0) {
   Matrix<double,Dynamic,1> x(4);
