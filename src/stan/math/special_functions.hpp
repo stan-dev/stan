f25455cf5d17c0b5828cfdafc0cd2bdc02421880
Daniel Lee <bearlee@alum.mit.edu>
2012-05-14 23:25:44 -0400

    added ibeta() to stan::math

--- src/stan/math/special_functions.hpp
+++ src/stan/math/special_functions.hpp
@@ -4,6 +4,7 @@
 #include <stdexcept>
 
 #include <boost/math/special_functions/gamma.hpp>
+#include <boost/math/special_functions/beta.hpp>
 #include <boost/math/tools/promotion.hpp>
 #include <boost/throw_exception.hpp>
 
@@ -548,6 +549,26 @@ namespace stan {
         return a + log1p(exp(b - a));
       return b + log1p(exp(a - b));
     }
+
+    /** 
+     * The normalized incomplete beta function of a, b, and x.
+     *
+     * Used to compute the cumulative density function for the beta
+     * distribution.
+     * 
+     * @param a Shape parameter.
+     * @param b Shape parameter.
+     * @param x Random variate.
+     * 
+     * @return The normalized incomplete beta function.
+     */
+    inline double ibeta(const double& a,
+                        const double& b,
+                        const double& x) {
+      return boost::math::ibeta(a, b, x);
+    }
+
+
 
     /**
      * Return the log of the sum of the exponentiated values of the specified
