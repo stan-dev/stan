244b0bdf141e52d7a7d5934f9568b6b3a61e40f5
Jiqiang Guo <guojq28@gmail.com>
2012-05-16 01:08:48 -0400

    implement var_context for R list

--- rstan/rstan/src/stanc.cpp
+++ rstan/rstan/src/stanc.cpp
@@ -12,21 +12,26 @@
 #include <string>
 
 SEXP version() {
+  BEGIN_RCPP;
   std::string stan_version 
     = stan::MAJOR_VERSION + "." +
       stan::MINOR_VERSION + "." +
       stan::PATCH_VERSION;
-   return  Rcpp::wrap(stan_version); 
+  return  Rcpp::wrap(stan_version); 
+  END_RCPP;
 } 
 
 SEXP stanc(SEXP model_stancode, SEXP model_name) { 
+  BEGIN_RCPP;
   static const int SUCCESS_RC = 0;
   static const int EXCEPTION_RC = -1;
   static const int PARSE_FAIL_RC = -2;
+  /*
   std::string stan_version 
     = stan::MAJOR_VERSION + "." +
       stan::MINOR_VERSION + "." +
       stan::PATCH_VERSION;
+  */
 
   std::string mcode_ = Rcpp::as<std::string>(model_stancode); 
   std::string mname_ = Rcpp::as<std::string>(model_name); 
@@ -37,20 +42,18 @@ SEXP stanc(SEXP model_stancode, SEXP model_name) {
     bool valid_model
       = stan::gm::compile(in,out,mname_);
     if (!valid_model) {
-      return Rcpp::List::create(Rcpp::Named("status") = PARSE_FAIL_RC,
-                                Rcpp::Named("stan_version") = stan_version); 
+      return Rcpp::List::create(Rcpp::Named("status") = PARSE_FAIL_RC); 
 
     }
   } catch(const std::exception& e) {
     REprintf("\nERROR PARSING\n %s\n", e.what()); 
-    return Rcpp::List::create(Rcpp::Named("status") = EXCEPTION_RC, 
-                              Rcpp::Named("stan_version") = stan_version); 
+    return Rcpp::List::create(Rcpp::Named("status") = EXCEPTION_RC); 
   }
   return Rcpp::List::create(Rcpp::Named("status") = SUCCESS_RC, 
-                            Rcpp::Named("stan_version") = stan_version,  
                             Rcpp::Named("model_name") = mname_,
                             Rcpp::Named("cppcode") = out.str());
 
+  END_RCPP;
 }
 
 
