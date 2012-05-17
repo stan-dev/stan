93a39e5b1da5dc6978ea8ab35e2563a074bb4c23
Daniel Lee <bearlee@alum.mit.edu>
2012-05-16 23:12:10 -0400

    implemented stan::math::ibeta and stan::agrad::ibeta
    For the grad versions, this includes the partial derivatives. This is currently implemented slowly without concern for numeric stability.

--- src/stan/prob/distributions/univariate/continuous/beta.hpp
+++ src/stan/prob/distributions/univariate/continuous/beta.hpp
@@ -109,6 +109,50 @@ namespace stan {
       return beta_log<false>(y,alpha,beta,stan::math::default_policy());
     }
 
+
+    template <typename T_y, typename T_scale_succ, typename T_scale_fail,
+              class Policy>
+    boost::math::tools::promote_args<T_y,T_scale_succ,T_scale_fail>::type
+    beta_p(const T_y& y, const T_scale_succ& alpha, const T_scale_fail& beta, 
+             const Policy&) {
+      static const char* function = "stan::prob::beta_p(%1%)";
+
+      using stan::math::check_positive;
+      using stan::math::check_finite;
+      using stan::math::check_not_nan;
+      using boost::math::tools::promote_args;
+      
+      typename promote_args<T_y,T_scale_succ,T_scale_fail>::type lp;
+      if (!check_finite(function, alpha,
+                        "Prior success sample size plus 1, alpha,",
+                        &lp, Policy()))
+        return lp;
+      if (!check_positive(function, alpha, 
+                          "Prior success sample size plus 1, alpha,",
+                          &lp, Policy()))
+        return lp;
+      if (!check_finite(function, beta, 
+                          "Prior failure sample size plus 1, beta,",
+                          &lp, Policy()))
+        return lp;
+      if (!check_positive(function, beta, 
+                          "Prior failure sample size plus 1, beta,",
+                          &lp, Policy()))
+        return lp;
+      if (!check_not_nan(function, y, "Random variate, y,", &lp, Policy()))
+        return lp;
+      
+      if (y < 0.0 || y > 1.0)
+        return LOG_ZERO;
+      
+      return 0.5 * erfc(-(y - mu)/(sigma * SQRT_2));
+    }
+
+    template <typename T_y, typename T_scale_succ, typename T_scale_fail>
+    boost::math::tools::promote_args<T_y,T_scale_succ,T_scale_fail>::type
+    beta_p(const T_y& y, const T_scale_succ& alpha, const T_scale_fail& beta) {
+      return beta_p(y,alpha,beta,stan::math::default_policy());
+    }
   }
 }
 #endif
