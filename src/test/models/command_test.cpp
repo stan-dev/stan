2261bd6f717a602d87f8306ba30d93d9ae9496c4
Daniel Lee <bearlee@alum.mit.edu>
2012-06-11 18:29:32 -0400

    test/models/command: starting to use tuples to autogenerate options

--- src/test/models/command_test.cpp
+++ src/test/models/command_test.cpp
@@ -1,24 +1,34 @@
+#include <gtest/gtest.h>
+
+#if GTEST_HAS_COMBINE
 /** 
  * This test tries out different command line options for a 
  * generated model.
  *
  */
-#include <gtest/gtest.h>
 #include <stdexcept>
 #include <boost/algorithm/string.hpp>
 #include <stan/mcmc/chains.hpp>
 
+using std::stringstream;
 using std::vector;
 using std::string;
 using std::pair;
+using testing::Combine;
+using testing::Values;
+using testing::ValuesIn;
+using std::tr1::tuple;
+using std::tr1::get;
 
-class ModelCommand : public ::testing::TestWithParam<int> {
+class ModelCommand : 
+  public ::testing::TestWithParam<tuple<int, string> > {
 private:
   static char path_separator;
-public:  
-  static vector<string> expected_help_options;
+public:
+  static string data_file_base;
   static string model_path;
-  static string samples_option;
+  static vector<string> expected_help_options;
+  static vector<pair<string, string> > expected_output;
 
   static char get_path_separator() {
     if (path_separator == 0) {
@@ -31,8 +41,45 @@ public:
     return path_separator;
   }
   
+  static std::string get_path(const char* path_array[], size_t length) {
+    std::string path;
+    if (length==0)
+      return path;
+    
+    path.append(path_array[0]);
+    for (size_t i = 1; i < length; i++) {
+      path.append(1, get_path_separator());
+      path.append(path_array[i]);
+    }
+    return path;
+  }
+
+
+
+  static std::string get_path(const vector<string>& path_vector) {
+    std::string path;
+    if (path_vector.size()==0)
+      return path;
+    
+    path.append(path_vector[0]);
+    for (size_t i = 1; i < path_vector.size(); i++) {
+      path.append(1, get_path_separator());
+      path.append(path_vector[i]);
+    }
+    return path;
+  }
   
   void static SetUpTestCase() {
+    model_path.append("models");
+    model_path.append(1, get_path_separator());
+    model_path.append("command");
+
+    data_file_base.append("src");
+    data_file_base.append(1, get_path_separator());
+    data_file_base.append("test");
+    data_file_base.append(1, get_path_separator());
+    data_file_base.append(model_path);
+
     expected_help_options.push_back("help");
     expected_help_options.push_back("data");
     expected_help_options.push_back("init");
@@ -53,13 +100,24 @@ public:
     expected_help_options.push_back("gamma");
     expected_help_options.push_back("test_grad");
 
-    model_path.append("models");
-    model_path.append(1, get_path_separator());
-    model_path.append("command");
+    expected_output.push_back(pair<string,string>("data","(specified model requires no data)"));
+    expected_output.push_back(pair<string,string>("init", "random initialization"));
+    expected_output.push_back(pair<string,string>("init tries", "1"));
+    expected_output.push_back(pair<string,string>("samples", model_path+".csv"));
+    expected_output.push_back(pair<string,string>("append_samples", "0"));
+    expected_output.push_back(pair<string,string>("seed", ""));
+    expected_output.push_back(pair<string,string>("chain_id", "1 (default)"));
+    expected_output.push_back(pair<string,string>("iter", "2000"));
+    expected_output.push_back(pair<string,string>("warmup", "1000"));
+    expected_output.push_back(pair<string,string>("thin", "1 (default)"));
+    expected_output.push_back(pair<string,string>("unit_mass_matrix", "0"));
+    expected_output.push_back(pair<string,string>("leapfrog_steps", "-1"));
+    expected_output.push_back(pair<string,string>("max_treedepth", "10"));
+    expected_output.push_back(pair<string,string>("epsilon", "-1"));
+    expected_output.push_back(pair<string,string>("epsilon_pm", "0"));
+    expected_output.push_back(pair<string,string>("delta", "0.5"));
+    expected_output.push_back(pair<string,string>("gamma", "0.05"));
 
-    samples_option = " --samples=";
-    samples_option.append(model_path);
-    samples_option.append(".csv");
   }
 
   /** 
@@ -146,47 +204,12 @@ public:
   }
 
   void check_output(const string& command_output,
-                    const vector<pair<string, string> >& defaults) {
-    vector<pair<string, string> > expected_output;
-    expected_output.push_back(pair<string,string>("data", 
-                                                  "(specified model requires no data)"));
-    expected_output.push_back(pair<string,string>("init", 
-                                                  "random initialization"));
-    expected_output.push_back(pair<string,string>("init tries", 
-                                                  "1"));
-    expected_output.push_back(pair<string,string>("samples", 
-                                                  model_path+".csv"));    
-    expected_output.push_back(pair<string,string>("append_samples",
-                                                  "0"));    
-    expected_output.push_back(pair<string,string>("seed", 
-                                                  ""));    
-    expected_output.push_back(pair<string,string>("chain_id", 
-                                                  "1 (default)"));    
-    expected_output.push_back(pair<string,string>("iter", 
-                                                  "2000"));    
-    expected_output.push_back(pair<string,string>("warmup", 
-                                                  "1000"));    
-    expected_output.push_back(pair<string,string>("thin", 
-                                                  "1 (default)"));    
-    expected_output.push_back(pair<string,string>("unit_mass_matrix", 
-                                                  "0"));    
-    expected_output.push_back(pair<string,string>("leapfrog_steps", 
-                                                  "-1"));    
-    expected_output.push_back(pair<string,string>("max_treedepth", 
-                                                  "10"));    
-    expected_output.push_back(pair<string,string>("epsilon", 
-                                                  "-1"));    
-    expected_output.push_back(pair<string,string>("epsilon_pm", 
-                                                  "0"));    
-    expected_output.push_back(pair<string,string>("delta", 
-                                                  "0.5"));
-    expected_output.push_back(pair<string,string>("gamma", 
-                                                  "0.05"));    
-
-    for (size_t i = 0; i < defaults.size(); i++) {
+                    const vector<pair<string, string> >& changed_options) {
+    vector<pair<string, string> > expected_output(this->expected_output);
+    for (size_t i = 0; i < changed_options.size(); i++) {
       for (size_t j = 0; j < expected_output.size(); j++) {
-        if (expected_output[j].first == defaults[i].first) {
-          expected_output[j].second = defaults[i].second;
+        if (expected_output[j].first == changed_options[i].first) {
+          expected_output[j].second = changed_options[i].second;
           break;
         }
       }
@@ -213,15 +236,16 @@ public:
 
   }
   void check_output(const string& command_output) {
-    vector<pair<string, string> > defaults;
-    check_output(command_output, defaults);
+    vector<pair<string, string> > changed_options;
+    check_output(command_output, changed_options);
   }
 };
 
 vector<string> ModelCommand::expected_help_options;
 string ModelCommand::model_path;
-string ModelCommand::samples_option;
+string ModelCommand::data_file_base;
 char ModelCommand::path_separator = 0;
+vector<pair<string, string> > ModelCommand::expected_output;
 
 TEST_F(ModelCommand, HelpOptionsMatch) {
   string help_command = model_path;
@@ -236,55 +260,74 @@ TEST_F(ModelCommand, HelpOptionsMatch) {
   }
 }
 
-TEST_F(ModelCommand, DataOption) {
-  string data_file_base;
-  data_file_base.append("src");
-  data_file_base.append(1, get_path_separator());
-  data_file_base.append("test");
-  data_file_base.append(1, get_path_separator());
-  data_file_base.append(model_path);
+vector<int> DataOptions() {
+  vector<int> options;
+  options.push_back(1);
+  options.push_back(2);
+  return (options);
+}
 
-  string data_command1 = model_path;
-  data_command1.append(" --data=");
-  data_command1.append(data_file_base);
-  data_command1.append("1.Rdata");
-  data_command1.append(samples_option);
+vector<string> InitOptions() {
+  const char* path_array[] = {"src", "test", "models", "command"};
+  string base = ModelCommand::get_path(path_array, 4U);
+  
+  vector<string> options;
+  options.push_back("");
+  options.push_back(base + "_init.Rdata");
+  return (options);
+}
+ 
+TEST_P(ModelCommand, OptionsTest) {
+  tuple<int, string> options = GetParam();
+  vector<pair<string, string> > changed_options;
 
-  vector<pair<string, string> > defaults;
+  stringstream command;
+  command << model_path;
+  // data
+  {
+    stringstream data_file;
+    data_file << data_file_base
+              << get<0>(options)
+              << ".Rdata";
+    command << " --data="
+            << data_file.str();
+    changed_options.push_back(pair<string,string>("data", data_file.str()));
+  }
+  if (get<1>(options) != "") {
+    command << " --init="
+            << get<1>(options);
+  }
+  command << " --samples="
+          << model_path 
+          << ".csv";
+  
+  std::cout << "command: \n" << command.str() << "\n\n";
+  
+  check_output(run_command(command.str()), changed_options);
 
-  defaults.push_back(pair<string, string>("data", data_file_base+"1.Rdata"));
-  check_output(run_command(data_command1), defaults);
+  //std::cout << "command: \n" << command.str() << "\n\n";
+  //std::cout << "output : \n" << run_command(command.str()) << "\n\n";
+  
+
   // test sampled values
   vector<string> names;
   vector<vector<size_t> > dimss;
   stan::mcmc::read_variables(model_path+".csv", 2U,
                              names, dimss);
       
-  stan::mcmc::chains<> c1(1U, names, dimss);
-  stan::mcmc::add_chain(c1, 0, model_path+".csv", 2U);
-  EXPECT_NEAR(c1.mean(0U), 0, 1);
-
-
-  string data_command2 = model_path;
-  data_command2.append(" --data=");
-  data_command2.append(data_file_base);
-  data_command2.append("2.Rdata");
-  data_command2.append(samples_option);
-
+  stan::mcmc::chains<> c(1U, names, dimss);
+  stan::mcmc::add_chain(c, 0, model_path+".csv", 2U);
   
-  defaults.clear();
-  defaults.push_back(pair<string, string>("data", data_file_base+"2.Rdata"));
-  check_output(run_command(data_command2), defaults);
-  // test sampled values
-  stan::mcmc::chains<> c2(1U, names, dimss);
-  stan::mcmc::add_chain(c2, 0, model_path+".csv", 2U);
-  EXPECT_NEAR(c2.mean(0U), 100, 1);
-
+  double expected_mean = (get<0>(options)-1)*100.0; // 1: mean = 0, 2: mean = 100
+  EXPECT_NEAR(expected_mean, c.mean(0U), 3);
 }
-/*TEST_P(ModelCommand, PTest) {
-std::cout << "PTest: " << GetParam() << std::endl;
 
-}*/
+INSTANTIATE_TEST_CASE_P(, 
+                        ModelCommand, 
+                        Combine(ValuesIn(DataOptions()),
+                                ValuesIn(InitOptions())));
 
-//INSTANTIATE_TEST_CASE_P(abc, ModelCommand, testing::Range(1, 10));
+#else
+TEST(ModelCommand, CombineIsNotSupportedOnThisPlatform) {}
 
+#endif
