183cab2e2f8573bba4c4908fd0a2410d040c3e46
Bob Carpenter <carp@alias-i.com>
2012-06-18 01:28:16 -0400

    added partials_vari and tests

--- src/stan/agrad/partials_vari.hpp
+++ src/stan/agrad/partials_vari.hpp
@@ -8,51 +8,109 @@ namespace stan {
   namespace agrad {
 
     /**
+     * A variable implementation that stores a single operand and its
+     * derivative with respect to the variable.
      */
-    struct partials1_vari : public vari {
-      vari* subexpr1_;
+    class partials1_vari : public vari {
+    private:
+      vari* operand1_;
+    public: 
       const double partial1_;
-      partials1_vari(double value, vari* subexpr1, double partial1) 
-        : vari(value), subexpr1_(subexpr1), partial1_(partial1) 
+      /**
+       * Construct a variable implementation with the specified value,
+       * operand and derivative.
+       * @param value Value of variable.
+       * @param operand1 Implementation of first operand.
+       * @param partial1 Derivative of variable with respect to first operand.
+       */
+      partials1_vari(double value, vari* operand1, double partial1) 
+        : vari(value), operand1_(operand1), partial1_(partial1) 
       { }
+      /**
+       * Apply chain rule by incrementing this variable's operand's
+       * adjoint by the product of this variable's adjoint and
+       * the stored partial.
+       */
       void chain() {
-        subexpr1_->adj_ += adj_ * partial1_;
+        operand1_->adj_ += adj_ * partial1_;
       }
     };
 
-    struct partials2_vari : public vari {
-      vari* subexpr1_;      const double partial1_;
-      vari* subexpr2_;      const double partial2_;
+    /**
+     * A variable implementation that stores two operands and their
+     * derivatives with respect to the variable.
+     */
+    class partials2_vari : public vari {
+    private:
+      vari* operand1_;      const double partial1_;
+      vari* operand2_;      const double partial2_;
+    public:
+      /**
+       * Construct a variable implementation with the specified value,
+       * operands and derivatives.
+       * @param value Value of variable.
+       * @param operand1 Implementation of first operand.
+       * @param partial1 Derivative of variable with respect to first operand.
+       * @param operand2 Implementation of second operand.
+       * @param partial2 Derivative of variable with respect to second operand.
+       */
       partials2_vari(double value, 
-                     vari* subexpr1, double partial1,
-                     vari* subexpr2, double partial2) 
+                     vari* operand1, double partial1,
+                     vari* operand2, double partial2) 
         : vari(value), 
-          subexpr1_(subexpr1), partial1_(partial1),
-          subexpr2_(subexpr2), partial2_(partial2)
+          operand1_(operand1), partial1_(partial1),
+          operand2_(operand2), partial2_(partial2)
       { }
+      /**
+       * Apply chain rule by incrementing each variable's operand's
+       * adjoint by the product of this variable's adjoint and
+       * the derivative with respect to the operand.
+       */
       void chain() {
-        subexpr1_->adj_ += adj_ * partial1_;
-        subexpr2_->adj_ += adj_ * partial2_;
+        operand1_->adj_ += adj_ * partial1_;
+        operand2_->adj_ += adj_ * partial2_;
       }
     };
 
-      struct partials3_vari : public vari {
-      vari* subexpr1_;  const double partial1_;
-      vari* subexpr2_;  const double partial2_;
-      vari* subexpr3_;  const double partial3_;
+    /**
+     * A variable implementation that stores three operands and their
+     * derivatives with respect to the variable.
+     */
+    class partials3_vari : public vari {
+    private:
+      vari* operand1_;  const double partial1_;
+      vari* operand2_;  const double partial2_;
+      vari* operand3_;  const double partial3_;
+    public:
+      /**
+       * Construct a variable implementation with the specified value,
+       * operands and derivatives.
+       * @param value Value of variable.
+       * @param operand1 Implementation of first operand.
+       * @param partial1 Derivative of variable with respect to first operand.
+       * @param operand2 Implementation of second operand.
+       * @param partial2 Derivative of variable with respect to second operand.
+       * @param operand3 Implementation of second operand.
+       * @param partial3 Derivative of variable with respect to third operand.
+       */
       partials3_vari(double value, 
-                     vari* subexpr1, double partial1,
-                     vari* subexpr2, double partial2,
-                     vari* subexpr3, double partial3)
+                     vari* operand1, double partial1,
+                     vari* operand2, double partial2,
+                     vari* operand3, double partial3)
         : vari(value), 
-          subexpr1_(subexpr1), partial1_(partial1),
-          subexpr2_(subexpr2), partial2_(partial2),
-          subexpr3_(subexpr3), partial3_(partial3)
+          operand1_(operand1), partial1_(partial1),
+          operand2_(operand2), partial2_(partial2),
+          operand3_(operand3), partial3_(partial3)
       { }
+      /**
+       * Apply chain rule by incrementing each variable's operand's
+       * adjoint by the product of this variable's adjoint and
+       * the derivative with respect to the operand.
+       */
       void chain() {
-        subexpr1_->adj_ += adj_ * partial1_;
-        subexpr2_->adj_ += adj_ * partial2_;
-        subexpr3_->adj_ += adj_ * partial3_;
+        operand1_->adj_ += adj_ * partial1_;
+        operand2_->adj_ += adj_ * partial2_;
+        operand3_->adj_ += adj_ * partial3_;
       }
     };
 
--- src/test/agrad/partials_vari_test.cpp
+++ src/test/agrad/partials_vari_test.cpp
@@ -1,7 +1,44 @@
 #include <gtest/gtest.h>
 #include <stan/agrad/partials_vari.hpp>
 
-
-TEST(AgradPartialsVari,test1) {
-  EXPECT_EQ(1,1);
+TEST(AgradPartialsVari,partials1_vari) {
+  using stan::agrad::var;
+  using stan::agrad::partials1_vari;
+  var x = 2.0;
+  var z = -3.0 * x;  // dz/dx = -3
+  var y(new partials1_vari(-1.0,z.vi_,5.0)); // dy/dz = 5
+  stan::agrad::grad(y.vi_);
+  EXPECT_FLOAT_EQ(-15.0, x.adj());  // dy/dx = -15
+}
+TEST(AgradPartialsVari,partials2_vari) {
+  using stan::agrad::var;
+  using stan::agrad::partials2_vari;
+  var x1 = 2.0;
+  var x2 = 3.0;
+  var z1 = -5.0 * x1; // dz1/dx1 = -5
+  var z2 = -7.0 * x2; // dz2/dx2 = -7
+  var y(new partials2_vari(-1.0,
+                           z1.vi_,11.0,   // dy/dz1 = 11.0
+                           z2.vi_,13.0)); // dy/dz2 = 13.0
+  stan::agrad::grad(y.vi_);
+  EXPECT_FLOAT_EQ(-55.0, x1.adj());  // dy/dx1 = -55
+  EXPECT_FLOAT_EQ(-91.0, x2.adj());  // dy/dx2 = -91
+}
+TEST(AgradPartialsVari,partials3_vari) {
+  using stan::agrad::var;
+  using stan::agrad::partials3_vari;
+  var x1 = 2.0;
+  var x2 = 3.0;
+  var x3 = 5.0;
+  var z1 = -7.0 * x1;  // dz1/dx1 = -5
+  var z2 = -9.0 * x2;  // dz2/dx2 = -7
+  var z3 = -11.0 * x3; // dz3/dx3 = -11
+  var y(new partials3_vari(-1.0,
+                           z1.vi_,17.0,   // dy/dz1 = 17.0
+                           z2.vi_,19.0,   // dy/dz2 = 19.0
+                           z3.vi_,23.0)); // dy/dz3 = 23.0
+  stan::agrad::grad(y.vi_);
+  EXPECT_FLOAT_EQ(-119.0, x1.adj());  // dy/dx1 = -119
+  EXPECT_FLOAT_EQ(-171.0, x2.adj());  // dy/dx2 = -133
+  EXPECT_FLOAT_EQ(-253.0, x3.adj());  // dy/dx2 = -253
 }
