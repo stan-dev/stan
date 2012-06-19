7d79f6ba83edb7ce6982254faf31db567dfd941c
BrandonKessler <Brandon@Brandon-PC.(none)>
2012-06-19 16:33:43 -0400

    prob distributions univariate discrete beta_binomial test

---
+++ src/test/file_includes/stan_prob_distributions_univariate_discrete_beta_binomial_test.cpp
@@ -0,0 +1,6 @@
+#include <stan/prob/distributions/univariate/discrete/beta_binomial.hpp>
+#include <gtest/gtest.h>
+
+TEST(stanProbDistributionsUnivariateDiscreteBetaBinomial,headerParses) {
+  EXPECT_TRUE(true);
+}
