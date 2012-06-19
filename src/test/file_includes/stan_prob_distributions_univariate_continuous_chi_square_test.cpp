d9f5445ebca89b8b9fb246126c201963550b2322
BrandonKessler <Brandon@Brandon-PC.(none)>
2012-06-19 15:49:53 -0400

    prob distributions univariate continuous chi_sqare test

---
+++ src/test/file_includes/stan_prob_distributions_univariate_continuous_chi_square_test.cpp
@@ -0,0 +1,6 @@
+#include <stan/prob/distributions/univariate/continuous/chi_square.hpp>
+#include <gtest/gtest.h>
+
+TEST(stanProbDistributionsUnivariateContinuousChiSquare,headerParses) {
+  EXPECT_TRUE(true);
+}
