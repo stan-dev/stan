b6f38c353e3820bbdda90a0e59bd885a3af6c854
BrandonKessler <Brandon@Brandon-PC.(none)>
2012-06-19 16:44:42 -0400

    prob distributions univariate discrete ordered_logistic test

---
+++ src/test/file_includes/stan_prob_distributions_univariate_discrete_ordered_logistic_test.cpp
@@ -0,0 +1,6 @@
+#include <stan/prob/distributions/univariate/discrete/ordered_logistic.hpp>
+#include <gtest/gtest.h>
+
+TEST(stanProbDistributionsUnivariateDiscreteOrderedLogistic,headerParses) {
+  EXPECT_TRUE(true);
+}
