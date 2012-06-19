47b0e9c4108af7efe9433887bb0dc793282bfb40
BrandonKessler <Brandon@Brandon-PC.(none)>
2012-06-19 15:25:23 -0400

    prob distributions multivariate continuous multi_normal test

---
+++ src/test/file_includes/stan_prob_distributions_multivariate_continuous_multi_normal_test.cpp
@@ -0,0 +1,6 @@
+#include <stan/prob/distributions/multivariate/continuous/multi_normal.hpp>
+#include <gtest/gtest.h>
+
+TEST(stanProbDistributionsMultivariateContinuousMultiNormal,headerParses) {
+  EXPECT_TRUE(true);
+}
