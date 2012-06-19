fcbabcf79914c2972819aa265ca108a439cd3653
BrandonKessler <Brandon@Brandon-PC.(none)>
2012-06-19 16:25:04 -0400

    prob distributions univariate continuous uniform test

---
+++ src/test/file_includes/stan_prob_distributions_univariate_continuous_uniform_test.cpp
@@ -0,0 +1,6 @@
+#include <stan/prob/distributions/univariate/continuous/uniform.hpp>
+#include <gtest/gtest.h>
+
+TEST(stanProbDistributionsUnivariateContinuousUniform,headerParses) {
+  EXPECT_TRUE(true);
+}
