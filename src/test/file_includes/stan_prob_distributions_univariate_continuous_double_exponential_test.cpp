01d3be860c0675060f154bd6b14f17b406256a6f
BrandonKessler <Brandon@Brandon-PC.(none)>
2012-06-19 15:52:49 -0400

    prob distributions univariate continuous double_exponential test

---
+++ src/test/file_includes/stan_prob_distributions_univariate_continuous_double_exponential_test.cpp
@@ -0,0 +1,6 @@
+#include <stan/prob/distributions/univariate/continuous/double_exponential.hpp>
+#include <gtest/gtest.h>
+
+TEST(stanProbDistributionsUnivariateContinuousDoubleExponential,headerParses) {
+  EXPECT_TRUE(true);
+}
