9525a96963ce73efa0cf6ff8af6e28b165fb1703
Marcus Brubaker <marcus.a.brubaker@gmail.com>
2012-06-29 17:47:36 -0400

    Code cleanup.

--- src/stan/agrad/matrix.hpp
+++ src/stan/agrad/matrix.hpp
@@ -17,28 +17,21 @@ protected:
   stan::agrad::vari** v2_;
   double dotval_;
   size_t length_;
-  inline static double eval_dot(const stan::agrad::var* v1, int stride1,
-	  	  	        const stan::agrad::var* v2, int stride2,
-                                size_t length) {
-    double result = 0;
-    for (size_t i = 0; i < length; i++)
-      result += v1[i*stride1].vi_->val_ * v2[i*stride2].vi_->val_;
-    return result;
-  }
   inline static double eval_gevv(const stan::agrad::var* alpha,
 	                         const stan::agrad::var* v1, int stride1,
 	  	  	         const stan::agrad::var* v2, int stride2,
-                                 size_t length) {
+                                 size_t length, double *dotprod) {
     double result = 0;
     for (size_t i = 0; i < length; i++)
       result += v1[i*stride1].vi_->val_ * v2[i*stride2].vi_->val_;
+    *dotprod = result;
     return alpha->vi_->val_ * result;
   }
 public:
   gevv_vvv_vari(const stan::agrad::var* alpha, 
 		const stan::agrad::var* v1, int stride1, 
 		const stan::agrad::var* v2, int stride2, size_t length) : 
-      vari(eval_gevv(alpha,v1,stride1,v2,stride2,length)), dotval_(eval_dot(v1, stride1, v2, stride2, length)), length_(length) {
+      vari(eval_gevv(alpha,v1,stride1,v2,stride2,length,&dotval_)), length_(length) {
     alpha_ = alpha->vi_;
     v1_ = (stan::agrad::vari**)stan::agrad::memalloc_.alloc(2*length_*sizeof(stan::agrad::vari*));
     v2_ = v1_ + length_;
@@ -56,46 +49,6 @@ public:
     alpha_->adj_ += adj_ * dotval_;
   }
 };
-/*class gevv_vvvv_vari : public stan::agrad::vari {
-protected:
-  stan::agrad::vari* r_;
-  stan::agrad::vari* alpha_;
-  stan::agrad::vari** v1_;
-  stan::agrad::vari** v2_;
-  size_t length_;
-  inline static double eval_gevv(const stan::agrad::var* res,
-                                 const stan::agrad::var* alpha,
-	                         const stan::agrad::var* v1, int stride1,
-	  	  	         const stan::agrad::var* v2, int stride2,
-                                 size_t length) {
-    double result = 0;
-    for (size_t i = 0; i < length; i++)
-      result += v1[i*stride1].vi_->val_ * v2[i*stride2].vi_->val_;
-    return alpha->vi_->val_ * result + res->vi_->val_;
-  }
-public:
-  gevv_vvvv_vari(const stan::agrad::var* res,
-                 const stan::agrad::var* alpha, 
-		 const stan::agrad::var* v1, int stride1, 
-		 const stan::agrad::var* v2, int stride2, size_t length) : 
-        vari(eval_gevv(res, alpha, v1, stride1, v2, stride2, length)), length_(length) {
-    r_ = res->vi_;
-    alpha_ = alpha->vi_;
-    v1_ = (stan::agrad::vari**)stan::agrad::memalloc_.alloc(2*length_*sizeof(stan::agrad::vari*));
-    v2_ = v1_ + length_;
-    for (size_t i = 0; i < length_; i++)
-      v1_[i] = v1[i*stride1].vi_;
-    for (size_t i = 0; i < length_; i++)
-      v2_[i] = v2[i*stride2].vi_;
-  }
-  void chain() {
-    for (size_t i = 0; i < length_; i++) {
-      v1_[i]->adj_ += adj_ * v2_[i]->val_ * alpha_->val_;
-      v2_[i]->adj_ += adj_ * v1_[i]->val_ * alpha_->val_;
-      alpha_->adj_ += adj_ * v1_[i]->val_ * v2_[i]->val_;
-    }
-  }
-};*/
 }
 }
 
