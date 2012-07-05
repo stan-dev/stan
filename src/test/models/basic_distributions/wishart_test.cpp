7787ed17f09b7155062ed8cd0c8e77cadfca61f1
Daniel Lee <bearlee@alum.mit.edu>
2012-07-05 15:15:47 -0400

    test-models: adding expected value tests for models/basic_distributions

--- src/test/models/basic_distributions/wishart_test.cpp
+++ src/test/models/basic_distributions/wishart_test.cpp
@@ -20,7 +20,14 @@ public:
 
   static std::vector<std::pair<size_t, double> >
   get_expected_values() {
+    using std::make_pair;
     std::vector<std::pair<size_t, double> > expected_values;
+    
+    expected_values.push_back(make_pair(0U, 2.0 * 4.0));
+    expected_values.push_back(make_pair(1U, 0.0));
+    expected_values.push_back(make_pair(2U, 0.0));
+    expected_values.push_back(make_pair(3U, 0.5 * 4.0));
+    
     return expected_values;
   }
 
