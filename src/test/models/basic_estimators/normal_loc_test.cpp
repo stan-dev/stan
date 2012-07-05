af50846855e05370a5ae6174839c341ecc2abe72
Daniel Lee <bearlee@alum.mit.edu>
2012-07-05 16:37:48 -0400

    test-models: adding tests for basic_estimators

--- src/test/models/basic_estimators/normal_loc_test.cpp
+++ src/test/models/basic_estimators/normal_loc_test.cpp
@@ -21,7 +21,10 @@ public:
 
   static std::vector<std::pair<size_t, double> >
   get_expected_values() {
+    using std::make_pair;
     std::vector<std::pair<size_t, double> > expected_values;
+
+    expected_values.push_back(make_pair(0U, 1.15));
     return expected_values;
   }
 
