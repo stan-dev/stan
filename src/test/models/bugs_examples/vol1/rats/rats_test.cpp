e53a974a46d29cf7188cdc35c9087dd636d273e4
Daniel Lee <bearlee@alum.mit.edu>
2012-07-07 17:03:48 -0400

    test-models: added test for sampled values for bugs_examples/vol1/rats/rats

--- src/test/models/bugs_examples/vol1/rats/rats_test.cpp
+++ src/test/models/bugs_examples/vol1/rats/rats_test.cpp
@@ -31,7 +31,24 @@ public:
 
   static std::vector<std::pair<size_t, double> >
   get_expected_values() {
+    using std::make_pair;
+    size_t index;
+    std::vector<size_t> idxs;
+    idxs.push_back(0);
     std::vector<std::pair<size_t, double> > expected_values;
+    
+    index = chains->get_total_param_index(chains->param_name_to_index("alpha0"),
+					  idxs);
+    expected_values.push_back(make_pair(index, 106.6));
+
+    index = chains->get_total_param_index(chains->param_name_to_index("mu_beta"),
+					  idxs);
+    expected_values.push_back(make_pair(index, 6.185));
+    
+    index = chains->get_total_param_index(chains->param_name_to_index("sigma_y"),
+					  idxs);
+expected_values.push_back(make_pair(index, 6.086));
+    
     return expected_values;
   }
 
