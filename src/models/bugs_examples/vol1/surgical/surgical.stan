8544c433962e7c71ec0ca9effd8b41fcf159521a
Daniel Lee <bearlee@alum.mit.edu>
2012-07-09 15:31:40 -0400

    test-models: adding value tests for surgical

--- src/models/bugs_examples/vol1/surgical/surgical.stan
+++ src/models/bugs_examples/vol1/surgical/surgical.stan
@@ -1,6 +1,4 @@
-
 ## http://openbugs.info/Examples/Surgical.html
-
 ## random effects model 
 data {
   int(0,) N;
@@ -8,22 +6,24 @@ data {
   int n[N];
 }
 parameters {
-   real b[N];
    real mu;
    real(0,) sigmasq;
+   real b[N];
 }
 transformed parameters {
-  real(0,) sigma; 
+  real(0,) sigma;
+  real(0,1) p[N];
   sigma <- sqrt(sigmasq); 
+  for (i in 1:N)
+    p[i] <- inv_logit(b[i]);
 }
 model {
+  mu ~ normal(0.0, 1000.0); 
+  sigmasq ~ inv_gamma(0.001, 0.001);
   b ~ normal(mu, sigma);
   for (i in 1:N) 
     r[i] ~ binomial(n[i], inv_logit(b[i]));
-  mu ~ normal(0.0, 1000); 
-  sigmasq ~ inv_gamma(0.001, 0.001);
 }
-
 generated quantities {
   real pop_mean;
   pop_mean <- inv_logit(mu); 
--- src/test/models/bugs_examples/vol1/surgical/surgical_test.cpp
+++ src/test/models/bugs_examples/vol1/surgical/surgical_test.cpp
@@ -31,7 +31,25 @@ public:
 
   static std::vector<std::pair<size_t, double> >
   get_expected_values() {
+    using std::make_pair;
     std::vector<std::pair<size_t, double> > expected_values;
+
+    expected_values.push_back(make_pair( 0U, -2.558));   // mu
+    expected_values.push_back(make_pair(15U, 0.05302));  // p[1]
+    expected_values.push_back(make_pair(16U, 0.1029));   // p[2]
+    expected_values.push_back(make_pair(17U, 0.07044));  // p[3]
+    expected_values.push_back(make_pair(18U, 0.0593));   // p[4]
+    expected_values.push_back(make_pair(19U, 0.05187));  // p[5]
+    expected_values.push_back(make_pair(20U, 0.06903));  // p[6]
+    expected_values.push_back(make_pair(21U, 0.06682));  // p[7]
+    expected_values.push_back(make_pair(22U, 0.1226));   // p[8]
+    expected_values.push_back(make_pair(23U, 0.0698));   // p[9]
+    expected_values.push_back(make_pair(24U, 0.07851));  // p[10]
+    expected_values.push_back(make_pair(25U, 0.1021));   // p[11]
+    expected_values.push_back(make_pair(26U, 0.06858));  // p[12]
+    expected_values.push_back(make_pair(27U, 0.07259));  // pop.mean
+    expected_values.push_back(make_pair(14U, 0.4028));   // sigma
+
     return expected_values;
   }
 
