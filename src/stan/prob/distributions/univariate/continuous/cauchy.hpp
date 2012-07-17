7249bba7d3f1c7702c76727980a916e9f7241bc7
Daniel Lee <bearlee@alum.mit.edu>
2012-07-17 00:06:36 -0400

    distributions: vectorized cauchy

--- src/stan/prob/distributions/univariate/continuous/cauchy.hpp
+++ src/stan/prob/distributions/univariate/continuous/cauchy.hpp
@@ -1,6 +1,7 @@
 #ifndef __STAN__PROB__DISTRIBUTIONS__CAUCHY_HPP__
 #define __STAN__PROB__DISTRIBUTIONS__CAUCHY_HPP__
 
+#include <stan/agrad.hpp>
 #include <stan/prob/traits.hpp>
 #include <stan/math/error_handling.hpp>
 #include <stan/math/special_functions.hpp>
@@ -10,11 +11,28 @@ namespace stan {
 
   namespace prob {
 
-    // Cauchy(y|mu,sigma)  [sigma > 0]
-    template <bool propto,
+    /**
+     * The log of the Cauchy density for the specified scalar(s) given the specified
+     * location parameter(s) and scale parameter(s). y, mu, or sigma can each either be scalar or std::vector.
+     * Any vector inputs must be the same length.
+     *
+     * <p> The result log probability is defined to be the sum of
+     * the log probabilities for each observation/mu/sigma triple.
+     *
+     * @param y (Sequence of) scalar(s).
+     * @param mu (Sequence of) location(s).
+     * @param sigma (Sequence of) scale(s).
+     * @return The log of the product of densities.
+     * @tparam T_y Type of scalar outcome.
+     * @tparam T_loc Type of location.
+     * @tparam T_scale Type of scale.
+     * @error_policy
+     *    @li sigma must be positive.
+     */
+    template <bool Prop,
               typename T_y, typename T_loc, typename T_scale, 
               class Policy>
-    typename boost::math::tools::promote_args<T_y,T_loc,T_scale>::type
+    typename return_type<T_y,T_loc,T_scale>::type
     cauchy_log(const T_y& y, const T_loc& mu, const T_scale& sigma, 
                const Policy&) {
       static const char* function = "stan::prob::cauchy_log<%1%>(%1%)";
@@ -22,38 +40,102 @@ namespace stan {
       using stan::math::check_positive;
       using stan::math::check_finite;
       using stan::math::check_not_nan;
-      using boost::math::tools::promote_args;
+      using stan::math::check_consistent_sizes;
+      using stan::math::value_of;
+
+      // check if no variables are involved and prop-to
+      if (!include_summand<Prop,T_y,T_loc,T_scale>::value)
+        return 0.0;
+      // check if any vectors are zero length
+      if (!(stan::length(y) 
+            && stan::length(mu) 
+            && stan::length(sigma)))
+        return 0.0;
       
-      typename promote_args<T_y,T_loc,T_scale>::type lp(0.0);
-      if(!check_not_nan(function, y, "Random variate, y,", &lp, Policy()))
-        return lp;
-      if(!check_finite(function, mu, "Location parameter, mu,", 
-                       &lp, Policy()))
-        return lp;
-      if(!check_finite(function, sigma, "Scale parameter, sigma,", 
-                       &lp, Policy()))
-        return lp;
-      if(!check_positive(function, sigma, "Scale parameter, sigma,", 
-                         &lp, Policy()))
-        return lp;
+      // set up return value accumulator
+      double logp(0.0);
+
+      // validate args (here done over var, which should be OK)
+      if (!check_not_nan(function, y, "Random variate, y,", &logp, Policy()))
+        return logp;
+      if (!check_finite(function, mu, "Location parameter, mu,", 
+                        &logp, Policy()))
+        return logp;
+      if (!check_positive(function, sigma, "Scale parameter, sigma,", 
+                          &logp, Policy()))
+        return logp;
+      if (!check_finite(function, sigma, "Scale parameter, sigma,", 
+                        &logp, Policy()))
+        return logp;
+      if (!(check_consistent_sizes(function,
+                                   y,mu,sigma,"y","mu","sigma",
+                                   &logp, Policy())))
+        return logp;
 
       using stan::math::log1p;
       using stan::math::square;
       
-      if (include_summand<propto>::value)
-        lp += NEG_LOG_PI;
-      if (include_summand<propto,T_scale>::value)
-        lp -= log(sigma);
-      if (include_summand<propto,T_y,T_loc,T_scale>::value)
-        lp -= log1p(square((y - mu) / sigma));
-      return lp;
+      // set up template expressions wrapping scalars into vector views
+      VectorView<const T_y> y_vec(y);
+      VectorView<const T_loc> mu_vec(mu);
+      VectorView<const T_scale> sigma_vec(sigma);
+      size_t N = max_size(y, mu, sigma);
+
+      AmbiguousVector<double, is_vector<T_scale>::value> 
+        inv_sigma(length(sigma));
+      AmbiguousVector<double, is_vector<T_scale>::value> 
+        log_sigma(length(sigma));
+      for (size_t i = 0; i < length(sigma); i++) {
+        inv_sigma[i] = 1.0 / value_of(sigma_vec[i]);
+        log_sigma[i] = log(value_of(sigma_vec[i]));
+      }
+
+
+      agrad::OperandsAndPartials<T_y, T_loc, T_scale>
+        operands_and_partials(y, mu, sigma, y_vec, mu_vec, sigma_vec);
+
+      for (size_t n = 0; n < N; n++) {
+	// pull out values of arguments
+        const double y_dbl = value_of(y_vec[n]);
+        const double mu_dbl = value_of(mu_vec[n]);
+        const double sigma_dbl = value_of(sigma_vec[n]);
+	
+	// reusable subexpression values
+	const double y_minus_mu
+	  = y_dbl - mu_dbl;
+	const double y_minus_mu_squared
+	  = y_minus_mu * y_minus_mu;
+        const double y_minus_mu_over_sigma 
+          = y_minus_mu * inv_sigma[n];
+        const double y_minus_mu_over_sigma_squared 
+          = y_minus_mu_over_sigma * y_minus_mu_over_sigma;
+	const double sigma_squared
+	  = sigma_dbl * sigma_dbl;
+
+	// log probability
+	if (include_summand<Prop>::value)
+	  logp += NEG_LOG_PI;
+	if (include_summand<Prop,T_scale>::value)
+	  logp -= log_sigma[n];
+	if (include_summand<Prop,T_y,T_loc,T_scale>::value)
+	  logp -= log1p(y_minus_mu_over_sigma_squared);
+	
+        // gradients
+	if (!is_constant<typename is_vector<T_y>::type>::value)
+	  operands_and_partials.d_x1[n] -= 2 * y_minus_mu / (sigma_squared + y_minus_mu_squared);
+        if (!is_constant<typename is_vector<T_loc>::type>::value)
+          operands_and_partials.d_x2[n] += 2 * y_minus_mu / (sigma_squared + y_minus_mu_squared);
+        if (!is_constant<typename is_vector<T_scale>::type>::value)
+          operands_and_partials.d_x3[n] += (y_minus_mu_squared - sigma_squared) / sigma_dbl / (sigma_squared + y_minus_mu_squared);
+      }
+      return operands_and_partials.to_var(logp);
     }
 
 
     template <bool propto,
               typename T_y, typename T_loc, typename T_scale>
     inline
-    typename boost::math::tools::promote_args<T_y,T_loc,T_scale>::type
+    typename return_type<T_y,T_loc,T_scale>::type
     cauchy_log(const T_y& y, const T_loc& mu, const T_scale& sigma) {
       return cauchy_log<propto>(y,mu,sigma,stan::math::default_policy());
     }
@@ -61,7 +143,7 @@ namespace stan {
     template <typename T_y, typename T_loc, typename T_scale, 
               class Policy>
     inline
-    typename boost::math::tools::promote_args<T_y,T_loc,T_scale>::type
+    typename return_type<T_y,T_loc,T_scale>::type
     cauchy_log(const T_y& y, const T_loc& mu, const T_scale& sigma,
                const Policy&) {
       return cauchy_log<false>(y,mu,sigma,Policy());
@@ -92,7 +174,7 @@ namespace stan {
      */
     template <typename T_y, typename T_loc, typename T_scale, 
               class Policy>
-    typename boost::math::tools::promote_args<T_y,T_loc,T_scale>::type
+    typename return_type<T_y,T_loc,T_scale>::type
     cauchy_p(const T_y& y, const T_loc& mu, const T_scale& sigma, 
                const Policy&) {
       static const char* function = "stan::prob::cauchy_p<%1%>(%1%)";
@@ -122,7 +204,7 @@ namespace stan {
    }
 
     template <typename T_y, typename T_loc, typename T_scale>
-    typename boost::math::tools::promote_args<T_y,T_loc,T_scale>::type
+    typename return_type<T_y,T_loc,T_scale>::type
     cauchy_p(const T_y& y, const T_loc& mu, const T_scale& sigma) {
       return cauchy_p(y, mu, sigma, stan::math::default_policy());
     }
