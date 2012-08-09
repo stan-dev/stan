18c5b8abb77cac02584a640ac4a89470c9e1b761
Daniel Lee <bearlee@alum.mit.edu>
2012-08-09 10:28:10 -0400

    vectorized check_less_or_equal

--- src/test/math/error_handling_test.cpp
+++ src/test/math/error_handling_test.cpp
@@ -1405,3 +1405,197 @@ TEST(MathErrorHandling,CheckLessOrEqualErrnoPolicy) {
   EXPECT_FALSE(std::isnan(result));
 }
 
+TEST(MathErrorHandling,CheckLessOrEqualMatrixDefaultPolicy) {
+  const char* function = "check_less_or_equal(%1%)";
+  double result;
+  double x;
+  double high;
+  Eigen::Matrix<double,Eigen::Dynamic,1> x_vec;
+  Eigen::Matrix<double,Eigen::Dynamic,1> high_vec;
+  x_vec.resize(3);
+  high_vec.resize(3);
+  
+  
+  // x_vec, high
+  result = 0;
+  x_vec << -5, 0, 5;
+  high = 10;
+  EXPECT_TRUE(check_less_or_equal(function, x_vec, high, "x", &result));
+
+  result = 0;
+  x_vec << -5, 0, 5;
+  high = std::numeric_limits<double>::infinity();
+  EXPECT_TRUE(check_less_or_equal(function, x_vec, high, "x", &result));
+
+  result = 0;
+  x_vec << -5, 0, 5;
+  high = 5;
+  EXPECT_TRUE(check_less_or_equal(function, x_vec, high, "x", &result));
+  
+  result = 0;
+  x_vec << -5, 0, std::numeric_limits<double>::infinity();
+  high = 5;
+  EXPECT_THROW(check_less_or_equal(function, x_vec, high, "x", &result), std::domain_error);
+
+  result = 0;
+  x_vec << -5, 0, std::numeric_limits<double>::infinity();
+  high = std::numeric_limits<double>::infinity();
+  EXPECT_TRUE(check_less_or_equal(function, x_vec, high, "x", &result));
+  
+  // x_vec, high_vec
+  result = 0;
+  x_vec << -5, 0, 5;
+  high_vec << 0, 5, 10;
+  EXPECT_TRUE(check_less_or_equal(function, x_vec, high_vec, "x", &result));
+
+  result = 0;
+  x_vec << -5, 0, 5;
+  high_vec << std::numeric_limits<double>::infinity(), 10, 10;
+  EXPECT_TRUE(check_less_or_equal(function, x_vec, high_vec, "x", &result));
+
+  result = 0;
+  x_vec << -5, 0, 5;
+  high_vec << 10, 10, 5;
+  EXPECT_TRUE(check_less_or_equal(function, x_vec, high_vec, "x", &result));
+  
+  result = 0;
+  x_vec << -5, 0, std::numeric_limits<double>::infinity();
+  high_vec << 10, 10, 10;
+  EXPECT_THROW(check_less_or_equal(function, x_vec, high_vec, "x", &result), std::domain_error);
+
+  result = 0;
+  x_vec << -5, 0, std::numeric_limits<double>::infinity();
+  high_vec << 10, 10, std::numeric_limits<double>::infinity();
+  EXPECT_TRUE(check_less_or_equal(function, x_vec, high_vec, "x", &result));
+
+  
+  // x, high_vec
+  result = 0;
+  x = -100;
+  high_vec << 0, 5, 10;
+  EXPECT_TRUE(check_less_or_equal(function, x, high_vec, "x", &result));
+
+  result = 0;
+  x = 10;
+  high_vec << 100, 200, std::numeric_limits<double>::infinity();
+  EXPECT_TRUE(check_less_or_equal(function, x, high_vec, "x", &result));
+
+  result = 0;
+  x = 5;
+  high_vec << 100, 200, 5;
+  EXPECT_TRUE(check_less_or_equal(function, x, high_vec, "x", &result));
+  
+  result = 0;
+  x = std::numeric_limits<double>::infinity();
+  high_vec << 10, 20, 30;
+  EXPECT_THROW(check_less_or_equal(function, x, high_vec, "x", &result), std::domain_error);
+
+  result = 0;
+  x = std::numeric_limits<double>::infinity();
+  high_vec << std::numeric_limits<double>::infinity(), std::numeric_limits<double>::infinity(), std::numeric_limits<double>::infinity();
+  EXPECT_TRUE(check_less_or_equal(function, x, high_vec, "x", &result));
+}
+
+TEST(MathErrorHandling,CheckLessOrEqualMatrixErrnoPolicy) {
+  const char* function = "check_less_or_equal(%1%)";
+  double result;
+  double x;
+  double high;
+  Eigen::Matrix<double,Eigen::Dynamic,1> x_vec;
+  Eigen::Matrix<double,Eigen::Dynamic,1> high_vec;
+  x_vec.resize(3);
+  high_vec.resize(3);
+  
+  // x_vec, high
+  result = 0;
+  x_vec << -5, 0, 5;
+  high = 10;
+  EXPECT_TRUE(check_less_or_equal(function, x_vec, high, "x", &result, errno_policy()));
+  EXPECT_TRUE(!std::isnan(result));
+
+  result = 0;
+  x_vec << -5, 0, 5;
+  high = std::numeric_limits<double>::infinity();
+  EXPECT_TRUE(check_less_or_equal(function, x_vec, high, "x", &result, errno_policy()));
+  EXPECT_TRUE(!std::isnan(result));
+
+  result = 0;
+  x_vec << -5, 0, 5;
+  high = 5;
+  EXPECT_TRUE(check_less_or_equal(function, x_vec, high, "x", &result, errno_policy()));
+  EXPECT_TRUE(!std::isnan(result));
+  
+  result = 0;
+  x_vec << -5, 0, std::numeric_limits<double>::infinity();
+  high = 5;
+  EXPECT_FALSE(check_less_or_equal(function, x_vec, high, "x", &result, errno_policy()));
+  EXPECT_FALSE(!std::isnan(result));
+  
+  result = 0;
+  x_vec << -5, 0, std::numeric_limits<double>::infinity();
+  high = std::numeric_limits<double>::infinity();
+  EXPECT_TRUE(check_less_or_equal(function, x_vec, high, "x", &result, errno_policy()));
+  EXPECT_TRUE(!std::isnan(result));
+  
+  // x_vec, high_vec
+  result = 0;
+  x_vec << -5, 0, 5;
+  high_vec << 0, 5, 10;
+  EXPECT_TRUE(check_less_or_equal(function, x_vec, high_vec, "x", &result, errno_policy()));
+  EXPECT_TRUE(!std::isnan(result));
+
+  result = 0;
+  x_vec << -5, 0, 5;
+  high_vec << std::numeric_limits<double>::infinity(), 10, 10;
+  EXPECT_TRUE(check_less_or_equal(function, x_vec, high_vec, "x", &result, errno_policy()));
+  EXPECT_TRUE(!std::isnan(result));
+
+  result = 0;
+  x_vec << -5, 0, 5;
+  high_vec << 10, 10, 5;
+  EXPECT_TRUE(check_less_or_equal(function, x_vec, high_vec, "x", &result, errno_policy()));
+  EXPECT_TRUE(!std::isnan(result));
+
+  result = 0;
+  x_vec << -5, 0, std::numeric_limits<double>::infinity();
+  high_vec << 10, 10, 10;
+  EXPECT_FALSE(check_less_or_equal(function, x_vec, high_vec, "x", &result, errno_policy()));
+  EXPECT_FALSE(!std::isnan(result));
+
+  result = 0;
+  x_vec << -5, 0, std::numeric_limits<double>::infinity();
+  high_vec << 10, 10, std::numeric_limits<double>::infinity();
+  EXPECT_TRUE(check_less_or_equal(function, x_vec, high_vec, "x", &result, errno_policy()));
+  EXPECT_TRUE(!std::isnan(result));
+  
+  // x, high_vec
+  result = 0;
+  x = -100;
+  high_vec << 0, 5, 10;
+  EXPECT_TRUE(check_less_or_equal(function, x, high_vec, "x", &result, errno_policy()));
+  EXPECT_TRUE(!std::isnan(result));
+
+  result = 0;
+  x = 10;
+  high_vec << 100, 200, std::numeric_limits<double>::infinity();
+  EXPECT_TRUE(check_less_or_equal(function, x, high_vec, "x", &result, errno_policy()));
+  EXPECT_TRUE(!std::isnan(result));
+
+  result = 0;
+  x = 5;
+  high_vec << 100, 200, 5;
+  EXPECT_TRUE(check_less_or_equal(function, x, high_vec, "x", &result, errno_policy()));
+  EXPECT_TRUE(!std::isnan(result));
+
+  result = 0;
+  x = std::numeric_limits<double>::infinity();
+  high_vec << 10, 20, 30;
+  EXPECT_FALSE(check_less_or_equal(function, x, high_vec, "x", &result, errno_policy()));
+  EXPECT_FALSE(!std::isnan(result));
+
+  result = 0;
+  x = std::numeric_limits<double>::infinity();
+  high_vec << std::numeric_limits<double>::infinity(), std::numeric_limits<double>::infinity(), std::numeric_limits<double>::infinity();
+  EXPECT_TRUE(check_less_or_equal(function, x, high_vec, "x", &result, errno_policy()));
+  EXPECT_TRUE(!std::isnan(result));
+}
