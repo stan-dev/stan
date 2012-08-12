6fefc4d6c89063d5cd0566cc853ad69c2d12ed56
Jiqiang Guo <guojq28@gmail.com>
2012-08-12 01:32:27 -0400

    rstan: add gpl license header to some files

--- rstan/rstan/R/Rcpp_module_def.R
+++ rstan/rstan/R/Rcpp_module_def.R
@@ -1,3 +1,21 @@
+# Part of the rstan package for an R interface to Stan 
+# Copyright (C) 2012 Columbia University
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
 get_Rcpp_module_def_code <- function(model.name) {
   def_Rcpp_module_hpp_file <- 
     system.file('include', '/rstan/rcpp_module_def_for_rstan.hpp', package = 'rstan') 
--- rstan/rstan/R/zzz.R
+++ rstan/rstan/R/zzz.R
@@ -1,6 +1,33 @@
+# Part of the rstan package for an R interface to Stan 
+# Copyright (C) 2012 Columbia University
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 .onLoad <- function(libname, pkgname) {
     # require("methods", character = TRUE, quietly = TRUE)
     loadRcppModules()
 }
+
+.onAttach <- function(...) {
+  rstanLib <- dirname(system.file(package = "rstan"))
+  version <- packageDescription("rstan", lib.loc = rstanLib)$Version
+  builddate <- packageDescription("rstan", lib.loc = rstanLib)$Packaged
+  packageStartupMessage(paste("rstan (Version ", version, ", packaged: ", builddate, ")", sep = ""))
+  packageStartupMessage("rstan  Copyright (C) 2012 Columbia University")
+  packageStartupMessage("This program comes with ABSOLUTELY NO WARRANTY.")
+  packageStartupMessage("This is free software, and you are welcome to redistribute it")
+  packageStartupMessage("under the General Public License version 3 or later.")
+} 
 
