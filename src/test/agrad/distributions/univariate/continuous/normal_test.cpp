ed4a8c510ecb3dfcaf2e229f6ce07004ea4768f5
Daniel Lee <bearlee@alum.mit.edu>
2012-09-04 20:49:23 -0400

    updating test to compile and pass

--- src/test/agrad/distributions/univariate/continuous/normal_test.cpp
+++ src/test/agrad/distributions/univariate/continuous/normal_test.cpp
@@ -325,14 +325,14 @@ TEST(AgradDistributions,NormalCdfGrad) {
   var y = 1.0;
   var mu = 5.0;
   var sigma = 12.0;
-  std::vector x(3);
+  std::vector<var> x(3);
   x[0] = y;
   x[1] = mu;
   x[2] = sigma;
 
-  var p = normal_p(y,mu,sigma);
+  var p = stan::prob::normal_p(y,mu,sigma);
 
-  std::vector g;
+  std::vector<double> g;
   p.grad(x,g);
   
 }
