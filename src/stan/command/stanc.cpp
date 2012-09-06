464fe331e534e84d8f250bdfd031feb808c4dac9
Bob Carpenter <carp@alias-i.com>
2012-09-06 14:16:24 -0400

    fixed stanc to remove output file if parsing fails, closing issue 57

--- src/stan/command/stanc.cpp
+++ src/stan/command/stanc.cpp
@@ -1,6 +1,7 @@
 #include <stan/version.hpp>
 #include <stan/gm/compiler.hpp>
 
+#include <cstdio>
 #include <exception>
 #include <fstream>
 #include <iostream>
@@ -55,6 +56,16 @@ void print_stanc_help() {
   
 }
 
+void delete_file(const std::string& file_name) {
+  int deleted = std::remove(file_name.c_str());
+  std::cout << (deleted == 0
+                ? "OUTPUT FILE REMOVED"
+                : "COULD NOT REMOVE OUTPUT FILE")
+            << ", file=" << file_name.c_str() 
+            << std::endl;
+}      
+
+
 /** 
  * The Stan compiler (stanc).
  *
@@ -81,87 +92,83 @@ int main(int argc, const char* argv[]) {
     return SUCCESS_RC;
   }
 
-  
+  if (cmd.bare_size() != 1) {
+    std::string msg("require file name to compile as input. "
+                    "execute \"stanc --help\" for more information");
+    throw std::invalid_argument(msg);
+  }
+  std::string in_file_name;
+  cmd.bare(0,in_file_name);
+  std::fstream in(in_file_name.c_str());
 
-  stan::gm::program prog;
-  try {
-    if (cmd.bare_size() != 1) {
-      std::string msg("require file name to compile as input. "
-                      "execute \"stanc --help\" for more information");
-      throw std::invalid_argument(msg);
+  std::string model_name;
+  if (cmd.has_key("name")) {
+    cmd.val("name",model_name);
+  } else {
+    size_t slashInd = in_file_name.rfind('/');
+    size_t ptInd = in_file_name.rfind('.');
+    if (ptInd == std::string::npos)
+      ptInd = in_file_name.length();
+    if (slashInd == std::string::npos) {
+      slashInd = in_file_name.rfind('\\');
     }
-    std::string in_file_name;
-    cmd.bare(0,in_file_name);
-    std::fstream in(in_file_name.c_str());
-
-    std::string model_name;
-    if (cmd.has_key("name")) {
-      cmd.val("name",model_name);
+    if (slashInd == std::string::npos) {
+      slashInd = 0;
     } else {
-      size_t slashInd = in_file_name.rfind('/');
-      size_t ptInd = in_file_name.rfind('.');
-      if (ptInd == std::string::npos)
-        ptInd = in_file_name.length();
-      if (slashInd == std::string::npos) {
-        slashInd = in_file_name.rfind('\\');
-      }
-      if (slashInd == std::string::npos) {
-        slashInd = 0;
-      } else {
-        slashInd++;
-      }
-      model_name = in_file_name.substr(slashInd,ptInd - slashInd) + "_model";
-      for (std::string::iterator strIt = model_name.begin();
-           strIt != model_name.end(); strIt++) {
-        if (!isalnum(*strIt) && *strIt != '_') {
-          *strIt = '_';
-        }
-      }
+      slashInd++;
     }
-    if (!isalpha(model_name[0]) && model_name[0] != '_') {
-      std::string msg("model_name must not start with a number or symbol other than _");
-      throw std::invalid_argument(msg);
-    }
+    model_name = in_file_name.substr(slashInd,ptInd - slashInd) + "_model";
     for (std::string::iterator strIt = model_name.begin();
          strIt != model_name.end(); strIt++) {
       if (!isalnum(*strIt) && *strIt != '_') {
-        std::string msg("model_name must contain only letters, numbers and _");
-        throw std::invalid_argument(msg);
+        *strIt = '_';
       }
     }
-    
-    std::string out_file_name;
-    if (cmd.has_key("o")) {
-      cmd.val("o",out_file_name);
-    } else {
-      out_file_name = model_name;
-      out_file_name += ".cpp";
+  }
+
+  std::string out_file_name;
+  if (cmd.has_key("o")) {
+    cmd.val("o",out_file_name);
+  } else {
+    out_file_name = model_name;
+    out_file_name += ".cpp";
+  }
+
+  if (!isalpha(model_name[0]) && model_name[0] != '_') {
+    std::string msg("model_name must not start with a number or symbol other than _");
+    throw std::invalid_argument(msg);
+  }
+  for (std::string::iterator strIt = model_name.begin();
+       strIt != model_name.end(); strIt++) {
+    if (!isalnum(*strIt) && *strIt != '_') {
+      std::string msg("model_name must contain only letters, numbers and _");
+      throw std::invalid_argument(msg);
     }
+  }
+    
+  stan::gm::program prog;
+  try {
     std::fstream out(out_file_name.c_str(),
                      std::fstream::out);
-
     std::cout << "Model name=" << model_name << std::endl;
     std::cout << "Input file=" << in_file_name << std::endl;
     std::cout << "Output file=" << out_file_name << std::endl;
-
     bool include_main = !cmd.has_flag("no_main");
-
     bool valid_model 
       = stan::gm::compile(in,out,model_name,include_main);
-    
     out.close();
-
     if (!valid_model) {
       std::cout << "PARSING FAILED." << std::endl;
+      delete_file(out_file_name);
       return PARSE_FAIL_RC;
     }
-
   } catch (const std::exception& e) {
     std::cout << std::endl
               << "ERROR PARSING"
               << std::endl
               << e.what()
               << std::endl;
+    delete_file(out_file_name);
     return EXCEPTION_RC;
   }
   
