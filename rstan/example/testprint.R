1836b72be6f0d89e00f827a5647bffaca28e79e7
Jiqiang Guo <guojq28@gmail.com>
2012-09-18 22:14:25 -0400

    implement support of print statement
    and add M B to the author list as on mc-stan.org

---
+++ rstan/example/testprint.R
@@ -0,0 +1,32 @@
+# add a print statement 
+
+library(rstan)
+
+school_code <- "
+data {
+    int<lower=0> J;           // number of schools
+    real y[J];                // estimated treatment effect (school j)
+    real<lower=0> sigma[J];   // std dev of effect estimate (school j)
+}
+parameters {
+    real mu;
+    real theta[J];
+    real<lower=0> tau;
+}
+model {
+    theta ~ normal(mu, tau); 
+    print(theta); 
+    y ~ normal(theta,sigma);
+}
+"
+
+J <- 8L 
+y <- c(28,  8, -3,  7, -1,  1, 18, 12)
+sigma <- c(15, 10, 16, 11,  9, 11, 10, 18)
+
+iter <- 1000
+dat <- c("J", "y", "sigma") 
+ssp <- stan(model_code = school_code, data = dat, iter = iter, chains = 4, refresh = -1) 
+ssp2 <- stan(fit = ssp, data = dat) 
+print(ssp)
+
