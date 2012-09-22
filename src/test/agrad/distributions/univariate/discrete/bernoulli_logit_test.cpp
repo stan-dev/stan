58761e52d03853385f8da17fb748fc8fdff0c03b
Daniel Lee <bearlee@alum.mit.edu>
2012-09-22 03:18:09 -0400

    updated bernoulli test, added bernoulli_logit test

---
+++ src/test/agrad/distributions/univariate/discrete/bernoulli_logit_test.cpp
@@ -0,0 +1,84 @@
+#define _LOG_PROB_ bernoulli_logit_log
+#include <stan/prob/distributions/univariate/discrete/bernoulli_logit.hpp>
+
+#include <test/agrad/distributions/distribution_test_fixture.hpp>
+#include <test/agrad/distributions/discrete_distribution_tests_2_params.hpp>
+
+using std::vector;
+using std::numeric_limits;
+using stan::agrad::var;
+
+class AgradDistributionsBernoulliLogistic : public AgradDistributionTest {
+public:
+  void valid_values(vector<vector<double> >& parameters) {
+    using stan::math::logit;
+    using std::exp;
+    vector<double> param(2);
+
+    param[0] = 1;           // n
+    param[1] = logit(0.25); // theta
+    parameters.push_back(param);
+
+    param[0] = 0;           // n
+    param[1] = logit(0.25); // theta
+    parameters.push_back(param);
+
+    param[0] = 1;           // n
+    param[1] = logit(0.01); // theta
+    parameters.push_back(param);
+
+    param[0] = 0;           // n
+    param[1] = logit(0.01); // theta
+    parameters.push_back(param);
+
+    param[0] = 0;            // n
+    param[1] = 25;           // theta
+    parameters.push_back(param);
+
+    param[0] = 1;            // n
+    param[1] = -25;          // theta
+    parameters.push_back(param);
+    
+    param[0] = 0;           // n
+    param[1] = -25;         // theta
+    parameters.push_back(param);
+  }
+ 
+  void invalid_values(vector<size_t>& index, 
+		      vector<double>& value) {
+    // y
+    index.push_back(0U);
+    value.push_back(-1);
+
+    index.push_back(0U);
+    value.push_back(2);
+
+    // theta
+  }
+
+  template <class T_prob>
+  var log_prob(const int n, const T_prob& theta) {
+
+    using std::log;
+    using stan::math::log1m;
+    using stan::prob::include_summand;
+
+    if (include_summand<true,T_prob>::value) {
+      T_prob ntheta = (2*n-1) * theta;
+      // Handle extreme values gracefully using Taylor approximations.
+      const static double cutoff = 20.0;
+      if (ntheta > cutoff)
+	return -exp(-ntheta);
+      else if (ntheta < -cutoff)
+	return ntheta;
+      else
+	return -log(1 + exp(-ntheta));
+    }
+    return 0.0;
+  }
+};
+
+INSTANTIATE_TYPED_TEST_CASE_P(AgradDistributionsBernoulliLogistic,
+			      AgradDistributionTestFixture,
+			      AgradDistributionsBernoulliLogistic);
+
--- src/test/agrad/distributions/univariate/discrete/bernoulli_test.cpp
+++ src/test/agrad/distributions/univariate/discrete/bernoulli_test.cpp
@@ -70,36 +70,3 @@ INSTANTIATE_TYPED_TEST_CASE_P(AgradDistributionsBernoulli,
 			      AgradDistributionsBernoulli);
 
 
-
-#include <gtest/gtest.h>
-#include <test/agrad/distributions/expect_eq_diffs.hpp>
-#include <stan/agrad/agrad.hpp>
-#include <stan/agrad/special_functions.hpp>
-#include <stan/meta/traits.hpp>
-#include <stan/prob/distributions/univariate/discrete/bernoulli.hpp>
-
-template <typename T_prob>
-void expect_propto(unsigned int n1, T_prob alpha1, 
-                   unsigned int n2, T_prob alpha2, 
-                   std::string message) {
-  expect_eq_diffs(stan::prob::bernoulli_log<false>(n1, alpha1),
-                  stan::prob::bernoulli_log<false>(n2, alpha2),
-                  stan::prob::bernoulli_log<true>(n1, alpha1),
-                  stan::prob::bernoulli_log<true>(n2, alpha2),
-                  message);
-}
-
-using stan::agrad::var;
-
-TEST(AgradDistributionsBernoulli,Propto) {
-  unsigned int n;
-  n = 0;
-  expect_propto<var>(n, 0.3,
-                     n, 0.75,
-                     "var: prob, n=0");
-  n = 1;
-  expect_propto<var>(n, 0.3,
-                     n, 0.75,
-                     "var: prob, n=1");
-
-}
