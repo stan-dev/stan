adee891bd92cec87784251ff53c6c091b6d669f3
Daniel Lee <bearlee@alum.mit.edu>
2012-10-01 19:00:00 -0400

    vectorized logistic smarter

--- src/stan/prob/distributions/univariate/continuous/logistic.hpp
+++ src/stan/prob/distributions/univariate/continuous/logistic.hpp
@@ -68,25 +68,54 @@ namespace stan {
       VectorView<const T_scale> sigma_vec(sigma);
       size_t N = max_size(y, mu, sigma);
 
+      DoubleVectorView<true,T_scale> inv_sigma(length(sigma));
+      DoubleVectorView<include_summand<propto,T_scale>::value,T_scale> log_sigma(length(sigma));
+      for (size_t i = 0; i < length(sigma); i++) {
+        inv_sigma[i] = 1.0 / value_of(sigma_vec[i]);
+	if (include_summand<propto,T_scale>::value)
+	  log_sigma[i] = log(value_of(sigma_vec[i]));
+      }
+
+      DoubleVectorView<!is_constant_struct<T_loc>::value, double, is_vector<T_loc>::value || is_vector<T_scale>::value> 
+	exp_mu_div_sigma(max_size(mu,sigma));
+      DoubleVectorView<!is_constant_struct<T_loc>::value, double, is_vector<T_y>::value || is_vector<T_scale>::value> 
+	exp_y_div_sigma(max_size(y,sigma));
+      if (!is_constant_struct<T_loc>::value) {
+	for (size_t n = 0; n < max_size(mu,sigma); n++) 
+	  exp_mu_div_sigma[n] = exp(value_of(mu_vec[n]) / value_of(sigma_vec[n]));
+	for (size_t n = 0; n < max_size(y,sigma); n++) 
+	  exp_y_div_sigma[n] = exp(value_of(y_vec[n]) / value_of(sigma_vec[n]));
+      }
+
       using stan::math::log1p;
       for (size_t n = 0; n < N; n++) {
 	const double y_dbl = value_of(y_vec[n]);
 	const double mu_dbl = value_of(mu_vec[n]);
-	const double sigma_dbl = value_of(sigma_vec[n]);
 	
+	const double y_minus_mu = y_dbl - mu_dbl;
+	const double y_minus_mu_div_sigma = y_minus_mu * inv_sigma[n];
+	double exp_m_y_minus_mu_div_sigma(0);
 	if (include_summand<propto,T_y,T_loc,T_scale>::value)
-	  logp -= (y_dbl - mu_dbl)/sigma_dbl;
+	  exp_m_y_minus_mu_div_sigma = exp(-y_minus_mu_div_sigma);
+	double inv_1p_exp_y_minus_mu_div_sigma(0);
+	if (!is_constant_struct<T_y>::value || !is_constant_struct<T_scale>::value)
+	  inv_1p_exp_y_minus_mu_div_sigma = 1 / (1 + exp(y_minus_mu_div_sigma));
+
+	if (include_summand<propto,T_y,T_loc,T_scale>::value)
+	  logp -= y_minus_mu_div_sigma;
 	if (include_summand<propto,T_scale>::value)
-	  logp -= log(sigma_dbl);
+	  logp -= log_sigma[n];
 	if (include_summand<propto,T_y,T_loc,T_scale>::value)
-	  logp -= 2.0 * log1p(exp(-(y_dbl - mu_dbl)/sigma_dbl));
+	  logp -= 2.0 * log1p(exp_m_y_minus_mu_div_sigma);
 
 	if (!is_constant_struct<T_y>::value)
-	  operands_and_partials.d_x1[n] += -1/sigma_dbl + 2 / (1 + exp(-(y_dbl - mu_dbl)/sigma_dbl)) * exp(-(y_dbl - mu_dbl)/sigma_dbl) / sigma_dbl;
+	  operands_and_partials.d_x1[n] += (2 * inv_1p_exp_y_minus_mu_div_sigma - 1) * inv_sigma[n];
 	if (!is_constant_struct<T_loc>::value)
-	  operands_and_partials.d_x2[n] += 1/sigma_dbl - 2 * exp(mu_dbl/sigma_dbl) / (sigma_dbl * (exp(mu_dbl/sigma_dbl) + exp(y_dbl/sigma_dbl)));
+	  operands_and_partials.d_x2[n] +=
+	    (1 - 2 * exp_mu_div_sigma[n] / (exp_mu_div_sigma[n] + exp_y_div_sigma[n])) * inv_sigma[n];
 	if (!is_constant_struct<T_scale>::value)
-	  operands_and_partials.d_x3[n] += (y_dbl - mu_dbl)/sigma_dbl/sigma_dbl - 1/sigma_dbl - 2*(y_dbl - mu_dbl) * exp(-(y_dbl - mu_dbl)/sigma_dbl) / sigma_dbl / sigma_dbl / (exp(-(y_dbl - mu_dbl)/sigma_dbl) + 1);
+	  operands_and_partials.d_x3[n] += 
+	    ((1 - 2 * inv_1p_exp_y_minus_mu_div_sigma)*y_minus_mu*inv_sigma[n] - 1) * inv_sigma[n];
       }
       return operands_and_partials.to_var(logp);
     }
