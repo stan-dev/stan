78fc4a19c1246597b37ea6854a09d231591c6478
Daniel Lee <bearlee@alum.mit.edu>
2012-10-02 11:31:20 -0400

    pareto: calculated derivatives

--- src/stan/prob/distributions/univariate/continuous/pareto.hpp
+++ src/stan/prob/distributions/univariate/continuous/pareto.hpp
@@ -21,12 +21,23 @@ namespace stan {
                const Policy&) {
       static const char* function = "stan::prob::pareto_log(%1%)";
       
+      using stan::math::value_of;
       using stan::math::check_finite;
       using stan::math::check_positive;
       using stan::math::check_not_nan;
       using stan::math::check_consistent_sizes;
+
       
-      typename return_type<T_y,T_scale,T_shape>::type logp(0.0);
+      // check if any vectors are zero length
+      if (!(stan::length(y) 
+            && stan::length(y_min) 
+            && stan::length(alpha)))
+        return 0.0;
+      
+      // set up return value accumulator
+      double logp(0.0);
+      
+      // validate args (here done over var, which should be OK)
       if (!check_not_nan(function, y, "Random variable", &logp, Policy()))
         return logp;
       if (!check_finite(function, y_min, "Scale parameter",
@@ -47,27 +58,48 @@ namespace stan {
                                    &logp, Policy())))
         return logp;
 
-          
-      using stan::math::multiply_log;
-
+      // check if no variables are involved and prop-to
+      if (!include_summand<propto,T_y,T_scale,T_shape>::value)
+	return 0.0;
+      
       VectorView<const T_y> y_vec(y);
       VectorView<const T_scale> y_min_vec(y_min);
       VectorView<const T_shape> alpha_vec(alpha);
       size_t N = max_size(y, y_min, alpha);
 
       for (size_t n = 0; n < N; n++) {
-	if (include_summand<propto,T_y,T_scale>::value)
-	  if (y_vec[n] < y_min_vec[n])
-	    return LOG_ZERO;
-      
-	if (include_summand<propto,T_shape>::value)
-	  logp += log(alpha_vec[n]);
-	if (include_summand<propto,T_scale,T_shape>::value)
-	  logp += multiply_log(alpha_vec[n], y_min_vec[n]);
-	if (include_summand<propto,T_y,T_shape>::value)
-	  logp -= multiply_log(alpha_vec[n]+1.0, y_vec[n]);
+	if (y_vec[n] < y_min_vec[n])
+	  return LOG_ZERO;
       }
-      return logp;
+
+      // set up template expressions wrapping scalars into vector views
+      agrad::OperandsAndPartials<T_y,T_scale,T_shape> operands_and_partials(y, y_min, alpha);
+      
+      DoubleVectorView<include_summand<propto,T_y,T_shape>::value,T_y> log_y(length(y));
+      if (include_summand<propto,T_y,T_shape>::value)
+	for (size_t n = 0; n < length(y); n++)
+	  log_y[n] = log(value_of(y_vec[n]));
+      
+      
+      
+      using stan::math::multiply_log;
+
+      for (size_t n = 0; n < N; n++) {
+	if (include_summand<propto,T_shape>::value)
+	  logp += log(value_of(alpha_vec[n]));
+	if (include_summand<propto,T_scale,T_shape>::value)
+	  logp += multiply_log(value_of(alpha_vec[n]), value_of(y_min_vec[n]));
+	if (include_summand<propto,T_y,T_shape>::value)
+	  logp -= multiply_log(value_of(alpha_vec[n])+1.0, value_of(y_vec[n]));
+	
+	if (!is_constant_struct<T_y>::value)
+	  operands_and_partials.d_x1[n] -= (value_of(alpha_vec[n]) + 1) / value_of(y_vec[n]);
+	if (!is_constant_struct<T_scale>::value)
+	  operands_and_partials.d_x2[n] += (value_of(alpha_vec[n])) / value_of(y_min_vec[n]);
+	if (!is_constant_struct<T_shape>::value)
+	  operands_and_partials.d_x3[n] += 1 / value_of(alpha_vec[n]) + log(value_of(y_min_vec[n])) - log(value_of(y_vec[n]));
+      }
+      return operands_and_partials.to_var(logp);
     }
 
 
