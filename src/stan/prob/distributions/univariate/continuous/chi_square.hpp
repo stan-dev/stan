5eaed3927549cca9b5894841cdd79e30e7402a27
Daniel Lee <bearlee@alum.mit.edu>
2012-10-03 11:37:12 -0400

    vectorized chi_square, added test framework for 2 parameters

--- src/stan/prob/distributions/univariate/continuous/chi_square.hpp
+++ src/stan/prob/distributions/univariate/continuous/chi_square.hpp
@@ -1,11 +1,12 @@
 #ifndef __STAN__PROB__DISTRIBUTIONS__UNIVARIATE__CONTINUOUS__CHI_SQUARE_HPP__
 #define __STAN__PROB__DISTRIBUTIONS__UNIVARIATE__CONTINUOUS__CHI_SQUARE_HPP__
 
-#include <stan/prob/traits.hpp>
+#include <stan/agrad.hpp>
 #include <stan/math/error_handling.hpp>
 #include <stan/math/special_functions.hpp>
+#include <stan/meta/traits.hpp>
 #include <stan/prob/constants.hpp>
-#include <stan/prob/distributions/univariate/continuous/gamma.hpp>
+#include <stan/prob/traits.hpp>
 
 namespace stan {
 
@@ -33,44 +34,68 @@ namespace stan {
     template <bool propto,
               typename T_y, typename T_dof, 
               class Policy>
-    typename boost::math::tools::promote_args<T_y,T_dof>::type
+    typename return_type<T_y,T_dof>::type
     chi_square_log(const T_y& y, const T_dof& nu, const Policy&) {
       static const char* function = "stan::prob::chi_square_log(%1%)";
 
+      // check if any vectors are zero length
+      if (!(stan::length(y) 
+            && stan::length(nu)))
+	return 0.0;
+      
       using stan::math::check_positive;
       using stan::math::check_finite;
       using stan::math::check_not_nan;
-      using boost::math::tools::promote_args;
+      using stan::math::check_consistent_sizes;
+      using stan::math::value_of;
+      
+      typename return_type<T_y,T_dof>::type logp(0);
+      if (!check_not_nan(function, y, "Random variable", &logp, Policy()))
+        return logp;
+      if (!check_finite(function, nu, "Degrees of freedom parameter", &logp, Policy()))
+        return logp;
+      if (!check_positive(function, nu, "Degrees of freedom parameter", &logp, Policy()))
+        return logp;
+	  
+      if (!(check_consistent_sizes(function,
+                                   y,nu,
+				   "Random variable","Degrees of freedom parameter",
+                                   &logp, Policy())))
+        return logp;
+	  
+      
+      // set up template expressions wrapping scalars into vector views
+      VectorView<const T_y> y_vec(y);
+      VectorView<const T_dof> nu_vec(nu);
+      size_t N = max_size(y, nu);
+      
+      for (size_t n = 0; n < length(y); n++) 
+	if (value_of(y_vec[n]) < 0)
+	  return LOG_ZERO;
 
-      typename promote_args<T_y,T_dof>::type lp;
-      if (!check_not_nan(function, y, "Random variable", &lp, Policy()))
-        return lp;
-      if (!check_finite(function, nu, "Degrees of freedom parameter", &lp, Policy()))
-        return lp;
-      if (!check_positive(function, nu, "Degrees of freedom parameter", &lp, Policy()))
-        return lp;
-      
-      if (y < 0)
-        return LOG_ZERO;
-      
+      // check if no variables are involved and prop-to
+      if (!include_summand<propto,T_y,T_dof>::value)
+	return 0.0;
+
       using boost::math::lgamma;
       using stan::math::multiply_log;
       
-      lp = 0.0;
-      if (include_summand<propto,T_dof>::value)
-        lp += nu * NEG_LOG_TWO_OVER_TWO - lgamma(0.5 * nu);
-      if (include_summand<propto,T_y,T_dof>::value)
-        lp += multiply_log(0.5*nu-1.0, y);
-      if (include_summand<propto,T_y>::value)
-        lp -= 0.5 * y;
-      return lp;
+      for (size_t n = 0; n < N; n++) {
+	if (include_summand<propto,T_dof>::value)
+	  logp += nu_vec[n] * NEG_LOG_TWO_OVER_TWO - lgamma(0.5 * nu_vec[n]);
+	if (include_summand<propto,T_y,T_dof>::value)
+	  logp += multiply_log(0.5*nu_vec[n]-1.0, y_vec[n]);
+	if (include_summand<propto,T_y>::value)
+	  logp -= 0.5 * y_vec[n];
+      }
+      return logp;
     }
 
 
     template <bool propto,
               typename T_y, typename T_dof>
     inline
-    typename boost::math::tools::promote_args<T_y,T_dof>::type
+    typename return_type<T_y,T_dof>::type
     chi_square_log(const T_y& y, const T_dof& nu) {
       return chi_square_log<propto>(y,nu,stan::math::default_policy());
     }
@@ -79,7 +104,7 @@ namespace stan {
     template <typename T_y, typename T_dof, 
               class Policy>
     inline
-    typename boost::math::tools::promote_args<T_y,T_dof>::type
+    typename return_type<T_y,T_dof>::type
     chi_square_log(const T_y& y, const T_dof& nu, const Policy&) {
       return chi_square_log<false>(y,nu,Policy());
     }
@@ -87,7 +112,7 @@ namespace stan {
 
     template <typename T_y, typename T_dof>
     inline
-    typename boost::math::tools::promote_args<T_y,T_dof>::type
+    typename return_type<T_y,T_dof>::type
     chi_square_log(const T_y& y, const T_dof& nu) {
       return chi_square_log<false>(y,nu,stan::math::default_policy());
     }
@@ -103,16 +128,16 @@ namespace stan {
      */
     /*template <typename T_y, typename T_dof, 
               class Policy>
-    typename boost::math::tools::promote_args<T_y,T_dof>::type
+    typename return_type<T_y,T_dof>::type
     chi_square_cdf(const T_y& y, const T_dof& nu, const Policy&) {
       static const char* function = "stan::prob::chi_square_cdf(%1%)";
 
       using stan::math::check_positive;
       using stan::math::check_finite;
       using stan::math::check_not_nan;
-      using boost::math::tools::promote_args;
+      using return_type;
 
-      typename promote_args<T_y,T_dof>::type lp;
+      typename return_type<T_y,T_dof>::type lp;
       if (!check_not_nan(function, y, "Random variable", &lp, Policy()))
         return lp;
       if (!check_finite(function, nu, "Degrees of freedom parameter", &lp, Policy()))
@@ -125,7 +150,7 @@ namespace stan {
     }
 
     template <typename T_y, typename T_dof>
-    typename boost::math::tools::promote_args<T_y,T_dof>::type
+    typename return_type<T_y,T_dof>::type
     chi_square_cdf(const T_y& y, const T_dof& nu) {
       return chi_square_cdf(y, nu, stan::math::default_policy());
     }*/
