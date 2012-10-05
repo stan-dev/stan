ca0a8dcec2485ac48c9c8664c91401bb30df7b59
Daniel Lee <bearlee@alum.mit.edu>
2012-10-05 12:02:08 -0400

    vectorized scaled_inv_chi_square, added tests, updated function signatures, updated doc

--- src/stan/gm/function_signatures.hpp
+++ src/stan/gm/function_signatures.hpp
@@ -260,7 +260,12 @@ add("row",ROW_VECTOR_T,MATRIX_T,INT_T);
 add("rows",INT_T,VECTOR_T);
 add("rows",INT_T,ROW_VECTOR_T);
 add("rows",INT_T,MATRIX_T);
-add_ternary("scaled_inv_chi_square_log");
+for (size_t i = 0; i < vector_types.size(); ++i)
+  for (size_t j = 0; j < vector_types.size(); ++j)
+    for (size_t k = 0; k < vector_types.size(); ++k)
+      add("scaled_inv_chi_square_log",
+          DOUBLE_T, // result
+          vector_types[i], vector_types[j], vector_types[k]); // args
 add("sd",DOUBLE_T,expr_type(DOUBLE_T,1));
 add("sd",DOUBLE_T,VECTOR_T);
 add("sd",DOUBLE_T,ROW_VECTOR_T);
