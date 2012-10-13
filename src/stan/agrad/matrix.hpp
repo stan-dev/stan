b9dd2bbc9bf584c593d2e00352d3b0dc0dee92d0
Marcus Brubaker <marcus.a.brubaker@gmail.com>
2012-10-13 18:43:19 -0400

    Implemented a specialized check_pos_definite for var matrices which first converts to a double.

--- src/stan/agrad/matrix.hpp
+++ src/stan/agrad/matrix.hpp
@@ -8,6 +8,7 @@
 #include <stan/agrad/special_functions.hpp>
 #include <stan/math/boost_error_handling.hpp>
 #include <stan/math/matrix.hpp>
+#include <stan/math/matrix_error_handling.hpp>
 
 namespace stan {
   namespace agrad {
@@ -1005,9 +1006,9 @@ namespace stan {
 
     /**
      * Returns the sum of the coefficients of the specified
-     * column vector.
-     * @param v Specified vector.
-     * @return Sum of coefficients of vector.
+     * matrix, column vector or row vector.
+     * @param m Specified matrix or vector.
+     * @return Sum of coefficients of matrix.
      */
     template <int R, int C>
     inline var sum(const Eigen::Matrix<var,R,C>& m) {
@@ -1975,6 +1976,22 @@ namespace stan {
       n_lhs = n_rhs;  // FIXME: no call -- just filler to instantiate
     }
 
+    template <typename T_result, class Policy>
+    inline bool check_pos_definite(const char* function,
+                                   const Eigen::Matrix<var,Eigen::Dynamic,Eigen::Dynamic>& y,
+                                   const char* name,
+                                   T_result* result,
+                                   const Policy&) {
+        typedef 
+        typename Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic>::size_type 
+        size_type;
+        Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic> y_d(y.rows(),y.cols());
+        for (size_type i = 0; i < y_d.rows(); i++) 
+          for (size_type j = 0; j < y_d.cols(); j++)
+            y_d(i,j) = y(i,j).val();
+        return stan::math::check_pos_definite(function,y_d,name,result,Policy());
+    }
+    
     template <typename LHS, typename RHS>
     inline void assign_to_var(std::vector<LHS>& x, const std::vector<RHS>& y) {
       for (size_t i = 0; i < x.size(); ++i)
