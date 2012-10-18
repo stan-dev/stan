f390feece9c9900cfe72ee7d87a2683318d2e351
Jiqiang Guo <guojq28@gmail.com>
2012-10-18 19:51:54 -0400

    rstan: not reset the seed after initialization

--- rstan/rstan/inst/include/rstan/stan_fit.hpp
+++ rstan/rstan/inst/include/rstan/stan_fit.hpp
@@ -497,7 +497,7 @@ namespace rstan {
         rstan::io::rcout << "params_i[" << i << "]=" << params_i[i] << std::endl; 
       */
       // reset the seed 
-      base_rng.seed(random_seed); 
+      // base_rng.seed(random_seed); 
       base_rng.discard(DISCARD_STRIDE * (chain_id - 1));
 
       std::fstream sample_stream; 
