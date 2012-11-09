0d57d1f1d3633223684b50283c694a6eacca6f8f
Bob Carpenter <carp@alias-i.com>
2012-11-09 15:09:16 -0500

    updated funnel density code and text in manual

--- src/docs/stan-reference/R/funnel-plot.R
+++ src/docs/stan-reference/R/funnel-plot.R
@@ -4,7 +4,7 @@ p_funnel <- function(x1,y) {
   return(dnorm(y,0.0,3.0,log=TRUE) + dnorm(x1,0.0,exp(y),log=TRUE));
 }
 
-K = 500;
+K = 200;
 
 x1 <- rep(0,(K+1)^2);
 y <- rep(0,(K+1)^2);
@@ -12,8 +12,8 @@ log_p_y_x1 <- rep(0,(K+1)^2);
 pos <- 1;
 for (m in 1:(K+1)) {
   for (n in 1:(K+1)) {
-    y[pos] <- -6.0 + 12.0 * (m - 1) / K;    
-    x1[pos] <- -30.0 + 60.0 * (n - 1) / K;
+    y[pos] <- -9.0 + 18.0 * (m - 1) / K;    
+    x1[pos] <- -20.0 + 40.0 * (n - 1) / K;
     log_p_y_x1[pos] <- p_funnel(x1[pos],y[pos]);
     pos <- pos + 1;
   }
@@ -26,18 +26,16 @@ df <- data.frame(x1=x1,
                 log_p_y_x1=log_p_y_x1);
 funnel_plot <-
      ggplot(df, aes(x1,y,fill = log_p_y_x1)) +
-     labs(title = "Funnel Density\n") +
+     labs(title = "Funnel Density (log scale)\n") +
      geom_tile() +
-     scale_x_continuous("x[1]",expand=c(0,0),limits=c(-30,30)) +
-     scale_y_continuous(expand=c(0,0),limits=c(-4,4), breaks=c(-4,-2,0,2,4)) +
+     scale_x_continuous("x[1]",expand=c(0,0),limits=c(-20,20), breaks=c(-20,-10,0,10,20)) +
+     scale_y_continuous(expand=c(0,0),limits=c(-9,9), breaks=c(-9,-6,-3,0,3,6,9)) +
      scale_fill_gradient2("log p(y,x[1])\n",
-                          limits=c(-8,1),midpoint=-3,
-                          low="white", mid="black", high="black", na.value="white");                       
-#                         midpoint=-7, 
-#                          low="white", mid="gray", high="black", na.value="white");
-#                          low="orange", mid="white", high="blue", na.value="white");
-#                          breaks=c(0,-25,-50,-75,-100),
-#                          labels=c("0","-25","-50","-75","< -100"));
+                          limits=c(-18,2),   
+                          midpoint=-6.75, mid="lightyellow",
+                          low="gray95", high="darkblue", na.value="transparent",
+                          breaks=c(0,-8,-16),
+                          labels=c("0","-8","-16"));
 
 png(filename="funnel.png", width=1500,height=1200,res=300);
 print(funnel_plot);
