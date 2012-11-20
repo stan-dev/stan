9f3b7e16f7e55ac0459a85db4b48aa6580b76f49
Bob Carpenter <carp@alias-i.com>
2012-11-20 01:53:52 -0500

    vectorized gradient calcs for poisson

--- src/stan/prob/distributions/univariate/discrete/poisson.hpp
+++ src/stan/prob/distributions/univariate/discrete/poisson.hpp
@@ -30,10 +30,13 @@ namespace stan {
       
       // check if any vectors are zero length
       if (!(stan::length(n)
-	    && stan::length(lambda)))
-	return 0.0;
+            && stan::length(lambda)))
+        return 0.0;
 
-      typename return_type<T_rate>::type logp(0.0);
+      // set up return value accumulator
+      double logp(0.0);
+
+      // validate args
       if (!check_nonnegative(function, n, "Random variable", &logp, Policy()))
         return logp;
       if (!check_not_nan(function, lambda,
@@ -44,40 +47,51 @@ namespace stan {
         return logp;
       if (!(check_consistent_sizes(function,
                                    n,lambda,
-				   "Random variable","Rate parameter",
+                                   "Random variable","Rate parameter",
                                    &logp, Policy())))
         return logp;
       
       // check if no variables are involved and prop-to
       if (!include_summand<propto,T_rate>::value)
-	return 0.0;
+        return 0.0;
 
-      // set up template expressions wrapping scalars into vector views
+      // set up expression templates wrapping scalars/vecs into vector views
       VectorView<const T_n> n_vec(n);
       VectorView<const T_rate> lambda_vec(lambda);
       size_t size = max_size(n, lambda);
 
       for (size_t i = 0; i < size; i++)
-	if (std::isinf(lambda_vec[i]))
-	  return LOG_ZERO;
+        if (std::isinf(lambda_vec[i]))
+          return LOG_ZERO;
       for (size_t i = 0; i < size; i++)
-	if (lambda_vec[i] == 0 && n_vec[i] != 0)
-	  return LOG_ZERO;
+        if (lambda_vec[i] == 0 && n_vec[i] != 0)
+          return LOG_ZERO;
       
+      // return accumulator with gradients
+      agrad::OperandsAndPartials<T_rate> operands_and_partials(lambda);
+
       using stan::math::multiply_log;
       for (size_t i = 0; i < size; i++) {
-	if (!(lambda_vec[i] == 0 && n_vec[i] == 0)) {
-	  if (include_summand<propto>::value)
-	    logp -= lgamma(n_vec[i] + 1.0);
-	  if (include_summand<propto,T_rate>::value)
-	    logp += multiply_log(n_vec[i], lambda_vec[i]) - lambda_vec[i];
-	}
+        if (!(lambda_vec[i] == 0 && n_vec[i] == 0)) {
+          if (include_summand<propto>::value)
+            logp -= lgamma(n_vec[i] + 1.0);
+          if (include_summand<propto,T_rate>::value)
+            logp += multiply_log(n_vec[i], value_of(lambda_vec[i])) 
+              - value_of(lambda_vec[i]);
+        }
+
+        // gradients
+        if (!is_constant_struct<T_rate>::value)
+          operands_and_partials.d_x1[i] += n_vec[i] / value_of(lambda_vec[i]) - 1.0;
+        
       }
-      return logp;
+
+
+      return operands_and_partials.to_var(logp);
     }
     
     template <bool propto,
-	      typename T_n,
+              typename T_n,
               typename T_rate>
     inline
     typename return_type<T_rate>::type
@@ -87,7 +101,7 @@ namespace stan {
 
 
     template <typename T_n,
-	      typename T_rate, 
+              typename T_rate, 
               class Policy>
     inline
     typename return_type<T_rate>::type
@@ -98,7 +112,7 @@ namespace stan {
 
 
     template <typename T_n,
-	      typename T_rate>
+              typename T_rate>
     inline
     typename return_type<T_rate>::type
     poisson_log(const T_n& n, const T_rate& lambda) {
