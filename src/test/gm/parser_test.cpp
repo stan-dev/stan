f7ebc8a227662488a5cd99477122b850957ed98c
Bob Carpenter <carp@alias-i.com>
2012-12-14 02:45:02 -0500

    replaced _def.hpp with .hpp includes for grammars/ast

--- src/test/gm/parser_test.cpp
+++ src/test/gm/parser_test.cpp
@@ -5,14 +5,14 @@
 #include <exception>
 #include <stdexcept>
 
-#include "stan/gm/ast_def.cpp"
+#include "stan/gm/ast.hpp"
 #include "stan/gm/parser.hpp"
 #include <stan/gm/generator.hpp>
-#include <stan/gm/grammars/program_grammar_def.hpp>
-#include <stan/gm/grammars/whitespace_grammar_def.hpp>
-#include <stan/gm/grammars/expression_grammar_def.hpp>
-#include <stan/gm/grammars/statement_grammar_def.hpp>
-#include <stan/gm/grammars/var_decls_grammar_def.hpp>
+#include <stan/gm/grammars/program_grammar.hpp>
+#include <stan/gm/grammars/whitespace_grammar.hpp>
+#include <stan/gm/grammars/expression_grammar.hpp>
+#include <stan/gm/grammars/statement_grammar.hpp>
+#include <stan/gm/grammars/var_decls_grammar.hpp>
 
 
 bool is_parsable(const std::string& file_name) {
