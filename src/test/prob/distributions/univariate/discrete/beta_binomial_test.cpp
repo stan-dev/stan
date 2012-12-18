9485e56aeec44a36e4fc01fa4af6fd4f6fd187bb
Daniel Lee <bearlee@alum.mit.edu>
2012-12-18 15:52:17 -0500

    updated prob beta_binomial test

--- src/test/prob/distributions/univariate/discrete/beta_binomial_test.cpp
+++ src/test/prob/distributions/univariate/discrete/beta_binomial_test.cpp
@@ -1,95 +1,120 @@
-#include <gtest/gtest.h>
+#define _LOG_PROB_ beta_binomial_log
 #include <stan/prob/distributions/univariate/discrete/beta_binomial.hpp>
 
-TEST(ProbDistributionsBetaBinomial,BetaBinomial) {
-  EXPECT_FLOAT_EQ(-1.854007, stan::prob::beta_binomial_log(5,20,10.0,25.0));
-  EXPECT_FLOAT_EQ(-4.376696, stan::prob::beta_binomial_log(25,100,30.0,50.0));
-}
-TEST(ProbDistributionsBetaBinomial,Propto) {
-  EXPECT_FLOAT_EQ(0.0, stan::prob::beta_binomial_log<true>(5,20,10.0,25.0));
-  EXPECT_FLOAT_EQ(0.0, stan::prob::beta_binomial_log<true>(25,100,30.0,50.0));
-}
+#include <test/prob/distributions/distribution_test_fixture.hpp>
+#include <test/prob/distributions/distribution_tests_2_discrete_2_params.hpp>
 
-using boost::math::policies::policy;
-using boost::math::policies::evaluation_error;
-using boost::math::policies::domain_error;
-using boost::math::policies::overflow_error;
-using boost::math::policies::domain_error;
-using boost::math::policies::pole_error;
-using boost::math::policies::errno_on_error;
+using std::vector;
+using std::log;
+using std::numeric_limits;
 
-typedef policy<
-  domain_error<errno_on_error>, 
-  pole_error<errno_on_error>,
-  overflow_error<errno_on_error>,
-  evaluation_error<errno_on_error> 
-  > errno_policy;
+class ProbDistributionsBetaBinomial : public DistributionTest {
+public:
+  void valid_values(vector<vector<double> >& parameters,
+		    vector<double>& log_prob) {
+    vector<double> param(4);
 
-using stan::prob::beta_binomial_log;
 
-TEST(ProbDistributionsBetaBinomial,DefaultPolicy) {
-  double nan = std::numeric_limits<double>::quiet_NaN();
-  double inf = std::numeric_limits<double>::infinity();
+    param[0] = 5;            // n
+    param[1] = 20;           // N
+    param[2] = 10.0;         // alpha
+    param[3] = 25.0;         // beta
+    parameters.push_back(param);
+    log_prob.push_back(-1.854007); // expected log_prob
 
-  int n = 5;
-  int N = 15;
-  double alpha = 3.0;
-  double beta = 4.5;
+    param[0] = 25;           // n
+    param[1] = 100;          // N
+    param[2] = 30.0;         // alpha
+    param[3] = 50.0;         // beta
+    parameters.push_back(param);
+    log_prob.push_back(-4.376696); // expected log_prob
+  }
+ 
+  void invalid_values(vector<size_t>& index, 
+		      vector<double>& value) {
+    // n
+    
+    // N
+    index.push_back(1U);
+    value.push_back(-1);
+    
+    // alpha
+    index.push_back(2U);
+    value.push_back(0.0);
+    
+    index.push_back(2U);
+    value.push_back(-1.0);
 
-  EXPECT_NO_THROW(beta_binomial_log(n,N,alpha,beta));
-  EXPECT_NO_THROW(beta_binomial_log(n,0,alpha,beta));
+    index.push_back(2U);
+    value.push_back(std::numeric_limits<double>::infinity());
+
+    // beta
+    index.push_back(3U);
+    value.push_back(0.0);
+    
+    index.push_back(3U);
+    value.push_back(-1.0);
+
+    index.push_back(3U);
+    value.push_back(std::numeric_limits<double>::infinity());
+  }
+};
+
+INSTANTIATE_TYPED_TEST_CASE_P(ProbDistributionsBetaBinomial,
+			      DistributionTestFixture,
+			      ProbDistributionsBetaBinomial);
+
+
+// #include <gtest/gtest.h>
+// #include <stan/prob/distributions/univariate/discrete/beta_binomial.hpp>
+
+// TEST(ProbDistributionsBetaBinomial,DefaultPolicy) {
+//   double nan = std::numeric_limits<double>::quiet_NaN();
+//   double inf = std::numeric_limits<double>::infinity();
+
+//   int n = 5;
+//   int N = 15;
+//   double alpha = 3.0;
+//   double beta = 4.5;
+
+// }
+// TEST(ProbDistributionsBetaBinomial,ErrnoPolicy) {
+//   double nan = std::numeric_limits<double>::quiet_NaN();
+//   double inf = std::numeric_limits<double>::infinity();
+
+//   double result;
+//   int n = 5;
+//   int N = 15;
+//   double alpha = 3.0;
+//   double beta = 4.5;
+
+//   result = beta_binomial_log(n,N,alpha,beta, errno_policy());
+//   EXPECT_FALSE(std::isnan(result));
+//   result = beta_binomial_log(n,0,alpha,beta, errno_policy());
+//   EXPECT_FALSE(std::isnan(result));
   
-  EXPECT_THROW(beta_binomial_log(n,-1,alpha,beta), std::domain_error);
+//   result = beta_binomial_log(n,-1,alpha,beta, errno_policy());
+//   EXPECT_TRUE(std::isnan(result));
   
-  EXPECT_THROW(beta_binomial_log(n,N,nan,beta), std::domain_error);
-  EXPECT_THROW(beta_binomial_log(n,N,0.0,beta), std::domain_error);
-  EXPECT_THROW(beta_binomial_log(n,N,-1.0,beta), std::domain_error);
-  EXPECT_THROW(beta_binomial_log(n,N,-inf,beta), std::domain_error);
-  EXPECT_THROW(beta_binomial_log(n,N,inf,beta), std::domain_error);
+//   result = beta_binomial_log(n,N,nan,beta, errno_policy());
+//   EXPECT_TRUE(std::isnan(result));
+//   result = beta_binomial_log(n,N,0.0,beta, errno_policy());
+//   EXPECT_TRUE(std::isnan(result));
+//   result = beta_binomial_log(n,N,-1.0,beta, errno_policy());
+//   EXPECT_TRUE(std::isnan(result));
+//   result = beta_binomial_log(n,N,-inf,beta, errno_policy());
+//   EXPECT_TRUE(std::isnan(result));
+//   result = beta_binomial_log(n,N,inf,beta, errno_policy());
+//   EXPECT_TRUE(std::isnan(result));
   
-  EXPECT_THROW(beta_binomial_log(n,N,alpha,nan), std::domain_error);
-  EXPECT_THROW(beta_binomial_log(n,N,alpha,0.0), std::domain_error);
-  EXPECT_THROW(beta_binomial_log(n,N,alpha,-1.0), std::domain_error);
-  EXPECT_THROW(beta_binomial_log(n,N,alpha,-inf), std::domain_error);
-  EXPECT_THROW(beta_binomial_log(n,N,alpha,inf), std::domain_error);
-}
-TEST(ProbDistributionsBetaBinomial,ErrnoPolicy) {
-  double nan = std::numeric_limits<double>::quiet_NaN();
-  double inf = std::numeric_limits<double>::infinity();
-
-  double result;
-  int n = 5;
-  int N = 15;
-  double alpha = 3.0;
-  double beta = 4.5;
-
-  result = beta_binomial_log(n,N,alpha,beta, errno_policy());
-  EXPECT_FALSE(std::isnan(result));
-  result = beta_binomial_log(n,0,alpha,beta, errno_policy());
-  EXPECT_FALSE(std::isnan(result));
-  
-  result = beta_binomial_log(n,-1,alpha,beta, errno_policy());
-  EXPECT_TRUE(std::isnan(result));
-  
-  result = beta_binomial_log(n,N,nan,beta, errno_policy());
-  EXPECT_TRUE(std::isnan(result));
-  result = beta_binomial_log(n,N,0.0,beta, errno_policy());
-  EXPECT_TRUE(std::isnan(result));
-  result = beta_binomial_log(n,N,-1.0,beta, errno_policy());
-  EXPECT_TRUE(std::isnan(result));
-  result = beta_binomial_log(n,N,-inf,beta, errno_policy());
-  EXPECT_TRUE(std::isnan(result));
-  result = beta_binomial_log(n,N,inf,beta, errno_policy());
-  EXPECT_TRUE(std::isnan(result));
-  
-  result = beta_binomial_log(n,N,alpha,nan, errno_policy());
-  EXPECT_TRUE(std::isnan(result));
-  result = beta_binomial_log(n,N,alpha,0.0, errno_policy());
-  EXPECT_TRUE(std::isnan(result));
-  result = beta_binomial_log(n,N,alpha,-1.0, errno_policy());
-  EXPECT_TRUE(std::isnan(result));
-  result = beta_binomial_log(n,N,alpha,-inf, errno_policy());
-  EXPECT_TRUE(std::isnan(result));
-  result = beta_binomial_log(n,N,alpha,inf, errno_policy());
-  EXPECT_TRUE(std::isnan(result));
-}
+//   result = beta_binomial_log(n,N,alpha,nan, errno_policy());
+//   EXPECT_TRUE(std::isnan(result));
+//   result = beta_binomial_log(n,N,alpha,0.0, errno_policy());
+//   EXPECT_TRUE(std::isnan(result));
+//   result = beta_binomial_log(n,N,alpha,-1.0, errno_policy());
+//   EXPECT_TRUE(std::isnan(result));
+//   result = beta_binomial_log(n,N,alpha,-inf, errno_policy());
+//   EXPECT_TRUE(std::isnan(result));
+//   result = beta_binomial_log(n,N,alpha,inf, errno_policy());
+//   EXPECT_TRUE(std::isnan(result));
+// }
