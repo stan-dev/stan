5ac7bb7ac7a4c76d6464d005b7dd98a4ffce1183
Ben Goodrich <bg2382@columbia.edu>
2012-12-18 20:55:56 -0500

    provide psuedo-extension after sed -i

--- src/docs/stan-reference/diff.sh
+++ src/docs/stan-reference/diff.sh
@@ -38,15 +38,15 @@ for TEX_FILE in $TEX_FILES; do
 	sed ${IGNORE_COMMENT_LINES} | \
         sed ${DELETE_BRACEPLUS} | \
         sed ${DELETE_PLUSBRACE} | \
-        sed ${IGNORE_LINES_WITH_LEADING_SLASH} |
-        sed ${IGNORE_LINES_WITH_LEADING_GREATER_THAN} |
-        sed ${IGNORE_LINES_WITH_RIGHT_BRACKET} |
-        sed ${IGNORE_LINES_THAT_START_WITH_DATA} |
-        sed ${IGNORE_LINES_THAT_START_WITH_TRANSFORMED_DATA} |
-        sed ${IGNORE_LINES_THAT_START_WITH_PARAMETERS} |
-        sed ${IGNORE_LINES_THAT_START_WITH_TRANSFORMED_PARAMETERS} |
-        sed ${IGNORE_LINES_THAT_START_WITH_MODEL} |
-        sed ${IGNORE_LINES_THAT_START_WITH_GENERATED_QUANTITIES} |
+        sed ${IGNORE_LINES_WITH_LEADING_SLASH} | \
+        sed ${IGNORE_LINES_WITH_LEADING_GREATER_THAN} | \
+        sed ${IGNORE_LINES_WITH_RIGHT_BRACKET} | \
+        sed ${IGNORE_LINES_THAT_START_WITH_DATA} | \
+        sed ${IGNORE_LINES_THAT_START_WITH_TRANSFORMED_DATA} | \
+        sed ${IGNORE_LINES_THAT_START_WITH_PARAMETERS} | \
+        sed ${IGNORE_LINES_THAT_START_WITH_TRANSFORMED_PARAMETERS} | \
+        sed ${IGNORE_LINES_THAT_START_WITH_MODEL} | \
+        sed ${IGNORE_LINES_THAT_START_WITH_GENERATED_QUANTITIES} | \
         sed ${IGNORE_LINES_THAT_START_WITH_CODE_COMMENTS} > \
 	additions.sh
 	echo 'Processing additions to' $TEX_FILE
@@ -56,7 +56,7 @@ for TEX_FILE in $TEX_FILES; do
                 UNIQUE_LINES=`wc -l -- search.txt | sed 's/[^[0-9]//g'`
 		if  [ $UNIQUE_LINES -eq "1" ]; then
                         LINE_NUM=`sed 's/[^0-9]//g' search.txt`
-			sed -i "${LINE_NUM}s@^${line}@\\\\A{${line}} \\\\FXA \\\\ @" $TEX_FILE
+			sed -i '' "${LINE_NUM}s@^${line}@\\\\A{${line}} \\\\FXA \\\\ @" $TEX_FILE
 		else
 			echo "In ${TEX_FILE}, no unique match for:" "${line}"
 		fi
@@ -79,15 +79,15 @@ for TEX_FILE in $TEX_FILES; do
         sed ${IGNORE_COMMENT_LINES} | \
         sed ${DELETE_BRACEPLUS} | \
         sed ${DELETE_PLUSBRACE} | \
-        sed ${IGNORE_LINES_WITH_LEADING_SLASH} |
-        sed ${IGNORE_LINES_WITH_LEADING_GREATER_THAN} |
-        sed ${IGNORE_LINES_WITH_RIGHT_BRACKET} |
-        sed ${IGNORE_LINES_THAT_START_WITH_DATA} |
-        sed ${IGNORE_LINES_THAT_START_WITH_TRANSFORMED_DATA} |
-        sed ${IGNORE_LINES_THAT_START_WITH_PARAMETERS} |
-        sed ${IGNORE_LINES_THAT_START_WITH_TRANSFORMED_PARAMETERS} |
-        sed ${IGNORE_LINES_THAT_START_WITH_MODEL} |
-        sed ${IGNORE_LINES_THAT_START_WITH_GENERATED_QUANTITIES} |
+        sed ${IGNORE_LINES_WITH_LEADING_SLASH} | \
+        sed ${IGNORE_LINES_WITH_LEADING_GREATER_THAN} | \
+        sed ${IGNORE_LINES_WITH_RIGHT_BRACKET} | \
+        sed ${IGNORE_LINES_THAT_START_WITH_DATA} | \
+        sed ${IGNORE_LINES_THAT_START_WITH_TRANSFORMED_DATA} | \
+        sed ${IGNORE_LINES_THAT_START_WITH_PARAMETERS} | \
+        sed ${IGNORE_LINES_THAT_START_WITH_TRANSFORMED_PARAMETERS} | \
+        sed ${IGNORE_LINES_THAT_START_WITH_MODEL} | \
+        sed ${IGNORE_LINES_THAT_START_WITH_GENERATED_QUANTITIES} | \
         sed ${IGNORE_LINES_THAT_START_WITH_CODE_COMMENTS} > \
         changes.sh
         echo 'Processing changes to' $TEX_FILE
@@ -97,7 +97,7 @@ for TEX_FILE in $TEX_FILES; do
                 UNIQUE_LINES=`wc -l -- search.txt | sed 's/[^[0-9]//g'`
                 if  [ $UNIQUE_LINES -eq "1" ]; then
                         LINE_NUM=`sed 's/[^0-9]//g' search.txt`
-                        sed -i "${LINE_NUM}s@^${line}@\\\\A{${line}} \\\\FXA \\\\ @" $TEX_FILE
+                        sed -i '' "${LINE_NUM}s@^${line}@\\\\A{${line}} \\\\FXA \\\\ @" $TEX_FILE
                 else
                         echo "In ${TEX_FILE}, no unique match for:" "${line}"
                 fi
