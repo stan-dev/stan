964c32b2c787d2351dd22ddc8433935112e9a583
Michael Betancourt <betanalpha@gmail.com>
2012-12-23 13:04:59 -0500

    Poisson CDF

--- src/stan/prob/distributions/univariate/discrete/poisson.hpp
+++ src/stan/prob/distributions/univariate/discrete/poisson.hpp
@@ -231,7 +231,114 @@ namespace stan {
       return poisson_log_log<false>(n,alpha,stan::math::default_policy());
     }
 
+      // Poisson CDF
+      template <bool propto, typename T_n, typename T_rate, class Policy>
+      typename return_type<T_rate>::type
+      poisson_cdf(const T_n& n, const T_rate& lambda, const Policy&) {
+          
+          static const char* function = "stan::prob::poisson_cdf(%1%)";
+          
+          using stan::math::check_not_nan;
+          using stan::math::check_nonnegative;
+          using stan::math::value_of;
+          using stan::math::check_consistent_sizes;
+          using stan::prob::include_summand;
+          
+          // Ensure non-zero argument slengths
+          if (!(stan::length(n) && stan::length(lambda))) 
+              return 0.0;
+          
+          double P(1.0);
+          
+          // Validate arguments
+          if (!check_nonnegative(function, n, "Random variable", &P, Policy()))
+              return P;
+          
+          if (!check_not_nan(function, lambda, "Rate parameter", &P, Policy()))
+              return P;
+          
+          if (!check_nonnegative(function, lambda, "Rate parameter", &P, Policy()))
+              return P;
+          
+          if (!(check_consistent_sizes(function, n,lambda,
+                                       "Random variable","Rate parameter",
+                                       &P, Policy())))
+              return P;
+          
+          // Return if everything is constant and only proportionality is required
+          if (!include_summand<propto,T_rate>::value)
+              return 0.0;
+          
+          // Wrap arguments into vector views
+          VectorView<const T_n> n_vec(n);
+          VectorView<const T_rate> lambda_vec(lambda);
+          size_t size = max_size(n, lambda);
+          
+          // Validate vector views - redundant with the above validations?
+          for (size_t i = 0; i < size; i++)
+              if (std::isinf(lambda_vec[i])) return LOG_ZERO;
+          
+          for (size_t i = 0; i < size; i++)
+              if (lambda_vec[i] == 0 && n_vec[i] != 0)
+                  return LOG_ZERO;
+          
 
+          // Compute vectorized CDF and gradient
+          using stan::math::value_of;
+          using boost::math::gamma_p_derivative;
+          using boost::math::gamma_p;
+          
+          agrad::OperandsAndPartials<T_rate> operands_and_partials(lambda);
+          
+          std::fill(operands_and_partials.all_partials,
+                    operands_and_partials.all_partials + operands_and_partials.nvaris, 0.0);
+          
+          for (size_t i = 0; i < size; i++) 
+          {
+
+              const double n_dbl = value_of(n_vec[i]);
+              const double lambda_dbl = value_of(lambda_vec[i]);
+              
+              const double Pi = gamma_p(n_dbl, lambda_dbl);
+              
+              P *= Pi;
+              
+              if (!is_constant_struct<T_rate>::value)
+                  operands_and_partials.d_x1[i] += gamma_p_derivative(n_dbl, lambda_dbl) / Pi;
+              
+          }
+          
+          for (size_t i = 0; i < size; i++) {
+              
+              if (!is_constant_struct<T_rate>::value)
+                  operands_and_partials.d_x1[n] *= P;
+              
+          }
+          
+          return operands_and_partials.to_var(P);
+      }
+      
+      template <bool propto, typename T_n, typename T_rate>
+      inline typename return_type<T_rate>::type
+      poisson_cdf(const T_n& n, const T_rate& lambda) {
+          return poisson_cdf<propto>(n, lambda, stan::math::default_policy());
+      }
+      
+      
+      template <typename T_n, typename T_rate, class Policy>
+      inline typename return_type<T_rate>::type
+      poisson_cdf(const T_n& n, const T_rate& lambda, const Policy&) {
+          return poisson_cdf<false>(n, lambda, Policy());
+      }
+      
+      
+      template <typename T_n, typename T_rate>
+      inline typename return_type<T_rate>::type
+      poisson_cdf(const T_n& n, const T_rate& lambda) {
+          return poisson_cdf<false>(n, lambda, stan::math::default_policy());
+      }
+
+      
   }
 }
 #endif
