a5b3b7f244cd79c73b8b4d00a4081737c2f4ae30
Michael Betancourt <betanalpha@gmail.com>
2012-12-23 12:42:47 -0500

    Pareto CDF

--- src/stan/prob/distributions/univariate/continuous/pareto.hpp
+++ src/stan/prob/distributions/univariate/continuous/pareto.hpp
@@ -146,7 +146,113 @@ namespace stan {
       return pareto_log<false>(y,y_min,alpha,stan::math::default_policy());
     }
 
-
+      template <typename T_y, typename T_scale, typename T_shape, class Policy>
+      typename return_type<T_y, T_scale, T_shape>::type
+      pareto_cdf(const T_y& y, const T_scale& y_min, const T_shape& alpha, const Policy&) {
+          
+          // Check sizes
+          // Size checks
+          if ( !( stan::length(y) && stan::length(y_min) && stan::length(alpha) ) ) return 0.0;
+          
+          // Check errors
+          static const char* function = "stan::prob::pareto_cdf(%1%)";
+          
+          using stan::math::check_finite;
+          using stan::math::check_positive;
+          using stan::math::check_not_nan;
+          using stan::math::check_greater_or_equal;
+          using stan::math::check_consistent_sizes;
+          
+          using boost::math::tools::promote_args;
+          
+          double P(1.0);
+          
+          if (!check_not_nan(function, y, "Random variable", &P, Policy()))
+              return P;
+          
+          if (!check_greater_or_equal(function, y, y_min, "Random variable", &P, Policy()))
+              return P;        
+          
+          if (!check_finite(function, y_min, "Scale parameter", &P, Policy()))
+              return P;
+          
+          if (!check_positive(function, y_min, "Scale parameter", &P, Policy()))
+              return P;
+          
+          if (!check_finite(function, alpha, "Shape parameter", &P, Policy()))
+              return P;
+          
+          if (!check_positive(function, alpha, "Shape parameter", &P, Policy()))
+              return P;
+          
+          if (!(check_consistent_sizes(function, y, y_min, alpha,
+                                       "Random variable", "Scale parameter", "Shape parameter",
+                                       &P, Policy())))
+              return P;
+          
+          // Wrap arguments in vectors
+          VectorView<const T_y> y_vec(y);
+          VectorView<const T_scale> y_min_vec(y_min);
+          VectorView<const T_shape> alpha_vec(alpha);
+          size_t N = max_size(y, y_min, alpha);
+          
+          agrad::OperandsAndPartials<T_y, T_scale, T_shape> operands_and_partials(y, y_min, alpha);
+          
+          std::fill(operands_and_partials.all_partials,
+                    operands_and_partials.all_partials + operands_and_partials.nvaris, 0.0);
+          
+          // Compute vectorized CDF and its gradients
+          using stan::math::value_of;
+          
+          for (size_t n = 0; n < N; n++) {
+              
+              // Pull out values
+              const double log_dbl = log( value_of(y_min_vec[n]) / value_of(y_vec[n]) );
+              const double y_min_inv_dbl = 1.0 / value_of(y_min_vec[n]);
+              const double alpha_dbl = value_of(alpha_vec[n]);
+              
+              // Compute
+              const double Pn = 1.0 - exp( alpha_dbl * log_dbl );
+              
+              P *= Pn;
+              
+              if (!is_constant_struct<T_y>::value)
+                  operands_and_partials.d_x1[n] 
+                  += alpha_dbl * y_min_inv_dbl * exp( (alpha_dbl + 1) * log_dbl ) / Pn;
+              
+              if (!is_constant_struct<T_scale>::value)
+                  operands_and_partials.d_x2[n] 
+                  += - alpha_dbl * y_min_inv_dbl * exp( alpha_dbl * log_dbl ) / Pn;
+              
+              if (!is_constant_struct<T_shape>::value)
+                  operands_and_partials.d_x3[n] 
+                  += - exp( alpha_dbl * log_dbl ) * log_dbl / Pn;
+              
+          }
+          
+          for (size_t n = 0; n < N; n++) {
+              
+              if (!is_constant_struct<T_y>::value)
+                  operands_and_partials.d_x1[n] *= P;
+              
+              if (!is_constant_struct<T_scale>::value)
+                  operands_and_partials.d_x2[n] *= P;
+              
+              if (!is_constant_struct<T_shape>::value)
+                  operands_and_partials.d_x3[n] *= P;
+              
+          }
+          
+          return operands_and_partials.to_var(P);
+          
+      }
+      
+      template <typename T_y, typename T_scale, typename T_shape>
+      inline typename return_type<T_y, T_scale, T_shape>::type
+      pareto_cdf(const T_y& y, const T_scale& y_min, const T_shape& alpha) {
+          return pareto_cdf(y, y_min, alpha, stan::math::default_policy());
+      }
+      
   }
 }
 #endif
