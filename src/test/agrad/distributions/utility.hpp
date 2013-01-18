a4da15b4363d20a45cd603e3e95d7e5eff4ee326
Daniel Lee <bearlee@alum.mit.edu>
2013-01-18 16:24:59 -0500

    implemented CallAllVersions

---
+++ src/test/agrad/distributions/utility.hpp
@@ -0,0 +1,105 @@
+#ifndef __TEST__AGRAD__DISTRIBUTIONS__UTILITY_HPP__
+#define __TEST__AGRAD__DISTRIBUTIONS__UTILITY_HPP__
+
+#include <boost/math/policies/policy.hpp>
+#include <vector>
+
+using std::vector;
+using stan::agrad::var;
+
+
+//------------------------------------------------------------
+
+struct empty {};
+
+//------------------------------------------------------------
+
+typedef stan::math::default_policy default_policy;
+
+typedef boost::math::policies::policy<
+  boost::math::policies::domain_error<boost::math::policies::errno_on_error>
+  > errno_policy;
+
+//------------------------------------------------------------
+
+std::ostream& operator<<(std::ostream& os, const vector<double>& param) {
+  os << "(";
+  for (size_t n = 0; n < param.size(); n++) {
+    os << param[n];
+    if (n < param.size()-1)
+      os << ", ";
+  }
+  os << ")";
+  return os;
+}
+
+//------------------------------------------------------------
+// default template handles Eigen::Matrix
+template <typename T>
+T get_params(const vector<vector<double> >& parameters, const size_t p) {
+  T param(parameters.size());
+  for (size_t n = 0; n < parameters.size(); n++)
+    if (p < parameters[0].size())
+      param(n) = parameters[n][p];
+  return param;
+}
+
+// handle empty
+template <>
+empty get_params<empty>(const vector<vector<double> >& /*parameters*/, const size_t /*p*/) {
+  return empty();
+}
+// handle scalars
+template<>
+double get_params<double>(const vector<vector<double> >& parameters, const size_t p) {
+  double param(0);
+  if (p < parameters[0].size())
+    param = parameters[0][p];
+  return param;
+}
+template<>
+var get_params<var>(const vector<vector<double> >& parameters, const size_t p) {
+  var param(0);
+  if (p < parameters[0].size())
+    param = parameters[0][p];
+  return param;
+}
+template<>
+int get_params<int>(const vector<vector<double> >& parameters, const size_t p) {
+  int param(0);
+  if (p < parameters[0].size())
+    param = (int)parameters[0][p];
+  return param;
+}
+// handle vectors
+template <>
+vector<int> get_params<vector<int> >(const vector<vector<double> >& parameters, const size_t p) {
+  vector<int> param(parameters.size());
+  for (size_t n = 0; n < parameters.size(); n++)
+    if (p < parameters[0].size())
+      param[n] = parameters[n][p];
+  return param;
+}
+template <>
+vector<double> get_params<vector<double> >(const vector<vector<double> >& parameters, const size_t p) {
+  vector<double> param(parameters.size());
+  for (size_t n = 0; n < parameters.size(); n++)
+    if (p < parameters[0].size())
+      param[n] = parameters[n][p];
+  return param;
+}
+template <>
+vector<var> get_params<vector<var> >(const vector<vector<double> >& parameters, const size_t p) {
+  vector<var> param(parameters.size());
+  for (size_t n = 0; n < parameters.size(); n++)
+    if (p < parameters[0].size())
+      param[n] = parameters[n][p];
+  return param;
+}
+
+
+//------------------------------------------------------------
+
+
+#endif
+
