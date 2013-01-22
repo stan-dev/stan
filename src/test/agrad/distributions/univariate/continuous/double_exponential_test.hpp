36313d67fe4a4625cee9d42169552b2cabfa5170
Daniel Lee <bearlee@alum.mit.edu>
2013-01-22 10:44:48 -0500

    refactored double_exponential distribution test

--- src/test/agrad/distributions/univariate/continuous/double_exponential_test.hpp
+++ src/test/agrad/distributions/univariate/continuous/double_exponential_test.hpp
@@ -1,8 +1,5 @@
-#define _LOG_PROB_ double_exponential_log
+// Arguments: Doubles, Doubles, Doubles
 #include <stan/prob/distributions/univariate/continuous/double_exponential.hpp>
-
-#include <test/agrad/distributions/distribution_test_fixture.hpp>
-#include <test/agrad/distributions/distribution_tests_3_params.hpp>
 
 using std::vector;
 using std::numeric_limits;
@@ -10,38 +7,45 @@ using stan::agrad::var;
 
 class AgradDistributionsDoubleExponential : public AgradDistributionTest {
 public:
-  void valid_values(vector<vector<double> >& parameters) {
+  void valid_values(vector<vector<double> >& parameters,
+                    vector<double>& log_prob) {
     vector<double> param(3);
-	       
+    
     param[0] = 1.0;                  // y
     param[1] = 1.0;                  // mu
     param[2] = 1.0;                  // sigma
     parameters.push_back(param);
+    log_prob.push_back(-0.6931472);  // expected log_prob
 
     param[0] = 2.0;                  // y
     param[1] = 1.0;                  // mu
     param[2] = 1.0;                  // sigma
     parameters.push_back(param);
+    log_prob.push_back(-1.693147);   // expected log_prob
     
     param[0] = -3.0;                 // y
     param[1] = 2.0;                  // mu
     param[2] = 1.0;                  // sigma
     parameters.push_back(param);
+    log_prob.push_back(-5.693147);   // expected log_prob
     
     param[0] = 1.0;                  // y
     param[1] = 0.0;                  // mu
     param[2] = 2.0;                  // sigma
     parameters.push_back(param);
+    log_prob.push_back(-1.886294);   // expected log_prob
 
     param[0] = 1.9;                  // y
     param[1] = 2.3;                  // mu
     param[2] = 0.5;                  // sigma
     parameters.push_back(param);
+    log_prob.push_back(-0.8);        // expected log_prob
 
     param[0] = 1.9;                  // y
     param[1] = 2.3;                  // mu
     param[2] = 0.25;                  // sigma
     parameters.push_back(param);
+    log_prob.push_back(-0.9068528);        // expected log_prob
   }
  
   void invalid_values(vector<size_t>& index, 
@@ -74,12 +78,45 @@ public:
     value.push_back(-numeric_limits<double>::infinity());
   }
 
-  template <class T_y, class T_loc, class T_scale,
+  template <typename T_y, typename T_loc, typename T_scale,
 	    typename T3, typename T4, typename T5, 
-	    typename T6, typename T7, typename T8, typename T9>
-  var log_prob(const T_y& y, const T_loc& mu, const T_scale& sigma,
-	       const T3&, const T4&, const T5&, 
-	       const T6&, const T7&, const T8&, const T9&) {
+	    typename T6, typename T7, typename T8, 
+	    typename T9>
+  typename stan::return_type<T_y, T_loc, T_scale>::type 
+  log_prob(const T_y& y, const T_loc& mu, const T_scale& sigma,
+	   const T3&, const T4&, const T5&, const T6&, const T7&, const T8&, const T9&) {
+    return stan::prob::double_exponential_log(y, mu, sigma);
+  }
+
+  template <bool propto, 
+	    typename T_y, typename T_loc, typename T_scale,
+	    typename T3, typename T4, typename T5, 
+	    typename T6, typename T7, typename T8, 
+	    typename T9>
+  typename stan::return_type<T_y, T_loc, T_scale>::type 
+  log_prob(const T_y& y, const T_loc& mu, const T_scale& sigma,
+	   const T3&, const T4&, const T5&, const T6&, const T7&, const T8&, const T9&) {
+    return stan::prob::double_exponential_log<propto>(y, mu, sigma);
+  }
+  
+  template <bool propto, 
+	    typename T_y, typename T_loc, typename T_scale,
+	    typename T3, typename T4, typename T5, 
+	    typename T6, typename T7, typename T8, 
+	    typename T9, 
+	    class Policy>
+  typename stan::return_type<T_y, T_loc, T_scale>::type 
+  log_prob(const T_y& y, const T_loc& mu, const T_scale& sigma,
+	   const T3&, const T4&, const T5&, const T6&, const T7&, const T8&, const T9&) {
+    return stan::prob::double_exponential_log<propto>(y, mu, sigma, Policy());
+  }
+  
+  template <typename T_y, typename T_loc, typename T_scale,
+	    typename T3, typename T4, typename T5, 
+	    typename T6, typename T7, typename T8, 
+	    typename T9>
+  var log_prob_function(const T_y& y, const T_loc& mu, const T_scale& sigma,
+			const T3&, const T4&, const T5&, const T6&, const T7&, const T8&, const T9&) {
     using std::log;
     using stan::math::log;
     using std::fabs;
@@ -96,12 +133,4 @@ public:
       logp -= fabs(y - mu) / sigma;
     return logp;
   }
-
 };
-
-INSTANTIATE_TYPED_TEST_CASE_P(AgradDistributionsDoubleExponential,
-			      AgradDistributionTestFixture,
-			      AgradDistributionsDoubleExponential);
-INSTANTIATE_TYPED_TEST_CASE_P(AgradDistributionsDoubleExponential,
-			      AgradDistributionTestFixture2,
-			      AgradDistributionsDoubleExponential);
