646cafa2e60c11fde3878e5143f849f54eca84c3
Daniel Lee <bearlee@alum.mit.edu>
2013-01-22 23:31:55 -0500

    moved cauchy cdf test

--- src/test/agrad/distributions/univariate/continuous/cauchy_test.hpp
+++ src/test/agrad/distributions/univariate/continuous/cauchy_test.hpp
@@ -108,3 +108,11 @@ public:
     return lp;
   }
 };
+
+
+TEST(ProbDistributionsCauchy,Cumulative) {
+  using stan::prob::cauchy_cdf;
+  EXPECT_FLOAT_EQ(0.75, cauchy_cdf(1.0, 0.0, 1.0));
+  EXPECT_FLOAT_EQ(0.187167, cauchy_cdf(-1.5, 0.0, 1.0));
+  EXPECT_FLOAT_EQ(0.187167, cauchy_cdf(-2.5, -1.0, 1.0));
+}
