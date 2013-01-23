8190e64bfcff24b44df951d83e1133bf93a15546
Daniel Lee <bearlee@alum.mit.edu>
2013-01-22 23:35:03 -0500

    moving exponential cdf test

--- src/test/agrad/distributions/univariate/continuous/exponential_test.hpp
+++ src/test/agrad/distributions/univariate/continuous/exponential_test.hpp
@@ -96,3 +96,21 @@ public:
     return logp;
   }
 };
+
+TEST(ProbDistributionsExponential,Cumulative) {
+  using std::numeric_limits;
+  using stan::prob::exponential_cdf;
+  EXPECT_FLOAT_EQ(0.95021293, exponential_cdf(2.0,1.5));
+  EXPECT_FLOAT_EQ(1.0, exponential_cdf(15.0,3.9));
+  EXPECT_FLOAT_EQ(0.62280765, exponential_cdf(0.25,3.9));
+
+  // ??
+  // EXPECT_FLOAT_EQ(0.0,
+  //                 exponential_cdf(-numeric_limits<double>::infinity(),
+  //                                 1.5));
+  EXPECT_FLOAT_EQ(0.0, exponential_cdf(0.0,1.5));
+  EXPECT_FLOAT_EQ(1.0,
+                  exponential_cdf(numeric_limits<double>::infinity(),
+                                  1.5));
+
+}
