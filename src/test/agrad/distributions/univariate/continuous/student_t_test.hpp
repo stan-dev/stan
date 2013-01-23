eb6eca2d9fc7ac90709e65637917a8a2098defd6
Daniel Lee <bearlee@alum.mit.edu>
2013-01-23 00:10:22 -0500

    refactoring student_t test

--- src/test/agrad/distributions/univariate/continuous/student_t_test.hpp
+++ src/test/agrad/distributions/univariate/continuous/student_t_test.hpp
@@ -1,8 +1,5 @@
-#define _LOG_PROB_ student_t_log
+// Arguments: Double, Double, Double, Double
 #include <stan/prob/distributions/univariate/continuous/student_t.hpp>
-
-#include <test/agrad/distributions/distribution_test_fixture.hpp>
-#include <test/agrad/distributions/distribution_tests_4_params.hpp>
 
 using std::vector;
 using std::numeric_limits;
@@ -10,7 +7,8 @@ using stan::agrad::var;
 
 class AgradDistributionsStudentT : public AgradDistributionTest {
 public:
-  void valid_values(vector<vector<double> >& parameters) {
+  void valid_values(vector<vector<double> >& parameters,
+                    vector<double>& log_prob) {
     vector<double> param(4);
 
     param[0] = 1.0;           // y
@@ -18,24 +16,27 @@ public:
     param[2] = 0.0;           // mu
     param[3] = 1.0;           // sigma
     parameters.push_back(param);
+    log_prob.push_back(-1.837877); // expected log_prob
 
     param[0] = -3.0;          // y
     param[1] = 2.0;           // nu
     param[2] = 0.0;           // mu
     param[3] = 1.0;           // sigma
     parameters.push_back(param);
+    log_prob.push_back(-3.596843); // expected log_prob
 
     param[0] = 2.0;           // y
     param[1] = 1.0;           // nu
     param[2] = 0.0;           // mu
     param[3] = 2.0;           // sigma
     parameters.push_back(param);
+    log_prob.push_back(-2.531024); // expected log_prob
   }
  
-  void invalid_values(vector<size_t>& index, 
-		      vector<double>& value) {
+  void invalid_values(vector<size_t>& index,
+                      vector<double>& value) {
     // y
-    
+   
     // nu
     index.push_back(1U);
     value.push_back(0.0);
@@ -73,9 +74,38 @@ public:
   template <class T_y, class T_dof, class T_loc, class T_scale,
 	    typename T4, typename T5, typename T6, 
 	    typename T7, typename T8, typename T9>
-  var log_prob(const T_y& y, const T_dof& nu, const T_loc& mu, const T_scale& sigma,
-	       const T4&, const T5&, const T6&, 
-	       const T7&, const T8&, const T9&) {
+  typename stan::return_type<T_y, T_dof, T_loc, T_scale>::type 
+  log_prob(const T_y& y, const T_dof& nu, const T_loc& mu, const T_scale& sigma,
+	   const T4&, const T5&, const T6&, const T7&, const T8&, const T9&) {
+    return stan::prob::student_t_log(y, nu, mu, sigma);
+  }
+  
+  template <bool propto, 
+	    class T_y, class T_dof, class T_loc, class T_scale,
+	    typename T4, typename T5, typename T6, 
+	    typename T7, typename T8, typename T9>
+  typename stan::return_type<T_y, T_dof, T_loc, T_scale>::type 
+  log_prob(const T_y& y, const T_dof& nu, const T_loc& mu, const T_scale& sigma,
+	   const T4&, const T5&, const T6&, const T7&, const T8&, const T9&) {
+    return stan::prob::student_t_log<propto>(y, nu, mu, sigma);
+  }
+  
+  template <bool propto, 
+	    class T_y, class T_dof, class T_loc, class T_scale,
+	    typename T4, typename T5, typename T6, 
+	    typename T7, typename T8, typename T9,
+	    class Policy>
+  typename stan::return_type<T_y, T_dof, T_loc, T_scale>::type 
+  log_prob(const T_y& y, const T_dof& nu, const T_loc& mu, const T_scale& sigma,
+	   const T4&, const T5&, const T6&, const T7&, const T8&, const T9&) {
+    return stan::prob::student_t_log<propto>(y, nu, mu, sigma, Policy());
+  }
+  
+  template <class T_y, class T_dof, class T_loc, class T_scale,
+	    typename T4, typename T5, typename T6, 
+	    typename T7, typename T8, typename T9>
+  var log_prob_function(const T_y& y, const T_dof& nu, const T_loc& mu, const T_scale& sigma,
+			const T4&, const T5&, const T6&, const T7&, const T8&, const T9&) {
     using std::log;
     using stan::math::square;
     using stan::math::log1p;
@@ -97,21 +127,9 @@ public:
 	* log1p( square(((y - mu) / sigma)) / nu);
     return logp;
   }
-
 };
 
-INSTANTIATE_TYPED_TEST_CASE_P(AgradDistributionsStudentT,
-			      AgradDistributionTestFixture,
-			      AgradDistributionsStudentT);
-INSTANTIATE_TYPED_TEST_CASE_P(AgradDistributionsStudentT,
-			      AgradDistributionTestFixture2,
-			      AgradDistributionsStudentT);
-INSTANTIATE_TYPED_TEST_CASE_P(AgradDistributionsStudentT,
-			      AgradDistributionTestFixture3,
-			      AgradDistributionsStudentT);
-INSTANTIATE_TYPED_TEST_CASE_P(AgradDistributionsStudentT,
-			      AgradDistributionTestFixture4,
-			      AgradDistributionsStudentT);
-INSTANTIATE_TYPED_TEST_CASE_P(AgradDistributionsStudentT,
-			      AgradDistributionTestFixture5,
-			      AgradDistributionsStudentT);
+TEST(ProbDistributionsStudentTCDF,Values) {
+    EXPECT_FLOAT_EQ(0.75, stan::prob::student_t_cdf(1.0,1.0,0.0,1.0));
+    EXPECT_FLOAT_EQ(0.047732983, stan::prob::student_t_cdf(-3.0,2.0,0.0,1.0));
+}
