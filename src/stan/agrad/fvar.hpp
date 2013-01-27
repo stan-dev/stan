01a10ea467387e430fa60d03d849d1de12c2ac5b
Peter Li <li.peter94@gmail.com>
2013-01-26 21:39:10 -0500

    added erf and erfc with tests in fvar

--- src/stan/agrad/fvar.hpp
+++ src/stan/agrad/fvar.hpp
@@ -385,39 +385,6 @@ namespace stan {
     }
 
 //trig functions
-    template <typename T1, typename T2>
-    inline
-    fvar<typename stan::return_type<T1,T2>::typ>
-    hypot(const fvar<T1>& x1, const fvar<T2>& x2) {
-      using boost::math::hypot;
-      using std::sqrt;
-    return fvar<typename 
-		stan::return_type<T1,T2>::type>(hypot(x1.val_, x2.val_), 
-                    (x1.d_ * x1.val_ + x2.d_ * x2.val_) / hypot(x1.val_, x2.val_));
-    }
-
-    template <typename T1, typename T2>
-    inline
-    fvar<typename stan::return_type<T1,T2>::typ>
-    hypot(const fvar<T1>& x1, const T2& x2) {
-      using boost::math::hypot;
-      using std::sqrt;
-    return fvar<typename 
-		stan::return_type<T1,T2>::type>(hypot(x1.val_, x2), 
-                       (x1.d_ * x1.val_) / hypot(x1.val_, x2));
-    }
-
-    template <typename T1, typename T2>
-    inline
-    fvar<typename stan::return_type<T1,T2>::typ>
-    hypot(const T1& x1, const fvar<T2>& x2) {
-      using boost::math::hypot;
-      using std::sqrt;
-    return fvar<typename 
-		stan::return_type<T1,T2>::type>(hypot(x1, x2.val_), 
-                       (x2.d_ * x2.val_) / hypot(x1, x2.val_));
-    }
-
     template <typename T>
     inline
     fvar<T>
--- src/test/agrad/fvar_test.cpp
+++ src/test/agrad/fvar_test.cpp
@@ -687,33 +687,6 @@ TEST(AgradFvar, pow) {
   EXPECT_FLOAT_EQ(log(5.0) * pow(5.0, 0.5), b.d_);
 }
 
-TEST(AgradFvar, hypot) {
-  using stan::agrad::fvar;
-  using boost::math::hypot;
-  using std::isnan;
-
-  fvar<double> x(0.5);
-  fvar<double> y(2.3);
-  x.d_ = 1.0;
-  y.d_ = 2.0;
-
-  fvar<double> a = hypot(x, y);
-  EXPECT_FLOAT_EQ(hypot(0.5, 2.3), a.val_);
-  EXPECT_FLOAT_EQ((0.5 * 2.0 + 2.3 * 1.0) / hypot(0.5, 2.3), a.d_);
-
-  fvar<double> z(0.0);
-  fvar<double> w(-2.3);
-  z.d_ = 1.0;
-  w.d_ = 2.0;
-  fvar<double> b = hypot(x, z);
-  EXPECT_FLOAT_EQ(0.5, b.val_);
-  EXPECT_FLOAT_EQ(1.0, b.d_);
-
-  fvar<double> c = hypot(x, w);
-  isnan(c.val_);
-  isnan(c.d_);
-}
-
 TEST(AgradFvar, sin) {
   using stan::agrad::fvar;
   using std::sin;
