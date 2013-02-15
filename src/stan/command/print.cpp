c70ab16da21a250d73152b4b7a453cc81847ad1f
Daniel Lee <bearlee@alum.mit.edu>
2013-02-15 17:23:27 -0500

    adding placeholder print function; updated make/command to build print

---
+++ src/stan/command/print.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+/** 
+ * The Stan print function.
+ *
+ * @param argc Number of arguments
+ * @param argv Arguments
+ * 
+ * @return 0 for success, 
+ *         non-zero otherwise
+ */
+int main(int argc, const char* argv[]) {
+  std::cout << "--------------------" << std::endl;
+  std::cout << "inside print" << std::endl;
+  std::cout << "--------------------" << std::endl;
+
+  return -1;
+}
+
+
+
