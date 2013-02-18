3426c0b67bf20d3a241b343902491dd0eeb5662b
Daniel Lee <bearlee@alum.mit.edu>
2013-02-18 18:07:55 -0500

    added test for metadata

--- src/stan/io/stan_csv_reader.hpp
+++ src/stan/io/stan_csv_reader.hpp
@@ -7,13 +7,38 @@
 namespace stan {
   namespace io {
 
+    // FIXME: should consolidate with the options from the command line in stan::gm
+    struct stan_csv_metadata {
+      int stan_version_major;
+      int stan_version_minor;
+      int stan_version_patch;
+      
+      std::string data;
+      std::string init;
+      bool append_samples;
+      bool save_warmup;
+      size_t seed;
+      bool random_seed;
+      size_t chain_id;
+      size_t iter;
+      size_t warmup;
+      size_t thin;
+      bool equal_step_sizes;
+      int leapfrog_steps;
+      int max_treedepth;
+      double epsilon;
+      double epsilon_pm;
+      double delta;
+      double gamma;
+    };
+
     /**
      * Reads from a Stan output csv file.
      */
     class stan_csv_reader {
     private:
       std::istream& in_;
-      
+      stan_csv_metadata metadata_;
 
     public:
       /** 
@@ -37,7 +62,9 @@ namespace stan {
        */
       ~stan_csv_reader() { }
 
-      void read_metadata() { }
+      void read_metadata() {
+	
+      }
       void read_header() { }
       void read_adaptation() { }
       void read_samples() { }
@@ -47,12 +74,15 @@ namespace stan {
        * 
        */
       void parse() {
-	// read_metadata
-	// read_header
-	// read_adaptation
-	// read_samples
+	// read_metadata()
+	// read_header()
+	// read_adaptation()
+	// read_samples()
       }
       
+      stan_csv_metadata metadata() {
+	return metadata_;
+      }
       
     };
     
--- src/test/io/stan_csv_reader_test.cpp
+++ src/test/io/stan_csv_reader_test.cpp
@@ -16,15 +16,42 @@ public:
 
   void TearDown() {
     blocker0.close();
+    metadata.close();
+    header.close();
+    adaptation.close();
+    samples.close();
   }
 
   std::ifstream blocker0;
   std::ifstream metadata, header, adaptation, samples;
 };
 
-TEST_F(StanIoStanCsvReader,DISABLED_read_metadata) {
+TEST_F(StanIoStanCsvReader,read_metadata) {
   stan::io::stan_csv_reader reader(metadata);
   reader.read_metadata();
+  
+  stan::io::stan_csv_metadata metadata = reader.metadata();
+  EXPECT_EQ(1U, metadata.stan_version_major);
+  EXPECT_EQ(1U, metadata.stan_version_minor);
+  EXPECT_EQ(1U, metadata.stan_version_patch);
+  
+  EXPECT_EQ("models\\bugs_examples\\vol1\\blocker\\blocker.data.R", metadata.data);
+  EXPECT_EQ("models\\bugs_examples\\vol1\\blocker\\blocker.init.R", metadata.init);
+  EXPECT_EQ(false, metadata.append_samples);
+  EXPECT_EQ(false, metadata.save_warmup);
+  EXPECT_EQ(4085885484U, metadata.seed);
+  EXPECT_EQ(false, metadata.random_seed);
+  EXPECT_EQ(0U, metadata.chain_id);
+  EXPECT_EQ(4000U, metadata.iter);
+  EXPECT_EQ(2000U, metadata.warmup);
+  EXPECT_EQ(2, metadata.thin);
+  EXPECT_EQ(false, metadata.equal_step_sizes);
+  EXPECT_EQ(-1, metadata.leapfrog_steps);
+  EXPECT_EQ(10, metadata.max_treedepth);
+  EXPECT_FLOAT_EQ(-1, metadata.epsilon);
+  EXPECT_FLOAT_EQ(0, metadata.epsilon_pm);
+  EXPECT_FLOAT_EQ(0.5, metadata.delta);
+  EXPECT_FLOAT_EQ(0.05, metadata.gamma);
 }
 TEST_F(StanIoStanCsvReader,DISABLED_read_header) { 
   stan::io::stan_csv_reader reader(header);
