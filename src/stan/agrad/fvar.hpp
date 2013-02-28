7e69d70e66b7b9db1d2de6301caca69abc40ad5c
Peter Li <li.peter94@gmail.com>
2013-02-27 20:09:15 -0500

    fixed numeric_limits for fvar and added tests

--- src/stan/agrad/fvar.hpp
+++ src/stan/agrad/fvar.hpp
@@ -1389,16 +1389,16 @@ namespace std {
 template <typename T>
 struct numeric_limits<stan::agrad::fvar<T> > {
     static const bool is_specialized = true;
-    static stan::agrad::var min() { return numeric_limits<T>::min(); }
-    static stan::agrad::var max() { return numeric_limits<T>::max(); }
+    static stan::agrad::fvar<T> min() { return numeric_limits<T>::min(); }
+    static stan::agrad::fvar<T> max() { return numeric_limits<T>::max(); }
     static const int digits = numeric_limits<T>::digits;
     static const int digits10 = numeric_limits<T>::digits10;
     static const bool is_signed = numeric_limits<T>::is_signed;
     static const bool is_integer = numeric_limits<T>::is_integer;
     static const bool is_exact = numeric_limits<T>::is_exact;
     static const int radix = numeric_limits<T>::radix;
-    static stan::agrad::var epsilon() { return numeric_limits<T>::epsilon(); }
-    static stan::agrad::var round_error() { return numeric_limits<T>::round_error(); }
+    static stan::agrad::fvar<T> epsilon() { return numeric_limits<T>::epsilon(); }
+    static stan::agrad::fvar<T> round_error() { return numeric_limits<T>::round_error(); }
 
     static const int  min_exponent = numeric_limits<T>::min_exponent;
     static const int  min_exponent10 = numeric_limits<T>::min_exponent10;
@@ -1410,10 +1410,10 @@ struct numeric_limits<stan::agrad::fvar<T> > {
     static const bool has_signaling_NaN = numeric_limits<T>::has_signaling_NaN;
     static const float_denorm_style has_denorm = numeric_limits<T>::has_denorm;
     static const bool has_denorm_loss = numeric_limits<T>::has_denorm_loss;
-    static stan::agrad::var infinity() { return numeric_limits<T>::infinity(); }
-    static stan::agrad::var quiet_NaN() { return numeric_limits<T>::quiet_NaN(); }
-    static stan::agrad::var signaling_NaN() { return numeric_limits<T>::signaling_NaN(); }
-    static stan::agrad::var denorm_min() { return numeric_limits<T>::denorm_min(); }
+    static stan::agrad::fvar<T> infinity() { return numeric_limits<T>::infinity(); }
+    static stan::agrad::fvar<T> quiet_NaN() { return numeric_limits<T>::quiet_NaN(); } 
+    static stan::agrad::fvar<T> signaling_NaN() { return numeric_limits<T>::signaling_NaN(); }
+    static stan::agrad::fvar<T> denorm_min() { return numeric_limits<T>::denorm_min(); }
 
     static const bool is_iec559 = numeric_limits<T>::is_iec559;
     static const bool is_bounded = numeric_limits<T>::is_bounded;
--- src/test/agrad/fvar_test.cpp
+++ src/test/agrad/fvar_test.cpp
@@ -2087,6 +2087,8 @@ TEST(AgradFvar, eq) {
 
 TEST(AgradFvar, numeric_limits) {
   using stan::agrad::fvar;
+  using std::isnan;
+  using stan::math::INFTY;
 
   EXPECT_TRUE(std::numeric_limits<stan::agrad::fvar<double> >::is_specialized);
   EXPECT_TRUE(std::numeric_limits<stan::agrad::fvar<int> >::is_specialized);
@@ -2094,9 +2096,15 @@ TEST(AgradFvar, numeric_limits) {
   EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<double> >::digits, 53);
   EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<int> >::digits, 31);
 
+  fvar<int> a = std::numeric_limits<stan::agrad::fvar<int> >::min();
+  fvar<double> b = std::numeric_limits<stan::agrad::fvar<double> >::min();
+  EXPECT_FLOAT_EQ(a.val_, -2147483648);
+  EXPECT_FLOAT_EQ(b.val_, 2.22507e-308);
 
-
-
+  fvar<int> c = std::numeric_limits<stan::agrad::fvar<int> >::max();
+  fvar<double> d = std::numeric_limits<stan::agrad::fvar<double> >::max();
+  EXPECT_FLOAT_EQ(c.val_, 2147483647);
+  EXPECT_FLOAT_EQ(d.val_, 1.79769e+308);
 
   EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<double> >::digits10, 15);
   EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<int> >::digits10, 9);
@@ -2113,9 +2121,15 @@ TEST(AgradFvar, numeric_limits) {
   EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<double> >::radix, 2);
   EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<int> >::radix, 2);
 
+  fvar<int> e = std::numeric_limits<stan::agrad::fvar<int> >::epsilon();
+  fvar<double> f = std::numeric_limits<stan::agrad::fvar<double> >::epsilon();
+  EXPECT_FLOAT_EQ(e.val_, 0);
+  EXPECT_FLOAT_EQ(f.val_, 2.220446e-16);
 
-
-
+  fvar<int> g = std::numeric_limits<stan::agrad::fvar<int> >::round_error();
+  fvar<double> h = std::numeric_limits<stan::agrad::fvar<double> >::round_error();
+  EXPECT_FLOAT_EQ(g.val_, 0);
+  EXPECT_FLOAT_EQ(h.val_,0.5);
 
   EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<double> >::min_exponent, -1021);
   EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<int> >::min_exponent, 0);
@@ -2144,14 +2158,25 @@ TEST(AgradFvar, numeric_limits) {
   EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<double> >::has_denorm_loss, 0);
   EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<int> >::has_denorm_loss, 0);
 
+  fvar<int> i = std::numeric_limits<stan::agrad::fvar<int> >::infinity();
+  fvar<double> j = std::numeric_limits<stan::agrad::fvar<double> >::infinity();
+  EXPECT_FLOAT_EQ(i.val_, 0);
+  EXPECT_FLOAT_EQ(j.val_, INFTY);
 
+  fvar<int> k = std::numeric_limits<stan::agrad::fvar<int> >::quiet_NaN();
+  fvar<double> l = std::numeric_limits<stan::agrad::fvar<double> >::quiet_NaN();
+  EXPECT_FLOAT_EQ(k.val_, 0);
+  isnan(l.val_);
 
+  fvar<int> m = std::numeric_limits<stan::agrad::fvar<int> >::signaling_NaN();
+  fvar<double> n = std::numeric_limits<stan::agrad::fvar<double> >::signaling_NaN();
+  EXPECT_FLOAT_EQ(m.val_, 0);
+  isnan(n.val_);
 
-
-
-
-
-
+  fvar<int> o = std::numeric_limits<stan::agrad::fvar<int> >::denorm_min();
+  fvar<double> p = std::numeric_limits<stan::agrad::fvar<double> >::denorm_min();
+  EXPECT_FLOAT_EQ(o.val_, 0);
+  EXPECT_FLOAT_EQ(p.val_, 4.94066e-324);
 
   EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<double> >::is_iec559, 1);
   EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<int> >::is_iec559, 0);
