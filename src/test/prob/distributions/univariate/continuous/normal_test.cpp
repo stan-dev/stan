d10a91beec6c47d6f57337429d8079c864fbead9
Peter Li <li.peter94@gmail.com>
2013-03-01 20:38:10 -0500

    added chi square goodness of fit test to normal

--- src/test/prob/distributions/univariate/continuous/normal_test.cpp
+++ src/test/prob/distributions/univariate/continuous/normal_test.cpp
@@ -1,8 +1,48 @@
 #include <stan/prob/distributions/univariate/continuous/normal.hpp>
+#include<boost/math/distributions.hpp>
 #include <gtest/gtest.h>
 #include <boost/random/mersenne_twister.hpp>
+#include <iostream>
 
 TEST(ProbDistributionsNormal, random) {
   boost::random::mt19937 rng;
-  EXPECT_NO_THROW(stan::prob::normal_rng(2.0,1.0,rng));
+  EXPECT_NO_THROW(stan::prob::normal_rng(10.0,2.0,rng));
+}
+
+TEST(ProbDistributionsNormal, isnormal) {
+  boost::random::mt19937 rng;
+
+  boost::math::normal_distribution<>dist (10.0, 2.0);
+
+  double loc[4];
+  for(int i = 1; i < 5; i++)
+    loc[i - 1] = quantile(dist, 0.2 * i);
+
+  int count = 0;
+  double a = 0;
+  int bin [5] = {0, 0, 0, 0, 0};
+
+  while(count < 10000)
+    {
+      a = stan::prob::normal_rng(10.0,2.0,rng);
+      if(a > loc[3])
+	bin[4]++;
+      else if(a < loc[3] && a > loc[2])
+	bin[3]++;
+      else if(a < loc[2] && a > loc[1])
+	bin[2]++;
+      else if(a < loc[1] && a > loc[0])
+	bin[1]++;
+      else
+	bin[0]++;
+      count++;
+    }
+
+  double chi = 0;
+  double expect [5] = {2000, 2000, 2000, 2000, 2000};
+
+  for(int j = 0; j < 5; j++)
+    chi += ((bin[j] - expect[j]) * (bin[j] - expect[j]) / expect[j]);
+
+  EXPECT_TRUE(chi < 9.49);
 }
