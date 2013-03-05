1d9e7ffef5ec499a014e56555928eca0f3c1f74b
Bob Carpenter <carp@alias-i.com>
2013-03-05 15:41:13 -0500

    moved error checking to doubles for simplex in categorical, still not maximally efficient because of alloc of new vector

--- src/stan/prob/distributions/multivariate/discrete/categorical.hpp
+++ src/stan/prob/distributions/multivariate/discrete/categorical.hpp
@@ -16,7 +16,7 @@ namespace stan {
               typename T_prob, 
               class Policy>
     typename boost::math::tools::promote_args<T_prob>::type
-    categorical_log(const typename Eigen::Matrix<T_prob,Eigen::Dynamic,1>::size_type n, 
+    categorical_log(int n, 
                     const Eigen::Matrix<T_prob,Eigen::Dynamic,1>& theta, 
                     const Policy&) {
       static const char* function = "stan::prob::categorical_log(%1%)";
@@ -24,19 +24,31 @@ namespace stan {
       using stan::math::check_bounded;
       using stan::math::check_simplex;
       using boost::math::tools::promote_args;
+      using stan::math::value_of;
 
       typename Eigen::Matrix<T_prob,Eigen::Dynamic,1>::size_type lb = 1;
 
-      typename promote_args<T_prob>::type lp(0.0);
+      double lp = 0.0;
       if (!check_bounded(function, n, lb, theta.size(),
                          "Number of categories",
                          &lp, Policy()))
         return lp;
-      if (!check_simplex(function, theta,
-                         "Probabilities parameter",
-                         &lp, Policy()))
-        return lp;
-  
+      
+      if (!stan::is_constant_struct<T_prob>::value) {
+        Eigen::Matrix<double,Eigen::Dynamic,1> theta_dbl(theta.size());
+        for (int i = 0; i < theta_dbl.size(); ++i)
+          theta_dbl(i) = value_of(theta(i));
+        if (!check_simplex(function, theta,
+                           "Probabilities parameter",
+                           &lp, Policy()))
+          return lp;
+      } else {
+        if (!check_simplex(function, theta,
+                           "Probabilities parameter",
+                           &lp, Policy()))
+          return lp;
+      }
+
       if (include_summand<propto,T_prob>::value)
         return log(theta(n-1));
       return 0.0;
