0cd6ddea722aceb23faacff42dc9bb31642d7dd4
Daniel Lee <bearlee@alum.mit.edu>
2013-03-18 16:55:58 -0400

    fixing test/agrad/special_functions_test.cpp

--- src/test/agrad/special_functions_test.cpp
+++ src/test/agrad/special_functions_test.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include <stan/agrad/special_functions.hpp>
+#include <stan/math.hpp>
 
 // cut and paste helpers and typedefs from agrad_test.cpp
 typedef stan::agrad::var AVAR;
