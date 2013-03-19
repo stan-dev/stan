1c66a9956fb4008c5019583238da367474fff58c
Daniel Lee <bearlee@alum.mit.edu>
2013-03-19 14:38:50 -0400

    moving constant functions out of src/stan/math/special_functions.hpp

--- src/test/math/special_functions_test.cpp
+++ src/test/math/special_functions_test.cpp
@@ -20,35 +20,3 @@ TEST(MathSpecialFunctions,BoostNoDeclTypeDef) {
  EXPECT_TRUE(BOOST_NO_DECLTYPE_is_defined);
 }
 
-
-TEST(MathsSpecialFunctions, pi_fun) {
-  EXPECT_FLOAT_EQ(4.0 * std::atan(1.0), stan::math::pi());
-}
-TEST(MathsSpecialFunctions, e_fun) {
-  EXPECT_FLOAT_EQ(std::exp(1.0), stan::math::e());
-}
-TEST(MathsSpecialFunctions, sqrt2_fun) {
-  EXPECT_FLOAT_EQ(std::sqrt(2.0), stan::math::sqrt2());
-}
-TEST(MathsSpecialFunctions, log10_fun) {
-  EXPECT_FLOAT_EQ(std::log(10.0), stan::math::log10());
-}
-
-TEST(MathsSpecialFunctions, infty) {
-  EXPECT_FLOAT_EQ(std::numeric_limits<double>::infinity(), stan::math::positive_infinity());
-}
-TEST(MathsSpecialFunctions, neg_infty) {
-  EXPECT_FLOAT_EQ(-std::numeric_limits<double>::infinity(), 
-                  stan::math::negative_infinity());
-}
-TEST(MathsSpecialFunctions, nan) {
-  EXPECT_TRUE(std::isnan(stan::math::not_a_number()));
-}
-TEST(MathsSpecialFunctions, epsilon) {
-  EXPECT_FLOAT_EQ(std::numeric_limits<double>::epsilon(),
-                  stan::math::epsilon());
-}
-TEST(MathsSpecialFunctions, negative_epsilon) {
-  EXPECT_FLOAT_EQ(-std::numeric_limits<double>::epsilon(),
-                  stan::math::negative_epsilon());
-}
