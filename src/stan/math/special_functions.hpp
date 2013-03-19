6737d11592c04d0771b723dc7ed5de2063fb3820
Daniel Lee <bearlee@alum.mit.edu>
2013-03-19 11:29:49 -0400

    moving log_sum_exp out of src/stan/math/special_functions.hpp

--- src/stan/math/special_functions.hpp
+++ src/stan/math/special_functions.hpp
@@ -76,23 +76,6 @@ namespace stan {
     }
 
 
-    /**
-     * Calculates the log sum of exponetials without overflow.
-     *
-     * \f$\log (\exp(a) + \exp(b)) = m + \log(\exp(a-m) + \exp(b-m))\f$,
-     *
-     * where \f$m = max(a,b)\f$.
-     * 
-     * @param a the first variable
-     * @param b the second variable
-     */
-    inline double log_sum_exp(const double& a, const double& b) {
-      using std::exp;
-      if (a > b)
-        return a + log1p(exp(b - a));
-      return b + log1p(exp(a - b));
-    }
-
     /** 
      * The normalized incomplete beta function of a, b, and x.
      *
@@ -112,34 +95,6 @@ namespace stan {
     }
 
 
-
-    /**
-     * Return the log of the sum of the exponentiated values of the specified
-     * sequence of values.
-     *
-     * The function is defined as follows to prevent overflow in exponential
-     * calculations.
-     *
-     * \f$\log \sum_{n=1}^N \exp(x_n) = \max(x) + \log \sum_{n=1}^N \exp(x_n - \max(x))\f$.
-     * 
-     * @param x array of specified values
-     */
-    double log_sum_exp(const std::vector<double>& x) {
-      using std::numeric_limits;
-      using std::log;
-      using std::exp;
-      double max = -numeric_limits<double>::infinity();
-      for (size_t ii = 0; ii < x.size(); ii++) 
-        if (x[ii] > max) 
-          max = x[ii];
-            
-      double sum = 0.0;
-      for (size_t ii = 0; ii < x.size(); ii++) 
-        if (x[ii] != -numeric_limits<double>::infinity()) 
-          sum += exp(x[ii] - max);
-          
-      return max + log(sum);
-    }
 
     /**
      * The logical negation function which returns 1 if the input
--- src/test/math/special_functions_test.cpp
+++ src/test/math/special_functions_test.cpp
@@ -3,89 +3,8 @@
 #include <gtest/gtest.h>
 #include "stan/math/special_functions.hpp"
 
-TEST(MathsSpecialFunctions, pi_fun) {
-  EXPECT_FLOAT_EQ(4.0 * std::atan(1.0), stan::math::pi());
-}
-TEST(MathsSpecialFunctions, e_fun) {
-  EXPECT_FLOAT_EQ(std::exp(1.0), stan::math::e());
-}
-TEST(MathsSpecialFunctions, sqrt2_fun) {
-  EXPECT_FLOAT_EQ(std::sqrt(2.0), stan::math::sqrt2());
-}
-TEST(MathsSpecialFunctions, log10_fun) {
-  EXPECT_FLOAT_EQ(std::log(10.0), stan::math::log10());
-}
 
-TEST(MathsSpecialFunctions, infty) {
-  EXPECT_FLOAT_EQ(std::numeric_limits<double>::infinity(), stan::math::positive_infinity());
-}
-TEST(MathsSpecialFunctions, neg_infty) {
-  EXPECT_FLOAT_EQ(-std::numeric_limits<double>::infinity(), 
-                  stan::math::negative_infinity());
-}
-TEST(MathsSpecialFunctions, nan) {
-  EXPECT_TRUE(std::isnan(stan::math::not_a_number()));
-}
-TEST(MathsSpecialFunctions, epsilon) {
-  EXPECT_FLOAT_EQ(std::numeric_limits<double>::epsilon(),
-                  stan::math::epsilon());
-}
-TEST(MathsSpecialFunctions, negative_epsilon) {
-  EXPECT_FLOAT_EQ(-std::numeric_limits<double>::epsilon(),
-                  stan::math::negative_epsilon());
-}
-
-void test_log_sum_exp(double a, double b) {
-  using std::log;
-  using std::exp;
-  using stan::math::log_sum_exp;
-  EXPECT_FLOAT_EQ(log(exp(a) + exp(b)),
-                  log_sum_exp(a,b));
-}
-
-void test_log_sum_exp(const std::vector<double>& as) {
-  using std::log;
-  using std::exp;
-  using stan::math::log_sum_exp;
-  double sum_exp = 0.0;
-  for (size_t n = 0; n < as.size(); ++n)
-    sum_exp += exp(as[n]);
-  EXPECT_FLOAT_EQ(log(sum_exp),
-                  log_sum_exp(as));
-}
-
-
-
-TEST(MathsSpecialFunctions, log_sum_exp) {
-  using stan::math::log_sum_exp;
-  std::vector<double> as;
-  test_log_sum_exp(as);
-  as.push_back(0.0);
-  test_log_sum_exp(as);
-  as.push_back(1.0);
-  test_log_sum_exp(as);
-  as.push_back(-1.0);
-  test_log_sum_exp(as);
-  as.push_back(-10000.0);
-  test_log_sum_exp(as);
-  
-  as.push_back(10000.0);
-  EXPECT_FLOAT_EQ(10000.0, log_sum_exp(as));
-}
-TEST(MathsSpecialFunctions, log_sum_exp_2) {
-  using stan::math::log_sum_exp;
-  test_log_sum_exp(1.0,2.0);
-  test_log_sum_exp(1.0,1.0);
-  test_log_sum_exp(3.0,2.0);
-  test_log_sum_exp(-20.0,12);
-  test_log_sum_exp(-20.0,12);
-
-  // exp(10000.0) overflows
-  EXPECT_FLOAT_EQ(10000.0,log_sum_exp(10000.0,0.0));
-  EXPECT_FLOAT_EQ(0.0,log_sum_exp(-10000.0,0.0));
-}
 TEST(MathsSpecialFunctions, log1p_exp) {
-  using stan::math::log_sum_exp;
   using stan::math::log1p_exp;
 
   // exp(10000.0) overflows
@@ -269,4 +188,37 @@ TEST(MathSpecialFunctions,BoostNoDeclTypeDef) {
  BOOST_NO_DECLTYPE_is_defined = true;
 #endif
  EXPECT_TRUE(BOOST_NO_DECLTYPE_is_defined);
+}
+
+
+TEST(MathsSpecialFunctions, pi_fun) {
+  EXPECT_FLOAT_EQ(4.0 * std::atan(1.0), stan::math::pi());
+}
+TEST(MathsSpecialFunctions, e_fun) {
+  EXPECT_FLOAT_EQ(std::exp(1.0), stan::math::e());
+}
+TEST(MathsSpecialFunctions, sqrt2_fun) {
+  EXPECT_FLOAT_EQ(std::sqrt(2.0), stan::math::sqrt2());
+}
+TEST(MathsSpecialFunctions, log10_fun) {
+  EXPECT_FLOAT_EQ(std::log(10.0), stan::math::log10());
+}
+
+TEST(MathsSpecialFunctions, infty) {
+  EXPECT_FLOAT_EQ(std::numeric_limits<double>::infinity(), stan::math::positive_infinity());
+}
+TEST(MathsSpecialFunctions, neg_infty) {
+  EXPECT_FLOAT_EQ(-std::numeric_limits<double>::infinity(), 
+                  stan::math::negative_infinity());
+}
+TEST(MathsSpecialFunctions, nan) {
+  EXPECT_TRUE(std::isnan(stan::math::not_a_number()));
+}
+TEST(MathsSpecialFunctions, epsilon) {
+  EXPECT_FLOAT_EQ(std::numeric_limits<double>::epsilon(),
+                  stan::math::epsilon());
+}
+TEST(MathsSpecialFunctions, negative_epsilon) {
+  EXPECT_FLOAT_EQ(-std::numeric_limits<double>::epsilon(),
+                  stan::math::negative_epsilon());
 }
