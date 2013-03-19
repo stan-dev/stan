26fbb4fe8d9eef65b032696a492b9827d2c0f66d
Daniel Lee <bearlee@alum.mit.edu>
2013-03-18 21:48:14 -0400

    moving log1p out of src/stan/math/special_functions.hpp

--- src/stan/math/special_functions.hpp
+++ src/stan/math/special_functions.hpp
@@ -20,30 +20,6 @@ namespace stan {
     // PROBABILITY-RELATED FUNCTIONS
 
     /**
-     * Return the natural logarithm of one plus the specified value.
-     *
-     * The main use of this function is to cut down on intermediate
-     * values during algorithmic differentiation.
-     *
-     * @param x Specified value.
-     * @return Natural log of one plus <code>x</code>.
-     */
-    template <typename T>
-    inline typename boost::math::tools::promote_args<T>::type
-    log1p(T x) {
-      using std::log;
-      if (x < -1.0)
-        BOOST_THROW_EXCEPTION(std::domain_error ("x can not be less than -1"));
-
-      if (x > 1e-9 || x < -1e-9)
-        return log(1.0 + x);     // direct, if distant from 1
-      else if (x > 1e-16 || x < -1e-16)
-        return x - 0.5 * x * x;  // 2nd order Taylor, if close to 1
-      else
-        return x;                // 1st order Taylor, if very close to 1
-    }
-
-    /**
      * Return the natural logarithm of one minus the specified value.
      *
      * The main use of this function is to cut down on intermediate
