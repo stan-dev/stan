3b91a1607af7bf68c984ca225d422615c18999cd
Daniel Lee <bearlee@alum.mit.edu>
2013-03-19 13:19:43 -0400

    moving log1m_inv_logit out of src/stan/math/special_functions.hpp

--- src/test/math/special_functions_test.cpp
+++ src/test/math/special_functions_test.cpp
@@ -3,15 +3,6 @@
 #include <gtest/gtest.h>
 #include "stan/math/special_functions.hpp"
 
-TEST(MathSpecialFunctions, log1m_inv_logit) {
-  using stan::math::log1m_inv_logit;
-  using std::log;
-  using stan::math::inv_logit;
-
-  EXPECT_FLOAT_EQ(log(1 - inv_logit(-7.2)), log1m_inv_logit(-7.2));
-  EXPECT_FLOAT_EQ(log(1 - inv_logit(0.0)), log1m_inv_logit(0.0));
-  EXPECT_FLOAT_EQ(log(1 - inv_logit(1.9)), log1m_inv_logit(1.9));
-}
 TEST(MathSpecialFunctions,logicalNegation) {
   using stan::math::logical_negation;
   EXPECT_TRUE(logical_negation(0));
