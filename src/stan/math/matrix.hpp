95143607c741e3615d7cf7503a3ffaad74297493
Daniel Lee <bearlee@alum.mit.edu>
2013-03-19 22:34:01 -0400

    moving diag_pre_multiply out of stan/math/matrix.hpp

--- src/stan/math/matrix.hpp
+++ src/stan/math/matrix.hpp
@@ -1827,22 +1827,6 @@ namespace stan {
         result(i) = m1(i) * m2(i);
       return result;
     }
-
-    template <typename T1, typename T2, int R1,int C1,int R2,int C2>
-    Eigen::Matrix<typename boost::math::tools::promote_args<T1,T2>::type, R2, C2>
-    diag_pre_multiply(const Eigen::Matrix<T1,R1,C1>& m1,
-                  const Eigen::Matrix<T2,R2,C2>& m2) {
-      if (m1.cols() != 1 && m1.rows() != 1)
-        throw std::domain_error("m1 must be a vector");
-      if (m1.size() != m2.rows())
-        throw std::domain_error("m1 must have same length as m2 has rows");
-      Eigen::Matrix<typename boost::math::tools::promote_args<T1,T2>::type, R2, C2>
-        result(m2.rows(),m2.cols());
-      for (int i = 0; i < m2.rows(); ++i)
-        for (int j = 0; i < m2.cols(); ++j)
-          result(i,j) = m1(i) * m2(i,j);
-      return result;
-    }
         
     // void eigen_decompose_sym(const matrix_d& m,
     //                          vector_d& eigenvalues,
