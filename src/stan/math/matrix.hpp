aedf14e5c88ef58f056d2bc676dfabc215f1a613
Daniel Lee <bearlee@alum.mit.edu>
2013-03-20 08:58:24 -0400

    moving trace out of math/matrix.hpp

--- src/stan/math/matrix.hpp
+++ src/stan/math/matrix.hpp
@@ -1614,19 +1614,6 @@ namespace stan {
       return v.prod();
     }
 
-    /**
-     * Returns the trace of the specified matrix.  The trace
-     * is defined as the sum of the elements on the diagonal.
-     * The matrix is not required to be square.  Returns 0 if
-     * matrix is empty.
-     *
-     * @param[in] m Specified matrix.
-     * @return Trace of the matrix.
-     */
-    template <typename T>
-    inline T trace(const Eigen::Matrix<T,Eigen::Dynamic,Eigen::Dynamic>& m) {
-      return m.trace();
-    }
 
 
     // vector and matrix returns
--- src/stan/math/matrix/trace.hpp
+++ src/stan/math/matrix/trace.hpp
@@ -6,7 +6,19 @@
 namespace stan {
   namespace math {
     
-    
+    /**
+     * Returns the trace of the specified matrix.  The trace
+     * is defined as the sum of the elements on the diagonal.
+     * The matrix is not required to be square.  Returns 0 if
+     * matrix is empty.
+     *
+     * @param[in] m Specified matrix.
+     * @return Trace of the matrix.
+     */
+    template <typename T>
+    inline T trace(const Eigen::Matrix<T,Eigen::Dynamic,Eigen::Dynamic>& m) {
+      return m.trace();
+    }
     
   }
 }
