ad060697d4af77b641f06f88461c123c8f16382e
Daniel Lee <bearlee@alum.mit.edu>
2013-03-20 11:06:27 -0400

    fixing includes for math/matrix.hpp

--- src/test/prob/transform_test.cpp
+++ src/test/prob/transform_test.cpp
@@ -6,6 +6,7 @@
 #include <stan/agrad/agrad.hpp>
 #include <stan/agrad/special_functions.hpp>
 #include <stan/prob/transform.hpp>
+#include <stan/math/matrix/determinant.hpp>
 
 using Eigen::Matrix;
 using Eigen::Dynamic;
