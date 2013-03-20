e85d765b0b4929b4dfeeadb8caf952c8053f55f5
Daniel Lee <bearlee@alum.mit.edu>
2013-03-19 22:50:37 -0400

    moving add out of stan/math/matrix.hpp

--- src/stan/math/matrix/minus.hpp
+++ src/stan/math/matrix/minus.hpp
@@ -6,6 +6,19 @@
 namespace stan {
   namespace math {
 
+    /**
+     * Returns the negation of the specified scalar or matrix.
+     *
+     * @tparam T Type of subtrahend.
+     * @param x Subtrahend.
+     * @return Negation of subtrahend.
+     */
+    template <typename T>
+    inline
+    T minus(const T& x) {
+      return -x;
+    }
+
   }
 }
 #endif
--- src/test/math/matrix/minus_test.cpp
+++ src/test/math/matrix/minus_test.cpp
@@ -1,2 +1,12 @@
 #include <stan/math/matrix/minus.hpp>
 #include <gtest/gtest.h>
+
+TEST(MathMatrix, minus) {
+  vector_d v0;
+  row_vector_d rv0;
+  matrix_d m0;
+
+  EXPECT_EQ(0,stan::math::minus(v0).size());
+  EXPECT_EQ(0,stan::math::minus(rv0).size());
+  EXPECT_EQ(0,stan::math::minus(m0).size());
+}
