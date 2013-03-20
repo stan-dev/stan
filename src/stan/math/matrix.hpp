7cfb502b2e53553435ff6294b79e32ceac5c0538
Daniel Lee <bearlee@alum.mit.edu>
2013-03-20 08:56:37 -0400

    moving log out of math/matrix.hpp

--- src/stan/math/matrix.hpp
+++ src/stan/math/matrix.hpp
@@ -1628,16 +1628,6 @@ namespace stan {
       return m.trace();
     }
 
-    /**
-     * Return the element-wise logarithm of the matrix or vector.
-     *
-     * @param m The matrix or vector.
-     * @return ret(i,j) = log(m(i,j))
-     */
-    template<typename T, int Rows, int Cols>
-    inline Eigen::Matrix<T,Rows,Cols> log(const Eigen::Matrix<T,Rows,Cols>& m) {
-      return m.array().log().matrix();
-    }
 
     // vector and matrix returns
 
