9bb1bdfb67277b5b1613a1867f38e5662bd8734e
Daniel Lee <bearlee@alum.mit.edu>
2013-03-20 09:25:23 -0400

    moving max out of math/matrix.hpp

--- src/stan/math/matrix.hpp
+++ src/stan/math/matrix.hpp
@@ -1395,60 +1395,6 @@ namespace stan {
       return m.minCoeff();
     }
 
-
-
-    /**
-     * Returns the maximum coefficient in the specified
-     * column vector.
-     * @param v Specified vector.
-     * @return Maximum coefficient value in the vector.
-     * @tparam Type of values being compared and returned
-     * @throw std::domain_error If the size of the vector is zero.
-     */
-    inline int max(const std::vector<int>& x) {
-      if (x.size() == 0)
-        throw std::domain_error("error: cannot take max of empty int vector");
-      int max = x[0];
-      for (size_t i = 1; i < x.size(); ++i)
-        if (x[i] > max) 
-          max = x[i];
-      return max;
-    }
-
-    /**
-     * Returns the maximum coefficient in the specified
-     * column vector.
-     * @param v Specified vector.
-     * @return Maximum coefficient value in the vector.
-     * @tparam T Type of values being compared and returned
-     */
-    template <typename T>
-    inline T max(const std::vector<T>& x) {
-      if (x.size() == 0)
-        return -std::numeric_limits<T>::infinity();
-      T max = x[0];
-      for (size_t i = 1; i < x.size(); ++i)
-        if (x[i] > max) 
-          max = x[i];
-      return max;
-    }
-
-    /**
-     * Returns the maximum coefficient in the specified
-     * vector, row vector, or matrix.
-     * @param v Specified vector, row vector, or matrix.
-     * @return Maximum coefficient value in the vector.
-     */
-    template <typename T, int R, int C>
-    inline T max(const Eigen::Matrix<T,R,C>& m) {
-      if (m.size() == 0)
-        return -std::numeric_limits<double>::infinity();
-      return m.maxCoeff();
-    }
-
-
-
-
     // vector and matrix returns
 
     // void eigen_decompose_sym(const matrix_d& m,
