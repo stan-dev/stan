59e331121d51c7340bd41c9d80d9bc4b51baa77f
Daniel Lee <bearlee@alum.mit.edu>
2013-03-19 22:36:48 -0400

    moving elt_multiply out of stan/math/matrix.hpp

--- src/stan/math/matrix.hpp
+++ src/stan/math/matrix.hpp
@@ -1803,31 +1803,6 @@ namespace stan {
            double c) {
       return m / c;
     }
-
-    /**
-     * Return the elementwise multiplication of the specified
-     * matrices.  
-     *
-     * @tparam T1 Type of scalars in first matrix.
-     * @tparam T2 Type of scalars in second matrix.
-     * @tparam R Row type of both matrices.
-     * @tparam C Column type of both matrices.
-     * @param m1 First matrix
-     * @param m2 Second matrix
-     * @return Elementwise product of matrices.
-     */
-    template <typename T1, typename T2, int R, int C>
-    Eigen::Matrix<typename boost::math::tools::promote_args<T1,T2>::type, R, C>
-    elt_multiply(const Eigen::Matrix<T1,R,C>& m1,
-                 const Eigen::Matrix<T2,R,C>& m2) {
-      stan::math::validate_matching_dims(m1,m2,"elt_multiply");
-      Eigen::Matrix<typename boost::math::tools::promote_args<T1,T2>::type, R, C>
-        result(m1.rows(),m2.cols());
-      for (int i = 0; i < m1.size(); ++i)
-        result(i) = m1(i) * m2(i);
-      return result;
-    }
-        
     // void eigen_decompose_sym(const matrix_d& m,
     //                          vector_d& eigenvalues,
     //                          matrix_d& eigenvectors) {
--- src/test/math/old_matrix_test.cpp
+++ src/test/math/old_matrix_test.cpp
@@ -634,69 +634,6 @@ TEST(matrix_test,stdVectorProd) {
   m << 1.0, 2.0, 3.0, 4.0, 5.0, 6.0;
   EXPECT_FLOAT_EQ(720.0,prod(m));
 }
-
-TEST(matrixTest,eltMultiplyVec) {
-  vector_d v1(2);
-  vector_d v2(2);
-  v1 << 1, 2;
-  v2 << 10, 100;
-  vector_d v = stan::math::elt_multiply(v1,v2);
-  EXPECT_FLOAT_EQ(10.0, v(0));
-  EXPECT_FLOAT_EQ(200.0, v(1));
-}
-TEST(matrixTest,eltMultiplyVecException) {
-  vector_d v1(2);
-  vector_d v2(3);
-  v1 << 1, 2;
-  v2 << 10, 100, 1000;
-  EXPECT_THROW(stan::math::elt_multiply(v1,v2), std::domain_error);
-}
-TEST(matrixTest,eltMultiplyRowVec) {
-  row_vector_d v1(2);
-  row_vector_d v2(2);
-  v1 << 1, 2;
-  v2 << 10, 100;
-  row_vector_d v = stan::math::elt_multiply(v1,v2);
-  EXPECT_FLOAT_EQ(10.0, v(0));
-  EXPECT_FLOAT_EQ(200.0, v(1));
-}
-TEST(matrixTest,eltMultiplyRowVecException) {
-  row_vector_d v1(2);
-  row_vector_d v2(3);
-  v1 << 1, 2;
-  v2 << 10, 100, 1000;
-  EXPECT_THROW(stan::math::elt_multiply(v1,v2), std::domain_error);
-}
-TEST(matrixTest,eltMultiplyMatrix) {
-  matrix_d m1(2,3);
-  matrix_d m2(2,3);
-  m1 << 1, 2, 3, 4, 5, 6;
-  m2 << 10, 100, 1000, 10000, 100000, 1000000;
-  matrix_d m = stan::math::elt_multiply(m1,m2);
-  
-  EXPECT_EQ(2,m.rows());
-  EXPECT_EQ(3,m.cols());
-  EXPECT_FLOAT_EQ(10.0, m(0,0));
-  EXPECT_FLOAT_EQ(200.0, m(0,1));
-  EXPECT_FLOAT_EQ(3000.0, m(0,2));
-  EXPECT_FLOAT_EQ(40000.0, m(1,0));
-  EXPECT_FLOAT_EQ(500000.0, m(1,1));
-  EXPECT_FLOAT_EQ(6000000.0, m(1,2));
-}
-TEST(matrixTest,eltMultiplyMatrixException) {
-  matrix_d m1(2,3);
-  matrix_d m2(2,4);
-  matrix_d m3(4,3);
-  m1 << 1, 2, 3, 4, 5, 6;
-  m2 << -1, -2, -3, -4, -5, -6, -7, -8;
-  m3 << 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24;
-  EXPECT_THROW(stan::math::elt_multiply(m1,m2),std::domain_error);
-  EXPECT_THROW(stan::math::elt_multiply(m1,m3),std::domain_error);
-}
-
-
-
-
 TEST(MathMatrix,dot_self) {
   using stan::math::dot_self;
 
