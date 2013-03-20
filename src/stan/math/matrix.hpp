2766fc3d36a710949a2aaca832b994e10dc3ab97
Daniel Lee <bearlee@alum.mit.edu>
2013-03-20 09:41:09 -0400

    moving dot_self out of math/matrix.hpp

--- src/stan/math/matrix.hpp
+++ src/stan/math/matrix.hpp
@@ -1253,24 +1253,6 @@ namespace stan {
       return m.colPivHouseholderQr().logAbsDeterminant();
     }
     
-
-    /**
-     * Returns the dot product of the specified vector with itself.
-     * @param v Vector.
-     * @tparam R number of rows or <code>Eigen::Dynamic</code> for dynamic
-     * @tparam C number of rows or <code>Eigen::Dyanmic</code> for dynamic
-     * @throw std::domain_error If v is not vector dimensioned.
-     */
-    template <int R, int C>
-    inline double dot_self(const Eigen::Matrix<double, R, C>& v) {
-      validate_vector(v,"dot_self");
-      double sum = 0.0;
-      for (int i = 0; i < v.size(); ++i)
-        sum += v(i) * v(i);
-      return sum;
-    }
-
-
     // vector and matrix returns
 
     // void eigen_decompose_sym(const matrix_d& m,
--- src/test/math/old_matrix_test.cpp
+++ src/test/math/old_matrix_test.cpp
@@ -311,51 +311,6 @@ TEST(matrix,get_base1_8) {
                                             "x8",1));
 }
 
-
-
-
-
-TEST(MathMatrix,dot_self) {
-  using stan::math::dot_self;
-
-  Eigen::Matrix<double,Eigen::Dynamic,1> v1(1);
-  v1 << 2.0;
-  EXPECT_NEAR(4.0,dot_self(v1),1E-12);
-  Eigen::Matrix<double,Eigen::Dynamic,1> v2(2);
-  v2 << 2.0, 3.0;
-  EXPECT_NEAR(13.0,dot_self(v2),1E-12);
-  Eigen::Matrix<double,Eigen::Dynamic,1> v3(3);
-  v3 << 2.0, 3.0, 4.0;
-  EXPECT_NEAR(29.0,dot_self(v3),1E-12);
-
-  Eigen::Matrix<double,1,Eigen::Dynamic> rv1(1);
-  rv1 << 2.0;
-  EXPECT_NEAR(4.0,dot_self(rv1),1E-12);
-  Eigen::Matrix<double,1,Eigen::Dynamic> rv2(2);
-  rv2 << 2.0, 3.0;
-  EXPECT_NEAR(13.0,dot_self(rv2),1E-12);
-  Eigen::Matrix<double,1,Eigen::Dynamic> rv3(3);
-  rv3 << 2.0, 3.0, 4.0;
-  EXPECT_NEAR(29.0,dot_self(rv3),1E-12);
-
-  Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic> m1(1,1);
-  m1 << 2.0;
-  EXPECT_NEAR(4.0,dot_self(m1),1E-12);
-  Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic> m2(2,1);
-  m2 << 2.0, 3.0;
-  EXPECT_NEAR(13.0,dot_self(m2),1E-12);
-  Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic> m3(3,1);
-  m3 << 2.0, 3.0, 4.0;
-  EXPECT_NEAR(29.0,dot_self(m3),1E-12);
-
-  Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic> mm2(1,2);
-  mm2 << 2.0, 3.0;
-  EXPECT_NEAR(13.0,dot_self(mm2),1E-12);
-  Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic> mm3(1,3);
-  mm3 << 2.0, 3.0, 4.0;
-  EXPECT_NEAR(29.0,dot_self(mm3),1E-12);
-
-}
 TEST(MathMatrix,dimensionValidation) {
   using stan::math::determinant;
   using Eigen::Matrix;
