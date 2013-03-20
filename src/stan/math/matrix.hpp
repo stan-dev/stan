7bd2e319b7e7eb715fed616608d5c350ddaed63f
Daniel Lee <bearlee@alum.mit.edu>
2013-03-19 22:00:30 -0400

    moving block out of stan/math/matrix.hpp

--- src/stan/math/matrix.hpp
+++ src/stan/math/matrix.hpp
@@ -2047,26 +2047,6 @@ namespace stan {
       return m.col(j - 1);
     }
 
-    /**
-     * Return a nrows x ncols submatrix starting at (i,j).
-     *
-     * @param m Matrix
-     * @param i Starting row
-     * @param j Starting column
-     * @param nrows Number of rows in block
-     * @param ncols Number of columns in block
-     **/
-    template <typename T>
-    inline
-    Eigen::Matrix<T,Eigen::Dynamic,Eigen::Dynamic>
-    block(const Eigen::Matrix<T,Eigen::Dynamic,Eigen::Dynamic>& m,
-          size_t i, size_t j, size_t nrows, size_t ncols) {
-      validate_row_index(m,i,"block");
-      validate_row_index(m,i+nrows-1,"block");
-      validate_column_index(m,j,"block");
-      validate_column_index(m,j+ncols-1,"block");
-      return m.block(i - 1,j - 1,nrows,ncols);
-    }
 
     // void eigen_decompose_sym(const matrix_d& m,
     //                          vector_d& eigenvalues,
