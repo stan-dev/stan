fc8c5a03382ba8ac8b7566122d2b7a25f6474e73
Daniel Lee <bearlee@alum.mit.edu>
2013-03-20 09:32:51 -0400

    moving min out of math/matrix.hpp

--- src/stan/math/matrix.hpp
+++ src/stan/math/matrix.hpp
@@ -1347,54 +1347,6 @@ namespace stan {
       return dot_product(&v1[0], &v2[0], v1.size());
     }
 
-    /**
-     * Returns the minimum coefficient in the specified
-     * column vector.
-     * @param v Specified vector.
-     * @return Minimum coefficient value in the vector.
-     * @tparam Type of values being compared and returned
-     */
-    inline int min(const std::vector<int>& x) {
-      if (x.size() == 0)
-        throw std::domain_error("error: cannot take min of empty int vector");
-      int min = x[0];
-      for (size_t i = 1; i < x.size(); ++i)
-        if (x[i] < min) 
-          min = x[i];
-      return min;
-    }
-
-    /**
-     * Returns the minimum coefficient in the specified
-     * column vector.
-     * @param v Specified vector.
-     * @return Minimum coefficient value in the vector.
-     * @tparam Type of values being compared and returned
-     */
-    template <typename T>
-    inline T min(const std::vector<T>& x) {
-      if (x.size() == 0)
-        return std::numeric_limits<T>::infinity();
-      T min = x[0];
-      for (size_t i = 1; i < x.size(); ++i)
-        if (x[i] < min) 
-          min = x[i];
-      return min;
-    }
-
-    /**
-     * Returns the minimum coefficient in the specified
-     * matrix, vector, or row vector.
-     * @param v Specified matrix, vector, or row vector.
-     * @return Minimum coefficient value in the vector.
-     */
-    template <typename T, int R, int C>
-    inline T min(const Eigen::Matrix<T,R,C>& m) {
-      if (m.size() == 0) 
-        return std::numeric_limits<double>::infinity();
-      return m.minCoeff();
-    }
-
     // vector and matrix returns
 
     // void eigen_decompose_sym(const matrix_d& m,
