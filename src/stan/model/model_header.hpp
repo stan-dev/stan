92b702c0f535774af07445cc3d7d4b31c9379fe4
Daniel Lee <bearlee@alum.mit.edu>
2013-03-19 23:32:28 -0400

    updating other parts that depend on matrix.hpp

--- src/stan/model/model_header.hpp
+++ src/stan/model/model_header.hpp
@@ -25,6 +25,30 @@
 #include <stan/io/csv_writer.hpp>
 #include <stan/math.hpp>
 #include <stan/math/matrix.hpp>
+
+// FIXME: these should go in matrix.hpp
+#include <stan/math/matrix/add.hpp>
+#include <stan/math/matrix/cholesky_decompose.hpp>
+#include <stan/math/matrix/col.hpp>
+#include <stan/math/matrix/cumulative_sum.hpp>
+#include <stan/math/matrix/diag_matrix.hpp>
+#include <stan/math/matrix/divide.hpp>
+#include <stan/math/matrix/eigenvalues_sym.hpp>
+#include <stan/math/matrix/elt_divide.hpp>
+#include <stan/math/matrix/elt_multiply.hpp>
+#include <stan/math/matrix/inverse.hpp>
+#include <stan/math/matrix/mdivide_left.hpp>
+#include <stan/math/matrix/mdivide_left_tri.hpp>
+#include <stan/math/matrix/mdivide_right.hpp>
+#include <stan/math/matrix/minus.hpp>
+#include <stan/math/matrix/multiply.hpp>
+#include <stan/math/matrix/multiply_lower_tri_self_transpose.hpp>
+#include <stan/math/matrix/row.hpp>
+#include <stan/math/matrix/singular_values.hpp>
+#include <stan/math/matrix/softmax.hpp>
+#include <stan/math/matrix/subtract.hpp>
+#include <stan/math/matrix/transpose.hpp>
+
 #include <stan/mcmc/sampler.hpp>
 #include <stan/model/prob_grad_ad.hpp>
 #include <stan/prob/distributions.hpp>
