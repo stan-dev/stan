0ba2677123698a7f5ad08bac570627625ef78508
Daniel Lee <bearlee@alum.mit.edu>
2013-03-19 21:23:51 -0400

    moving mdivide_right_tri, mdivide_right_tri_low out of stan/math/matrix.hpp

---
+++ src/test/math/matrix/mdivide_right_test.cpp
@@ -0,0 +1,17 @@
+#include <stan/math/matrix/mdivide_right.hpp>
+#include <gtest/gtest.h>
+
+TEST(matrixTest,mdivide_right_val) {
+  using stan::math::mdivide_right;
+  stan::math::matrix_d Ad(2,2);
+  stan::math::matrix_d I;
+
+  Ad << 2.0, 3.0, 
+        5.0, 7.0;
+
+  I = mdivide_right(Ad,Ad);
+  EXPECT_NEAR(1.0,I(0,0),1.0E-12);
+  EXPECT_NEAR(0.0,I(0,1),1.0E-12);
+  EXPECT_NEAR(0.0,I(1,0),1.0E-12);
+  EXPECT_NEAR(1.0,I(1,1),1.0e-12);
+}
---
+++ src/test/math/matrix/mdivide_right_tri_low_test.cpp
@@ -0,0 +1,2 @@
+#include <stan/math/matrix/mdivide_right_tri_low.hpp>
+#include <gtest/gtest.h>
