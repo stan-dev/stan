12d6144b224a298d8fc9c0ee748be4badff58a87
Daniel Lee <bearlee@alum.mit.edu>
2013-03-23 16:53:28 -0400

    moving validate_less_or_equal out of stan/math/matrix.hpp

--- src/stan/math/matrix.hpp
+++ src/stan/math/matrix.hpp
@@ -995,17 +995,6 @@ namespace stan {
 
     template <typename T1, typename T2>
     inline
-    void validate_less_or_equal(const T1& x, const T2& y,
-                                const char* x_name, const char* y_name, 
-                                const char* fun_name) {
-      if (x <= y) return;
-      std::stringstream ss;
-      ss << "require " << x_name << " <= " << y_name
-         << " in " << fun_name
-         << "; found " << x_name << "=" << x
-         << ", " << y_name << "=" << y;
-      throw std::domain_error(ss.str());
-    }
 
 
     // scalar returns
