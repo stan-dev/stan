4d86c2d7677a908d8d89e51be4e679989a649482
Daniel Lee <bearlee@alum.mit.edu>
2013-03-23 16:52:30 -0400

    moving validate_less out of stan/math/matrix.hpp

--- src/stan/math/matrix/validate_less.hpp
+++ src/stan/math/matrix/validate_less.hpp
@@ -6,6 +6,19 @@
 namespace stan {
   namespace math {
     
+    template <typename T1, typename T2>
+    inline
+    void validate_less(const T1& x, const T2& y,
+                       const char* x_name, const char* y_name, 
+                       const char* fun_name) {
+      if (x < y) return;
+      std::stringstream ss;
+      ss << "require " << x_name << " < " << y_name
+         << " in " << fun_name
+         << "; found " << x_name << "=" << x
+         << ", " << y_name << "=" << y;
+      throw std::domain_error(ss.str());
+    }
     
   }
 }
