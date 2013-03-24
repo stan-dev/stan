1ae81f00bb7a5f77d1720a2a926ae4f15bdda539
Daniel Lee <bearlee@alum.mit.edu>
2013-03-23 21:35:34 -0400

    moving array_builder out of stan/math/matrix.hpp

--- src/stan/math/matrix.hpp
+++ src/stan/math/matrix.hpp
@@ -120,32 +120,6 @@ namespace stan {
         ::promote_to(u);
     }
 
-
-
-
-    /**
-     * Structure for building up arrays in an expression (rather than
-     * in statements) using an argumentchaining add() method and 
-     * a getter method array() to return the result.
-     */
-    template <typename T>
-    struct array_builder {
-      std::vector<T> x_;
-      array_builder() : x_() { }
-      template <typename F>
-      array_builder& add(const F& u) {
-        T t;
-        promoter<F,T>::promote(u,t);
-        x_.push_back(t);
-        return *this;
-      }
-      std::vector<T> array() {
-        return x_;
-      }
-    };
-
-
-
     // polymorphic gets with bounds checking
 
 
