b658c4e07677418e3a9b600fb12304c767d27f16
Daniel Lee <bearlee@alum.mit.edu>
2013-03-26 17:30:14 -0400

    moving cosh out of stan/agrad.hpp

--- src/stan/agrad/agrad.hpp
+++ src/stan/agrad/agrad.hpp
@@ -76,15 +76,6 @@ namespace stan {
 
 
 
-      class cosh_vari : public op_v_vari {
-      public:
-        cosh_vari(vari* avi) :
-          op_v_vari(std::cosh(avi->val_),avi) {
-        }
-        void chain() {
-          avi_->adj_ += adj_ * std::sinh(avi_->val_);
-        }
-      };
 
       class sinh_vari : public op_v_vari {
       public:
@@ -161,19 +152,6 @@ namespace stan {
 
     // HYPERBOLIC FUNCTIONS
 
-    /**
-     * Return the hyperbolic cosine of the specified variable (cmath).
-     *
-     * The derivative is defined by
-     *
-     * \f$\frac{d}{dx} \cosh x = \sinh x\f$.
-     *
-     * @param a Variable.
-     * @return Hyperbolic cosine of variable.
-     */
-    inline var cosh(const var& a) {
-      return var(new cosh_vari(a.vi_));
-    }
 
     /**
      * Return the hyperbolic sine of the specified variable (cmath).
