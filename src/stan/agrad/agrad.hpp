f94e6b6e2e9aa0d0abce46f50599fba4bb9fab87
Daniel Lee <bearlee@alum.mit.edu>
2013-03-26 17:24:05 -0400

    moving sin out of stan/agrad.hpp

--- src/stan/agrad/agrad.hpp
+++ src/stan/agrad/agrad.hpp
@@ -75,15 +75,6 @@ namespace stan {
     namespace {
 
 
-      class sin_vari : public op_v_vari {
-      public:
-        sin_vari(vari* avi) :
-          op_v_vari(std::sin(avi->val_),avi) {
-        }
-        void chain() {
-          avi_->adj_ += adj_ * std::cos(avi_->val_);
-        }
-      };
 
       class tan_vari : public op_v_vari {
       public:
@@ -240,19 +231,6 @@ namespace stan {
 
     // TRIG FUNCTIONS
 
-    /**
-     * Return the sine of a radian-scaled variable (cmath).
-     *
-     * The derivative is defined by
-     *
-     * \f$\frac{d}{dx} \sin x = \cos x\f$.
-     *
-     * @param a Variable for radians of angle.
-     * @return Sine of variable. 
-     */
-    inline var sin(const var& a) {
-      return var(new sin_vari(a.vi_));
-    }
 
     /**
      * Return the tangent of a radian-scaled variable (cmath).
