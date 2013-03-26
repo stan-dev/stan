51e7f2503fb01865584bcd3619e397836e5e2c25
Daniel Lee <bearlee@alum.mit.edu>
2013-03-26 17:15:56 -0400

    moving log out of stan/agrad.hpp

--- src/stan/agrad/agrad.hpp
+++ src/stan/agrad/agrad.hpp
@@ -80,15 +80,6 @@ namespace stan {
 
 
 
-      class log_vari : public op_v_vari {
-      public:
-        log_vari(vari* avi) :
-          op_v_vari(std::log(avi->val_),avi) {
-        }
-        void chain() {
-          avi_->adj_ += adj_ / avi_->val_;
-        }
-      };
 
       double LOG_10 = std::log(10.0);
     
@@ -325,19 +316,6 @@ namespace stan {
     // CMATH EXP AND LOG
   
 
-    /**
-     * Return the natural log of the specified variable (cmath).
-     *
-     * The derivative is defined by
-     *
-     * \f$\frac{d}{dx} \log x = \frac{1}{x}\f$.
-     *
-     * @param a Variable whose log is taken.
-     * @return Natural log of variable.
-     */
-    inline var log(const var& a) {
-      return var(new log_vari(a.vi_));
-    }
 
     /**
      * Return the base 10 log of the specified variable (cmath).
--- src/stan/agrad/rev/log.hpp
+++ src/stan/agrad/rev/log.hpp
@@ -6,6 +6,32 @@
 namespace stan {
   namespace agrad {
 
+    namespace {
+      class log_vari : public op_v_vari {
+      public:
+        log_vari(vari* avi) :
+          op_v_vari(std::log(avi->val_),avi) {
+        }
+        void chain() {
+          avi_->adj_ += adj_ / avi_->val_;
+        }
+      };
+    }
+
+    /**
+     * Return the natural log of the specified variable (cmath).
+     *
+     * The derivative is defined by
+     *
+     * \f$\frac{d}{dx} \log x = \frac{1}{x}\f$.
+     *
+     * @param a Variable whose log is taken.
+     * @return Natural log of variable.
+     */
+    inline var log(const var& a) {
+      return var(new log_vari(a.vi_));
+    }
+    
   }
 }
 #endif
