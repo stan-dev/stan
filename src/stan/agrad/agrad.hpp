3ffc6501b467f78ea7a59ef9fdc3c99f8939ff9f
Daniel Lee <bearlee@alum.mit.edu>
2013-03-26 17:38:42 -0400

    moving abs out of stan/agrad.hpp

--- src/stan/agrad/agrad.hpp
+++ src/stan/agrad/agrad.hpp
@@ -74,33 +74,6 @@ namespace stan {
 
 
 
-    // STD LIB FUNCTIONS
-
-    /**
-     * Return the absolute value of the variable (std).  
-     *
-     * The value at the undifferentiable point 0 is conveniently set
-     * 0, so that
-     *
-     * \f$\frac{d}{dx}|x| = \mbox{sgn}(x)\f$.
-     *
-     * The function fabs() provides identical behavior, with
-     * abs() defined in stdlib.h and fabs() defined in cmath.
-     *
-     * @param a Variable input.
-     * @return Absolute value of variable.
-     */
-    inline var abs(const var& a) {   
-      // cut-and-paste from fabs()
-      if (a.val() > 0.0)
-        return a;
-      if (a.val() < 0.0)
-        return var(new neg_vari(a.vi_));
-      // FIXME:  same as fabs() -- is this right?
-      return var(new vari(0.0));
-    }
-
-
     /**
      * Recover memory used for all variables for reuse.
      */
