f14e45bdb2211e3d1eb35d21e3044023a8c0e211
Daniel Lee <bearlee@alum.mit.edu>
2013-03-26 16:29:53 -0400

    moving operator-- out of stan/agrad.hpp

--- src/stan/agrad/agrad.hpp
+++ src/stan/agrad/agrad.hpp
@@ -54,15 +54,6 @@ namespace stan {
     namespace {
 
 
-      class decrement_vari : public op_v_vari {
-      public:
-        decrement_vari(vari* avi) :
-          op_v_vari(avi->val_ - 1.0, avi) {
-        }
-        void chain() {
-          avi_->adj_ += adj_;
-        }
-      };
 
 
 
@@ -342,40 +333,8 @@ namespace stan {
       return temp;
     }
 
-    /**
-     * Prefix decrement operator for variables (C++).  
-     *
-     * Following C++, <code>(--a)</code> is defined to behave exactly as 
-     *
-     * <code>a = a - 1.0)</code>
-     *
-     * does, but is faster and uses less memory.  In particular,
-     * the result is an assignable lvalue.
-     *
-     * @param a Variable to decrement.
-     * @return Reference the result of decrementing this input variable.
-     */
-    inline var& operator--(var& a) {
-      a.vi_ = new decrement_vari(a.vi_);
-      return a;
-    }
 
-    /**
-     * Postfix decrement operator for variables (C++).  
-     * 
-     * Following C++, the expression <code>(a--)</code> is defined to
-     * behave like the sequence of operations
-     *
-     * <code>var temp = a;  a = a - 1.0;  return temp;</code>
-     *
-     * @param a Variable to decrement.
-     * @return Input variable. 
-     */
-    inline var operator--(var& a, int /*dummy*/) {
-      var temp(a);
-      a.vi_ = new decrement_vari(a.vi_);
-      return temp;
-    }
+
 
     // CMATH EXP AND LOG
   
