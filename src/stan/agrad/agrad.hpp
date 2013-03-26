1e355c93e5aa4cd1f7f12643607bae2e018adc82
Daniel Lee <bearlee@alum.mit.edu>
2013-03-26 17:26:53 -0400

    moving asin out of stan/agrad.hpp

--- src/stan/agrad/agrad.hpp
+++ src/stan/agrad/agrad.hpp
@@ -74,29 +74,6 @@ namespace stan {
 
     namespace {
 
-
-
-
-      class acos_vari : public op_v_vari {
-      public:
-        acos_vari(vari* avi) :
-          op_v_vari(std::acos(avi->val_),avi) {
-        }
-        void chain() {
-          avi_->adj_ -= adj_ / std::sqrt(1.0 - (avi_->val_ * avi_->val_));
-        }
-      };
-
-      class asin_vari : public op_v_vari {
-      public:
-        asin_vari(vari* avi) :
-          op_v_vari(std::asin(avi->val_),avi) {
-        }
-        void chain() {
-          avi_->adj_ += adj_ / std::sqrt(1.0 - (avi_->val_ * avi_->val_));
-        }
-      };
-
       class atan_vari : public op_v_vari {
       public:
         atan_vari(vari* avi) :
@@ -221,39 +198,6 @@ namespace stan {
     }
 
     // TRIG FUNCTIONS
-
-
-
-
-    /**
-     * Return the principal value of the arc cosine of a variable,
-     * in radians (cmath).
-     *
-     * The derivative is defined by
-     *
-     * \f$\frac{d}{dx} \arccos x = \frac{-1}{\sqrt{1 - x^2}}\f$.
-     *
-     * @param a Variable in range [-1,1].
-     * @return Arc cosine of variable, in radians. 
-     */
-    inline var acos(const var& a) {
-      return var(new acos_vari(a.vi_));
-    }
-
-    /**
-     * Return the principal value of the arc sine, in radians, of the
-     * specified variable (cmath).
-     *
-     * The derivative is defined by
-     *
-     * \f$\frac{d}{dx} \arcsin x = \frac{1}{\sqrt{1 - x^2}}\f$.
-     *
-     * @param a Variable in range [-1,1].
-     * @return Arc sine of variable, in radians. 
-     */
-    inline var asin(const var& a) {
-      return var(new asin_vari(a.vi_));
-    }
 
     /**
      * Return the principal value of the arc tangent, in radians, of the
