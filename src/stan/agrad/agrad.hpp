6cfff14ab38c3ee7a156f02c0f9c729dd3101b99
Daniel Lee <bearlee@alum.mit.edu>
2013-03-26 17:22:58 -0400

    moving cos out of stan/agrad.hpp

--- src/stan/agrad/agrad.hpp
+++ src/stan/agrad/agrad.hpp
@@ -74,15 +74,6 @@ namespace stan {
 
     namespace {
 
-      class cos_vari : public op_v_vari {
-      public:
-        cos_vari(vari* avi) :
-          op_v_vari(std::cos(avi->val_),avi) {
-        }
-        void chain() {
-          avi_->adj_ -= adj_ * std::sin(avi_->val_);
-        }
-      };
 
       class sin_vari : public op_v_vari {
       public:
@@ -247,26 +238,7 @@ namespace stan {
       };
     }
 
-    // POWER FUNCTIONS
-
-
-
-
     // TRIG FUNCTIONS
-
-    /**
-     * Return the cosine of a radian-scaled variable (cmath).
-     *
-     * The derivative is defined by
-     *
-     * \f$\frac{d}{dx} \cos x = - \sin x\f$.
-     *
-     * @param a Variable for radians of angle.
-     * @return Cosine of variable. 
-     */
-    inline var cos(const var& a) {
-      return var(new cos_vari(a.vi_));
-    }
 
     /**
      * Return the sine of a radian-scaled variable (cmath).
