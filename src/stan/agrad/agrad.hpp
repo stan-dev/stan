d27ccbc3b91d3a3be765f338c042f3b9e2c66292
Daniel Lee <bearlee@alum.mit.edu>
2013-03-26 17:20:35 -0400

    moving sqrt out of stan/agrad.hpp

--- src/stan/agrad/agrad.hpp
+++ src/stan/agrad/agrad.hpp
@@ -74,15 +74,6 @@ namespace stan {
 
     namespace {
 
-      class sqrt_vari : public op_v_vari {
-      public:
-        sqrt_vari(vari* avi) :
-          op_v_vari(std::sqrt(avi->val_),avi) {
-        }
-        void chain() {
-          avi_->adj_ += adj_ / (2.0 * val_);
-        }
-      };
 
       class pow_vv_vari : public op_vv_vari {
       public:
@@ -291,29 +282,8 @@ namespace stan {
       };
     }
 
-
-    // CMATH EXP AND LOG
-  
-
-
-
-
-
     // POWER FUNCTIONS
 
-    /**
-     * Return the square root of the specified variable (cmath).
-     *
-     * The derivative is defined by
-     *
-     * \f$\frac{d}{dx} \sqrt{x} = \frac{1}{2 \sqrt{x}}\f$.
-     * 
-     * @param a Variable whose square root is taken.
-     * @return Square root of variable.
-     */
-    inline var sqrt(const var& a) {
-      return var(new sqrt_vari(a.vi_));
-    }
 
     /**
      * Return the base raised to the power of the exponent (cmath).
