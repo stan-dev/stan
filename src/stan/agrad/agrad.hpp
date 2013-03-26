6fd9130fb9bf05c3d14b6555603fab3c7fcc45b2
Daniel Lee <bearlee@alum.mit.edu>
2013-03-26 17:25:08 -0400

    moving tan out of stan/agrad.hpp

--- src/stan/agrad/agrad.hpp
+++ src/stan/agrad/agrad.hpp
@@ -76,15 +76,6 @@ namespace stan {
 
 
 
-      class tan_vari : public op_v_vari {
-      public:
-        tan_vari(vari* avi) :
-          op_v_vari(std::tan(avi->val_),avi) {
-        }
-        void chain() {
-          avi_->adj_ += adj_ * (1.0 + val_ * val_); 
-        }
-      };
 
       class acos_vari : public op_v_vari {
       public:
@@ -232,19 +223,7 @@ namespace stan {
     // TRIG FUNCTIONS
 
 
-    /**
-     * Return the tangent of a radian-scaled variable (cmath).
-     *
-     * The derivative is defined by
-     *
-     * \f$\frac{d}{dx} \tan x = \sec^2 x\f$.
-     *
-     * @param a Variable for radians of angle.
-     * @return Tangent of variable. 
-     */
-    inline var tan(const var& a) {
-      return var(new tan_vari(a.vi_));
-    }
+
 
     /**
      * Return the principal value of the arc cosine of a variable,
