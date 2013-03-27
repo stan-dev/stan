0c0208c57a5785754691ce264977eb8407593f5b
Daniel Lee <bearlee@alum.mit.edu>
2013-03-27 00:26:20 -0400

    splitting sinh tests out of test/agrad/agrad_test.cpp

--- src/test/agrad/rev/sinh_test.cpp
+++ src/test/agrad/rev/sinh_test.cpp
@@ -2,3 +2,13 @@
 #include <test/agrad/util.hpp>
 #include <gtest/gtest.h>
 
+TEST(AgradRev,sinh_var) {
+  AVAR a = 0.68;
+  AVAR f = sinh(a);
+  EXPECT_FLOAT_EQ(sinh(0.68), f.val());
+
+  AVEC x = createAVEC(a);
+  VEC g;
+  f.grad(x,g);
+  EXPECT_FLOAT_EQ(cosh(0.68),g[0]);
+}
