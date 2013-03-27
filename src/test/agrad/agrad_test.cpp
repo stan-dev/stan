ca91c2318d1cc988fc68196e82c12048ffb5c4fe
Daniel Lee <bearlee@alum.mit.edu>
2013-03-27 00:12:31 -0400

    splitting operator+ tests out of test/agrad/agrad_test.cpp

--- src/test/agrad/agrad_test.cpp
+++ src/test/agrad/agrad_test.cpp
@@ -2,47 +2,6 @@
 #include <stan/agrad/agrad.hpp>
 #include <test/agrad/util.hpp>
 
-TEST(AgradRev,a_eq_b) {
-  AVAR a = 2.0;
-  AVAR b = 2.0;
-  EXPECT_TRUE(a == b);
-  EXPECT_TRUE(b == a);
-  AVAR c = 3.0;
-  EXPECT_FALSE(a == c);
-  EXPECT_FALSE(c == a);
-}
-
-TEST(AgradRev,x_eq_b) {
-  double x = 2.0;
-  AVAR b = 2.0;
-  EXPECT_TRUE(x == b);
-  EXPECT_TRUE(b == x);
-  AVAR c = 3.0;
-  EXPECT_FALSE(x == c);
-  EXPECT_FALSE(c == x);
-}
-
-TEST(AgradRev,a_eq_y) {
-  AVAR a = 2.0;
-  double y = 2.0;
-  EXPECT_TRUE(a == y);
-  EXPECT_TRUE(y == a);
-  double z = 3.0;
-  EXPECT_FALSE(a == z);
-  EXPECT_FALSE(z == a);
-}
-
-
-TEST(AgradRev,a_neq_y) {
-  AVAR a = 2.0;
-  double y = 3.0;
-  EXPECT_TRUE(a != y);
-  EXPECT_TRUE(y != a);
-  double z = 2.0;
-  EXPECT_FALSE(a != z);
-  EXPECT_FALSE(z != a);
-}
-
 TEST(AgradRev,pos_a) {
   AVAR a = 5.0;
   AVAR f = +a;
--- src/test/agrad/rev/operator_unary_plus_test.cpp
+++ src/test/agrad/rev/operator_unary_plus_test.cpp
@@ -2,3 +2,13 @@
 #include <test/agrad/util.hpp>
 #include <gtest/gtest.h>
 
+TEST(AgradRev,pos_a) {
+  AVAR a = 5.0;
+  AVAR f = +a;
+  EXPECT_FLOAT_EQ(5.0,f.val());
+  EXPECT_TRUE(a == +a);
+  AVEC x = createAVEC(a);
+  VEC dx;
+  f.grad(x,dx);
+  EXPECT_FLOAT_EQ(1.0,dx[0]);
+}
