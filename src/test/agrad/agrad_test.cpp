db01553f4a62cc7d66502d8fe5715a7c576458a2
Daniel Lee <bearlee@alum.mit.edu>
2013-03-27 00:24:46 -0400

    splitting atan tests out of test/agrad/agrad_test.cpp

--- src/test/agrad/agrad_test.cpp
+++ src/test/agrad/agrad_test.cpp
@@ -2,16 +2,6 @@
 #include <stan/agrad/agrad.hpp>
 #include <test/agrad/util.hpp>
 
-TEST(AgradRev,atan_var) {
-  AVAR a = 0.68;
-  AVAR f = atan(a);
-  EXPECT_FLOAT_EQ(atan(0.68), f.val());
-
-  AVEC x = createAVEC(a);
-  VEC g;
-  f.grad(x,g);
-  EXPECT_FLOAT_EQ(1.0/(1.0 + (0.68 * 0.68)), g[0]);
-}
 
 TEST(AgradRev,atan2_var_var) {
   AVAR a = 1.2;
