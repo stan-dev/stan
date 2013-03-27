a7f0874a1180362b3e0a0906c358f3a2deb34a47
Daniel Lee <bearlee@alum.mit.edu>
2013-03-27 00:11:08 -0400

    splitting operator>= tests out of test/agrad/agrad_test.cpp

--- src/test/agrad/agrad_test.cpp
+++ src/test/agrad/agrad_test.cpp
@@ -3,67 +3,8 @@
 #include <test/agrad/util.hpp>
 
 
-TEST(AgradRev,a_lte_b) {
-  AVAR a = 5.0;
-  AVAR b = 6.0;
-  EXPECT_TRUE(a <= b);
-  EXPECT_FALSE(b <= a);
-  AVAR c = 6.0;
-  EXPECT_TRUE(b <= c);
-  EXPECT_TRUE(c <= b);
-}
-
-TEST(AgradRev,a_lte_y) {
-  AVAR a = 5.0;
-  double y = 6.0;
-  EXPECT_TRUE(a <= y);
-  EXPECT_FALSE(y <= a);
-  AVAR c = 5.0;
-  EXPECT_TRUE(a <= c);
-  EXPECT_TRUE(c <= a);
-}
 
 
-TEST(AgradRev,x_lte_b) {
-  double x = 5.0;
-  AVAR b = 6.0;
-  EXPECT_TRUE(x <= b);
-  EXPECT_FALSE(b <= x);
-  double y = 6.0;
-  EXPECT_TRUE(b <= y);
-  EXPECT_TRUE(y <= b);
-}
-
-
-TEST(AgradRev,a_gte_b) {
-  AVAR a = 5.0;
-  AVAR b = 6.0;
-  EXPECT_TRUE(b >= a);
-  EXPECT_FALSE(a >= b);
-  AVAR c = 6.0;
-  EXPECT_TRUE(b >= c);
-  EXPECT_TRUE(c >= b);
-}
-
-TEST(AgradRev,a_gte_y) {
-  AVAR a = 6.0;
-  double y = 5.0;
-  EXPECT_TRUE(a >= y);
-  EXPECT_FALSE(y >= a);
-  AVAR c = 6.0;
-  EXPECT_TRUE(a >= c);
-  EXPECT_TRUE(c >= a);
-}
-
-TEST(AgradRev,x_gte_b) {
-  double x = 6.0;
-  AVAR b = 5.0;
-  EXPECT_TRUE(x >= b);
-  EXPECT_FALSE(b >= x);
-  double y = 5.0;
-  EXPECT_TRUE(b >= y);
-  EXPECT_TRUE(y >= b);
-}
 
 TEST(AgradRev,a_eq_b) {
   AVAR a = 2.0;
