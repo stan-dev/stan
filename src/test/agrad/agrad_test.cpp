2303b0f5fdde467cdffcb6a2b0fb1a2bac881cc8
Daniel Lee <bearlee@alum.mit.edu>
2013-03-27 00:17:33 -0400

    splitting operator+= tests out of test/agrad/agrad_test.cpp

--- src/test/agrad/agrad_test.cpp
+++ src/test/agrad/agrad_test.cpp
@@ -2,37 +2,6 @@
 #include <stan/agrad/agrad.hpp>
 #include <test/agrad/util.hpp>
 
-
-
-
-
-
-TEST(AgradRev,a_pluseq_b) {
-  AVAR a(5.0);
-  AVAR b(-1.0);
-  AVEC x = createAVEC(a,b);
-  AVAR f = (a += b);
-  EXPECT_FLOAT_EQ(4.0,f.val());
-  EXPECT_FLOAT_EQ(4.0,a.val());
-  EXPECT_FLOAT_EQ(-1.0,b.val());
-  VEC g;
-  f.grad(x,g);
-  EXPECT_FLOAT_EQ(1.0,g[0]);
-  EXPECT_FLOAT_EQ(1.0,g[1]);
-}
-
-TEST(AgradRev,a_pluseq_bd) {
-  AVAR a(5.0);
-  double b = -1.0;
-  AVEC x = createAVEC(a);
-  AVAR f = (a += b);
-  EXPECT_FLOAT_EQ(4.0,f.val());
-  EXPECT_FLOAT_EQ(4.0,a.val());
-  VEC g;
-  f.grad(x,g);
-  EXPECT_FLOAT_EQ(1.0,g[0]);
-}
-
 TEST(AgradRev,a_minuseq_b) {
   AVAR a(5.0);
   AVAR b(-1.0);
