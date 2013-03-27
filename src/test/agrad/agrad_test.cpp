837be2aeef3f2131c343a6fd2164c9c717ae66b1
Daniel Lee <bearlee@alum.mit.edu>
2013-03-26 22:17:01 -0400

    updating test/agrad

--- src/test/agrad/agrad_test.cpp
+++ src/test/agrad/agrad_test.cpp
@@ -1,28 +1,6 @@
 #include <gtest/gtest.h>
 #include <stan/agrad/agrad.hpp>
-
-typedef stan::agrad::var AVAR;
-typedef std::vector<AVAR> AVEC;
-typedef std::vector<double> VEC;
-
-AVEC createAVEC(AVAR x) {
-  AVEC v;
-  v.push_back(x);
-  return v;
-}
-AVEC createAVEC(AVAR x1, AVAR x2) {
-  AVEC v;
-  v.push_back(x1);
-  v.push_back(x2);
-  return v;
-}
-AVEC createAVEC(AVAR x1, AVAR x2, AVAR x3) {
-  AVEC v;
-  v.push_back(x1);
-  v.push_back(x2);
-  v.push_back(x3);
-  return v;
-}
+#include <test/agrad/util.hpp>
 
 TEST(AgradAgrad,undefined) {
   AVAR a;
---
+++ src/test/agrad/util.hpp
@@ -0,0 +1,22 @@
+typedef stan::agrad::var AVAR;
+typedef std::vector<AVAR> AVEC;
+typedef std::vector<double> VEC;
+
+AVEC createAVEC(AVAR x) {
+  AVEC v;
+  v.push_back(x);
+  return v;
+}
+AVEC createAVEC(AVAR x1, AVAR x2) {
+  AVEC v;
+  v.push_back(x1);
+  v.push_back(x2);
+  return v;
+}
+AVEC createAVEC(AVAR x1, AVAR x2, AVAR x3) {
+  AVEC v;
+  v.push_back(x1);
+  v.push_back(x2);
+  v.push_back(x3);
+  return v;
+}
