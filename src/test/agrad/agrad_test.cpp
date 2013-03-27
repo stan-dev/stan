ee18bf75875ea6a7ad255757ee1e59f8882fa75c
Daniel Lee <bearlee@alum.mit.edu>
2013-03-27 00:10:08 -0400

    splitting operator> tests out of test/agrad/agrad_test.cpp

--- src/test/agrad/agrad_test.cpp
+++ src/test/agrad/agrad_test.cpp
@@ -2,35 +2,6 @@
 #include <stan/agrad/agrad.hpp>
 #include <test/agrad/util.hpp>
 
-TEST(AgradRev,a_gt_b) {
-  AVAR a = 5.0;
-  AVAR b = 6.0;
-  EXPECT_TRUE(b > a);
-  EXPECT_FALSE(a > b);
-  AVAR c = 6.0;
-  EXPECT_FALSE(b > c);
-  EXPECT_FALSE(c > b);
-}
-
-TEST(AgradRev,a_gt_y) {
-  AVAR a = 6.0;
-  double y = 5.0;
-  EXPECT_TRUE(a > y);
-  EXPECT_FALSE(y > a);
-  AVAR c = 6.0;
-  EXPECT_FALSE(a > c);
-  EXPECT_FALSE(c > a);
-}
-
-TEST(AgradRev,x_gt_b) {
-  double x = 6.0;
-  AVAR b = 5.0;
-  EXPECT_TRUE(x > b);
-  EXPECT_FALSE(b > x);
-  double y = 5.0;
-  EXPECT_FALSE(b > y);
-  EXPECT_FALSE(y > b);
-}
 
 TEST(AgradRev,a_lte_b) {
   AVAR a = 5.0;
