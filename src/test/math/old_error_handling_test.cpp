3fb7d800a724a23eef38649dc4bd1de31a3a2c87
Daniel Lee <bearlee@alum.mit.edu>
2013-03-27 17:14:07 -0400

    splitting out check_not_nan tests out of test/math/old_error_handling_test.cpp

--- src/test/math/old_error_handling_test.cpp
+++ src/test/math/old_error_handling_test.cpp
@@ -6,55 +6,6 @@ typedef boost::math::policies::policy<
   > errno_policy;
 using namespace stan::math;
 
-//---------- check_not_nan tests ----------
-TEST(MathErrorHandling,CheckNotNanDefaultPolicy) {
-  const char* function = "check_not_nan(%1%)";
-  double x = 0;
-  double result;
- 
-  EXPECT_TRUE(check_not_nan(function, x, "x", &result, default_policy())) << "check_not_nan should be true with finite x: " << x;
-  x = std::numeric_limits<double>::infinity();
-  EXPECT_TRUE(check_not_nan(function, x, "x", &result, default_policy())) << "check_not_nan should be true with x = Inf: " << x;
-  x = -std::numeric_limits<double>::infinity();
-  EXPECT_TRUE(check_not_nan(function, x, "x", &result, default_policy())) << "check_not_nan should be true with x = -Inf: " << x;
-
-  x = std::numeric_limits<double>::quiet_NaN();
-  EXPECT_THROW(check_not_nan(function, x, "x", &result, default_policy()), std::domain_error) << "check_not_nan should throw exception on NaN: " << x;
-}
-
-TEST(MathErrorHandling,CheckNotNanErrnoPolicy) {
-  const char* function = "check_not_nan(%1%)";
-  double x = 0;
-  double result = 0;
-
-  EXPECT_TRUE(check_not_nan(function, x, "x", &result, errno_policy())) << "check_not_nan should be true with finite x: " << x;
-  x = std::numeric_limits<double>::infinity();
-  EXPECT_TRUE(check_not_nan(function, x, "x", &result, errno_policy())) << "check_not_nan should be true with x = Inf: " << x;
-  EXPECT_FALSE(std::isnan (result)) << "check_not_nan should not have returned nan: " << x;
-
-  x = -std::numeric_limits<double>::infinity();
-  EXPECT_TRUE(check_not_nan (function, x, "x", &result, errno_policy())) << "check_not_nan should be true with x = -Inf: " << x;
-  EXPECT_FALSE(std::isnan (result)) << "check_not_nan should have returned nan: " << x;
- 
-  x = std::numeric_limits<double>::quiet_NaN();
-  EXPECT_FALSE(check_not_nan (function, x, "x", &result, errno_policy())) << "check_not_nan should return FALSE on nan: " << x;
-  EXPECT_TRUE(std::isnan (result)) << "check_not_nan should have returned nan: " << x;
-}
-
-TEST(MathErrorHandling,CheckNotNanDefaultPolicyDefaultResult) {
-  const char* function = "check_not_nan(%1%)";
-  double x = 0;
- 
-  EXPECT_TRUE(check_not_nan(function, x, "x")) << "check_not_nan should be true with finite x: " << x;
-  x = std::numeric_limits<double>::infinity();
-  EXPECT_TRUE(check_not_nan(function, x, "x")) << "check_not_nan should be true with x = Inf: " << x;
-  x = -std::numeric_limits<double>::infinity();
-  EXPECT_TRUE(check_not_nan(function, x, "x")) << "check_not_nan should be true with x = -Inf: " << x;
-
-  x = std::numeric_limits<double>::quiet_NaN();
-  EXPECT_THROW(check_not_nan(function, x, "x"), std::domain_error) << "check_not_nan should throw exception on NaN: " << x;
-}
-
 //---------- check_finite tests ----------
 TEST(MathErrorHandling,CheckFiniteDefaultPolicy) {
   const char* function = "check_finite (%1%)";
