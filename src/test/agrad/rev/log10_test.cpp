ad422a9396b70e8fe5a395954d9be0081f2b7603
Daniel Lee <bearlee@alum.mit.edu>
2013-03-27 00:21:50 -0400

    splitting log10 tests out of test/agrad/agrad_test.cpp

--- src/test/agrad/rev/log10_test.cpp
+++ src/test/agrad/rev/log10_test.cpp
@@ -2,3 +2,12 @@
 #include <test/agrad/util.hpp>
 #include <gtest/gtest.h>
 
+TEST(AgradRev,log10_a) {
+  AVAR a(5.0);
+  AVAR f = log10(a); 
+  EXPECT_FLOAT_EQ(log10(5.0),f.val());
+  AVEC x = createAVEC(a);
+  VEC g;
+  f.grad(x,g);
+  EXPECT_FLOAT_EQ(1.0/(log(10.0) * 5.0),g[0]);
+}
