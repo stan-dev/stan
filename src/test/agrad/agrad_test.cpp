27876b692c072d239bef44478beec7c9421953c8
Daniel Lee <bearlee@alum.mit.edu>
2013-03-27 00:24:05 -0400

    splitting acos tests out of test/agrad/agrad_test.cpp

--- src/test/agrad/agrad_test.cpp
+++ src/test/agrad/agrad_test.cpp
@@ -4,27 +4,7 @@
 
 
 
-TEST(AgradRev,tan_var) {
-  AVAR a = 0.68;
-  AVAR f = tan(a);
-  EXPECT_FLOAT_EQ(tan(0.68), f.val());
 
-  AVEC x = createAVEC(a);
-  VEC g;
-  f.grad(x,g);
-  EXPECT_FLOAT_EQ(1 + tan(0.68)*tan(0.68), g[0]);
-}
-
-TEST(AgradRev,acos_var) {
-  AVAR a = 0.68;
-  AVAR f = acos(a);
-  EXPECT_FLOAT_EQ(acos(0.68), f.val());
-
-  AVEC x = createAVEC(a);
-  VEC g;
-  f.grad(x,g);
-  EXPECT_FLOAT_EQ(-1.0/sqrt(1.0 - (0.68 * 0.68)), g[0]);
-}
 
 TEST(AgradRev,asin_var) {
   AVAR a = 0.68;
