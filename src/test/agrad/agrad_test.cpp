5b4bc8c26f4e081a75c0f345fe9fbb34752a26c6
Daniel Lee <bearlee@alum.mit.edu>
2013-03-27 00:13:29 -0400

    splitting operator+ tests out of test/agrad/agrad_test.cpp

--- src/test/agrad/agrad_test.cpp
+++ src/test/agrad/agrad_test.cpp
@@ -2,66 +2,6 @@
 #include <stan/agrad/agrad.hpp>
 #include <test/agrad/util.hpp>
 
-
-
-TEST(AgradRev,a_plus_b) {
-  AVAR a = 5.0;
-  AVAR b = -1.0;
-  AVAR f = a + b;
-  EXPECT_FLOAT_EQ(4.0,f.val());
-  AVEC x = createAVEC(a,b);
-  VEC dx;
-  f.grad(x,dx);
-  EXPECT_FLOAT_EQ(1.0,dx[0]);
-  EXPECT_FLOAT_EQ(1.0,dx[1]);
-}
-
-TEST(AgradRev,a_plus_a) {
-  AVAR a = 5.0;
-  AVAR f = a + a;
-  EXPECT_FLOAT_EQ(10.0,f.val());
-  AVEC x = createAVEC(a);
-  VEC dx;
-  f.grad(x,dx);
-  EXPECT_FLOAT_EQ(2.0,dx[0]);
-}
-
-TEST(AgradRev,a_plus_neg_b) {
-  AVAR a = 5.0;
-  AVAR b = -1.0;
-  AVAR f = a + -b;
-  EXPECT_FLOAT_EQ(6.0,f.val());
-  AVEC x = createAVEC(a,b);
-  VEC dx;
-  f.grad(x,dx);
-  EXPECT_FLOAT_EQ(1.0,dx[0]);
-  EXPECT_FLOAT_EQ(-1.0,dx[1]);
-}
-
-TEST(AgradRev,a_plus_x) {
-  AVAR a = 5.0;
-  double z = 3.0;
-  AVAR f = a + z;
-  EXPECT_FLOAT_EQ(8.0,f.val());
-  AVEC x = createAVEC(a);
-  VEC dx;
-  f.grad(x,dx);
-  EXPECT_FLOAT_EQ(1.0,dx[0]);
-}
-
-TEST(AgradRev,x_plus_a) {
-  AVAR a = 5.0;
-  double z = 3.0;
-  AVAR f = z + a;
-  EXPECT_FLOAT_EQ(8.0,f.val());
-  AVEC x = createAVEC(a);
-  VEC dx;
-  f.grad(x,dx);
-  EXPECT_FLOAT_EQ(1.0,dx[0]);
-}
-
-
-
 TEST(AgradRev,a_minus_b) {
   AVAR a = 5.0;
   AVAR b = 2.0;
