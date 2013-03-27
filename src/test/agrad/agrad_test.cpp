a619b446a217670c6e0f63eec5e039123ab5ebce
Daniel Lee <bearlee@alum.mit.edu>
2013-03-27 00:26:46 -0400

    splitting tanh tests out of test/agrad/agrad_test.cpp

--- src/test/agrad/agrad_test.cpp
+++ src/test/agrad/agrad_test.cpp
@@ -4,38 +4,6 @@
 
 
 
-TEST(AgradRev,cosh_var) {
-  AVAR a = 0.68;
-  AVAR f = cosh(a);
-  EXPECT_FLOAT_EQ(cosh(0.68), f.val());
-
-  AVEC x = createAVEC(a);
-  VEC g;
-  f.grad(x,g);
-  EXPECT_FLOAT_EQ(sinh(0.68), g[0]);
-}
-
-TEST(AgradRev,sinh_var) {
-  AVAR a = 0.68;
-  AVAR f = sinh(a);
-  EXPECT_FLOAT_EQ(sinh(0.68), f.val());
-
-  AVEC x = createAVEC(a);
-  VEC g;
-  f.grad(x,g);
-  EXPECT_FLOAT_EQ(cosh(0.68),g[0]);
-}
-
-TEST(AgradRev,tanh_var) {
-  AVAR a = 0.68;
-  AVAR f = tanh(a);
-  EXPECT_FLOAT_EQ(tanh(0.68), f.val());
-
-  AVEC x = createAVEC(a);
-  VEC g;
-  f.grad(x,g);
-  EXPECT_FLOAT_EQ(1.0/(cosh(0.68) * cosh(0.68)), g[0]);
-}
 
 TEST(AgradRev,fabs_var) {
   AVAR a = 0.68;
