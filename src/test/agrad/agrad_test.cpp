7bc15a8dfd94a0bfc38047de84509b815e16f661
Daniel Lee <bearlee@alum.mit.edu>
2013-03-27 00:22:43 -0400

    splitting pow tests out of test/agrad/agrad_test.cpp

--- src/test/agrad/agrad_test.cpp
+++ src/test/agrad/agrad_test.cpp
@@ -2,67 +2,6 @@
 #include <stan/agrad/agrad.hpp>
 #include <test/agrad/util.hpp>
 
-
-
-
-TEST(AgradRev,log10_a) {
-  AVAR a(5.0);
-  AVAR f = log10(a); 
-  EXPECT_FLOAT_EQ(log10(5.0),f.val());
-  AVEC x = createAVEC(a);
-  VEC g;
-  f.grad(x,g);
-  EXPECT_FLOAT_EQ(1.0/(log(10.0) * 5.0),g[0]);
-}
-
-TEST(AgradRev,sqrt_a) {
-  AVAR a(5.0);
-  AVAR f = sqrt(a); 
-  EXPECT_FLOAT_EQ(sqrt(5.0),f.val());
-  AVEC x = createAVEC(a);
-  VEC g;
-  f.grad(x,g);
-  EXPECT_FLOAT_EQ((1.0/2.0) * pow(5.0,-0.5), g[0]);
-}
-
-TEST(AgradRev,pow_var_var) {
-  AVAR a(3.0);
-  AVAR b(4.0);
-  AVAR f = pow(a,b);
-  EXPECT_FLOAT_EQ(81.0,f.val());
-
-  AVEC x = createAVEC(a,b);
-  VEC g;
-  f.grad(x,g);
-  EXPECT_FLOAT_EQ(4.0 * pow(3.0,4.0-1.0), g[0]);
-  EXPECT_FLOAT_EQ(log(3.0) * pow(3.0,4.0), g[1]);
-}
-
-TEST(AgradRev,pow_var_double) {
-  AVAR a(3.0);
-  double b = 4.0;
-  AVAR f = pow(a,b);
-  EXPECT_FLOAT_EQ(81.0,f.val());
-
-  AVEC x = createAVEC(a);
-  VEC g;
-  f.grad(x,g);
-  EXPECT_FLOAT_EQ(4.0 * pow(3.0,4.0-1.0), g[0]);
-}
-
-
-TEST(AgradRev,pow_double_var) {
-  double a = 3.0;
-  AVAR b(4.0);
-  AVAR f = pow(a,b);
-  EXPECT_FLOAT_EQ(81.0,f.val());
-
-  AVEC x = createAVEC(b);
-  VEC g;
-  f.grad(x,g);
-  EXPECT_FLOAT_EQ(log(3.0) * pow(3.0,4.0), g[0]);
-}
-
 TEST(AgradRev,cos_var) {
   AVAR a = 0.43;
   AVAR f = cos(a);
