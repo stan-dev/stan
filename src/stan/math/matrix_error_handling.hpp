d1d63096968a5ee470b99cc1e1dbc0ef74a50e27
Daniel Lee <bearlee@alum.mit.edu>
2013-03-27 15:03:49 -0400

    moving check_corr_matrix out of stan/math/matrix_error_handling.hpp

--- src/stan/math/matrix_error_handling.hpp
+++ src/stan/math/matrix_error_handling.hpp
@@ -6,6 +6,7 @@
 #include <stan/math/error_handling/matrix/check_symmetric.hpp>
 #include <stan/math/error_handling/matrix/check_pos_definite.hpp>
 #include <stan/math/error_handling/matrix/check_cov_matrix.hpp>
+#include <stan/math/error_handling/matrix/check_corr_matrix.hpp>
 
 #include <sstream>
 #include <stan/math/matrix.hpp>
@@ -32,77 +33,6 @@ namespace stan {
       }
     }
 
-
-
-
-    /**
-     * Return <code>true</code> if the specified matrix is a valid
-     * correlation matrix.  A valid correlation matrix is symmetric,
-     * has a unit diagonal (all 1 values), and has all values between
-     * -1 and 1 (inclussive).  
-     *
-     * @param function 
-     * @param y Matrix to test.
-     * @param name 
-     * @param result 
-     * 
-     * @return <code>true</code> if the specified matrix is a valid
-     * correlation matrix.
-     * @tparam T Type of scalar.
-     */
-    // FIXME: update warnings
-    template <typename T_y, typename T_result, class Policy>
-    inline bool check_corr_matrix(const char* function,
-                  const Eigen::Matrix<T_y,Eigen::Dynamic,Eigen::Dynamic>& y,
-                  const char* name,
-                  T_result* result,
-                  const Policy&) {
-      if (!check_size_match(function, 
-          y.rows(), "Rows of correlation matrix",
-          y.cols(), "columns of correlation matrix",
-          result, Policy())) 
-        return false;
-      if (!check_positive(function, y.rows(), "rows", result, Policy()))
-        return false;
-      if (!check_symmetric(function, y, "y", result, Policy()))
-        return false;
-      for (typename Eigen::Matrix<T_y,Eigen::Dynamic,Eigen::Dynamic>::size_type
-             k = 0; k < y.rows(); ++k) {
-        if (fabs(y(k,k) - 1.0) > CONSTRAINT_TOLERANCE) {
-          std::ostringstream message;
-          message << name << " is not a valid correlation matrix. " 
-                  << name << "(" << k << "," << k 
-                  << ") is %1%, but should be near 1.0";
-          T_result tmp 
-            = policies::raise_domain_error<T_y>(function,
-                                                message.str().c_str(),
-                                                y(k,k), Policy());
-          if (result != 0)
-            *result = tmp;
-          return false;
-        }
-      }
-      if (!check_pos_definite(function, y, "y", result, Policy()))
-        return false;
-      return true;
-    }
-
-
-    template <typename T_y, typename T_result>
-    inline bool check_corr_matrix(const char* function,
-                  const Eigen::Matrix<T_y,Eigen::Dynamic,Eigen::Dynamic>& y,
-                  const char* name,
-                  T_result* result) {
-      return check_corr_matrix(function,y,name,result,default_policy());
-    }
-
-    template <typename T>
-    inline bool check_corr_matrix(const char* function,
-                  const Eigen::Matrix<T,Eigen::Dynamic,Eigen::Dynamic>& y,
-                  const char* name,
-                  T* result = 0) {
-      return check_corr_matrix(function,y,name,result,default_policy());
-    }
 
     /**
      * Return <code>true</code> if the specified matrix is a valid
