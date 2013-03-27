5ab7df78875ffc1226753d35afd0b65d15057520
Daniel Lee <bearlee@alum.mit.edu>
2013-03-27 00:07:11 -0400

    splitting var tests out of test/agrad/agrad_test.cpp

--- src/test/agrad/agrad_test.cpp
+++ src/test/agrad/agrad_test.cpp
@@ -2,23 +2,6 @@
 #include <stan/agrad/agrad.hpp>
 #include <test/agrad/util.hpp>
 
-
-TEST(AgradRev,a_eq_x) {
-  AVAR a = 5.0;
-  EXPECT_FLOAT_EQ(5.0,a.val());
-}
-
-TEST(AgradRev,a_of_x) {
-  AVAR a(6.0);
-  EXPECT_FLOAT_EQ(6.0,a.val());
-}
-
-TEST(AgradRev,a__a_eq_x) {
-  AVAR a;
-  a = 7.0;
-  EXPECT_FLOAT_EQ(7.0,a.val());
-}
-
 TEST(AgradRev,a) {
   AVAR a = 5.0;
   AVEC x = createAVEC(a);
--- src/test/agrad/rev/var_test.cpp
+++ src/test/agrad/rev/var_test.cpp
@@ -1,4 +1,18 @@
 #include <stan/agrad/rev/var.hpp>
-#include <test/agrad/util.hpp>
 #include <gtest/gtest.h>
 
+TEST(AgradRev,a_eq_x) {
+  stan::agrad::var a = 5.0;
+  EXPECT_FLOAT_EQ(5.0,a.val());
+}
+
+TEST(AgradRev,a_of_x) {
+  stan::agrad::var a(6.0);
+  EXPECT_FLOAT_EQ(6.0,a.val());
+}
+
+TEST(AgradRev,a__a_eq_x) {
+  stan::agrad::var a;
+  a = 7.0;
+  EXPECT_FLOAT_EQ(7.0,a.val());
+}
