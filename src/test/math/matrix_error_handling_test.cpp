5790e81a992c5deef2afc70390cc9dead8b10443
Daniel Lee <bearlee@alum.mit.edu>
2013-03-27 20:50:39 -0400

    removing unused test

--- src/test/math/matrix_error_handling_test.cpp
+++
@@ -1,7 +0,0 @@
-#include <stan/math/matrix_error_handling.hpp>
-#include <stan/math/error_handling/check_not_nan.hpp>
-#include <stan/math/error_handling/check_consistent_size.hpp>
-#include <stan/math/error_handling/check_consistent_sizes.hpp>
-#include <gtest/gtest.h>
-
-
