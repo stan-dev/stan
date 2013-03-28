6cd0a60a25ba1dee42d2bc895f2d08c5ab79fb30
Daniel Lee <bearlee@alum.mit.edu>
2013-03-28 00:12:30 -0400

    splitting tgamma out of agrad/special_functions.hpp

--- src/stan/agrad/special_functions.hpp
+++ src/stan/agrad/special_functions.hpp
@@ -424,25 +424,6 @@ namespace stan {
 
 
 
-    /**
-     * Return the Gamma function applied to the specified variable (C99).
-     *
-     * See boost::math::tgamma() for the double-based version.
-     *
-     * The derivative with respect to the argument is
-     *
-     * \f$\frac{d}{dx} \Gamma(x) = \Gamma(x) \Psi^{(0)}(x)\f$
-     *
-     * where \f$\Psi^{(0)}(x)\f$ is the digamma function.
-     *
-     * See boost::math::digamma() for the double-based version.
-     *
-     * @param a Argument to function.
-     * @return The Gamma function applied to the specified argument.
-     */
-    inline var tgamma(const stan::agrad::var& a) {
-      return var(new tgamma_vari(a.vi_));
-    }
 
     /**
      * Return the step, or heaviside, function applied to the
