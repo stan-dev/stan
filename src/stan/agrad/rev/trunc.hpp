4544237d4dde49ed0dd1f47a483799525576f3e5
Daniel Lee <bearlee@alum.mit.edu>
2013-03-28 14:34:06 -0400

    moving trunc tests out of test/agrad/special_functions

--- src/stan/agrad/rev/trunc.hpp
+++ src/stan/agrad/rev/trunc.hpp
@@ -1,6 +1,7 @@
 #ifndef __STAN__AGRAD__REV__TRUNC_HPP__
 #define __STAN__AGRAD__REV__TRUNC_HPP__
 
+#include <boost/math/special_functions/trunc.hpp>
 #include <stan/agrad/rev/var.hpp>
 #include <stan/agrad/rev/vari.hpp>
 
