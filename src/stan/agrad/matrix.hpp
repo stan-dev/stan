114f794ce2377e54d9c8fda8c4b73a087dd49933
Daniel Lee <bearlee@alum.mit.edu>
2013-03-28 15:24:54 -0400

    splitting src/stan/agrad/matrix.hpp

--- src/stan/agrad/matrix.hpp
+++ src/stan/agrad/matrix.hpp
@@ -22,6 +22,7 @@
 #include <stan/agrad/rev/matrix/to_var.hpp>
 #include <stan/agrad/rev/matrix/dot_self.hpp>
 #include <stan/agrad/rev/matrix/dot_product.hpp>
+#include <stan/agrad/rev/matrix/sum.hpp>
 
 
 
@@ -30,81 +31,6 @@ namespace stan {
   namespace agrad {
 
     // scalar returns
-
-    // /**
-    //  * Determinant of the matrix.
-    //  *
-    //  * Returns the determinant of the specified
-    //  * square matrix.
-    //  *
-    //  * @param m Specified matrix.
-    //  * @return Determinant of the matrix.
-    //  * @throw std::domain_error if m is not a square matrix
-    //  */
-    // var determinant(const Eigen::Matrix<var, Eigen::Dynamic, Eigen::Dynamic>& m);
-    
-    namespace {
-      class sum_v_vari : public vari{
-      protected:
-        vari** v_;
-        size_t length_;
-        inline static double var_sum(const var *v, size_t length) {
-          double result = 0;
-          for (size_t i = 0; i < length; i++)
-            result += v[i].vi_->val_;
-          return result;
-        } 
-        template<typename Derived>
-        inline static double var_sum(const Eigen::DenseBase<Derived> &v) {
-          double result = 0;
-          for (int i = 0; i < v.size(); i++)
-            result += v(i).vi_->val_;
-          return result;
-        } 
-      public:
-        template<typename Derived>
-        sum_v_vari(const Eigen::DenseBase<Derived> &v) :
-          vari(var_sum(v)), length_(v.size()) {
-          v_ = (vari**)memalloc_.alloc(length_*sizeof(vari*));
-          for (size_t i = 0; i < length_; i++)
-            v_[i] = v(i).vi_;
-        }
-        template<int R1,int C1>
-        sum_v_vari(const Eigen::Matrix<var,R1,C1> &v1) :
-          vari(var_sum(v1)), length_(v1.size()) {
-          v_ = (vari**)memalloc_.alloc(length_*sizeof(vari*));
-          for (size_t i = 0; i < length_; i++)
-            v_[i] = v1(i).vi_;
-        }
-        sum_v_vari(const var *v, size_t len) :
-          vari(var_sum(v,len)), length_(len) {
-          v_ = (vari**)memalloc_.alloc(length_*sizeof(vari*));
-          for (size_t i = 0; i < length_; i++)
-            v_[i] = v[i].vi_;
-        }
-        virtual void chain() {
-          for (size_t i = 0; i < length_; i++) {
-            v_[i]->adj_ += adj_;
-          }
-        }
-      };
-
-    }
-    
-    
-
-    /**
-     * Returns the sum of the coefficients of the specified
-     * matrix, column vector or row vector.
-     * @param m Specified matrix or vector.
-     * @return Sum of coefficients of matrix.
-     */
-    template <int R, int C>
-    inline var sum(const Eigen::Matrix<var,R,C>& m) {
-      if (m.size() == 0)
-        return 0.0;
-      return var(new sum_v_vari(m));
-    }
 
     template <int R1,int C1,int R2,int C2>
     class mdivide_left_vv_vari : public vari {
---
+++ src/test/agrad/rev/matrix/sum_test.cpp
@@ -0,0 +1,3 @@
+#include <stan/agrad/rev/matrix/sum.hpp>
+#include <gtest/gtest.h>
+#include <test/agrad/util.hpp>
