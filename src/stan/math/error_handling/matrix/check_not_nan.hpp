fe372b6ec34c752f3182a8f3aefa88bc244e39eb
Daniel Lee <bearlee@alum.mit.edu>
2013-03-27 20:42:24 -0400

    moving check_not_nan tests out of math/matrix_error_handling_test.cpp

--- src/stan/math/error_handling/matrix/check_not_nan.hpp
+++ src/stan/math/error_handling/matrix/check_not_nan.hpp
@@ -2,6 +2,7 @@
 #define __STAN__MATH__ERROR_HANDLING__MATRIX__CHECK_NOT_NAN_HPP__
 
 #include <sstream>
+#include <boost/math/special_functions/fpclassify.hpp>
 #include <stan/math/matrix/Eigen.hpp>
 #include <stan/math/error_handling/default_policy.hpp>
 #include <stan/math/error_handling/raise_domain_error.hpp>
@@ -9,12 +10,14 @@
 namespace stan {
   namespace math {
 
-    template <typename T_y, typename T_result, class Policy>
+    template <typename T_y, typename T_result, 
+              int R, int C,
+              class Policy>
     inline bool check_not_nan(const char* function,
-                  const Eigen::Matrix<T_y,Eigen::Dynamic,Eigen::Dynamic>& y,
-                  const char* name,
-                  T_result* result,
-                  const Policy&) {
+                              const Eigen::Matrix<T_y,R,C>& y,
+                              const char* name,
+                              T_result* result,
+                              const Policy&) {
       for (int i = 0; i < y.rows(); i++) {
         for (int j = 0; j < y.cols(); j++) {
           if (boost::math::isnan(y(i,j))) {
@@ -33,18 +36,20 @@ namespace stan {
       }
       return true;
     }
-    template <typename T_y, typename T_result>
+    template <typename T_y, typename T_result,
+              int R, int C>
     inline bool check_not_nan(const char* function,
-                  const Eigen::Matrix<T_y,Eigen::Dynamic,Eigen::Dynamic>& y,
-                  const char* name,
-                  T_result* result) {
+                              const Eigen::Matrix<T_y,R,C>& y,
+                              const char* name,
+                              T_result* result) {
       return check_not_nan(function,y,name,result,default_policy());
     }
-    template <typename T>
+    template <typename T,
+              int R, int C>
     inline bool check_not_nan(const char* function,
-                  const Eigen::Matrix<T,Eigen::Dynamic,Eigen::Dynamic>& y,
-                  const char* name,
-                  T* result = 0) {
+                              const Eigen::Matrix<T,R,C>& y,
+                              const char* name,
+                              T* result = 0) {
       return check_not_nan(function,y,name,result,default_policy());
     }
 
--- src/test/math/matrix_error_handling_test.cpp
+++ src/test/math/matrix_error_handling_test.cpp
@@ -7,23 +7,7 @@
 
 using stan::math::default_policy;
 
-TEST(stanMathMatrixErrorHandling, checkNotNanEigenRow) {
-  stan::math::vector_d y;
-  double result;
-  y.resize(3);
-  
-  EXPECT_TRUE(stan::math::check_not_nan("checkNotNanEigenRow(%1)",
-                                        y, "y", &result));
-  EXPECT_TRUE(stan::math::check_not_nan("checkNotNanEigenRow(%1)",
-                                        y, "y"));
-  
-  y(1) = std::numeric_limits<double>::quiet_NaN();
-  EXPECT_THROW(stan::math::check_not_nan("checkNotNanEigenRow(%1%)", y, "y", &result), 
-               std::domain_error);
-  EXPECT_THROW(stan::math::check_not_nan("checkNotNanEigenRow(%1%)", y, "y"), 
-               std::domain_error);
-  
-}
+
 TEST(stanMathMatrixErrorHandling, checkUnitVector) {
   Eigen::Matrix<double,Eigen::Dynamic,1> y(2);
   double result;
