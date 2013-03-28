94c04bc01053286112af78783d4ebe9284fa9d1c
Daniel Lee <bearlee@alum.mit.edu>
2013-03-27 20:45:11 -0400

    moving check_symmetric tests out of math/matrix_error_handling_test.cpp

--- src/test/math/matrix_error_handling_test.cpp
+++ src/test/math/matrix_error_handling_test.cpp
@@ -8,39 +8,6 @@
 using stan::math::default_policy;
 
 
-TEST(MathErrorHandlingMatrix, checkSimplex) {
-  Eigen::Matrix<double,Eigen::Dynamic,1> y(2);
-  double result;
-  y << 0.5, 0.5;
-  
-  EXPECT_TRUE(stan::math::check_simplex("checkSimplex(%1%)",
-                                        y, "y", &result));
-  EXPECT_TRUE(stan::math::check_simplex("checkSimplex(%1%)",
-                                        y, "y"));
-                  
-  y[1] = 0.55;
-  EXPECT_THROW(stan::math::check_simplex("checkSimplex(%1%)", y, "y", &result), 
-               std::domain_error);
-  EXPECT_THROW(stan::math::check_simplex("checkSimplex(%1%)", y, "y"),
-               std::domain_error);
-}
-TEST(MathErrorHandlingMatrix, checkSymmetric) {
-  Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic> y;
-  double result;
-  
-  y.resize(2,2);
-  y << 1, 3, 3, 1;
-  EXPECT_TRUE(stan::math::check_symmetric("checkSymmetric(%1%)",
-                                          y, "y", &result));
-  EXPECT_TRUE(stan::math::check_symmetric("checkSymmetric(%1%)",
-                                          y, "y"));
-
-  y(0,1) = 3.5;
-  EXPECT_THROW(stan::math::check_symmetric("checkSymmetric(%1%)", y, "y", &result), 
-               std::domain_error);
-  EXPECT_THROW(stan::math::check_symmetric("checkSymmetric(%1%)", y, "y"),
-               std::domain_error);
-}
 TEST(MathErrorHandlingMatrix, checkCovMatrix) {
   Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic> y;
   double result;
