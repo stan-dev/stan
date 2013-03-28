7ba38bb667c84c921532c451717544eb0eae630b
Daniel Lee <bearlee@alum.mit.edu>
2013-03-28 12:10:24 -0400

    moving hypot tests out of test/agrad/special_functions

--- src/test/agrad/special_functions_test.cpp
+++ src/test/agrad/special_functions_test.cpp
@@ -97,47 +97,6 @@ TEST(AgradRev,log_loss_one) {
   EXPECT_FLOAT_EQ(-1.0 / 0.2, grad_f[0]);
 }
 
-
-TEST(AgradRev,hypot_vv) {
-  AVAR a = 3.0;
-  AVAR b = 4.0;
-  AVAR f = hypot(a,b);
-  EXPECT_FLOAT_EQ(5.0,f.val());
-
-  AVEC x = createAVEC(a,b);
-  VEC grad_f;
-  f.grad(x,grad_f);
-  // arbitrary, but doc this way
-  EXPECT_FLOAT_EQ(3.0/5.0,grad_f[0]);
-  EXPECT_FLOAT_EQ(4.0/5.0,grad_f[1]);
-}  
-
-TEST(AgradRev,hypot_vd) {
-  AVAR a = 3.0;
-  double b = 4.0;
-  AVAR f = hypot(a,b);
-  EXPECT_FLOAT_EQ(5.0,f.val());
-
-  AVEC x = createAVEC(a);
-  VEC grad_f;
-  f.grad(x,grad_f);
-  // arbitrary, but doc this way
-  EXPECT_FLOAT_EQ(3.0/5.0,grad_f[0]);
-}  
-
-TEST(AgradRev,hypot_dv) {
-  double a = 3.0;
-  AVAR b = 4.0;
-  AVAR f = hypot(a,b);
-  EXPECT_FLOAT_EQ(5.0,f.val());
-
-  AVEC x = createAVEC(b);
-  VEC grad_f;
-  f.grad(x,grad_f);
-  // arbitrary, but doc this way
-  EXPECT_FLOAT_EQ(4.0/5.0,grad_f[0]);
-}  
-
 TEST(AgradRev,log2_defaultpolicy) {
   AVAR a = 3.0;
   AVAR f = log2(a);
