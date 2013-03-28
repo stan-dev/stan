443cc7bc151e4530da453a43250246ab9a699254
Daniel Lee <bearlee@alum.mit.edu>
2013-03-28 14:38:54 -0400

    adding test for int_step

--- src/test/agrad/special_functions_test.cpp
+++ src/test/agrad/special_functions_test.cpp
@@ -29,22 +29,6 @@ AVEC createAVEC(AVAR x1, AVAR x2, AVAR x3) {
 
 
 
-TEST(AgradRev,int_step) {
-  using stan::math::int_step;
-
-  AVAR a(5.0);
-  AVAR b(0.0);
-  AVAR c(-1.0);
-  
-  EXPECT_EQ(1U,int_step(a));
-  EXPECT_EQ(0U,int_step(b));
-  EXPECT_EQ(0U,int_step(c));
-}
-
-
-
-
-
 
 void test_log1m_inv_logit(const double x) {
   using stan::agrad::var;
