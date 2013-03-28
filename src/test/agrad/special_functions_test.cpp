cecd780f80e3553ad881785c7396be8507f9e822
Daniel Lee <bearlee@alum.mit.edu>
2013-03-28 14:35:42 -0400

    moving value_of tests out of test/agrad/special_functions

--- src/test/agrad/special_functions_test.cpp
+++ src/test/agrad/special_functions_test.cpp
@@ -43,15 +43,6 @@ TEST(AgradRev,int_step) {
 
 
 
-TEST(AgradRev,value_of) {
-  using stan::agrad::var;
-  using stan::math::value_of;
-
-  var a = 5.0;
-  EXPECT_FLOAT_EQ(5.0, value_of(a));
-  EXPECT_FLOAT_EQ(5.0, value_of(5.0)); // make sure all work together
-  EXPECT_FLOAT_EQ(5.0, value_of(5));
-}
 
 
 
