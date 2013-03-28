51c69e0e42a021d213d5b0165eb0c944c16f5d5a
Daniel Lee <bearlee@alum.mit.edu>
2013-03-28 12:21:08 -0400

    moving inv_logit tests out of test/agrad/special_functions

--- src/test/agrad/special_functions_test.cpp
+++ src/test/agrad/special_functions_test.cpp
@@ -39,17 +39,6 @@ TEST(AgradRev,lgamma) {
 }
 
 
-TEST(AgradRev,inv_logit) {
-  AVAR a = 2.0;
-  AVAR f = inv_logit(a);
-  EXPECT_FLOAT_EQ(1.0 / (1.0 + exp(-2.0)),f.val());
-
-  AVEC x = createAVEC(a);
-  VEC grad_f;
-  f.grad(x,grad_f);
-  EXPECT_FLOAT_EQ(exp(-2.0)/pow(1 + exp(-2.0),2.0),
-                  grad_f[0]);
-}
 
 TEST(AgradRev,log1p) {
   AVAR a = 0.1;
