c35534299c0737afdb80decd64a20833a4efe3fa
Daniel Lee <bearlee@alum.mit.edu>
2013-03-28 11:48:10 -0400

    moving cbrt tests out of test/agrad/special_functions

--- src/test/agrad/special_functions_test.cpp
+++ src/test/agrad/special_functions_test.cpp
@@ -521,16 +521,6 @@ TEST(AgradRev,log2_defaultpolicy) {
 }
 
 
-TEST(AgradRev,cbrt) {
-  AVAR a = 27.0;
-  AVAR f = cbrt(a);
-  EXPECT_FLOAT_EQ(3.0, f.val());
-  
-  AVEC x = createAVEC(a);
-  VEC grad_f;
-  f.grad(x,grad_f);
-  EXPECT_FLOAT_EQ(1.0 / 3.0 / std::pow(27.0,2.0/3.0), grad_f[0]);
-}
 
 TEST(AgradRev,trunc) {
   AVAR a = 1.2;
