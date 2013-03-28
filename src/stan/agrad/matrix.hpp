728dbaa0d7d2a88a0f02807b71178f2477f6bda8
Daniel Lee <bearlee@alum.mit.edu>
2013-03-28 15:38:44 -0400

    splitting src/stan/agrad/matrix.hpp

--- src/stan/agrad/matrix.hpp
+++ src/stan/agrad/matrix.hpp
@@ -27,41 +27,11 @@
 #include <stan/agrad/rev/matrix/mdivide_left_tri.hpp>
 #include <stan/agrad/rev/matrix/determinant.hpp>
 #include <stan/agrad/rev/matrix/log_determinant.hpp>
+#include <stan/agrad/rev/matrix/divide.hpp>
 
 namespace stan {
 
   namespace agrad {
-
-    /**
-     * Return the division of the first scalar by
-     * the second scalar.
-     * @param[in] v Specified vector.
-     * @param[in] c Specified scalar.
-     * @return Vector divided by the scalar.
-     */
-    inline double
-    divide(double x, double y) { 
-      return x / y; 
-    }
-    template <typename T1, typename T2>
-    inline var
-    divide(const T1& v, const T2& c) {
-      return to_var(v) / to_var(c);
-    }
-    /**
-     * Return the division of the specified column vector by
-     * the specified scalar.
-     * @param[in] v Specified vector.
-     * @param[in] c Specified scalar.
-     * @return Vector divided by the scalar.
-     */
-    template <typename T1, typename T2, int R, int C>
-    inline Eigen::Matrix<var,R,C>
-    divide(const Eigen::Matrix<T1, R,C>& v, const T2& c) {
-      return to_var(v) / to_var(c);
-    }
-
-
     
     /**
      * Return the product of two scalars.
