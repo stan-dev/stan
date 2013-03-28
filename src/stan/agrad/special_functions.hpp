bb30eb6da8567aaaaf22c273c6fe1a88cd685d78
Daniel Lee <bearlee@alum.mit.edu>
2013-03-27 23:30:42 -0400

    splitting asinh out of agrad/special_functions.hpp

--- src/stan/agrad/special_functions.hpp
+++ src/stan/agrad/special_functions.hpp
@@ -648,21 +648,6 @@ namespace stan {
 
 
 
-    /**
-     * The inverse hyperbolic sine function for variables (C99).
-     * 
-     * For non-variable function, see boost::math::asinh().
-     *
-     * The derivative is defined by
-     *
-     * \f$\frac{d}{dx} \mbox{asinh}(x) = \frac{x}{x^2 + 1}\f$.
-     *
-     * @param a The variable.
-     * @return Inverse hyperbolic sine of the variable.
-     */
-    inline var asinh(const stan::agrad::var& a) {
-      return var(new asinh_vari(a.vi_));
-    }
 
     /**
      * The inverse hyperbolic tangent function for variables (C99).
