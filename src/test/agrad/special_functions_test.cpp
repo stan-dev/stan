0a5aa7b8b570d6cfd866647e55d90a397de0f8c5
Daniel Lee <bearlee@alum.mit.edu>
2013-03-28 11:59:18 -0400

    moving exp2 tests out of test/agrad/special_functions

--- src/test/agrad/special_functions_test.cpp
+++ src/test/agrad/special_functions_test.cpp
@@ -192,20 +192,6 @@ TEST(AgradRev,log_loss_one) {
 
   
 
-TEST(AgradRev,exp2_defaultpolicy) {
-  AVAR a = 1.3;
-  AVAR f = exp2(a);
-  EXPECT_FLOAT_EQ(std::pow(2.0,1.3), f.val());
-
-  AVEC x = createAVEC(a);
-  VEC grad_f;
-  f.grad(x,grad_f);
-  EXPECT_FLOAT_EQ(std::pow(2.0,1.3) * std::log(2.0),grad_f[0]);
-  
-  a = std::numeric_limits<AVAR>::infinity();
-  EXPECT_FLOAT_EQ(std::numeric_limits<double>::infinity(),
-                  stan::math::exp2(a).val());
-}
 
 TEST(AgradRev,expm1) {
   AVAR a = 1.3;
