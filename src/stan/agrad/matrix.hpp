29de6c21d639b8cb130a65157bcb7e7c5a13153c
Daniel Lee <bearlee@alum.mit.edu>
2013-03-28 15:01:17 -0400

    splitting src/stan/agrad/matrix.hpp

--- src/stan/agrad/matrix.hpp
+++ src/stan/agrad/matrix.hpp
@@ -24,34 +24,6 @@ namespace stan {
   namespace agrad {
 
     /**
-     * Initialize variable to value.  (Function may look pointless, but
-     * its needed to bottom out recursion.)
-     */
-    inline void initialize_variable(var& variable, const var& value) {
-      variable = value;
-    }
-
-    /**
-     * Initialize every cell in the matrix to the specified value.
-     * 
-     */
-    template <int R, int C>
-    inline void initialize_variable(Eigen::Matrix<var,R,C>& matrix, const var& value) {
-      for (int i = 0; i < matrix.size(); ++i)
-        matrix(i) = value;
-    }
-
-    /**
-     * Initialize the variables in the standard vector recursively.
-     */
-    template <typename T>
-    inline void initialize_variable(std::vector<T>& variables, const var& value) {
-      for (size_t i = 0; i < variables.size(); ++i)
-        initialize_variable(variables[i],value);
-    }
-
-
-    /**
      * Converts argument to an automatic differentiation variable.
      *
      * Returns a stan::agrad::var variable with the input value.
