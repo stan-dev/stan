6e05cb1df198b6f055d63defdfe887d98eea7a71
Daniel Lee <bearlee@alum.mit.edu>
2013-03-28 13:42:42 -0400

    moving tgamma tests out of test/agrad/special_functions

--- src/test/agrad/special_functions_test.cpp
+++ src/test/agrad/special_functions_test.cpp
@@ -51,16 +51,6 @@ TEST(AgradRev,trunc_2) {
 
 
 
-TEST(AgradRev,tgamma) {
-  AVAR a = 3.5;
-  AVAR f = tgamma(a);
-  EXPECT_FLOAT_EQ(boost::math::tgamma(3.5),f.val());
-
-  AVEC x = createAVEC(a);
-  VEC grad_f;
-  f.grad(x,grad_f);
-  EXPECT_FLOAT_EQ(boost::math::digamma(3.5) * boost::math::tgamma(3.5),grad_f[0]);
-}  
 
  
 
