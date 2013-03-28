78a2b32ec7274f5b3ca501510af4ca5fc7b66158
Daniel Lee <bearlee@alum.mit.edu>
2013-03-28 13:40:04 -0400

    moving square tests out of test/agrad/special_functions

--- src/test/agrad/special_functions_test.cpp
+++ src/test/agrad/special_functions_test.cpp
@@ -97,17 +97,6 @@ TEST(AgradRev,step_3) {
  
 
 
-TEST(AgradRev,square) {
-  AVAR a = 7.0;
-  AVEC x = createAVEC(a);
-  AVAR f = square(a);
-  EXPECT_FLOAT_EQ(49.0, f.val());
-
-  VEC grad_f;
-  f.grad(x,grad_f);
-  EXPECT_EQ(1U,grad_f.size());
-  EXPECT_FLOAT_EQ(14.0, grad_f[0]);
-}
 
 
 
