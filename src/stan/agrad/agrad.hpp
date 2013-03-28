b565db87e5c3c5c57a1db2dfc1e984cb7796fb77
Daniel Lee <bearlee@alum.mit.edu>
2013-03-28 00:20:09 -0400

    splitting inv_logit out of agrad/special_functions.hpp

--- src/stan/agrad/agrad.hpp
+++ src/stan/agrad/agrad.hpp
@@ -89,5 +89,6 @@
 #include <stan/agrad/rev/step.hpp>
 #include <stan/agrad/rev/inv_cloglog.hpp>
 #include <stan/agrad/rev/Phi.hpp>
+#include <stan/agrad/rev/inv_logit.hpp>
 
 #endif
--- src/stan/agrad/special_functions.hpp
+++ src/stan/agrad/special_functions.hpp
@@ -45,23 +45,6 @@ namespace stan {
         }
       };
 
-
-
-      class inv_logit_vari : public op_v_vari {
-      public:
-        inv_logit_vari(vari* avi) :
-          op_v_vari(math::inv_logit(avi->val_),avi) {
-        }
-        void chain() {
-          avi_->adj_ +=  adj_ * val_ * (1.0 - val_);
-        }
-      };
-
-
-
-
-
-
       inline double calculate_chain(const double& x, const double& val) {
         return std::exp(x - val); // works out to inv_logit(x)
       }
@@ -398,21 +381,6 @@ namespace stan {
 
 
 
-    /**
-     * The inverse logit function for variables (stan).
-     *
-     * See stan::math::inv_logit() for the double-based version.
-     *
-     * The derivative of inverse logit is
-     *
-     * \f$\frac{d}{dx} \mbox{logit}^{-1}(x) = \mbox{logit}^{-1}(x) (1 - \mbox{logit}^{-1}(x))\f$.
-     *
-     * @param a Argument variable.
-     * @return Inverse logit of argument.
-     */
-    inline var inv_logit(const stan::agrad::var& a) {
-      return var(new inv_logit_vari(a.vi_));
-    }
 
     /**
      * Approximation of the unit normal CDF for variables (stan).
