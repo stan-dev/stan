5e94fe236a382e881eacdb07bcaee2848f62fe27
Daniel Lee <bearlee@alum.mit.edu>
2013-03-29 14:37:26 -0400

    all tests pass on windows with this commit

--- src/stan/agrad/rev/multiply_log.hpp
+++ src/stan/agrad/rev/multiply_log.hpp
@@ -1,6 +1,7 @@
 #ifndef __STAN__AGRAD__REV__MULTIPLY_LOG_HPP__
 #define __STAN__AGRAD__REV__MULTIPLY_LOG_HPP__
 
+#include <limits>
 #include <stan/agrad/rev/var.hpp>
 #include <stan/agrad/rev/op/vv_vari.hpp>
 #include <stan/agrad/rev/op/vd_vari.hpp>
