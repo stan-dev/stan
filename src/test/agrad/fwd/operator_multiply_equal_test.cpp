bddcf9515e5cd0cab21aa8238c9a1f95f91670fe
Peter Li <li.peter94@gmail.com>
2013-04-03 22:03:16 -0400

    refactor fvar test for operator_multiply_equal

---
+++ src/test/agrad/fwd/operator_multiply_equal_test.cpp
@@ -0,0 +1,37 @@
+#include <gtest/gtest.h>
+#include <stan/agrad/fvar.hpp>
+
+TEST(AgradFvar, operatorMultiplyEqual){
+  using stan::agrad::fvar;
+
+  fvar<double> a(0.5);
+  fvar<double> x1(0.4);
+  a.d_ = 1.0;
+  x1.d_ = 2.0;
+  a *= x1;
+  EXPECT_FLOAT_EQ(0.5 * 0.4, a.val_);
+  EXPECT_FLOAT_EQ(1.0 * 0.4 + 2.0 * 0.5, a.d_);
+
+  fvar<double> b(0.5);
+  fvar<double> x2(0.4);
+  b.d_ = 1.0;
+  x2.d_ = 2.0;
+  b *= -x2;
+  EXPECT_FLOAT_EQ(0.5 * -0.4, b.val_);
+  EXPECT_FLOAT_EQ(1.0 * -0.4 + -2.0 * 0.5, b.d_);
+
+  fvar<double> c(0.6);
+  double x3(0.3);
+  c.d_ = 3.0;
+  c *= x3;
+  EXPECT_FLOAT_EQ(0.6 * 0.3, c.val_);
+  EXPECT_FLOAT_EQ(3.0, c.d_);
+
+  fvar<double> d(0.5);
+  fvar<double> x4(-0.4);
+  d.d_ = 1.0;
+  x4.d_ = 2.0;
+  d *= x4;
+  EXPECT_FLOAT_EQ(0.5 * -0.4, d.val_);
+  EXPECT_FLOAT_EQ(1.0 * -0.4 + 2.0 * 0.5, d.d_);
+}
