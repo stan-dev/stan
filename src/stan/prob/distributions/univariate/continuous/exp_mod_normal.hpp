f45907aafe0e54111acbba1dc86d691385380891
Daniel Lee <bearlee@alum.mit.edu>
2013-04-09 23:42:23 -0400

    adding compiled model tests for exp_mod_normal, gumbel, skew_normal cdfs

--- src/stan/prob/distributions/univariate/continuous/exp_mod_normal.hpp
+++ src/stan/prob/distributions/univariate/continuous/exp_mod_normal.hpp
@@ -22,7 +22,7 @@ namespace stan {
               typename T_y, typename T_loc, typename T_scale,typename T_inv_scale, class Policy>
     typename return_type<T_y,T_loc,T_scale, T_inv_scale>::type
     exp_mod_normal_log(const T_y& y, const T_loc& mu, const T_scale& sigma, 
-           const T_inv_scale& lambda, const Policy& /*policy*/) {
+                       const T_inv_scale& lambda, const Policy& /*policy*/) {
       static const char* function = "stan::prob::exp_mod_normal_log(%1%)";
 
       using stan::is_constant_struct;
@@ -37,7 +37,7 @@ namespace stan {
       if (!(stan::length(y) 
             && stan::length(mu) 
             && stan::length(sigma)
-          && stan::length(lambda)))
+            && stan::length(lambda)))
         return 0.0;
 
       // set up return value accumulator
@@ -53,7 +53,7 @@ namespace stan {
                         &logp, Policy()))
         return logp;
       if (!check_positive(function, lambda, "Inv_scale parameter", 
-                        &logp, Policy()))
+                          &logp, Policy()))
         return logp;
       if (!check_positive(function, sigma, "Scale parameter", 
                           &logp, Policy()))
@@ -81,21 +81,21 @@ namespace stan {
         //pull out values of arguments
         const double y_dbl = value_of(y_vec[n]);
         const double mu_dbl = value_of(mu_vec[n]);
-      const double sigma_dbl = value_of(sigma_vec[n]);
-      const double lambda_dbl = value_of(lambda_vec[n]);
+        const double sigma_dbl = value_of(sigma_vec[n]);
+        const double lambda_dbl = value_of(lambda_vec[n]);
 
-      const double pi_dbl = boost::math::constants::pi<double>();
+        const double pi_dbl = boost::math::constants::pi<double>();
 
         // log probability
         if (include_summand<propto>::value)
           logp -= log(2.0);
         if (include_summand<propto, T_inv_scale>::value)
-    logp += log(lambda_dbl);
+          logp += log(lambda_dbl);
         if (include_summand<propto,T_y,T_loc,T_scale,T_inv_scale>::value)
           logp += lambda_dbl * (mu_dbl + 0.5 * lambda_dbl * sigma_dbl * sigma_dbl - y_dbl) + log(boost::math::erfc((mu_dbl + lambda_dbl * sigma_dbl * sigma_dbl - y_dbl) / (std::sqrt(2.0) * sigma_dbl)));
 
         // gradients
-  const double deriv_logerfc = -2.0 / std::sqrt(pi_dbl) * exp(-(mu_dbl + lambda_dbl * sigma_dbl * sigma_dbl - y_dbl) / (std::sqrt(2.0) * sigma_dbl) * (mu_dbl + lambda_dbl * sigma_dbl * sigma_dbl - y_dbl) / (sigma_dbl * std::sqrt(2.0))) / boost::math::erfc((mu_dbl + lambda_dbl * sigma_dbl * sigma_dbl - y_dbl) / (sigma_dbl * std::sqrt(2.0)));
+        const double deriv_logerfc = -2.0 / std::sqrt(pi_dbl) * exp(-(mu_dbl + lambda_dbl * sigma_dbl * sigma_dbl - y_dbl) / (std::sqrt(2.0) * sigma_dbl) * (mu_dbl + lambda_dbl * sigma_dbl * sigma_dbl - y_dbl) / (sigma_dbl * std::sqrt(2.0))) / boost::math::erfc((mu_dbl + lambda_dbl * sigma_dbl * sigma_dbl - y_dbl) / (sigma_dbl * std::sqrt(2.0)));
 
         if (!is_constant_struct<T_y>::value)
           operands_and_partials.d_x1[n] += -lambda_dbl + deriv_logerfc * -1.0 / (sigma_dbl * std::sqrt(2.0));
@@ -136,7 +136,7 @@ namespace stan {
               class Policy>
     typename return_type<T_y,T_loc,T_scale,T_inv_scale>::type
     exp_mod_normal_cdf(const T_y& y, const T_loc& mu, const T_scale& sigma, const T_inv_scale& lambda,
-             const Policy&) {
+                       const Policy&) {
       static const char* function = "stan::prob::exp_mod_normal_cdf(%1%)";
 
       using stan::math::check_positive;
@@ -150,7 +150,7 @@ namespace stan {
       if (!(stan::length(y) 
             && stan::length(mu) 
             && stan::length(sigma)
-          && stan::length(lambda)))
+            && stan::length(lambda)))
         return cdf;
 
       if (!check_not_nan(function, y, "Random variable", &cdf, Policy()))
@@ -168,7 +168,7 @@ namespace stan {
       if (!check_finite(function, lambda, "Inv_scale parameter", &cdf, Policy()))
         return cdf;
       if (!check_positive(function, lambda, "Inv_scale parameter", 
-                         &cdf, Policy()))
+                          &cdf, Policy()))
         return cdf;
       if (!(check_consistent_sizes(function,
                                    y,mu,sigma,lambda,
@@ -183,11 +183,11 @@ namespace stan {
       size_t N = max_size(y, mu, sigma, lambda);
       
       for (size_t n = 0; n < N; n++) {
-  if(boost::math::isinf(y_vec[n]))
-    {
-      if (y_vec[n] < 0.0)
-        return cdf * 0.0;
-    }
+        if(boost::math::isinf(y_vec[n]))
+          {
+            if (y_vec[n] < 0.0)
+              return cdf * 0.0;
+          }
         cdf *= 0.5 * (1 + erf((y_vec[n] - mu_vec[n]) / (sqrt(2.0) * sigma_vec[n]))) - exp(-lambda_vec[n] * (y_vec[n] - mu_vec[n]) + lambda_vec[n] * sigma_vec[n] * lambda_vec[n] * sigma_vec[n] / 2.0) * (0.5 * (1 + erf((y_vec[n] - mu_vec[n] - sigma_vec[n] * lambda_vec[n] * sigma_vec[n]) / (sqrt(2.0) * sigma_vec[n]))));
       }
 
@@ -204,9 +204,9 @@ namespace stan {
     template <class RNG>
     inline double
     exp_mod_normal_rng(const double mu,
-           const double sigma,
-           const double lambda,
-           RNG& rng) {
+                       const double sigma,
+                       const double lambda,
+                       RNG& rng) {
       return stan::prob::normal_rng(mu, sigma,rng) + stan::prob::exponential_rng(lambda, rng);
     }
   }
