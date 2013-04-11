da36fc8e737d5a2d27cbe1ed8d45b6c0b89a3e3c
Bob Carpenter <carp@alias-i.com>
2013-04-11 16:44:23 -0400

    more fixes to get 1.2 behavior back in log_prob templated functions

--- src/stan/gm/generator.hpp
+++ src/stan/gm/generator.hpp
@@ -1243,7 +1243,7 @@ namespace stan {
       void operator()(sample const& x) const {
         if (!include_sampling_) return;
         generate_indent(indent_,o_);
-        o_ << "lp__ += stan::prob::" << x.dist_.family_ << "_log<propto__>(";
+        o_ << "lp__ += stan::prob::" << x.dist_.family_ << "_log<true>(";
         generate_expression(x.expr_,o_);
         for (size_t i = 0; i < x.dist_.args_.size(); ++i) {
           o_ << ", ";
