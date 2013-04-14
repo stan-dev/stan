994a9844adda5157b8f7dc534347fc8f4af83bb4
Daniel Lee <bearlee@alum.mit.edu>
2013-04-14 14:00:35 -0400

    policies_refactor: updating check_greater

--- src/stan/math/error_handling/check_bounded.hpp
+++ src/stan/math/error_handling/check_bounded.hpp
@@ -1,7 +1,6 @@
 #ifndef __STAN__MATH__ERROR_HANDLING__CHECK_BOUNDED_HPP__
 #define __STAN__MATH__ERROR_HANDLING__CHECK_BOUNDED_HPP__
 
-#include <stan/math/error_handling/default_policy.hpp>
 #include <stan/math/error_handling/dom_err.hpp>
 #include <stan/math/error_handling/dom_err_vec.hpp>
 
--- src/stan/math/error_handling/check_greater.hpp
+++ src/stan/math/error_handling/check_greater.hpp
@@ -1,7 +1,6 @@
 #ifndef __STAN__MATH__ERROR_HANDLING__CHECK_GREATER_HPP__
 #define __STAN__MATH__ERROR_HANDLING__CHECK_GREATER_HPP__
 
-#include <stan/math/error_handling/default_policy.hpp>
 #include <stan/math/error_handling/dom_err.hpp>
 #include <stan/math/error_handling/dom_err_vec.hpp>
 
@@ -12,22 +11,20 @@ namespace stan {
       template <typename T_y,
                 typename T_low,
                 typename T_result,
-                class Policy,
                 bool is_vec>
       struct greater {
         static bool check(const char* function,
                           const T_y& y,
                           const T_low& low,
                           const char* name,  
-                          T_result* result,
-                          const Policy&) {
+                          T_result* result) {
           using stan::length;
           VectorView<const T_low> low_vec(low);
           for (size_t n = 0; n < length(low); n++) {
             if (!(y > low_vec[n]))
               return dom_err(function,y,name,
                              " is %1%, but must be greater than ",
-                             low_vec[n],result,Policy());
+                             low_vec[n],result);
           }
           return true;
         }
@@ -35,37 +32,25 @@ namespace stan {
     
       template <typename T_y,
                 typename T_low,
-                typename T_result,
-                class Policy>
-      struct greater<T_y, T_low, T_result, Policy, true> {
+                typename T_result>
+      struct greater<T_y, T_low, T_result, true> {
         static bool check(const char* function,
                           const T_y& y,
                           const T_low& low,
                           const char* name,
-                          T_result* result,
-                          const Policy&) {
+                          T_result* result) {
           using stan::length;
           VectorView<const T_low> low_vec(low);
           for (size_t n = 0; n < length(y); n++) {
             if (!(stan::get(y,n) > low_vec[n])) {
               return dom_err_vec(n,function,y,name,
                                  " is %1%, but must be greater than ",
-                                 low_vec[n],result,Policy());
+                                 low_vec[n],result);
             }
           }
           return true;
         }
       };
-    }
-    template <typename T_y, typename T_low, typename T_result, class Policy>
-    inline bool check_greater(const char* function,
-                              const T_y& y,
-                              const T_low& low,
-                              const char* name,  
-                              T_result* result,
-                              const Policy&) {
-      return greater<T_y,T_low,T_result,Policy,
-        is_vector_like<T_y>::value>::check(function,y,low,name,result,Policy());
     }
     template <typename T_y, typename T_low, typename T_result>
     inline bool check_greater(const char* function,
@@ -73,7 +58,8 @@ namespace stan {
                               const T_low& low,
                               const char* name,  
                               T_result* result) {
-      return check_greater(function,y,low,name,result,default_policy());
+      return greater<T_y,T_low,T_result,is_vector_like<T_y>::value>
+        ::check(function,y,low,name,result);
     }
     template <typename T_y, typename T_low>
     inline bool check_greater(const char* function,
@@ -81,7 +67,7 @@ namespace stan {
                               const T_low& low,
                               const char* name) {
       return check_greater<T_y,T_low,typename scalar_type<T_y>::type *>
-        (function,y,low,name,0,default_policy());
+        (function,y,low,name,0);
     }
  
   }
