191a2f1e4aeda473d89c4041d6934489e2e34a11
Daniel Lee <bearlee@alum.mit.edu>
2013-04-14 14:19:41 -0400

    policies_refactor: updating check_positive

--- src/stan/math/error_handling/check_positive.hpp
+++ src/stan/math/error_handling/check_positive.hpp
@@ -3,7 +3,6 @@
 
 #include <boost/type_traits/is_unsigned.hpp>
 #include <stan/meta/traits.hpp>
-#include <stan/math/error_handling/default_policy.hpp>
 #include <stan/math/error_handling/dom_err.hpp>
 #include <stan/math/error_handling/dom_err_vec.hpp>
 
@@ -11,70 +10,54 @@ namespace stan {
   namespace math {
 
     namespace {
-      template <typename T_y,
-                typename T_result,
-                class Policy,
-                bool is_vec>
+      template <typename T_y, typename T_result, bool is_vec>
       struct positive {
         static bool check(const char* function,
                           const T_y& y,
                           const char* name,
-                          T_result* result,
-                          const Policy&) {
+                          T_result* result) {
           // have to use not is_unsigned. is_signed will be false
           // floating point types that have no unsigned versions.
           if (!boost::is_unsigned<T_y>::value && !(y > 0)) 
             return dom_err(function,y,name,
                            " is %1%, but must be > 0!","",
-                           result,Policy());
+                           result);
           return true;
         }
       };
     
-      template <typename T_y,
-                typename T_result,
-                class Policy>
-      struct positive<T_y, T_result, Policy, true> {
+      template <typename T_y, typename T_result>
+      struct positive<T_y, T_result, true> {
         static bool check(const char* function,
                           const T_y& y,
                           const char* name,
-                          T_result* result,
-                          const Policy&) {
+                          T_result* result) {
           using stan::length;
           for (size_t n = 0; n < length(y); n++) {
             if (!boost::is_unsigned<typename T_y::value_type>::value
                 && !(stan::get(y,n) > 0)) 
               return dom_err_vec(n,function,y,name,
                                  " is %1%, but must be > 0!","",
-                                 result,Policy());
+                                 result);
           }
           return true;
         }
       };
     }
-    template <typename T_y, typename T_result, class Policy>
+    template <typename T_y, typename T_result>
     inline bool check_positive(const char* function,
                                const T_y& y,
                                const char* name,
-                               T_result* result,
-                               const Policy&) {
-      return positive<T_y,T_result,Policy,
-        is_vector_like<T_y>::value>
-        ::check(function, y, name, result, Policy());
-    }
-    template <typename T_x, typename T_result>
-    inline bool check_positive(const char* function,
-                               const T_x& x,
-                               const char* name,
                                T_result* result) {
-      return check_positive(function,x,name,result,default_policy());
+      return positive<T_y,T_result,is_vector_like<T_y>::value>
+        ::check(function, y, name, result);
     }
     template <typename T>
     inline bool check_positive(const char* function,
                                const T& x,
                                const char* name) {
       return check_positive<T,typename scalar_type<T>::type *>
-        (function,x,name,0,default_policy());
+        (function,x,name,0);
     }
 
   }
