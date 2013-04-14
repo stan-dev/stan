6511928e5b646d93bcf7a5a7e79db22301aa516b
Daniel Lee <bearlee@alum.mit.edu>
2013-04-14 14:06:34 -0400

    policies_refactor: updating check_less

--- src/stan/math/error_handling/check_less.hpp
+++ src/stan/math/error_handling/check_less.hpp
@@ -1,7 +1,6 @@
 #ifndef __STAN__MATH__ERROR_HANDLING__CHECK_LESS_HPP__
 #define __STAN__MATH__ERROR_HANDLING__CHECK_LESS_HPP__
 
-#include <stan/math/error_handling/default_policy.hpp>
 #include <stan/math/error_handling/dom_err.hpp>
 #include <stan/math/error_handling/dom_err_vec.hpp>
 
@@ -12,22 +11,20 @@ namespace stan {
       template <typename T_y,
                 typename T_high,
                 typename T_result,
-                class Policy,
                 bool is_vec>
       struct less {
         static bool check(const char* function,
                           const T_y& y,
                           const T_high& high,
                           const char* name,  
-                          T_result* result,
-                          const Policy&) {
+                          T_result* result) {
           using stan::length;
           VectorView<const T_high> high_vec(high);
           for (size_t n = 0; n < length(high); n++) {
             if (!(y < high_vec[n]))
               return dom_err(function,y,name,
                              " is %1%, but must be less than ",
-                             high_vec[n],result,Policy());
+                             high_vec[n],result);
           }
           return true;
         }
@@ -35,36 +32,24 @@ namespace stan {
     
       template <typename T_y,
                 typename T_high,
-                typename T_result,
-                class Policy>
-      struct less<T_y, T_high, T_result, Policy, true> {
+                typename T_result>
+      struct less<T_y, T_high, T_result, true> {
         static bool check(const char* function,
                           const T_y& y,
                           const T_high& high,
                           const char* name,
-                          T_result* result,
-                          const Policy&) {
+                          T_result* result) {
           using stan::length;
           VectorView<const T_high> high_vec(high);
           for (size_t n = 0; n < length(y); n++) {
             if (!(stan::get(y,n) < high_vec[n]))
               return dom_err_vec(n,function,y,name,
                                  " is %1%, but must be less than ",
-                                 high_vec[n],result,Policy());
+                                 high_vec[n],result);
           }
           return true;
         }
       };
-    }
-    template <typename T_y, typename T_high, typename T_result, class Policy>
-    inline bool check_less(const char* function,
-                           const T_y& y,
-                           const T_high& high,
-                           const char* name,  
-                           T_result* result,
-                           const Policy&) {
-      return less<T_y,T_high,T_result,Policy,is_vector_like<T_y>::value>
-        ::check(function,y,high,name,result,Policy());
     }
     template <typename T_y, typename T_high, typename T_result>
     inline bool check_less(const char* function,
@@ -72,7 +57,8 @@ namespace stan {
                            const T_high& high,
                            const char* name,  
                            T_result* result) {
-      return check_less(function,y,high,name,result,default_policy());
+      return less<T_y,T_high,T_result,is_vector_like<T_y>::value>
+        ::check(function,y,high,name,result);
     }
     template <typename T_y, typename T_high>
     inline bool check_less(const char* function,
@@ -80,7 +66,7 @@ namespace stan {
                            const T_high& high,
                            const char* name) {
       return check_less<T_y,T_high,typename scalar_type<T_y>::type *>
-        (function,y,high,name,0,default_policy());
+        (function,y,high,name,0);
     }
 
   }
