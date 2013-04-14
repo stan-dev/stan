d526f179832a08401d9f0ea60a9c71c8648b381d
Daniel Lee <bearlee@alum.mit.edu>
2013-04-14 14:13:01 -0400

    policies_refactor: updating check_nonnegative

--- src/stan/math/error_handling/check_nonnegative.hpp
+++ src/stan/math/error_handling/check_nonnegative.hpp
@@ -1,7 +1,6 @@
 #ifndef __STAN__MATH__ERROR_HANDLING__CHECK_NONNEGATIVE_HPP__
 #define __STAN__MATH__ERROR_HANDLING__CHECK_NONNEGATIVE_HPP__
 
-#include <stan/math/error_handling/default_policy.hpp>
 #include <stan/math/error_handling/dom_err.hpp>
 #include <stan/math/error_handling/dom_err_vec.hpp>
 
@@ -9,70 +8,54 @@ namespace stan {
   namespace math {
 
     namespace {
-      template <typename T_y,
-                typename T_result,
-                class Policy,
-                bool is_vec>
+      template <typename T_y, typename T_result, bool is_vec>
       struct nonnegative {
         static bool check(const char* function,
                           const T_y& y,
                           const char* name,
-                          T_result* result,
-                          const Policy&) {
+                          T_result* result) {
           // have to use not is_unsigned. is_signed will be false
           // floating point types that have no unsigned versions.
           if (!boost::is_unsigned<T_y>::value && !(y >= 0)) 
             return dom_err(function,y,name,
                            " is %1%, but must be >= 0!","",
-                           result,Policy());
+                           result);
           return true;
         }
       };
     
-      template <typename T_y,
-                typename T_result,
-                class Policy>
-      struct nonnegative<T_y, T_result, Policy, true> {
+      template <typename T_y, typename T_result>
+      struct nonnegative<T_y, T_result, true> {
         static bool check(const char* function,
                           const T_y& y,
                           const char* name,
-                          T_result* result,
-                          const Policy&) {
+                          T_result* result) {
           using stan::length;
           for (size_t n = 0; n < length(y); n++) {
             if (!boost::is_unsigned<typename T_y::value_type>::value 
                 && !(stan::get(y,n) >= 0)) 
               return dom_err_vec(n,function,y,name,
                                  " is %1%, but must be >= 0!","",
-                                 result,Policy());
+                                 result);
           }
           return true;
         }
       };
-    }
-    template <typename T_y, typename T_result, class Policy>
-    inline bool check_nonnegative(const char* function,
-                                  const T_y& y,
-                                  const char* name,
-                                  T_result* result,
-                                  const Policy&) {
-      return nonnegative<T_y,T_result,Policy,
-                         is_vector_like<T_y>::value>
-        ::check(function, y, name, result, Policy());
     }
     template <typename T_y, typename T_result>
     inline bool check_nonnegative(const char* function,
                                   const T_y& y,
                                   const char* name,
                                   T_result* result) {
-      return check_nonnegative(function,y,name,result,default_policy());
+      return nonnegative<T_y,T_result,is_vector_like<T_y>::value>
+        ::check(function, y, name, result);
     }
     template <typename T_y>
     inline bool check_nonnegative(const char* function,
                                   const T_y& y,
                                   const char* name) {
       return check_nonnegative<T_y,typename scalar_type<T_y>::type *>
-        (function,y,name,0,default_policy());
+        (function,y,name,0);
     }
 
   }
