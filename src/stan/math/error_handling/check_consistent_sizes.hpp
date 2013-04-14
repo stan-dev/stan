cc0c35ff3caaf635b0c96f86081e9f0e4b455dd5
Daniel Lee <bearlee@alum.mit.edu>
2013-04-14 13:49:21 -0400

    policies_refactor: updating check_consistent_sizes

--- src/stan/math/error_handling/check_consistent_sizes.hpp
+++ src/stan/math/error_handling/check_consistent_sizes.hpp
@@ -2,27 +2,12 @@
 #define __STAN__MATH__ERROR_HANDLING__CHECK_CONSISTENT_SIZES_HPP__
 
 #include <stan/math/error_handling/check_consistent_size.hpp>
-#include <stan/math/error_handling/default_policy.hpp>
 #include <stan/math/error_handling/dom_err.hpp>
 #include <stan/meta/traits.hpp>
 
 namespace stan {
   namespace math {
 
-    template <typename T1, typename T2, typename T_result, 
-              class Policy>
-    inline bool check_consistent_sizes(const char* function,
-                                       const T1& x1, 
-                                       const T2& x2, 
-                                       const char* name1,
-                                       const char* name2,
-                                       T_result* result,
-                                       const Policy&) {
-      size_t max_size = std::max(size_of(x1),
-                                 size_of(x2));
-      return check_consistent_size(max_size,function,x1,name1,result,Policy())
-        && check_consistent_size(max_size,function,x2,name2,result,Policy());
-    }
     template <typename T1, typename T2, typename T_result>
     inline bool check_consistent_sizes(const char* function,
                                        const T1& x1, 
@@ -30,27 +15,12 @@ namespace stan {
                                        const char* name1,
                                        const char* name2,
                                        T_result* result) {
-      return check_consistent_sizes(function,x1,x2,name1,name2,
-                                    result,default_policy());
+      size_t max_size = std::max(size_of(x1),
+                                 size_of(x2));
+      return check_consistent_size(max_size,function,x1,name1,result)
+        && check_consistent_size(max_size,function,x2,name2,result);
     }
 
-    template <typename T1, typename T2, typename T3, typename T_result, 
-              class Policy>
-    inline bool check_consistent_sizes(const char* function,
-                                       const T1& x1, 
-                                       const T2& x2, 
-                                       const T3& x3,
-                                       const char* name1,
-                                       const char* name2,
-                                       const char* name3,
-                                       T_result* result,
-                                       const Policy&) {
-      size_t max_size = std::max(size_of(x1),
-                                 std::max(size_of(x2),size_of(x3)));
-      return check_consistent_size(max_size,function,x1,name1,result,Policy())
-        && check_consistent_size(max_size,function,x2,name2,result,Policy())
-        && check_consistent_size(max_size,function,x3,name3,result,Policy());
-    }
     template <typename T1, typename T2, typename T3, typename T_result>
     inline bool check_consistent_sizes(const char* function,
                                        const T1& x1, 
@@ -60,30 +30,11 @@ namespace stan {
                                        const char* name2,
                                        const char* name3,
                                        T_result* result) {
-      return check_consistent_sizes(function,x1,x2,x3,name1,name2,name3,
-                                    result,default_policy());
-    }
-    template <typename T1, typename T2, typename T3, typename T4, 
-              typename T_result, 
-              class Policy>
-    inline bool check_consistent_sizes(const char* function,
-                                       const T1& x1, 
-                                       const T2& x2, 
-                                       const T3& x3,
-                                       const T4& x4,
-                                       const char* name1,
-                                       const char* name2,
-                                       const char* name3,
-                                       const char* name4,
-                                       T_result* result,
-                                       const Policy&) {
       size_t max_size = std::max(size_of(x1),
-                                 std::max(size_of(x2),
-                                          std::max(size_of(x3), size_of(x4))));
-      return check_consistent_size(max_size,function,x1,name1,result,Policy())
-        && check_consistent_size(max_size,function,x2,name2,result,Policy())
-        && check_consistent_size(max_size,function,x3,name3,result,Policy())
-        && check_consistent_size(max_size,function,x4,name4,result,Policy());
+                                 std::max(size_of(x2),size_of(x3)));
+      return check_consistent_size(max_size,function,x1,name1,result)
+        && check_consistent_size(max_size,function,x2,name2,result)
+        && check_consistent_size(max_size,function,x3,name3,result);
     }
     template <typename T1, typename T2, typename T3, typename T4, typename T_result>
     inline bool check_consistent_sizes(const char* function,
@@ -96,9 +47,13 @@ namespace stan {
                                        const char* name3,
                                        const char* name4,
                                        T_result* result) {
-      return check_consistent_sizes(function,x1,x2,x3,x4,
-                                    name1,name2,name3,name4,
-                                    result,default_policy());
+      size_t max_size = std::max(size_of(x1),
+                                 std::max(size_of(x2),
+                                          std::max(size_of(x3), size_of(x4))));
+      return check_consistent_size(max_size,function,x1,name1,result)
+        && check_consistent_size(max_size,function,x2,name2,result)
+        && check_consistent_size(max_size,function,x3,name3,result)
+        && check_consistent_size(max_size,function,x4,name4,result);
     }
 
   }
