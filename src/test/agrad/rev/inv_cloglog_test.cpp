da62660945b32747fb8616dac9cf7f7f690ed4a6
Bob Carpenter <carp@alias-i.com>
2013-04-16 19:06:08 -0400

    fixed error in inv cloglog test for 1 - val

--- src/test/agrad/rev/inv_cloglog_test.cpp
+++ src/test/agrad/rev/inv_cloglog_test.cpp
@@ -1,13 +1,9 @@
 #include <stan/agrad/rev/inv_cloglog.hpp>
-<<<<<<< HEAD
 #include <stan/agrad/rev/exp.hpp>
 #include <stan/agrad/rev/operator_unary_negative.hpp>
 #include <stan/agrad/rev/operator_subtraction.hpp>
 
 #include <stan/math/functions/inv_cloglog.hpp>
-
-=======
->>>>>>> 364e38d0e7724275230da060d6afc7f93e8c3d53
 #include <test/agrad/util.hpp>
 #include <gtest/gtest.h>
 
@@ -16,12 +12,11 @@ TEST(AgradRev,inv_cloglog) {
   using stan::agrad::exp;
   AVAR a = 2.7;
   AVAR f = inv_cloglog(a);
-  EXPECT_FLOAT_EQ(std::exp(-std::exp(2.7)),f.val());
+  EXPECT_FLOAT_EQ(1 - std::exp(-std::exp(2.7)),f.val());
 
   AVEC x = createAVEC(a);
   VEC grad_f;
   f.grad(x,grad_f);
-<<<<<<< HEAD
 
   AVAR a2 = 2.7;
   AVEC x2 = createAVEC(a2);
@@ -31,7 +26,4 @@ TEST(AgradRev,inv_cloglog) {
 
   EXPECT_EQ(1,grad_f.size());
   EXPECT_FLOAT_EQ(grad_f2[0],grad_f[0]);
-=======
-  EXPECT_FLOAT_EQ(-std::exp(2.7 - std::exp(2.7)),grad_f[0]);
->>>>>>> 364e38d0e7724275230da060d6afc7f93e8c3d53
 }
