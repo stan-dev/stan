fa4764f9c1300e25aabff02cde2082b66a66aaa9
Peter Li <li.peter94@gmail.com>
2013-04-16 17:13:48 -0400

    fixed inv_cloglog in fvar with test

--- src/test/agrad/fwd/inv_cloglog_test.cpp
+++ src/test/agrad/fwd/inv_cloglog_test.cpp
@@ -11,19 +11,19 @@ TEST(AgradFvar, invCLogLog) {
 
   fvar<double> a = inv_cloglog(x);
   EXPECT_FLOAT_EQ(inv_cloglog(0.5), a.val_);
-  EXPECT_FLOAT_EQ(-exp(0.5 - exp(0.5)), a.d_);
+  EXPECT_FLOAT_EQ(exp(0.5) * exp(-exp(0.5)), a.d_);
 
   fvar<double> y(-1.2);
   y.d_ = 1.0;
 
   fvar<double> b = inv_cloglog(y);
   EXPECT_FLOAT_EQ(inv_cloglog(-1.2), b.val_);
-  EXPECT_FLOAT_EQ(-exp(-1.2 - exp(-1.2)), b.d_);
+  EXPECT_FLOAT_EQ(exp(-1.2) * exp(-exp(-1.2)), b.d_);
 
   fvar<double> z(1.5);
-  z.d_ = 1.0;
+  z.d_ = 2.0;
 
   fvar<double> c = inv_cloglog(z);
   EXPECT_FLOAT_EQ(inv_cloglog(1.5), c.val_);
-  EXPECT_FLOAT_EQ(-exp(1.5 - exp(1.5)), c.d_);
+  EXPECT_FLOAT_EQ(2.0 * exp(1.5) * exp(-exp(1.5)), c.d_);
 }
