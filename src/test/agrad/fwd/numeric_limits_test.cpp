0b8d5a9afb1fb64dc24c0c79f4d4ae88c04caf16
Peter Li <li.peter94@gmail.com>
2013-04-16 21:28:49 -0400

    fixed numeric_limits_test for fvar

--- src/test/agrad/fwd/numeric_limits_test.cpp
+++ src/test/agrad/fwd/numeric_limits_test.cpp
@@ -10,136 +10,140 @@ TEST(AgradFvar, numeric_limits) {
   EXPECT_TRUE(std::numeric_limits<stan::agrad::fvar<double> >::is_specialized);
   EXPECT_TRUE(std::numeric_limits<stan::agrad::fvar<int> >::is_specialized);
 
-  EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<double> >::digits, 53);
-  EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<int> >::digits, 31);
+  EXPECT_FLOAT_EQ(53, std::numeric_limits<stan::agrad::fvar<double> >::digits);
+  EXPECT_FLOAT_EQ(31, std::numeric_limits<stan::agrad::fvar<int> >::digits);
 
   fvar<int> a = std::numeric_limits<stan::agrad::fvar<int> >::min();
+  a.val_ = -(a.val_ + 1);
   fvar<double> b = std::numeric_limits<stan::agrad::fvar<double> >::min();
-  EXPECT_FLOAT_EQ(a.val_, -2147483648);
-  EXPECT_FLOAT_EQ(b.val_, 2.22507e-308);
+  EXPECT_FLOAT_EQ(2.147483647e+009, a.val_);
+  EXPECT_FLOAT_EQ(2.22507e-308, b.val_);
 
   fvar<int> c = std::numeric_limits<stan::agrad::fvar<int> >::max();
   fvar<double> d = std::numeric_limits<stan::agrad::fvar<double> >::max();
-  EXPECT_FLOAT_EQ(c.val_, 2147483647);
-  EXPECT_FLOAT_EQ(d.val_, 1.79769e+308);
+  EXPECT_FLOAT_EQ(2147483647, c.val_);
+  EXPECT_FLOAT_EQ(1.79769e+308, d.val_);
 
-  EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<double> >::digits10, 
-                  15);
-  EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<int> >::digits10, 9);
+  EXPECT_FLOAT_EQ(15,
+                  std::numeric_limits<stan::agrad::fvar<double> >::digits10);
+  EXPECT_FLOAT_EQ(9,
+                  std::numeric_limits<stan::agrad::fvar<int> >::digits10);
 
-  EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<double> >::is_signed, 
-                  1);
-  EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<int> >::is_signed, 1);
+  EXPECT_FLOAT_EQ(1,
+                  std::numeric_limits<stan::agrad::fvar<double> >::is_signed);
+  EXPECT_FLOAT_EQ(1, std::numeric_limits<stan::agrad::fvar<int> >::is_signed);
 
-  EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<double> >::is_integer,
-                  0);
-  EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<int> >::is_integer, 1);
+  EXPECT_FLOAT_EQ(0,
+                  std::numeric_limits<stan::agrad::fvar<double> >::is_integer);
 
-  EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<double> >::is_exact, 
-                  0);
-  EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<int> >::is_exact, 1);
-  EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<double> >::radix, 2);
-  EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<int> >::radix, 2);
+  EXPECT_FLOAT_EQ(1, std::numeric_limits<stan::agrad::fvar<int> >::is_integer);
+
+  EXPECT_FLOAT_EQ(0,
+                  std::numeric_limits<stan::agrad::fvar<double> >::is_exact);
+
+  EXPECT_FLOAT_EQ(1, std::numeric_limits<stan::agrad::fvar<int> >::is_exact);
+  EXPECT_FLOAT_EQ(2, std::numeric_limits<stan::agrad::fvar<double> >::radix);
+  EXPECT_FLOAT_EQ(2, std::numeric_limits<stan::agrad::fvar<int> >::radix);
 
   fvar<int> e = std::numeric_limits<stan::agrad::fvar<int> >::epsilon();
   fvar<double> f = std::numeric_limits<stan::agrad::fvar<double> >::epsilon();
-  EXPECT_FLOAT_EQ(e.val_, 0);
-  EXPECT_FLOAT_EQ(f.val_, 2.220446e-16);
+  EXPECT_FLOAT_EQ(0, e.val_);
+  EXPECT_FLOAT_EQ(2.220446e-16, f.val_);
 
   fvar<int> g = std::numeric_limits<stan::agrad::fvar<int> >::round_error();
   fvar<double> h = std::numeric_limits<stan::agrad::fvar<double> >::round_error();
-  EXPECT_FLOAT_EQ(g.val_, 0);
-  EXPECT_FLOAT_EQ(h.val_,0.5);
+  EXPECT_FLOAT_EQ(0, g.val_);
+  EXPECT_FLOAT_EQ(0.5, h.val_);
 
   EXPECT_FLOAT_EQ(
       std::numeric_limits<stan::agrad::fvar<double> >::min_exponent, -1021);
-  EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<int> >::min_exponent, 
-                  0);
+  EXPECT_FLOAT_EQ(0,
+                  std::numeric_limits<stan::agrad::fvar<int> >::min_exponent);
 
-  EXPECT_FLOAT_EQ(
-        std::numeric_limits<stan::agrad::fvar<double> >::min_exponent10, -307);
-  EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<int> >::min_exponent10,
-                  0);
+  EXPECT_FLOAT_EQ(-307,
+                std::numeric_limits<stan::agrad::fvar<double> >::min_exponent10);
+  EXPECT_FLOAT_EQ(0,
+                  std::numeric_limits<stan::agrad::fvar<int> >::min_exponent10);
 
-  EXPECT_FLOAT_EQ(
-          std::numeric_limits<stan::agrad::fvar<double> >::max_exponent, 1024);
-  EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<int> >::max_exponent,
-                  0);
+  EXPECT_FLOAT_EQ(1024,
+                  std::numeric_limits<stan::agrad::fvar<double> >::max_exponent);
+  EXPECT_FLOAT_EQ(0,
+                  std::numeric_limits<stan::agrad::fvar<int> >::max_exponent);
 
-  EXPECT_FLOAT_EQ(
-         std::numeric_limits<stan::agrad::fvar<double> >::max_exponent10, 308);
-  EXPECT_FLOAT_EQ(
-              std::numeric_limits<stan::agrad::fvar<int> >::max_exponent10, 0);
+  EXPECT_FLOAT_EQ(308,
+               std::numeric_limits<stan::agrad::fvar<double> >::max_exponent10);
+  EXPECT_FLOAT_EQ(0,
+                  std::numeric_limits<stan::agrad::fvar<int> >::max_exponent10);
 
-  EXPECT_FLOAT_EQ(
-             std::numeric_limits<stan::agrad::fvar<double> >::has_infinity, 1);
-  EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<int> >::has_infinity, 
-                  0);
+  EXPECT_FLOAT_EQ(1,
+                  std::numeric_limits<stan::agrad::fvar<double> >::has_infinity);
+  EXPECT_FLOAT_EQ(0,
+                  std::numeric_limits<stan::agrad::fvar<int> >::has_infinity);
 
-  EXPECT_FLOAT_EQ(
-            std::numeric_limits<stan::agrad::fvar<double> >::has_quiet_NaN, 1);
-  EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<int> >::has_quiet_NaN,
-                  0);
+  EXPECT_FLOAT_EQ(1,
+                 std::numeric_limits<stan::agrad::fvar<double> >::has_quiet_NaN);
+  EXPECT_FLOAT_EQ(0,
+                  std::numeric_limits<stan::agrad::fvar<int> >::has_quiet_NaN);
 
-  EXPECT_FLOAT_EQ(
-        std::numeric_limits<stan::agrad::fvar<double> >::has_signaling_NaN, 1);
-  EXPECT_FLOAT_EQ(
-           std::numeric_limits<stan::agrad::fvar<int> >::has_signaling_NaN, 0);
+  EXPECT_FLOAT_EQ(1,
+             std::numeric_limits<stan::agrad::fvar<double> >::has_signaling_NaN);
+  EXPECT_FLOAT_EQ(0,
+                std::numeric_limits<stan::agrad::fvar<int> >::has_signaling_NaN);
 
-  EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<double> >::has_denorm,
-                  1);
-  EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<int> >::has_denorm, 0);
+  EXPECT_FLOAT_EQ(1,
+                  std::numeric_limits<stan::agrad::fvar<double> >::has_denorm);
+  EXPECT_FLOAT_EQ(0, std::numeric_limits<stan::agrad::fvar<int> >::has_denorm);
 
-  EXPECT_FLOAT_EQ(
-          std::numeric_limits<stan::agrad::fvar<double> >::has_denorm_loss, 0);
-  EXPECT_FLOAT_EQ(
-             std::numeric_limits<stan::agrad::fvar<int> >::has_denorm_loss, 0);
+  EXPECT_FLOAT_EQ(0,
+            std::numeric_limits<stan::agrad::fvar<double> >::has_denorm_loss);
+  EXPECT_FLOAT_EQ(0,
+                 std::numeric_limits<stan::agrad::fvar<int> >::has_denorm_loss);
 
   fvar<int> i = std::numeric_limits<stan::agrad::fvar<int> >::infinity();
   fvar<double> j = std::numeric_limits<stan::agrad::fvar<double> >::infinity();
-  EXPECT_FLOAT_EQ(i.val_, 0);
-  EXPECT_FLOAT_EQ(j.val_, INFTY);
+  EXPECT_FLOAT_EQ(0, i.val_);
+  EXPECT_FLOAT_EQ(INFTY, j.val_);
 
   fvar<int> k = std::numeric_limits<stan::agrad::fvar<int> >::quiet_NaN();
   fvar<double> l = 
     std::numeric_limits<stan::agrad::fvar<double> >::quiet_NaN();
-  EXPECT_FLOAT_EQ(k.val_, 0);
+  EXPECT_FLOAT_EQ(0, k.val_);
   isnan(l.val_);
 
   fvar<int> m = std::numeric_limits<stan::agrad::fvar<int> >::signaling_NaN();
   fvar<double> n = 
     std::numeric_limits<stan::agrad::fvar<double> >::signaling_NaN();
-  EXPECT_FLOAT_EQ(m.val_, 0);
+  EXPECT_FLOAT_EQ(0, m.val_);
   isnan(n.val_);
 
   fvar<int> o = std::numeric_limits<stan::agrad::fvar<int> >::denorm_min();
   fvar<double> p = 
     std::numeric_limits<stan::agrad::fvar<double> >::denorm_min();
-  EXPECT_FLOAT_EQ(o.val_, 0);
-  EXPECT_FLOAT_EQ(p.val_, 4.94066e-324);
+  EXPECT_FLOAT_EQ(0, o.val_);
+  EXPECT_FLOAT_EQ(4.94066e-324, p.val_);
 
-  EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<double> >::is_iec559, 
-                  1);
-  EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<int> >::is_iec559, 0);
+  EXPECT_FLOAT_EQ(1,
+                  std::numeric_limits<stan::agrad::fvar<double> >::is_iec559);
+  EXPECT_FLOAT_EQ(0, std::numeric_limits<stan::agrad::fvar<int> >::is_iec559);
 
-  EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<double> >::is_bounded,
-                  1);
-  EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<int> >::is_bounded, 1);
+  EXPECT_FLOAT_EQ(1, 
+                  std::numeric_limits<stan::agrad::fvar<double> >::is_bounded);
+  EXPECT_FLOAT_EQ(1, std::numeric_limits<stan::agrad::fvar<int> >::is_bounded);
 
-  EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<double> >::is_modulo, 
-                  0);
-  EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<int> >::is_modulo, 1);
+  EXPECT_FLOAT_EQ(0,
+                  std::numeric_limits<stan::agrad::fvar<double> >::is_modulo);
+  EXPECT_FLOAT_EQ(1, std::numeric_limits<stan::agrad::fvar<int> >::is_modulo);
 
-  EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<double> >::traps, 0);
-  EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<int> >::traps, 1);
+  EXPECT_FLOAT_EQ(0, std::numeric_limits<stan::agrad::fvar<double> >::traps);
+  EXPECT_FLOAT_EQ(1, std::numeric_limits<stan::agrad::fvar<int> >::traps);
 
-  EXPECT_FLOAT_EQ(
-          std::numeric_limits<stan::agrad::fvar<double> >::tinyness_before, 0);
-  EXPECT_FLOAT_EQ(
-             std::numeric_limits<stan::agrad::fvar<int> >::tinyness_before, 0);
+  EXPECT_FLOAT_EQ(0,
+              std::numeric_limits<stan::agrad::fvar<double> >::tinyness_before);
+  EXPECT_FLOAT_EQ(0,
+                  std::numeric_limits<stan::agrad::fvar<int> >::tinyness_before);
 
-  EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<double> >::round_style,
-                  1);
-  EXPECT_FLOAT_EQ(std::numeric_limits<stan::agrad::fvar<int> >::round_style, 
-                  0);
+  EXPECT_FLOAT_EQ(1,
+                  std::numeric_limits<stan::agrad::fvar<double> >::round_style);
+  EXPECT_FLOAT_EQ(0,
+                  std::numeric_limits<stan::agrad::fvar<int> >::round_style);
 }
