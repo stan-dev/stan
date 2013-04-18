8f259f056ccb51a17feab67cb9c6de4934a858df
Peter Li <li.peter94@gmail.com>
2013-04-18 12:08:25 -0400

    added compilation of fvar/matrix tests for easy testing

---
+++ src/test/agrad/fvar_matrix_test.cpp
@@ -0,0 +1,70 @@
+
+
+#include <test/agrad/fwd/matrix/to_fvar_test.cpp>  
+
+//INTEGER-VALUED MATRIX SIZE FUNCTIONS -- DONE
+#include <test/agrad/fwd/matrix/rows_test.cpp>       
+#include <test/agrad/fwd/matrix/cols_test.cpp>       
+
+//MATRIX ARITHMETIC OPEPRATORS
+//Negation Prefix Operators -- DONE
+#include <test/agrad/fwd/matrix/minus_test.cpp>       
+
+//Infix Matrix Operators -- DONE
+#include <test/agrad/fwd/matrix/operator_addition_test.cpp>       
+#include <test/agrad/fwd/matrix/operator_subtraction_test.cpp>       
+#include <test/agrad/fwd/matrix/operator_multiplication_test.cpp>       
+#include <test/agrad/fwd/matrix/operator_division_test.cpp>       
+
+//Broadcast Infix Operators -- DONE
+  //Operator+ is in <test/agrad/fwd/matrix/operator_addition_test.cpp>
+  //Operator- is in <test/agrad/fwd/matrix/operator_subtraction_test.cpp>
+
+//Elementwise Products -- DONE
+#include <test/agrad/fwd/matrix/elt_multiply_test.cpp>       
+#include <test/agrad/fwd/matrix/elt_divide_test.cpp>       
+
+//Elementwise Logarithms -- DONE
+#include <test/agrad/fwd/matrix/log_test.cpp>       
+#include <test/agrad/fwd/matrix/exp_test.cpp>       
+
+//Cumulative Sums -- DONE
+#include <test/agrad/fwd/matrix/cumulative_sum_test.cpp>       
+
+//Dot Products -- DONE
+#include <test/agrad/fwd/matrix/dot_product_test.cpp>       
+#include <test/agrad/fwd/matrix/dot_self_test.cpp>     
+
+//Specialized Products
+
+//REDUCTIONS -- DONE
+//Minimum and Maximum -- DONE
+#include <test/agrad/fwd/matrix/min_test.cpp>     
+#include <test/agrad/fwd/matrix/max_test.cpp>     
+
+//Sums and Products -- DONE
+#include <test/agrad/fwd/matrix/sum_test.cpp>     
+#include <test/agrad/fwd/matrix/prod_test.cpp>     
+
+//Sample Moments -- DONE
+#include <test/agrad/fwd/matrix/mean_test.cpp>     
+#include <test/agrad/fwd/matrix/variance_test.cpp>     
+#include <test/agrad/fwd/matrix/sd_test.cpp>     
+
+//SLICE AND PACKAGE FUNCTIONS -- DONE
+//Diagonal Matrices -- DONE
+#include <test/agrad/fwd/matrix/diagonal_test.cpp>     
+#include <test/agrad/fwd/matrix/diag_matrix_test.cpp>     
+#include <test/agrad/fwd/matrix/col_test.cpp>     
+#include <test/agrad/fwd/matrix/row_test.cpp>     
+
+//Block Operation -- DONE
+#include <test/agrad/fwd/matrix/block_test.cpp>     
+
+//Transposition Postfix Operator -- DONE
+#include <test/agrad/fwd/matrix/transpose_test.cpp>     
+
+//SPECIAL MATRIX FUNCTIONS -- DONE
+#include <test/agrad/fwd/matrix/softmax_test.cpp>     
+
+//LINEAR ALGEBRA FUNCTIONS AND SOLVERS
