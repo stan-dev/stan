a8f91b63bfb4a8a5315a7b151f14091f7bd4cc51
Peter Li <li.peter94@gmail.com>
2013-04-24 17:23:46 -0400

    added unit_metro

---
+++ src/stan/mcmc/metro/unit_metro.hpp
@@ -0,0 +1,33 @@
+#ifndef __STAN__MCMC__UNIT__METRO__HPP
+#define __STAN__MCMC__UNIT__METRO__HPP
+
+#include <stan/mcmc/metro/base_metro.hpp>
+#include <stan/prob/distributions/univariate/continuous/normal.hpp>
+
+namespace stan {
+
+  namespace mcmc {
+    
+    template <class M, class BaseRNG>
+    class unit_metro: public base_metro<M, BaseRNG> {
+      
+    public:
+      
+      unit_metro(M& m, BaseRNG& rng): base_metro<M,
+                                                 BaseRNG>(m, rng)
+      { this->_name = "Metropolis with a unit metric"; }
+
+      void _propose(std::vector<double>& q,
+                    BaseRNG& rng) {
+        for (size_t i = 0; i < q.size(); ++i) 
+          q[i] = stan::prob::normal_rng(0,this->_nom_epsilon,this->_rand_int);
+      }
+                        
+    };
+
+  } // mcmc
+
+} // stan
+          
+
+#endif
