8a67ac2af88912132b1b94935ecbe195c82b9744
Peter Li <li.peter94@gmail.com>
2013-04-25 15:34:54 -0400

    added adapt_unit_metro

---
+++ src/stan/mcmc/metro/adapt_unit_metro.hpp
@@ -0,0 +1,41 @@
+#ifndef __STAN__MCMC__ADAPT__UNIT__METRO__HPP
+#define __STAN__MCMC__ADAPT__UNIT__METRO__HPP
+
+#include <stan/mcmc/stepsize_adapter.hpp>
+#include <stan/mcmc/metro/unit_metro.hpp>
+
+namespace stan {
+  
+  namespace mcmc {
+    
+    template <typename M, class BaseRNG>
+    class adapt_unit_metro: public unit_metro<M, BaseRNG>,
+                            public stepsize_adapter {
+      
+    public:
+      
+      adapt_unit_metro(M &m, BaseRNG& rng, std::ostream* error_msg): unit_metro<M, BaseRNG>(m, rng, error_msg) {};
+      
+      ~adapt_unit_metro() {};
+      
+      sample transition(sample& init_sample) {
+        
+        sample s = unit_metro<M, BaseRNG>::transition(init_sample);
+        
+        if (_adapt_flag) {      
+          _stepsize_adaptation.learn_stepsize(this->_nom_epsilon, 
+                                              s.accept_stat());
+        }
+        
+        return s;
+        
+      }
+      
+    };
+    
+  } // mcmc
+  
+} // stan
+
+
+#endif
