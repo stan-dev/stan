951b28226bc16a0c30f45355b1fb9960c958f4df
Ben Goodrich <bg2382@columbia.edu>
2013-05-06 18:32:11 -0400

    calculate average n_eff in rstan/example/demo.R

--- rstan/example/demo.R
+++ rstan/example/demo.R
@@ -8,12 +8,18 @@ bench <- function(i) {
   mark <- "elapsed"
   compile <- system.time(fit <- stan_demo(i, chains = 0, iter = 0))[mark]
   execute <- system.time(fit <- stan_demo(i, fit = fit, seed = 12345, refresh = -1))[mark]
-  if (length(dim(fit)) == 0) execute <- NA_real_ # failure
-  return(c(compile, execute))
+  if (length(dim(fit)) == 0) { # failure
+    execute <- NA_real_
+    n_eff <- NA_real_
+  }
+  else {
+    n_eff <- mean(summary(fit)$summary[,"n_eff"])
+  }
+  return(c(compile, execute, n_eff))
 }
 
 results <- mclapply(1:length(model_names), mc.cores = 4L, FUN = bench,
                     mc.preschedule = FALSE)
-results <- matrix(unlist(results), ncol = 2, byrow = TRUE)
+results <- matrix(unlist(results), ncol = length(results[[1]]), byrow = TRUE)
 rownames(results) <- model_names
 save(results, file = 'time1.RData', compress = 'xz')
