969c73ee56d37a6b4b40d8faa9f454c4614f940a
Marcus Brubaker <marcus.a.brubaker@gmail.com>
2013-05-17 15:03:38 -0400

    Update matrix includes to include quad_forms.

--- src/stan/agrad/matrix.hpp
+++ src/stan/agrad/matrix.hpp
@@ -6,6 +6,7 @@
 #include <stan/agrad/rev/matrix/initialize_variable.hpp>
 #include <stan/agrad/rev/matrix/typedefs.hpp>
 #include <stan/agrad/rev/matrix/to_var.hpp>
+#include <stan/agrad/rev/matrix/value_of.hpp>
 #include <stan/agrad/rev/matrix/squared_dist.hpp>
 #include <stan/agrad/rev/matrix/dot_self.hpp>
 #include <stan/agrad/rev/matrix/dot_product.hpp>
@@ -14,6 +15,7 @@
 #include <stan/agrad/rev/matrix/mdivide_left_spd.hpp>
 #include <stan/agrad/rev/matrix/mdivide_left_tri.hpp>
 #include <stan/agrad/rev/matrix/determinant.hpp>
+#include <stan/agrad/rev/matrix/quad_form.hpp>
 #include <stan/agrad/rev/matrix/ldlt.hpp>
 #include <stan/agrad/rev/matrix/log_determinant.hpp>
 #include <stan/agrad/rev/matrix/log_determinant_spd.hpp>
@@ -21,6 +23,7 @@
 #include <stan/agrad/rev/matrix/multiply.hpp>
 #include <stan/agrad/rev/matrix/multiply_lower_tri_self_transpose.hpp>
 #include <stan/agrad/rev/matrix/tcrossprod.hpp>
+#include <stan/agrad/rev/matrix/trace_quad_form.hpp>
 #include <stan/agrad/rev/matrix/crossprod.hpp>
 #include <stan/agrad/rev/matrix/assign_to_var.hpp>
 #include <stan/agrad/rev/matrix/assign.hpp>
