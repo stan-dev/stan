de3d0efc2bd6e3ebf8e715397b8ef4e0727bbc0e
Peter Li <li.peter94@gmail.com>
2013-05-28 14:44:43 -0400

    added fvar<var> and fvar<fvar<T>> tests for log1m_inv_logit, log1m, log1p_exp

--- src/test/agrad/fwd/log1m_test.cpp
+++ src/test/agrad/fwd/log1m_test.cpp
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
 #include <stan/agrad/fvar.hpp>
 #include <stan/math/functions/log1m.hpp>
+#include <stan/agrad/var.hpp>
+#include <test/agrad/util.hpp>
 
 TEST(AgradFvar, log1m){
   using stan::agrad::fvar;
@@ -26,3 +28,58 @@ TEST(AgradFvar, log1m){
   isnan(c.val_);
   isnan(c.d_);
 }
+
+
+TEST(AgradFvarVar, log1m) {
+  using stan::agrad::fvar;
+  using stan::agrad::var;
+  using stan::math::log1m;
+
+  fvar<var> x;
+  x.val_ = 0.5;
+  x.d_ = 1.3;
+  fvar<var> a = log1m(x);
+
+  EXPECT_FLOAT_EQ(log1m(0.5), a.val_.val());
+  EXPECT_FLOAT_EQ(-1.3 / (0.5), a.d_.val());
+
+  AVEC y = createAVEC(x.val_);
+  VEC g;
+  a.val_.grad(y,g);
+  EXPECT_FLOAT_EQ(-1 / (0.5), g[0]);
+
+  y = createAVEC(x.d_);
+  a.d_.grad(y,g);
+  EXPECT_FLOAT_EQ(0, g[0]);
+}
+
+TEST(AgradFvarFvar, log1m) {
+  using stan::agrad::fvar;
+  using stan::math::log1m;
+
+  fvar<fvar<double> > x;
+  x.val_.val_ = 0.5;
+  x.val_.d_ = 1.0;
+  x.d_.val_ = 0.0;
+  x.d_.d_ = 0.0;
+
+  fvar<fvar<double> > a = log1m(x);
+
+  EXPECT_FLOAT_EQ(log1m(0.5), a.val_.val_);
+  EXPECT_FLOAT_EQ(-1 / (0.5), a.val_.d_);
+  EXPECT_FLOAT_EQ(0, a.d_.val_);
+  EXPECT_FLOAT_EQ(0, a.d_.d_);
+
+  fvar<fvar<double> > y;
+  y.val_.val_ = 0.5;
+  y.val_.d_ = 0.0;
+  y.d_.val_ = 1.0;
+  y.d_.d_ = 0.0;
+
+  a = log1m(y);
+  EXPECT_FLOAT_EQ(log1m(0.5), a.val_.val_);
+  EXPECT_FLOAT_EQ(0, a.val_.d_);
+  EXPECT_FLOAT_EQ(-1 / (0.5), a.d_.val_);
+  EXPECT_FLOAT_EQ(0, a.d_.d_);
+}
+
--- src/test/agrad/fwd/log1p_exp_test.cpp
+++ src/test/agrad/fwd/log1p_exp_test.cpp
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
 #include <stan/agrad/fvar.hpp>
 #include <stan/math/functions/log1p_exp.hpp>
+#include <stan/agrad/var.hpp>
+#include <test/agrad/util.hpp>
 
 TEST(AgradFvar, log1p_exp){
   using stan::agrad::fvar;
@@ -21,4 +23,59 @@ TEST(AgradFvar, log1p_exp){
   fvar<double> b = log1p_exp(y);
   EXPECT_FLOAT_EQ(log1p_exp(1.0), b.val_);
   EXPECT_FLOAT_EQ(2.0 * exp(1.0) / (1 + exp(1.0)), b.d_);
+}
+
+TEST(AgradFvarVar, log1p_exp) {
+  using stan::agrad::fvar;
+  using stan::agrad::var;
+  using stan::math::log1p_exp;
+  using std::exp;
+
+  fvar<var> x;
+  x.val_ = 0.5;
+  x.d_ = 1.3;
+  fvar<var> a = log1p_exp(x);
+
+  EXPECT_FLOAT_EQ(log1p_exp(0.5), a.val_.val());
+  EXPECT_FLOAT_EQ(1.3 * exp(0.5) / (1 + exp(0.5)), a.d_.val());
+
+  AVEC y = createAVEC(x.val_);
+  VEC g;
+  a.val_.grad(y,g);
+  EXPECT_FLOAT_EQ(exp(0.5) / (1 + exp(0.5)), g[0]);
+
+  y = createAVEC(x.d_);
+  a.d_.grad(y,g);
+  EXPECT_FLOAT_EQ(0, g[0]);
+}
+
+TEST(AgradFvarFvar, log1p_exp) {
+  using stan::agrad::fvar;
+  using stan::math::log1p_exp;
+  using std::exp;
+
+  fvar<fvar<double> > x;
+  x.val_.val_ = 0.5;
+  x.val_.d_ = 1.0;
+  x.d_.val_ = 0.0;
+  x.d_.d_ = 0.0;
+
+  fvar<fvar<double> > a = log1p_exp(x);
+
+  EXPECT_FLOAT_EQ(log1p_exp(0.5), a.val_.val_);
+  EXPECT_FLOAT_EQ(exp(0.5) / (1 + exp(0.5)), a.val_.d_);
+  EXPECT_FLOAT_EQ(0, a.d_.val_);
+  EXPECT_FLOAT_EQ(0, a.d_.d_);
+
+  fvar<fvar<double> > y;
+  y.val_.val_ = 0.5;
+  y.val_.d_ = 0.0;
+  y.d_.val_ = 1.0;
+  y.d_.d_ = 0.0;
+
+  a = log1p_exp(y);
+  EXPECT_FLOAT_EQ(log1p_exp(0.5), a.val_.val_);
+  EXPECT_FLOAT_EQ(0, a.val_.d_);
+  EXPECT_FLOAT_EQ(exp(0.5) / (1 + exp(0.5)), a.d_.val_);
+  EXPECT_FLOAT_EQ(0, a.d_.d_);
 }
