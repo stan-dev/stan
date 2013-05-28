a20c39c07c0489a681480cb2908944725453d2e7
Peter Li <li.peter94@gmail.com>
2013-05-28 13:16:26 -0400

    added fvar<var> and fvar<fvar<T>> tests to ceil and cbrt

--- src/test/agrad/fwd/cbrt_test.cpp
+++ src/test/agrad/fwd/cbrt_test.cpp
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
 #include <stan/agrad/fvar.hpp>
 #include <boost/math/special_functions/cbrt.hpp>
+#include <stan/agrad/var.hpp>
+#include <test/agrad/util.hpp>
 
 TEST(AgradFvar, cbrt) {
   using stan::agrad::fvar;
@@ -35,4 +37,57 @@ TEST(AgradFvar, cbrt) {
   fvar<double> f = cbrt(y);
   EXPECT_FLOAT_EQ(cbrt(0.0), f.val_);
   isnan(f.d_);
+}
+
+TEST(AgradFvarVar, cbrt) {
+  using stan::agrad::fvar;
+  using stan::agrad::var;
+  using boost::math::cbrt;
+
+  fvar<var> x;
+  x.val_ = 1.5;
+  x.d_ = 1.3;
+  fvar<var> a = cbrt(x);
+
+  EXPECT_FLOAT_EQ(cbrt(1.5), a.val_.val());
+  EXPECT_FLOAT_EQ(1.3 / (3 * cbrt(1.5) * cbrt(1.5)), a.d_.val());
+
+  AVEC y = createAVEC(x.val_);
+  VEC g;
+  a.val_.grad(y,g);
+  EXPECT_FLOAT_EQ(1.0 / (3.0 * cbrt(1.5) * cbrt(1.5)), g[0]);
+
+  y = createAVEC(x.d_);
+  a.d_.grad(y,g);
+  EXPECT_FLOAT_EQ(0, g[0]);
+}
+
+TEST(AgradFvarFvar, cbrt) {
+  using stan::agrad::fvar;
+  using boost::math::cbrt;
+
+  fvar<fvar<double> > x;
+  x.val_.val_ = 1.5;
+  x.val_.d_ = 2.0;
+  x.d_.val_ = 0.0;
+  x.d_.d_ = 0.0;
+
+  fvar<fvar<double> > a = cbrt(x);
+
+  EXPECT_FLOAT_EQ(cbrt(1.5), a.val_.val_);
+  EXPECT_FLOAT_EQ(2.0 / (3.0 * cbrt(1.5) * cbrt(1.5)), a.val_.d_);
+  EXPECT_FLOAT_EQ(0, a.d_.val_);
+  EXPECT_FLOAT_EQ(0, a.d_.d_);
+
+  fvar<fvar<double> > y;
+  y.val_.val_ = 1.5;
+  y.val_.d_ = 0.0;
+  y.d_.val_ = 2.0;
+  y.d_.d_ = 0.0;
+
+  a = cbrt(y);
+  EXPECT_FLOAT_EQ(cbrt(1.5), a.val_.val_);
+  EXPECT_FLOAT_EQ(0, a.val_.d_);
+  EXPECT_FLOAT_EQ(2.0 / (3.0 * cbrt(1.5) * cbrt(1.5)), a.d_.val_);
+  EXPECT_FLOAT_EQ(0, a.d_.d_);
 }
