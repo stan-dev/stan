160aca9cf1bd409bb640fcc9fe5a5859c5d89e80
Peter Li <li.peter94@gmail.com>
2013-05-29 11:35:47 -0400

    added fvar<fvar<T>> test to multiply_log

--- src/test/agrad/fwd/multiply_log_test.cpp
+++ src/test/agrad/fwd/multiply_log_test.cpp
@@ -40,3 +40,56 @@ TEST(AgradFvar,multiply_log) {
   isnan(e.val_);
   isnan(e.d_);
 }
+
+// TEST(AgradFvarVar, multiply_log) {
+//   using stan::agrad::fvar;
+//   using stan::agrad::var;
+//   using std::log;
+//   using stan::math::multiply_log;
+
+//   fvar<var> x;
+//   x.val_ = 1.5;
+//   x.d_ = 1.3;
+
+//   fvar<var> z;
+//   x.val_ = 1.8;
+//   x.d_ = 1.1;
+//   fvar<var> a = multiply_log(x,z);
+
+//   EXPECT_FLOAT_EQ(multiply_log(1.5,1.8), a.val_.val());
+//   EXPECT_FLOAT_EQ(log(1.8) * 1.3 + 1.5 * 1.1 / 1.8, a.d_.val());
+
+//   AVEC y = createAVEC(x.val_);
+//   VEC g;
+//   a.val_.grad(y,g);
+//   EXPECT_FLOAT_EQ(log(1.8) / 1.5 + log(1.5) / 1.8, g[0]);
+
+//   y = createAVEC(x.d_);
+//   a.d_.grad(y,g);
+//   EXPECT_FLOAT_EQ(0, g[0]);
+// }
+
+TEST(AgradFvarFvar, multiply_log) {
+  using stan::agrad::fvar;
+  using std::log;
+  using stan::math::multiply_log;
+
+  fvar<fvar<double> > x;
+  x.val_.val_ = 1.5;
+  x.val_.d_ = 1.3;
+  x.d_.val_ = 0.0;
+  x.d_.d_ = 0.0;
+
+  fvar<fvar<double> > y;
+  y.val_.val_ = 1.8;
+  y.val_.d_ = 0;
+  y.d_.val_ = 1.1;
+  y.d_.d_ = 0.0;
+
+  fvar<fvar<double> > a = multiply_log(x,y);
+
+  EXPECT_FLOAT_EQ(multiply_log(1.5,1.8), a.val_.val_);
+  EXPECT_FLOAT_EQ(log(1.8) * 1.3, a.val_.d_);
+  EXPECT_FLOAT_EQ(1.5 / 1.8 * 1.1, a.d_.val_);
+  EXPECT_FLOAT_EQ(143.0 / 180.0, a.d_.d_);
+}
