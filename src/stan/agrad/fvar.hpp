a60c56a761884f7be37751993ac4f62f2c141d13
Peter Li <li.peter94@gmail.com>
2013-05-29 16:43:16 -0400

    added bessel_second_kind to fvar with tests

--- src/stan/agrad/fvar.hpp
+++ src/stan/agrad/fvar.hpp
@@ -101,4 +101,5 @@
 #include <stan/agrad/fwd/log1m_inv_logit.hpp>
 
 #include <stan/agrad/fwd/bessel_first_kind.hpp>
+#include <stan/agrad/fwd/bessel_second_kind.hpp>
 #endif
---
+++ src/test/agrad/fwd/bessel_second_kind_test.cpp
@@ -0,0 +1,29 @@
+#include <gtest/gtest.h>
+#include <stan/agrad/fvar.hpp>
+
+TEST(AgradFvar, bessel_second_kind) {
+  using stan::agrad::fvar;
+  using stan::agrad::bessel_second_kind;
+
+  fvar<double> a;
+  a.val_ = 4.0;
+  a.d_ = 1.0;
+  int b = 0;
+  fvar<double> x = bessel_second_kind(b,a);
+  EXPECT_FLOAT_EQ(-0.01694073932506499190363513444715321824049258989801, 
+                  x.val_);
+  EXPECT_FLOAT_EQ(-0.39792571055710000525397997245079185227118918162290,
+                  x.d_);
+
+  fvar<double> c;
+  c.val_ = 3.0;
+  c.d_ = 2.0;
+
+  x = bessel_second_kind(1, c);
+  EXPECT_FLOAT_EQ(0.3246744247917999784370128392879532396692751433723549,
+                  x.val_);
+  EXPECT_FLOAT_EQ(0.5372504034977141116428784919345973293208741759303264,
+                  x.d_);
+
+  EXPECT_THROW(bessel_second_kind(0, -a), std::domain_error);
+}
