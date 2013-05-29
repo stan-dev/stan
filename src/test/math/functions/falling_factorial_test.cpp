e3a360e15c2ba119163fd4a654bf699b52885c0f
Peter Li <li.peter94@gmail.com>
2013-05-29 18:37:26 -0400

    modified falling_factorial and rising_factorial tests

--- src/test/math/functions/falling_factorial_test.cpp
+++ src/test/math/functions/falling_factorial_test.cpp
@@ -4,7 +4,7 @@
 TEST(MathFunctions, falling_factorial) {
   using stan::math::falling_factorial;
   
-  EXPECT_FLOAT_EQ(4, falling_factorial(4,3));
-  EXPECT_FLOAT_EQ(0.25, falling_factorial(3,4));
+  EXPECT_FLOAT_EQ(4, falling_factorial(4.0,3));
+  EXPECT_FLOAT_EQ(0.25, falling_factorial(3.0,4));
   EXPECT_THROW(falling_factorial(-1, 4), std::domain_error);
 }
