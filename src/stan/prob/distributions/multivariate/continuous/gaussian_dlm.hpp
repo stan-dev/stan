08f1b450da6b1fac7f6423b750c75b9e17305d45
Jeff Arnold <jeffrey.arnold@gmail.com>
2013-06-13 11:07:20 -0400

    update signatures

--- src/stan/prob/distributions/multivariate/continuous/gaussian_dlm.hpp
+++ src/stan/prob/distributions/multivariate/continuous/gaussian_dlm.hpp
@@ -19,22 +19,23 @@
 #include <stan/math/matrix/transpose.hpp>
 #include <stan/math/matrix/inverse.hpp>
 #include <stan/math/matrix/ldlt.hpp>
+#include <stan/math/matrix/trace_quad_form.hpp>
+#include <stan/math/matrix/quad_form.hpp>
+#include <stan/math/matrix/tcrossprod.hpp>
 
 namespace stan {
   namespace prob {
     /**
-     * The log of a multivariate Gaussian Process for the given y, Sigma, and
-     * w.  y is a dxN matrix, where each column is a different observation and each
-     * row is a different output dimension.  The Guassian Process is assumed to
-     * have a scaled kernel matrix with a different scale for each output dimension.
-     * This distribution is equivalent to, for \f$t = 1:N$,
+     * The log of a Gaussian dynamic linear model (GDLM).
+     * This distribution is equivalent to, for \f$t = 1:N\f$,
      * \f{eqnarray*}{
      * y_t & \sim N(F' \theta_t, V) \\
      * \theta_t & \sim N(G \theta_{t-1}, W) \\
      * \theta_0 & \sim N(0, diag(10^{6}))
-     * }
+     * \f}
      *
-     * @param y A r x T matrix of observations.
+     * @param y A r x T matrix of observations. Rows are variables,
+     * columns are observations.
      * @param F A n x r matrix. The design matrix.
      * @param G A n x n matrix. The transition matrix.
      * @param V A r x r matrix. The observation covariance matrix.
@@ -72,9 +73,10 @@ namespace stan {
       using stan::math::transpose;
       using stan::math::inverse;
       using stan::math::subtract;
-      using stan::math::LDLT_factor;
-      using stan::math::trace_inv_quad_form_ldlt;
-      using stan::math::mdivide_right_ldlt;
+      using stan::math::quad_form_sym;
+      // using stan::math::LDLT_factor;
+      // using stan::math::trace_inv_quad_form_ldlt;
+      // using stan::math::mdivide_right_ldlt;
 
       int r = y.rows(); // number of variables
       int T = y.cols(); // number of observations
@@ -118,7 +120,7 @@ namespace stan {
         return lp;
 
       if (include_summand<propto>::value) {
-        lp += 0.5 *NEG_LOG_SQRT_TWO_PI * r * T;
+        lp += 0.5 * NEG_LOG_SQRT_TWO_PI * r * T;
       }
       
       if (include_summand<propto,T_y,T_F,T_G,T_V,T_W>::value) {
@@ -127,14 +129,10 @@ namespace stan {
         // boost::math::tools::promote_args (max 6)
         // and would require altering function signatures in the
         // parser to accept more arguments.
-        Eigen::Matrix<typename 
-                      boost::math::tools::promote_args<T_y,T_F,T_G,T_V,T_W>::type,
-                      Eigen::Dynamic, 1> m(n);
+        Eigen::Matrix<T_lp,Eigen::Dynamic,1> m(n);
         for (int i = 0; i < m.size(); i ++)
           m(i) = 0.0;
-        Eigen::Matrix<typename 
-                      boost::math::tools::promote_args<T_y,T_F,T_G,T_V,T_W>::type,
-                      Eigen::Dynamic, Eigen::Dynamic> C(n, n);
+        Eigen::Matrix<T_lp,Eigen::Dynamic, Eigen::Dynamic> C(n, n);
         for (int i = 0; i < C.rows(); i ++) {
           for (int j = 0; j < C.cols(); j ++) {          
             if (i == j) {
@@ -145,30 +143,15 @@ namespace stan {
           }
         }
 
-          Eigen::Matrix<typename
-                        boost::math::tools::promote_args<T_y>::type,
-                        Eigen::Dynamic, 1> yi(r);
-          Eigen::Matrix<typename 
-                        boost::math::tools::promote_args<T_y,T_F,T_G,T_V,T_W>::type,
-                        Eigen::Dynamic, 1> a(n);
-          Eigen::Matrix<typename
-                        boost::math::tools::promote_args<T_y,T_F,T_G,T_V,T_W>::type,
-                        Eigen::Dynamic, Eigen::Dynamic> R(n, n);
-          Eigen::Matrix<typename
-                        boost::math::tools::promote_args<T_y,T_F,T_G,T_V,T_W>::type,
-                        Eigen::Dynamic, 1> f(r);
-          Eigen::Matrix<typename
-                        boost::math::tools::promote_args<T_y,T_F,T_G,T_V,T_W>::type,
-                        Eigen::Dynamic, Eigen::Dynamic> Q(r, r);
-          Eigen::Matrix<typename
-                        boost::math::tools::promote_args<T_y,T_F,T_G,T_V,T_W>::type,
-                        Eigen::Dynamic, Eigen::Dynamic> Q_inv(r, r);
-          Eigen::Matrix<typename
-                        boost::math::tools::promote_args<T_y,T_F,T_G,T_V,T_W>::type,
-                        Eigen::Dynamic, 1> e(r);
-          Eigen::Matrix<typename
-                        boost::math::tools::promote_args<T_y,T_F,T_G,T_V,T_W>::type,
-                        Eigen::Dynamic, Eigen::Dynamic> A(n, r);
+        Eigen::Matrix<typename boost::math::tools::promote_args<T_y>::type,
+                      Eigen::Dynamic, 1> yi(r);
+        Eigen::Matrix<T_lp,Eigen::Dynamic, 1> a(n);
+        Eigen::Matrix<T_lp,Eigen::Dynamic, Eigen::Dynamic> R(n, n);
+        Eigen::Matrix<T_lp,Eigen::Dynamic, 1> f(r);
+        Eigen::Matrix<T_lp,Eigen::Dynamic, Eigen::Dynamic> Q(r, r);
+        Eigen::Matrix<T_lp,Eigen::Dynamic, Eigen::Dynamic> Q_inv(r, r);
+        Eigen::Matrix<T_lp,Eigen::Dynamic, 1> e(r);
+        Eigen::Matrix<T_lp,Eigen::Dynamic, Eigen::Dynamic> A(n, r);
 
         for (int i = 0; i < T; ++i) {
           yi = y.col(i);
@@ -179,9 +162,8 @@ namespace stan {
           f = multiply(transpose(F), a);
           Q = quad_form_sym(R, F) + V;
           Q_inv = inverse(Q);
-          // LDLT_factor<typename 
-          //             boost::math::tools::promote_args<T_y,T_F,T_G,T_V,T_W>::type,
-          //             Eigen::Dynamic,Eigen::Dynamic> ldlt_Q(Q);
+          // LDLT weirdly seems to be going slower.
+          // LDLT_factor<T_lp,Eigen::Dynamic,Eigen::Dynamic> ldlt_Q(Q);
           // if (!ldlt_Q.success()) {
           //   std::ostringstream message;
           //   message << "Q is not positive definite (%1%).";
@@ -216,31 +198,9 @@ namespace stan {
     }
 
     /**
-     * The log of a multivariate Gaussian Process for the given y, Sigma, and
-     * w.  y is a dxN matrix, where each column is a different observation and each
-     * row is a different output dimension.  The Guassian Process is assumed to
-     * have a scaled kernel matrix with a different scale for each output dimension.
-     * This distribution is equivalent to, for \f$t = 1:N$,
-     * \f{eqnarray*}{
-     * y_t & \sim N(F' \theta_t, V) \\
-     * \theta_t & \sim N(G \theta_{t-1}, W) \\
-     * \theta_0 & \sim N(0, diag(10^{6}))
-     * }
-     *
-     * @param y A r x T matrix of observations.
-     * @param F A n x r matrix. The design matrix.
-     * @param G A n x n matrix. The transition matrix.
-     * @param V A size r vector. The diagonal of the observation covariance matrix.
-     * @param W A n x n matrix. The state covariance matrix.
-     * @return The log of the joint density of the GDLM.
-     * @throw std::domain_error if a matrix in the Kalman filter is
-     * not semi-positive definite.
-     * @tparam T_y Type of scalar.
-     * @tparam T_F Type of design matrix.
-     * @tparam T_G Type of transition matrix.
-     * @tparam T_V Type of observation covariance matrix.
-     * @tparam T_W Type of state covariance matrix.
-     */
+     * If V is a vector, then the sequential version of the Kalman
+     * filter is used.
+     **/
     template <bool propto,
               typename T_y, 
               typename T_F, typename T_G,
@@ -260,14 +220,15 @@ namespace stan {
       using stan::math::check_size_match;
       using stan::math::check_finite;
       using stan::math::check_cov_matrix;
+      using stan::math::check_positive;
       using stan::math::add;
       using stan::math::multiply;
       using stan::math::transpose;
       using stan::math::inverse;
       using stan::math::subtract;
-      using stan::math::LDLT_factor;
-      using stan::math::trace_inv_quad_form_ldlt;
-      using stan::math::mdivide_right_ldlt;
+      using stan::math::trace_quad_form;
+      using stan::math::quad_form_sym;
+      using stan::math::tcrossprod;
 
       int r = y.rows(); // number of variables
       int T = y.cols(); // number of observations
@@ -291,10 +252,10 @@ namespace stan {
                             &lp))
         return lp;
       // check V
-      if (!check_cov_matrix(function, V, "V", &lp))
+      if (!check_positive(function, V, "V", &lp))
         return lp;
       if (!check_size_match(function,
-                            V.rows(), "rows of V",
+                            V.size(), "size of V",
                             y.rows(), "rows of y",
                             &lp))
         return lp;
@@ -311,7 +272,7 @@ namespace stan {
         return lp;
 
       if (include_summand<propto>::value) {
-        lp += 0.5 *NEG_LOG_SQRT_TWO_PI * r * T;
+        lp += 0.5 * NEG_LOG_SQRT_TWO_PI * r * T;
       }
       
       if (include_summand<propto,T_y,T_F,T_G,T_V,T_W>::value) {
@@ -320,14 +281,10 @@ namespace stan {
         // boost::math::tools::promote_args (max 6)
         // and would require altering function signatures in the
         // parser to accept more arguments.
-        Eigen::Matrix<typename 
-                      boost::math::tools::promote_args<T_y,T_F,T_G,T_V,T_W>::type,
-                      Eigen::Dynamic, 1> m(n);
+        Eigen::Matrix<T_lp, Eigen::Dynamic, 1> m(n);
         for (int i = 0; i < m.size(); i ++)
           m(i) = 0.0;
-        Eigen::Matrix<typename 
-                      boost::math::tools::promote_args<T_y,T_F,T_G,T_V,T_W>::type,
-                      Eigen::Dynamic, Eigen::Dynamic> C(n, n);
+        Eigen::Matrix<T_lp, Eigen::Dynamic, Eigen::Dynamic> C(n, n);
         for (int i = 0; i < C.rows(); i ++) {
           for (int j = 0; j < C.cols(); j ++) {          
             if (i == j) {
@@ -338,34 +295,31 @@ namespace stan {
           }
         }
 
-        Eigen::Matrix<typename 
-                      boost::math::tools::promote_args<T_y,T_F,T_G,T_V,T_W>::type,
-                      Eigen::Dynamic, 1> a(n);
-        Eigen::Matrix<typename
-                      boost::math::tools::promote_args<T_y,T_F,T_G,T_V,T_W>::type,
-                      Eigen::Dynamic, Eigen::Dynamic> R(n, n);
-        <typename boost::math::tools::promote_args<T_y,T_F,T_G,T_V,T_W>::type f(r);
-        <typename boost::math::tools::promote_args<T_y,T_F,T_G,T_V,T_W>::type Q;
-        <typename boost::math::tools::promote_args<T_y,T_F,T_G,T_V,T_W>::type Q_inv;
-        <typename boost::math::tools::promote_args<T_y,T_F,T_G,T_V,T_W>::type e;
-        Eigen::Matrix<typename
-                      boost::math::tools::promote_args<T_y,T_F,T_G,T_V,T_W>::type,
-                      Eigen::Dynamic, 1> A(n);
+        T_lp f;
+        T_lp Q;
+        T_lp Q_inv;
+        T_lp e;
+        Eigen::Matrix<T_lp, Eigen::Dynamic, 1> A(n);
         
-        for (int i = 0; i < T; ++i) {
-          // Predict
-          a = multiply(G, m);
-          R = quad_form_sym(C, transpose(G)) + W;
-          // Filter
-          for (int j = 0; i < r; ++j) {
-            <typename boost::math::tools::promote_args<T_y>::type yij(y(i, j));
-            // select row of F
-            f = multiply(transpose(f.col(j)), a);
-            Q = quad_form_sym(R, F) + V;
-            e = yi - f;
-            A = multiply(multiply(R, F), Q_inv);            
-            m = add(a, multiply(A, e));
-            C = subtract(R, quad_form_sym(Q, transpose(A)));
+        for (int i = 0; i < y.cols(); ++i) {
+          // Predict (reuse m and C)
+          m = multiply(G, m);
+          C = quad_form_sym(C, transpose(G)) + W;
+          for (int j = 0; j < y.rows(); ++j) {
+            T_lp yij(y(j, i));
+            Eigen::Matrix<T_lp, Eigen::Dynamic, 1> Fj = F.col(j);
+            // one step forecast of y(i, j)
+            f = dot_product(Fj, m);
+            Q = trace_quad_form(C, Fj) + V(j);
+            Q_inv = 1.0 / Q;
+            // posterior after observing y(i, j)
+            e = yij - f;
+            // A = multiply(multiply(C, Fj), Q_inv);            
+            A = C * Fj * Q_inv;
+            m += A * e;
+            // tcrossprod throws an error (ambiguous)
+            // C -= Q * tcrossprod(A);
+            C -= Q * transpose(A) * A;
             lp -= 0.5 * (abs(Q) + pow(e, 2) * Q_inv);
           }
         }
