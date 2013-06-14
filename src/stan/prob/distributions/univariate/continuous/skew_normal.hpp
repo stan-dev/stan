d4550a0c23c0b6d558d962b9126476f201dca1cf
Peter Li <li.peter94@gmail.com>
2013-06-14 16:35:37 -0400

    added cdf_log to skew_normal distr

--- src/stan/prob/distributions/univariate/continuous/skew_normal.hpp
+++ src/stan/prob/distributions/univariate/continuous/skew_normal.hpp
@@ -242,6 +242,96 @@ namespace stan {
       return operands_and_partials.to_var(cdf);    
     }
 
+    template <typename T_y, typename T_loc, typename T_scale, typename T_shape>
+    typename return_type<T_y,T_loc,T_scale,T_shape>::type
+    skew_normal_cdf_log(const T_y& y, const T_loc& mu, const T_scale& sigma, 
+                    const T_shape& alpha) {
+      static const char* function = "stan::prob::skew_normal_cdf_log(%1%)";
+
+      using stan::math::check_positive;
+      using stan::math::check_finite;
+      using stan::math::check_not_nan;
+      using stan::math::check_consistent_sizes;
+      using stan::agrad::owens_t;
+      using stan::math::owens_t;
+
+      double cdf_log(0.0);
+      
+      // check if any vectors are zero length
+      if (!(stan::length(y) 
+            && stan::length(mu) 
+            && stan::length(sigma)
+            && stan::length(alpha)))
+        return cdf_log;
+
+      if (!check_not_nan(function, y, "Random variable", &cdf_log))
+        return cdf_log;
+      if (!check_finite(function, mu, "Location parameter", &cdf_log))
+        return cdf_log;
+      if (!check_not_nan(function, sigma, "Scale parameter", &cdf_log))
+        return cdf_log;
+      if (!check_positive(function, sigma, "Scale parameter", &cdf_log))
+        return cdf_log;
+      if (!check_finite(function, alpha, "Shape parameter", &cdf_log))
+        return cdf_log;
+      if (!check_not_nan(function, alpha, "Shape parameter", &cdf_log))
+        return cdf_log;
+      if (!(check_consistent_sizes(function,
+                                   y,mu,sigma,alpha,
+                                   "Random variable","Location parameter",
+                                   "Scale parameter","Shape paramter",
+                                   &cdf_log)))
+        return cdf_log;
+
+      agrad::OperandsAndPartials<T_y, T_loc, T_scale, T_shape> 
+        operands_and_partials(y, mu, sigma, alpha);
+
+      using stan::math::SQRT_2;
+      using stan::math::pi;
+
+      VectorView<const T_y> y_vec(y);
+      VectorView<const T_loc> mu_vec(mu);
+      VectorView<const T_scale> sigma_vec(sigma);
+      VectorView<const T_shape> alpha_vec(alpha);
+      size_t N = max_size(y, mu, sigma, alpha);
+      const double SQRT_TWO_OVER_PI = std::sqrt(2.0 / stan::math::pi());
+
+      for (size_t n = 0; n < N; n++) {
+        const double y_dbl = value_of(y_vec[n]);
+        const double mu_dbl = value_of(mu_vec[n]);
+        const double sigma_dbl = value_of(sigma_vec[n]);
+        const double alpha_dbl = value_of(alpha_vec[n]);
+        const double alpha_dbl_sq = alpha_dbl * alpha_dbl;
+        const double diff = (y_dbl - mu_dbl) / sigma_dbl;
+        const double diff_sq = diff * diff;
+        const double scaled_diff =  diff / SQRT_2;
+        const double scaled_diff_sq =  diff_sq * 0.5;
+        const double cdf_log_ = 0.5 * erfc(-scaled_diff) - 2 * owens_t(diff, 
+                                                                 alpha_dbl);
+        //cdf_log
+        cdf_log += log(cdf_log_);
+
+        //gradients
+        const double deriv_erfc = SQRT_TWO_OVER_PI * 0.5 * exp(-scaled_diff_sq)
+          / sigma_dbl;
+        const double deriv_owens = erf(alpha_dbl * scaled_diff) 
+          * exp(-scaled_diff_sq) / SQRT_TWO_OVER_PI / (-2.0 * pi()) / sigma_dbl;
+        const double rep_deriv = (-2.0 * deriv_owens + deriv_erfc) / cdf_log_;
+
+        if (!is_constant_struct<T_y>::value)
+          operands_and_partials.d_x1[n] += rep_deriv;
+        if (!is_constant_struct<T_loc>::value)
+          operands_and_partials.d_x2[n] -= rep_deriv;
+        if (!is_constant_struct<T_scale>::value)
+          operands_and_partials.d_x3[n] -= rep_deriv * diff;
+        if (!is_constant_struct<T_shape>::value)
+          operands_and_partials.d_x4[n] += -2.0 * exp(-0.5 * diff_sq * (1.0 
+                   + alpha_dbl_sq)) / ((1 + alpha_dbl_sq) * 2.0 * pi()) / cdf_log_;
+      }
+
+      return operands_and_partials.to_var(cdf_log);    
+    }
+
     template <class RNG>
     inline double
     skew_normal_rng(const double mu,
