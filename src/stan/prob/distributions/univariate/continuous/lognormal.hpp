84966dd673b197cecc01e4e261eb8545e0519da7
Peter Li <li.peter94@gmail.com>
2013-06-14 14:15:14 -0400

    added operands_and_partials to cdf for lognormal

--- src/stan/prob/distributions/univariate/continuous/lognormal.hpp
+++ src/stan/prob/distributions/univariate/continuous/lognormal.hpp
@@ -26,7 +26,8 @@ namespace stan {
       using stan::is_constant_struct;
       using stan::math::check_not_nan;
       using stan::math::check_finite;
-      using stan::math::check_positive;      
+      using stan::math::check_positive;
+      using stan::math::check_nonnegative;      
       using stan::math::check_consistent_sizes;
       using stan::math::value_of;
       using stan::prob::include_summand;
@@ -43,6 +44,8 @@ namespace stan {
 
       // validate args (here done over var, which should be OK)
       if (!check_not_nan(function, y, "Random variable", &logp))
+        return logp;
+      if (!check_nonnegative(function, y, "Random variable", &logp))
         return logp;
       if (!check_finite(function, mu, "Location parameter", 
                         &logp))
@@ -145,28 +148,83 @@ namespace stan {
 
 
     template <typename T_y, typename T_loc, typename T_scale>
-    typename boost::math::tools::promote_args<T_y,T_loc,T_scale>::type
+    typename return_type<T_y,T_loc,T_scale>::type
     lognormal_cdf(const T_y& y, const T_loc& mu, const T_scale& sigma) {
       static const char* function = "stan::prob::lognormal_cdf(%1%)";
-
+      
+      double cdf = 1.0;
+      
       using stan::math::check_not_nan;
       using stan::math::check_finite;
+      using stan::math::check_nonnegative;
       using stan::math::check_positive;
       using boost::math::tools::promote_args;
 
-      typename promote_args<T_y,T_loc,T_scale>::type lp;
-      if (!check_not_nan(function, y, "Random variable", &lp))
-        return lp;
-      if (!check_finite(function, mu, "Location parameter", &lp))
-        return lp;
-      if (!check_finite(function, sigma, "Scale parameter", 
-                        &lp))
-        return lp;
-      if (!check_positive(function, sigma, "Scale parameter", 
-                          &lp))
-        return lp;
+      // check if any vectors are zero length
+      if (!(stan::length(y) 
+            && stan::length(mu) 
+            && stan::length(sigma)))
+        return cdf;
 
-      return 0.5 * erfc(-(log(y) - mu)/(sigma * SQRT_2));
+      if (!check_not_nan(function, y, "Random variable", &cdf))
+        return cdf;
+      if (!check_nonnegative(function, y, "Random variable", &cdf))
+        return cdf;
+      if (!check_finite(function, mu, "Location parameter", &cdf))
+        return cdf;
+      if (!check_finite(function, sigma, "Scale parameter", &cdf))
+        return cdf;
+      if (!check_positive(function, sigma, "Scale parameter", &cdf))
+        return cdf;
+
+      agrad::OperandsAndPartials<T_y, T_loc, T_scale> 
+        operands_and_partials(y, mu, sigma);
+
+      VectorView<const T_y> y_vec(y);
+      VectorView<const T_loc> mu_vec(mu);
+      VectorView<const T_scale> sigma_vec(sigma);
+      size_t N = max_size(y, mu, sigma);
+
+      const double sqrt_pi = std::sqrt(stan::math::pi());
+
+      for (size_t i = 0; i < stan::length(y); i++) {
+        if (value_of(y_vec[i]) == 0.0) 
+          return operands_and_partials.to_var(0.0);
+      }
+
+      for (size_t n = 0; n < N; n++) {
+        const double y_dbl = value_of(y_vec[n]);
+        const double mu_dbl = value_of(mu_vec[n]);
+        const double sigma_dbl = value_of(sigma_vec[n]);
+        const double scaled_diff = (log(y_dbl) - mu_dbl) / (sigma_dbl * SQRT_2);
+        const double rep_deriv = SQRT_2 * 0.5 / sqrt_pi 
+          * exp(-scaled_diff * scaled_diff) / sigma_dbl;
+
+        //cdf
+        const double cdf_ = 0.5 * erfc(-scaled_diff);
+        cdf *= cdf_;
+
+        //gradients
+       if (!is_constant_struct<T_y>::value)
+          operands_and_partials.d_x1[n] += rep_deriv / cdf_ / y_dbl ;
+        if (!is_constant_struct<T_loc>::value)
+          operands_and_partials.d_x2[n] -= rep_deriv / cdf_ ;
+        if (!is_constant_struct<T_scale>::value)
+          operands_and_partials.d_x3[n] -= rep_deriv * scaled_diff * SQRT_2 
+            / cdf_;
+      }
+
+      if (!is_constant_struct<T_y>::value)
+        for (size_t n = 0; n < stan::length(y); ++n) 
+          operands_and_partials.d_x1[n] *= cdf;
+      if (!is_constant_struct<T_loc>::value)
+        for (size_t n = 0; n < stan::length(mu); ++n) 
+          operands_and_partials.d_x2[n] *= cdf;
+      if (!is_constant_struct<T_scale>::value)
+        for (size_t n = 0; n < stan::length(sigma); ++n) 
+          operands_and_partials.d_x3[n] *= cdf;
+
+      return operands_and_partials.to_var(cdf);
     }
 
     template <class RNG>
