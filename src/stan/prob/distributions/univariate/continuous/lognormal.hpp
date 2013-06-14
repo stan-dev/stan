6d8e99f07b97368ac45db2470366a8f4209b5294
Peter Li <li.peter94@gmail.com>
2013-06-14 14:22:51 -0400

    added cdf_log to lognormal distr

--- src/stan/prob/distributions/univariate/continuous/lognormal.hpp
+++ src/stan/prob/distributions/univariate/continuous/lognormal.hpp
@@ -227,6 +227,78 @@ namespace stan {
       return operands_and_partials.to_var(cdf);
     }
 
+    template <typename T_y, typename T_loc, typename T_scale>
+    typename return_type<T_y,T_loc,T_scale>::type
+    lognormal_cdf_log(const T_y& y, const T_loc& mu, const T_scale& sigma) {
+      static const char* function = "stan::prob::lognormal_cdf_log(%1%)";
+      
+      double cdf_log = 0.0;
+      
+      using stan::math::check_not_nan;
+      using stan::math::check_finite;
+      using stan::math::check_nonnegative;
+      using stan::math::check_positive;
+      using boost::math::tools::promote_args;
+
+      // check if any vectors are zero length
+      if (!(stan::length(y) 
+            && stan::length(mu) 
+            && stan::length(sigma)))
+        return cdf_log;
+
+      if (!check_not_nan(function, y, "Random variable", &cdf_log))
+        return cdf_log;
+      if (!check_nonnegative(function, y, "Random variable", &cdf_log))
+        return cdf_log;
+      if (!check_finite(function, mu, "Location parameter", &cdf_log))
+        return cdf_log;
+      if (!check_finite(function, sigma, "Scale parameter", &cdf_log))
+        return cdf_log;
+      if (!check_positive(function, sigma, "Scale parameter", &cdf_log))
+        return cdf_log;
+
+      agrad::OperandsAndPartials<T_y, T_loc, T_scale> 
+        operands_and_partials(y, mu, sigma);
+
+      VectorView<const T_y> y_vec(y);
+      VectorView<const T_loc> mu_vec(mu);
+      VectorView<const T_scale> sigma_vec(sigma);
+      size_t N = max_size(y, mu, sigma);
+
+      const double sqrt_pi = std::sqrt(stan::math::pi());
+
+      for (size_t i = 0; i < stan::length(y); i++) {
+        if (value_of(y_vec[i]) == 0.0) 
+          return operands_and_partials.to_var(stan::math::negative_infinity());
+      }
+
+      const double log_half = std::log(0.5);
+
+      for (size_t n = 0; n < N; n++) {
+        const double y_dbl = value_of(y_vec[n]);
+        const double mu_dbl = value_of(mu_vec[n]);
+        const double sigma_dbl = value_of(sigma_vec[n]);
+        const double scaled_diff = (log(y_dbl) - mu_dbl) / (sigma_dbl * SQRT_2);
+        const double rep_deriv = SQRT_2 / sqrt_pi 
+          * exp(-scaled_diff * scaled_diff) / sigma_dbl;
+
+        //cdf_log
+        const double erfc_calc = erfc(-scaled_diff);
+        cdf_log += log_half + log(erfc_calc);
+
+        //gradients
+       if (!is_constant_struct<T_y>::value)
+          operands_and_partials.d_x1[n] += rep_deriv / erfc_calc / y_dbl ;
+        if (!is_constant_struct<T_loc>::value)
+          operands_and_partials.d_x2[n] -= rep_deriv / erfc_calc;
+        if (!is_constant_struct<T_scale>::value)
+          operands_and_partials.d_x3[n] -= rep_deriv * scaled_diff * SQRT_2 
+            / erfc_calc;
+      }
+
+      return operands_and_partials.to_var(cdf_log);
+    }
+
     template <class RNG>
     inline double
     lognormal_rng(const double mu,
