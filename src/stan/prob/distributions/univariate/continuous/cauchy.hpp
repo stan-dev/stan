eeaf7f58114461b964db828a5507e99b8183ddd0
Peter Li <li.peter94@gmail.com>
2013-06-16 12:24:56 -0400

    added ccdf_log to cauchy distr

--- src/stan/prob/distributions/univariate/continuous/cauchy.hpp
+++ src/stan/prob/distributions/univariate/continuous/cauchy.hpp
@@ -329,7 +329,7 @@ namespace stan {
         const double Pn = atan(z) / pi() + 0.5;
         cdf_log += log(Pn);
             
-        const double rep_deriv = 1.0 / ((atan(z) + stan::math::pi() / 2.0) 
+        const double rep_deriv = 1.0 / (pi() * Pn 
                                         * (z * z * sigma_dbl + sigma_dbl));
         if (!is_constant_struct<T_y>::value)
           operands_and_partials.d_x1[n] += rep_deriv;
@@ -339,6 +339,82 @@ namespace stan {
           operands_and_partials.d_x3[n] -= rep_deriv * z;
       }
       return operands_and_partials.to_var(cdf_log);
+    }
+
+    template <typename T_y, typename T_loc, typename T_scale>
+    typename return_type<T_y,T_loc,T_scale>::type
+    cauchy_ccdf_log(const T_y& y, const T_loc& mu, const T_scale& sigma) {
+        
+      // Size checks
+      if ( !( stan::length(y) && stan::length(mu) 
+              && stan::length(sigma) ) ) 
+        return 0.0;
+        
+      static const char* function = "stan::prob::cauchy_cdf(%1%)";
+      
+      using stan::math::check_positive;
+      using stan::math::check_finite;
+      using stan::math::check_not_nan;
+      using stan::math::check_consistent_sizes;
+      using boost::math::tools::promote_args;
+      using stan::math::value_of;
+
+      double ccdf_log(0.0);
+        
+      if(!check_not_nan(function, y, "Random variable", &ccdf_log))
+        return ccdf_log;
+      if(!check_finite(function, mu, "Location parameter", &ccdf_log))
+        return ccdf_log;
+      if(!check_finite(function, sigma, "Scale parameter", &ccdf_log))
+        return ccdf_log;
+      if(!check_positive(function, sigma, "Scale parameter", &ccdf_log))
+        return ccdf_log;
+      if (!(check_consistent_sizes(function, y, mu, sigma,
+                                   "Random variable", "Location parameter", 
+                                   "Scale Parameter", &ccdf_log)))
+        return ccdf_log;
+        
+      // Wrap arguments in vectors
+      VectorView<const T_y> y_vec(y);
+      VectorView<const T_loc> mu_vec(mu);
+      VectorView<const T_scale> sigma_vec(sigma);
+      size_t N = max_size(y, mu, sigma);
+        
+      agrad::OperandsAndPartials<T_y, T_loc, T_scale> 
+        operands_and_partials(y, mu, sigma);
+        
+      std::fill(operands_and_partials.all_partials,
+                operands_and_partials.all_partials 
+                + operands_and_partials.nvaris, 0.0);
+        
+      // Compute CDFLog and its gradients
+      using std::atan;
+      using stan::math::pi;
+
+      // Compute vectorized CDF and gradient
+      for (size_t n = 0; n < N; n++) {
+            
+        // Pull out values
+        const double y_dbl = value_of(y_vec[n]);
+        const double mu_dbl = value_of(mu_vec[n]);
+        const double sigma_inv_dbl = 1.0 / value_of(sigma_vec[n]);
+        const double sigma_dbl = value_of(sigma_vec[n]);
+        const double z = (y_dbl - mu_dbl) * sigma_inv_dbl;
+          
+        // Compute
+        const double Pn = 0.5 - atan(z) / pi();
+        ccdf_log += log(Pn);
+            
+        const double rep_deriv = 1.0 / (Pn * pi() 
+                                        * (z * z * sigma_dbl + sigma_dbl));
+        if (!is_constant_struct<T_y>::value)
+          operands_and_partials.d_x1[n] -= rep_deriv;
+        if (!is_constant_struct<T_loc>::value)
+          operands_and_partials.d_x2[n] += rep_deriv;
+        if (!is_constant_struct<T_scale>::value)
+          operands_and_partials.d_x3[n] += rep_deriv * z;
+      }
+      return operands_and_partials.to_var(ccdf_log);
     }
 
     template <class RNG>
