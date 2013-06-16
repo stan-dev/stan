7382b4c374abf61247b76bc629ffd437f2f60f03
Peter Li <li.peter94@gmail.com>
2013-06-16 16:06:54 -0400

    added ccdf_log to logistic distr

--- src/stan/prob/distributions/univariate/continuous/logistic.hpp
+++ src/stan/prob/distributions/univariate/continuous/logistic.hpp
@@ -314,6 +314,95 @@ namespace stan {
       return operands_and_partials.to_var(P);
     }
 
+    template <typename T_y, typename T_loc, typename T_scale>
+    typename return_type<T_y, T_loc, T_scale>::type
+    logistic_ccdf_log(const T_y& y, const T_loc& mu, const T_scale& sigma) {
+          
+      // Size checks
+      if ( !( stan::length(y) && stan::length(mu) && stan::length(sigma) ) ) 
+        return 0.0;
+          
+      // Error checks
+      static const char* function = "stan::prob::logistic_cdf_log(%1%)";
+          
+      using stan::math::check_not_nan;
+      using stan::math::check_positive;
+      using stan::math::check_finite;
+      using stan::math::check_consistent_sizes;
+      using stan::math::value_of;
+      using boost::math::tools::promote_args;
+          
+      double P(0.0);
+          
+      if (!check_not_nan(function, y, "Random variable", &P))
+        return P;
+      if (!check_finite(function, mu, "Location parameter", &P))
+        return P;
+      if (!check_finite(function, sigma, "Scale parameter", &P))
+        return P;
+      if (!check_positive(function, sigma, "Scale parameter", &P))
+        return P;
+      if (!(check_consistent_sizes(function, y, mu, sigma,
+                                   "Random variable", "Location parameter", 
+                                   "Scale parameter", &P)))
+        return P;
+          
+      // Wrap arguments in vectors
+      VectorView<const T_y> y_vec(y);
+      VectorView<const T_loc> mu_vec(mu);
+      VectorView<const T_scale> sigma_vec(sigma);
+      size_t N = max_size(y, mu, sigma);
+          
+      agrad::OperandsAndPartials<T_y, T_loc, T_scale> 
+        operands_and_partials(y, mu, sigma);
+          
+      std::fill(operands_and_partials.all_partials,
+                operands_and_partials.all_partials 
+                + operands_and_partials.nvaris, 0.0);
+          
+      // Explicit return for extreme values
+      // The gradients are technically ill-defined, but treated as zero
+          
+      for (size_t i = 0; i < stan::length(y); i++) {
+        if (value_of(y_vec[i]) == -std::numeric_limits<double>::infinity()) 
+          return operands_and_partials.to_var(0.0);
+      }
+          
+      // Compute vectorized cdf_log and its gradients
+      for (size_t n = 0; n < N; n++) {
+              
+        // Explicit results for extreme values
+        // The gradients are technically ill-defined, but treated as zero
+        if (value_of(y_vec[n]) == std::numeric_limits<double>::infinity()) {
+          return operands_and_partials.to_var(stan::math::negative_infinity());
+        }
+              
+        // Pull out values
+        const double y_dbl = value_of(y_vec[n]);
+        const double mu_dbl = value_of(mu_vec[n]);
+        const double sigma_dbl = value_of(sigma_vec[n]);
+        const double sigma_inv_vec = 1.0 / value_of(sigma_vec[n]);
+              
+        // Compute
+        const double Pn = 1.0 - 1.0 / ( 1.0 + exp( - (y_dbl - mu_dbl) 
+                                             * sigma_inv_vec ) );
+        P += log(Pn);
+              
+        if (!is_constant_struct<T_y>::value)
+          operands_and_partials.d_x1[n] 
+            -= exp(logistic_log(y_dbl, mu_dbl, sigma_dbl)) / Pn;
+        if (!is_constant_struct<T_loc>::value)
+          operands_and_partials.d_x2[n] 
+            -= - exp(logistic_log(y_dbl, mu_dbl, sigma_dbl)) / Pn;
+        if (!is_constant_struct<T_scale>::value)
+          operands_and_partials.d_x3[n] 
+            -= - (y_dbl - mu_dbl) * sigma_inv_vec * exp(logistic_log(y_dbl, mu_dbl, sigma_dbl)) / Pn;
+              
+      }
+          
+      return operands_and_partials.to_var(P);
+    }
+
     template <class RNG>
     inline double
     logistic_rng(const double mu,
