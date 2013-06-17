1ba8a8e12a74a9ffa8e6de79595e29e20a8ac678
Peter Li <li.peter94@gmail.com>
2013-06-17 18:59:57 -0400

    added ccdf_log to beta_binomial distr

--- src/stan/prob/distributions/univariate/discrete/beta_binomial.hpp
+++ src/stan/prob/distributions/univariate/discrete/beta_binomial.hpp
@@ -456,6 +456,132 @@ DoubleVectorView<!is_constant_struct<T_size1>::value,
     return operands_and_partials.to_var(P);
   }
       
+  template <typename T_n, typename T_N, 
+            typename T_size1, typename T_size2>
+  typename return_type<T_size1,T_size2>::type
+  beta_binomial_ccdf_log(const T_n& n, const T_N& N, const T_size1& alpha, 
+                        const T_size2& beta) {
+          
+    static const char* function = "stan::prob::beta_binomial_ccdf_log(%1%)";
+          
+    using stan::math::check_finite;
+    using stan::math::check_nonnegative;
+    using stan::math::check_positive;
+    using stan::math::value_of;
+    using stan::math::check_consistent_sizes;
+    using stan::prob::include_summand;
+          
+    // Ensure non-zero argument lengths
+    if (!(stan::length(n) && stan::length(N) && stan::length(alpha) 
+          && stan::length(beta)))
+      return 0.0;
+          
+    double P(0.0);
+          
+    // Validate arguments
+    if (!check_nonnegative(function, N, "Population size parameter", &P))
+      return P;
+    if (!check_finite(function, alpha, "First prior sample size parameter", &P))
+      return P;
+    if (!check_positive(function, alpha, "First prior sample size parameter", &P))
+      return P;
+    if (!check_finite(function, beta, "Second prior sample size parameter", &P))
+      return P;
+    if (!check_positive(function, beta, "Second prior sample size parameter", &P))
+      return P;
+    if (!(check_consistent_sizes(function,
+                                 n, N, alpha, beta,
+                                 "Successes variable",
+                                 "Population size parameter",
+                                 "First prior sample size parameter",
+                                 "Second prior sample size parameter",
+                                 &P)))
+      return P;
+          
+    // Wrap arguments in vector views
+    VectorView<const T_n> n_vec(n);
+    VectorView<const T_N> N_vec(N);
+    VectorView<const T_size1> alpha_vec(alpha);
+    VectorView<const T_size2> beta_vec(beta);
+    size_t size = max_size(n, N, alpha, beta);
+          
+    // Compute vectorized cdf_log and gradient
+    using boost::math::lgamma;
+    using boost::math::digamma;
+
+    agrad::OperandsAndPartials<T_size1, T_size2> 
+      operands_and_partials(alpha, beta);
+          
+    std::fill(operands_and_partials.all_partials,
+              operands_and_partials.all_partials 
+              + operands_and_partials.nvaris, 
+              0.0);
+          
+    // Explicit return for extreme values
+    // The gradients are technically ill-defined, but treated as neg infinity
+    for (size_t i = 0; i < stan::length(n); i++) {
+      if (value_of(n_vec[i]) <= 0) 
+        return operands_and_partials.to_var(0.0);
+    }
+          
+    for (size_t i = 0; i < size; i++) {
+      // Explicit results for extreme values
+      // The gradients are technically ill-defined, but treated as zero
+      if (value_of(n_vec[i]) >= value_of(N_vec[i])) {
+        return operands_and_partials.to_var(stan::math::negative_infinity());
+      }
+              
+      const double n_dbl = value_of(n_vec[i]);
+      const double N_dbl = value_of(N_vec[i]);
+      const double alpha_dbl = value_of(alpha_vec[i]);
+      const double beta_dbl = value_of(beta_vec[i]);
+              
+      const double mu = alpha_dbl + n_dbl + 1;
+      const double nu = beta_dbl + N_dbl - n_dbl - 1;
+              
+      const double F = stan::math::F32(1, mu, -N_dbl + n_dbl + 1, n_dbl + 2, 
+                                       1 - nu, 1);
+              
+      double C = lgamma(nu) - lgamma(N_dbl - n_dbl);
+      C += lgamma(mu) - lgamma(n_dbl + 2);
+      C += lgamma(N_dbl + 2) - lgamma(N_dbl + alpha_dbl + beta_dbl);
+      C = std::exp(C);
+                
+      C *= F / boost::math::beta(alpha_dbl, beta_dbl);
+      C /= N_dbl + 1;
+              
+      const double Pi = C;
+              
+      P += log(Pi);
+              
+      double dF[6];
+      double digammaOne = 0;
+      double digammaTwo = 0;
+              
+      if ( (!is_constant_struct<T_size1>::value) 
+           || (!is_constant_struct<T_size2>::value) ) {
+        digammaOne = digamma(mu + nu);
+        digammaTwo = digamma(alpha_dbl + beta_dbl);
+        stan::math::gradF32(dF, 1, mu, -N_dbl + n_dbl + 1, n_dbl + 2,
+                            1 - nu, 1);
+      }
+      if (!is_constant_struct<T_size1>::value) {
+        const double g 
+          = - C * (digamma(mu) - digammaOne + dF[1] / F
+                   - digamma(alpha_dbl) + digammaTwo);
+        operands_and_partials.d_x1[i] -= g / Pi;
+      }
+      if (!is_constant_struct<T_size2>::value) {
+        const double g 
+          = - C * (digamma(nu) - digammaOne - dF[4] / F - digamma(beta_dbl) 
+                   + digammaTwo);
+        operands_and_partials.d_x2[i] -= g / Pi;
+      }
+    }
+          
+    return operands_and_partials.to_var(P);
+  }
+
   template <class RNG>
   inline int
   beta_binomial_rng(const int N,
