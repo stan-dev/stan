139c4b9699e0ce1974233c36bac5acfdabf2394c
Peter Li <li.peter94@gmail.com>
2013-06-19 14:57:20 -0400

    vectorized normal cdf and removed broken tests

--- src/stan/gm/function_signatures.hpp
+++ src/stan/gm/function_signatures.hpp
@@ -580,13 +580,12 @@ for (size_t i = 0; i < int_vector_types.size(); ++i)
 add("neg_binomial_rng",INT_T,DOUBLE_T,DOUBLE_T);
 add_nullary("negative_epsilon");
 add_nullary("negative_infinity");
-add_ternary("normal_cdf");
-// for (size_t i = 0; i < vector_types.size(); ++i)
-//   for (size_t j = 0; j < vector_types.size(); ++j)
-//     for (size_t k = 0; k < vector_types.size(); ++k)
-//       add("normal_cdf",
-//           DOUBLE_T, // result
-//           vector_types[i], vector_types[j], vector_types[k]); // args
+for (size_t i = 0; i < vector_types.size(); ++i)
+  for (size_t j = 0; j < vector_types.size(); ++j)
+    for (size_t k = 0; k < vector_types.size(); ++k)
+      add("normal_cdf",
+          DOUBLE_T, // result
+          vector_types[i], vector_types[j], vector_types[k]); // args
 for (size_t i = 0; i < vector_types.size(); ++i)
   for (size_t j = 0; j < vector_types.size(); ++j)
     for (size_t k = 0; k < vector_types.size(); ++k)
