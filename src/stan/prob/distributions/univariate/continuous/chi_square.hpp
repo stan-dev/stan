a2686a030cac94629cdb2e9ecc690eb2981fa63d
Peter Li <li.peter94@gmail.com>
2013-06-19 15:09:52 -0400

    added cdf_log and ccdf_log to chi_square distr

--- src/stan/prob/distributions/univariate/continuous/chi_square.hpp
+++ src/stan/prob/distributions/univariate/continuous/chi_square.hpp
@@ -263,6 +263,213 @@ namespace stan {
       return operands_and_partials.to_var(cdf);
     }
 
+    template <typename T_y, typename T_dof>
+    typename return_type<T_y,T_dof>::type
+    chi_square_cdf_log(const T_y& y, const T_dof& nu) {
+      static const char* function = "stan::prob::chi_square_cdf_log(%1%)";
+
+      using stan::math::check_positive;
+      using stan::math::check_finite;
+      using stan::math::check_nonnegative;
+      using stan::math::check_not_nan;
+      using stan::math::check_consistent_sizes;
+      using stan::math::value_of;
+
+      double cdf_log(0.0);
+
+     // Size checks
+      if (!(stan::length(y) && stan::length(nu))) 
+        return cdf_log;
+
+      if (!check_not_nan(function, y, "Random variable", &cdf_log))
+        return cdf_log;
+      if (!check_nonnegative(function, y, "Random variable", &cdf_log))
+        return cdf_log;
+      if (!check_finite(function, nu, "Degrees of freedom parameter", &cdf_log))
+        return cdf_log;
+      if (!check_positive(function, nu, "Degrees of freedom parameter", &cdf_log))
+        return cdf_log;
+      if (!(check_consistent_sizes(function,
+                                   y,nu,
+                                   "Random variable","Degrees of freedom parameter",
+                                   &cdf_log)))
+        return cdf_log;
+
+      // Wrap arguments in vectors
+      VectorView<const T_y> y_vec(y);
+      VectorView<const T_dof> nu_vec(nu);
+      size_t N = max_size(y, nu);
+          
+      agrad::OperandsAndPartials<T_y, T_dof> 
+        operands_and_partials(y, nu);
+          
+      std::fill(operands_and_partials.all_partials,
+                operands_and_partials.all_partials 
+                + operands_and_partials.nvaris, 0.0);
+          
+      // Explicit return for extreme values
+      // The gradients are technically ill-defined, but treated as zero
+      for (size_t i = 0; i < stan::length(y); i++) {
+        if (value_of(y_vec[i]) == 0) 
+          return operands_and_partials.to_var(stan::math::negative_infinity());
+      }
+          
+      // Compute cdf_log and its gradients
+      using boost::math::gamma_p_derivative;
+      using boost::math::gamma_p;
+      using boost::math::digamma;
+      using boost::math::tgamma;
+          
+      // Cache a few expensive function calls if nu is a parameter
+      DoubleVectorView<!is_constant_struct<T_dof>::value,
+                       is_vector<T_dof>::value> 
+        gamma_vec(stan::length(nu));
+      DoubleVectorView<!is_constant_struct<T_dof>::value,
+                       is_vector<T_dof>::value> 
+        digamma_vec(stan::length(nu));
+          
+      if (!is_constant_struct<T_dof>::value) {
+        for (size_t i = 0; i < stan::length(nu); i++) {
+          const double alpha_dbl = value_of(nu_vec[i]) * 0.5;
+          gamma_vec[i] = tgamma(alpha_dbl);
+          digamma_vec[i] = digamma(alpha_dbl);
+        }
+      }
+          
+      // Compute vectorized cdf_log and gradient
+      for (size_t n = 0; n < N; n++) {
+        // Explicit results for extreme values
+        // The gradients are technically ill-defined, but treated as zero
+        if (value_of(y_vec[n]) == std::numeric_limits<double>::infinity())
+          return operands_and_partials.to_var(0.0);
+              
+        // Pull out values
+        const double y_dbl = value_of(y_vec[n]);
+        const double alpha_dbl = value_of(nu_vec[n]) * 0.5;
+        const double beta_dbl = 0.5;
+              
+        // Compute
+        const double Pn = gamma_p(alpha_dbl, beta_dbl * y_dbl);
+              
+        cdf_log += log(Pn);
+              
+        if (!is_constant_struct<T_y>::value)
+          operands_and_partials.d_x1[n] 
+            += beta_dbl * gamma_p_derivative(alpha_dbl, beta_dbl * y_dbl) 
+            / Pn;
+        if (!is_constant_struct<T_dof>::value)
+          operands_and_partials.d_x2[n] 
+            -= 0.5 * stan::math::gradRegIncGamma(alpha_dbl, beta_dbl
+                                                 * y_dbl, gamma_vec[n],
+                                                 digamma_vec[n]) / Pn;
+      }
+          
+      return operands_and_partials.to_var(cdf_log);
+    }
+
+    template <typename T_y, typename T_dof>
+    typename return_type<T_y,T_dof>::type
+    chi_square_ccdf_log(const T_y& y, const T_dof& nu) {
+      static const char* function = "stan::prob::chi_square_ccdf_log(%1%)";
+
+      using stan::math::check_positive;
+      using stan::math::check_finite;
+      using stan::math::check_nonnegative;
+      using stan::math::check_not_nan;
+      using stan::math::check_consistent_sizes;
+      using stan::math::value_of;
+
+      double ccdf_log(0.0);
+
+     // Size checks
+      if (!(stan::length(y) && stan::length(nu))) 
+        return ccdf_log;
+
+      if (!check_not_nan(function, y, "Random variable", &ccdf_log))
+        return ccdf_log;
+      if (!check_nonnegative(function, y, "Random variable", &ccdf_log))
+        return ccdf_log;
+      if (!check_finite(function, nu, "Degrees of freedom parameter", &ccdf_log))
+        return ccdf_log;
+      if (!check_positive(function, nu, "Degrees of freedom parameter", &ccdf_log))
+        return ccdf_log;
+      if (!(check_consistent_sizes(function,
+                                   y,nu,
+                                   "Random variable","Degrees of freedom parameter",
+                                   &ccdf_log)))
+        return ccdf_log;
+
+      // Wrap arguments in vectors
+      VectorView<const T_y> y_vec(y);
+      VectorView<const T_dof> nu_vec(nu);
+      size_t N = max_size(y, nu);
+          
+      agrad::OperandsAndPartials<T_y, T_dof> 
+        operands_and_partials(y, nu);
+          
+      std::fill(operands_and_partials.all_partials,
+                operands_and_partials.all_partials 
+                + operands_and_partials.nvaris, 0.0);
+          
+      // Explicit return for extreme values
+      // The gradients are technically ill-defined, but treated as zero
+      for (size_t i = 0; i < stan::length(y); i++) {
+        if (value_of(y_vec[i]) == 0) 
+          return operands_and_partials.to_var(0.0);
+      }
+          
+      // Compute ccdf_log and its gradients
+      using boost::math::gamma_p_derivative;
+      using boost::math::gamma_p;
+      using boost::math::digamma;
+      using boost::math::tgamma;
+          
+      // Cache a few expensive function calls if nu is a parameter
+      DoubleVectorView<!is_constant_struct<T_dof>::value,
+                       is_vector<T_dof>::value> 
+        gamma_vec(stan::length(nu));
+      DoubleVectorView<!is_constant_struct<T_dof>::value,
+                       is_vector<T_dof>::value> 
+        digamma_vec(stan::length(nu));
+          
+      if (!is_constant_struct<T_dof>::value) {
+        for (size_t i = 0; i < stan::length(nu); i++) {
+          const double alpha_dbl = value_of(nu_vec[i]) * 0.5;
+          gamma_vec[i] = tgamma(alpha_dbl);
+          digamma_vec[i] = digamma(alpha_dbl);
+        }
+      }
+          
+      // Compute vectorized ccdf_log and gradient
+      for (size_t n = 0; n < N; n++) {
+        // Explicit results for extreme values
+        // The gradients are technically ill-defined, but treated as zero
+        if (value_of(y_vec[n]) == std::numeric_limits<double>::infinity())
+          return operands_and_partials.to_var(stan::math::negative_infinity());
+              
+        // Pull out values
+        const double y_dbl = value_of(y_vec[n]);
+        const double alpha_dbl = value_of(nu_vec[n]) * 0.5;
+        const double beta_dbl = 0.5;
+              
+        // Compute
+        const double Pn = 1.0 - gamma_p(alpha_dbl, beta_dbl * y_dbl);
+              
+        ccdf_log += log(Pn);
+              
+        if (!is_constant_struct<T_y>::value)
+          operands_and_partials.d_x1[n] 
+            -= beta_dbl * gamma_p_derivative(alpha_dbl, beta_dbl * y_dbl) 
+            / Pn;
+        if (!is_constant_struct<T_dof>::value)
+          operands_and_partials.d_x2[n] 
+            += 0.5 * stan::math::gradRegIncGamma(alpha_dbl, beta_dbl
+                                                 * y_dbl, gamma_vec[n],
+                                                 digamma_vec[n]) / Pn;
+      }
+          
+      return operands_and_partials.to_var(ccdf_log);
+    }
 
     template <class RNG>
     inline double
