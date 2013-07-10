57173c02459729f6e080b8323510f82beff75fbc
Peter Li <li.peter94@gmail.com>
2013-07-10 14:24:52 -0400

    added autodiff tests to rows_dot_prod,rows_dot_self,sd,softmax

--- src/test/agrad/fwd/matrix/softmax_test.cpp
+++ src/test/agrad/fwd/matrix/softmax_test.cpp
@@ -49,7 +49,7 @@ TEST(AgradFwdMatrixSoftmax,fd) {
   EXPECT_FLOAT_EQ(-2.0605762e-09, theta3[1].d_);
   EXPECT_FLOAT_EQ(-1.6697022e-05, theta3[2].d_);
 }
-TEST(AgradFwdMatrixSoftmax,fv) {
+TEST(AgradFwdMatrixSoftmax,fv_1stDeriv) {
   using stan::math::softmax;
   using Eigen::Matrix;
   using Eigen::Dynamic;
@@ -94,6 +94,36 @@ TEST(AgradFwdMatrixSoftmax,fv) {
   EXPECT_FLOAT_EQ(1.6699081e-05, theta3[0].d_.val());
   EXPECT_FLOAT_EQ(-2.0605762e-09, theta3[1].d_.val());
   EXPECT_FLOAT_EQ(-1.6697022e-05, theta3[2].d_.val());
+
+  AVEC q = createAVEC(x3(0).val(),x3(1).val(),x3(2).val());
+  VEC h;
+  theta3[0].val_.grad(q,h);
+  EXPECT_FLOAT_EQ(1.6699081e-05,h[0]);
+  EXPECT_FLOAT_EQ(-2.0605762e-09,h[1]);
+  EXPECT_FLOAT_EQ(-1.6697022e-05,h[2]);
+}
+TEST(AgradFwdMatrixSoftmax,fv_2ndDeriv) {
+  using stan::math::softmax;
+  using Eigen::Matrix;
+  using Eigen::Dynamic;
+  using stan::agrad::vector_fv;
+  using stan::agrad::fvar;
+  using stan::agrad::var;
+
+  Matrix<fvar<var>,Dynamic,1> x3(3);
+  x3 << -1.0, 1.0, 10.0;
+   x3(0).d_ = 2.0;
+   x3(1).d_ = 1.0;
+   x3(2).d_ = 1.0;
+
+  Matrix<fvar<var>,Dynamic,1> theta3 = softmax(x3);
+
+  AVEC q = createAVEC(x3(0).val(),x3(1).val(),x3(2).val());
+  VEC h;
+  theta3[0].d_.grad(q,h);
+  EXPECT_FLOAT_EQ(1.6698525e-05,h[0]);
+  EXPECT_FLOAT_EQ(-2.0605073e-09,h[1]);
+  EXPECT_FLOAT_EQ(-1.6696464e-05,h[2]);
 }
 TEST(AgradFwdMatrixSoftmax,ffd) {
   using stan::math::softmax;
@@ -139,4 +169,128 @@ TEST(AgradFwdMatrixSoftmax,ffd) {
   EXPECT_FLOAT_EQ(1.6699081e-05, theta3[0].d_.val());
   EXPECT_FLOAT_EQ(-2.0605762e-09, theta3[1].d_.val());
   EXPECT_FLOAT_EQ(-1.6697022e-05, theta3[2].d_.val());
+}
+TEST(AgradFwdMatrixSoftmax,ffv_1stDeriv) {
+  using stan::math::softmax;
+  using Eigen::Matrix;
+  using Eigen::Dynamic;
+  using stan::agrad::vector_ffv;
+  using stan::agrad::fvar;
+  using stan::agrad::var;
+
+  EXPECT_THROW(softmax(vector_ffv()),std::domain_error);
+  
+  Matrix<fvar<fvar<var> >,Dynamic,1> x(1);
+  x << 0.0;
+   x(0).d_ = 1.0;
+  
+  Matrix<fvar<fvar<var> >,Dynamic,1> theta = softmax(x);
+  EXPECT_EQ(1,theta.size());
+  EXPECT_FLOAT_EQ(1.0,theta[0].val_.val().val());
+  EXPECT_FLOAT_EQ(0.0,theta[0].d_.val().val());
+
+  Matrix<fvar<fvar<var> >,Dynamic,1> x2(2);
+  x2 << -1.0, 1.0;
+   x2(0).d_ = 2.0;
+   x2(1).d_ = 1.0;
+
+  Matrix<fvar<fvar<var> >,Dynamic,1> theta2 = softmax(x2);
+  EXPECT_EQ(2,theta2.size());
+  EXPECT_FLOAT_EQ(exp(-1)/(exp(-1) + exp(1)), theta2[0].val_.val().val());
+  EXPECT_FLOAT_EQ(exp(1)/(exp(-1) + exp(1)), theta2[1].val_.val().val());
+  EXPECT_FLOAT_EQ(0.10499358, theta2[0].d_.val().val());
+  EXPECT_FLOAT_EQ(-0.10499358, theta2[1].d_.val().val());
+
+  Matrix<fvar<fvar<var> >,Dynamic,1> x3(3);
+  x3 << -1.0, 1.0, 10.0;
+   x3(0).d_ = 2.0;
+   x3(1).d_ = 1.0;
+   x3(2).d_ = 1.0;
+
+  Matrix<fvar<fvar<var> >,Dynamic,1> theta3 = softmax(x3);
+  EXPECT_EQ(3,theta3.size());
+  EXPECT_FLOAT_EQ(exp(-1)/(exp(-1) + exp(1) + exp(10.0)), theta3[0].val_.val().val());
+  EXPECT_FLOAT_EQ(exp(1)/(exp(-1) + exp(1) + exp(10.0)), theta3[1].val_.val().val());
+  EXPECT_FLOAT_EQ(exp(10)/(exp(-1) + exp(1) + exp(10.0)), theta3[2].val_.val().val());
+  EXPECT_FLOAT_EQ(1.6699081e-05, theta3[0].d_.val().val());
+  EXPECT_FLOAT_EQ(-2.0605762e-09, theta3[1].d_.val().val());
+  EXPECT_FLOAT_EQ(-1.6697022e-05, theta3[2].d_.val().val());
+
+  AVEC q = createAVEC(x3(0).val().val(),x3(1).val().val(),x3(2).val().val());
+  VEC h;
+  theta3[0].val_.val().grad(q,h);
+  EXPECT_FLOAT_EQ(1.6699081e-05,h[0]);
+  EXPECT_FLOAT_EQ(-2.0605762e-09,h[1]);
+  EXPECT_FLOAT_EQ(-1.6697022e-05,h[2]);
+}
+TEST(AgradFwdMatrixSoftmax,ffv_2ndDeriv_1) {
+  using stan::math::softmax;
+  using Eigen::Matrix;
+  using Eigen::Dynamic;
+  using stan::agrad::vector_ffv;
+  using stan::agrad::fvar;
+  using stan::agrad::var;
+
+  Matrix<fvar<fvar<var> >,Dynamic,1> x3(3);
+  x3 << -1.0, 1.0, 10.0;
+   x3(0).d_ = 2.0;
+   x3(1).d_ = 1.0;
+   x3(2).d_ = 1.0;
+
+  Matrix<fvar<fvar<var> >,Dynamic,1> theta3 = softmax(x3);
+
+  AVEC q = createAVEC(x3(0).val().val(),x3(1).val().val(),x3(2).val().val());
+  VEC h;
+  theta3[0].val().d_.grad(q,h);
+  EXPECT_FLOAT_EQ(0,h[0]);
+  EXPECT_FLOAT_EQ(0,h[1]);
+  EXPECT_FLOAT_EQ(0,h[2]);
+}
+
+TEST(AgradFwdMatrixSoftmax,ffv_2ndDeriv_2) {
+  using stan::math::softmax;
+  using Eigen::Matrix;
+  using Eigen::Dynamic;
+  using stan::agrad::vector_ffv;
+  using stan::agrad::fvar;
+  using stan::agrad::var;
+
+  Matrix<fvar<fvar<var> >,Dynamic,1> x3(3);
+  x3 << -1.0, 1.0, 10.0;
+   x3(0).d_ = 2.0;
+   x3(1).d_ = 1.0;
+   x3(2).d_ = 1.0;
+
+  Matrix<fvar<fvar<var> >,Dynamic,1> theta3 = softmax(x3);
+
+  AVEC q = createAVEC(x3(0).val().val(),x3(1).val().val(),x3(2).val().val());
+  VEC h;
+  theta3[0].d_.val().grad(q,h);
+  EXPECT_FLOAT_EQ(1.6698525e-05,h[0]);
+  EXPECT_FLOAT_EQ(-2.0605073e-09,h[1]);
+  EXPECT_FLOAT_EQ(-1.6696464e-05,h[2]);
+}
+
+TEST(AgradFwdMatrixSoftmax,ffv_3rdDeriv) {
+  using stan::math::softmax;
+  using Eigen::Matrix;
+  using Eigen::Dynamic;
+  using stan::agrad::vector_ffv;
+  using stan::agrad::fvar;
+  using stan::agrad::var;
+
+  Matrix<fvar<fvar<var> >,Dynamic,1> x3(3);
+  x3 << -1.0, 1.0, 10.0;
+   x3(0).d_ = 2.0;
+   x3(1).d_ = 1.0;
+   x3(2).d_ = 1.0;
+
+  Matrix<fvar<fvar<var> >,Dynamic,1> theta3 = softmax(x3);
+
+  AVEC q = createAVEC(x3(0).val().val(),x3(1).val().val(),x3(2).val().val());
+  VEC h;
+  theta3[0].d_.d_.grad(q,h);
+  EXPECT_FLOAT_EQ(0,h[0]);
+  EXPECT_FLOAT_EQ(0,h[1]);
+  EXPECT_FLOAT_EQ(0,h[2]);
 }
