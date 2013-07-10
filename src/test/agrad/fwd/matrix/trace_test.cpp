dad66e3c209f057e4a6b879b8df5843b955646d6
Peter Li <li.peter94@gmail.com>
2013-07-10 15:28:05 -0400

    added autodiff tests for trace,transpose,variance

--- src/test/agrad/fwd/matrix/trace_test.cpp
+++ src/test/agrad/fwd/matrix/trace_test.cpp
@@ -58,3 +58,22 @@ TEST(AgradFwdMatrixTrace,ffd) {
   EXPECT_FLOAT_EQ(9.0,s.val_.val());
   EXPECT_FLOAT_EQ(2.0,s.d_.val());
 }  
+
+TEST(AgradFwdMatrixTrace,ffv) {
+  using stan::math::trace;
+  using stan::agrad::matrix_ffv;
+  using stan::agrad::fvar;
+  using stan::agrad::var;
+
+  matrix_ffv a(2,2);
+  a << -1.0, 2.0, 
+    5.0, 10.0;
+   a(0,0).d_ = 1.0;
+   a(0,1).d_ = 1.0;
+   a(1,0).d_ = 1.0;
+   a(1,1).d_ = 1.0;
+  
+  fvar<fvar<var> > s = trace(a);
+  EXPECT_FLOAT_EQ(9.0,s.val_.val().val());
+  EXPECT_FLOAT_EQ(2.0,s.d_.val().val());
+}
