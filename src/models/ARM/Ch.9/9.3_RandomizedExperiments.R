0bd75106a152717fd73c0e204d705305042284c5
Peter Li <li.peter94@gmail.com>
2013-07-18 14:29:51 -0400

    added graph in ARM 9.3

--- src/models/ARM/Ch.9/9.3_RandomizedExperiments.R
+++ src/models/ARM/Ch.9/9.3_RandomizedExperiments.R
@@ -110,5 +110,19 @@ sigma.mean8 <- mean(sigma.post6)
 #graphs on Figure 9.5 FIXME
  
 
-## Controlling for pre-treatment predictors (Figure 9.6)
+## Controlling for pre-treatment predictors (Figure 9.6) FIXME:CHECK
+for (j in 1:4){
+  ok <- Grade==j
+  pret <- c (treated.Pretest[ok], control.Pretest[ok])
+  postt <-c (treated.Posttest[ok], control.Posttest[ok])
+  dataList.1 <- list(N=length(pret), post_test=postt, pre_test=pret)
+  electric_one_pred.sf1 <- sampling(electric_one_pred.sm, dataList.1)
+  beta.post1 <- extract(electric_one_pred.sf1, "beta")$beta
+  beta.mean1 <- colMeans(beta.post1)
 
+  frame1 = data.frame(x1=treated.Pretest[ok],y1=treated.Posttest[ok])
+  frame2 = data.frame(x2=control.Pretest[ok],y2=control.Posttest[ok])
+  m <- ggplot()
+  m <- m + geom_point(data=frame1,aes(x=x1,y=y1),type=20) + geom_point(data=frame2,aes(x=x2,y=y2),type=21) + scale_y_continuous("Posttest",limits=c(-.01,1)) + scale_x_continuous("Pretest") + theme_bw() + labs(title=paste("Grade ",j))
+  m <- m + geom_abline(yintercept=beta.mean[1],slope=beta.mean[2]) + geom_abline(yintercept=beta.mean[1] +beta.mean[3],slope=beta.mean[2])
+}
