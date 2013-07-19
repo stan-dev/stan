abbb11710655549157248aa1e1fe136b5860bc6d
Peter Li <li.peter94@gmail.com>
2013-07-19 13:06:40 -0400

    added 10.6

---
+++ src/models/ARM/Ch.10/10.5_CasualEffectsUsingIV.R
@@ -0,0 +1,42 @@
+library(rstan)
+library(ggplot2)
+library(foreign)
+
+sesame <- read.dta("sesame.dta")
+attach(sesame)
+
+## Rename variables of interest
+watched <- regular
+encouraged <- encour
+y <- postlet
+
+## Instrumental variables estimate (sesame_one_pred_a.stan)
+## lm (watched ~ encouraged)
+if (!file.exists("sesame_one_pred_a.sm.RData")) {
+    rt <- stanc("sesame_one_pred_a.stan", model_name="sesame_one_pred_a")
+    sesame_one_pred_a.sm <- stan_model(stanc_ret=rt)
+    save(sesame_one_pred_a.sm, file="sesame_one_pred_a.sm.RData")
+} else {
+    load("sesame_one_pred_a.sm.RData", verbose=TRUE)
+}
+
+dataList.1 <- list(N=length(watched), watched=watched,encouraged=encouraged)
+sesame_one_pred_a.sf1 <- sampling(sesame_one_pred_a.sm, dataList.1)
+print(sesame_one_pred_a.sf1)
+
+beta.post <- extract(sesame_one_pred_a.sf1, "beta")$beta
+beta.mean1 <- colMeans(beta.post)
+
+## (sesame_one_pred_b.stan)
+## lm (y ~ encouraged)
+
+dataList.2 <- list(N=length(y), watched=y,encouraged=encouraged)
+sesame_one_pred_b.sf1 <- sampling(sesame_one_pred_a.sm, dataList.2)
+print(sesame_one_pred_b.sf1)
+
+beta.post <- extract(sesame_one_pred_b.sf1, "beta")$beta
+beta.mean2 <- colMeans(beta.post)
+
+
+iv.est.1 <- beta.mean2[2] / beta.mean1[2]
+print(iv.est.1)
---
+++ src/models/ARM/Ch.10/10.6_IVinaRegressionFramework.R
@@ -0,0 +1,66 @@
+library(rstan)
+library(ggplot2)
+
+source("10.5_CasualEffectsUsingIV.R") # where data was cleaned
+
+## Rename variables of interest
+
+pretest <- prelet
+
+## 2 stage least squares (sesame_one_pred_a.stan)
+## lm (watched ~ encouraged)
+
+if (!file.exists("sesame_one_pred_a.sm.RData")) {
+    rt <- stanc("sesame_one_pred_a.stan", model_name="sesame_one_pred_a")
+    sesame_one_pred_a.sm <- stan_model(stanc_ret=rt)
+    save(sesame_one_pred_a.sm, file="sesame_one_pred_a.sm.RData")
+} else {
+    load("sesame_one_pred_a.sm.RData", verbose=TRUE)
+}
+
+dataList.1 <- list(N=length(watched), watched=watched,encouraged=encouraged)
+sesame_one_pred_2a.sf1 <- sampling(sesame_one_pred_a.sm, dataList.1)
+print(sesame_one_pred_2a.sf1)
+beta.post <- extract(sesame_one_pred_2a.sf1, "beta")$beta
+beta.mean2a <- colMeans(beta.post)
+
+watched.hat <- beta.mean2a[1] + beta.mean2a[2] * encouraged
+
+## (sesame_one_pred_2b.stan)
+## lm (y ~ watched.hat)
+
+dataList.2 <- list(N=length(y), watched=y,encouraged=watched.hat)
+sesame_one_pred_2b.sf1 <- sampling(sesame_one_pred_a.sm, dataList.2)
+print(sesame_one_pred_2b.sf1)
+
+## Adjusting for covariates in a IV framework (sesame_multi_preds_3a.stan)
+## lm (watched ~ encouraged + pretest + as.factor(site) + setting)
+if (!file.exists("sesame_multi_preds_3a.sm.RData")) {
+    rt <- stanc("sesame_multi_preds_3a.stan", model_name="sesame_multi_preds_3a")
+    sesame_multi_preds_3a.sm <- stan_model(stanc_ret=rt)
+    save(sesame_multi_preds_3a.sm, file="sesame_multi_preds_3a.sm.RData")
+} else {
+    load("sesame_multi_preds_3a.sm.RData", verbose=TRUE)
+}
+dataList.3 <- list(N=length(watched), watched=watched,encouraged=encouraged,pretest=pretest, site=site,setting=setting)
+sesame_multi_pred_3a.sf1 <- sampling(sesame_multi_preds_3a.sm, dataList.3)
+print(sesame_multi_pred_3a.sf1)
+
+beta.post <- extract(sesame_multi_pred_3a.sf1, "beta")$beta
+beta.mean3a <- colMeans(beta.post)
+
+watched.hat <- beta.mean3a[1] + beta.mean3a[2] * encouraged + beta.mean3a[3] * pretest + beta.mean3a[4] * (site==2) + beta.mean3a[5] * (site==3) + beta.mean3a[6] * (site==4) + beta.mean3a[7] * (site==5) + beta.mean3a[8] * setting
+
+## (sesame_multi_preds_3b.stan)
+## lm (y ~ watched.hat + pretest + as.factor(site) + setting)
+dataList.4 <- list(N=length(watched.hat), watched=y,encouraged=watched.hat,pretest=pretest, site=site,setting=setting)
+sesame_multi_pred_3b.sf1 <- sampling(sesame_multi_preds_3a.sm, dataList.4)
+print(sesame_multi_pred_3b.sf1)
+
+## Se for IV estimates (FIXME)
+
+## Performing 2sls automatically
+
+ # regression without pre-treatment variables
+
+ # regression controlling for pre-treatment variables
