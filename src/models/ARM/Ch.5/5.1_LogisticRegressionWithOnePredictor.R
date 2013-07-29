38bef8bf83c14c2c9a3dbd424838c1635279da1b
Peter Li <li.peter94@gmail.com>
2013-07-29 11:29:02 -0400

    clean up 5.1

--- src/models/ARM/Ch.5/5.1_LogisticRegressionWithOnePredictor.R
+++ src/models/ARM/Ch.5/5.1_LogisticRegressionWithOnePredictor.R
@@ -1,21 +1,23 @@
 library(rstan)
 library(ggplot2)
-source("nes.data.R")    
+source("nes.data.R", echo = TRUE)    
 
  # Estimation (nes.stan)
  # glm (vote ~ income, family=binomial(link="logit"))
 
-if (!file.exists("nes.sm.RData")) {
-    rt <- stanc("nes.stan", model_name="nes")
-    nes.sm <- stan_model(stanc_ret=rt)
-    save(nes.sm, file="nes.sm.RData")
-} else {
-    load("nes.sm.RData", verbose=TRUE)
+if (!exists("nes.sm")) {
+    if (file.exists("nes.sm.RData")) {
+        load("nes.sm.RData", verbose = TRUE)
+    } else {
+        rt <- stanc("nes.stan", model_name = "nes")
+        nes.sm <- stan_model(stanc_ret = rt)
+        save(nes.sm, file = "nes.sm.RData")
+    }
 }
 
-dataList.1 <- list(N=N, income=income, vote=vote)
+dataList.1 <- c("N","income","vote")
 nes.sf1 <- sampling(nes.sm, dataList.1)
-print(nes.sf1)
+print(nes.sf1, pars = c("beta", "lp__"))
 
 fit1.post <- extract(nes.sf1)
 beta.mean <- colMeans(fit1.post$beta)
@@ -23,12 +25,22 @@ beta.mean <- colMeans(fit1.post$beta)
  # Graph figure 5.1 (a)
 
 frame1 = data.frame(income=income,vote=vote)
-m <- ggplot(frame1,aes(x=income,y=vote))
-m <- m + geom_point() + scale_y_continuous("Pr(Republican Vote)",limits=c(-.01,1)) + scale_x_continuous("Income",limits=c(-2,8)) + theme_bw() + stat_smooth(method="glm",family="binomial",se=F,size=2,colour="black") + geom_jitter(position=position_jitter(height=.08,width=.4)) + stat_function(fun=function(x) 1.0 / (1 + exp(-fit1.post$beta[1] - fit1.post$beta[2] * x)))
+p1 <- ggplot(frame1,aes(x=income,y=vote)) +
+      geom_jitter(position=position_jitter(height=.08,width=.4)) +
+      scale_y_continuous("Pr(Republican Vote)",limits=c(-.01,1)) +
+      scale_x_continuous("Income",limits=c(-2,8)) + theme_bw() +
+      stat_smooth(method="glm",family="binomial",se=F,size=2,colour="black") +
+      stat_function(fun=function(x) 1.0 / (1 + exp(-beta.mean[1] - beta.mean[2] * x)))
+print(p1)
 
  # Graph figure 5.1 (b)
 
-m2 <- "ggplot(frame1,aes(x=income,y=vote)) + scale_y_continuous('Pr(Republican Vote)',limits=c(-.01,1)) + scale_x_continuous('Income') + theme_bw() + stat_smooth(method='glm',family='binomial',se=F,colour='black') + geom_jitter(position=position_jitter(height=.08,width=.4))"
+m2 <- "ggplot(frame1,aes(x=income,y=vote)) +
+       scale_y_continuous('Pr(Republican Vote)',limits=c(-.01,1)) +
+       scale_x_continuous('Income') +
+       theme_bw() +
+       stat_smooth(method='glm',family='binomial',se=F,colour='black') +
+       geom_jitter(position=position_jitter(height=.08,width=.4))"
 for (i in 1:20) {
   m2 <- paste(m2,"+ stat_function(aes(y=0),fun=function(x) 1.0 / (1 + exp(-fit1.post$beta[4000-",i,",1]-fit1.post$beta[4000-",i,",2]*x)),colour='grey')")
 }
