ec05440af8caa13f956882d3de86a71fe8eeec01
Peter Li <li.peter94@gmail.com>
2013-07-29 12:16:33 -0400

    updated ch6 models

--- src/models/ARM/Ch.6/6.5_Ordered&UnorderedCategoricalRegression.R
+++ src/models/ARM/Ch.6/6.5_Ordered&UnorderedCategoricalRegression.R
@@ -1,5 +1,6 @@
 library(rstan)
 library(ggplot2)
+library(boot)
 ## Read in the data
 
 ##FIXME: MISSING data?
@@ -10,8 +11,8 @@ library(ggplot2)
 
 ## Displaying the fitted model
 expected <- function (x, c1.5, c2.5, sigma){
-  p1.5 <- invlogit ((x-c1.5)/sigma)
-  p2.5 <- invlogit ((x-c2.5)/sigma)
+  p1.5 <- inv.logit ((x-c1.5)/sigma)
+  p2.5 <- inv.logit ((x-c2.5)/sigma)
   return ((1*(1-p1.5) + 2*(p1.5-p2.5) + 3*p2.5))
 }
 
--- src/models/ARM/Ch.6/6.8_ConstructiveChoiceModels.R
+++ src/models/ARM/Ch.6/6.8_ConstructiveChoiceModels.R
@@ -1,19 +1,21 @@
 library(rstan)
 library(ggplot2)
-source("wells.data.R")    
+source("wells.data.R", echo = TRUE)    
 
 ## Fitting the model (wells_logit.stan)
-if (!file.exists("wells_logit.sm.RData")) {
-    rt <- stanc("wells_logit.stan", model_name="wells_logit")
-    wells_logit.sm <- stan_model(stanc_ret=rt)
-    save(wells_logit.sm, file="wells_logit.sm.RData")
-} else {
-    load("wells_logit.sm.RData", verbose=TRUE)
+if (!exists("wells_logit.sm")) {
+    if (file.exists("wells_logit.sm.RData")) {
+        load("wells_logit.sm.RData", verbose = TRUE)
+    } else {
+        rt <- stanc("wells_logit.stan", model_name = "wells_logit")
+        wells_logit.sm <- stan_model(stanc_ret = rt)
+        save(wells_logit.sm, file = "wells_logit.sm.RData")
+    }
 }
 
 ## Probit or logit
 ## glm (switch ~ dist100, family=binomial(link="logit"))
-dataList <- list(N=N, switc=switc, dist=dist)
+dataList <- c("N","switc","dist")
 wells_logit.sf1 <- sampling(wells_logit.sm, dataList)
 print(wells_logit.sf1)
 
