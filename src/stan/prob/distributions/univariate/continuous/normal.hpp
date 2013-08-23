2443e9a506f30bfc19973d441f1d0bf4bb4c6c77
Peter Li <li.peter94@gmail.com>
2013-08-23 11:50:46 -0400

    fixed error in normal distr

--- src/stan/prob/distributions/univariate/continuous/normal.hpp
+++ src/stan/prob/distributions/univariate/continuous/normal.hpp
@@ -135,7 +135,7 @@ namespace stan {
       return normal_log<false>(y,mu,sigma);
     }
 
-    /**
+       /**
      * Calculates the normal cumulative distribution function for the given
      * variate, location, and scale.
      * 
@@ -161,10 +161,11 @@ namespace stan {
       using stan::math::check_positive;
       using stan::math::check_finite;
       using stan::math::check_not_nan;
+      using stan::math::value_of;
       using stan::math::check_consistent_sizes;
 
+      double cdf(1.0);
 
-      typename return_type<T_y, T_loc, T_scale>::type cdf(1);
       // check if any vectors are zero length
       if (!(stan::length(y) 
             && stan::length(mu) 
@@ -183,19 +184,190 @@ namespace stan {
         return cdf;
       if (!(check_consistent_sizes(function,
                                    y,mu,sigma,
-                                   "Random variable","Location parameter","Scale parameter",
+                                   "Random variable","Location parameter",
+                                   "Scale parameter",
                                    &cdf)))
         return cdf;
+
+     agrad::OperandsAndPartials<T_y, T_loc, T_scale> 
+       operands_and_partials(y, mu, sigma);
 
       VectorView<const T_y> y_vec(y);
       VectorView<const T_loc> mu_vec(mu);
       VectorView<const T_scale> sigma_vec(sigma);
       size_t N = max_size(y, mu, sigma);
-      
+      const double SQRT_TWO_OVER_PI = std::sqrt(2.0 / stan::math::pi());
+
       for (size_t n = 0; n < N; n++) {
-        cdf *= 0.5 + 0.5 * erf((y_vec[n] - mu_vec[n]) / (sigma_vec[n] * SQRT_2));
+        const double y_dbl = value_of(y_vec[n]);
+        const double mu_dbl = value_of(mu_vec[n]);
+        const double sigma_dbl = value_of(sigma_vec[n]);
+        const double scaled_diff = (y_dbl - mu_dbl) / (sigma_dbl * SQRT_2);
+        const double cdf_ = 0.5 * (1.0 + erf(scaled_diff));
+
+        // cdf
+        cdf *= cdf_;
+
+        // gradients
+        const double rep_deriv = SQRT_TWO_OVER_PI * 0.5 
+          * exp(-scaled_diff * scaled_diff) / cdf_ / sigma_dbl;
+        if (!is_constant_struct<T_y>::value)
+          operands_and_partials.d_x1[n] += rep_deriv;
+        if (!is_constant_struct<T_loc>::value)
+          operands_and_partials.d_x2[n] -= rep_deriv;
+        if (!is_constant_struct<T_scale>::value)
+          operands_and_partials.d_x3[n] -= rep_deriv * scaled_diff * SQRT_2;
       }
-      return cdf;
+
+      if (!is_constant_struct<T_y>::value)
+        for (size_t n = 0; n < stan::length(y); ++n) 
+          operands_and_partials.d_x1[n] *= cdf;
+      if (!is_constant_struct<T_loc>::value)
+        for (size_t n = 0; n < stan::length(mu); ++n) 
+          operands_and_partials.d_x2[n] *= cdf;
+      if (!is_constant_struct<T_scale>::value)
+        for (size_t n = 0; n < stan::length(sigma); ++n) 
+          operands_and_partials.d_x3[n] *= cdf;
+
+      return operands_and_partials.to_var(cdf);
+    }
+
+    template <typename T_y, typename T_loc, typename T_scale>
+    typename return_type<T_y,T_loc,T_scale>::type
+    normal_cdf_log(const T_y& y, const T_loc& mu, const T_scale& sigma) {
+      static const char* function = "stan::prob::normal_cdf_log(%1%)";
+
+      using stan::math::check_positive;
+      using stan::math::check_finite;
+      using stan::math::check_not_nan;
+      using stan::math::check_consistent_sizes;
+      using stan::math::value_of;
+
+      double cdf_log(0.0);
+      // check if any vectors are zero length
+      if (!(stan::length(y) 
+            && stan::length(mu) 
+            && stan::length(sigma)))
+        return cdf_log;
+
+      if (!check_not_nan(function, y, "Random variable", &cdf_log))
+        return cdf_log;
+      if (!check_finite(function, mu, "Location parameter", &cdf_log))
+        return cdf_log;
+      if (!check_not_nan(function, sigma, "Scale parameter", 
+                         &cdf_log))
+        return cdf_log;
+      if (!check_positive(function, sigma, "Scale parameter", 
+                          &cdf_log))
+        return cdf_log;
+      if (!(check_consistent_sizes(function,
+                                   y,mu,sigma,
+                                   "Random variable","Location parameter",
+                                   "Scale parameter", &cdf_log)))
+        return cdf_log;
+
+      agrad::OperandsAndPartials<T_y, T_loc, T_scale> 
+        operands_and_partials(y, mu, sigma);
+
+      VectorView<const T_y> y_vec(y);
+      VectorView<const T_loc> mu_vec(mu);
+      VectorView<const T_scale> sigma_vec(sigma);
+      size_t N = max_size(y, mu, sigma);
+      double log_half = std::log(0.5);  
+    
+      const double SQRT_TWO_OVER_PI = std::sqrt(2.0 / stan::math::pi());
+      for (size_t n = 0; n < N; n++) {
+        const double y_dbl = value_of(y_vec[n]);
+        const double mu_dbl = value_of(mu_vec[n]);
+        const double sigma_dbl = value_of(sigma_vec[n]);
+
+        const double scaled_diff = (y_dbl - mu_dbl) / (sigma_dbl * SQRT_2);
+        
+        const double one_p_erf = 1.0 + erf(scaled_diff);
+        // log cdf
+        cdf_log += log_half + log(one_p_erf);
+
+        // gradients
+        const double rep_deriv = SQRT_TWO_OVER_PI 
+          * exp(-scaled_diff * scaled_diff) / one_p_erf;
+        if (!is_constant_struct<T_y>::value)
+          operands_and_partials.d_x1[n] += rep_deriv / sigma_dbl;
+        if (!is_constant_struct<T_loc>::value)
+          operands_and_partials.d_x2[n] -= rep_deriv / sigma_dbl;
+        if (!is_constant_struct<T_scale>::value)
+          operands_and_partials.d_x3[n] -= rep_deriv * scaled_diff 
+            * stan::math::SQRT_2 / sigma_dbl;
+      }
+      return operands_and_partials.to_var(cdf_log);
+    }
+
+    template <typename T_y, typename T_loc, typename T_scale>
+    typename return_type<T_y,T_loc,T_scale>::type
+    normal_ccdf_log(const T_y& y, const T_loc& mu, const T_scale& sigma) {
+      static const char* function = "stan::prob::normal_ccdf_log(%1%)";
+
+      using stan::math::check_positive;
+      using stan::math::check_finite;
+      using stan::math::check_not_nan;
+      using stan::math::check_consistent_sizes;
+      using stan::math::value_of;
+
+      double ccdf_log(0.0);
+      // check if any vectors are zero length
+      if (!(stan::length(y) 
+            && stan::length(mu) 
+            && stan::length(sigma)))
+        return ccdf_log;
+
+      if (!check_not_nan(function, y, "Random variable", &ccdf_log))
+        return ccdf_log;
+      if (!check_finite(function, mu, "Location parameter", &ccdf_log))
+        return ccdf_log;
+      if (!check_not_nan(function, sigma, "Scale parameter", 
+                         &ccdf_log))
+        return ccdf_log;
+      if (!check_positive(function, sigma, "Scale parameter", 
+                          &ccdf_log))
+        return ccdf_log;
+      if (!(check_consistent_sizes(function,
+                                   y,mu,sigma,
+                                   "Random variable","Location parameter",
+                                   "Scale parameter", &ccdf_log)))
+        return ccdf_log;
+
+      agrad::OperandsAndPartials<T_y, T_loc, T_scale>
+        operands_and_partials(y, mu, sigma);
+
+      VectorView<const T_y> y_vec(y);
+      VectorView<const T_loc> mu_vec(mu);
+      VectorView<const T_scale> sigma_vec(sigma);
+      size_t N = max_size(y, mu, sigma);
+      double log_half = std::log(0.5);  
+    
+      const double SQRT_TWO_OVER_PI = std::sqrt(2.0 / stan::math::pi());
+      for (size_t n = 0; n < N; n++) {
+        const double y_dbl = value_of(y_vec[n]);
+        const double mu_dbl = value_of(mu_vec[n]);
+        const double sigma_dbl = value_of(sigma_vec[n]);
+
+        const double scaled_diff = (y_dbl - mu_dbl) / (sigma_dbl * SQRT_2);
+        
+        const double one_m_erf = 1.0 - erf(scaled_diff);
+        // log ccdf
+        ccdf_log += log_half + log(one_m_erf);
+
+        // gradients
+        const double rep_deriv = SQRT_TWO_OVER_PI 
+          * exp(-scaled_diff * scaled_diff) / one_m_erf;
+        if (!is_constant_struct<T_y>::value)
+          operands_and_partials.d_x1[n] -= rep_deriv / sigma_dbl;
+        if (!is_constant_struct<T_loc>::value)
+          operands_and_partials.d_x2[n] += rep_deriv / sigma_dbl;
+        if (!is_constant_struct<T_scale>::value)
+          operands_and_partials.d_x3[n] += rep_deriv * scaled_diff 
+            * stan::math::SQRT_2 / sigma_dbl;
+      }
+      return operands_and_partials.to_var(ccdf_log);
     }
 
     template <class RNG>
