8c2825fd29a11ade5ab975c3aa03c406632e5124
Daniel Lee <bearlee@alum.mit.edu>
2013-09-05 14:31:47 -0400

    working on argument_configuration_test

--- src/test/gm/arguments/argument_configuration_test.cpp
+++ src/test/gm/arguments/argument_configuration_test.cpp
@@ -1,5 +1,5 @@
 #include <gtest/gtest.h>
-
+#include <stan/gm/error_codes.hpp>
 #include <stan/gm/arguments/argument_probe.hpp>
 #include <stan/gm/arguments/arg_method.hpp>
 #include <stan/gm/arguments/arg_id.hpp>
@@ -58,14 +58,20 @@ TEST(StanGmArgumentsConfiguration, TestMethod) {
   std::stringstream output;
   
   while (s.good()) {
-  
     std::getline(s, l1);
-    if (!s.good()) continue;
+    //std::cout << "s.good()?: " << s.good() << std::endl;
+    //std::cout << "l1: " << l1 << std::endl;
+    if (!s.good()) 
+      continue;
     
-    if      (l1 == "good") expected_success = true;
-    else if (l1 == "bad") expected_success = false;
-    else if (l1 != "") expected_output << l1 << std::endl;
+    if (l1 == "good") 
+      expected_success = true;
+    else if (l1 == "bad") 
+      expected_success = false;
+    else if (l1 != "") 
+      expected_output << l1 << std::endl;
     else {
+      //std::cout << "inside else" << std::endl;
       int n_output = 0;
       
       std::string l2;
@@ -73,71 +79,108 @@ TEST(StanGmArgumentsConfiguration, TestMethod) {
       
       while (expected_output.good()) {
         std::getline(expected_output, l2);
-        if (!expected_output.good()) continue;
+        //std::cout << "l2: " << l2 << std::endl;
+        //std::cout << "expected_output.good(): " << expected_output.good() << std::endl;
+        if (!expected_output.good()) 
+          continue;
         clean_line(l2);
         argument += " " + l2;
         ++n_output;
       }
       
-      if (argument.length() == 0) continue;
+      if (argument.length() == 0) 
+        continue;
       
+      //std::cout << "before remove duplicates" << std::endl;
       remove_duplicates(argument);
+      //std::cout << "after" << std::endl;
       
+      //std::cout << "argument: " << argument << std::endl;
       std::string command = convert_model_path(model_path) + argument;
       std::string command_output;
       long time;
       int err_code;
 
+      SCOPED_TRACE(command);
       try {
         command_output = run_command(command, time, err_code);
       } catch(...) {
-        ADD_FAILURE() << "Failed running command: " << command;
+        // no-op: err_code indicates success
       }
-      
+
+      //std::cout << "just ran command" << std::endl;
       expected_output.clear();
       expected_output.seekg(std::ios_base::beg);
       
+      std::cout << "expected_success: " << expected_success << std::endl;
       if (expected_success == false) {
-                
+        std::cout << "inside false" << std::endl;
+        std::cout << "command_output: " << command_output << std::endl;
         unsigned int c1 = command_output.find("is not");
+        //std::cout << "1" << std::endl;
         command_output.erase(0, c1);
+        //std::cout << "2" << std::endl;
+
         unsigned int c2 = command_output.find(" \"");
+        //std::cout << "3" << std::endl;
+
         unsigned int c3 = command_output.find("Failed to parse");
-        command_output.replace(c2, c3 - c2, "\n");
+        //std::cout << "4" << std::endl;
+
+        //std::cout << "c1: " << c1 << std::endl;
+        //std::cout << "c2: " << c2 << std::endl;
+        //std::cout << "c3: " << c3 << std::endl;
+        //std::cout << "c3 - c2: " << (c3-c2) << std::endl;
+
+        if (c3 != c2)
+          command_output.replace(c2, c3 - c2, "\n");
+        //std::cout << "5" << std::endl;
+
         
         expected_output.str(std::string());
+        //std::cout << "6" << std::endl;
+
         expected_output << "is not a valid value for" << std::endl;
         expected_output << "Failed to parse arguments, terminating Stan" << std::endl;
         n_output = 2;
-        
+        EXPECT_EQ(int(stan::gm::error_codes::USAGE), err_code);
+      } else {
+        EXPECT_EQ(int(stan::gm::error_codes::OK), err_code);
       }
       
       output.clear();
       output.seekg(std::ios_base::beg);
       output.str(command_output);
       
+      //std::cout << "command: " << command << std::endl;
+      //std::cout << "output: " << command_output << std::endl;
+      //std::cout << "n_output: " << n_output << std::endl;
       for (int i = 0; i < n_output; ++i) {
-        
+        //std::cout << i << ": getline (expected)" << std::endl;
         std::string expected_line;
         std::getline(expected_output, expected_line);
         
+        std::cout << "expected: " << expected_line << std::endl;
+
+        //std::cout << i << ": getline (actual)" << std::endl;
         std::string actual_line;
         std::getline(output, actual_line);
         
         EXPECT_EQ(expected_line, actual_line);
-        
       }
       
       expected_output.clear();
       expected_output.seekg(std::ios_base::beg);
       expected_output.str(std::string());
-      
+      //std::cout << "at end here" << std::endl;
     }
-    
+    //std::cout << "second loop" << std::endl;
   }
   
+  //std::cout << "Delete loop" << std::endl;
   for (int i = 0; i < valid_arguments.size(); ++i)
     delete valid_arguments.at(i);
+  //std::cout << "end" << std::endl;
   
 }
 
