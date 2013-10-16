89b4746fe498632a7b5d11882aca76065e82aed9
Daniel Lee <bearlee@alum.mit.edu>
2013-10-16 14:59:37 -0400

    adding doc to the source repository

---
+++ doc/api/html/search/namespaces_73.js
@@ -0,0 +1,16 @@
+var searchData=
+[
+  ['agrad',['agrad',['../namespacestan_1_1agrad.html',1,'stan']]],
+  ['detail',['detail',['../namespacestan_1_1math_1_1policies_1_1detail.html',1,'stan::math::policies']]],
+  ['gm',['gm',['../namespacestan_1_1gm.html',1,'stan']]],
+  ['io',['io',['../namespacestan_1_1io.html',1,'stan']]],
+  ['math',['math',['../namespacestan_1_1math.html',1,'stan']]],
+  ['mcmc',['mcmc',['../namespacestan_1_1mcmc.html',1,'stan']]],
+  ['memory',['memory',['../namespacestan_1_1memory.html',1,'stan']]],
+  ['model',['model',['../namespacestan_1_1model.html',1,'stan']]],
+  ['optimization',['optimization',['../namespacestan_1_1optimization.html',1,'stan']]],
+  ['policies',['policies',['../namespacestan_1_1math_1_1policies.html',1,'stan::math']]],
+  ['prob',['prob',['../namespacestan_1_1prob.html',1,'stan']]],
+  ['stan',['stan',['../namespacestan.html',1,'']]],
+  ['std',['std',['../namespacestd.html',1,'']]]
+];
---
+++ doc/api/html/search/typedefs_6d.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['map_5fmatrix_5ft',['map_matrix_t',['../classstan_1_1io_1_1reader.html#a05f161f2ef596f356d249afbb0b43ca6',1,'stan::io::reader']]],
+  ['map_5frow_5fvector_5ft',['map_row_vector_t',['../classstan_1_1io_1_1reader.html#a71946c44faafcf4669f3cfde52816633',1,'stan::io::reader']]],
+  ['map_5fvector_5ft',['map_vector_t',['../classstan_1_1io_1_1reader.html#a1285611f704d4a3ec0d0e10ff98206d4',1,'stan::io::reader']]],
+  ['matrix_5fd',['matrix_d',['../namespacestan_1_1math.html#ac1a24821b475ee296830ebac12f70460',1,'stan::math::matrix_d()'],['../namespacestan_1_1optimization.html#afd985f1c123f049985ad18ad074474c4',1,'stan::optimization::matrix_d()']]],
+  ['matrix_5ffv',['matrix_fv',['../namespacestan_1_1agrad.html#ab98368cbab6489ad4274b58c3cb4be60',1,'stan::agrad']]],
+  ['matrix_5ft',['matrix_t',['../classstan_1_1io_1_1reader.html#aded74129931435c293b0fd0885535d93',1,'stan::io::reader::matrix_t()'],['../classstan_1_1io_1_1writer.html#aadbf07fa0a161b2037f3dfbe0548f263',1,'stan::io::writer::matrix_t()']]],
+  ['matrix_5fv',['matrix_v',['../namespacestan_1_1agrad.html#a31a0e0ac129c0a7d965b1fb17301c942',1,'stan::agrad']]]
+];
