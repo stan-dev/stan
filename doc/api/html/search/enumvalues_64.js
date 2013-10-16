70efc09f3dfe4de72e40bdfe57f4fa9c46986b0f
Daniel Lee <bearlee@alum.mit.edu>
2013-10-16 16:14:34 -0400

    removing stan-reference-2.0.0.pdf and doxygen from develop.

--- doc/api/html/search/enumvalues_64.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['dataerr',['DATAERR',['../structstan_1_1gm_1_1error__codes.html#aa63873e8f7e9dd7f8af3502e4003292fa6b2a96b1b804a9792a1355d28cc9c44d',1,'stan::gm::error_codes']]]
-];
