0953b377c2ffc393467fc785e50c947b5725100e
Marcus Brubaker <mbrubake@cs.toronto.edu>
2013-10-29 19:16:14 -0400

    Rename trace_inv_quad_form_ldlt with three arguments to trace_gen_inv_quad_form_ldlt to be consistent with the non-inverse form of the trace-of-quadratic-form functions.

--- src/stan/math/matrix/ldlt.hpp
+++ src/stan/math/matrix/ldlt.hpp
@@ -110,13 +110,13 @@ namespace stan {
      */
     template <int R1,int C1,int R2,int C2,int R3,int C3>
     inline double
-    trace_inv_quad_form_ldlt(const Eigen::Matrix<double,R1,C1> &D,
-                             const stan::math::LDLT_factor<double,R2,C2> &A,
-                             const Eigen::Matrix<double,R3,C3> &B)
+    trace_gen_inv_quad_form_ldlt(const Eigen::Matrix<double,R1,C1> &D,
+                                 const stan::math::LDLT_factor<double,R2,C2> &A,
+                                 const Eigen::Matrix<double,R3,C3> &B)
     {
-      stan::math::validate_square(D,"trace_inv_quad_form_ldlt");
-      stan::math::validate_multiplicable(A,B,"trace_inv_quad_form_ldlt");
-      stan::math::validate_multiplicable(B,D,"trace_inv_quad_form_ldlt");
+      stan::math::validate_square(D,"trace_gen_inv_quad_form_ldlt");
+      stan::math::validate_multiplicable(A,B,"trace_gen_inv_quad_form_ldlt");
+      stan::math::validate_multiplicable(B,D,"trace_gen_inv_quad_form_ldlt");
       
       return (D*B.transpose()*A._ldltP->solve(B)).trace();
     }
