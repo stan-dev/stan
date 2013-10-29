9df01c17d8bc5687f861114cdd6dd59f4f2ee2fd
Michael Betancourt <betanalpha@gmail.com>
2013-10-29 10:24:35 +0000

    Added test for integrator symplecticness

--- src/test/mcmc/hmc/integrators/expl_leapfrog_test.cpp
+++ src/test/mcmc/hmc/integrators/expl_leapfrog_test.cpp
@@ -398,7 +398,90 @@ TEST_F(McmcHmcIntegratorsExplLeapfrog, energy_conservation) {
     
   }
 
+  // Average error in Hamiltonian should be O(epsilon^{2})
+  // in general, smaller for the gaussian case due to cancellations
   EXPECT_NEAR(aveDeltaH, 0, epsilon * epsilon);
+  
+}
+
+TEST_F(McmcHmcIntegratorsExplLeapfrog, symplecticness) {
+  
+  rng_t base_rng(0);
+  
+  std::fstream data_stream(std::string("").c_str(), std::fstream::in);
+  stan::io::dump data_var_context(data_stream);
+  data_stream.close();
+  
+  gauss_namespace::gauss model(data_var_context, &std::cout);
+  
+  stan::mcmc::expl_leapfrog<
+  stan::mcmc::unit_e_metric<gauss_namespace::gauss, rng_t>,
+  stan::mcmc::unit_e_point> integrator;
+  
+  stan::mcmc::unit_e_metric<gauss_namespace::gauss, rng_t> metric(model, &std::cout);
+  
+  // Create a circle of points
+  const int n_points = 1000;
+  
+  double pi = 3.141592653589793;
+  double r = 1.5;
+  double q0 = 1;
+  double p0 = 0;
+  
+  std::vector<stan::mcmc::unit_e_point> z;
+  
+  for (int i = 0; i < n_points; ++i) {
+    z.push_back(stan::mcmc::unit_e_point(1, 0));
+    
+    double theta = 2 * pi * (double)i / (double)n_points;
+    z.back().q.at(0) = r * cos(theta) + q0;
+    z.back().p(0)    = r * sin(theta) + p0;
+  }
+  
+  // Evolve circle
+  double epsilon = 1e-3;
+  size_t L = pi / epsilon;
+  
+  for (int i = 0; i < n_points; ++i)
+    metric.init(z.at(i));
+  
+  for (int n = 0; n < L; ++n)
+    for (int i = 0; i < n_points; ++i)
+      integrator.evolve(z.at(i), metric, epsilon);
+  
+  // Compute area of evolved shape using divergence theorem in 2D
+  double area = 0;
+  
+  for (int i = 0; i < n_points; ++i) {
+    
+    double x1 = z[i].q.at(0);
+    double y1 = z[i].p(0);
+    double x2 = z[(i + 1) % n_points].q.at(0);
+    double y2 = z[(i + 1) % n_points].p(0);
+    
+    double x_bary = 0.5 * (x1 + x2);
+    double y_bary = 0.5 * (y1 + y2);
+    
+    double x_delta = x2 - x1;
+    double y_delta = y2 - y1;
+    
+    double a = sqrt( x_delta * x_delta + y_delta * y_delta);
+    
+    double x_norm = 1;
+    double y_norm = - x_delta / y_delta;
+    double norm = sqrt( x_norm * x_norm + y_norm * y_norm );
+    
+    a *= (x_bary * x_norm + y_bary * y_norm) / norm;
+    a = a < 0 ? -a : a;
+    
+    area += a;
+    
+  }
+  
+  area *= 0.5;
+  
+  // Symplectic integrators preserve volume (area in 2D)
+  EXPECT_NEAR(area, pi * r * r, 1e-2);
   
 }
 
