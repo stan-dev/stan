555c9d0de711ac580009b0caeaa215717a88f521
Daniel Lee <bearlee@alum.mit.edu>
2013-11-11 17:19:10 -0500

    removing generated doxygen and manual

--- doc/api/html/search/all_6a.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['jacobian',['jacobian',['../namespacestan_1_1agrad.html#a660e1cd77e3dcb11faf98da2521ee0f8',1,'stan::agrad::jacobian(const F &amp;f, const Eigen::Matrix&lt; double, Eigen::Dynamic, 1 &gt; &amp;x, Eigen::Matrix&lt; double, Eigen::Dynamic, 1 &gt; &amp;fx, Eigen::Matrix&lt; double, Eigen::Dynamic, Eigen::Dynamic &gt; &amp;J)'],['../namespacestan_1_1agrad.html#a7d496770e4f01fa73a4adf17e2ea1313',1,'stan::agrad::jacobian(const F &amp;f, const Eigen::Matrix&lt; T, Eigen::Dynamic, 1 &gt; &amp;x, Eigen::Matrix&lt; T, Eigen::Dynamic, 1 &gt; &amp;fx, Eigen::Matrix&lt; T, Eigen::Dynamic, Eigen::Dynamic &gt; &amp;J)'],['../namespacestan_1_1agrad.html#a4121d2877f4a3569f878b8e9207af3c1',1,'stan::agrad::jacobian(std::vector&lt; var &gt; &amp;dependents, std::vector&lt; var &gt; &amp;independents, std::vector&lt; std::vector&lt; double &gt; &gt; &amp;jacobian)']]],
-  ['jacobian_2ehpp',['jacobian.hpp',['../jacobian_8hpp.html',1,'']]]
-];
--- doc/api/html/search/typedefs_6e.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['nested',['Nested',['../struct_eigen_1_1_num_traits_3_01stan_1_1agrad_1_1var_01_4.html#aa313e91d6d8163393d270e3c70ad50cf',1,'Eigen::NumTraits&lt; stan::agrad::var &gt;']]],
-  ['noninteger',['NonInteger',['../struct_eigen_1_1_num_traits_3_01stan_1_1agrad_1_1var_01_4.html#a0b7008c189ffa66353158a30af2aabaf',1,'Eigen::NumTraits&lt; stan::agrad::var &gt;']]]
-];
