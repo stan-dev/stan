16b6f98cf0d52ad42944540580b43a112b83e8db
Michael Betancourt <betanalpha@gmail.com>
2013-11-14 23:04:56 +0000

    Updated tests

--- src/test/mcmc/fixed_param_sampler_test.cpp
+++ src/test/mcmc/fixed_param_sampler_test.cpp
@@ -1,12 +1,12 @@
 #include <stan/mcmc/chains.hpp>
-#include <stan/mcmc/persistent_sampler.hpp>
+#include <stan/mcmc/fixed_param_sampler.hpp>
 #include <stan/io/stan_csv_reader.hpp>
 
 #include <gtest/gtest.h>
 #include <test/models/utility.hpp>
 
 
-TEST(McmcPersistentSampler, check_persistency) {
+TEST(McmcFixedParamSampler, check_persistency) {
   
   std::vector<std::string> model_path;
   model_path.push_back("src");
@@ -16,7 +16,7 @@ TEST(McmcPersistentSampler, check_persistency) {
   model_path.push_back("proper");
   
   std::string command = convert_model_path(model_path);
-  command += " sample algorithm=persist output file=" + convert_model_path(model_path) + ".csv";
+  command += " sample algorithm=fixed_param output file=" + convert_model_path(model_path) + ".csv";
   run_command_output command_output;
   
   try {
@@ -34,5 +34,31 @@ TEST(McmcPersistentSampler, check_persistency) {
   for (int i = 0; i < chains.num_params(); ++i) {
     EXPECT_EQ(chains.samples(0, i).tail(1)(0), chains.samples(0, i).head(1)(0));
   }
+  
+}
+
+
+TEST(McmcFixedParamSampler, check_empty) {
+  
+  std::vector<std::string> model_path;
+  model_path.push_back("src");
+  model_path.push_back("test");
+  model_path.push_back("mcmc");
+  model_path.push_back("models");
+  model_path.push_back("empty");
+  
+  std::string command = convert_model_path(model_path);
+  command += " sample algorithm=fixed_param output file=" + convert_model_path(model_path) + ".csv";
+  run_command_output command_output;
+  
+  bool success = TRUE;
+  
+  try {
+    command_output = run_command(command);
+  } catch(...) {
+    success = FALSE;
+  }
+
+  EXPECT_EQ(success, TRUE);
   
 }
---
+++ src/test/mcmc/models/empty.stan
@@ -0,0 +1,10 @@
+parameters {
+}
+
+model {
+}
+
+generated_quantities {
+  real mu;
+  mu <- normal_rng(0, 1);
+}
