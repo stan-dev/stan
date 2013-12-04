9aa3f4bd49f4ff745fb6f0cca9f6debf6f838a3a
Daniel Lee <bearlee@alum.mit.edu>
2013-12-04 12:11:50 -0500

    added failing finite diff test for agrad mdivide_left_ldlt

--- src/test/agrad/rev/matrix/mdivide_left_ldlt_test.cpp
+++ src/test/agrad/rev/matrix/mdivide_left_ldlt_test.cpp
@@ -6,6 +6,57 @@
 #include <stan/math/matrix/mdivide_left_spd.hpp>
 #include <stan/math/matrix/multiply.hpp>
 #include <stan/agrad/rev/matrix/typedefs.hpp>
+#include <stan/math/matrix/typedefs.hpp>
+
+std::vector<double> finite_differences(const size_t row, const size_t col,
+                                         const stan::math::matrix_d A,
+                                         const bool calc_A_partials,
+                                         const stan::math::matrix_d B,
+                                         const bool calc_B_partials) {
+  double e = 1e-10;
+  std::vector<double> finite_diff;
+  stan::math::matrix_d C_plus, C_minus;
+
+  if (calc_A_partials) {
+    for (size_t j = 0; j < A.cols(); j++) {
+      for (size_t i = 0; i < A.rows(); i++) {
+        stan::math::matrix_d A_plus(A), A_minus(A);
+        A_plus(i,j) += e;
+        A_plus(j,i) = A_plus(i,j);
+        A_minus(i,j) -= e;
+        A_minus(j,i) = A_minus(i,j);
+        stan::math::LDLT_factor<double,-1,-1> ldlt_A_plus;
+        stan::math::LDLT_factor<double,-1,-1> ldlt_A_minus;
+        ldlt_A_plus.compute(A_plus);
+        ldlt_A_minus.compute(A_minus);
+      
+        C_plus = stan::math::mdivide_left_ldlt(ldlt_A_plus, B);
+        C_minus = stan::math::mdivide_left_ldlt(ldlt_A_minus, B);
+        finite_diff.push_back((C_plus(row,col) - C_minus(row,col)) / (2*e));
+      }
+    }
+  }
+  if (calc_B_partials) {
+    stan::math::LDLT_factor<double,-1,-1> ldlt_A;
+    ldlt_A.compute(A);
+    for (size_t j = 0; j < B.cols(); j++) {
+      for (size_t i = 0; i < B.rows(); i++) {
+        stan::math::matrix_d B_plus(B);
+        stan::math::matrix_d B_minus(B);
+        B_plus(i,j) += e;
+        B_plus(j,i) = B_plus(i,j);
+        B_minus(i,j) -= e;
+        B_minus(j,i) = B_minus(i,j);
+        
+        C_plus = stan::math::mdivide_left_ldlt(ldlt_A, B_plus);
+        C_minus = stan::math::mdivide_left_ldlt(ldlt_A, B_minus);
+        finite_diff.push_back((C_plus(row,col) - C_minus(row,col)) / (2*e));
+      }
+    }
+  }
+  return finite_diff;
+}
+
 
 TEST(AgradRevMatrix,mdivide_left_ldlt_val) {
   using stan::math::matrix_d;
@@ -20,7 +71,7 @@ TEST(AgradRevMatrix,mdivide_left_ldlt_val) {
     3.0, 7.0;
   Ad << 2.0, 3.0, 
     3.0, 7.0;
-
+  
   ldlt_Av.compute(Av);
   ldlt_Ad.compute(Ad);
 
@@ -48,8 +99,6 @@ TEST(AgradRevMatrix,mdivide_left_ldlt_grad_vv) {
   using stan::agrad::matrix_v;
   using stan::math::multiply;
   using stan::math::mdivide_left_spd;
-
-  
 
   matrix_d Ad(2,2), Ad_tmp(2,2);
   matrix_d Bd(2,2), Bd_tmp(2,2);
@@ -99,6 +148,60 @@ TEST(AgradRevMatrix,mdivide_left_ldlt_grad_vv) {
   }
 }
 
+TEST(AgradRevMatrix,mdivide_left_ldlt_finite_diff_vv) {
+  using stan::math::matrix_d;
+  using stan::agrad::matrix_v;
+  using stan::math::multiply;
+  using stan::math::mdivide_left_spd;
+
+  matrix_d Ad(2,2), Ad_tmp(2,2);
+  matrix_d Bd(2,2), Bd_tmp(2,2);
+  matrix_d Cd(2,2);
+  
+  Ad << 2.0, 3.0, 
+  3.0, 7.0;
+  Bd << 12.0, 13.0, 
+  15.0, 17.0;
+
+  stan::math::LDLT_factor<double,-1,-1> ldlt_Ad;
+  ldlt_Ad.compute(Ad);
+  
+  for (size_t i = 0; i < Bd.rows(); i++) {
+    for (size_t j = 0; j < Bd.cols(); j++) {
+      // compute derivatives
+      matrix_v A(2,2), B(2,2), C;
+      for (size_t k = 0; k < 4; k++) {
+        A(k) = Ad(k);
+        B(k) = Bd(k);
+      }
+      stan::math::LDLT_factor<stan::agrad::var,-1,-1> ldlt_A;
+      ldlt_A.compute(A);
+      
+      C = mdivide_left_ldlt(ldlt_A,B);
+      AVEC x = createAVEC(A(0,0),A(1,0),A(0,1),A(1,1),
+                          B(0,0),B(1,0),B(0,1),B(1,1));
+      VEC gradient;
+      C(i,j).grad(x,gradient);
+
+      // compute finite differences
+      VEC finite_diffs = finite_differences(i, j, Ad, true, Bd, true);
+      
+      ASSERT_EQ(gradient.size(), finite_diffs.size());
+      for (size_t k = 0; k < gradient.size(); k++)
+        EXPECT_NEAR(finite_diffs[k], gradient[k], 1e-4);
+
+      //----------------------------------------
+      std::cout << std::setw(15) << "grad" 
+                << std::setw(15) << "finite diff" << std::endl;
+      for (size_t k = 0; k < gradient.size(); k++) {
+        std::cout << std::setw(15) << gradient[k]
+                  << std::setw(15) << finite_diffs[k] << std::endl;
+      }
+      //----------------------------------------
+    }
+  }
+}
+
 TEST(AgradRevMatrix,mdivide_left_ldlt_grad_dv) {
   using stan::math::matrix_d;
   using stan::agrad::matrix_v;
@@ -144,6 +247,57 @@ TEST(AgradRevMatrix,mdivide_left_ldlt_grad_dv) {
   }
 }
 
+TEST(AgradRevMatrix,mdivide_left_ldlt_finite_diff_dv) {
+  using stan::math::matrix_d;
+  using stan::agrad::matrix_v;
+  using stan::math::multiply;
+  using stan::math::mdivide_left_spd;
+
+  matrix_d Ad(2,2), Ad_tmp(2,2);
+  matrix_d Bd(2,2), Bd_tmp(2,2);
+  matrix_d Cd(2,2);
+  
+  Ad << 2.0, 3.0, 
+  3.0, 7.0;
+  Bd << 12.0, 13.0, 
+  15.0, 17.0;
+
+  stan::math::LDLT_factor<double,-1,-1> ldlt_Ad;
+  ldlt_Ad.compute(Ad);
+  
+  for (size_t i = 0; i < Bd.rows(); i++) {
+    for (size_t j = 0; j < Bd.cols(); j++) {
+      // compute derivatives
+      matrix_v B(2,2), C;
+      for (size_t k = 0; k < 4; k++) {
+        B(k) = Bd(k);
+      }
+
+      C = mdivide_left_ldlt(ldlt_Ad,B);
+      AVEC x = createAVEC(B(0,0),B(1,0),B(0,1),B(1,1));
+      VEC gradient;
+      C(i,j).grad(x,gradient);
+
+      // compute finite differences
+      VEC finite_diffs = finite_differences(i, j, Ad, true, Bd, false);
+
+      ASSERT_EQ(gradient.size(), finite_diffs.size());
+      for (size_t k = 0; k < gradient.size(); k++)
+        EXPECT_NEAR(finite_diffs[k], gradient[k], 1e-4);
+
+      //----------------------------------------
+      std::cout << std::setw(15) << "grad" 
+                << std::setw(15) << "finite diff" << std::endl;
+      for (size_t k = 0; k < gradient.size(); k++) {
+        std::cout << std::setw(15) << gradient[k]
+                  << std::setw(15) << finite_diffs[k] << std::endl;
+      }
+      //----------------------------------------
+    }
+  }
+}
+
+
 TEST(AgradRevMatrix,mdivide_left_ldlt_grad_vd) {
   using stan::math::matrix_d;
   using stan::agrad::matrix_v;
@@ -185,6 +339,59 @@ TEST(AgradRevMatrix,mdivide_left_ldlt_grad_vd) {
         Cd = -mdivide_left_spd(Ad,multiply(Ad_tmp,mdivide_left_spd(Ad,Bd)));
         EXPECT_NEAR(Cd(i,j),g[k],1.0E-12);
       }
+    }
+  }
+}
+
+TEST(AgradRevMatrix,mdivide_left_ldlt_finite_diff_vd) {
+  using stan::math::matrix_d;
+  using stan::agrad::matrix_v;
+  using stan::math::multiply;
+  using stan::math::mdivide_left_spd;
+
+  matrix_d Ad(2,2), Ad_tmp(2,2);
+  matrix_d Bd(2,2), Bd_tmp(2,2);
+  matrix_d Cd(2,2);
+  
+  Ad << 2.0, 3.0, 
+  3.0, 7.0;
+  Bd << 12.0, 13.0, 
+  15.0, 17.0;
+
+  stan::math::LDLT_factor<double,-1,-1> ldlt_Ad;
+  ldlt_Ad.compute(Ad);
+  
+  for (size_t i = 0; i < Bd.rows(); i++) {
+    for (size_t j = 0; j < Bd.cols(); j++) {
+      // compute derivatives
+      matrix_v A(2,2), C;
+      for (size_t k = 0; k < 4; k++) {
+        A(k) = Ad(k);
+      }
+      stan::math::LDLT_factor<stan::agrad::var,-1,-1> ldlt_A;
+      ldlt_A.compute(A);
+      
+      C = mdivide_left_ldlt(ldlt_A,Bd);
+      AVEC x = createAVEC(A(0,0),A(1,0),A(0,1),A(1,1));
+                          
+      VEC gradient;
+      C(i,j).grad(x,gradient);
+      
+      // compute finite differences
+      VEC finite_diffs = finite_differences(i, j, Ad, false, Bd, true);
+
+      ASSERT_EQ(gradient.size(), finite_diffs.size());
+      for (size_t k = 0; k < gradient.size(); k++)
+        EXPECT_NEAR(finite_diffs[k], gradient[k], 1e-4);
+
+      //----------------------------------------
+      std::cout << std::setw(15) << "grad" 
+                << std::setw(15) << "finite diff" << std::endl;
+      for (size_t k = 0; k < gradient.size(); k++) {
+        std::cout << std::setw(15) << gradient[k]
+                  << std::setw(15) << finite_diffs[k] << std::endl;
+      }
+      //----------------------------------------
     }
   }
 }
