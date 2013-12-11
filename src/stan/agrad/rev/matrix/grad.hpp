d8740cfee05fe706f96c6446a9d66915d4d453f2
Michael Betancourt <betanalpha@gmail.com>
2013-12-11 18:00:39 -0500

    Whoops, forgot the all important new agrad file

---
+++ src/stan/agrad/rev/matrix/grad.hpp
@@ -0,0 +1,24 @@
+#ifndef __STAN__AGRAD__REV__MATRIX__GRAD_HPP__
+#define __STAN__AGRAD__REV__MATRIX__GRAD_HPP__
+
+#include <stan/math/matrix/Eigen.hpp>
+#include <stan/agrad/rev/var.hpp>
+
+namespace stan {
+
+  namespace agrad {
+   
+    void grad(var& v,
+              Eigen::Matrix<var,Eigen::Dynamic,1>& x,
+              Eigen::VectorXd& g) {
+      stan::agrad::grad(v.vi_);
+      g.resize(x.size());
+      for (size_t i = 0; i < x.size(); ++i)
+        g(i) = x(i).vi_->adj_;
+      recover_memory();
+    }
+    
+  }
+}
+
+#endif
