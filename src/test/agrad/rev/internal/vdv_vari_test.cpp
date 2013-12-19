6e7e287b10e10421890b05a1db5c366349e30f2a
Daniel Lee <bearlee@alum.mit.edu>
2013-12-18 21:32:42 -0500

    moved agrad tests to src/test/unit/agrad; left distribution tests in place.

--- src/test/agrad/rev/internal/vdv_vari_test.cpp
+++
@@ -1,4 +0,0 @@
-#include <stan/agrad/rev/internal/vdv_vari.hpp>
-#include <test/agrad/util.hpp>
-#include <gtest/gtest.h>
-
--- src/test/agrad/rev/vari_test.cpp
+++
@@ -1,4 +0,0 @@
-#include <stan/agrad/rev/vari.hpp>
-#include <test/agrad/util.hpp>
-#include <gtest/gtest.h>
-
