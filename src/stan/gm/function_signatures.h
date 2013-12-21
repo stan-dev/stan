343d26dff9673db2407cc369e0dfad264a752a03
Bob Carpenter <carp@alias-i.com>
2013-12-21 15:59:16 -0500

    fixes issue #464

--- src/stan/gm/function_signatures.h
+++ src/stan/gm/function_signatures.h
@@ -171,6 +171,8 @@ add("cumulative_sum", VECTOR_T, VECTOR_T);
 add("cumulative_sum", ROW_VECTOR_T, ROW_VECTOR_T);
 add("determinant",DOUBLE_T,MATRIX_T);
 add("diag_matrix",MATRIX_T,VECTOR_T);
+add("diag_pre_multiply",MATRIX_T, VECTOR_T,MATRIX_T);
+add("diag_pre_multiply",MATRIX_T, ROW_VECTOR_T,MATRIX_T);
 add("diagonal",VECTOR_T,MATRIX_T);
 add_unary("digamma");
 // dims() is polymorphic by size
