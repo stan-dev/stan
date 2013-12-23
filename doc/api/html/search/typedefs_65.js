c245bc4887b63cae32a1dfda12e34d83ecc10db0
Daniel Lee <bearlee@alum.mit.edu>
2013-12-23 16:00:44 -0500

    Revert "adding artifacts (manual + api doc) to the repository for v2.1.0"
    This reverts commit cb8a91067d40ed64793aa88ad3ffefadbe665ce2.

--- doc/api/html/search/typedefs_65.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['expression_5ft',['expression_t',['../structstan_1_1gm_1_1expression.html#abae5cb68846d118c4e78e11afbaa4826',1,'stan::gm::expression']]]
-];
--- doc/api/html/search/variables_77.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['warmup',['warmup',['../structstan_1_1io_1_1stan__csv__timing.html#a26e38502db9dfbb27cf5747c8e96d229',1,'stan::io::stan_csv_timing']]],
-  ['while_5fstatement_5fr',['while_statement_r',['../structstan_1_1gm_1_1statement__grammar.html#ab8208e8ab3c4e29d3a153da6566a076d',1,'stan::gm::statement_grammar']]],
-  ['whitespace',['whitespace',['../structstan_1_1gm_1_1whitespace__grammar.html#a5b8a5755ab01dd301f1a32e1ef3dbf22',1,'stan::gm::whitespace_grammar']]]
-];
