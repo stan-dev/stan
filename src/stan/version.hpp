8b035d86612e4d872e789974f744827477128d79
Daniel Lee <bearlee@alum.mit.edu>
2014-02-13 20:41:10 -0500

    bumping version numbers

--- src/stan/version.hpp
+++ src/stan/version.hpp
@@ -9,7 +9,7 @@ namespace stan {
   const std::string MAJOR_VERSION = "2";
 
   /** Minor version number for Stan package. */
-  const std::string MINOR_VERSION = "1";
+  const std::string MINOR_VERSION = "2";
 
   /** Patch version for Stan package. */
   const std::string PATCH_VERSION = "0";
