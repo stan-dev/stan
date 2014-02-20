7af1295bc5f00247ffa3ae18c1a2fa72c2c19813
Daniel Lee <bearlee@alum.mit.edu>
2014-02-20 07:26:50 -0500

    Revert "A possible way to use to solve sign-compare warnings"
    This reverts commit c4a46eb1ff58bcfe27c5fd799ea9092ede42ed16.

--- src/stan/math/matrix/sort_indices.hpp
+++ src/stan/math/matrix/sort_indices.hpp
@@ -56,11 +56,12 @@ namespace stan {
        * @param xs Container to sort
        * @return sorted version of container
        */
-      template <bool ascending, typename C, typename signal>
-      std::vector<int> sort_indices(const C& xs, const signal size) {
+      template <bool ascending, typename C>
+      std::vector<int> sort_indices(const C& xs) {
+        size_t size = xs.size();
         std::vector<int> idxs;
         idxs.resize(size);
-        for (signal i(0); i < size; ++i)
+        for (int i = 0; i < size; ++i)
           idxs[i] = i + 1;
         index_comparator<ascending,C> comparator(xs);
         std::sort(idxs.begin(), idxs.end(), comparator);
@@ -78,7 +79,7 @@ namespace stan {
      */
     template <typename C>
     std::vector<int> sort_indices_asc(const C& xs) {
-      return sort_indices<true>(xs, xs.size());
+      return sort_indices<true>(xs);
     }
 
     /**
@@ -90,7 +91,7 @@ namespace stan {
      */
     template <typename C>
     std::vector<int> sort_indices_desc(const C& xs) {
-      return sort_indices<false>(xs, xs.size());
+      return sort_indices<false>(xs);
     }
 
 
