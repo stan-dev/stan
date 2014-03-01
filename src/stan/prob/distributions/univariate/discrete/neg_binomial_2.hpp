d0773f09cf141c64f122e159e75f6bc948bbde4c
Marco Inacio <marcoigarapava@gmail.com>
2014-02-28 23:08:12 -0300

    Added neg_binomial_2_log

--- src/stan/prob/distributions/univariate/discrete/neg_binomial_2.hpp
+++ src/stan/prob/distributions/univariate/discrete/neg_binomial_2.hpp
@@ -23,6 +23,130 @@ namespace stan {
 
   namespace prob {
 
+    // NegBinomial(n|mu,phi)  [mu >= 0; phi > 0;  n >= 0]
+    template <bool propto,
+              typename T_n,
+              typename T_location, typename T_inv_scale>
+    typename return_type<T_location, T_inv_scale>::type
+    neg_binomial_2_log(const T_n& n,
+                     const T_location& mu,
+                     const T_inv_scale& phi) {
+
+      static const char* function = "stan::prob::neg_binomial_log(%1%)";
+
+      using stan::math::check_finite;
+      using stan::math::check_nonnegative;
+      using stan::math::check_positive;
+      using stan::math::value_of;
+      using stan::math::check_consistent_sizes;
+      using stan::prob::include_summand;
+
+      // check if any vectors are zero length
+      if (!(stan::length(n)
+            && stan::length(mu)
+            && stan::length(phi)))
+        return 0.0;
+
+      double logp(0.0);
+      if (!check_nonnegative(function, n, "Failures variable", &logp))
+        return logp;
+      if (!check_finite(function, mu, "Location parameter", &logp))
+        return logp;
+      if (!check_positive(function, mu, "Location parameter", &logp))
+        return logp;
+      if (!check_finite(function, phi, "Inverse scale parameter",
+                        &logp))
+        return logp;
+      if (!check_positive(function, phi, "Inverse scale parameter",
+                          &logp))
+        return logp;
+      if (!(check_consistent_sizes(function,
+                                   n,mu,phi,
+                                   "Failures variable",
+                                   "Location parameter","Inverse scale parameter",
+                                   &logp)))
+        return logp;
+
+      // check if no variables are involved and prop-to
+      if (!include_summand<propto,T_location,T_inv_scale>::value)
+        return 0.0;
+
+      using stan::math::multiply_log;
+      using stan::math::log_sum_exp;
+      using stan::math::binomial_coefficient_log;
+      using boost::math::digamma;
+      using boost::math::lgamma;
+
+      // set up template expressions wrapping scalars into vector views
+      VectorView<const T_n> n_vec(n);
+      VectorView<const T_location> mu_vec(mu);
+      VectorView<const T_inv_scale> phi_vec(phi);
+      size_t size = max_size(n, mu, phi);
+
+      agrad::OperandsAndPartials<T_location,T_inv_scale>
+        operands_and_partials(mu, phi);
+
+      size_t len_ep = max_size(mu, phi);
+      size_t len_np = max_size(n, phi);
+
+      DoubleVectorView<true,is_vector<T_inv_scale>::value>
+        log_phi(length(phi));
+      for (size_t i = 0; i < length(phi); ++i)
+        log_phi[i] = log(value_of(phi_vec[i]));
+
+      DoubleVectorView<true,(is_vector<T_location>::value
+                             || is_vector<T_inv_scale>::value)>
+        log_mu_plus_phi(len_ep);
+      for (size_t i = 0; i < len_ep; ++i)
+        log_mu_plus_phi[i] = log(value_of(mu_vec[i]) + value_of(phi_vec[i]));
+
+      DoubleVectorView<true,(is_vector<T_n>::value
+                             || is_vector<T_inv_scale>::value)>
+        n_plus_phi(len_np);
+      for (size_t i = 0; i < len_np; ++i)
+        n_plus_phi[i] = n_vec[i] + value_of(phi_vec[i]);
+
+      for (size_t i = 0; i < size; i++) {
+        if (include_summand<propto>::value)
+          logp -= lgamma(n_vec[i] + 1.0);
+        if (include_summand<propto,T_inv_scale>::value)
+          logp += multiply_log(value_of(phi_vec[i]), value_of(phi_vec[i])) - lgamma(value_of(phi_vec[i]));
+        if (include_summand<propto,T_location,T_inv_scale>::value)
+          logp -= (n_plus_phi[i])*log_mu_plus_phi[i];
+        if (include_summand<propto,T_location>::value)
+          logp += multiply_log(n_vec[i], value_of(mu_vec[i]));
+        if (include_summand<propto,T_inv_scale>::value)
+          logp += lgamma(n_plus_phi[i]);
+
+        if (!is_constant_struct<T_location>::value)
+          operands_and_partials.d_x1[i]
+            += n_vec[i]/value_of(mu_vec[i]) 
+            - (n_vec[i] + value_of(phi_vec[i]))
+            / (value_of(mu_vec[i]) + value_of(phi_vec[i]));
+        if (!is_constant_struct<T_inv_scale>::value)
+          operands_and_partials.d_x2[i]
+            += 1.0 - n_plus_phi[i]/(value_of(mu_vec[i]) + value_of(phi_vec[i]))
+            + log_phi[i] - log_mu_plus_phi[i] - digamma(value_of(phi_vec[i])) + digamma(n_plus_phi[i]);
+      }
+      return operands_and_partials.to_var(logp);
+    }
+
+    template <typename T_n,
+              typename T_location, typename T_inv_scale>
+    inline
+    typename return_type<T_location, T_inv_scale>::type
+    neg_binomial_2_log(const T_n& n,
+                     const T_location& mu,
+                     const T_inv_scale& phi) {
+      return neg_binomial_2_log<false>(n, mu, phi);
+    }
+
+
+
+
+
+
+
     // NegBinomial(n|eta,phi)  [phi > 0;  n >= 0]
     template <bool propto,
               typename T_n,
@@ -52,12 +176,10 @@ namespace stan {
         return logp;
       if (!check_finite(function, eta, "Log location parameter", &logp))
         return logp;
-      //if (!check_positive(function, eta, "Log location parameter", &logp))
-      //  return logp;
       if (!check_finite(function, phi, "Inverse scale parameter",
                         &logp))
         return logp;
-      if (!check_nonnegative(function, phi, "Inverse scale parameter",
+      if (!check_positive(function, phi, "Inverse scale parameter",
                           &logp))
         return logp;
       if (!(check_consistent_sizes(function,
