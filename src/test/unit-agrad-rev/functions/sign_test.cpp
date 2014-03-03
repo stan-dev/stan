ecc42dc2750cfdc90ddce060fe88bca593c85dbd
Ben Goodrich <bg2382@columbia.edu>
2014-03-03 18:22:05 -0500

    add sign tests for rev and fwd

--- src/test/unit-agrad-rev/functions/sign_test.cpp
+++ src/test/unit-agrad-rev/functions/sign_test.cpp
@@ -1,9 +1,11 @@
 #include <stan/math/functions/sign.hpp>
+#include <stan/agrad/rev.hpp>
+
 #include <gtest/gtest.h>
 
 TEST(MathFunctions, sign) {
-  double x;
-
+  using stan::agrad::var;
+  var x;
   x = 0;
   EXPECT_EQ(0, stan::math::sign(x));
   x = 0.0000001;
