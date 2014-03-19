46389646781721154384ec5317d981f0c70c78c6
Daniel Lee <bearlee@alum.mit.edu>
2014-03-19 14:47:28 -0400

    adding documentation to mcmc_writer

--- src/stan/io/mcmc_writer.hpp
+++ src/stan/io/mcmc_writer.hpp
@@ -14,11 +14,27 @@ namespace stan {
   
   namespace io {
     
+    /**
+     * mcmc_writer writes out headers and samples
+     *
+     * @tparam M Model class
+     */
     template <class M>
     class mcmc_writer {
       
     public:
       
+      /**
+       * Constructor.
+       *
+       * @param sample_stream samples are "written" to this stream (can abstract this?)
+       * @param diagnostic_stream diagnostic information is "written" to this stream
+       * @param msg_stream messages are output to this stream
+       *
+       * @pre arguments == 0 if and only if they are not meant to be used
+       * @post none
+       * @sideeffects streams are stored in this object 
+       */
       mcmc_writer(std::ostream* sample_stream,
                   std::ostream* diagnostic_stream,
                   std::ostream* msg_stream = 0)
@@ -27,11 +43,29 @@ namespace stan {
           msg_stream_(msg_stream) {
       }
       
+      /**
+       * Outputs parameter string names. First outputs the names stored in 
+       * the sample object (stan::mcmc::sample), then uses the sampler provided
+       * to output sampler specific names, then adds the model constrained
+       * parameter names.
+       * 
+       * The names are written to the sample_stream as comma separated values
+       * with a newline at the end.
+       *
+       * @param sample a sample (unconstrained) that works with the model
+       * @param sampler a stan::mcmc::base_mcmc object
+       * @param model the model
+       *
+       * @pre none
+       * @post none
+       * @sideeffects sample_stream_ is written to with comma separated values
+       *   with a newline at the end
+       */
       void print_sample_names(stan::mcmc::sample& sample,
                               stan::mcmc::base_mcmc* sampler,
                               M& model) {
-        
-        if (!sample_stream_) return;
+        if (!sample_stream_) 
+          return;
         
         std::vector<std::string> names;
         
@@ -44,16 +78,30 @@ namespace stan {
           (*sample_stream_) << "," << names.at(i);
         }
         (*sample_stream_) << std::endl;
-        
       }
 
+
+      /**
+       * Outputs samples. First outputs the values of the sample params
+       * from a stan::mcmc::sample, then outputs the values of the sampler
+       * params from a stan::mcmc::base_mcmc, then finally outputs the values
+       * of the model.
+       *
+       * The samples are written to the sample_stream as comma separated values
+       * with a newline at the end.
+       *
+       * @param rng random number generator (used by model.write_array())
+       * @param sample the sample in constrained space
+       * @param sampler the sampler
+       * @param model the model
+       */
       template <class RNG>
       void print_sample_params(RNG& rng, 
                                stan::mcmc::sample& sample,
                                stan::mcmc::base_mcmc& sampler,
                                M& model) {
-        
-        if (!sample_stream_) return;
+        if (!sample_stream_) 
+          return;
         
         std::vector<double> values;
         
@@ -67,7 +115,7 @@ namespace stan {
                           model_values,
                           true, true,
                           msg_stream_); 
-
+        
         for (int i = 0; i < model_values.size(); ++i)
           values.push_back(model_values(i));
         
@@ -79,25 +127,52 @@ namespace stan {
         
       }
       
+      /**
+       * Prints additional sampler info to the stream.
+       * 
+       * @param sampler sampler
+       * @param stream stream to output stuff to
+       */
       void print_adapt_finish(stan::mcmc::base_mcmc* sampler, std::ostream* stream) {
-        
-        if (!stream) return;
+        if (!stream) 
+          return;
         
         *stream << "# Adaptation terminated" << std::endl;
         sampler->write_sampler_state(stream);
         
       }
       
+
+      /**
+       * Prints additional info to the streams
+       *
+       * Prints to both the sample stream and the diagnostic stream
+       *
+       * @param sampler sampler
+       */
       void print_adapt_finish(stan::mcmc::base_mcmc* sampler) {
         print_adapt_finish(sampler, sample_stream_);
         print_adapt_finish(sampler, diagnostic_stream_);
       }
-      
+
+
+      /**
+       * Print diagnostic names
+       *
+       * @param sample unconstrained sample
+       * @param sampler sampler
+       * @param model model
+       *
+       * @pre sample, sampler, and model are consistent.
+       * @post none
+       * @sideeffects diagnostic_stream_ is appended with comma
+       *   separated names with newline at the end
+       */
       void print_diagnostic_names(stan::mcmc::sample sample,
                                   stan::mcmc::base_mcmc* sampler,
                                   M& model) {
-        
-        if (!diagnostic_stream_) return;
+        if (!diagnostic_stream_) 
+          return;
         
         std::vector<std::string> names;
         
@@ -114,13 +189,24 @@ namespace stan {
           *diagnostic_stream_ << names.at(i);
         }
         *diagnostic_stream_ << std::endl;
-        
       }
       
+      /**
+       * Print diagnostic params to the diagnostic stream.
+       *
+       * @param sample unconstrained sample
+       * @param sampler sampler
+       * 
+       * @pre sample and sampler are consistent
+       * @post none.
+       * @sideeffects diagnostic_stream_ is appended with csv values of the
+       *   sample's get_sample_params(), the sampler's get_sampler_params(),
+       *   and get_sampler_diagnostics()
+       */
       void print_diagnostic_params(stan::mcmc::sample& sample,
                                    stan::mcmc::base_mcmc* sampler) {
-        
-        if (!diagnostic_stream_) return;
+        if (!diagnostic_stream_) 
+          return;
         
         std::vector<double> values;
         
@@ -133,9 +219,21 @@ namespace stan {
           (*diagnostic_stream_) << "," << values.at(i);
         }
         (*diagnostic_stream_) << std::endl;
-        
       }
       
+      /**
+       * Prints timing information
+       *
+       * @param warmDeltaT warmup time in seconds
+       * @param sampleDeltaT sample time in seconds
+       * @param stream output stream
+       * @param prefix prepend each line with the prefix; default is ""
+       *
+       * @pre none
+       * @post none
+       * @sideeffects stream is updated with information about timing
+       *
+       */
       void print_timing(double warmDeltaT, double sampleDeltaT, 
                         std::ostream* stream, const std::string& prefix = "") {
         if (!stream) return;
@@ -153,6 +251,13 @@ namespace stan {
                 << std::endl;
       }
       
+      
+      /**
+       * Print timing information to all streams
+       *
+       * @param warmDeltaT warmup time (sec)
+       * @param sampleDeltaT sample time (sec)
+       */
       void print_timing(double warmDeltaT, double sampleDeltaT) {
         print_timing(warmDeltaT, sampleDeltaT, sample_stream_, "#");
         print_timing(warmDeltaT, sampleDeltaT, diagnostic_stream_, "#");
@@ -160,7 +265,6 @@ namespace stan {
       }
       
     private:
-      
       std::ostream* sample_stream_;
       std::ostream* diagnostic_stream_;
       std::ostream* msg_stream_;
