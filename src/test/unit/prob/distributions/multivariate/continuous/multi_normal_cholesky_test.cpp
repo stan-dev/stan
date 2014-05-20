e83bd75892d8a1c6db2b429b1fbbe6f77eb1b51e
Marco Inacio <marcoigarapava@gmail.com>
2014-05-20 18:10:24 -0300

    multi_normal_cholesky: this test was added again by mistake when soving merging conflicts

--- src/test/unit/prob/distributions/multivariate/continuous/multi_normal_cholesky_test.cpp
+++ src/test/unit/prob/distributions/multivariate/continuous/multi_normal_cholesky_test.cpp
@@ -96,20 +96,6 @@ TEST(ProbDistributionsMultiNormalCholesky,MultiNormalOneRow) {
   EXPECT_FLOAT_EQ(-11.73908, stan::prob::multi_normal_cholesky_log(y,mu,L));
 }
 
-TEST(ProbDistributionsMultiNormal,MultiNormalMultiRow) {
-  Matrix<double,Dynamic,Dynamic> y(2,3);
-  y << 2.0, -2.0, 11.0,
-       4.0, -4.0, 22.0;
-  Matrix<double,Dynamic,1> mu(3,1);
-  mu << 1.0, -1.0, 3.0;
-  Matrix<double,Dynamic,Dynamic> Sigma(3,3);
-  Sigma << 9.0, -3.0, 0.0,
-    -3.0,  4.0, 0.0,
-    0.0, 0.0, 5.0;
-  Matrix<double,Dynamic,Dynamic> L = Sigma.llt().matrixL();
-  EXPECT_FLOAT_EQ(-54.2152, stan::prob::multi_normal_cholesky_log(y,mu,L));
-}
-
 
 TEST(ProbDistributionsMultiNormalCholesky, error_check) {
   boost::random::mt19937 rng;
