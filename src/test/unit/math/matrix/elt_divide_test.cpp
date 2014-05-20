e710d56a0b5f694840b6fdbc20dfec10a9042628
Alexey Stukalov <astukalov@cemm.oeaw.ac.at>
2014-05-21 01:19:01 +0200

    support scalars in elementwise division (./)
    fixes #656 by adding support for
    elementwise division of a scalar by vector/matrix
    and elementwise division of a vector/matrix by scalar

--- src/test/unit/math/matrix/elt_divide_test.cpp
+++ src/test/unit/math/matrix/elt_divide_test.cpp
@@ -18,6 +18,20 @@ TEST(MathMatrix,eltDivideVecException) {
   v2 << 10, 100, 1000;
   EXPECT_THROW(stan::math::elt_divide(v1,v2), std::domain_error);
 }
+TEST(MathMatrix,eltDivideVecByScalar) {
+  stan::math::vector_d v1(2);
+  v1 << 1, 2;
+  stan::math::vector_d v = stan::math::elt_divide(v1,10);
+  EXPECT_FLOAT_EQ(0.1, v(0));
+  EXPECT_FLOAT_EQ(0.2, v(1));
+}
+TEST(MathMatrix,eltDivideScalarByVec) {
+  stan::math::vector_d v1(2);
+  v1 << 1, 2;
+  stan::math::vector_d v = stan::math::elt_divide(10,v1);
+  EXPECT_FLOAT_EQ(10, v(0));
+  EXPECT_FLOAT_EQ(5, v(1));
+}
 TEST(MathMatrix,eltDivideRowVec) {
   stan::math::row_vector_d v1(2);
   stan::math::row_vector_d v2(2);
@@ -33,6 +47,20 @@ TEST(MathMatrix,eltDivideRowVecException) {
   v1 << 1, 2;
   v2 << 10, 100, 1000;
   EXPECT_THROW(stan::math::elt_divide(v1,v2), std::domain_error);
+}
+TEST(MathMatrix,eltDivideRowVecByScalar) {
+  stan::math::row_vector_d v1(2);
+  v1 << 1, 2;
+  stan::math::row_vector_d v = stan::math::elt_divide(v1,10);
+  EXPECT_FLOAT_EQ(0.1, v(0));
+  EXPECT_FLOAT_EQ(0.2, v(1));
+}
+TEST(MathMatrix,eltDivideScalarByRowVec) {
+  stan::math::row_vector_d v1(2);
+  v1 << 1, 2;
+  stan::math::row_vector_d v = stan::math::elt_divide(10,v1);
+  EXPECT_FLOAT_EQ(10, v(0));
+  EXPECT_FLOAT_EQ(5, v(1));
 }
 TEST(MathMatrix,eltDivideMatrix) {
   stan::math::matrix_d m1(2,3);
@@ -59,4 +87,32 @@ TEST(MathMatrix,eltDivideMatrixException) {
   m3 << 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24;
   EXPECT_THROW(stan::math::elt_divide(m1,m2),std::domain_error);
   EXPECT_THROW(stan::math::elt_divide(m1,m3),std::domain_error);
+}
+TEST(MathMatrix,eltDivideMatrixByScalar) {
+  stan::math::matrix_d m1(2,3);
+  m1 << 1, 2, 3, 4, 5, 6;
+  stan::math::matrix_d m = stan::math::elt_divide(m1,10);
+  
+  EXPECT_EQ(2,m.rows());
+  EXPECT_EQ(3,m.cols());
+  EXPECT_FLOAT_EQ(0.1, m(0,0));
+  EXPECT_FLOAT_EQ(0.2, m(0,1));
+  EXPECT_FLOAT_EQ(0.3, m(0,2));
+  EXPECT_FLOAT_EQ(0.4, m(1,0));
+  EXPECT_FLOAT_EQ(0.5, m(1,1));
+  EXPECT_FLOAT_EQ(0.6, m(1,2));
+}
+TEST(MathMatrix,eltDivideScalarByMatrix) {
+  stan::math::matrix_d m1(2,3);
+  m1 << 10, 100, 1000, 10000, 100000, 1000000;
+  stan::math::matrix_d m = stan::math::elt_divide(10,m1);
+  
+  EXPECT_EQ(2,m.rows());
+  EXPECT_EQ(3,m.cols());
+  EXPECT_FLOAT_EQ(1, m(0,0));
+  EXPECT_FLOAT_EQ(0.1, m(0,1));
+  EXPECT_FLOAT_EQ(0.01, m(0,2));
+  EXPECT_FLOAT_EQ(0.001, m(1,0));
+  EXPECT_FLOAT_EQ(0.0001, m(1,1));
+  EXPECT_FLOAT_EQ(0.00001, m(1,2));
 }
