6c57a8e194b50090cbe42dacbb87dd252e5af673
Daniel Lee <bearlee@alum.mit.edu>
2014-05-22 15:10:04 -0400

    refactoring: reimplemented error_handling/matrix

--- src/stan/io/reader.hpp
+++ src/stan/io/reader.hpp
@@ -765,7 +765,8 @@ namespace stan {
        */
       inline vector_t unit_vector(size_t k) {
         vector_t theta(vector(k));
-        stan::math::check_unit_vector("stan::io::unit_vector(%1%)", theta, "Constrained vector");
+        stan::math::check_unit_vector("stan::io::unit_vector(%1%)", theta, "Constrained vector",
+                                      (double*)0);
         return theta;
       }
 
@@ -812,7 +813,8 @@ namespace stan {
        */
       inline vector_t simplex(size_t k) {
         vector_t theta(vector(k));
-        stan::math::check_simplex("stan::io::simplex(%1%)", theta, "Constrained vector");
+        stan::math::check_simplex("stan::io::simplex(%1%)", theta, "Constrained vector",
+                                  (double*)0);
         return theta;
       }
 
@@ -903,7 +905,9 @@ namespace stan {
        */
       inline vector_t positive_ordered(size_t k) {
         vector_t x(vector(k));
-        stan::math::check_positive_ordered("stan::io::positive_ordered(%1%)", x, "Constrained vector");
+        stan::math::check_positive_ordered("stan::io::positive_ordered(%1%)", 
+                                           x, "Constrained vector",
+                                           (double*)0);
         return x;
       }
 
@@ -947,7 +951,9 @@ namespace stan {
        */
       inline matrix_t cholesky_factor(size_t M, size_t N) {
         matrix_t y(matrix(M,N));
-        stan::math::check_cholesky_factor("stan::io::cholesky_factor(%1%)", y, "Constrained matrix");
+        stan::math::check_cholesky_factor("stan::io::cholesky_factor(%1%)", 
+                                          y, "Constrained matrix",
+                                          (double*)0);
         return y;
       }
 
@@ -998,7 +1004,8 @@ namespace stan {
        */
       inline matrix_t cov_matrix(size_t k) {
         matrix_t y(matrix(k,k));
-        stan::math::check_cov_matrix("stan::io::cov_matrix(%1%)", y, "Constrained matrix");
+        stan::math::check_cov_matrix("stan::io::cov_matrix(%1%)", y, "Constrained matrix",
+                                     (double*)0);
         return y;
       }
 
@@ -1043,7 +1050,9 @@ namespace stan {
        */
       inline matrix_t corr_matrix(size_t k) {
         matrix_t x(matrix(k,k));
-        stan::math::check_corr_matrix("stan::math::corr_matrix(%1%)", x, "Constrained matrix");
+        stan::math::check_corr_matrix("stan::math::corr_matrix(%1%)", 
+                                      x, "Constrained matrix",
+                                      (double*)0);
         return x;
       }
 
