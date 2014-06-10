4765b18e7e4b7d82fc557498088095950fe5823c
Peter Li <Li.Peter94@gmail.com>
2014-06-09 20:20:39 -0400

    added more tests for normal distr with fvar

--- src/test/unit-agrad-fwd/prob/univariate/continuous/normal_test.cpp
+++ src/test/unit-agrad-fwd/prob/univariate/continuous/normal_test.cpp
@@ -3,6 +3,7 @@
 #include <stan/prob/distributions/univariate/continuous/normal.hpp>
 #include <gtest/gtest.h>
 #include <vector>
+#include <test/unit/agrad/util.hpp>
 
 
 std::vector<double> test_fun(double y, double mu, double sigma) {
@@ -47,10 +48,52 @@ TEST(ProbAgradDistributionsNormal, fwd) {
 
 TEST(ProbAgradDistributionsNormal, derivatives) {
   using stan::agrad::fvar;
+  using stan::agrad::var;
   using stan::prob::normal_log;
    
   std::vector<double> grad = test_fun(0,0,1);
 
   fvar<double> lp = normal_log<false>(0,0,fvar<double>(1.0,1));
   EXPECT_FLOAT_EQ(grad[2], lp.tangent());
+
+  fvar<var> y (2,1);
+  fvar<var> mu (0,1);
+  fvar<var> sigma (1,1);
+
+  fvar<var> logp = normal_log(y,mu,sigma);
+
+}
+TEST(ProbAgradDistributionsNormal, FvarVar_1stDeriv) {
+  using stan::agrad::fvar;
+  using stan::agrad::var;
+  using stan::prob::normal_log;
+   
+  fvar<var> y_ (2,1);
+  double mu (0);
+  double sigma (1);
+
+  fvar<var> logp = normal_log(y_,mu,sigma);
+
+  AVEC y = createAVEC(y_.val_);
+  VEC g;
+  logp.val_.grad(y,g);
+  EXPECT_FLOAT_EQ(-2, g[0]);
+
+}
+TEST(ProbAgradDistributionsNormal, FvarVar_2ndDeriv) {
+  using stan::agrad::fvar;
+  using stan::agrad::var;
+  using stan::prob::normal_log;
+   
+  fvar<var> y_ (2,1);
+  double mu (0);
+  double sigma (1);
+
+  fvar<var> logp = normal_log(y_,mu,sigma);
+
+  AVEC y = createAVEC(y_.val_);
+  VEC g;
+  logp.d_.grad(y,g);
+  EXPECT_FLOAT_EQ(-1, g[0]);
+
 }
