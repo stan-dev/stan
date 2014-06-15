9467ef71929810092cc312a353210312759ee8c3
Peter Li <Li.Peter94@gmail.com>
2014-06-14 21:21:47 -0400

    generalized trace_gen_quad_form for autodiff types and added tests

--- src/stan/math/matrix/trace_gen_quad_form.hpp
+++ src/stan/math/matrix/trace_gen_quad_form.hpp
@@ -6,25 +6,35 @@
 #include <stan/math/matrix/Eigen.hpp>
 #include <stan/math/error_handling/matrix/check_multiplicable.hpp>
 #include <stan/math/error_handling/matrix/check_square.hpp>
+#include <stan/agrad/fwd/matrix/multiply.hpp>
+#include <stan/agrad/rev/matrix/multiply.hpp>
+#include <stan/math/matrix/trace.hpp>
+#include <stan/math/matrix/multiply.hpp>
+#include <stan/math/matrix/transpose.hpp>
+#include <stan/agrad/rev/operators.hpp>
 
 namespace stan {
   namespace math {
     /**
      * Compute trace(D B^T A B).
      **/
-    template<int RD,int CD,int RA,int CA,int RB,int CB>
-    inline double
-    trace_gen_quad_form(const Eigen::Matrix<double,RD,CD> &D,
-                        const Eigen::Matrix<double,RA,CA> &A,
-                        const Eigen::Matrix<double,RB,CB> &B)
+    template<int RD,int CD,int RA,int CA,int RB,int CB,typename T>
+    inline T
+    trace_gen_quad_form(const Eigen::Matrix<T,RD,CD> &D,
+                        const Eigen::Matrix<T,RA,CA> &A,
+                        const Eigen::Matrix<T,RB,CB> &B)
     {
+      using stan::agrad::multiply;
+      using stan::math::multiply;
+
       stan::math::check_square("trace_gen_quad_form(%1%)",A,"A",(double*)0);
       stan::math::check_square("trace_gen_quad_form(%1%)",D,"D",(double*)0);
       stan::math::check_multiplicable("trace_gen_quad_form(%1%)",A,"A",
                                       B,"B",(double*)0);
       stan::math::check_multiplicable("trace_gen_quad_form(%1%)",B,"B",
                                       D,"D",(double*)0);
-      return (D*B.transpose()*A*B).trace();
+      return stan::math::trace(multiply(multiply(D,stan::math::transpose(B)),
+                                        multiply(A,B)));
     }
   }
 }
