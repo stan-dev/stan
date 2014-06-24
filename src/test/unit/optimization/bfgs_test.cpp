d07ceee7b2da9e1d5332b086166b6d880ae6f8f5
Daniel Lee <bearlee@alum.mit.edu>
2014-06-23 23:03:03 -0400

    adding placeholder tests for each untested method in bfgs.hpp

--- src/test/unit/optimization/bfgs_test.cpp
+++ src/test/unit/optimization/bfgs_test.cpp
@@ -196,3 +196,195 @@ TEST(OptimizationBfgs, rosenbrock_lbfgs_termconds) {
   EXPECT_EQ(ret,stan::optimization::TERM_RELGRAD);
   bfgs._conv_opts.tolRelGrad = 0;
 }
+
+TEST(OptimizationBfgs, ConvergenceOptions) {
+  FAIL() 
+    << "add tests as a means of fixing behavior";
+}
+
+TEST(OptimizationBfgs, LsOptions) {
+  FAIL() 
+    << "add tests as a means of fixing behavior";
+}
+
+TEST(OptimizationBfgs, BfgsMinimizer) {
+  FAIL()
+    << "add tests for class BFGSMinimizer (construction)";
+}
+
+TEST(OptimizationBfgs, BfgsMinimizer__ls_opts) {
+  FAIL()
+    << "add tests for class BFGSMinimizer._ls_opts";
+}
+
+TEST(OptimizationBfgs, BfgsMinimizer__conv_opts) {
+  FAIL()
+    << "add tests for class BFGSMinimizer._conv_opts";
+}
+
+TEST(OptimizationBfgs, BfgsMinimizer_get_qnupdate) {
+  FAIL()
+    << "add tests for class BFGSMinimizer.get_qnupdate()";
+}
+
+TEST(OptimizationBfgs, BfgsMinimizer_curr_f) {
+  FAIL()
+    << "add tests for class BFGSMinimizer.curr_f()";
+}
+
+TEST(OptimizationBfgs, BfgsMinimizer_curr_x) {
+  FAIL()
+    << "add tests for class BFGSMinimizer.curr_x()";
+}
+
+TEST(OptimizationBfgs, BfgsMinimizer_curr_g) {
+  FAIL()
+    << "add tests for class BFGSMinimizer.curr_g()";
+}
+
+TEST(OptimizationBfgs, BfgsMinimizer_curr_p) {
+  FAIL()
+    << "add tests for class BFGSMinimizer.curr_p()";
+}
+
+TEST(OptimizationBfgs, BfgsMinimizer_prev_f) {
+  FAIL()
+    << "add tests for class BFGSMinimizer.prev_f()";
+}
+
+TEST(OptimizationBfgs, BfgsMinimizer_prev_x) {
+  FAIL()
+    << "add tests for class BFGSMinimizer.prev_x()";
+}
+
+TEST(OptimizationBfgs, BfgsMinimizer_prev_g) {
+  FAIL()
+    << "add tests for class BFGSMinimizer.prev_g()";
+}
+
+TEST(OptimizationBfgs, BfgsMinimizer_prev_p) {
+  FAIL()
+    << "add tests for class BFGSMinimizer.prev_p()";
+}
+
+TEST(OptimizationBfgs, BfgsMinimizer_prev_step_size) {
+  FAIL()
+    << "add tests for class BFGSMinimizer.prev_step_size()";
+}
+
+TEST(OptimizationBfgs, BfgsMinimizer_rel_grad_norm) {
+  FAIL()
+    << "add tests for class BFGSMinimizer.rel_grad_norm()";
+}
+
+TEST(OptimizationBfgs, BfgsMinimizer_rel_obj_decrease) {
+  FAIL()
+    << "add tests for class BFGSMinimizer.rel_grad_norm()";
+}
+
+TEST(OptimizationBfgs, BfgsMinimizer_alpha0) {
+  FAIL()
+    << "add tests for class BFGSMinimizer.alpha0()";
+}
+
+TEST(OptimizationBfgs, BfgsMinimizer_alpha) {
+  FAIL()
+    << "add tests for class BFGSMinimizer.alpha()";
+}
+
+TEST(OptimizationBfgs, BfgsMinimizer_iter_num) {
+  FAIL()
+    << "add tests for class BFGSMinimizer.iter_num()";
+}
+
+TEST(OptimizationBfgs, BfgsMinimizer_note) {
+  FAIL()
+    << "add tests for class BFGSMinimizer.note()";
+}
+
+TEST(OptimizationBfgs, BfgsMinimizer_get_code_string) {
+  FAIL()
+    << "add tests for class BFGSMinimizer.get_code_string()";
+}
+
+TEST(OptimizationBfgs, BfgsMinimizer_initialize) {
+  FAIL()
+    << "add tests for class BFGSMinimizer.initialize()";
+}
+
+TEST(OptimizationBfgs, BfgsMinimizer_step) {
+  FAIL()
+    << "add tests for class BFGSMinimizer.step()";
+}
+
+TEST(OptimizationBfgs, BfgsMinimizer_minimize) {
+  FAIL()
+    << "add tests for class BFGSMinimizer.minimize()";
+}
+
+
+TEST(OptimizationBfgs, lp_no_jacobian) {
+  FAIL()
+    << "add tests for lp_no_jacobian() <- is this not used and should it be removed?";
+}
+
+TEST(OptimizationBfgs, ModelAdaptor) {
+  FAIL() 
+    << "add tests for ModelAdaptor (construction)";
+}
+
+TEST(OptimizationBfgs, ModelAdaptor_fevals) {
+  FAIL() 
+    << "add tests for ModelAdaptor.fevals()";
+}
+
+TEST(OptimizationBfgs, ModelAdaptor_operator_parens__matrix_double) {
+  FAIL() 
+    << "add tests for ModelAdaptor.operator(Eigen::Matrix, double)";
+}
+
+TEST(OptimizationBfgs, ModelAdaptor_operator_parens__matrix_double_matrix) {
+  FAIL() 
+    << "add tests for ModelAdaptor.operator(Eigen::Matrix, double, Eigen::Matrix)";
+}
+
+TEST(OptimizationBfgs, ModelAdaptor_df) {
+  FAIL() 
+    << "add tests for ModelAdaptor.fevals()";
+}
+
+TEST(OptimizationBfgs, BFGSLineSearch) {
+  FAIL() 
+    << "add tests for BFGSLineSearch (construction) -- see tests above";
+}
+
+
+TEST(OptimizationBfgs, BFGSLineSearch_initialize) {
+  FAIL() 
+    << "add tests for BFGSLineSearch.initialize()";
+}
+
+TEST(OptimizationBfgs, BFGSLineSearch_grad_evals) {
+  FAIL() 
+    << "add tests for BFGSLineSearch.grad_evals()";
+}
+
+TEST(OptimizationBfgs, BFGSLineSearch_logp) {
+  FAIL() 
+    << "add tests for BFGSLineSearch.logp()";
+}
+
+TEST(OptimizationBfgs, BFGSLineSearch_grad_norm) {
+  FAIL() 
+    << "add tests for BFGSLineSearch.grad_norm()";
+}
+
+TEST(OptimizationBfgs, BFGSLineSearch_grad) {
+  FAIL() 
+    << "add tests for BFGSLineSearch.grad()";
+}
+
+TEST(OptimizationBfgs, BFGSLineSearch_params_r) {
+  FAIL() 
+    << "add tests for BFGSLineSearch.params_r()";
+}
