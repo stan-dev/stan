061220e997a2e955b108fdc3a03591faa6bf36eb
Marco Inacio <marcoigarapava@gmail.com>
2014-06-26 17:32:31 -0300

    Minor fix to operator ^

--- src/stan/gm/grammars/term_grammar_def.hpp
+++ src/stan/gm/grammars/term_grammar_def.hpp
@@ -168,7 +168,6 @@ namespace stan {
         //            && !expr2.expression_type().is_primitive()) {
         // handle raising each member of a vector to a power?
         // }
-        print_var_origin(error_msgs,var_origin);
         if (!expr1.expression_type().is_primitive() 
             || !expr2.expression_type().is_primitive()) {
           error_msgs << "arguments to ^ must be primitive (real or int)"
