2e7f33af982993d0501c0568b68ac0ab22b48fcb
Peter Li <Li.Peter94@gmail.com>
2014-07-02 12:47:43 -0400

    added bfgs_linesearch tests

--- src/test/unit/optimization/bfgs_linesearch_test.cpp
+++ src/test/unit/optimization/bfgs_linesearch_test.cpp
@@ -39,8 +39,39 @@ TEST(OptimizationBfgsLinesearch, CubicInterp) {
 }
 
 TEST(OptimizationBfgsLinesearch, CubicInterp_6arg) {
-  FAIL() 
-    << "add tests for the 6 argument version of CubicInterp()";
+  using stan::optimization::CubicInterp;
+  static const unsigned int nVals = 5;
+  static const double xVals[5] = { -2.0, -1.0, 0.0, 1.0, 2.0 };
+  double x;
+
+  for (unsigned int i = 0; i < nVals; i++) {
+    const double &x0 = xVals[i];
+    const double f0 = x0*x0*x0/3.0 - x0;
+    const double df0 = x0*x0 - 1.0;
+    for (unsigned int j = 0; j < nVals; j++) {
+      if (i == j)
+        continue;
+
+      const double &x1 = xVals[j];
+      const double f1 = x1*x1*x1/3.0 - x1;
+      const double df1 = x1*x1 - 1.0;
+
+      x = CubicInterp(df0,x1-x0,f1-f0,df1,-3.0-x0,2.0-x0);
+      EXPECT_NEAR(-3.0,x0+x,1e-8);
+
+      x = CubicInterp(df0,x1-x0,f1-f0,df1,-3.0-x0,0.0-x0);
+      EXPECT_NEAR(-3.0,x0+x,1e-8);
+
+      x = CubicInterp(df0,x1-x0,f1-f0,df1,-1.0-x0,2.0-x0);
+      EXPECT_NEAR(1.0,x0+x,1e-8);
+
+      x = CubicInterp(df0,x1-x0,f1-f0,df1,0.0-x0,2.0-x0);
+      EXPECT_NEAR(1.0,x0+x,1e-8);
+
+      x = CubicInterp(df0,x1-x0,f1-f0,df1,0.5-x0,1.5-x0);
+      EXPECT_NEAR(1.0,x0+x,1e-8);
+    }
+  }
 }
 
 class linesearch_testfunc {
@@ -57,8 +88,63 @@ public:
 };
 
 TEST(OptimizationBfgsLinesearch, WolfLSZoom) {
-  FAIL()
-    << "add tests for WolfLSZoom()";
+  using stan::optimization::WolfLSZoom;
+
+  static const double c1 = 1e-4;
+  static const double c2 = 0.9;
+  static const double minAlpha = 1e-16;
+
+  linesearch_testfunc func1;
+  Eigen::Matrix<double,-1,1> x0,x1;
+  double f0,f1;
+  Eigen::Matrix<double,-1,1> p, gradx0,gradx1;
+  double alpha;
+  int ret;
+
+  x0.setOnes(5,1);
+  p = -gradx0;
+
+  func1(x0,f0,gradx0);
+
+  p = -gradx0;
+
+  double dfp = gradx0.dot(p);
+  alpha = 2.0;
+  x1 = x0 + alpha*p;
+  func1(x1,f1,gradx1);
+
+  double dfp2 = gradx1.dot(p);
+
+  ret = WolfLSZoom(alpha, x1, f1, gradx1,
+                   func1, x0, f0, dfp,
+                   c1*dfp, c2*dfp, p,
+                   minAlpha, f0, dfp,
+                   alpha, f1, dfp2, 1e-16);
+  EXPECT_EQ(0,ret);
+  EXPECT_NEAR(0.5,alpha,1e-8);
+  EXPECT_NEAR(0,(x1 - (x0 + alpha*p)).norm(),1e-8);
+  EXPECT_EQ(f1,func1(x1));
+  EXPECT_LE(f1,f0 + c1*alpha*p.dot(gradx0));
+  EXPECT_LE(std::fabs(p.dot(gradx1)),c2*std::fabs(p.dot(gradx0)));
+
+  alpha = 10.0;
+  x1 = x0 + alpha*p;
+  func1(x1,f1,gradx1);
+
+  dfp2 = gradx1.dot(p);
+
+  ret = WolfLSZoom(alpha, x1, f1, gradx1,
+                   func1, x0, f0, dfp,
+                   c1*dfp, c2*dfp, p,
+                   minAlpha, f0, dfp,
+                   alpha, f1, dfp2, 1e-16);
+
+  EXPECT_EQ(0,ret);
+  EXPECT_NEAR(0.5,alpha,1e-8);
+  EXPECT_NEAR(0,(x1 - (x0 + alpha*p)).norm(),1e-8);
+  EXPECT_EQ(f1,func1(x1));
+  EXPECT_LE(f1,f0 + c1*alpha*p.dot(gradx0));
+  EXPECT_LE(std::fabs(p.dot(gradx1)),c2*std::fabs(p.dot(gradx0)));
 }
 
 
