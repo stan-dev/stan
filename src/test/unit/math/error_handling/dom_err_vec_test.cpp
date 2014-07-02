791273c8a455f05479485211cff833604f1880a2
Daniel Lee <bearlee@alum.mit.edu>
2014-07-02 16:15:18 -0400

    adding test for 0 vs 1 indexing within dom_err_vec

--- src/test/unit/math/error_handling/dom_err_vec_test.cpp
+++ src/test/unit/math/error_handling/dom_err_vec_test.cpp
@@ -22,7 +22,7 @@ public:
                      << typeid(typename T::value_type).name()
                      << "): "
                      << y_name_
-                     << "[" << index_ << "] "
+                     << "[" << 1 + index_ << "] "
                      << " error_message "
                      << y[index_]
                      << " "
@@ -34,7 +34,7 @@ public:
   template <class T, class T_result, class T_msg>
   void test_throw(T y, T_msg msg2) {
     try {
-      stan::math::dom_err_vec<0, T, T_result, T_msg>
+      stan::math::dom_err_vec<1, T, T_result, T_msg>
         (index_, function_, y, y_name_, error_message_, msg2, 0);
       FAIL() << "expecting call to dom_err_vec<> to throw a domain_error,"
              << "but threw nothing";
@@ -99,4 +99,43 @@ TEST_F(MathErrorHandling_dom_err_vec, vvar_var_var) {
   T_msg msg2 = 50;
   
   test_throw<T, T_result, T_msg>(y,msg2);
+}
+
+
+TEST_F(MathErrorHandling_dom_err_vec, zero_indexed) {
+  std::string message;
+  int n = 5;
+  std::vector<double> y(20);
+  try {
+    stan::math::dom_err_vec<0>
+      (n, function_, y, y_name_, error_message_, "", (double*)0);
+    FAIL() << "expecting call to dom_err_vec<> to throw a domain_error,"
+           << "but threw nothing";
+  } catch(std::domain_error& e) {
+    message = e.what();
+  } catch(...) {
+    FAIL() << "expecting call to dom_err_vec<> to throw a domain_error,"
+           << "but threw a different type";
+  }
+
+  EXPECT_NE(std::string::npos, message.find("[5]"));
+}
+
+TEST_F(MathErrorHandling_dom_err_vec, one_indexed) {
+  std::string message;
+  int n = 5;
+  std::vector<double> y(20);
+  try {
+    stan::math::dom_err_vec<1>
+      (n, function_, y, y_name_, error_message_, "", (double*)0);
+    FAIL() << "expecting call to dom_err_vec<> to throw a domain_error,"
+           << "but threw nothing";
+  } catch(std::domain_error& e) {
+    message = e.what();
+  } catch(...) {
+    FAIL() << "expecting call to dom_err_vec<> to throw a domain_error,"
+           << "but threw a different type";
+  }
+
+  EXPECT_NE(std::string::npos, message.find("[6]"));
 }
