cd777b5e825dce2ec28171c814e953e907c6f77e
Marco Inacio <marcoigarapava@gmail.com>
2014-07-11 12:12:49 -0300

    Including auto differentiation to wiener first passage time density

--- src/stan/prob/distributions/univariate/continuous/wiener.hpp
+++ src/stan/prob/distributions/univariate/continuous/wiener.hpp
@@ -20,7 +20,6 @@
 #include <stan/agrad.hpp>
 #include <stan/math/functions/value_of.hpp>
 
-#define WIENER_ERR      0.000001
 
 using std::vector;
 using std::log;
@@ -47,7 +46,8 @@ namespace stan {
      */
 
     const double MY_PI = boost::math::constants::pi<double>(),
-                 MY_LN_SQRT_PI = std::log(std::sqrt(boost::math::constants::pi<double>()));
+                 MY_LN_SQRT_PI = std::log(std::sqrt(boost::math::constants::pi<double>())),
+                 WIENER_ERR = 0.000001;
 
     template <bool propto,
     typename T_y, typename T_alpha, typename T_tau, typename T_beta, typename T_delta>
@@ -66,39 +66,43 @@ namespace stan {
       using boost::math::isinf;
       using boost::math::isfinite;
 
-      typename promote_args<T_y,T_alpha,T_tau,T_beta,T_delta>::type lp(0.0);
+      typedef typename promote_args<T_y,T_alpha,T_tau,T_beta,T_delta>::type return_type;
+      return_type lp(0.0);
 
-      if (!check_not_nan (function, alpha, "Boundary separation", &lp))  return lp;
-      if (!check_not_nan (function, beta , "A-priori bias"      , &lp))  return lp;
-      if (!check_not_nan (function, tau  , "Nondecision time"   , &lp))  return lp;
-      if (!check_not_nan (function, delta, "Drift rate"         , &lp))  return lp;
-      if (!check_finite  (function, alpha, "Boundary separation", &lp))  return lp;
-      if (!check_finite  (function, beta , "A-priori bias"      , &lp))  return lp;
-      if (!check_finite  (function, tau  , "Nondecision time"   , &lp))  return lp;
-      if (!check_finite  (function, delta, "Drift rate"         , &lp))  return lp;
-      if (!check_positive(function, alpha, "Boundary separation", &lp))  return lp;
-      if (!check_positive(function, tau  , "Nondecision time"   , &lp))  return lp;
-      if (!check_bounded (function, beta , 0, 1, "A-priori bias", &lp))  return lp;
+      check_not_nan (function, y, "Random variable", &lp);
+      check_not_nan (function, alpha, "Boundary separation", &lp);
+      check_not_nan (function, beta , "A-priori bias"      , &lp);
+      check_not_nan (function, tau  , "Nondecision time"   , &lp);
+      check_not_nan (function, delta, "Drift rate"         , &lp);
+      check_finite  (function, alpha, "Boundary separation", &lp);
+      check_finite  (function, beta , "A-priori bias"      , &lp);
+      check_finite  (function, tau  , "Nondecision time"   , &lp);
+      check_finite  (function, delta, "Drift rate"         , &lp);
+      check_positive(function, y, "Random variable", &lp);
+      check_positive(function, alpha, "Boundary separation", &lp);
+      check_positive(function, tau  , "Nondecision time"   , &lp);
+      check_bounded (function, beta , 0, 1, "A-priori bias", &lp);
+      
 
-      if (fabs(y) < tau) {
+      if (y < tau) {
         lp = LOG_ZERO;
         return lp;
       }
-
-      double v = value_of(delta), w = value_of(beta), a = value_of(alpha), t = value_of(tau);
-      double kl, ks, tmp = 0, x = value_of(y), a2 = a * a;
-      int k, K;
-
-      // extract RT and accuracy from x
-      if (x < 0.0)  // error response
-        x = fabs(x);
-      else {      // correct response
-        w = 1.0 - w;
-        v = -v;
+      
+      if (!include_summand<propto,T_y,T_alpha,T_tau,T_beta,T_delta>::value) {
+        return 0;
       }
 
-      x = x - t; // remove non-decision time from x
-      x = x / a2; // convert t to normalized time tt
+
+      T_beta one_minus_beta = 1.0 - beta;
+      T_alpha alpha2 = alpha * alpha;
+      return_type x = y;
+      return_type kl, ks, tmp = 0;
+      return_type k, K;
+
+
+      x = x - tau; // remove non-decision time from x
+      x = x / alpha2; // convert t to normalized time tt
 
       // calculate number of terms needed for large t
       if (MY_PI * x * WIENER_ERR < 1) { // if error threshold is set low enough
@@ -120,18 +124,18 @@ namespace stan {
       if (ks < kl) { // if small t is better (i.e., lambda<0)
         K = ceil(ks); // round to smallest integer meeting error
         for (k = -floor((K - 1.0) / 2.0); k <= ceil((K - 1.0) / 2.0); k++)
-          tmp += (w + 2.0 * k) * exp(-(pow((w + 2.0 * k), 2)) / 2.0 / x); // increment sum
+          tmp += (one_minus_beta + 2.0 * k) * exp(-(pow((one_minus_beta + 2.0 * k), 2)) / 2.0 / x); // increment sum
         tmp = log(tmp) - 0.5 * log(2.0) - MY_LN_SQRT_PI - 1.5 * log(x); // add constant term
       }
       else { // if large t is better...
         K = ceil(kl); // round to smallest integer meeting error
         for (k = 1; k <= K; k++)
-          tmp += k * exp(-(pow(k, 2)) * (pow(MY_PI, 2)) * x / 2.0) * sin(k * MY_PI * w); // increment sum
+          tmp += k * exp(-(pow(k, 2)) * (pow(MY_PI, 2)) * x / 2.0) * sin(k * MY_PI * one_minus_beta); // increment sum
         tmp = log(tmp) + 2.0 * MY_LN_SQRT_PI; // add constant term
       }
 
       // convert to f(t|v,a,w) and return result
-      lp = tmp + ((-v * a * w -(pow(v, 2)) * (x * a2) / 2.0) - log(a2));
+      lp = delta * alpha * one_minus_beta - pow(delta, 2) * x * alpha2 / 2.0 - log(alpha2) + tmp;
 
       return lp;
     }
@@ -142,7 +146,8 @@ namespace stan {
     wiener_log(const T_y& y, const T_alpha& alpha, const T_tau& tau, const T_beta& beta, const T_delta& delta) {
       return wiener_log<false>(y,alpha,tau,beta,delta);
     }
-
+    
+    /* Not actually implemented yet
     template <class RNG>
     inline double
     wiener_rng(const double alpha,
@@ -151,10 +156,11 @@ namespace stan {
          const double delta,
                      RNG& rng) {
       using boost::variate_generator;
-      double a = tau+alpha*beta/stan::prob::normal_rng(delta,1,rng); //not actually implemented yet
+      double a = tau+alpha*beta/stan::prob::normal_rng(delta,1,rng); 
       //stan::prob::wiener_rng(alpha, tau, beta, delta, rng);
       return a;
     }
+    */
   }
 }
 #endif
