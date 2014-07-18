18198c30894dbfab165acee0846fdd383c734306
mitzimorris <mitzi@panix.com>
2014-07-18 15:22:59 -0400

    adding tests for model block behaviors

--- src/test/unit/common/sample_test.cpp
+++ src/test/unit/common/sample_test.cpp
@@ -1,92 +1,10 @@
 #include <stan/common/sample.hpp>
 #include <gtest/gtest.h>
-#include <test/test-models/no-main/common/test_lp.cpp>
 #include <sstream>
+#include <test/test-models/no-main/common/test_lp.cpp>
+#include <test/unit/common/mock_instantiation.hpp>
 
 typedef boost::ecuyer1988 rng_t;
-
-class mock_sampler : public stan::mcmc::base_mcmc {
-public:
-  mock_sampler(std::ostream *output, std::ostream *error)
-    : base_mcmc(output, error), n_transition_called(0) { }
-
-  stan::mcmc::sample transition(stan::mcmc::sample& init_sample) {
-    n_transition_called++;
-    return init_sample;
-  }
-
-  int n_transition_called;
-};
-
-struct mock_callback {
-  int n;
-  mock_callback() : n(0) { }
-  
-  void operator()() {
-    n++;
-  }
-};
-
-class StanCommon : public testing::Test {
-public:
-  void SetUp() {
-    output.str("");
-    error.str("");
-
-    model_output.str("");
-    sample_output.str("");
-    diagnostic_output.str("");
-    message_output.str("");
-    writer_output.str("");
-
-    sampler = new mock_sampler(&output, &error);
-
-    std::fstream empty_data_stream(std::string("").c_str());
-    stan::io::dump empty_data_context(empty_data_stream);
-    empty_data_stream.close();
-    
-    model = new stan_model(empty_data_context, &model_output);
-    
-    stan::common::recorder::csv sample_recorder(&sample_output, "# ");
-    stan::common::recorder::csv diagnostic_recorder(&diagnostic_output, "# ");
-    stan::common::recorder::messages message_recorder(&message_output, "# ");
-
-    writer = new stan::io::mcmc_writer<stan_model,
-                                       stan::common::recorder::csv,
-                                       stan::common::recorder::csv,
-                                       stan::common::recorder::messages>
-      (sample_recorder, diagnostic_recorder, message_recorder, &writer_output);
-
-    base_rng.seed(123456);
-
-    q = Eigen::VectorXd(0,1);
-    log_prob = 0;
-    stat = 0;
-  }
-  void TearDown() {
-    delete sampler;
-    delete model;
-    delete writer;
-  }
-  
-  mock_sampler* sampler;
-  stan_model* model;
-  stan::io::mcmc_writer<stan_model,
-                        stan::common::recorder::csv,
-                        stan::common::recorder::csv,
-                        stan::common::recorder::messages>* writer;
-  rng_t base_rng;
-
-  Eigen::VectorXd q;
-  double log_prob;
-  double stat;
-
-  std::stringstream output, error;
-
-  std::stringstream model_output,
-    sample_output, diagnostic_output, message_output,
-    writer_output;
-};
 
 
 TEST_F(StanCommon, sample) {
