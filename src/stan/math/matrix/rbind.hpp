2cb614ee31231992d678111cae488a9d250137b4
Marco Inacio <marcoigarapava@gmail.com>
2014-07-22 03:30:32 -0300

    rbind: functions with correct promotion of args and added agrad-rev tests

--- src/stan/math/matrix/rbind.hpp
+++ src/stan/math/matrix/rbind.hpp
@@ -22,14 +22,24 @@ namespace stan {
     inline Matrix<typename return_type<T1, T2>::type, Dynamic, Dynamic>
     rbind(const Matrix<T1, R1, C1> & A,
           const Matrix<T2, R2, C2> & B) {
+      int Arows = A.rows();
+      int Brows = B.rows();
+      int Acols = A.cols();
+      int Bcols = B.cols();
       check_size_match("rbind(%1%)",
-                       A.cols(), "columns of A",
-                       B.cols(), "columns of B",
+                       Acols, "columns of A",
+                       Bcols, "columns of B",
                        (double*)0);            
       
       Matrix<typename return_type<T1, T2>::type, Dynamic, Dynamic>
-        result(A.rows() + B.rows(), A.cols());
-      result << A, B;
+        result(Arows + Brows, Acols);
+      for (int j = 0; j < Acols; j++) {
+        for (int i = 0; i < Arows; i++)
+          result(i, j) = A(i, j);
+        for (int i = Arows, k = 0; k < Brows; i++, k++)
+          result(i, j) = B(k, j);
+      }
+      
       return result;
     }
        
@@ -38,7 +48,42 @@ namespace stan {
     inline Matrix<typename return_type<T1, T2>::type, Dynamic, 1>
     rbind(const Matrix<T1, R1, 1> & A,
           const Matrix<T2, R1, 1> & B) {          
-      Matrix<typename return_type<T1, T2>::type, Dynamic, 1>
+      int Asize = A.size();
+      int Bsize = B.size();
+      Matrix<typename return_type<T1, T2>::type, 1, Dynamic>
+        result(Asize + Bsize);
+      for (int i = 0; i < Asize; i++)
+        result(i) = A(i);
+      for (int i = 0, j = Asize; i < Bsize; i++, j++)
+        result(j) = B(i);
+      return result;
+    }
+    
+    //matrix rbind(matrix, matrix)
+    //matrix rbind(matrix, row_vector)
+    //matrix rbind(row_vector, matrix)
+    //matrix rbind(row_vector, row_vector)
+    template <typename T, int R1, int C1, int R2, int C2>
+    inline Matrix<T, Dynamic, Dynamic>
+    rbind(const Matrix<T, R1, C1> & A,
+          const Matrix<T, R2, C2> & B) {
+      check_size_match("rbind(%1%)",
+                       A.cols(), "columns of A",
+                       B.cols(), "columns of B",
+                       (double*)0);            
+      
+      Matrix<T, Dynamic, Dynamic>
+        result(A.rows() + B.rows(), A.cols());
+      result << A, B;
+      return result;
+    }
+       
+    //matrix rbind(vector, vector)
+    template <typename T, int R1, int R2>
+    inline Matrix<T, Dynamic, 1>
+    rbind(const Matrix<T, R1, 1> & A,
+          const Matrix<T, R1, 1> & B) {          
+      Matrix<T, Dynamic, 1>
         result(A.size()+B.size());
       result << A, B;
       return result;
---
+++ src/test/unit-agrad-rev/matrix/rbind_test.cpp
@@ -0,0 +1,70 @@
+#include <stan/math/matrix/rbind.hpp>
+#include <gtest/gtest.h>
+#include <test/unit/agrad/util.hpp>
+#include <stan/agrad/rev.hpp>
+#include <stan/agrad/rev/matrix.hpp>
+#include <stan/math/matrix/sum.hpp>
+#include <stan/agrad/rev/functions/exp.hpp>
+
+using stan::math::sum;
+using stan::math::rbind;
+using stan::agrad::matrix_v;
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+using Eigen::Matrix;
+using Eigen::Dynamic;
+
+typedef Matrix<AVAR, Dynamic, 1> vec_v;
+
+TEST(AgradRevMatrix, rbind_matrix) {
+  matrix_v a(2,2);
+  matrix_v a_exp(2,2);
+  MatrixXd b(2,2);
+  
+  a << 2.0, 3.0,
+       9.0, -1.0;
+       
+  b << 4.0, 3.0,
+       0.0, 1.0;
+
+  AVEC x;
+  for (int i = 0; i < 2; ++i) {
+    for (int j = 0; j < 2; ++j) {
+      x.push_back(a(i,j));
+      a_exp(i, j) = stan::agrad::exp(a(i, j));
+    }
+  }
+  
+  AVAR rbind_ab = sum(rbind(a_exp, b));
+
+  VEC g = cgradvec(rbind_ab, x);
+  
+  size_t idx = 0;
+  for (int i = 0; i < 2; i++)
+    for (int j = 0; j < 2; j++)
+      EXPECT_FLOAT_EQ(std::exp(a(i, j).val()), g[idx++]);
+}
+
+TEST(AgradRevMatrix, rbind_row_vector) {
+  vec_v a(3);
+  vec_v a_exp(3);
+  VectorXd b(2);
+  
+  a << 2.0, 3.0, 9.0;
+       
+  b << 4.0, 3.0, 0.0;
+
+  AVEC x;
+  for (int i = 0; i < 3; ++i) {
+    x.push_back(a(i));
+    a_exp(i) = stan::agrad::exp(a(i));
+  }
+  
+  AVAR rbind_ab = sum(rbind(a_exp, b));
+
+  VEC g = cgradvec(rbind_ab, x);
+  
+  size_t idx = 0;
+  for (int i = 0; i < 3; i++)
+    EXPECT_FLOAT_EQ(std::exp(a(i).val()), g[idx++]);
+}
