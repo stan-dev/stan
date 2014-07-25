77012f691255883ad34d7960739837e20b30ba84
Peter Li <Li.Peter94@gmail.com>
2014-07-25 13:37:33 -0400

    added empty tests for solve_ode

---
+++ src/test/unit/math/ode/solve_ode_test.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+#include <stan/math/ode/solve_ode.hpp>
+
+TEST(MathODE,solve_ode_to_std_vec) {
+}
+TEST(MathODE,solve_ode_to_eigen_vec) {
+}
+TEST(MathODE,solve_ode_ode_system) {
+}
+TEST(MathODE,solve_ode_observer) {
+}
+TEST(MathODE,solve_ode_solve_ode) {
+}
