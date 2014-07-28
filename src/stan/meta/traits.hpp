033bafb8e83eab19cd0d16934deee246f730cc84
Peter Li <Li.Peter94@gmail.com>
2014-07-28 14:33:55 -0400

    remove unused is_var_or_arithmetic

--- src/stan/meta/traits.hpp
+++ src/stan/meta/traits.hpp
@@ -497,23 +497,6 @@ ERROR_INDEX
               typename T4 = double, 
               typename T5 = double, 
               typename T6 = double>
-    struct is_var_or_arithmetic {
-      enum {
-        value = (is_var<typename scalar_type<T1>::type>::value || boost::is_arithmetic<typename scalar_type<T1>::type>::value)
-        && (is_var<typename scalar_type<T2>::type>::value || boost::is_arithmetic<typename scalar_type<T2>::type>::value)
-        && (is_var<typename scalar_type<T3>::type>::value || boost::is_arithmetic<typename scalar_type<T3>::type>::value)
-        && (is_var<typename scalar_type<T4>::type>::value || boost::is_arithmetic<typename scalar_type<T4>::type>::value)
-        && (is_var<typename scalar_type<T5>::type>::value || boost::is_arithmetic<typename scalar_type<T5>::type>::value)
-        && (is_var<typename scalar_type<T6>::type>::value || boost::is_arithmetic<typename scalar_type<T6>::type>::value)
-      };
-    };
-
-    template <typename T1, 
-              typename T2 = double, 
-              typename T3 = double, 
-              typename T4 = double, 
-              typename T5 = double, 
-              typename T6 = double>
     struct partials_return_type {
       typedef typename 
       boost::math::tools::promote_args<typename partials_type<typename scalar_type<T1>::type>::type,
