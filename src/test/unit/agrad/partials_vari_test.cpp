f793421f6e040ad8879b067698cce92b475b959d
Peter Li <Li.Peter94@gmail.com>
2014-07-28 14:12:25 -0400

    added fvar vector test for operands and partials

--- src/test/unit/agrad/partials_vari_test.cpp
+++ src/test/unit/agrad/partials_vari_test.cpp
@@ -161,6 +161,31 @@ TEST(AgradPartialsVari, OperandsAndPartialsFvar) {
   EXPECT_FLOAT_EQ(107,y.d_);
   EXPECT_FLOAT_EQ(-1,y.val_);
 }
+TEST(AgradPartialsVari, OperandsAndPartialsFvarVec) {
+  using stan::agrad::OperandsAndPartials;
+  using stan::agrad::fvar;
+
+  std::vector<fvar<double> > x1;
+  x1.push_back(fvar<double>(2.0,2.0));
+  x1.push_back(fvar<double>(1.0,3.0));
+
+  fvar<double> x2 = 3.0;
+  fvar<double> x3 = 5.0;
+  x2.d_ = -1.0;
+  x3.d_ = 4.0;
+
+  OperandsAndPartials<std::vector<fvar<double> >,fvar<double>,fvar<double> > o(x1, x2, x3);
+  o.d_x1[0] += 17.0; 
+  o.d_x1[1] += 13.0; 
+  o.d_x2[0] += 19.0;  
+  o.d_x2[0] += 19.0;  
+  o.d_x3[0] += 23.0;
+  o.d_x3[0] += 23.0;
+  fvar<double> y = o.to_var(-1.0,x1,x2,x3);
+
+  EXPECT_FLOAT_EQ(2*17 + 3*13 - 2*19 + 2*4*23,y.d_);
+  EXPECT_FLOAT_EQ(-1,y.val_);
+}
 
 TEST(AgradPartialsVari, incr_deriv_double) {
   using stan::VectorView;
