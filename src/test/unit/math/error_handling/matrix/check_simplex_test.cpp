0f801053ccdeb5243ec446c0c06c9d0ef8a15c6c
Daniel Lee <bearlee@alum.mit.edu>
2014-07-29 20:41:22 -0400

    updating error message within check_simplex

--- src/test/unit/math/error_handling/matrix/check_simplex_test.cpp
+++ src/test/unit/math/error_handling/matrix/check_simplex_test.cpp
@@ -14,3 +14,96 @@ TEST(MathErrorHandlingMatrix, checkSimplex) {
                                          y, "y", &result), 
                std::domain_error);
 }
+
+TEST(MathErrorHandlingMatrix, checkSimplex_message_negative_value) {
+  Eigen::Matrix<double,Eigen::Dynamic,1> y(100);
+  std::string message;
+  double result;
+
+
+  y << -0.1, 1.1;
+  try {
+    stan::math::check_simplex("checkSimplex(%1%)",
+                              y, "y", &result);
+    FAIL() << "should have thrown";
+  } catch (std::domain_error& e) {
+    message = e.what();
+  } catch (...) {
+    FAIL() << "threw the wrong error";
+  }
+
+  EXPECT_TRUE(std::string::npos != message.find(" y is not a valid simplex"))
+    << message;
+
+  EXPECT_TRUE(std::string::npos != message.find("y[1] = -0.1"))
+    << message;
+
+
+
+  y << 0.1, -0.1, 1.0;
+  try {
+    stan::math::check_simplex("checkSimplex(%1%)",
+                              y, "y", &result);
+    FAIL() << "should have thrown";
+  } catch (std::domain_error& e) {
+    message = e.what();
+  } catch (...) {
+    FAIL() << "threw the wrong error";
+  }
+
+  EXPECT_TRUE(std::string::npos != message.find(" y is not a valid simplex"))
+    << message;
+
+  EXPECT_TRUE(std::string::npos != message.find(" y[2] = -0.1"))
+    << message;
+}
+
+TEST(MathErrorHandlingMatrix, checkSimplex_message_sum) {
+  Eigen::Matrix<double,Eigen::Dynamic,1> y(100);
+  std::string message;
+  double result;
+
+  for (int n = 0; n < 100; n++)
+    y[n] = 0;
+  y[13] = 0.9;
+
+  try {
+    stan::math::check_simplex("checkSimplex(%1%)",
+                              y, "y", &result);
+    FAIL() << "should have thrown";
+  } catch (std::domain_error& e) {
+    message = e.what();
+  } catch (...) {
+    FAIL() << "threw the wrong error";
+  }
+
+  EXPECT_TRUE(std::string::npos != message.find(" y is not a valid simplex"))
+    << message;
+
+  EXPECT_TRUE(std::string::npos != message.find("sum(y) = 0.9"))
+    << message;
+}
+
+
+TEST(MathErrorHandlingMatrix, checkSimplex_message_length) {
+  Eigen::Matrix<double,Eigen::Dynamic,1> y;
+  std::string message;
+  double result;
+  y.resize(0);
+
+  try {
+    stan::math::check_simplex("checkSimplex(%1%)",
+                              y, "y", &result);
+    FAIL() << "should have thrown";
+  } catch (std::domain_error& e) {
+    message = e.what();
+  } catch (...) {
+    FAIL() << "threw the wrong error";
+  }
+
+  EXPECT_TRUE(std::string::npos != message.find(" y is not a valid simplex"))
+    << message;
+
+  EXPECT_TRUE(std::string::npos != message.find("length(y) = 0"))
+    << message;
+}
