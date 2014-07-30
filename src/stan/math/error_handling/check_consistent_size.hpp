55ca6ad0bafe54f331c92bc53f8518166b7ff083
Daniel Lee <bearlee@alum.mit.edu>
2014-07-30 11:58:05 -0400

    updating error message and test

--- src/stan/math/error_handling/check_consistent_size.hpp
+++ src/stan/math/error_handling/check_consistent_size.hpp
@@ -1,6 +1,7 @@
 #ifndef STAN__MATH__ERROR_HANDLING__CHECK_CONSISTENT_SIZE_HPP
 #define STAN__MATH__ERROR_HANDLING__CHECK_CONSISTENT_SIZE_HPP
 
+#include <sstream>
 #include <stan/math/error_handling/dom_err.hpp>
 #include <stan/meta/traits.hpp>
 
@@ -18,8 +19,19 @@ namespace stan {
         return true;
       if (!is_vector<T>::value && x_size == 1)
         return true;
+      
+      std::stringstream msg;
+      msg << " dimension=%1%, expecting dimension of either 1 or "
+          << "max_size=" << max_size
+          << "; a vectorized function was called with arguments of different "
+          << "scalar, array, vector, or matrix types, and they were not "
+          << "consistently sized;  all arguments must be scalars or "
+          << "multidimensional values of the same shape.";
+
+      std::string tmp(msg.str());
+
       return dom_err(function,x_size,name,
-                     " (max size) is %1%, but must be consistent, 1 or max=",max_size,
+                     tmp.c_str(), "",                     
                      result);
     }
 
--- src/test/unit/math/error_handling/check_consistent_size_test.cpp
+++ src/test/unit/math/error_handling/check_consistent_size_test.cpp
@@ -17,3 +17,33 @@ TEST(MathMatrixErrorHandling, checkConsistentSize) {
   EXPECT_TRUE(check_consistent_size(4U, function, v1, name1, &result));
   EXPECT_THROW(check_consistent_size(2U, function, v1, name1, &result), std::domain_error);
 }
+
+TEST(MathMatrixErrorHandling, checkConsistentSize_message) {
+  
+  using Eigen::Matrix;
+  using Eigen::Dynamic;
+  using stan::math::check_consistent_size;
+  using stan::size_of;
+
+  const char* function = "checkConsistentSize(%1%)";
+  const char* name1 = "name1";
+  
+  double result;
+  std::string message;
+  Matrix<double,Dynamic,1> v1(4);
+  EXPECT_EQ(4U, size_of(v1));
+  
+  try {
+    check_consistent_size(2U, function, v1, name1, &result);
+    FAIL() << "should have thrown";
+  } catch (std::domain_error& e) {
+    message = e.what();
+  } catch (...) {
+    FAIL() << "threw the wrong error";
+  }
+
+  EXPECT_TRUE(std::string::npos != message.find("name1 dimension=4"))
+    << message;
+  EXPECT_TRUE(std::string::npos != message.find("1 or max_size=2"))
+    << message;
+}
