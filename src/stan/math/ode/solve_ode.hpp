6fd48c0443e551643e2e538a50a5c4302880808f
Peter Li <Li.Peter94@gmail.com>
2014-07-31 19:39:54 -0400

    template out types in solve_ode

--- src/stan/math/ode/solve_ode.hpp
+++ src/stan/math/ode/solve_ode.hpp
@@ -5,6 +5,7 @@
 #include <boost/numeric/odeint.hpp>
 #include <stan/math/ode/util.hpp>
 #include <stan/agrad/rev/var.hpp>
+#include <stan/meta/traits.hpp>
 #include <stan/agrad/rev/internal/precomputed_gradients.hpp>
 
 namespace stan {
@@ -74,29 +75,66 @@ namespace stan {
         dy_dt.insert(dy_dt.end(), coupled_sys.begin(), coupled_sys.end());
       }
     };
+
+    std::vector<std::vector<double> > 
+    compute_results(const std::vector<std::vector<double> >& y,
+                    const std::vector<double>& theta,
+                    const size_t& num_eqns) {
+      return y;
+    }
+
+    std::vector<std::vector<stan::agrad::var> > 
+    compute_results(const std::vector<std::vector<double> >& y,
+                    const std::vector<stan::agrad::var>& theta,
+                    const size_t& num_eqns) {
+
+      std::vector<stan::agrad::var> temp_vars;
+      std::vector<double> temp_gradients;
+      std::vector<std::vector<stan::agrad::var> > y_return(y.size());
+
+      for (int i = 0; i < y.size(); i++) {
+        temp_vars.clear();
+        
+        //iterate over number of equations
+        for (int j = 0; j < num_eqns; j++) { 
+          temp_gradients.clear();
+          
+          //iterate over parameters for each equation
+          for (int k = 0; k < theta.size(); k++) { 
+            temp_gradients.push_back(y[i][num_eqns + theta.size()*j + k]);
+          }
+
+          temp_vars.push_back(stan::agrad::precomputed_gradients(y[i][j], theta, temp_gradients));
+        }
+
+        y_return[i] = temp_vars;
+      }
+
+      return y_return;
+    }
     
-    template <typename F, typename T>
-    std::vector<std::vector<T> >
+    template <typename F, typename T1, typename T2>
+    std::vector<std::vector<typename stan::return_type<T1,T2>::type> >
     solve_ode(const F& f,
-              const std::vector<T>& y0, //I don't think it's possible to have both y0 and theta as vars because that implies and what if we want part of these to be given? then would that go in data (x)?
-              const double& t0,
-              const std::vector<double>& ts,
-              const std::vector<T>& theta, //this should always be var--if it's know then just plug it into the system? or do we want to be able to handle this case too..?
+              const std::vector<T1>& y0, 
+              const double& t0, // initial time
+              const std::vector<double>& ts, // times at desired solutions
+              const std::vector<T2>& theta, // parameters
               const std::vector<double>& x, // double data values
               const std::vector<int>& x_int) { // int data values.
       using namespace boost::numeric::odeint;  // FIXME: trim to what is used
 
       std::vector<double> theta_dbl;
       for (int i = 0; i < theta.size(); i++)
-        theta_dbl.push_back(theta[i].val());
+        theta_dbl.push_back(value_of(theta[i]));
 
       ode_system<F> system(f,theta_dbl,x,x_int);
       double absolute_tolerance = 1e-6;
       double relative_tolerance = 1e-6;
 
-      std::vector<double> y0_vec((1+theta.size())*y0.size()); //depends on number of parameters...
+      std::vector<double> y0_vec((1+theta.size())*y0.size()); 
       for (size_t n = 0; n < y0.size(); n++)
-        y0_vec[n] = y0[n].val();
+        y0_vec[n] = value_of(y0[n]);
       for (size_t n = y0.size(); n < (1+theta.size()) * y0.size(); n++)
         y0_vec[n] = 0.0;
 
@@ -122,28 +160,10 @@ namespace stan {
          obs);
 
       std::vector<std::vector<double> > y = obs.get();
-      std::vector<stan::agrad::var> temp_vars;
-      std::vector<double> temp_gradients;
-      std::vector<std::vector<stan::agrad::var> > y_return(y.size());
 
-      for (int i = 0; i < y.size(); i++) {
-        temp_vars.clear();
-        for (int j = 0; j < y0.size(); j++) { //iterate over number of equations
-          temp_gradients.clear();
-          for (int k = 0; k < theta.size(); k++) { //iterate over parameters for each equation
-            temp_gradients.push_back(y[i][y0.size() + theta.size()*j + k]);
-          }
-
-          temp_vars.push_back(stan::agrad::precomputed_gradients(y[i][j], theta, temp_gradients));
-        }
-
-        y_return[i] = temp_vars;
-      }
-
-      return y_return;
+      return compute_results(y, theta, y0.size());
     }
-
-
+                   
   }
 }
 #endif
