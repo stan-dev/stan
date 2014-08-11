3b54c4d231b87efa1cfe8c5fa16c34e2e7274e30
Daniel Lee <bearlee@alum.mit.edu>
2014-08-11 12:47:19 -0400

    fixes #850. throws exception on nan input

--- src/stan/math/functions/Phi.hpp
+++ src/stan/math/functions/Phi.hpp
@@ -4,6 +4,7 @@
 #include <boost/math/tools/promotion.hpp>
 #include <boost/math/special_functions/erf.hpp>
 #include <stan/math/constants.hpp>
+#include <stan/math/error_handling/check_not_nan.hpp>
 
 namespace stan {
   namespace math {
@@ -30,6 +31,7 @@ namespace stan {
     Phi(const T x) {
       // overridden in fvar and var, so can hard-code boost versions
       // here for scalars only
+      check_not_nan("Phi(%1%)", x, "x", (double *)0);
       if (x < -37.5)
         return 0;
       else if (x < -5.0)
