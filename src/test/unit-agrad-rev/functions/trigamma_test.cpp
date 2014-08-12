2f556df14c618075c804962ad0c61300532dea5f
Peter Li <Li.Peter94@gmail.com>
2014-08-12 15:06:43 -0400

    added NaN test for trigamma

--- src/test/unit-agrad-rev/functions/trigamma_test.cpp
+++ src/test/unit-agrad-rev/functions/trigamma_test.cpp
@@ -13,3 +13,16 @@ TEST(AgradRev,trigamma) {
   f.grad(x,grad_f);
   EXPECT_FLOAT_EQ(-16.8288, grad_f[0]);
 }  
+
+TEST(AgradRev,trigamma_nan) {
+  AVAR a = std::numeric_limits<double>::quiet_NaN();
+  AVAR f = stan::math::trigamma(a);
+
+  AVEC x = createAVEC(a);
+  VEC g;
+  f.grad(x,g);
+  
+  EXPECT_TRUE(boost::math::isnan(f.val()));
+  ASSERT_EQ(1U,g.size());
+  EXPECT_TRUE(boost::math::isnan(g[0]));
+}
