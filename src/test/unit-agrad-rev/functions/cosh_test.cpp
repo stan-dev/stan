8ca7f2ba75a819a7bcf3b12da8ad9baff042d8f7
Peter Li <Li.Peter94@gmail.com>
2014-08-12 14:27:03 -0400

    added NaN test for cosh

--- src/test/unit-agrad-rev/functions/cosh_test.cpp
+++ src/test/unit-agrad-rev/functions/cosh_test.cpp
@@ -49,3 +49,16 @@ TEST(AgradRev,cosh_neg_inf) {
   f.grad(x,g);
   EXPECT_TRUE(boost::math::isinf(g[0]) && (g[0] < 0));
 }
+
+TEST(AgradRev,cosh_nan) {
+  AVAR a = std::numeric_limits<double>::quiet_NaN();
+  AVAR f = stan::agrad::cosh(a);
+
+  AVEC x = createAVEC(a);
+  VEC g;
+  f.grad(x,g);
+  
+  EXPECT_TRUE(boost::math::isnan(f.val()));
+  ASSERT_EQ(1U,g.size());
+  EXPECT_TRUE(boost::math::isnan(g[0]));
+}
