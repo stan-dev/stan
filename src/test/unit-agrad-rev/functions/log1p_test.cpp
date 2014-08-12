de5b605da557d30b2223c0d34872b75c804b876c
Peter Li <Li.Peter94@gmail.com>
2014-08-12 15:13:25 -0400

    added NaN test for log1p

--- src/test/unit-agrad-rev/functions/log1p_test.cpp
+++ src/test/unit-agrad-rev/functions/log1p_test.cpp
@@ -12,3 +12,16 @@ TEST(AgradRev,log1p) {
   f.grad(x,grad_f);
   EXPECT_FLOAT_EQ(1.0 / (1.0 + 0.1), grad_f[0]);
 }
+
+TEST(AgradRev,log1p_nan) {
+  AVAR a = std::numeric_limits<double>::quiet_NaN();
+  AVAR f = stan::agrad::log1p(a);
+
+  AVEC x = createAVEC(a);
+  VEC g;
+  f.grad(x,g);
+  
+  EXPECT_TRUE(boost::math::isnan(f.val()));
+  ASSERT_EQ(1U,g.size());
+  EXPECT_TRUE(boost::math::isnan(g[0]));
+}
