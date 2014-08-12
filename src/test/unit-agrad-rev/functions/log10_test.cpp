b1e504e2cd93139ad871b49ef9dd03d880a0573a
Peter Li <Li.Peter94@gmail.com>
2014-08-12 15:13:36 -0400

    added NaN test for log10

--- src/test/unit-agrad-rev/functions/log10_test.cpp
+++ src/test/unit-agrad-rev/functions/log10_test.cpp
@@ -11,3 +11,16 @@ TEST(AgradRev,log10_a) {
   f.grad(x,g);
   EXPECT_FLOAT_EQ(1.0/(log(10.0) * 5.0),g[0]);
 }
+
+TEST(AgradRev,log10_nan) {
+  AVAR a = std::numeric_limits<double>::quiet_NaN();
+  AVAR f = stan::agrad::log10(a);
+
+  AVEC x = createAVEC(a);
+  VEC g;
+  f.grad(x,g);
+  
+  EXPECT_TRUE(boost::math::isnan(f.val()));
+  ASSERT_EQ(1U,g.size());
+  EXPECT_TRUE(boost::math::isnan(g[0]));
+}
