41b27fa0f400c8f61fa5dd36acc711cd6010e874
Peter Li <Li.Peter94@gmail.com>
2014-08-12 15:05:58 -0400

    added NaN test for tgamma

--- src/test/unit-agrad-rev/functions/tgamma_test.cpp
+++ src/test/unit-agrad-rev/functions/tgamma_test.cpp
@@ -14,3 +14,16 @@ TEST(AgradRev,tgamma) {
   f.grad(x,grad_f);
   EXPECT_FLOAT_EQ(boost::math::digamma(3.5) * boost::math::tgamma(3.5),grad_f[0]);
 }  
+
+TEST(AgradRev,tgamma_nan) {
+  AVAR a = std::numeric_limits<double>::quiet_NaN();
+  AVAR f = stan::agrad::tgamma(a);
+
+  AVEC x = createAVEC(a);
+  VEC g;
+  f.grad(x,g);
+  
+  EXPECT_TRUE(boost::math::isnan(f.val()));
+  ASSERT_EQ(1U,g.size());
+  EXPECT_TRUE(boost::math::isnan(g[0]));
+}
