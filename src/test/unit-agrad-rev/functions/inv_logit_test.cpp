f26ccbf4549927fc56fdc680b2d26452a95e863e
Peter Li <Li.Peter94@gmail.com>
2014-08-12 14:48:55 -0400

    added NaN test for inv_logit

--- src/test/unit-agrad-rev/functions/inv_logit_test.cpp
+++ src/test/unit-agrad-rev/functions/inv_logit_test.cpp
@@ -1,4 +1,5 @@
 #include <stan/agrad/rev/functions/inv_logit.hpp>
+#include <boost/math/special_functions/fpclassify.hpp>
 #include <test/unit/agrad/util.hpp>
 #include <gtest/gtest.h>
 
@@ -12,4 +13,17 @@ TEST(AgradRev,inv_logit) {
   f.grad(x,grad_f);
   EXPECT_FLOAT_EQ(exp(-2.0)/pow(1 + exp(-2.0),2.0),
                   grad_f[0]);
+}
+
+TEST(AgradRev,inv_logit_nan) {
+  AVAR a = std::numeric_limits<double>::quiet_NaN();
+  AVAR f = stan::agrad::inv_logit(a);
+
+  AVEC x = createAVEC(a);
+  VEC g;
+  f.grad(x,g);
+  
+  EXPECT_TRUE(boost::math::isnan(f.val()));
+  ASSERT_EQ(1U,g.size());
+  EXPECT_TRUE(boost::math::isnan(g[0]));
 }
