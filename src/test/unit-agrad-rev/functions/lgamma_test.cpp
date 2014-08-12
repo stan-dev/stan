8741ff8fce472aa0b58788fe4f00f116e1c8bad8
Peter Li <Li.Peter94@gmail.com>
2014-08-12 14:50:48 -0400

    added NaN test for lgamma

--- src/test/unit-agrad-rev/functions/lgamma_test.cpp
+++ src/test/unit-agrad-rev/functions/lgamma_test.cpp
@@ -13,3 +13,16 @@ TEST(AgradRev,lgamma) {
   f.grad(x,grad_f);
   EXPECT_FLOAT_EQ(boost::math::digamma(3.0),grad_f[0]);
 }
+
+TEST(AgradRev,lgamma_nan) {
+  AVAR a = std::numeric_limits<double>::quiet_NaN();
+  AVAR f = stan::agrad::lgamma(a);
+
+  AVEC x = createAVEC(a);
+  VEC g;
+  f.grad(x,g);
+  
+  EXPECT_TRUE(boost::math::isnan(f.val()));
+  ASSERT_EQ(1U,g.size());
+  EXPECT_TRUE(boost::math::isnan(g[0]));
+}
