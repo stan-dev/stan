c7cd5af6f8ed35d1a990f385b9795d7200ef92f7
Peter Li <Li.Peter94@gmail.com>
2014-08-12 15:03:49 -0400

    added NaN test for square

--- src/test/unit-agrad-rev/functions/square_test.cpp
+++ src/test/unit-agrad-rev/functions/square_test.cpp
@@ -1,5 +1,6 @@
 #include <stan/agrad/rev/functions/square.hpp>
 #include <test/unit/agrad/util.hpp>
+#include <boost/math/special_functions/fpclassify.hpp>
 #include <gtest/gtest.h>
 
 TEST(AgradRev,square) {
@@ -12,4 +13,17 @@ TEST(AgradRev,square) {
   f.grad(x,grad_f);
   EXPECT_EQ(1U,grad_f.size());
   EXPECT_FLOAT_EQ(14.0, grad_f[0]);
+}
+
+TEST(AgradRev,square_nan) {
+  AVAR a = std::numeric_limits<double>::quiet_NaN();
+  AVAR f = stan::agrad::square(a);
+
+  AVEC x = createAVEC(a);
+  VEC g;
+  f.grad(x,g);
+  
+  EXPECT_TRUE(boost::math::isnan(f.val()));
+  ASSERT_EQ(1U,g.size());
+  EXPECT_TRUE(boost::math::isnan(g[0]));
 }
