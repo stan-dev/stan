4433366847691cdd35c0900567209a50f885651f
Peter Li <Li.Peter94@gmail.com>
2014-08-12 14:30:16 -0400

    added NaN test for exp

--- src/test/unit-agrad-rev/functions/exp_test.cpp
+++ src/test/unit-agrad-rev/functions/exp_test.cpp
@@ -1,5 +1,6 @@
 #include <stan/agrad/rev/functions/exp.hpp>
 #include <test/unit/agrad/util.hpp>
+#include <boost/math/special_functions/fpclassify.hpp>
 #include <gtest/gtest.h>
 
 TEST(AgradRev,exp_a) {
@@ -10,4 +11,17 @@ TEST(AgradRev,exp_a) {
   VEC g;
   f.grad(x,g);
   EXPECT_FLOAT_EQ(exp(6.0),g[0]);
+}
+
+TEST(AgradRev,exp_nan) {
+  AVAR a = std::numeric_limits<double>::quiet_NaN();
+  AVAR f = stan::agrad::exp(a);
+
+  AVEC x = createAVEC(a);
+  VEC g;
+  f.grad(x,g);
+  
+  EXPECT_TRUE(boost::math::isnan(f.val()));
+  ASSERT_EQ(1U,g.size());
+  EXPECT_TRUE(boost::math::isnan(g[0]));
 }
