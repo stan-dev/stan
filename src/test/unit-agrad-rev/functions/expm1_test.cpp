5316fdadb825737b7586cbddbe83b2b68f2ba8a3
Peter Li <Li.Peter94@gmail.com>
2014-08-12 14:30:54 -0400

    added NaN test for expm1

--- src/test/unit-agrad-rev/functions/expm1_test.cpp
+++ src/test/unit-agrad-rev/functions/expm1_test.cpp
@@ -13,3 +13,16 @@ TEST(AgradRev,expm1) {
   f.grad(x,grad_f);
   EXPECT_FLOAT_EQ(std::exp(1.3), grad_f[0]);
 }  
+
+TEST(AgradRev,expm1_nan) {
+  AVAR a = std::numeric_limits<double>::quiet_NaN();
+  AVAR f = stan::agrad::expm1(a);
+
+  AVEC x = createAVEC(a);
+  VEC g;
+  f.grad(x,g);
+  
+  EXPECT_TRUE(boost::math::isnan(f.val()));
+  ASSERT_EQ(1U,g.size());
+  EXPECT_TRUE(boost::math::isnan(g[0]));
+}
