e314b9d30d1e4ea16ed47a8d6143067a1ed077b7
Daniel Lee <bearlee@alum.mit.edu>
2014-08-11 22:00:21 -0400

    adding failing ibeta test with nan input

--- src/test/unit/math/functions/ibeta_test.cpp
+++ src/test/unit/math/functions/ibeta_test.cpp
@@ -1,4 +1,4 @@
-#include "stan/math/functions/ibeta.hpp"
+#include <stan/math/functions/ibeta.hpp>
 #include <gtest/gtest.h>
 
 TEST(MathFunctions, ibeta) {
@@ -15,4 +15,29 @@ TEST(MathFunctions, ibeta) {
   EXPECT_FLOAT_EQ(0.9645342, ibeta(0.1, 1.5, 0.5))  << "reasonable values for a, b, x";
   EXPECT_FLOAT_EQ(0.9897264, ibeta(0.1, 1.5, 0.75))  << "reasonable values for a, b, x";
   EXPECT_FLOAT_EQ(1.0, ibeta(0.1, 1.5, 1.0))  << "reasonable values for a, b, x";
+}
+
+TEST(MathFunctions, ibeta_nan) {
+  double nan = std::numeric_limits<double>::quiet_NaN();
+  
+  EXPECT_PRED1(boost::math::isnan<double>,
+               stan::math::ibeta(0.5, 0.5, nan));
+
+  EXPECT_PRED1(boost::math::isnan<double>,
+               stan::math::ibeta(0.5, nan, 0.0));
+
+  EXPECT_PRED1(boost::math::isnan<double>,
+               stan::math::ibeta(0.5, nan, nan));
+
+  EXPECT_PRED1(boost::math::isnan<double>,
+               stan::math::ibeta(nan, 0.5, 0.0));
+
+  EXPECT_PRED1(boost::math::isnan<double>,
+               stan::math::ibeta(nan, 0.5, nan));
+
+  EXPECT_PRED1(boost::math::isnan<double>,
+               stan::math::ibeta(nan, nan, 0.0));
+
+  EXPECT_PRED1(boost::math::isnan<double>,
+               stan::math::ibeta(nan, nan, nan));
 }
