c394ba87b679df2865b1b1079515cf30c4830c78
Peter Li <Li.Peter94@gmail.com>
2014-08-12 15:02:04 -0400

    added NaN test for sinh

--- src/test/unit-agrad-rev/functions/sinh_test.cpp
+++ src/test/unit-agrad-rev/functions/sinh_test.cpp
@@ -1,5 +1,6 @@
 #include <stan/agrad/rev/functions/sinh.hpp>
 #include <test/unit/agrad/util.hpp>
+#include <boost/math/special_functions/fpclassify.hpp>
 #include <gtest/gtest.h>
 
 TEST(AgradRev,sinh_var) {
@@ -46,4 +47,17 @@ TEST(AgradRev,sinh_neg_inf) {
   VEC g;
   f.grad(x,g);
   EXPECT_FLOAT_EQ(inf,g[0]);
+}
+
+TEST(AgradRev,sinh_nan) {
+  AVAR a = std::numeric_limits<double>::quiet_NaN();
+  AVAR f = stan::agrad::sinh(a);
+
+  AVEC x = createAVEC(a);
+  VEC g;
+  f.grad(x,g);
+  
+  EXPECT_TRUE(boost::math::isnan(f.val()));
+  ASSERT_EQ(1U,g.size());
+  EXPECT_TRUE(boost::math::isnan(g[0]));
 }
