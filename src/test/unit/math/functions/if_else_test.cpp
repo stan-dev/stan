75724f617c0f83ac304755ae6f233981c028947b
Daniel Lee <bearlee@alum.mit.edu>
2014-08-11 22:15:05 -0400

    added NaN test for if_else

--- src/test/unit/math/functions/if_else_test.cpp
+++ src/test/unit/math/functions/if_else_test.cpp
@@ -1,4 +1,5 @@
-#include "stan/math/functions/if_else.hpp"
+#include <stan/math/functions/if_else.hpp>
+#include <boost/math/special_functions/fpclassify.hpp>
 #include <gtest/gtest.h>
 
 TEST(MathFunctions, if_else) {
@@ -30,4 +31,22 @@ TEST(MathFunctions, if_else_promote) {
   int y = -1;
   EXPECT_FLOAT_EQ(2.5, if_else(true,x,y));
   EXPECT_FLOAT_EQ(-1.0, if_else(false,x,y));
+}
+
+TEST(MathFunctions, if_else_nan) {
+  double nan = std::numeric_limits<double>::quiet_NaN();
+  
+  EXPECT_FLOAT_EQ(1.2, stan::math::if_else(true, 1.2, nan));
+  EXPECT_PRED1(boost::math::isnan<double>,
+               stan::math::if_else(false, 1.2, nan));
+
+
+  EXPECT_PRED1(boost::math::isnan<double>,
+               stan::math::if_else(true, nan, 2.4));
+  EXPECT_FLOAT_EQ(2.4, stan::math::if_else(false, nan, 2.4));
+
+  EXPECT_PRED1(boost::math::isnan<double>,
+               stan::math::if_else(true, nan, nan));
+  EXPECT_PRED1(boost::math::isnan<double>,
+               stan::math::if_else(false, nan, nan));
 }
