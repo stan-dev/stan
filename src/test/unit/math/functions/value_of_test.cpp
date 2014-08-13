ef401dffb1d7ff9b8c0db37a8cab0e709e9eef37
Daniel Lee <bearlee@alum.mit.edu>
2014-08-12 22:56:54 -0400

    added NaN test for value_of

--- src/test/unit/math/functions/value_of_test.cpp
+++ src/test/unit/math/functions/value_of_test.cpp
@@ -1,4 +1,5 @@
-#include "stan/math/functions/value_of.hpp"
+#include <stan/math/functions/value_of.hpp>
+#include <boost/math/special_functions/fpclassify.hpp>
 #include <gtest/gtest.h>
 
 TEST(MathFunctions, value_of) {
@@ -6,4 +7,12 @@ TEST(MathFunctions, value_of) {
   double x = 5.0;
   EXPECT_FLOAT_EQ(5.0,value_of(x));
   EXPECT_FLOAT_EQ(5.0,value_of(5));
+}
+
+TEST(MathFunctions, value_of_nan) {
+  double nan = std::numeric_limits<double>::quiet_NaN();
+  
+  EXPECT_PRED1(boost::math::isnan<double>,
+               stan::math::value_of(nan));
+
 }
