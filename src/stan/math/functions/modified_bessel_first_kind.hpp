a6fb9f01a67292463a4536393d31bb4ad0e368e7
Peter Li <Li.Peter94@gmail.com>
2014-08-14 14:47:31 -0400

    added check_not_nan to modified_bessel_first_kind

--- src/stan/math/functions/modified_bessel_first_kind.hpp
+++ src/stan/math/functions/modified_bessel_first_kind.hpp
@@ -2,6 +2,7 @@
 #define STAN__MATH__FUNCTIONS__MODIFIED_BESSEL_FIRST_KIND_HPP
 
 #include <boost/math/special_functions/bessel.hpp>
+#include <stan/math/error_handling/check_not_nan.hpp>
 
 namespace stan {
   namespace math {
@@ -9,6 +10,8 @@ namespace stan {
     template<typename T2>
     inline T2 
     modified_bessel_first_kind(const int v, const T2 z) { 
+      check_not_nan("modified_bessel_first_kind(%1%)", z, "z", static_cast<double*>(0));
+
       return boost::math::cyl_bessel_i(v,z); 
     }
 
