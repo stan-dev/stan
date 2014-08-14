5387d210d18c75eee9e71a9c197edbedfb1cb050
Peter Li <Li.Peter94@gmail.com>
2014-08-14 13:16:06 -0400

    added NaN test for sign

--- src/test/unit-agrad-rev/functions/sign_test.cpp
+++ src/test/unit-agrad-rev/functions/sign_test.cpp
@@ -1,6 +1,6 @@
 #include <stan/math/functions/sign.hpp>
 #include <stan/agrad/rev.hpp>
-
+#include <boost/math/special_functions/fpclassify.hpp>
 #include <gtest/gtest.h>
 
 TEST(MathFunctions, sign) {
@@ -12,4 +12,10 @@ TEST(MathFunctions, sign) {
   EXPECT_EQ(1, stan::math::sign(x));
   x = -0.001;
   EXPECT_EQ(-1, stan::math::sign(x));
+}
+
+TEST(MathFunctions, sign_nan) {
+  stan::agrad::var nan = std::numeric_limits<double>::quiet_NaN();
+
+  EXPECT_FLOAT_EQ(1, stan::math::sign(nan));
 }
