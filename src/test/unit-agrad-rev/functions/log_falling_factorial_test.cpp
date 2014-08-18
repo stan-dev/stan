7eeb63a722d1c0215b600833867aa639dd04844c
Peter Li <Li.Peter94@gmail.com>
2014-08-18 09:35:41 -0400

    added NaN test for log_falling_factorial

--- src/test/unit-agrad-rev/functions/log_falling_factorial_test.cpp
+++ src/test/unit-agrad-rev/functions/log_falling_factorial_test.cpp
@@ -2,6 +2,9 @@
 #include <test/unit/agrad/util.hpp>
 #include <gtest/gtest.h>
 #include <boost/math/special_functions/digamma.hpp>
+#include <test/unit-agrad-rev/nan_util.hpp>
+#include <stan/meta/traits.hpp>
+
 
 TEST(AgradRev,log_falling_factorial_var_double) {
   double a(1);
@@ -48,43 +51,17 @@ TEST(AgradRev, log_falling_factorial_var_var) {
   EXPECT_FLOAT_EQ(-boost::math::digamma(5), g[1]);
 }
 
-TEST(AgradRev,log_falling_factorial_nan_vv) {
-  AVAR a = std::numeric_limits<double>::quiet_NaN();
-  AVAR b = std::numeric_limits<double>::quiet_NaN();
-  AVAR f = stan::agrad::log_falling_factorial(a,b);
+struct log_falling_factorial_fun {
+  template <typename T0, typename T1>
+  inline 
+  typename stan::return_type<T0,T1>::type
+  operator()(const T0& arg1,
+             const T1& arg2) const {
+    return log_falling_factorial(arg1,arg2);
+  }
+};
 
-  AVEC x = createAVEC(a,b);
-  VEC g;
-  f.grad(x,g);
-  
-  EXPECT_TRUE(boost::math::isnan(f.val()));
-  ASSERT_EQ(2U,g.size());
-  EXPECT_TRUE(boost::math::isnan(g[0]));
-  EXPECT_TRUE(boost::math::isnan(g[1]));
-}
-
-TEST(AgradRev,log_falling_factorial_nan_vd) {
-  AVAR a = std::numeric_limits<double>::quiet_NaN();
-  AVAR f = stan::agrad::log_falling_factorial(a,1);
-
-  AVEC x = createAVEC(a);
-  VEC g;
-  f.grad(x,g);
-  
-  EXPECT_TRUE(boost::math::isnan(f.val()));
-  ASSERT_EQ(1U,g.size());
-  EXPECT_TRUE(boost::math::isnan(g[0]));
-}
-
-TEST(AgradRev,log_falling_factorial_nan_dv) {
-  AVAR a = std::numeric_limits<double>::quiet_NaN();
-  AVAR f = stan::agrad::log_falling_factorial(1,a);
-
-  AVEC x = createAVEC(a);
-  VEC g;
-  f.grad(x,g);
-  
-  EXPECT_TRUE(boost::math::isnan(f.val()));
-  ASSERT_EQ(1U,g.size());
-  EXPECT_TRUE(boost::math::isnan(g[0]));
+TEST(AgradRev, log_falling_factorial_nan) {
+  log_falling_factorial_fun log_falling_factorial_;
+  test_nan(log_falling_factorial_,4.0,4.0,false,false);
 }
