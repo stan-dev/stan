e6db304717453df7f5379f9990020b2cdc5c8189
Daniel Lee <bearlee@alum.mit.edu>
2014-08-17 21:46:38 -0400

    updating arithmetic in owens_t

--- src/stan/agrad/fwd/functions/owens_t.hpp
+++ src/stan/agrad/fwd/functions/owens_t.hpp
@@ -16,16 +16,19 @@ namespace stan {
     owens_t(const fvar<T>& x1, const fvar<T>& x2) {
       using stan::math::owens_t;
       using stan::math::pi;
+      using stan::math::INV_SQRT_2;
+      using stan::math::INV_SQRT_TWO_PI;
+      using stan::math::square;
       using std::exp;
       using ::erf;
 
-      T neg_x1_sq_div_2 = -x1.val_ * x1.val_ / 2.0;
-      T one_p_x2_sq = 1.0 + x2.val_ * x2.val_;
+      T neg_x1_sq_div_2 = -square(x1.val_) * 0.5;
+      T one_p_x2_sq = 1.0 + square(x2.val_);
 
       return fvar<T>(owens_t(x1.val_, x2.val_), 
                      - x1.d_ 
-                     * (erf(x2.val_ * x1.val_ / std::sqrt(2.0)) 
-                        * exp(neg_x1_sq_div_2) / std::sqrt(8.0 * pi())) 
+                     * (erf(x2.val_ * x1.val_ * INV_SQRT_2) 
+                        * exp(neg_x1_sq_div_2) * INV_SQRT_TWO_PI * 0.5)
                      + x2.d_ * exp(neg_x1_sq_div_2 * one_p_x2_sq) 
                      / (one_p_x2_sq * 2.0 * pi()));
     }
@@ -36,10 +39,11 @@ namespace stan {
     owens_t(const double x1, const fvar<T>& x2) {
       using stan::math::owens_t;
       using stan::math::pi;
+      using stan::math::square;
       using std::exp;
 
-      T neg_x1_sq_div_2 = -x1 * x1 / 2.0;
-      T one_p_x2_sq = 1.0 + x2.val_ * x2.val_;
+      T neg_x1_sq_div_2 = -square(x1) * 0.5;
+      T one_p_x2_sq = 1.0 + square(x2.val_);
 
       return fvar<T>(owens_t(x1, x2.val_), 
                      x2.d_ * exp(neg_x1_sq_div_2 * one_p_x2_sq) 
@@ -52,15 +56,18 @@ namespace stan {
     owens_t(const fvar<T>& x1, const double x2) {
       using stan::math::owens_t;
       using stan::math::pi;
+      using stan::math::square;
+      using stan::math::INV_SQRT_2;
+      using stan::math::INV_SQRT_TWO_PI;
       using std::exp;
       using ::erf;
 
-      T neg_x1_sq_div_2 = -x1.val_ * x1.val_ / 2.0;
+      T neg_x1_sq_div_2 = -square(x1.val_) * 0.5;
 
       return fvar<T>(owens_t(x1.val_, x2), 
-                     -x1.d_  * (erf(x2 * x1.val_ / std::sqrt(2.0)) 
+                     -x1.d_  * (erf(x2 * x1.val_ * INV_SQRT_2) 
                                 * exp(neg_x1_sq_div_2) 
-                                / std::sqrt(8.0 * pi())));
+                                * INV_SQRT_TWO_PI * 0.5));
     }
 
   }
