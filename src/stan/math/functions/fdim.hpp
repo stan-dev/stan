6b9cee8b0ed6962653f02685f7cc7e9906932fe9
Daniel Lee <bearlee@alum.mit.edu>
2014-08-17 23:03:15 -0400

    adding a check for isnan because Windows operates differently than Mac

--- src/stan/math/functions/fdim.hpp
+++ src/stan/math/functions/fdim.hpp
@@ -2,6 +2,7 @@
 #define STAN__MATH__FUNCTIONS__FDIM_HPP
 
 #include <math.h> 
+#include <boost/math/special_functions/fpclassify.hpp>
 #include <boost/math/tools/promotion.hpp>
 
 namespace stan {
@@ -20,6 +21,9 @@ namespace stan {
     template <typename T1, typename T2>
     inline typename boost::math::tools::promote_args<T1, T2>::type
     fdim(T1 a, T2 b) {
+      if (boost::math::isnan(a) || boost::math::isnan(b))
+        return std::numeric_limits<typename boost::math::tools::promote_args<T1, T2>::type>
+          ::quiet_NaN();
       return ::fdim(a, b);
     } 
   }
