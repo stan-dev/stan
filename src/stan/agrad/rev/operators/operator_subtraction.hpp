3f288adb649b51d90f6d6cac2cad3b9e846a48ae
Peter Li <Li.Peter94@gmail.com>
2014-08-25 11:09:11 -0400

    fixed operator_subtraction so gradient returns NaN for NaN input

--- src/stan/agrad/rev/operators/operator_subtraction.hpp
+++ src/stan/agrad/rev/operators/operator_subtraction.hpp
@@ -5,6 +5,7 @@
 #include <stan/agrad/rev/internal/vv_vari.hpp>
 #include <stan/agrad/rev/internal/vd_vari.hpp>
 #include <stan/agrad/rev/internal/dv_vari.hpp>
+#include <boost/math/special_functions/fpclassify.hpp>
 
 namespace stan {
   namespace agrad {
@@ -16,8 +17,14 @@ namespace stan {
           op_vv_vari(avi->val_ - bvi->val_, avi, bvi) {
         }
         void chain() {
-          avi_->adj_ += adj_;
-          bvi_->adj_ -= adj_;
+          if (unlikely(boost::math::isnan(avi_->val_)
+                       || boost::math::isnan(bvi_->val_))) {
+            avi_->adj_ = std::numeric_limits<double>::quiet_NaN();
+            bvi_->adj_ = std::numeric_limits<double>::quiet_NaN();
+          } else {
+            avi_->adj_ += adj_;
+            bvi_->adj_ -= adj_;
+          }
         }
       };
     
@@ -27,7 +34,11 @@ namespace stan {
           op_vd_vari(avi->val_ - b, avi, b) {
         }
         void chain() {
-          avi_->adj_ += adj_;
+          if (unlikely(boost::math::isnan(avi_->val_)
+                       || boost::math::isnan(bd_)))
+            avi_->adj_ = std::numeric_limits<double>::quiet_NaN();
+          else
+            avi_->adj_ += adj_;
         }
       };
 
@@ -37,7 +48,11 @@ namespace stan {
           op_dv_vari(a - bvi->val_, a, bvi) {
         }
         void chain() {
-          bvi_->adj_ -= adj_;
+          if (unlikely(boost::math::isnan(ad_)
+                       || boost::math::isnan(bvi_->val_)))
+            bvi_->adj_ = std::numeric_limits<double>::quiet_NaN();
+          else
+            bvi_->adj_ -= adj_;
         }
       };
     }
