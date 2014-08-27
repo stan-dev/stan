efd1122c1cc1b7039da5f662da1f66d02a4dc0de
Peter Li <Li.Peter94@gmail.com>
2014-08-27 17:52:32 -0400

    added std::vector version of check_ordered with tests

--- src/stan/math/error_handling/matrix/check_ordered.hpp
+++ src/stan/math/error_handling/matrix/check_ordered.hpp
@@ -20,8 +20,8 @@ namespace stan {
      * @param name
      * @param result
      * @return <code>true</code> if the vector has positive, ordered
-     * @return throws if any element in y is nan
      * values.
+     * @return throws if any element in y is nan
      */
     template <typename T_y, typename T_result>
     bool check_ordered(const char* function,
@@ -47,10 +47,41 @@ namespace stan {
         }
       }
       return true;
+    }    
+    template <typename T_y, typename T_result>
+    bool check_ordered(const char* function,
+                       const std::vector<T_y>& y,
+                       const char* name,
+                       T_result* result) {
+      if (y.size() == 0) {
+        return true;
+      }
+      for (int n = 1; n < y.size(); n++) {
+        if (!(y[n] > y[n-1])) {
+          std::ostringstream stream;
+          stream << " is not a valid ordered vector."
+                 << " The element at " << stan::error_index::value + n 
+                 << " is %1%, but should be greater than the previous element, "
+                 << y[n-1];
+          std::string msg(stream.str());
+          return dom_err(function,y[n],name,
+                         msg.c_str(),"",
+                         result);
+          return false;
+        }
+      }
+      return true;
     }                         
     template <typename T>
     bool check_ordered(const char* function,
                        const Eigen::Matrix<T,Eigen::Dynamic,1>& y,
+                       const char* name,
+                       T* result = 0) {
+      return check_ordered<T,T>(function,y,name,result);
+    }                    
+    template <typename T>
+    bool check_ordered(const char* function,
+                       const std::vector<T>& y,
                        const char* name,
                        T* result = 0) {
       return check_ordered<T,T>(function,y,name,result);
