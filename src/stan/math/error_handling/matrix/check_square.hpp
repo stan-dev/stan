c73eb1eccb66e3eb786f763bd4e7c7c17f03f6be
Peter Li <Li.Peter94@gmail.com>
2014-08-27 11:21:17 -0400

    Revert "added doc in code for check_square"
    This reverts commit 26ce674ac72aa39be2208062c4bc83c2d823ceef.

--- src/stan/math/error_handling/matrix/check_square.hpp
+++ src/stan/math/error_handling/matrix/check_square.hpp
@@ -11,8 +11,6 @@ namespace stan {
     /**
      * Return <code>true</code> if the specified matrix is square.
      *
-     * NOTE: this will not throw if any elements in y are nan.
-     *
      * @param function
      * @param y Matrix to test.
      * @param name
