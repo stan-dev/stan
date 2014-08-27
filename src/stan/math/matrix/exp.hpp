35276f1396396301e4581c91eefabb0ef9a721a6
Marco Inacio <marcoigarapava@gmail.com>
2014-08-27 18:01:17 -0300

    stan::math::exp(Eigen::Matrix) will have an specialized version for double that will check for nan inputs

--- src/stan/math/matrix/exp.hpp
+++ src/stan/math/matrix/exp.hpp
@@ -2,6 +2,7 @@
 #define STAN__MATH__MATRIX__EXP_HPP
 
 #include <stan/math/matrix/Eigen.hpp>
+#include <boost/math/special_functions/fpclassify.hpp>
 
 namespace stan {
   namespace math {
@@ -13,10 +14,20 @@ namespace stan {
      * @return ret(i,j) = exp(m(i,j))
      */
     template<typename T, int Rows, int Cols>
-    inline Eigen::Matrix<T,Rows,Cols> exp(Eigen::Matrix<T,Rows,Cols> mat) {
-      for (T * it = mat.data(), * end_ = it + mat.size(); it != end_; it++)
-        *it = exp(*it);
-      return mat;
+    inline Eigen::Matrix<T,Rows,Cols> exp(const Eigen::Matrix<T,Rows,Cols> & m) {
+      return m.array().exp().matrix();
+    }
+    
+    template<int Rows, int Cols>
+    inline Eigen::Matrix<double,Rows,Cols> exp(const Eigen::Matrix<double,Rows,Cols> & m) {
+      for (const double * it = m.data(), * end_ = it + m.size(); it != end_; it++)
+        if (boost::math::isnan(*it)) {
+          Eigen::Matrix<double,Rows,Cols> mat = m;
+          for (double * it = mat.data(), * end_ = it + mat.size(); it != end_; it++)
+            *it = std::exp(*it);
+          return mat;
+        }
+      return m.array().exp().matrix();
     }
     
   }
