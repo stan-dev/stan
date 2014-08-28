13f083ccc59c933a4b60d6b50f0759ab2f1ac885
Peter Li <Li.Peter94@gmail.com>
2014-08-28 16:39:57 -0400

    updated NaN test for agrad/rev/asin

--- src/test/unit-agrad-rev/functions/asin_test.cpp
+++ src/test/unit-agrad-rev/functions/asin_test.cpp
@@ -3,6 +3,7 @@
 #include <gtest/gtest.h>
 #include <stan/math/constants.hpp>
 #include <stan/agrad/rev/numeric_limits.hpp>
+#include <test/unit-agrad-rev/nan_util.hpp>
 
 TEST(AgradRev,asin_var) {
   AVAR a = 0.68;
@@ -45,15 +46,15 @@ TEST(AgradRev,asin_out_of_bounds) {
   EXPECT_TRUE(std::isnan(asin(a)));
 }
 
-TEST(AgradRev,asin_nan) {
-  AVAR a = std::numeric_limits<double>::quiet_NaN();
-  AVAR f = stan::agrad::asin(a);
+struct asin_fun {
+  template <typename T0>
+  inline T0
+  operator()(const T0& arg1) const {
+    return asin(arg1);
+  }
+};
 
-  AVEC x = createAVEC(a);
-  VEC g;
-  f.grad(x,g);
-  
-  EXPECT_TRUE(boost::math::isnan(f.val()));
-  ASSERT_EQ(1U,g.size());
-  EXPECT_TRUE(boost::math::isnan(g[0]));
+TEST(AgradRev,asin_NaN) {
+  asin_fun asin_;
+  test_nan(asin_,false,true);
 }
