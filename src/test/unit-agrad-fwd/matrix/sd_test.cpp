43e0d4611fe2dd73e5d7aa09114c3b966497851d
Peter Li <Li.Peter94@gmail.com>
2014-08-29 11:02:42 -0400

    updated fwd/matrix tests

--- src/test/unit-agrad-fwd/matrix/sd_test.cpp
+++ src/test/unit-agrad-fwd/matrix/sd_test.cpp
@@ -2,11 +2,7 @@
 #include <gtest/gtest.h>
 #include <stan/math/matrix/typedefs.hpp>
 #include <stan/agrad/fwd/matrix/typedefs.hpp>
-#include <stan/agrad/fwd/functions/sqrt.hpp>
-#include <stan/agrad/fwd/operators/operator_multiplication.hpp>
-#include <stan/agrad/fwd/operators/operator_addition.hpp>
-#include <stan/agrad/fwd/operators/operator_division.hpp>
-#include <stan/agrad/fwd/operators/operator_subtraction.hpp>
+#include <stan/agrad/fwd.hpp>
 #include <stan/agrad/rev.hpp>
 #include <test/unit/agrad/util.hpp>
 
