c23f3d0778267401d5f0c44b6536655cd8f3752a
Daniel Lee <bearlee@alum.mit.edu>
2014-09-09 15:08:07 -0400

    updating definitions of macros likely and unlikely

--- src/stan/meta/likely.hpp
+++ src/stan/meta/likely.hpp
@@ -2,11 +2,11 @@
 #define STAN__META__LIKELY_HPP
 
 #ifdef __GNUC__
-#define likely(x) (__builtin_expect((x),1))
-#define unlikely(x) (__builtin_expect((x),0))
+#define likely(x)      __builtin_expect(!!(x), 1)
+#define unlikely(x)    __builtin_expect(!!(x), 0)
 #else
-#define likely(x) (x)
-#define unlikely(x) (x)
+#define likely(x)     (x)
+#define unlikely(x)   (x)
 #endif
 
 #endif
