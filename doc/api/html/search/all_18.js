95c05549bea591900b0fb9dbe3a8f5b63eaaf046
Daniel Lee <bearlee@alum.mit.edu>
2014-09-11 00:58:44 -0400

    doxygen: removing checked in artifacts

--- doc/api/html/search/all_18.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['x',['x',['../classstan_1_1common_1_1recorder_1_1filtered__values.html#aa4ac315172fb8f9d7908cdca0e140322',1,'stan::common::recorder::filtered_values::x()'],['../classstan_1_1common_1_1recorder_1_1values.html#ac678f79c3420e4524e0e31a2fe698f1d',1,'stan::common::recorder::values::x()']]],
-  ['x_5f',['x_',['../structstan_1_1math_1_1array__builder.html#aeeffff75de3c356257e419fc8591afcb',1,'stan::math::array_builder']]],
-  ['xs_5f',['xs_',['../sort__indices_8hpp.html#ab9a7299348cbbbadbd23194af720d6e9',1,'sort_indices.hpp']]]
-];
