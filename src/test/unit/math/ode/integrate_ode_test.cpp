499ead54cb22182df18b9afe6e0bc505208a6f4b
Daniel Lee <bearlee@alum.mit.edu>
2014-10-04 22:18:57 -0400

    ode: refactored test

--- src/test/unit/math/ode/integrate_ode_test.cpp
+++ src/test/unit/math/ode/integrate_ode_test.cpp
@@ -30,28 +30,6 @@ void sho_death_test(F harm_osc,
 }
 
 
-template <typename E, typename F>
-void sho_exception_test(F harm_osc,
-                        std::vector<double>& y0,
-                        double t0,
-                        std::vector<double>& ts,
-                        std::vector<double>& theta,
-                        std::vector<double>& x,
-                        std::vector<int>& x_int,
-                        std::string expected_message) {
-  try {
-    stan::math::integrate_ode(harm_osc, y0, t0,
-                              ts, theta, x, x_int,0);
-  } catch (const E& e) {
-    EXPECT_EQ(1, count_matches(expected_message, e.what()))
-      << "expected message: " << expected_message << std::endl
-      << "found message:    " << e.what();
-    return;
-  }
-  FAIL() << "didn't throw an exception of the correct type";
-}
-
-
 template <typename F>
 void sho_value_test(F harm_osc,
                     std::vector<double>& y0,
@@ -125,6 +103,7 @@ TEST(StanMathOde_integrate_ode, harmonic_oscillator) {
 }
 
 TEST(StanMathOde_integrate_ode, error_conditions) {
+  using stan::math::integrate_ode;
   harm_osc_ode_data_fun harm_osc;
 
   std::vector<double> theta;
@@ -143,31 +122,40 @@ TEST(StanMathOde_integrate_ode, error_conditions) {
   std::vector<double> x(3,1);
   std::vector<int> x_int(2,0);
 
-
   std::vector<double> y0_bad;
-  sho_exception_test<std::domain_error>(harm_osc, y0_bad, t0, ts, theta, x, x_int, "has size 0");
-  sho_exception_test<std::domain_error>(harm_osc, y0_bad, t0, ts, theta, x, x_int, "initial state");
-  
+  EXPECT_THROW_MSG(integrate_ode(harm_osc, y0_bad, t0, ts, theta, x, x_int, 0),
+                   std::domain_error,
+                   "initial state has size 0");
   
   double t0_bad = 2.0;
-  sho_exception_test<std::domain_error>(harm_osc, y0, t0_bad, ts, theta, x, x_int, "initial time is 2, but must be less than 0.1");
+  EXPECT_THROW_MSG(integrate_ode(harm_osc, y0, t0_bad, ts, theta, x, x_int, 0),
+                   std::domain_error,
+                   "initial time is 2, but must be less than 0.1");
 
   std::vector<double> ts_bad;
-  sho_exception_test<std::domain_error>(harm_osc, y0, t0, ts_bad, theta, x, x_int, "has size 0");
-  sho_exception_test<std::domain_error>(harm_osc, y0, t0, ts_bad, theta, x, x_int, "times");
+  EXPECT_THROW_MSG(integrate_ode(harm_osc, y0, t0, ts_bad, theta, x, x_int, 0),
+                   std::domain_error,
+                   "times has size 0");
 
   ts_bad.push_back(3);
   ts_bad.push_back(1);
-  sho_exception_test<std::domain_error>(harm_osc, y0, t0, ts_bad, theta, x, x_int, "times is not a valid ordered vector");
+  EXPECT_THROW_MSG(integrate_ode(harm_osc, y0, t0, ts_bad, theta, x, x_int, 0),
+                   std::domain_error,
+                   "times is not a valid ordered vector");
 
 
   std::vector<double> theta_bad;
-  sho_exception_test<std::out_of_range>(harm_osc, y0, t0, ts, theta_bad, x, x_int, "vector");
+  EXPECT_THROW_MSG(integrate_ode(harm_osc, y0, t0, ts, theta_bad, x, x_int, 0),
+                   std::out_of_range,
+                   "vector");
   
   std::vector<double> x_bad;
-  sho_exception_test<std::out_of_range>(harm_osc, y0, t0, ts, theta, x_bad, x_int, "vector");
+  EXPECT_THROW_MSG(integrate_ode(harm_osc, y0, t0, ts, theta, x_bad, x_int, 0),
+                   std::out_of_range,
+                   "vector");
 
   std::vector<int> x_int_bad;
-  sho_exception_test<std::out_of_range>(harm_osc, y0, t0, ts, theta, x, x_int_bad, "vector");
-  
+  EXPECT_THROW_MSG(integrate_ode(harm_osc, y0, t0, ts, theta, x, x_int_bad, 0),
+                   std::out_of_range,
+                   "vector");
 }
