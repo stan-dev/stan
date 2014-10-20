4586d9f413e0e6ab834e555736426e3906318eb6
Daniel Lee <bearlee@alum.mit.edu>
2014-10-20 14:50:50 -0400

    fixes #1090. removes built doc

--- doc/api/html/search/enumvalues_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['config',['CONFIG',['../structstan_1_1gm_1_1error__codes.html#aa63873e8f7e9dd7f8af3502e4003292fa06ec7fc9f65aafc74d415edaba0a63bc',1,'stan::gm::error_codes']]]
-];
