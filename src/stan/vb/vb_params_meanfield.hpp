111286a2bc248afea9a200b511d8a45dbddcd2f2
Alp Kucukelbir <akucukelbir@gmail.com>
2014-10-31 17:04:17 -0400

    mean-field implemented, runtime nan bug...

---
+++ src/stan/vb/vb_params_meanfield.hpp
@@ -0,0 +1,79 @@
+#ifndef STAN__VB__VB_PARAMS_MEANFIELD__HPP
+#define STAN__VB__VB_PARAMS_MEANFIELD__HPP
+
+#include <vector>
+
+#include <stan/math/matrix/Eigen.hpp>
+#include <stan/math/error_handling/matrix/check_size_match.hpp>
+#include <stan/math/error_handling/check_not_nan.hpp>
+
+namespace stan {
+
+  namespace vb {
+
+    class vb_params_meanfield {
+
+    private:
+
+      Eigen::VectorXd mu_;     // Mean of location-scale family
+      Eigen::VectorXd sigma2_; // Variances of location-scale family
+      int dimension_;
+
+    public:
+
+      vb_params_meanfield(Eigen::VectorXd const& mu,
+                          Eigen::VectorXd const& sigma2) :
+      mu_(mu), sigma2_(sigma2), dimension_(mu.size()) {
+
+        static const char* function = "stan::vb::vb_params_meanfield(%1%)";
+
+        double tmp(0.0);
+        stan::math::check_size_match(function,
+                                 dimension_,     "Dimension of mean vector",
+                                 sigma2_.size(), "Dimension of variance vector",
+                                 &tmp);
+
+      };
+
+      virtual ~vb_params_meanfield() {}; // No-op
+
+      // Accessors
+      int dimension() const { return dimension_; }
+      Eigen::VectorXd const& mu()     const { return mu_; }
+      Eigen::VectorXd const& sigma2() const { return sigma2_; }
+
+      // Mutators
+      void set_mu(Eigen::VectorXd const& mu)         { mu_ = mu; }
+      void set_sigma2(Eigen::VectorXd const& sigma2) { sigma2_ = sigma2; }
+
+      // Implements f^{-1}(\check{z}) = sigma * \check{z} + \mu
+      Eigen::VectorXd to_unconstrained(Eigen::VectorXd const& z_check) const {
+        static const char* function = "stan::vb::vb_params_meanfield"
+                                      "::to_unconstrained(%1%)";
+
+        double tmp(0.0);
+        stan::math::check_size_match(function,
+                         dimension_,     "Dimension of mean vector",
+                         z_check.size(), "Dimension of input vector",
+                         &tmp);
+
+        stan::math::check_not_nan(function,                                         /////////////////////// FIXME
+            sigma2_,  "sigma2_",
+            &tmp);
+        stan::math::check_not_nan(function,
+            mu_,  "mu_",
+            &tmp);
+        stan::math::check_not_nan(function,
+            z_check,  "z_check",
+            &tmp);
+
+        return sigma2_.cwiseSqrt().cwiseProduct(z_check) + mu_;
+      };
+
+    };
+
+  } // vb
+
+} // stan
+
+#endif
