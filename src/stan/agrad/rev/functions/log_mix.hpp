637cc790027092a3e2b2776b243c37b80f912581
Rob Trangucci <robert.trangucci@gmail.com>
2014-11-10 13:34:10 -0500

    Added precomp_v versions of tests and function for log_mix

--- src/stan/agrad/rev/functions/log_mix.hpp
+++ src/stan/agrad/rev/functions/log_mix.hpp
@@ -188,6 +188,109 @@ namespace stan {
                                       lambda1.vi_, lambda2.vi_,
                                       d_lambda1, d_lambda2));
     }
+
+    var log_mix(var theta, double lambda1, double lambda2) {
+      using stan::math::log_mix;
+      using std::exp;
+      double theta_d = theta.val();
+      double lambda1_d = lambda1;
+      double lambda2_d = lambda2;
+      double result = stan::math::log_mix(theta_d, lambda1_d, lambda2_d);
+
+      double d_theta(0);
+      if (lambda1_d > lambda2_d) {
+        double lam2_m_lam1 = lambda2_d - lambda1_d;
+        double exp_lam2_m_lam1 = exp(lam2_m_lam1);
+        double one_m_exp_lam2_m_lam1 = 1 - exp_lam2_m_lam1;
+        double one_m_t = 1 - theta_d;
+        double one_m_t_prod_exp_lam2_m_lam1 = one_m_t * exp_lam2_m_lam1;
+        double t_plus_one_m_t_prod_exp_lam2_m_lam1 
+          = theta_d + one_m_t_prod_exp_lam2_m_lam1;
+        d_theta 
+          = one_m_exp_lam2_m_lam1 
+          / t_plus_one_m_t_prod_exp_lam2_m_lam1;
+      } else {
+        double lam1_m_lam2 = lambda1_d - lambda2_d;
+        double exp_lam1_m_lam2 = exp(lam1_m_lam2);
+        double exp_lam1_m_lam2_m_1 = exp_lam1_m_lam2 - 1;
+        double one_m_t = 1 - theta_d;
+        double t_prod_exp_lam1_m_lam2 = theta_d * exp_lam1_m_lam2;
+        double one_m_t_plus_t_prod_exp_lam1_m_lam2 
+          = one_m_t + t_prod_exp_lam1_m_lam2;
+        d_theta 
+          = exp_lam1_m_lam2_m_1
+          / one_m_t_plus_t_prod_exp_lam1_m_lam2;
+      }
+      return var(new precomp_v_vari(result, theta.vi_, d_theta));
+    }
+
+    var log_mix(double theta, var lambda1, double lambda2) {
+      using stan::math::log_mix;
+      using std::exp;
+      double theta_d = theta;
+      double lambda1_d = lambda1.val();
+      double lambda2_d = lambda2;
+      double result = stan::math::log_mix(theta_d, lambda1_d, lambda2_d);
+
+      double d_lambda1(0);
+      if (lambda1_d > lambda2_d) {
+        double lam2_m_lam1 = lambda2_d - lambda1_d;
+        double exp_lam2_m_lam1 = exp(lam2_m_lam1);
+        double one_m_t = 1 - theta_d;
+        double one_m_t_prod_exp_lam2_m_lam1 = one_m_t * exp_lam2_m_lam1;
+        double t_plus_one_m_t_prod_exp_lam2_m_lam1 
+          = theta_d + one_m_t_prod_exp_lam2_m_lam1;
+        d_lambda1
+          = theta_d
+          / t_plus_one_m_t_prod_exp_lam2_m_lam1;
+      } else {
+        double lam1_m_lam2 = lambda1_d - lambda2_d;
+        double exp_lam1_m_lam2 = exp(lam1_m_lam2);
+        double one_m_t = 1 - theta_d;
+        double t_prod_exp_lam1_m_lam2 = theta_d * exp_lam1_m_lam2;
+        double one_m_t_plus_t_prod_exp_lam1_m_lam2 
+          = one_m_t + t_prod_exp_lam1_m_lam2;
+        d_lambda1
+          = t_prod_exp_lam1_m_lam2
+          / one_m_t_plus_t_prod_exp_lam1_m_lam2;
+      }
+      return var(new precomp_v_vari(result, lambda1.vi_, d_lambda1));
+    }
+
+    var log_mix(double theta, double lambda1, var lambda2) {
+      using stan::math::log_mix;
+      using std::exp;
+      double theta_d = theta;
+      double lambda1_d = lambda1;
+      double lambda2_d = lambda2.val();
+      double result = stan::math::log_mix(theta_d, lambda1_d, lambda2_d);
+
+      double d_lambda2(0);
+      if (lambda1_d > lambda2_d) {
+        double lam2_m_lam1 = lambda2_d - lambda1_d;
+        double exp_lam2_m_lam1 = exp(lam2_m_lam1);
+        double one_m_t = 1 - theta_d;
+        double one_m_t_prod_exp_lam2_m_lam1 = one_m_t * exp_lam2_m_lam1;
+        double t_plus_one_m_t_prod_exp_lam2_m_lam1 
+          = theta_d + one_m_t_prod_exp_lam2_m_lam1;
+        d_lambda2
+          = one_m_t_prod_exp_lam2_m_lam1
+          / t_plus_one_m_t_prod_exp_lam2_m_lam1;
+      } else {
+        double lam1_m_lam2 = lambda1_d - lambda2_d;
+        double exp_lam1_m_lam2 = exp(lam1_m_lam2);
+        double one_m_t = 1 - theta_d;
+        double t_prod_exp_lam1_m_lam2 = theta_d * exp_lam1_m_lam2;
+        double one_m_t_plus_t_prod_exp_lam1_m_lam2 
+          = one_m_t + t_prod_exp_lam1_m_lam2;
+        d_lambda2
+          = one_m_t
+          / one_m_t_plus_t_prod_exp_lam1_m_lam2;
+      }
+      return var(new precomp_v_vari(result, lambda2.vi_, d_lambda2));
+    }
+
+
   } // namespace agrad
 
 } // namespace stan
--- src/test/unit/agrad/rev/functions/log_mix_test.cpp
+++ src/test/unit/agrad/rev/functions/log_mix_test.cpp
@@ -3,7 +3,6 @@
 #include <test/unit/agrad/util.hpp>
 #include <gtest/gtest.h>
 
-
 void test_log_mix_vvv(double theta, double lambda1, double lambda2) {
   using stan::agrad::var;
 
@@ -46,43 +45,6 @@ void test_log_mix_vvv(double theta, double lambda1, double lambda2) {
   stan::agrad::recover_memory();
 }
 
-void test_log_mix_vv_ex_theta(double theta, double lambda1, double lambda2) {
-  using stan::agrad::var;
-
-  var lambda1_v = lambda1;
-  var lambda2_v = lambda2;
-
-  std::vector<var> x;
-  x.push_back(lambda1_v);
-  x.push_back(lambda2_v);
-
-  var lp = log_mix(theta,lambda1_v,lambda2_v);
-  double val1 = lp.val();
-
-  std::vector<double> g;
-  lp.grad(x,g);
-  
-  stan::agrad::recover_memory();
-  
-  var lambda1_v2 = lambda1;
-  var lambda2_v2 = lambda2;
-  
-  std::vector<var> x2;
-  x2.push_back(lambda1_v2);
-  x2.push_back(lambda2_v2);
-
-  var lp2 = log(theta * exp(lambda1_v2) + (1 - theta) * exp(lambda2_v2));
-  double val2 = lp2.val();
-
-  std::vector<double> g2;
-  lp2.grad(x2,g2);
-
-  EXPECT_FLOAT_EQ(val2, val1);
-  EXPECT_EQ(g2.size(), g.size());
-  for (size_t i = 0; i < g2.size(); ++i)
-    EXPECT_FLOAT_EQ(g2[i], g[i]) << "failed on " << i << std::endl;
-  stan::agrad::recover_memory();
-}
 
 void test_log_mix_vv_ex_lam_2(double theta, double lambda1, double lambda2) {
   using stan::agrad::var;
@@ -160,6 +122,146 @@ void test_log_mix_vv_ex_lam_1(double theta, double lambda1, double lambda2) {
   stan::agrad::recover_memory();
 }
 
+void test_log_mix_vv_ex_theta(double theta, double lambda1, double lambda2) {
+  using stan::agrad::var;
+
+  var lambda1_v = lambda1;
+  var lambda2_v = lambda2;
+
+  std::vector<var> x;
+  x.push_back(lambda1_v);
+  x.push_back(lambda2_v);
+
+  var lp = log_mix(theta,lambda1_v,lambda2_v);
+  double val1 = lp.val();
+
+  std::vector<double> g;
+  lp.grad(x,g);
+  
+  stan::agrad::recover_memory();
+  
+  var lambda1_v2 = lambda1;
+  var lambda2_v2 = lambda2;
+  
+  std::vector<var> x2;
+  x2.push_back(lambda1_v2);
+  x2.push_back(lambda2_v2);
+
+  var lp2 = log(theta * exp(lambda1_v2) + (1 - theta) * exp(lambda2_v2));
+  double val2 = lp2.val();
+
+  std::vector<double> g2;
+  lp2.grad(x2,g2);
+
+  EXPECT_FLOAT_EQ(val2, val1);
+  EXPECT_EQ(g2.size(), g.size());
+  for (size_t i = 0; i < g2.size(); ++i)
+    EXPECT_FLOAT_EQ(g2[i], g[i]) << "failed on " << i << std::endl;
+  stan::agrad::recover_memory();
+}
+
+void test_log_mix_v_theta(double theta, double lambda1, double lambda2) {
+  using stan::agrad::var;
+
+  var theta_v = theta;
+
+  std::vector<var> x;
+  x.push_back(theta_v);
+
+  var lp = log_mix(theta_v,lambda1,lambda2);
+  double val1 = lp.val();
+
+  std::vector<double> g;
+  lp.grad(x,g);
+  
+  stan::agrad::recover_memory();
+  
+  var theta_v2 = theta;
+  
+  std::vector<var> x2;
+  x2.push_back(theta_v2);
+
+  var lp2 = log(theta_v2 * exp(lambda1) + (1 - theta_v2) * exp(lambda2));
+  double val2 = lp2.val();
+
+  std::vector<double> g2;
+  lp2.grad(x2,g2);
+
+  EXPECT_FLOAT_EQ(val2, val1);
+  EXPECT_EQ(g2.size(), g.size());
+  for (size_t i = 0; i < g2.size(); ++i)
+    EXPECT_FLOAT_EQ(g2[i], g[i]) << "failed on " << i << std::endl;
+  stan::agrad::recover_memory();
+}
+
+void test_log_mix_v_lam_1(double theta, double lambda1, double lambda2) {
+  using stan::agrad::var;
+
+  var lambda1_v = lambda1;
+
+  std::vector<var> x;
+  x.push_back(lambda1_v);
+
+  var lp = log_mix(theta,lambda1_v,lambda2);
+  double val1 = lp.val();
+
+  std::vector<double> g;
+  lp.grad(x,g);
+  
+  stan::agrad::recover_memory();
+  
+  var lambda1_v2 = lambda1;
+  
+  std::vector<var> x2;
+  x2.push_back(lambda1_v2);
+
+  var lp2 = log(theta * exp(lambda1_v2) + (1 - theta) * exp(lambda2));
+  double val2 = lp2.val();
+
+  std::vector<double> g2;
+  lp2.grad(x2,g2);
+
+  EXPECT_FLOAT_EQ(val2, val1);
+  EXPECT_EQ(g2.size(), g.size());
+  for (size_t i = 0; i < g2.size(); ++i)
+    EXPECT_FLOAT_EQ(g2[i], g[i]) << "failed on " << i << std::endl;
+  stan::agrad::recover_memory();
+}
+
+void test_log_mix_v_lam_2(double theta, double lambda1, double lambda2) {
+  using stan::agrad::var;
+
+  var lambda2_v = lambda2;
+
+  std::vector<var> x;
+  x.push_back(lambda2_v);
+
+  var lp = log_mix(theta,lambda1,lambda2_v);
+  double val1 = lp.val();
+
+  std::vector<double> g;
+  lp.grad(x,g);
+  
+  stan::agrad::recover_memory();
+  
+  var lambda2_v2 = lambda2;
+  
+  std::vector<var> x2;
+  x2.push_back(lambda2_v2);
+
+  var lp2 = log(theta * exp(lambda1) + (1 - theta) * exp(lambda2_v2));
+  double val2 = lp2.val();
+
+  std::vector<double> g2;
+  lp2.grad(x2,g2);
+
+  EXPECT_FLOAT_EQ(val2, val1);
+  EXPECT_EQ(g2.size(), g.size());
+  for (size_t i = 0; i < g2.size(); ++i)
+    EXPECT_FLOAT_EQ(g2[i], g[i]) << "failed on " << i << std::endl;
+  stan::agrad::recover_memory();
+}
+
 TEST(AgradRev,log_mix) {
 
   test_log_mix_vvv(0.3, 10.4, -10.9);
@@ -173,4 +275,13 @@ TEST(AgradRev,log_mix) {
 
   test_log_mix_vv_ex_theta(0.7, 1.4, -1.9);
   test_log_mix_vv_ex_theta(0.3, -10.4, 7.8);
+
+  test_log_mix_v_theta(0.7, 1.4, -1.9);
+  test_log_mix_v_theta(0.3, -1.4, 1.7);
+
+  test_log_mix_v_lam_1(0.7, 1.4, -1.9);
+  test_log_mix_v_lam_1(0.8, -1.9, 10.9);
+
+  test_log_mix_v_lam_2(0.7, 1.4, 3.99);
+  test_log_mix_v_lam_2(0.1, -1.4, 3.99);
 }
