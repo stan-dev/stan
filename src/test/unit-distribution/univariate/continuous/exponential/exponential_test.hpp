194b2fcb438f088d56b720d79f327321c6c21861
Rob Trangucci <robert.trangucci@gmail.com>
2014-12-22 14:26:36 -0500

    Exponential funciton now checks random variable is nonnegative and throws a domain error otherwise. Added an invalid value test as well

--- src/test/unit-distribution/univariate/continuous/exponential/exponential_test.hpp
+++ src/test/unit-distribution/univariate/continuous/exponential/exponential_test.hpp
@@ -27,6 +27,8 @@ public:
   void invalid_values(vector<size_t>& index, 
           vector<double>& value) {
     // y
+		index.push_back(0U);
+		value.push_back(-1.0);
     
     // beta
     index.push_back(1U);
