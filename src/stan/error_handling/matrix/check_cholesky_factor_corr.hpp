fa56c22cd7aeb68b51f027a05523d15c3627bb5c
Daniel Lee <bearlee@alum.mit.edu>
2015-01-05 17:04:21 -0500

    removed unused function

--- src/stan/error_handling/matrix/check_cholesky_factor_corr.hpp
+++ src/stan/error_handling/matrix/check_cholesky_factor_corr.hpp
@@ -14,56 +14,41 @@ namespace stan {
 
     /**
      * Return <code>true</code> if the specified matrix is a valid
-     * Cholesky factor.  A Cholesky factor is a lower triangular
-     * matrix whose diagonal elements are all positive.  Note that
-     * Cholesky factors need not be square, but require at least as
-     * many rows M as columns N (i.e., M &gt;= N).
+     * Cholesky factor of a correlation matrix.
+     *
+     * A Cholesky factor is a lower triangular matrix whose diagonal
+     * elements are all positive.  Note that Cholesky factors need not
+     * be square, but require at least as many rows M as columns N
+     * (i.e., M &gt;= N).
      *
      * Tolerance is specified by <code>math::CONSTRAINT_TOLERANCE</code>.
      *
-     * @param function
-     * @param y Matrix to test.
-     * @param name
-     * @return <code>true</code> if the matrix is a valid Cholesky factor.
-     * @return throws if any element in y is nan.
      * @tparam T_y Type of elements of Cholesky factor
+     *
+     * @param function Function name (for error messages)
+     * @param name Variable name (for error messages)
+     * @param y Matrix to test
+     * @return <code>true</code> if the matrix is a valid Cholesky factor 
+     *   of a correlation matrix
+     * @throw <code>std::domain_error</code> if y is not a valid Choleksy factor,
+     *   if number of rows is less than the number of columns, 
+     *   if there are 0 columns,
+     *   or if any element in matrix is NaN
      */
     template <typename T_y>
     bool check_cholesky_factor_corr(const std::string& function,
                                     const std::string& name,
                                     const Eigen::Matrix<T_y,Eigen::Dynamic,Eigen::Dynamic>& y) {
-      if (!check_square(function, name, y))
-        return false;
-      if (!check_lower_triangular(function, name, y))
-        return false;
+      check_square(function, name, y);
+      check_lower_triangular(function, name, y);
       for (int i = 0; i < y.rows(); ++i)
-        if (!check_positive(function, name, y(i,i)))
-          return false;
+        check_positive(function, name, y(i,i));
       for (int i = 0; i < y.rows(); ++i) {
         Eigen::Matrix<T_y,Eigen::Dynamic,1> 
           y_i = y.row(i).transpose();
-        if (!check_unit_vector(function, name, y_i))
-          return false;
+        check_unit_vector(function, name, y_i);
       }
       return true;
-    }
-
-
-    /**
-     * Return <code>true</code> if the specified matrix is a valid
-     * Cholesky factor (lower triangular, positive diagonal).
-     *
-     * @param function Name of function.
-     * @param y Matrix to test.
-     * @return <code>true</code> if the matrix is a valid Cholesky factor.
-     * @return throws if any element in matrix is nan
-     * @tparam T_y Type of elements of Cholesky factor
-     * @tparam T_result Type of result.
-     */
-    template <typename T_y>
-    inline bool check_cholesky_factor_corr(const std::string& function,
-                                           const Eigen::Matrix<T_y,Eigen::Dynamic,Eigen::Dynamic>& y) {
-      return check_cholesky_factor_corr(function, "(internal variable)", y);
     }
 
   }
