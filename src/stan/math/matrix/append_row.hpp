924dd9c85838e37cf9c21054a8aa9d76853c8944
Bob Carpenter <carp@alias-i.com>
2015-01-25 16:54:39 -0500

    append_row return type to vector w. tests

--- src/stan/math/matrix/append_row.hpp
+++ src/stan/math/matrix/append_row.hpp
@@ -21,7 +21,7 @@ namespace stan {
     template <typename T1, typename T2, int R1, int C1, int R2, int C2>
     inline Matrix<typename return_type<T1, T2>::type, Dynamic, Dynamic>
     append_row(const Matrix<T1, R1, C1> & A,
-          const Matrix<T2, R2, C2> & B) {
+               const Matrix<T2, R2, C2> & B) {
       int Arows = A.rows();
       int Brows = B.rows();
       int Acols = A.cols();
@@ -46,7 +46,7 @@ namespace stan {
     template <typename T1, typename T2, int R1, int R2>
     inline Matrix<typename return_type<T1, T2>::type, Dynamic, 1>
     append_row(const Matrix<T1, R1, 1> & A,
-          const Matrix<T2, R1, 1> & B) {          
+               const Matrix<T2, R1, 1> & B) {          
       int Asize = A.size();
       int Bsize = B.size();
       Matrix<typename return_type<T1, T2>::type, 1, Dynamic>
@@ -65,7 +65,7 @@ namespace stan {
     template <typename T, int R1, int C1, int R2, int C2>
     inline Matrix<T, Dynamic, Dynamic>
     append_row(const Matrix<T, R1, C1> & A,
-          const Matrix<T, R2, C2> & B) {
+               const Matrix<T, R2, C2> & B) {
       check_size_match("append_row",
                        "columns of A", A.cols(), 
                        "columns of B", B.cols());
@@ -77,12 +77,11 @@ namespace stan {
     }
        
     //vector append_row(vector, vector)
-    template <typename T, int R1, int R2>
+    template <typename T>
     inline Matrix<T, Dynamic, 1>
-    append_row(const Matrix<T, R1, 1> & A,
-          const Matrix<T, R1, 1> & B) {          
-      Matrix<T, Dynamic, 1>
-        result(A.size()+B.size());
+    append_row(const Matrix<T, Dynamic, 1> & A,
+               const Matrix<T, Dynamic, 1> & B) {          
+      Matrix<T, Dynamic, 1> result(A.size() + B.size());
       result << A, B;
       return result;
     }
