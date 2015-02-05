4bfa6c70f659134c3bea3d51b6a181e4974237ea
Rob Trangucci <robert.trangucci@gmail.com>
2015-02-05 16:39:56 -0500

    Proper autodiff tests for grad_hess, and updated documentation for autodiff.hpp/grad_hessian, added stan::agrad::grad_hessian

--- src/stan/agrad/autodiff.hpp
+++ src/stan/agrad/autodiff.hpp
@@ -359,6 +359,74 @@ namespace stan {
       stan::agrad::recover_memory_nested();
     }
 
-  }
-}
+    /**
+     * Calculate the value, the Hessian, and the gradient of the Hessian 
+     * of the specified function at the specified argument.  
+     *
+     * <p>The functor must implement 
+     * 
+     * <code>
+     * using stan::agrad::fvar;
+     * using stan::agrad::var;
+     *
+     * fvar<fvar<var> >
+     * operator()(const
+     * Eigen::Matrix<fvar<fvar<var> >,Eigen::Dynamic,1>&)
+     * </code>
+     *
+     * using only operations that are defined for
+     * <code>stan::agrad::fvar</code> and <code>stan::agrad::var</code>.  
+     *
+     * This latter constraint usually
+     * requires the functions to be defined in terms of the libraries
+     * defined in Stan or in terms of functions with appropriately
+     * general namespace imports that eventually depend on functions
+     * defined in Stan.
+     *
+     * @tparam F Type of function
+     * @param[in] f Function
+     * @param[in] x Argument to function
+     * @param[out] fx Function applied to argument
+     * @param[out] H Hessian of function at argument
+     * @param[out] grad_H Gradient of the Hessian of function at argument
+     */
+    template <typename F>
+    void
+    grad_hessian(const F& f,
+                 const Eigen::Matrix<double,Eigen::Dynamic,1>& x,
+                 double& fx,
+                 Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic>& H,
+                 std::vector<Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic> >& grad_H) {
+      start_nested();
+      fx = f(x);
+      try {
+        int d = x.size();
+        H.resize(d, d);
+        for (int i = 0; i < d; ++i)
+          grad_H.push_back(Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic>(d,d));
+        for (int i = 0; i < d; ++i) {
+          for (int j = i; j < d; ++j) {
+            Eigen::Matrix<fvar<fvar<var> >, Eigen::Dynamic, 1> x_ffvar(d);
+            for (int k = 0; k < d; ++k) 
+              x_ffvar(k) = fvar<fvar<var> >(fvar<var>(x(k),i==k),
+                                            fvar<var>(j==k,0));
+            fvar<fvar<var> > fx_ffvar = f(x_ffvar);
+            H(i,j) = fx_ffvar.d_.d_.val();
+            H(j,i) = H(i,j);
+            stan::agrad::grad(fx_ffvar.d_.d_.vi_);
+            for (int k = 0; k < d; ++k){
+              grad_H[i](j,k) = x_ffvar(k).val_.val_.adj();
+              grad_H[j](i,k) = grad_H[i](j,k);
+            }
+          }
+        }
+      } catch (const std::exception& e) {
+        stan::agrad::recover_memory_nested();
+        throw;
+      }
+      stan::agrad::recover_memory_nested();
+    }
+
+  } // namespace agrad
+} // namespace stan
 #endif
