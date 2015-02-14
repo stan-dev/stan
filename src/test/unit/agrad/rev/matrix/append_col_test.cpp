c9994711b465ad6140a0ce1dfe399674dc1db2b1
Marco Inacio <marcoigarapava@gmail.com>
2015-02-14 15:28:23 -0200

    Tests for return type of append_row and append_col when the inputs have different types

--- src/test/unit/agrad/rev/matrix/append_col_test.cpp
+++ src/test/unit/agrad/rev/matrix/append_col_test.cpp
@@ -68,3 +68,132 @@ TEST(AgradRevMatrix, append_col_row_vector) {
     EXPECT_FLOAT_EQ(std::exp(a(i).val()), g[idx++]);
   stan::agrad::recover_memory();
 }
+
+template <typename T, int R, int C>
+void correct_type_row_vector(const Eigen::Matrix<T,R,C>& x) {
+  EXPECT_EQ(Eigen::Dynamic, C);
+  EXPECT_EQ(1, R);
+}
+
+template <typename T, int R, int C>
+void correct_type_matrix(const Eigen::Matrix<T,R,C>& x) {
+  EXPECT_EQ(Eigen::Dynamic, C);
+  EXPECT_EQ(Eigen::Dynamic, R);
+}
+
+TEST(MathMatrix, append_col_different_types) {
+  using Eigen::Dynamic;
+  using Eigen::Matrix;
+  using Eigen::MatrixXd;
+  using Eigen::VectorXd;
+  using Eigen::RowVectorXd;
+  using stan::agrad::matrix_v;
+  using stan::agrad::row_vector_v;
+  using stan::agrad::vector_v;
+  using std::vector;  
+  
+  MatrixXd m33(3, 3);
+  m33 << 1, 2, 3,
+         4, 5, 6,
+         7, 8, 9;
+        
+  MatrixXd m32(3, 2);
+  m32 << 11, 12,
+         13, 14,
+         15, 16;
+
+  MatrixXd m23(2, 3);
+  m23 << 21, 22, 23,
+         24, 25, 26;
+         
+  MatrixXd m23b(2, 3);        
+  m23b = m23*1.101; //ensure some different values
+         
+  VectorXd v3(3);
+  v3 << 31, 
+        32,
+        33;
+        
+  VectorXd v3b(3);
+  v3b << 34, 
+         35,
+         36;
+
+  RowVectorXd rv3(3);
+  rv3 << 41, 42, 43;
+  
+  RowVectorXd rv3b(3);
+  rv3b << 44, 45, 46;
+  
+  MatrixXd vm33(3, 3);
+  vm33 << 1, 2, 3,
+         4, 5, 6,
+         7, 8, 9;
+        
+  MatrixXd vm32(3, 2);
+  vm32 << 11, 12,
+         13, 14,
+         15, 16;
+
+  MatrixXd vm23(2, 3);
+  vm23 << 21, 22, 23,
+         24, 25, 26;
+         
+  MatrixXd vm23b(2, 3);        
+  vm23b = m23*1.101; //ensure some different values
+         
+  VectorXd vv3(3);
+  vv3 << 31, 
+        32,
+        33;
+        
+  VectorXd vv3b(3);
+  vv3b << 34, 
+         35,
+         36;
+
+  RowVectorXd vrv3(3);
+  vrv3 << 41, 42, 43;
+  
+  RowVectorXd vrv3b(3);
+  vrv3b << 44, 45, 46;
+
+  correct_type_matrix(append_col(m32, vm33));
+  correct_type_matrix(append_col(m33, vm32));
+  correct_type_matrix(append_col(m23, vm23b));
+  correct_type_matrix(append_col(m23b, vm23));
+  correct_type_matrix(append_col(m33, vv3));
+  correct_type_matrix(append_col(v3, vm33));
+  correct_type_matrix(append_col(m32, vv3));
+  correct_type_matrix(append_col(v3, vm32));
+  correct_type_matrix(append_col(v3, vv3b));
+  correct_type_matrix(append_col(v3b, vv3));
+  correct_type_row_vector(append_col(rv3, vrv3b));
+  correct_type_row_vector(append_col(rv3b, vrv3));
+
+  correct_type_matrix(append_col(vm32, m33));
+  correct_type_matrix(append_col(vm33, m32));
+  correct_type_matrix(append_col(vm23, m23b));
+  correct_type_matrix(append_col(vm23b, m23));
+  correct_type_matrix(append_col(vm33, v3));
+  correct_type_matrix(append_col(vv3, m33));
+  correct_type_matrix(append_col(vm32, v3));
+  correct_type_matrix(append_col(vv3, m32));
+  correct_type_matrix(append_col(vv3, v3b));
+  correct_type_matrix(append_col(vv3b, v3));
+  correct_type_row_vector(append_col(vrv3, rv3b));
+  correct_type_row_vector(append_col(vrv3b, rv3));
+
+  correct_type_matrix(append_col(vm32, vm33));
+  correct_type_matrix(append_col(vm33, vm32));
+  correct_type_matrix(append_col(vm23, vm23b));
+  correct_type_matrix(append_col(vm23b, vm23));
+  correct_type_matrix(append_col(vm33, vv3));
+  correct_type_matrix(append_col(vv3, vm33));
+  correct_type_matrix(append_col(vm32, vv3));
+  correct_type_matrix(append_col(vv3, vm32));
+  correct_type_matrix(append_col(vv3, vv3b));
+  correct_type_matrix(append_col(vv3b, vv3));
+  correct_type_row_vector(append_col(vrv3, vrv3b));
+  correct_type_row_vector(append_col(vrv3b, vrv3));
+}
--- src/test/unit/agrad/rev/matrix/append_row_test.cpp
+++ src/test/unit/agrad/rev/matrix/append_row_test.cpp
@@ -65,3 +65,132 @@ TEST(AgradRevMatrix, append_row_row_vector) {
   for (int i = 0; i < 3; i++)
     EXPECT_FLOAT_EQ(std::exp(a(i).val()), g[idx++]);
 }
+
+template <typename T, int R, int C>
+void correct_type_vector(const Eigen::Matrix<T,R,C>& x) {
+  EXPECT_EQ(Eigen::Dynamic, R);
+  EXPECT_EQ(1, C);
+}
+
+template <typename T, int R, int C>
+void correct_type_matrix(const Eigen::Matrix<T,R,C>& x) {
+  EXPECT_EQ(Eigen::Dynamic, C);
+  EXPECT_EQ(Eigen::Dynamic, R);
+}
+
+TEST(MathMatrix, append_row_different_types) {
+  using Eigen::Dynamic;
+  using Eigen::Matrix;
+  using Eigen::MatrixXd;
+  using Eigen::VectorXd;
+  using Eigen::RowVectorXd;
+  using stan::agrad::matrix_v;
+  using stan::agrad::row_vector_v;
+  using stan::agrad::vector_v;
+  using std::vector;  
+  
+  MatrixXd m33(3, 3);
+  m33 << 1, 2, 3,
+         4, 5, 6,
+         7, 8, 9;
+        
+  MatrixXd m32(3, 2);
+  m32 << 11, 12,
+         13, 14,
+         15, 16;
+
+  MatrixXd m32b(2, 3);
+  m32b = m32*1.101; //ensure some different values
+
+  MatrixXd m23(2, 3);
+  m23 << 21, 22, 23,
+         24, 25, 26;
+         
+  VectorXd v3(3);
+  v3 << 31, 
+        32,
+        33;
+        
+  VectorXd v3b(3);
+  v3b << 34, 
+         35,
+         36;
+
+  RowVectorXd rv3(3);
+  rv3 << 41, 42, 43;
+  
+  RowVectorXd rv3b(3);
+  rv3b << 44, 45, 46;
+  
+  MatrixXd vm33(3, 3);
+  vm33 << 1, 2, 3,
+         4, 5, 6,
+         7, 8, 9;
+        
+  MatrixXd vm32(3, 2);
+  vm32 << 11, 12,
+         13, 14,
+         15, 16;
+
+  MatrixXd vm32b(2, 3);
+  vm32b = vm32*1.101; //ensure some different values
+
+  MatrixXd vm23(2, 3);
+  vm23 << 21, 22, 23,
+         24, 25, 26;
+         
+  VectorXd vv3(3);
+  vv3 << 31, 
+        32,
+        33;
+        
+  VectorXd vv3b(3);
+  vv3b << 34, 
+         35,
+         36;
+
+  RowVectorXd vrv3(3);
+  vrv3 << 41, 42, 43;
+  
+  RowVectorXd vrv3b(3);
+  vrv3b << 44, 45, 46;
+
+  correct_type_matrix(append_row(m23, vm33));
+  correct_type_matrix(append_row(m33, vm23));
+  correct_type_matrix(append_row(m32, vm32b));
+  correct_type_matrix(append_row(m32b, vm32));
+  correct_type_matrix(append_row(m33, vrv3));
+  correct_type_matrix(append_row(rv3, vm33));
+  correct_type_matrix(append_row(m23, vrv3));
+  correct_type_matrix(append_row(rv3, vm23));
+  correct_type_matrix(append_row(rv3, vrv3b));
+  correct_type_matrix(append_row(rv3b, vrv3));
+  correct_type_vector(append_row(v3, vv3b));
+  correct_type_vector(append_row(v3b, vv3));
+
+  correct_type_matrix(append_row(vm23, m33));
+  correct_type_matrix(append_row(vm33, m23));
+  correct_type_matrix(append_row(vm32, m32b));
+  correct_type_matrix(append_row(vm32b, m32));
+  correct_type_matrix(append_row(vm33, rv3));
+  correct_type_matrix(append_row(vrv3, m33));
+  correct_type_matrix(append_row(vm23, rv3));
+  correct_type_matrix(append_row(vrv3, m23));
+  correct_type_matrix(append_row(vrv3, rv3b));
+  correct_type_matrix(append_row(vrv3b, rv3));
+  correct_type_vector(append_row(vv3, v3b));
+  correct_type_vector(append_row(vv3b, v3));
+
+  correct_type_matrix(append_row(vm23, vm33));
+  correct_type_matrix(append_row(vm33, vm23));
+  correct_type_matrix(append_row(vm32, vm32b));
+  correct_type_matrix(append_row(vm32b, vm32));
+  correct_type_matrix(append_row(vm33, vrv3));
+  correct_type_matrix(append_row(vrv3, vm33));
+  correct_type_matrix(append_row(vm23, vrv3));
+  correct_type_matrix(append_row(vrv3, vm23));
+  correct_type_matrix(append_row(vrv3, vrv3b));
+  correct_type_matrix(append_row(vrv3b, vrv3));
+  correct_type_vector(append_row(vv3, vv3b));
+  correct_type_vector(append_row(vv3b, vv3));
+}
