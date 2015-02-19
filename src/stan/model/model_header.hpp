a8436dd2d52e3ae4b0152af98c7ef2b469dc0b44
Daniel Lee <bearlee@alum.mit.edu>
2015-02-19 16:09:35 -0500

    updating model header

--- src/stan/model/model_header.hpp
+++ src/stan/model/model_header.hpp
@@ -134,6 +134,8 @@
 #include <stan/math/prim/mat/fun/softmax.hpp>
 #include <stan/math/prim/mat/fun/sort.hpp>
 #include <stan/math/prim/mat/fun/sort_indices.hpp>
+#include <stan/math/prim/mat/fun/sort_indices_asc.hpp>
+#include <stan/math/prim/mat/fun/sort_indices_desc.hpp>
 #include <stan/math/prim/mat/fun/squared_distance.hpp>
 #include <stan/math/prim/mat/fun/stan_print.hpp>
 #include <stan/math/prim/mat/fun/sub_col.hpp>
