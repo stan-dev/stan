bd97b9736dae3c3c6ca8d78668c459d0a8297c35
Daniel Lee <bearlee@alum.mit.edu>
2015-02-20 11:25:45 -0500

    fixing is_fvar

--- src/stan/math/prim/scal/meta/is_fvar.hpp
+++ src/stan/math/prim/scal/meta/is_fvar.hpp
@@ -1,8 +1,6 @@
 #ifndef STAN__MATH__PRIM__SCAL__META__IS_FVAR_HPP
 #define STAN__MATH__PRIM__SCAL__META__IS_FVAR_HPP
 
-#include <stan/math/fwd/core/fvar.hpp>
-
 namespace stan {
 
   template <typename T>
