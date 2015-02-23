ca708e34b97bdc7ed246f2adaf38ce149207be92
Daniel Lee <bearlee@alum.mit.edu>
2015-02-23 16:38:47 -0500

    updated includes

--- src/stan/math/prim/scal/meta/get.hpp
+++ src/stan/math/prim/scal/meta/get.hpp
@@ -1,6 +1,8 @@
 #ifndef STAN__MATH__PRIM__SCAL__META__GET_HPP
 #define STAN__MATH__PRIM__SCAL__META__GET_HPP
 
+#include <cmath>
+
 namespace stan {
 
   template <typename T>
--- src/test/unit/math/prim/mat/fun/sort_test.cpp
+++ src/test/unit/math/prim/mat/fun/sort_test.cpp
@@ -1,6 +1,7 @@
 #include <stdexcept>
 #include <stan/math/prim/mat/fun/sort.hpp>
 #include <stan/math/prim/mat/meta/index_type.hpp>
+#include <stan/math/prim/arr/meta/index_type.hpp>
 #include <gtest/gtest.h>
 
 template <typename T>
