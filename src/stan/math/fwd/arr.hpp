b54f1954f38c72a6ebfb040f481340de419f7e5b
Daniel Lee <bearlee@alum.mit.edu>
2015-02-23 23:19:05 -0500

    adding overall includes

---
+++ src/stan/math/fwd/arr.hpp
@@ -0,0 +1,6 @@
+#ifndef STAN__MATH__FWD__ARR_HPP
+#define STAN__MATH__FWD__ARR_HPP
+
+#include <stan/math/fwd/arr/fun/log_sum_exp.hpp>
+
+#endif
---
+++ src/stan/math/rev/arr.hpp
@@ -0,0 +1,6 @@
+#ifndef STAN__MATH__REV__ARR_HPP
+#define STAN__MATH__REV__ARR_HPP
+
+#include <stan/math/rev/arr/functor/coupled_ode_system.hpp>
+
+#endif
