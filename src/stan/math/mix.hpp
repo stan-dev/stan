ce673ab8f3d4f1086955a99656e4e6f0d313a356
Daniel Lee <bearlee@alum.mit.edu>
2015-02-24 14:36:58 -0500

    added fwd.hpp and mix.hpp

---
+++ src/stan/math/mix.hpp
@@ -0,0 +1,9 @@
+#ifndef STAN__MATH__MIX_HPP
+#define STAN__MATH__MIX_HPP
+
+#include <stan/math/mix/core.hpp>
+#include <stan/math/mix/arr.hpp>
+#include <stan/math/mix/mat.hpp>
+#include <stan/math/mix/scal.hpp>
+
+#endif
