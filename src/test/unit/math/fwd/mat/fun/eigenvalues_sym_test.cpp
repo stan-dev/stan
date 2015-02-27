3801052a92f45cbd962f634edfec09d39c8622a6
Rob Trangucci <robert.trangucci@gmail.com>
2015-02-26 20:03:18 -0500

    f b/i-1222 split up more fwd test

--- src/test/unit/math/fwd/mat/fun/eigenvalues_sym_test.cpp
+++ src/test/unit/math/fwd/mat/fun/eigenvalues_sym_test.cpp
@@ -1,18 +1,11 @@
 #include <stan/math/prim/mat/fun/eigenvalues_sym.hpp>
 #include <stan/math/fwd/mat/fun/typedefs.hpp>
-#include <stan/math/mix/mat/fun/typedefs.hpp>
-#include <test/unit/math/rev/mat/fun/util.hpp>
 #include <gtest/gtest.h>
-#include <stan/math/rev/core.hpp>
 #include <stan/math/fwd/core.hpp>
 #include <stan/math/fwd/scal/fun/abs.hpp>
-#include <stan/math/rev/scal/fun/abs.hpp>
 #include <stan/math/fwd/scal/fun/fabs.hpp>
-#include <stan/math/rev/scal/fun/fabs.hpp>
 #include <stan/math/fwd/scal/fun/sqrt.hpp>
-#include <stan/math/rev/scal/fun/sqrt.hpp>
 #include <stan/math/fwd/scal/fun/value_of.hpp>
-#include <stan/math/rev/scal/fun/value_of.hpp>
 
 TEST(AgradFwdMatrixEigenvaluesSym, exceptions_matrix_fd) {
   stan::agrad::matrix_fd m0;
@@ -27,26 +20,6 @@ TEST(AgradFwdMatrixEigenvaluesSym, exceptions_matrix_fd) {
 TEST(AgradFwdMatrixEigenvaluesSym, exceptions_matrix_ffd) {
   stan::agrad::matrix_ffd m0;
   stan::agrad::matrix_ffd m1(2,3);
-  m1 << 1, 2, 3, 4, 5, 6;
-
-  using stan::math::eigenvalues_sym;
-  EXPECT_THROW(eigenvalues_sym(m0),std::invalid_argument);
-  EXPECT_THROW(eigenvalues_sym(m1),std::invalid_argument);
-}
-
-TEST(AgradFwdMatrixEigenvaluesSym, exceptions_matrix_fv) {
-  stan::agrad::matrix_fv m0;
-  stan::agrad::matrix_fv m1(2,3);
-  m1 << 1, 2, 3, 4, 5, 6;
-
-  using stan::math::eigenvalues_sym;
-  EXPECT_THROW(eigenvalues_sym(m0),std::invalid_argument);
-  EXPECT_THROW(eigenvalues_sym(m1),std::invalid_argument);
-}
-
-TEST(AgradFwdMatrixEigenvaluesSym, exceptions_matrix_ffv) {
-  stan::agrad::matrix_ffv m0;
-  stan::agrad::matrix_ffv m1(2,3);
   m1 << 1, 2, 3, 4, 5, 6;
 
   using stan::math::eigenvalues_sym;
@@ -86,132 +59,3 @@ TEST(AgradFwdMatrixEigenvaluesSym, matrix_ffd) {
   EXPECT_FLOAT_EQ(0, res0(0).d_.val_);
   EXPECT_FLOAT_EQ(2, res0(1).d_.val_);
 }
-
-TEST(AgradFwdMatrixEigenvaluesSym, matrix_fv_1st_deriv) {
-  stan::agrad::matrix_fv m0;
-  stan::agrad::matrix_fv m1(2,2);
-  m1 << 1, 2, 2,1;
-  m1(0,0).d_ = 1.0;
-  m1(0,1).d_ = 1.0;
-  m1(1,0).d_ = 1.0;
-  m1(1,1).d_ = 1.0;
-
-  stan::agrad::vector_fv res0 = stan::math::eigenvalues_sym(m1);
-
-  EXPECT_FLOAT_EQ(-1, res0(0).val_.val());
-  EXPECT_FLOAT_EQ(3, res0(1).val_.val());
-  EXPECT_FLOAT_EQ(0, res0(0).d_.val());
-  EXPECT_FLOAT_EQ(2, res0(1).d_.val());
-
-  AVEC z = createAVEC(m1(0,0).val_,m1(0,1).val_,m1(1,0).val_,m1(1,1).val_);
-  VEC h;
-  res0(1).val_.grad(z,h);
-  EXPECT_FLOAT_EQ(0.5,h[0]);
-  EXPECT_FLOAT_EQ(0.0,h[1]);
-  EXPECT_FLOAT_EQ(1.0,h[2]);
-  EXPECT_FLOAT_EQ(0.5,h[3]);
-}
-TEST(AgradFwdMatrixEigenvaluesSym, matrix_fv_2nd_deriv) {
-  stan::agrad::matrix_fv m0;
-  stan::agrad::matrix_fv m1(2,2);
-  m1 << 1, 2, 2,1;
-  m1(0,0).d_ = 1.0;
-  m1(0,1).d_ = 1.0;
-  m1(1,0).d_ = 1.0;
-  m1(1,1).d_ = 1.0;
-
-  stan::agrad::vector_fv res0 = stan::math::eigenvalues_sym(m1);
-
-  EXPECT_FLOAT_EQ(-1, res0(0).val_.val());
-  EXPECT_FLOAT_EQ(3, res0(1).val_.val());
-  EXPECT_FLOAT_EQ(0, res0(0).d_.val());
-  EXPECT_FLOAT_EQ(2, res0(1).d_.val());
-
-  AVEC z = createAVEC(m1(0,0).val_,m1(0,1).val_,m1(1,0).val_,m1(1,1).val_);
-  VEC h;
-  res0(1).d_.grad(z,h);
-  EXPECT_NEAR(-1.110223e-16,h[0],1e-8);
-  EXPECT_NEAR(0.0,h[1],1e-8);
-  EXPECT_NEAR(-2.7755576e-17,h[2],1e-8);
-  EXPECT_NEAR(1.110223e-16,h[3],1e-8);
-}
-
-
-TEST(AgradFwdMatrixEigenvaluesSym, matrix_ffv_1st_deriv) {
-  stan::agrad::matrix_ffv m0;
-  stan::agrad::matrix_ffv m1(2,2);
-  m1 << 1, 2, 2,1;
-  m1(0,0).d_ = 1.0;
-  m1(0,1).d_ = 1.0;
-  m1(1,0).d_ = 1.0;
-  m1(1,1).d_ = 1.0;
-
-  stan::agrad::vector_ffv res0 = stan::math::eigenvalues_sym(m1);
-
-  EXPECT_FLOAT_EQ(-1, res0(0).val_.val_.val());
-  EXPECT_FLOAT_EQ(3, res0(1).val_.val_.val());
-  EXPECT_FLOAT_EQ(0, res0(0).d_.val_.val());
-  EXPECT_FLOAT_EQ(2, res0(1).d_.val_.val());
-
-  AVEC z = createAVEC(m1(0,0).val_.val_,m1(0,1).val_.val_,m1(1,0).val_.val_,m1(1,1).val_.val_);
-  VEC h;
-  res0(1).val_.val_.grad(z,h);
-  EXPECT_FLOAT_EQ(0.5,h[0]);
-  EXPECT_FLOAT_EQ(0.0,h[1]);
-  EXPECT_FLOAT_EQ(1.0,h[2]);
-  EXPECT_FLOAT_EQ(0.5,h[3]);
-}
-TEST(AgradFwdMatrixEigenvaluesSym, matrix_ffv_2nd_deriv) {
-  stan::agrad::matrix_ffv m0;
-  stan::agrad::matrix_ffv m1(2,2);
-  m1 << 1, 2, 2,1;
-  m1(0,0).d_ = 1.0;
-  m1(0,1).d_ = 1.0;
-  m1(1,0).d_ = 1.0;
-  m1(1,1).d_ = 1.0;
-
-  stan::agrad::vector_ffv res0 = stan::math::eigenvalues_sym(m1);
-
-  EXPECT_FLOAT_EQ(-1, res0(0).val_.val_.val());
-  EXPECT_FLOAT_EQ(3, res0(1).val_.val_.val());
-  EXPECT_FLOAT_EQ(0, res0(0).d_.val_.val());
-  EXPECT_FLOAT_EQ(2, res0(1).d_.val_.val());
-
-  AVEC z = createAVEC(m1(0,0).val_.val_,m1(0,1).val_.val_,m1(1,0).val_.val_,m1(1,1).val_.val_);
-  VEC h;
-  res0(1).d_.val_.grad(z,h);
-  EXPECT_NEAR(-1.110223e-16,h[0],1e-8);
-  EXPECT_NEAR(0.0,h[1],1e-8);
-  EXPECT_NEAR(-2.7755576e-17,h[2],1e-8);
-  EXPECT_NEAR(1.110223e-16,h[3],1e-8);
-}
-TEST(AgradFwdMatrixEigenvaluesSym, matrix_ffv_3rd_deriv) {
-  stan::agrad::matrix_ffv m0;
-  stan::agrad::matrix_ffv m1(2,2);
-  m1 << 1, 2, 2,1;
-  m1(0,0).d_ = 1.0;
-  m1(0,1).d_ = 1.0;
-  m1(1,0).d_ = 1.0;
-  m1(1,1).d_ = 1.0;
-  m1(0,0).val_.d_ = 1.0;
-  m1(0,1).val_.d_ = 1.0;
-  m1(1,0).val_.d_ = 1.0;
-  m1(1,1).val_.d_ = 1.0;
-
-  stan::agrad::vector_ffv res0 = stan::math::eigenvalues_sym(m1);
-
-  EXPECT_FLOAT_EQ(-1, res0(0).val_.val_.val());
-  EXPECT_FLOAT_EQ(3, res0(1).val_.val_.val());
-  EXPECT_FLOAT_EQ(0, res0(0).d_.val_.val());
-  EXPECT_FLOAT_EQ(2, res0(1).d_.val_.val());
-
-  AVEC z = createAVEC(m1(0,0).val_.val_,m1(0,1).val_.val_,
-                      m1(1,0).val_.val_,m1(1,1).val_.val_);
-  VEC h;
-  res0(1).d_.d_.grad(z,h);
-  EXPECT_NEAR(1.110223e-16,h[0],1e-8);
-  EXPECT_NEAR(0.0,h[1],1e-8);
-  EXPECT_NEAR(2.7755576e-17,h[2],1e-8);
-  EXPECT_NEAR(-1.110223e-16,h[3],1e-8);
-}
-
--- src/test/unit/math/fwd/mat/fun/head_test.cpp
+++ src/test/unit/math/fwd/mat/fun/head_test.cpp
@@ -1,13 +1,10 @@
 #include <stdexcept>
 #include <stan/math/prim/mat/fun/head.hpp>
 #include <stan/math/fwd/mat/fun/typedefs.hpp>
-#include <stan/math/mix/mat/fun/typedefs.hpp>
 #include <stan/math/fwd/core.hpp>
-#include <stan/math/rev/core.hpp>
 #include <gtest/gtest.h>
 
 using stan::agrad::fvar;
-using stan::agrad::var;
 
 TEST(AgradFwdMatrixHead,HeadVector1_fd) {
   using stan::math::head;
@@ -98,95 +95,6 @@ TEST(AgradFwdMatrixHead,HeadStdVector4_fd) {
     EXPECT_FLOAT_EQ(v[n].val_,v01[n].val_);
   }
 }
-TEST(AgradFwdMatrixHead,HeadVector1_fv) {
-  using stan::math::head;
-  stan::agrad::vector_fv v(3);
-  v << 1, 2, 3;
-  EXPECT_EQ(0, head(v,0).size());
-}
-TEST(AgradFwdMatrixHead,HeadVector2_fv) {
-  using stan::math::head;
-  stan::agrad::vector_fv v(3);
-  v << 1, 2, 3;
-  EXPECT_EQ(3, head(v,3).size());
-}
-TEST(AgradFwdMatrixHead,HeadVector3_fv) {
-  using stan::math::head;
-  stan::agrad::vector_fv v(3);
-  v << 1, 2, 3;
-  EXPECT_THROW(head(v,4), std::out_of_range);
-}
-TEST(AgradFwdMatrixHead,HeadVector4_fv) {
-  using stan::math::head;
-  stan::agrad::vector_fv v(3);
-  v << 1, 2, 3;
-
-  stan::agrad::vector_fv v01 = head(v,2);
-  EXPECT_EQ(2,v01.size());
-  for (int n = 0; n < 2; ++n) {
-    EXPECT_FLOAT_EQ(v[n].val_.val(),v01[n].val_.val());
-  }
-}
-
-TEST(AgradFwdMatrixHead,HeadRowVector1_fv) {
-  using stan::math::head;
-  stan::agrad::row_vector_fv v(3);
-  v << 1, 2, 3;
-  EXPECT_EQ(0, head(v,0).size());
-}
-TEST(AgradFwdMatrixHead,HeadRowVector2_fv) {
-  using stan::math::head;
-  stan::agrad::row_vector_fv v(3);
-  v << 1, 2, 3;
-  EXPECT_EQ(3, head(v,3).size());
-}
-TEST(AgradFwdMatrixHead,HeadRowVector3_fv) {
-  using stan::math::head;
-  stan::agrad::row_vector_fv v(3);
-  v << 1, 2, 3;
-  EXPECT_THROW(head(v,4), std::out_of_range);
-}
-TEST(AgradFwdMatrixHead,HeadRowVector4_fv) {
-  using stan::math::head;
-   stan::agrad::row_vector_fv v(3);
-  v << 1, 2, 3;
-
-  stan::agrad::row_vector_fv v01 = head(v,2);
-  EXPECT_EQ(2,v01.size());
-  for (int n = 0; n < 2; ++n) {
-    EXPECT_FLOAT_EQ(v[n].val_.val(),v01[n].val_.val());
-  }
-}
-
-
-TEST(AgradFwdMatrixHead,HeadStdVector1_fv) {
-  using stan::math::head;
-  std::vector<fvar<var> > v;
-  v.push_back(1); v.push_back(2); v.push_back(3);
-  EXPECT_EQ(0U, head(v,0).size());
-}
-TEST(AgradFwdMatrixHead,HeadStdVector2_fv) {
-  using stan::math::head;
-  std::vector<fvar<var> > v;
-  v.push_back(1); v.push_back(2); v.push_back(3);
-  EXPECT_EQ(3U, head(v,3).size());
-}
-TEST(AgradFwdMatrixHead,HeadStdVector3_fv) {
-  using stan::math::head;
-  std::vector<fvar<var> > v;
-  v.push_back(1); v.push_back(2); v.push_back(3);
-  EXPECT_THROW(head(v,4), std::out_of_range);
-}
-TEST(AgradFwdMatrixHead,HeadStdVector4_fv) {
-  using stan::math::head;
-  std::vector<fvar<var> > v;
-  v.push_back(1); v.push_back(2); v.push_back(3);
-  std::vector<fvar<var> > v01 = head(v,2);
-  EXPECT_EQ(2U, v01.size());
-  for (int n = 0; n < 2; ++n) {
-    EXPECT_FLOAT_EQ(v[n].val_.val(),v01[n].val_.val());
-  }
-}
 TEST(AgradFwdMatrixHead,HeadVector1_ffd) {
   using stan::math::head;
   stan::agrad::vector_ffd v(3);
@@ -274,94 +182,5 @@ TEST(AgradFwdMatrixHead,HeadStdVector4_ffd) {
   EXPECT_EQ(2U, v01.size());
   for (int n = 0; n < 2; ++n) {
     EXPECT_FLOAT_EQ(v[n].val_.val_,v01[n].val_.val_);
-  }
-}
-TEST(AgradFwdMatrixHead,HeadVector1_ffv) {
-  using stan::math::head;
-  stan::agrad::vector_ffv v(3);
-  v << 1, 2, 3;
-  EXPECT_EQ(0, head(v,0).size());
-}
-TEST(AgradFwdMatrixHead,HeadVector2_ffv) {
-  using stan::math::head;
-  stan::agrad::vector_ffv v(3);
-  v << 1, 2, 3;
-  EXPECT_EQ(3, head(v,3).size());
-}
-TEST(AgradFwdMatrixHead,HeadVector3_ffv) {
-  using stan::math::head;
-  stan::agrad::vector_ffv v(3);
-  v << 1, 2, 3;
-  EXPECT_THROW(head(v,4), std::out_of_range);
-}
-TEST(AgradFwdMatrixHead,HeadVector4_ffv) {
-  using stan::math::head;
-  stan::agrad::vector_ffv v(3);
-  v << 1, 2, 3;
-
-  stan::agrad::vector_ffv v01 = head(v,2);
-  EXPECT_EQ(2,v01.size());
-  for (int n = 0; n < 2; ++n) {
-    EXPECT_FLOAT_EQ(v[n].val_.val_.val(),v01[n].val_.val_.val());
-  }
-}
-
-TEST(AgradFwdMatrixHead,HeadRowVector1_ffv) {
-  using stan::math::head;
-  stan::agrad::row_vector_ffv v(3);
-  v << 1, 2, 3;
-  EXPECT_EQ(0, head(v,0).size());
-}
-TEST(AgradFwdMatrixHead,HeadRowVector2_ffv) {
-  using stan::math::head;
-  stan::agrad::row_vector_ffv v(3);
-  v << 1, 2, 3;
-  EXPECT_EQ(3, head(v,3).size());
-}
-TEST(AgradFwdMatrixHead,HeadRowVector3_ffv) {
-  using stan::math::head;
-  stan::agrad::row_vector_ffv v(3);
-  v << 1, 2, 3;
-  EXPECT_THROW(head(v,4), std::out_of_range);
-}
-TEST(AgradFwdMatrixHead,HeadRowVector4_ffv) {
-  using stan::math::head;
-   stan::agrad::row_vector_ffv v(3);
-  v << 1, 2, 3;
-
-  stan::agrad::row_vector_ffv v01 = head(v,2);
-  EXPECT_EQ(2,v01.size());
-  for (int n = 0; n < 2; ++n) {
-    EXPECT_FLOAT_EQ(v[n].val_.val_.val(),v01[n].val_.val_.val());
-  }
-}
-
-
-TEST(AgradFwdMatrixHead,HeadStdVector1_ffv) {
-  using stan::math::head;
-  std::vector<fvar<fvar<var> > > v;
-  v.push_back(1); v.push_back(2); v.push_back(3);
-  EXPECT_EQ(0U, head(v,0).size());
-}
-TEST(AgradFwdMatrixHead,HeadStdVector2_ffv) {
-  using stan::math::head;
-  std::vector<fvar<fvar<var> > > v;
-  v.push_back(1); v.push_back(2); v.push_back(3);
-  EXPECT_EQ(3U, head(v,3).size());
-}
-TEST(AgradFwdMatrixHead,HeadStdVector3_ffv) {
-  using stan::math::head;
-  std::vector<fvar<fvar<var> > > v;
-  v.push_back(1); v.push_back(2); v.push_back(3);
-  EXPECT_THROW(head(v,4), std::out_of_range);
-}
-TEST(AgradFwdMatrixHead,HeadStdVector4_ffv) {
-  using stan::math::head;
-  std::vector<fvar<fvar<var> > > v;
-  v.push_back(1); v.push_back(2); v.push_back(3);
-  std::vector<fvar<fvar<var> > > v01 = head(v,2);
-  EXPECT_EQ(2U, v01.size());
-  for (int n = 0; n < 2; ++n) {
-    EXPECT_FLOAT_EQ(v[n].val_.val_.val(),v01[n].val_.val_.val());
   }
 }
