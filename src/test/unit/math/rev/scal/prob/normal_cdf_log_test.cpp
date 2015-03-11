1647ad59a0c6c7bfbcfeee037cef378d2b95a529
Daniel Lee <bearlee@alum.mit.edu>
2015-03-11 01:27:43 -0400

    fixes #1357: normal_cdf_log. added tests that fail in current version and applied a fix to the derivatives

--- src/test/unit/math/rev/scal/prob/normal_cdf_log_test.cpp
+++ src/test/unit/math/rev/scal/prob/normal_cdf_log_test.cpp
@@ -3,8 +3,9 @@
 #include <stan/math/rev/scal/fun/value_of.hpp>
 #include <stan/math/rev/scal/fun/value_of_rec.hpp>
 #include <stan/math/rev/core.hpp>
+#include <stan/math/prim/scal/fun/is_nan.hpp>
 
-TEST(ProbDistributionsNormal, cdf_log_tails) {
+TEST(normal_cdf_log, tails) {
   using stan::agrad::var;
   using stan::prob::normal_cdf_log;
   using std::exp;
@@ -104,5 +105,77 @@ TEST(ProbDistributionsNormal, cdf_log_tails) {
   EXPECT_FLOAT_EQ(1, 1 / exp(normal_cdf_log(var(8.5),0,1).val()));
   EXPECT_FLOAT_EQ(1, 1 / exp(normal_cdf_log(var(9),0,1).val()));
   EXPECT_FLOAT_EQ(1, 1 / exp(normal_cdf_log(var(9.5),0,1).val()));
-  EXPECT_FLOAT_EQ(1, 1 / exp(normal_cdf_log(var(10),0,1).val())); 
+  EXPECT_FLOAT_EQ(1, 1 / exp(normal_cdf_log(var(10),0,1).val()));
+
+  stan::agrad::recover_memory();
+}
+
+void test_value_and_derivatives(double expected_val,
+                                double y_dbl, double mu_dbl, double sigma_dbl) {
+  using stan::math::is_nan;
+  using stan::agrad::var;  
+  using stan::prob::normal_cdf_log;
+  std::stringstream msg_ss;
+  msg_ss << "parameters: (" << y_dbl << ", " << mu_dbl << ", " << sigma_dbl << ")";
+  std::string msg = msg_ss.str();
+
+  SCOPED_TRACE(msg);
+  
+  var y(y_dbl);
+  var mu(mu_dbl);
+  var sigma(sigma_dbl);
+
+  std::vector<double> gradients;
+  var val = normal_cdf_log(y, mu, sigma);
+  std::vector<var> x;
+  x.push_back(y);
+  x.push_back(mu);
+  x.push_back(sigma);
+  gradients.clear();
+  val.grad(x, gradients);
+
+  double e = 1e-10;
+  double inv2e = 0.5 / e;
+  std::vector<double> finite_diffs;
+  finite_diffs.resize(3);
+  finite_diffs[0] = (normal_cdf_log(y_dbl + e, mu_dbl, sigma_dbl)
+                     - normal_cdf_log(y_dbl - e, mu_dbl, sigma_dbl)) * inv2e;
+  finite_diffs[1] = (normal_cdf_log(y_dbl, mu_dbl + e, sigma_dbl)
+                     - normal_cdf_log(y_dbl, mu_dbl - e, sigma_dbl)) * inv2e;
+  finite_diffs[2] = (normal_cdf_log(y_dbl, mu_dbl, sigma_dbl + e)
+                     - normal_cdf_log(y_dbl, mu_dbl, sigma_dbl - e)) * inv2e;
+
+  
+  EXPECT_FLOAT_EQ(expected_val, val.val());
+  EXPECT_FALSE(is_nan(gradients[0]));
+  EXPECT_FALSE(is_nan(gradients[1]));
+  EXPECT_FALSE(is_nan(gradients[2]));
+  if (!is_nan(gradients[0])) {
+    if (!is_nan(finite_diffs[0]))
+      EXPECT_NEAR(finite_diffs[0], gradients[0], 1e-2);
+    else
+      EXPECT_FLOAT_EQ(0.0, gradients[0]);
+  }
+  if (!is_nan(gradients[1])) {
+    if (!is_nan(finite_diffs[1]))
+      EXPECT_NEAR(finite_diffs[1], gradients[1], 1e-2);
+    else
+      EXPECT_FLOAT_EQ(0.0, gradients[1]);
+  }
+  if (!is_nan(gradients[2])) {
+    if (!is_nan(finite_diffs[2]))
+      EXPECT_NEAR(finite_diffs[2], gradients[2], 1e-2);
+    else
+      EXPECT_FLOAT_EQ(0.0, gradients[2]);
+  }
+}
+
+
+TEST(normal_cdf_log, derivatives) {
+  test_value_and_derivatives(log(0.5), 10.0, 10.0, 0.5);
+  test_value_and_derivatives(-std::numeric_limits<double>::infinity(), -20.0, 10.0, 0.5);
+  test_value_and_derivatives(-std::numeric_limits<double>::infinity(), -30.0, 10.0, 0.5);
+  test_value_and_derivatives(-std::numeric_limits<double>::infinity(), -50.0, 10.0, 1.0);
+
+  test_value_and_derivatives(0.0, 30.0, 10.0, 0.5);
 }
