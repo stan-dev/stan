20befbd82457b88946b6c9372109d9dd85388d58
Krzysztof Sakrejda <krzysztof.sakrejda@gmail.com>
2015-03-13 11:03:53 -0400

    Just copied matrix stuff for tests, need to modify it before it will work.  Also added compressed-row version amenable to dot product variation.

--- src/stan/math/prim/mat/fun/sparse_multiply.hpp
+++ src/stan/math/prim/mat/fun/sparse_multiply.hpp
@@ -10,7 +10,6 @@ namespace stan {
 
   namespace math {
 
-
     /** Return the multiplication of the sparse matrix spefcified by
      * by values and indexing by the specified dense vector.
      *
@@ -56,6 +55,52 @@ namespace stan {
       return y;
     }
 
+    /** Return the multiplication of the sparse matrix spefcified by
+     * by values and indexing by the specified dense vector.
+     *
+     * The sparse matrix X of dimension m by n is represented by the
+     * vector w (of values), the integer array v (containing one-based
+     * column index of each value), the integer array u (containing
+     * one-based indexes of where each row starts in w), and the
+     * integer array z (containing the number of non-zero entries in
+     * each row of w).
+     *
+     * @tparam T1 Type of sparse matrix entries.
+     * @tparam T2 Type of dense vector entries.
+     * @param m Rows in matrix.
+     * @param n Columns in matrix.
+     * @param v One-based column index of each value.
+     * @param u one-based index of where each row starts in w.
+     * @param z number of non-zero entries in each row of w.
+     * @return dense vector for the product.
+     * @throw ... explain error conditions briefly ...
+     */
+    template <typename T1, typename T2>
+    inline
+    Eigen::Matrix<typename boost::math::tools::promote_args<T1, T2>::type, Eigen::Dynamic, 1>
+    sparse_multiply(const int& m,
+                    const int& n,
+                    const Eigen::Matrix<T1, Eigen::Dynamic,1>& w,
+                    const std::vector<int>& v,
+                    const std::vector<int>& u,
+                    const std::vector<int>& z,
+                    const Eigen::Matrix<T2, Eigen::Dynamic,1>& b) {
+
+      // a whole bunch of error checking goes here, e.g., m, n > 0,
+      // etc. etc.  we can't throw index out of bounds exceptions at
+      // run time
+
+      typedef typename boost::math::tools::promote_args<T1, T2>::type fun_scalar_t;
+      Eigen::Matrix<fun_scalar_t, Eigen::Dynamic, 1>  y(m);
+      for (int i = 0; i < m; ++i) {
+        int end = u[j] + z[j] - 1;
+        for (int q = u[j] - 1; q < end; ++q)
+					// Now this is the compressed row format version.  FIXME:
+					// Modify to use dot product function.
+          y(i) += w[q] * b[v[q]];
+      }
+      return y;
+    }
 
   }
 
