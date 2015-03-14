9803d1e0c58e58280ea4b97270ca10347b50dca5
Daniel Lee <bearlee@alum.mit.edu>
2015-03-14 15:44:43 -0400

    fixes #1376. Removing built documentation. [skip ci]

--- doc/api/html/search/typedefs_6.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['idx_5ft',['idx_t',['../classstan_1_1optimization_1_1_b_f_g_s_line_search.html#a7501d10f44237d264e637352bdc1ca4e',1,'stan::optimization::BFGSLineSearch']]],
-  ['input_5fiterator_5ft',['input_iterator_t',['../namespacestan_1_1lang.html#ae782260f5a96de91250c10d1ef3b3545',1,'stan::lang']]],
-  ['int_5fargument',['int_argument',['../namespacestan_1_1services.html#abf6790d43872b456350d91d16e85a147',1,'stan::services']]]
-];
--- doc/api/html/search/typedefs_9.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['nested',['Nested',['../struct_eigen_1_1_num_traits_3_01stan_1_1agrad_1_1fvar_3_01_t_01_4_01_4.html#a28662bcffa97fcfbe9a19b9c53c681fe',1,'Eigen::NumTraits&lt; stan::agrad::fvar&lt; T &gt; &gt;::Nested()'],['../struct_eigen_1_1_num_traits_3_01stan_1_1agrad_1_1var_01_4.html#aa313e91d6d8163393d270e3c70ad50cf',1,'Eigen::NumTraits&lt; stan::agrad::var &gt;::Nested()']]],
-  ['noninteger',['NonInteger',['../struct_eigen_1_1_num_traits_3_01stan_1_1agrad_1_1fvar_3_01_t_01_4_01_4.html#af7ef8575ca09c5aa9e8ee3fa383b3fc9',1,'Eigen::NumTraits&lt; stan::agrad::fvar&lt; T &gt; &gt;::NonInteger()'],['../struct_eigen_1_1_num_traits_3_01stan_1_1agrad_1_1var_01_4.html#a0b7008c189ffa66353158a30af2aabaf',1,'Eigen::NumTraits&lt; stan::agrad::var &gt;::NonInteger()']]]
-];
