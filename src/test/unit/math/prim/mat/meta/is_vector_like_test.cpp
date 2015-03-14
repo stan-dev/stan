8244c6849c6ff635871215b40b440a5fe8365d22
Daniel Lee <bearlee@alum.mit.edu>
2015-03-14 13:23:18 -0400

    added additional tests for is_vector_like

--- src/test/unit/math/prim/mat/meta/is_vector_like_test.cpp
+++ src/test/unit/math/prim/mat/meta/is_vector_like_test.cpp
@@ -11,3 +11,19 @@ TEST(is_vector_like, MatrixXd) {
 TEST(is_vector_like, vector_of_MatrixXd) {
   EXPECT_TRUE(stan::is_vector_like<std::vector<Eigen::MatrixXd> >::value);
 }
+
+TEST(is_vector_like, VectorXd) {
+  EXPECT_TRUE(stan::is_vector_like<Eigen::VectorXd>::value);
+}
+
+TEST(is_vector_like, vector_of_VectorXd) {
+  EXPECT_TRUE(stan::is_vector_like<std::vector<Eigen::VectorXd> >::value);
+}
+
+TEST(is_vector_like, RowVectorXd) {
+  EXPECT_TRUE(stan::is_vector_like<Eigen::RowVectorXd>::value);
+}
+
+TEST(is_vector_like, vector_of_RowVectorXd) {
+  EXPECT_TRUE(stan::is_vector_like<std::vector<Eigen::RowVectorXd> >::value);
+}
