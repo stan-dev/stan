95da0b8b3327110c0c74778c11c614545131161f
Krzysztof Sakrejda <krzysztof.sakrejda@gmail.com>
2015-03-16 14:51:15 -0400

    Added error checks for CSC format.

--- src/stan/math/prim/mat/fun/sparse_multiply.hpp
+++ src/stan/math/prim/mat/fun/sparse_multiply.hpp
@@ -33,6 +33,36 @@ namespace stan {
      * @return dense vector for the product.
      * @throw ... explain error conditions briefly ...
      */
+    /** Return the multiplication of the sparse matrix (specified by
+     * by values and indexing) by the specified dense vector.
+     *
+     * The sparse matrix X of dimension m by n is represented by the
+     * vector w (of values), the integer array v (containing one-based
+     * row index of each value), the integer array u (containing
+     * one-based indexes of where each column starts in w), and the
+     * integer array z (containing the number of non-zero entries in
+     * each column of w).
+     *
+     * @tparam T1 Type of sparse matrix entries.
+     * @tparam T2 Type of dense vector entries.
+     * @param m Number of rows in matrix.
+     * @param n Number of columns in matrix.
+		 * @param w Vector of non-zero values in matrix.
+     * @param v One-based row index of each non-zero value, same
+		 *          length as w.
+     * @param u one-based index of where each column starts in w, equal to
+		 *          the number of columns plus one.
+     * @param z number of non-zero entries in each column of w, equal to
+		 *          the number of columns..
+     * @return dense vector for the product.
+     * @throw std::domain_error if m and n are not positive or are nan.
+		 * @throw std::domain_error if the implied sparse matrix and b are 
+		 *                          not multiplicable.
+		 * @throw std::domain_error if m/n/w/v/u/z are not internally
+		 * consistent, as defined by the indexing scheme.  Extractors are
+		 * defined in Stan which guarantee a consistent set of m/n/w/v/u/z
+		 * for a given sparse matrix.  
+     */
     template <typename T1, typename T2>
     inline
     Eigen::Matrix<typename boost::math::tools::promote_args<T1, T2>::type, Eigen::Dynamic, 1>
@@ -44,9 +74,13 @@ namespace stan {
                     const std::vector<int>& z,
                     const Eigen::Matrix<T2, Eigen::Dynamic,1>& b) {
 
-      // a whole bunch of error checking goes here, e.g., m, n > 0,
-      // etc. etc.  we can't throw index out of bounds exceptions at
-      // run time
+			stan::math::check_positive("sparse_multiply_csr","m",m);
+			stan::math::check_positive("sparse_multiply_csr","n",n);
+			stan::math::check_equal("sparse_multiply_csr","n/b", n, b.size());
+			stan::math::check_equal("sparse_multiply_csr","n/u", n, u.size()-1);
+			stan::math::check_equal("sparse_multiply_csr","n/z", n, z.size()  );
+			stan::math::check_equal("sparse_multiply_csr","w/v", w.size(), v.size());
+			stan::math::check_equal("sparse_multiply_csr","u/z/v", u[n-1]+z[n-1]-1, v.size());
 
       typedef typename boost::math::tools::promote_args<T1, T2>::type fun_scalar_t;
       Eigen::Matrix<fun_scalar_t, Eigen::Dynamic, 1>  y(m);
@@ -80,7 +114,13 @@ namespace stan {
      * @param z number of non-zero entries in each row of w, equal to
 		 *          the number of rows..
      * @return dense vector for the product.
-     * @throw ... explain error conditions briefly ...
+     * @throw std::domain_error if m and n are not positive or are nan.
+		 * @throw std::domain_error if the implied sparse matrix and b are 
+		 *                          not multiplicable.
+		 * @throw std::domain_error if m/n/w/v/u/z are not internally
+		 * consistent, as defined by the indexing scheme.  Extractors are
+		 * defined in Stan which guarantee a consistent set of m/n/w/v/u/z
+		 * for a given sparse matrix.  
      */
     template <typename T1, typename T2>
     inline
