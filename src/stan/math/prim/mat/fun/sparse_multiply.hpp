cb3464a8f0161f8236f2941e62c059556c43ff8f
Krzysztof Sakrejda <krzysztof.sakrejda@gmail.com>
2015-03-17 10:51:17 -0400

    Documented extractors by group and removed cut-and-paste screw up from sparse_multiply.

--- src/stan/math/prim/mat/fun/sparse_multiply.hpp
+++ src/stan/math/prim/mat/fun/sparse_multiply.hpp
@@ -13,26 +13,6 @@ namespace stan {
 
   namespace math {
 
-    /** Return the multiplication of the sparse matrix spefcified by
-     * by values and indexing by the specified dense vector.
-     *
-     * The sparse matrix X of dimension m by n is represented by the
-     * vector w (of values), the integer array v (containing one-based
-     * row index of each value), the integer array u (containing
-     * one-based indexes of where each column starts in w), and the
-     * integer array z (containing the number of non-zero entries in
-     * each column of w).
-     *
-     * @tparam T1 Type of sparse matrix entries.
-     * @tparam T2 Type of dense vector entries.
-     * @param m Rows in matrix.
-     * @param n Columns in matrix.
-     * @param v One-based row index of each value.
-     * @param u one-based index of where each column starts in w.
-     * @param z number of non-zer entries in each column of w.
-     * @return dense vector for the product.
-     * @throw ... explain error conditions briefly ...
-     */
     /** Return the multiplication of the sparse matrix (specified by
      * by values and indexing) by the specified dense vector.
      *
