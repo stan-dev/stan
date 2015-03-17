d0086ad2b445dd461ffc5e3bd10745ec72634130
Daniel Lee <bearlee@alum.mit.edu>
2015-03-17 19:03:50 -0400

    release/v2.6.3: adding built documentation. [skip ci]

---
+++ doc/api/html/search/files_f.js
@@ -0,0 +1,12 @@
+var searchData=
+[
+  ['qr_5fq_2ehpp',['qr_Q.hpp',['../fwd_2mat_2fun_2qr___q_8hpp.html',1,'']]],
+  ['qr_5fq_2ehpp',['qr_Q.hpp',['../prim_2mat_2fun_2qr___q_8hpp.html',1,'']]],
+  ['qr_5fr_2ehpp',['qr_R.hpp',['../prim_2mat_2fun_2qr___r_8hpp.html',1,'']]],
+  ['qr_5fr_2ehpp',['qr_R.hpp',['../fwd_2mat_2fun_2qr___r_8hpp.html',1,'']]],
+  ['quad_5fform_2ehpp',['quad_form.hpp',['../prim_2mat_2fun_2quad__form_8hpp.html',1,'']]],
+  ['quad_5fform_2ehpp',['quad_form.hpp',['../rev_2mat_2fun_2quad__form_8hpp.html',1,'']]],
+  ['quad_5fform_5fdiag_2ehpp',['quad_form_diag.hpp',['../quad__form__diag_8hpp.html',1,'']]],
+  ['quad_5fform_5fsym_2ehpp',['quad_form_sym.hpp',['../prim_2mat_2fun_2quad__form__sym_8hpp.html',1,'']]],
+  ['quad_5fform_5fsym_2ehpp',['quad_form_sym.hpp',['../rev_2mat_2fun_2quad__form__sym_8hpp.html',1,'']]]
+];
---
+++ doc/api/html/search/variables_11.js
@@ -0,0 +1,21 @@
+var searchData=
+[
+  ['radix',['radix',['../structstd_1_1numeric__limits_3_01stan_1_1agrad_1_1fvar_3_01_t_01_4_01_4.html#afd5b4f9c99692d107b041363e1224f5c',1,'std::numeric_limits&lt; stan::agrad::fvar&lt; T &gt; &gt;::radix()'],['../structstd_1_1numeric__limits_3_01stan_1_1agrad_1_1var_01_4.html#ab0737f3e76c801f971530861695381fa',1,'std::numeric_limits&lt; stan::agrad::var &gt;::radix()']]],
+  ['rand_5fint_5f',['rand_int_',['../classstan_1_1mcmc_1_1base__hmc.html#a0df069452ef3dc3bcf273be3f977755a',1,'stan::mcmc::base_hmc']]],
+  ['rand_5funiform_5f',['rand_uniform_',['../classstan_1_1mcmc_1_1base__hmc.html#ae90f133d25649ffdbafa0f30a4ee5d11',1,'stan::mcmc::base_hmc']]],
+  ['random_5fseed',['random_seed',['../structstan_1_1io_1_1stan__csv__metadata.html#a11e87094a178f9c9942cd7e2f5d7bd02',1,'stan::io::stan_csv_metadata']]],
+  ['range_5f',['range_',['../structstan_1_1lang_1_1int__var__decl.html#a59dec3d3d774b81d823748725cb0226f',1,'stan::lang::int_var_decl::range_()'],['../structstan_1_1lang_1_1double__var__decl.html#ad66210f2741b3db6e84fc24f6ca865bb',1,'stan::lang::double_var_decl::range_()'],['../structstan_1_1lang_1_1vector__var__decl.html#a085037027feea63c181099623e90983f',1,'stan::lang::vector_var_decl::range_()'],['../structstan_1_1lang_1_1row__vector__var__decl.html#a5e4780586282073a0b04d6e602cf5fed',1,'stan::lang::row_vector_var_decl::range_()'],['../structstan_1_1lang_1_1matrix__var__decl.html#a5073235583b157d2bb417552ae4322b5',1,'stan::lang::matrix_var_decl::range_()'],['../structstan_1_1lang_1_1for__statement.html#a7f8f00a230f01a9942445a79dab2d544',1,'stan::lang::for_statement::range_()']]],
+  ['range_5fbrackets_5fdouble_5fr',['range_brackets_double_r',['../structstan_1_1lang_1_1var__decls__grammar.html#a3274c8f6f2f4a06ac958d3fed70295b7',1,'stan::lang::var_decls_grammar']]],
+  ['range_5fbrackets_5fint_5fr',['range_brackets_int_r',['../structstan_1_1lang_1_1var__decls__grammar.html#ae0380187a431f36a14872a5a5ba73177',1,'stan::lang::var_decls_grammar']]],
+  ['range_5fr',['range_r',['../structstan_1_1lang_1_1statement__grammar.html#a13d536dd73f8703c5cc739ee91550c16',1,'stan::lang::statement_grammar']]],
+  ['reject_5fstatement_5fr',['reject_statement_r',['../structstan_1_1lang_1_1statement__grammar.html#a7a2f7564fc3331749da3926e7417478c',1,'stan::lang::statement_grammar']]],
+  ['remove_5floop_5fidentifier_5ff',['remove_loop_identifier_f',['../statement__grammar__def_8hpp.html#acfb6eb8bf3f5d053cc7ca50697a5bd74',1,'statement_grammar_def.hpp']]],
+  ['reserved_5fword_5fset_5f',['reserved_word_set_',['../structvalidate__identifier.html#a2bef356e8d36b1102edf429b02063bae',1,'validate_identifier']]],
+  ['return_5fstatement_5fr',['return_statement_r',['../structstan_1_1lang_1_1statement__grammar.html#a0808e45a168fd8fa66f253cc5315c49b',1,'stan::lang::statement_grammar']]],
+  ['return_5ftype_5f',['return_type_',['../structstan_1_1lang_1_1returns__type__vis.html#a33d136122b2a795716355985abe52cff',1,'stan::lang::returns_type_vis::return_type_()'],['../structstan_1_1lang_1_1function__decl__def.html#a25c7c0ff47ac697962d6881223ccb781',1,'stan::lang::function_decl_def::return_type_()']]],
+  ['return_5fvalue_5f',['return_value_',['../structstan_1_1lang_1_1return__statement.html#acd9b3ae3b514962945c4b8a99823de3e',1,'stan::lang::return_statement']]],
+  ['right',['right',['../structstan_1_1lang_1_1binary__op.html#aa41b13102dec86da62ad6120593a8a95',1,'stan::lang::binary_op']]],
+  ['round_5fstyle',['round_style',['../structstd_1_1numeric__limits_3_01stan_1_1agrad_1_1fvar_3_01_t_01_4_01_4.html#a1c70d1cf58dba42f0bb1987810ae07a7',1,'std::numeric_limits&lt; stan::agrad::fvar&lt; T &gt; &gt;::round_style()'],['../structstd_1_1numeric__limits_3_01stan_1_1agrad_1_1var_01_4.html#a87a2f9fb70b542ca087f476622d51a57',1,'std::numeric_limits&lt; stan::agrad::var &gt;::round_style()']]],
+  ['row_5fvector_5fdecl_5fr',['row_vector_decl_r',['../structstan_1_1lang_1_1var__decls__grammar.html#a2ea4d536fc00f223d9ff3a7eb6c30348',1,'stan::lang::var_decls_grammar']]],
+  ['row_5fvector_5ft',['ROW_VECTOR_T',['../namespacestan_1_1lang.html#afa3a8b7a8c4ab9815be757a5ed109869',1,'stan::lang']]]
+];
