375382e9f5492b57a483b8680ec7bc0ae9eea7f8
Krzysztof Sakrejda <krzysztof.sakrejda@gmail.com>
2015-03-27 16:19:56 -0400

    linted sparse_extractors_csc, no meaningful changes.

--- src/stan/math/prim/mat/fun/sparse_extractors_csc.hpp
+++ src/stan/math/prim/mat/fun/sparse_extractors_csc.hpp
@@ -27,32 +27,33 @@ namespace stan {
      *  @{
      */
 
-    template <typename T> 
-    const Eigen::Matrix<T, Eigen::Dynamic,1> extract_w(Eigen::SparseMatrix<T> A) {
-      Eigen::Matrix<T,Eigen::Dynamic,1> w(A.nonZeros());
+    template <typename T>
+    const Eigen::Matrix<T, Eigen::Dynamic, 1>
+    extract_w(Eigen::SparseMatrix<T> A) {
+      Eigen::Matrix<T, Eigen::Dynamic, 1> w(A.nonZeros());
       w.setZero();
       for (int j = 0; j < A.nonZeros(); ++j)
         w[j] = *(A.valuePtr()+j);
       return w;
     }
-    
-    template <typename T> 
+
+    template <typename T>
     const std::vector<int> extract_v(Eigen::SparseMatrix<T> A) {
       std::vector<int> v(A.nonZeros());
       for (int j = 0; j < A.nonZeros(); ++j)
         v[j] = *(A.innerIndexPtr()+j) + 1;   // make 1-indexed
       return v;
     }
-    
-    template <typename T> 
+
+    template <typename T>
     const std::vector<int> extract_u(Eigen::SparseMatrix<T> A) {
       std::vector<int> u(A.outerSize()+1);
       for (int j = 0; j <= A.outerSize(); ++j)
-        u[j] = *(A.outerIndexPtr()+j) + 1; // make 1-indexed
+        u[j] = *(A.outerIndexPtr()+j) + 1;   // make 1-indexed
       return u;
     }
-    
-    template <typename T> 
+
+    template <typename T>
     const std::vector<int> extract_z(Eigen::SparseMatrix<T> A) {
       std::vector<int> u(A.outerSize()+1);
       std::vector<int> z(A.outerSize()+1);
@@ -61,8 +62,8 @@ namespace stan {
       z.erase(z.begin());
       return z;
     }
-    /** @} */ // end of sparse_csc group. 
-    
+    /** @} */   // end of sparse_csc group.
+
   }
 }
 
