ed68d88a9a15a378982912bc7660eb62cf47d56c
Krzysztof Sakrejda <krzysztof.sakrejda@gmail.com>
2015-03-27 16:32:00 -0400

    linted sparse_multiply_csc, no meaningful changes.

--- src/stan/math/prim/mat/fun/sparse_multiply_csc.hpp
+++ src/stan/math/prim/mat/fun/sparse_multiply_csc.hpp
@@ -36,31 +36,30 @@ namespace stan {
      *          the number of columns..
      * @return dense vector for the product.
      * @throw std::domain_error if m and n are not positive or are nan.
-     * @throw std::domain_error if the implied sparse matrix and b are 
+     * @throw std::domain_error if the implied sparse matrix and b are
      *                          not multiplicable.
      * @throw std::domain_error if m/n/w/v/u/z are not internally
      * consistent, as defined by the indexing scheme.  Extractors are
      * defined in Stan which guarantee a consistent set of m/n/w/v/u/z
-     * for a given sparse matrix.  
+     * for a given sparse matrix.
      */
     template <typename T1, typename T2>
     inline
     Eigen::Matrix<typename boost::math::tools::promote_args<T1, T2>::type, Eigen::Dynamic, 1>
     sparse_multiply_csc(const int& m,
                     const int& n,
-                    const Eigen::Matrix<T1, Eigen::Dynamic,1>& w,
+                    const Eigen::Matrix<T1, Eigen::Dynamic, 1>& w,
                     const std::vector<int>& v,
                     const std::vector<int>& u,
                     const std::vector<int>& z,
-                    const Eigen::Matrix<T2, Eigen::Dynamic,1>& b) {
-
-      stan::math::check_positive("sparse_multiply_csc","m",m);
-      stan::math::check_positive("sparse_multiply_csc","n",n);
-      stan::math::check_size_match("sparse_multiply_csc","n", n, "b", b.size()  );
-      stan::math::check_size_match("sparse_multiply_csc","n", n, "u", u.size()-1);
-      stan::math::check_size_match("sparse_multiply_csc","n", n, "z", z.size()  );
-      stan::math::check_size_match("sparse_multiply_csc","w", w.size(), "v", v.size());
-      stan::math::check_size_match("sparse_multiply_csc","u/z", u[n-1] + z[n-1]-1, "v", v.size());
+                    const Eigen::Matrix<T2, Eigen::Dynamic, 1>& b) {
+      stan::math::check_positive("sparse_multiply_csc", "m", m);
+      stan::math::check_positive("sparse_multiply_csc", "n", n);
+      stan::math::check_size_match("sparse_multiply_csc", "n", n, "b", b.size() );
+      stan::math::check_size_match("sparse_multiply_csc", "n", n, "u", u.size()-1);
+      stan::math::check_size_match("sparse_multiply_csc", "n", n, "z", z.size() );
+      stan::math::check_size_match("sparse_multiply_csc", "w", w.size(), "v", v.size());
+      stan::math::check_size_match("sparse_multiply_csc", "u/z", u[n-1] + z[n-1]-1, "v", v.size());
 
       typedef typename boost::math::tools::promote_args<T1, T2>::type fun_scalar_t;
       Eigen::Matrix<fun_scalar_t, Eigen::Dynamic, 1>  y(m);
