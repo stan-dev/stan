d8c3e1311570f367d60f6349649a45bad1603862
Rob Trangucci <robert.trangucci@gmail.com>
2015-04-28 17:45:23 -0400

    b f/i-1271 added correct headers so header test pass

--- src/stan/math/rev/mat/functor/finite_diff_grad_hessian.hpp
+++ src/stan/math/rev/mat/functor/finite_diff_grad_hessian.hpp
@@ -3,7 +3,7 @@
 
 #include <stan/math/prim/mat/fun/Eigen.hpp>
 #include <stan/math/rev/core.hpp>
-#include <stan/math/mix/mat/functor/gradient.hpp>
+#include <stan/math/mix/mat/functor/hessian.hpp>
 
 namespace stan {
   
--- src/stan/math/rev/mat/functor/finite_diff_hessian.hpp
+++ src/stan/math/rev/mat/functor/finite_diff_hessian.hpp
@@ -3,7 +3,7 @@
 
 #include <stan/math/prim/mat/fun/Eigen.hpp>
 #include <stan/math/rev/core.hpp>
-#include <stan/math/mix/mat/functor/gradient.hpp>
+#include <stan/math/rev/mat/functor/gradient.hpp>
 
 namespace stan {
   
