f91ba47957f763b38f7f2d7373b612304f8c5b7e
Daniel Lee <bearlee@alum.mit.edu>
2015-05-14 20:04:47 -0400

    split apart math/mix/mat/functor/jacobian.hpp into a rev and a fwd part

--- src/stan/model/util.hpp
+++ src/stan/model/util.hpp
@@ -14,7 +14,8 @@
 #include <stan/math/mix/mat/functor/gradient_dot_vector.hpp>
 #include <stan/math/mix/mat/functor/hessian.hpp>
 #include <stan/math/mix/mat/functor/hessian_times_vector.hpp>
-#include <stan/math/mix/mat/functor/jacobian.hpp>
+#include <stan/math/mix/fwd/functor/jacobian.hpp>
+#include <stan/math/mix/rev/functor/jacobian.hpp>
 #include <stan/math/mix/mat/functor/partial_derivative.hpp>
 
 #include <cmath>
