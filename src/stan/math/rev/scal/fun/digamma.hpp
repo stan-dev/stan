0219f38c0537caa83ff673c111a4eae8bd37bade
Daniel Lee <bearlee@alum.mit.edu>
2015-05-26 10:14:18 -0400

    updating log_rising_factorial for travis

--- src/stan/math/rev/scal/fun/digamma.hpp
+++ src/stan/math/rev/scal/fun/digamma.hpp
@@ -1,10 +1,10 @@
 #ifndef STAN_MATH_REV_SCAL_FUN_DIGAMMA_HPP
 #define STAN_MATH_REV_SCAL_FUN_DIGAMMA_HPP
 
-#include <boost/math/special_functions/digamma.hpp>
 #include <boost/math/special_functions/zeta.hpp>
 #include <stan/math/rev/core.hpp>
 #include <stan/math/prim/scal/fun/trigamma.hpp>
+#include <stan/math/prim/scal/fun/digamma.hpp>
 
 namespace stan {
   namespace math {
@@ -13,10 +13,10 @@ namespace stan {
       class digamma_vari : public op_v_vari {
       public:
         explicit digamma_vari(vari* avi) :
-          op_v_vari(boost::math::digamma(avi->val_), avi) {
+          op_v_vari(digamma(avi->val_), avi) {
         }
         void chain() {
-          avi_->adj_ += adj_ * stan::math::trigamma(avi_->val_);
+          avi_->adj_ += adj_ * trigamma(avi_->val_);
         }
       };
     }
