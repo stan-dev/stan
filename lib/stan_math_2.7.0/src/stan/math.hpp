6b16f9a9d2977b7fcac4bdb74a1063199e4fc8c3
Daniel Lee <bearlee@alum.mit.edu>
2015-06-03 23:21:22 -0400

    1465: updating version.hpp and including it in math.hpp

--- lib/stan_math_2.7.0/src/stan/math.hpp
+++ lib/stan_math_2.7.0/src/stan/math.hpp
@@ -1,6 +1,8 @@
 #ifndef STAN_MATH_HPP
 #define STAN_MATH_HPP
 
+#include <stan/math/version.hpp>
+
 #include <stan/math/prim/arr/meta/get.hpp>
 #include <stan/math/prim/arr/meta/index_type.hpp>
 #include <stan/math/prim/arr/meta/is_vector.hpp>
