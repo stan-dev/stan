0eecde6265619a974b5df76a2fe256782fbd36d6
Alp Kucukelbir <akucukelbir@gmail.com>
2015-06-10 18:06:11 -0400

    updated meanfield tests to match new notation

--- src/test/unit/variational/advi_params_meanfield_test.cpp
+++ src/test/unit/variational/advi_params_meanfield_test.cpp
@@ -8,13 +8,13 @@ TEST(advi_params_meanfield_test, dimension) {
   Eigen::Vector3d mu;
   mu << 5.7, -3.2, 0.1332;
 
-  Eigen::Vector3d sigma_tilde;
-  sigma_tilde << -0.42, 0.8922, 13.4;
+  Eigen::Vector3d omega;
+  omega << -0.42, 0.8922, 13.4;
 
-  stan::variational::advi_params_meanfield my_advi_params_meanfield(mu, sigma_tilde);
+  stan::variational::advi_params_meanfield my_advi_params_meanfield(mu, omega);
 
   EXPECT_FLOAT_EQ(mu.size(), my_advi_params_meanfield.dimension());
-  EXPECT_FLOAT_EQ(sigma_tilde.size(), my_advi_params_meanfield.dimension());
+  EXPECT_FLOAT_EQ(omega.size(), my_advi_params_meanfield.dimension());
 
 }
 
@@ -23,10 +23,10 @@ TEST(advi_params_meanfield_test, mean_vector) {
   Eigen::Vector3d mu;
   mu << 5.7, -3.2, 0.1332;
 
-  Eigen::Vector3d sigma_tilde;
-  sigma_tilde << -0.42, 0.8922, 13.4;
+  Eigen::Vector3d omega;
+  omega << -0.42, 0.8922, 13.4;
 
-  stan::variational::advi_params_meanfield my_advi_params_meanfield(mu, sigma_tilde);
+  stan::variational::advi_params_meanfield my_advi_params_meanfield(mu, omega);
 
   const Eigen::Vector3d& mu_out = my_advi_params_meanfield.mu();
 
@@ -35,35 +35,35 @@ TEST(advi_params_meanfield_test, mean_vector) {
 
   double nan = std::numeric_limits<double>::quiet_NaN();
   Eigen::Vector3d mu_nan = Eigen::VectorXd::Constant(3, nan);
-  EXPECT_THROW(stan::variational::advi_params_meanfield my_advi_params_meanfield_nan(mu_nan, sigma_tilde),
+  EXPECT_THROW(stan::variational::advi_params_meanfield my_advi_params_meanfield_nan(mu_nan, omega),
                    std::domain_error);
   EXPECT_THROW(my_advi_params_meanfield.set_mu(mu_nan),
                    std::domain_error);
-  Eigen::Vector3d sigma_tilde_nan = Eigen::VectorXd::Constant(3,nan);
-  EXPECT_THROW(stan::variational::advi_params_meanfield my_advi_params_meanfield_nan(mu, sigma_tilde_nan);,
+  Eigen::Vector3d omega_nan = Eigen::VectorXd::Constant(3,nan);
+  EXPECT_THROW(stan::variational::advi_params_meanfield my_advi_params_meanfield_nan(mu, omega_nan);,
                    std::domain_error);
 
 }
 
-TEST(advi_params_meanfield_test, sigma_tilde_vector) {
+TEST(advi_params_meanfield_test, omega_vector) {
 
   Eigen::Vector3d mu;
   mu << 5.7, -3.2, 0.1332;
 
-  Eigen::Vector3d sigma_tilde;
-  sigma_tilde << -0.42, 0.8922, 13.4;
+  Eigen::Vector3d omega;
+  omega << -0.42, 0.8922, 13.4;
 
-  stan::variational::advi_params_meanfield my_advi_params_meanfield(mu, sigma_tilde);
+  stan::variational::advi_params_meanfield my_advi_params_meanfield(mu, omega);
 
-  const Eigen::Vector3d& sigma_tilde_out = my_advi_params_meanfield.sigma_tilde();
+  const Eigen::Vector3d& omega_out = my_advi_params_meanfield.omega();
 
   for (int i = 0; i < my_advi_params_meanfield.dimension(); ++i)
-    EXPECT_FLOAT_EQ(sigma_tilde(i), sigma_tilde_out(i));
+    EXPECT_FLOAT_EQ(omega(i), omega_out(i));
 
   double nan = std::numeric_limits<double>::quiet_NaN();
-  Eigen::Vector3d sigma_tilde_nan = Eigen::VectorXd::Constant(3, nan);
+  Eigen::Vector3d omega_nan = Eigen::VectorXd::Constant(3, nan);
 
-  EXPECT_THROW(my_advi_params_meanfield.set_sigma_tilde(sigma_tilde_nan);,
+  EXPECT_THROW(my_advi_params_meanfield.set_omega(omega_nan);,
                    std::domain_error);
 
 }
@@ -73,12 +73,12 @@ TEST(advi_params_meanfield_test, entropy) {
   Eigen::Vector3d mu;
   mu << 5.7, -3.2, 0.1332;
 
-  Eigen::Vector3d sigma_tilde;
-  sigma_tilde << -0.42, 0.8922, 13.4;
+  Eigen::Vector3d omega;
+  omega << -0.42, 0.8922, 13.4;
 
   double entropy_true = 18.129015599614018;
 
-  stan::variational::advi_params_meanfield my_advi_params_meanfield(mu, sigma_tilde);
+  stan::variational::advi_params_meanfield my_advi_params_meanfield(mu, omega);
 
   const double entropy_out = my_advi_params_meanfield.entropy();
 
@@ -91,10 +91,10 @@ TEST(advi_params_meanfield_test, transform_loc_scale) {
   Eigen::Vector3d mu;
   mu << 5.7, -3.2, 0.1332;
 
-  Eigen::Vector3d sigma_tilde;
-  sigma_tilde << -0.42, 0.8922, 13.4;
+  Eigen::Vector3d omega;
+  omega << -0.42, 0.8922, 13.4;
 
-  stan::variational::advi_params_meanfield my_advi_params_meanfield(mu, sigma_tilde);
+  stan::variational::advi_params_meanfield my_advi_params_meanfield(mu, omega);
 
   Eigen::Vector3d x;
   x << 7.1, -9.2, 0.59;
