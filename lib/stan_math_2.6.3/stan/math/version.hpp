8acb74c3796773f9716056d8c9aeb33a872862b9
Daniel Lee <bearlee@alum.mit.edu>
2015-06-15 18:33:21 -0400

    1383: adds STAN_MAJOR, STAN_MINOR, STAN_PATCH defines

--- lib/stan_math_2.6.3/stan/math/version.hpp
+++ lib/stan_math_2.6.3/stan/math/version.hpp
@@ -2,18 +2,24 @@
 #define STAN_MATH_VERSION_HPP
 
 #include <string>
+#define STRING_EXPAND(s) #s
+#define STRING(s) STRING_EXPAND(s)
+
+#define STAN_MATH_MAJOR 2
+#define STAN_MATH_MINOR 6
+#define STAN_MATH_PATCH 3
 
 namespace stan {
   namespace math {
 
     /** Major version number for Stan math library. */
-    const std::string MAJOR_VERSION = "2";
+    const std::string MAJOR_VERSION = STRING(STAN_MATH_MAJOR);
 
     /** Minor version number for Stan math library. */
-    const std::string MINOR_VERSION = "6";
+    const std::string MINOR_VERSION = STRING(STAN_MATH_MINOR);
 
     /** Patch version for Stan math library. */
-    const std::string PATCH_VERSION = "3";
+    const std::string PATCH_VERSION = STRING(STAN_MATH_PATCH);
 
   }
 }
--- src/stan/version.hpp
+++ src/stan/version.hpp
@@ -2,17 +2,23 @@
 #define STAN_VERSION_HPP
 
 #include <string>
+#define STRING_EXPAND(s) #s
+#define STRING(s) STRING_EXPAND(s)
+
+#define STAN_MAJOR 2
+#define STAN_MINOR 6
+#define STAN_PATCH 3
 
 namespace stan {
 
   /** Major version number for Stan package. */
-  const std::string MAJOR_VERSION = "2";
+  const std::string MAJOR_VERSION = STRING(STAN_MAJOR);
 
   /** Minor version number for Stan package. */
-  const std::string MINOR_VERSION = "6";
+  const std::string MINOR_VERSION = STRING(STAN_MINOR);
 
   /** Patch version for Stan package. */
-  const std::string PATCH_VERSION = "3";
+  const std::string PATCH_VERSION = STRING(STAN_PATCH);
 
 }
 
