09bc84bb23b94dc89197b597150c3d96199528dc
Dustin Tran <dustinviettran@gmail.com>
2015-06-19 17:02:30 -0400

    fix hier_logistic* unit tests as full rank test makes no sense

--- src/test/unit/variational/hier_logistic_cp_test.cpp
+++ src/test/unit/variational/hier_logistic_cp_test.cpp
@@ -10,39 +10,6 @@
 typedef boost::ecuyer1988 rng_t;
 typedef hier_logistic_cp_model_namespace::hier_logistic_cp_model Model_cp;
 
-TEST(advi_test, hier_logistic_cp_constraint_fullrank) {
-
-  // Create mock data_var_context
-  std::fstream data_stream("src/test/test-models/good/variational/hier_logistic.data.R",
-                           std::fstream::in);
-  stan::io::dump data_var_context(data_stream);
-  data_stream.close();
-
-  // Instantiate model
-  Model_cp my_model(data_var_context);
-
-  // RNG
-  rng_t base_rng(0);
-
-  // Dummy input
-  Eigen::VectorXd cont_params = Eigen::VectorXd::Zero(my_model.num_params_r());
-
-  // ADVI
-  stan::variational::advi<Model_cp, rng_t> test_advi(my_model,
-                                                     cont_params,
-                                                     10,
-                                                     100,
-                                                     0.01,
-                                                     base_rng,
-                                                     100,
-                                                     1,
-                                                     &std::cout,
-                                                     &std::cout,
-                                                     &std::cout);
-
-  test_advi.run_fullrank(1,2e4);
-}
-
 TEST(advi_test, hier_logistic_cp_constraint_meanfield) {
 
   // Create mock data_var_context
--- src/test/unit/variational/hier_logistic_test.cpp
+++ src/test/unit/variational/hier_logistic_test.cpp
@@ -20,7 +20,7 @@ public:
 
     model_ = new stan_model(data_var_context, &model_stream_);
     model_null_stream_ = new stan_model(data_var_context, NULL);
-     
+
     base_rng_.seed(0);
     cont_params_ = Eigen::VectorXd::Zero(model_->num_params_r());
 
@@ -68,11 +68,6 @@ private:
   Eigen::VectorXd cont_params_;
 };
 
-
-TEST_F(advi_test, hier_logistic_constraint_fullrank) {
-  EXPECT_EQ(0, advi_->run_fullrank(1, 2e4));
-  SUCCEED() << "expecting it to compile and run without problems";
-}
 
 TEST_F(advi_test, hier_logistic_constraint_meanfield) {
   EXPECT_EQ(0, advi_->run_meanfield(1, 2e4));
