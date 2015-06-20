42e7acffe5b45d826792ed127adaf9cd990b5551
Dustin Tran <dustinviettran@gmail.com>
2015-06-19 22:58:57 -0400

    remove extraneous includes in base family class

--- src/stan/variational/base_family.hpp
+++ src/stan/variational/base_family.hpp
@@ -2,22 +2,7 @@
 #define STAN_VARIATIONAL_BASE_FAMILY_HPP
 
 #include <stan/math/prim/mat/fun/Eigen.hpp>
-#include <stan/math/prim/scal/fun/constants.hpp>
 
-#include <stan/math/prim/mat/meta/get.hpp>
-#include <stan/math/prim/arr/meta/get.hpp>
-#include <stan/math/prim/mat/meta/length.hpp>
-#include <stan/math/prim/mat/meta/is_vector.hpp>
-#include <stan/math/prim/mat/meta/is_vector_like.hpp>
-#include <stan/math/prim/mat/fun/value_of_rec.hpp>
-#include <stan/math/prim/scal/prob/normal_rng.hpp>
-
-#include <stan/math/prim/scal/err/check_size_match.hpp>
-#include <stan/math/prim/scal/err/check_not_nan.hpp>
-
-#include <stan/model/util.hpp>
-
-#include <vector>
 #include <ostream>
 
 namespace stan {
