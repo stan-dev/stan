3ac3271ba13ff3ec31f80f58723717f9c4bc61ce
Daniel Lee <bearlee@alum.mit.edu>
2015-07-09 16:14:29 -0400

    release/v2.7.0: adding built documentation. [skip ci]

---
+++ doc/api/html/search/functions_18.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['z',['z',['../classstan_1_1mcmc_1_1base__hmc.html#a7b9f7a04975626ce6ea23a81d903b325',1,'stan::mcmc::base_hmc']]]
+];
---
+++ doc/api/html/search/variables_d.js
@@ -0,0 +1,22 @@
+var searchData=
+[
+  ['n_5f',['N_',['../structstan_1_1lang_1_1row__vector__var__decl.html#aa528deec3a1386e08e70f6cca488ad32',1,'stan::lang::row_vector_var_decl::N_()'],['../structstan_1_1lang_1_1matrix__var__decl.html#a5a54ecdc72a4ce9612cf3e10091dae08',1,'stan::lang::matrix_var_decl::N_()'],['../structstan_1_1lang_1_1cholesky__factor__var__decl.html#ad6eabfcb6ac94df9a2fcec801d72fe8a',1,'stan::lang::cholesky_factor_var_decl::N_()']]],
+  ['n_5fdivergent_5f',['n_divergent_',['../classstan_1_1mcmc_1_1base__nuts.html#a5c6b41298abef165cacfabaa52603697',1,'stan::mcmc::base_nuts']]],
+  ['n_5fleapfrog_5f',['n_leapfrog_',['../classstan_1_1mcmc_1_1base__nuts.html#aef245324c1478b7802c504a5cb3b12cc',1,'stan::mcmc::base_nuts']]],
+  ['n_5fmonte_5fcarlo_5felbo_5f',['n_monte_carlo_elbo_',['../classstan_1_1variational_1_1advi.html#a99063ec199690d45c68c0fb850e1538e',1,'stan::variational::advi']]],
+  ['n_5fmonte_5fcarlo_5fgrad_5f',['n_monte_carlo_grad_',['../classstan_1_1variational_1_1advi.html#ab5b3aaf4942bbe4c06940139c3230622',1,'stan::variational::advi']]],
+  ['n_5fposterior_5fsamples_5f',['n_posterior_samples_',['../classstan_1_1variational_1_1advi.html#ac8930832c92fd401153ba79e87f46cb6',1,'stan::variational::advi']]],
+  ['n_5ftree',['n_tree',['../structstan_1_1mcmc_1_1nuts__util.html#ad3f10003284b647aa8a6d246e8d73935',1,'stan::mcmc::nuts_util']]],
+  ['name_5f',['name_',['../structstan_1_1lang_1_1variable__dims.html#a22f4ab70129ba1cbdfce999e6c6d358a',1,'stan::lang::variable_dims::name_()'],['../structstan_1_1lang_1_1variable.html#adf8d3615d62bc00166e69b94716121c0',1,'stan::lang::variable::name_()'],['../structstan_1_1lang_1_1fun.html#ae09c764d77a21c4bc199780adecf1e5e',1,'stan::lang::fun::name_()'],['../structstan_1_1lang_1_1base__var__decl.html#a0f8381c9dfc78043ee7a680d1c4be95a',1,'stan::lang::base_var_decl::name_()'],['../structstan_1_1lang_1_1arg__decl.html#aa8efc56a255324ca4288264b1df0b92c',1,'stan::lang::arg_decl::name_()'],['../structstan_1_1lang_1_1function__decl__def.html#a9b6f1c522ee5785f58934cdf71cf67cf',1,'stan::lang::function_decl_def::name_()'],['../classstan_1_1mcmc_1_1base__mcmc.html#a9110642e8056b647e867c753d60c33b8',1,'stan::mcmc::base_mcmc::name_()']]],
+  ['negate_5fexpr_5ff',['negate_expr_f',['../term__grammar__def_8hpp.html#aa06303059b7907e535e013b9c2271f2d',1,'term_grammar_def.hpp']]],
+  ['negated_5ffactor_5fr',['negated_factor_r',['../structstan_1_1lang_1_1term__grammar.html#a556d8dc8e6e71146a4be9cf592855aaf',1,'stan::lang::term_grammar']]],
+  ['next_5fchar_5f',['next_char_',['../json__parser_8hpp.html#a2c466121d68883000109a7dfa98ac135',1,'json_parser.hpp']]],
+  ['no_5fop_5fstatement_5fr',['no_op_statement_r',['../structstan_1_1lang_1_1statement__grammar.html#a5207e612570ecc27bb3a42bb9b144833',1,'stan::lang::statement_grammar']]],
+  ['nom_5fepsilon_5f',['nom_epsilon_',['../classstan_1_1mcmc_1_1base__hmc.html#a5ffc686e8b7feaaa5d3cbcb7c562acf2',1,'stan::mcmc::base_hmc']]],
+  ['non_5flvalue_5fassign_5fr',['non_lvalue_assign_r',['../structstan_1_1lang_1_1statement__grammar.html#af9d67d0e408fee77ea6e9335973a8dfd',1,'stan::lang::statement_grammar']]],
+  ['num_5fdims_5f',['num_dims_',['../structstan_1_1lang_1_1expr__type.html#af052df5946b67118d5b70751a6d5975e',1,'stan::lang::expr_type']]],
+  ['num_5fparams_5fr_5f_5f',['num_params_r__',['../classstan_1_1model_1_1prob__grad.html#a26e7379db0cea5dc64fa8c505f4edfda',1,'stan::model::prob_grad']]],
+  ['num_5fsamples',['num_samples',['../structstan_1_1io_1_1stan__csv__metadata.html#ab9d717fdea1c34f826112cd197ca530b',1,'stan::io::stan_csv_metadata']]],
+  ['num_5fwarmup',['num_warmup',['../structstan_1_1io_1_1stan__csv__metadata.html#a220bc990428b915e65cebf3877658008',1,'stan::io::stan_csv_metadata']]],
+  ['num_5fwarmup_5f',['num_warmup_',['../classstan_1_1mcmc_1_1windowed__adaptation.html#ab19c37c91cbb31bd53a0c37c05674d4b',1,'stan::mcmc::windowed_adaptation']]]
+];
