46fd2fc7e31bd9edd3e9dc738b2e26adbc90fbe1
Daniel Lee <bearlee@alum.mit.edu>
2015-07-09 16:14:29 -0400

    release/v2.7.0: adding built documentation. [skip ci]

---
+++ doc/api/html/search/classes_c.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['ordered_5fvar_5fdecl',['ordered_var_decl',['../structstan_1_1lang_1_1ordered__var__decl.html',1,'stan::lang']]]
+];
---
+++ doc/api/html/search/variables_18.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['y0_5f',['y0_',['../structstan_1_1lang_1_1integrate__ode.html#a1a4a60c8e050dc311ef8095667b61ff6',1,'stan::lang::integrate_ode']]]
+];
