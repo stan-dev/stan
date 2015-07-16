f0546e9aa3d7cb3b619a43782d4e715d34cb3254
Daniel Lee <bearlee@alum.mit.edu>
2015-07-15 21:54:43 -0400

    doc should have been removed on develop with pr #1535

--- doc/api/html/search/typedefs_c.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['var_5fcontext_5ft',['var_context_t',['../classstan_1_1interface_1_1var__context__factory_1_1var__context__factory.html#a5a6b1d0caa55ba1fc2ac13a5e4ccc86b',1,'stan::interface::var_context_factory::var_context_factory']]],
-  ['var_5fdecl_5ft',['var_decl_t',['../structstan_1_1lang_1_1var__decl.html#a2162444ce039a925398e154d5ab942be',1,'stan::lang::var_decl']]],
-  ['var_5forigin',['var_origin',['../namespacestan_1_1lang.html#a06a1646fb0d80ad71b2af32080829f39',1,'stan::lang']]],
-  ['vector_5fd',['vector_d',['../namespacestan_1_1optimization.html#af557a7d4dc0f32c569400f2b71c76d1d',1,'stan::optimization']]],
-  ['vector_5ft',['vector_t',['../classstan_1_1io_1_1reader.html#a2cf175423e2fcc4e9a4563c128e41dd0',1,'stan::io::reader::vector_t()'],['../classstan_1_1io_1_1writer.html#a6b2ba499dbbd7598173d87a3dec30962',1,'stan::io::writer::vector_t()'],['../classstan_1_1optimization_1_1_b_f_g_s_line_search.html#ad01e0e9cd97697cf24485d5ba7246145',1,'stan::optimization::BFGSLineSearch::vector_t()']]],
-  ['vectort',['VectorT',['../classstan_1_1optimization_1_1_b_f_g_s_minimizer.html#a72bd900faf26086e7a0487024534357c',1,'stan::optimization::BFGSMinimizer::VectorT()'],['../classstan_1_1optimization_1_1_b_f_g_s_update___h_inv.html#a32ef5a77bb851e667f20c388eff64c9c',1,'stan::optimization::BFGSUpdate_HInv::VectorT()'],['../classstan_1_1optimization_1_1_l_b_f_g_s_update.html#a658b4aa0ae1a5638831fc3ae7638e9d2',1,'stan::optimization::LBFGSUpdate::VectorT()']]]
-];
