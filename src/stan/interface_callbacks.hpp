7bab3905e3cde43bea77ff57086b4a10f96d83a3
Daniel Lee <bearlee@alum.mit.edu>
2015-07-21 11:22:02 -0400

    removing folder-level include

--- src/stan/interface_callbacks.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef STAN_INTERFACE_CALLBACKS_HPP
-#define STAN_INTERFACE_CALLBACKS_HPP
-
-#include <stan/interface_callbacks/interrupt.hpp>
-#include <stan/interface_callbacks/writer.hpp>
-#include <stan/interface_callbacks/var_context_factory.hpp>
-
-#endif
