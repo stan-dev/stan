5595cda20ef55ff25177cd15de7a4f7a4c75b1c3
Daniel Lee <bearlee@alum.mit.edu>
2015-09-08 15:47:17 -0400

    Removing built documentation. [skip ci]

--- doc/api/html/search/functions_17.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['x',['x',['../classstan_1_1interface__callbacks_1_1writer_1_1filtered__values.html#a8921a5a09b386e37ea74582699d121ac',1,'stan::interface_callbacks::writer::filtered_values::x()'],['../classstan_1_1interface__callbacks_1_1writer_1_1values.html#a3d99e017b411c0f264210283c96ae268',1,'stan::interface_callbacks::writer::values::x()']]]
-];
--- doc/api/html/search/functions_9.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['json_5fdata',['json_data',['../classstan_1_1json_1_1json__data.html#ac682d748655db52c1acd8401313c531b',1,'stan::json::json_data']]],
-  ['json_5fdata_5fhandler',['json_data_handler',['../classstan_1_1json_1_1json__data__handler.html#abecaf727e1c8d563631e773e605b8630',1,'stan::json::json_data_handler']]],
-  ['json_5ferror',['json_error',['../structstan_1_1json_1_1json__error.html#ab60ced8169137cb3494c7037bddd0f31',1,'stan::json::json_error']]],
-  ['json_5fhandler',['json_handler',['../classstan_1_1json_1_1json__handler.html#acc397de37df44db1f54dc3b5e1d2a5da',1,'stan::json::json_handler']]]
-];
