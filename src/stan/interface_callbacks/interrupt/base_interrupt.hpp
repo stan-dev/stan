832650b45a73ebdcaa2e86270a19c925d1d785b2
Daniel Lee <bearlee@alum.mit.edu>
2015-11-06 23:39:54 -0500

    Cutting down file diffs

--- src/stan/interface_callbacks/interrupt/base_interrupt.hpp
+++ src/stan/interface_callbacks/interrupt/base_interrupt.hpp
@@ -5,13 +5,12 @@ namespace stan {
   namespace interface_callbacks {
     namespace interrupt {
 
-      // This callback allows the interfaces to interrupt
-      // execution at various points in command.hpp
       class base_interrupt {
       public:
         base_interrupt() {}
         virtual void operator()() = 0;
       };
+
     }
   }
 }
