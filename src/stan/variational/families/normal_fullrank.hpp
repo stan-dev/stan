e18df661d3bfba126e1951d521b5b5c4c2918e1c
Daniel Lee <bearlee@alum.mit.edu>
2015-11-20 10:05:29 -0500

    Changing 'x += 1' to '++x'

--- src/stan/variational/families/normal_fullrank.hpp
+++ src/stan/variational/families/normal_fullrank.hpp
@@ -453,7 +453,7 @@ namespace stan {
             }
             ++i;
           } catch (const std::exception& e) {
-            n_monte_carlo_drop += 1;
+            ++n_monte_carlo_drop;
             if (n_monte_carlo_drop >= n_retries * n_monte_carlo_grad) {
               const char* name = "The number of dropped evaluations";
               const char* msg1 = "has reached its maximum amount (";
