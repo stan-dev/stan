79ba995b54880656aa332815a222cea6993d9c3d
Daniel Lee <bearlee@alum.mit.edu>
2015-12-04 00:11:20 -0500

    Removing built documentation. [skip ci]

--- doc/api/html/search/typedefs_9.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['statement_5ft',['statement_t',['../structstan_1_1lang_1_1statement.html#abcc625ea4afba252f4535bc94008c5d7',1,'stan::lang::statement']]],
-  ['string_5fargument',['string_argument',['../namespacestan_1_1services.html#a626d2f28004f94b43f69592034613b45',1,'stan::services']]]
-];
