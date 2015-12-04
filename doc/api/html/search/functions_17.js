dfcfe4a84ca9aa2f4bf747f949234eb2aff53e4a
Daniel Lee <bearlee@alum.mit.edu>
2015-12-03 23:16:35 -0500

    release/v2.9.0: adding built documentation. [skip ci]

---
+++ doc/api/html/search/functions_17.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['z',['z',['../classstan_1_1mcmc_1_1base__hmc.html#a300d6dbc5ffe21d81c3a92ab67036324',1,'stan::mcmc::base_hmc']]]
+];
---
+++ doc/api/html/search/typedefs_6.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['map_5fmatrix_5ft',['map_matrix_t',['../classstan_1_1io_1_1reader.html#a05f161f2ef596f356d249afbb0b43ca6',1,'stan::io::reader']]],
+  ['map_5frow_5fvector_5ft',['map_row_vector_t',['../classstan_1_1io_1_1reader.html#a71946c44faafcf4669f3cfde52816633',1,'stan::io::reader']]],
+  ['map_5fvector_5ft',['map_vector_t',['../classstan_1_1io_1_1reader.html#a1285611f704d4a3ec0d0e10ff98206d4',1,'stan::io::reader']]],
+  ['matrix_5fd',['matrix_d',['../namespacestan_1_1optimization.html#afd985f1c123f049985ad18ad074474c4',1,'stan::optimization']]],
+  ['matrix_5ft',['matrix_t',['../classstan_1_1io_1_1reader.html#a96a6c8c011e0876210058762dddc2e2a',1,'stan::io::reader::matrix_t()'],['../classstan_1_1io_1_1writer.html#a52ae49aa8033be51e09208265e041680',1,'stan::io::writer::matrix_t()']]]
+];
