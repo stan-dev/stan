c8f3520a30e6924a1e2a6bbca39636ea37c6ebe7
Daniel Lee <bearlee@alum.mit.edu>
2015-12-16 19:54:57 -0500

    Updating write_stan to use writers

--- src/stan/services/io/write_stan.hpp
+++ src/stan/services/io/write_stan.hpp
@@ -1,28 +1,22 @@
 #ifndef STAN_SERVICES_IO_WRITE_STAN_HPP
 #define STAN_SERVICES_IO_WRITE_STAN_HPP
 
+#include <stan/interface_callbacks/writer/base_writer.hpp>
 #include <stan/version.hpp>
-#include <ostream>
 #include <string>
 
 namespace stan {
   namespace services {
     namespace io {
 
-      void write_stan(std::ostream* s, const std::string prefix = "") {
-        if (!s)
-          return;
-
-        *s << prefix
-           << " stan_version_major = " << stan::MAJOR_VERSION << std::endl;
-        *s << prefix
-           << " stan_version_minor = " << stan::MINOR_VERSION << std::endl;
-        *s << prefix
-           << " stan_version_patch = " << stan::PATCH_VERSION << std::endl;
+      void write_stan(interface_callbacks::writer::base_writer& writer,
+                      const std::string prefix = "") {
+        writer(prefix + " stan_version_major = " + stan::MAJOR_VERSION);
+        writer(prefix + " stan_version_minor = " + stan::MINOR_VERSION);
+        writer(prefix + " stan_version_patch = " + stan::PATCH_VERSION);
       }
 
     }
   }
 }
-
 #endif
