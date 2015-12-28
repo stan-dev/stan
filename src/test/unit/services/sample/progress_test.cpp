e5ec8a318d67eaee2db2eceabe03af7c6336e364
Daniel Lee <bearlee@alum.mit.edu>
2015-12-28 15:55:48 -0500

    Updating stan::services::sample::progress()

--- src/test/unit/services/sample/progress_test.cpp
+++ src/test/unit/services/sample/progress_test.cpp
@@ -3,43 +3,30 @@
 #include <sstream>
 
 TEST(StanUi, progress) {
-  std::stringstream out;
   int m;
   int start;
   int finish;
   int refresh;
   bool warmup;
-  std::string prefix;
-  std::string suffix;
+  std::string out;
 
   start = 0;
   finish = 100;
   refresh = 1;
   warmup = true;
-  prefix = "";
-  suffix = "";
 
-  out.str("");
   m = 0;
-  stan::services::sample::progress(m, start, finish, refresh, warmup,
-                                   prefix, suffix, out);
-  EXPECT_EQ("Iteration:  1 / 100 [  1%]  (Warmup)", out.str());
+  out = stan::services::sample::progress(m, start, finish, refresh, warmup);
+  EXPECT_EQ("Iteration:  1 / 100 [  1%]  (Warmup)", out);
 
-  out.str("");
   m = 1;
-  prefix = "\r";
-  suffix = "\n";
-  stan::services::sample::progress(m, start, finish, refresh, warmup,
-                                   prefix, suffix, out);
-  EXPECT_EQ("\rIteration:  2 / 100 [  2%]  (Warmup)\n", out.str());
+  out = stan::services::sample::progress(m, start, finish, refresh, warmup);
 
-  out.str("");
+  EXPECT_EQ("Iteration:  2 / 100 [  2%]  (Warmup)", out);
+
   m = 2;
   warmup = false;
-  prefix = "";
-  suffix = "";
-  stan::services::sample::progress(m, start, finish, refresh, warmup,
-                                   prefix, suffix, out);
-  EXPECT_EQ("Iteration:  3 / 100 [  3%]  (Sampling)", out.str());
+  out = stan::services::sample::progress(m, start, finish, refresh, warmup);
+  EXPECT_EQ("Iteration:  3 / 100 [  3%]  (Sampling)", out);
 }
 
