9815159cb036907f8f520c7767e61f7ebf83fc77
Daniel Lee <bearlee@alum.mit.edu>
2016-01-22 15:56:37 -0500

    Updating test for check_timing

--- src/test/unit/services/check_timing_test.cpp
+++ src/test/unit/services/check_timing_test.cpp
@@ -4,15 +4,21 @@
 
 class mock_model {
 public:
+  mutable int called;
+
+  mock_model()
+    : called(0) { }
+  
   template <bool propto__, bool jacobian__, typename T__>
   T__ log_prob(Eigen::Matrix<T__, Eigen::Dynamic, 1>& x,
                std::ostream* pstream__ = 0) const {
     T__ lp__(0.0);
+    called++;
     return lp__;
   }
 
   int num_params_r() {
-    return 2;
+    return 4;
   }
 };
 
@@ -26,4 +32,6 @@ TEST(ServicesCheckTiming, output) {
   
   stan::services::check_timing(model, cont_params, writer);
   EXPECT_TRUE(ss.str().find("Gradient evaluation took") != std::string::npos);
+
+  EXPECT_EQ(1, model.called);
 }
