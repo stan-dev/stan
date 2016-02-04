7396c7ac67d2dfd532d1423b02306047644d1753
Bob Carpenter <carp@alias-i.com>
2016-02-04 18:20:47 -0500

    fixes #1754 allowing empty indexes

--- src/stan/model/indexing.hpp
+++ src/stan/model/indexing.hpp
@@ -1,6 +1,7 @@
 #ifndef STAN_MODEL_INDEXING_HPP
 #define STAN_MODEL_INDEXING_HPP
 
+#include <stan/model/indexing/deep_copy.hpp>
 #include <stan/model/indexing/index.hpp>
 #include <stan/model/indexing/index_list.hpp>
 #include <stan/model/indexing/lvalue.hpp>
