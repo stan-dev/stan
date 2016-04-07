be57f4600de1342e5559807c7080d4f4ee008718
betanalpha <betanalpha@gmail.com>
2016-04-07 12:38:06 +0100

    Add classic NUTS with original implementation

---
+++ src/test/unit/mcmc/hmc/derived_nuts_classic_test.cpp
@@ -0,0 +1,120 @@
+#include <gtest/gtest.h>
+
+#include <stan/interface_callbacks/writer/noop_writer.hpp>
+#include <stan/mcmc/hmc/hamiltonians/unit_e_point.hpp>
+#include <stan/mcmc/hmc/hamiltonians/diag_e_point.hpp>
+#include <stan/mcmc/hmc/hamiltonians/dense_e_point.hpp>
+#include <stan/mcmc/hmc/nuts_classic/unit_e_nuts_classic.hpp>
+#include <stan/mcmc/hmc/nuts_classic/diag_e_nuts_classic.hpp>
+#include <stan/mcmc/hmc/nuts_classic/dense_e_nuts_classic.hpp>
+#include <test/unit/mcmc/hmc/mock_hmc.hpp>
+#include <boost/random/additive_combine.hpp>
+
+typedef boost::ecuyer1988 rng_t;
+
+TEST(McmcDerivedNutsClassic, compute_criterion_unit_e) {
+
+  rng_t base_rng(0);
+
+  int model_size = 1;
+
+  stan::mcmc::ps_point start(model_size);
+  stan::mcmc::unit_e_point finish(model_size);
+  Eigen::VectorXd rho(model_size);
+
+  stan::mcmc::mock_model model(model_size);
+  stan::mcmc::unit_e_nuts_classic<stan::mcmc::mock_model, rng_t>
+    sampler(model, base_rng);
+
+  start.q(0) = 1;
+  start.p(0) = 1;
+
+  finish.q(0) = 2;
+  finish.p(0) = 1;
+
+  rho = start.p + finish.p;
+
+  EXPECT_EQ(true, sampler.compute_criterion(start, finish, rho));
+
+  start.q(0) = 1;
+  start.p(0) = 1;
+
+  finish.q(0) = 2;
+  finish.p(0) = -1;
+
+  rho = start.p + finish.p;
+
+  EXPECT_FALSE(sampler.compute_criterion(start, finish, rho));
+
+}
+
+TEST(McmcDerivedNutsClassic, compute_criterion_diag_e) {
+
+  rng_t base_rng(0);
+
+  int model_size = 1;
+
+  stan::mcmc::ps_point start(model_size);
+  stan::mcmc::diag_e_point finish(model_size);
+  Eigen::VectorXd rho(model_size);
+
+  stan::mcmc::mock_model model(model_size);
+  stan::mcmc::diag_e_nuts_classic<stan::mcmc::mock_model, rng_t>
+    sampler(model, base_rng);
+
+  start.q(0) = 1;
+  start.p(0) = 1;
+
+  finish.q(0) = 2;
+  finish.p(0) = 1;
+
+  rho = start.p + finish.p;
+
+  EXPECT_EQ(true, sampler.compute_criterion(start, finish, rho));
+
+  start.q(0) = 1;
+  start.p(0) = 1;
+
+  finish.q(0) = 2;
+  finish.p(0) = -1;
+
+  rho = start.p + finish.p;
+
+  EXPECT_FALSE(sampler.compute_criterion(start, finish, rho));
+}
+
+TEST(McmcDerivedNutsClassic, compute_criterion_dense_e) {
+
+  rng_t base_rng(0);
+
+  int model_size = 1;
+
+  stan::mcmc::ps_point start(model_size);
+  stan::mcmc::dense_e_point finish(model_size);
+  Eigen::VectorXd rho(model_size);
+
+  stan::mcmc::mock_model model(model_size);
+  stan::mcmc::dense_e_nuts_classic<stan::mcmc::mock_model, rng_t>
+    sampler(model, base_rng);
+
+  start.q(0) = 1;
+  start.p(0) = 1;
+
+  finish.q(0) = 2;
+  finish.p(0) = 1;
+
+  rho = start.p + finish.p;
+
+  EXPECT_EQ(true, sampler.compute_criterion(start, finish, rho));
+
+  start.q(0) = 1;
+  start.p(0) = 1;
+
+  finish.q(0) = 2;
+  finish.p(0) = -1;
+
+  rho = start.p + finish.p;
+
+  EXPECT_FALSE(sampler.compute_criterion(start, finish, rho));
+
+}
