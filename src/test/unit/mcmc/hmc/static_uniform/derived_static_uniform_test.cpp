0b7bdd9cf41b5176996542f7f297e31f364b7a6b
betanalpha <betanalpha@gmail.com>
2016-04-14 11:37:53 +0100

    Riemannian Static Uniform

--- src/test/unit/mcmc/hmc/static_uniform/derived_static_uniform_test.cpp
+++ src/test/unit/mcmc/hmc/static_uniform/derived_static_uniform_test.cpp
@@ -1,9 +1,11 @@
 #include <stan/mcmc/hmc/static_uniform/unit_e_static_uniform.hpp>
 #include <stan/mcmc/hmc/static_uniform/diag_e_static_uniform.hpp>
 #include <stan/mcmc/hmc/static_uniform/dense_e_static_uniform.hpp>
+#include <stan/mcmc/hmc/static_uniform/softabs_static_uniform.hpp>
 #include <stan/mcmc/hmc/static_uniform/adapt_unit_e_static_uniform.hpp>
 #include <stan/mcmc/hmc/static_uniform/adapt_diag_e_static_uniform.hpp>
 #include <stan/mcmc/hmc/static_uniform/adapt_dense_e_static_uniform.hpp>
+#include <stan/mcmc/hmc/static_uniform/adapt_softabs_static_uniform.hpp>
 #include <stan/interface_callbacks/writer/base_writer.hpp>
 #include <stan/interface_callbacks/writer/stream_writer.hpp>
 #include <test/test-models/good/mcmc/hmc/common/gauss.hpp>
@@ -12,7 +14,7 @@
 
 typedef boost::ecuyer1988 rng_t;
 
-TEST(McmcStaticUniformUnitE, transition) {
+TEST(McmcStaticUniform, unit_e_transition) {
   rng_t base_rng(4839294);
 
   stan::mcmc::unit_e_point z_init(1);
@@ -45,7 +47,7 @@ TEST(McmcStaticUniformUnitE, transition) {
   EXPECT_EQ("", output.str());
 }
 
-TEST(McmcStaticUniformDiagE, transition) {
+TEST(McmcStaticUniform, diag_e_transition) {
   rng_t base_rng(4839294);
 
   stan::mcmc::diag_e_point z_init(1);
@@ -78,7 +80,7 @@ TEST(McmcStaticUniformDiagE, transition) {
   EXPECT_EQ("", output.str());
 }
 
-TEST(McmcStaticUniformDenseE, transition) {
+TEST(McmcStaticUniform, dense_e_transition) {
   rng_t base_rng(4839294);
 
   stan::mcmc::dense_e_point z_init(1);
@@ -111,7 +113,40 @@ TEST(McmcStaticUniformDenseE, transition) {
   EXPECT_EQ("", output.str());
 }
 
-TEST(McmcAdaptStaticUniformUnitE, transition) {
+TEST(McmcStaticUniform, softabs_transition) {
+  rng_t base_rng(4839294);
+
+  stan::mcmc::softabs_point z_init(1);
+  z_init.q(0) = 1;
+  z_init.p(0) = -1;
+
+  std::stringstream output;
+  stan::interface_callbacks::writer::stream_writer writer(output);
+
+  std::fstream empty_stream("", std::fstream::in);
+  stan::io::dump data_var_context(empty_stream);
+  gauss_model_namespace::gauss_model model(data_var_context);
+
+  stan::mcmc::softabs_static_uniform<gauss_model_namespace::gauss_model, rng_t>
+    sampler(model, base_rng);
+
+  sampler.z() = z_init;
+  sampler.init_hamiltonian(writer);
+  sampler.set_nominal_stepsize(0.1);
+  sampler.set_stepsize_jitter(0);
+  sampler.sample_stepsize();
+
+  stan::mcmc::sample init_sample(z_init.q, 0, 0);
+
+  stan::mcmc::sample s = sampler.transition(init_sample, writer);
+
+  EXPECT_FLOAT_EQ(0.37006485, s.cont_params()(0));
+  EXPECT_FLOAT_EQ(-0.068473995, s.log_prob());
+  EXPECT_FLOAT_EQ(0.9999119, s.accept_stat());
+  EXPECT_EQ("", output.str());
+}
+
+TEST(McmcStaticUniform, adapt_unit_e_transition) {
   rng_t base_rng(4839294);
 
   stan::mcmc::unit_e_point z_init(1);
@@ -144,7 +179,7 @@ TEST(McmcAdaptStaticUniformUnitE, transition) {
   EXPECT_EQ("", output.str());
 }
 
-TEST(McmcAdaptStaticUniformDiagE, transition) {
+TEST(McmcStaticUniform, adapt_diag_e_transition) {
   rng_t base_rng(4839294);
 
   stan::mcmc::diag_e_point z_init(1);
@@ -177,7 +212,7 @@ TEST(McmcAdaptStaticUniformDiagE, transition) {
   EXPECT_EQ("", output.str());
 }
 
-TEST(McmcAdaptStaticUniformDenseE, transition) {
+TEST(McmcStaticUniform, adapt_dense_e_transition) {
   rng_t base_rng(4839294);
 
   stan::mcmc::dense_e_point z_init(1);
@@ -207,5 +242,38 @@ TEST(McmcAdaptStaticUniformDenseE, transition) {
   EXPECT_FLOAT_EQ(0.27224374, s.cont_params()(0));
   EXPECT_FLOAT_EQ(-0.037058324, s.log_prob());
   EXPECT_FLOAT_EQ(0.9998666, s.accept_stat());
+  EXPECT_EQ("", output.str());
+}
+
+TEST(McmcStaticUniform, adapt_softabs_e_transition) {
+  rng_t base_rng(4839294);
+
+  stan::mcmc::softabs_point z_init(1);
+  z_init.q(0) = 1;
+  z_init.p(0) = -1;
+
+  std::stringstream output;
+  stan::interface_callbacks::writer::stream_writer writer(output);
+
+  std::fstream empty_stream("", std::fstream::in);
+  stan::io::dump data_var_context(empty_stream);
+  gauss_model_namespace::gauss_model model(data_var_context);
+
+  stan::mcmc::adapt_softabs_static_uniform<gauss_model_namespace::gauss_model, rng_t>
+    sampler(model, base_rng);
+
+  sampler.z() = z_init;
+  sampler.init_hamiltonian(writer);
+  sampler.set_nominal_stepsize(0.1);
+  sampler.set_stepsize_jitter(0);
+  sampler.sample_stepsize();
+
+  stan::mcmc::sample init_sample(z_init.q, 0, 0);
+
+  stan::mcmc::sample s = sampler.transition(init_sample, writer);
+
+  EXPECT_FLOAT_EQ(0.37006485, s.cont_params()(0));
+  EXPECT_FLOAT_EQ(-0.068473995, s.log_prob());
+  EXPECT_FLOAT_EQ(0.9999119, s.accept_stat());
   EXPECT_EQ("", output.str());
 }
