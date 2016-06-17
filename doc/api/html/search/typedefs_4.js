65222b11c64bf662214c45c36245163fb92ca2df
Daniel Lee <bearlee@alum.mit.edu>
2016-06-17 11:05:01 -0400

    Removing built documentation.

--- doc/api/html/search/typedefs_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['hessiant',['HessianT',['../classstan_1_1optimization_1_1_b_f_g_s_minimizer.html#af846c77187ca9795a19523ce82b98e34',1,'stan::optimization::BFGSMinimizer::HessianT()'],['../classstan_1_1optimization_1_1_b_f_g_s_update___h_inv.html#a49438a3dcbd398798522fb125f3f0b33',1,'stan::optimization::BFGSUpdate_HInv::HessianT()'],['../classstan_1_1optimization_1_1_l_b_f_g_s_update.html#a154f710ca055b8ac22cc68ca70f9a67a',1,'stan::optimization::LBFGSUpdate::HessianT()']]]
-];
