b47407d4b31ac638a80e65442f025357a97c8e8f
Daniel Lee <bearlee@alum.mit.edu>
2016-09-06 22:03:05 -0400

    release/v2.12.0: updating version numbers.

--- src/test/unit/version_test.cpp
+++ src/test/unit/version_test.cpp
@@ -3,12 +3,12 @@
 
 TEST(Stan, macro) {
   EXPECT_EQ(2, STAN_MAJOR);
-  EXPECT_EQ(11, STAN_MINOR);
+  EXPECT_EQ(12, STAN_MINOR);
   EXPECT_EQ(0, STAN_PATCH);
 }
 
 TEST(Stan, version) {
   EXPECT_EQ("2", stan::MAJOR_VERSION);
-  EXPECT_EQ("11", stan::MINOR_VERSION);
+  EXPECT_EQ("12", stan::MINOR_VERSION);
   EXPECT_EQ("0", stan::PATCH_VERSION);
 }
