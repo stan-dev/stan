b361edccf350ba307000a74f8cbd6ff1dd78e9df
Daniel Lee <bearlee@alum.mit.edu>
2016-09-06 22:06:59 -0400

    Removing built documentation.

--- doc/api/html/search/variables_19.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['y0_5f',['y0_',['../structstan_1_1lang_1_1integrate__ode.html#a1a4a60c8e050dc311ef8095667b61ff6',1,'stan::lang::integrate_ode::y0_()'],['../structstan_1_1lang_1_1integrate__ode__control.html#a08cc2911112ff03088bf1f778543f335',1,'stan::lang::integrate_ode_control::y0_()']]]
-];
