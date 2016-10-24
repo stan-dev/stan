9295cc8b9b210a1683d4d800b0592b23aac72b99
Krzysztof Sakrejda <krzysztof@gmail.com>
2016-10-24 16:11:03 -0400

    Other two tests.

--- src/test/unit/lang/generator_functions_only_test.cpp
+++ src/test/unit/lang/generator_functions_only_test.cpp
@@ -8,12 +8,26 @@ class DoublesFunctions : public testing::Test { };
 
 TEST_F(DoublesFunctions, f01) {
   double result;
-  result = f01(1, &std::cout);
+  result = f01(1.0, &std::cout);
   EXPECT_EQ(2, result);
-  result = f01(3, &std::cout);
+  result = f01(3.0, &std::cout);
   EXPECT_EQ(6, result);
 }
 
+TEST_F(DoublesFunctions, f02) {
+  double result;
+  result = f02(1.0, 2,  &std::cout);
+  EXPECT_EQ(3, result);
+  result = f02(1.0, 3, &std::cout);
+  EXPECT_EQ(4, result);
+}
 
+TEST_F(DoublesFunctions, f03) {
+  double result;
+  result = f03(1.0, 2, 33.3,  &std::cout);
+  EXPECT_EQ(36.3, result);
+  result = f03(1.0, 3, 53.3, &std::cout);
+  EXPECT_EQ(57.3, result);
+}
 
 
