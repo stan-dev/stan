1c719b5c2c0e731cde0102bde38e15b37d5c3f3e
Daniel Lee <bearlee@alum.mit.edu>
2016-11-03 22:02:57 -0400

    Updating test

--- src/test/unit/lang/stanc_helper_test.cpp
+++ src/test/unit/lang/stanc_helper_test.cpp
@@ -26,36 +26,6 @@ TEST(commandStancHelper, printStancHelp) {
   expect_find(ss.str(), "OPTIONS:");
 }
 
-bool create_test_file(const std::string& path, const std::string& program) {
-  std::string cmd = "echo \"";
-  cmd += program;
-  cmd += "\" > ";
-  cmd += path;
-  cmd += "; chmod 444 ";
-  cmd += path;
-  int return_code = system(cmd.c_str());
-  return return_code == 0;
-}
-
-bool create_test_file() {
-  std::string program
-    = "parameters { real y; } model { y ~ normal(0, 1); }";
-  return create_test_file("test/test-models/temp-bar.stan", program);
-}
-
-TEST(commandStancHelper, deleteFile) {
-  if (!create_test_file()) return;
-  std::stringstream ss;
-  delete_file(&ss, "test/test-models/temp-bar.stan");
-  // test there's no error message
-  EXPECT_EQ(0, ss.str().size());
-  // and then test the file stream can't be opened
-  std::fstream fs;
-  fs.open("test/test-models/temp-bar.stan", std::fstream::in);
-  EXPECT_FALSE(fs.is_open());
-  fs.close();
-}
-
 int run_helper(const std::string& path,
                std::ostream& out, std::ostream& err) {
   int argc = 2;
@@ -67,38 +37,26 @@ int run_helper(const std::string& path,
 }
 
 TEST(commandStancHelper, readOnlyOK) {
-  if (!create_test_file()) return;
-
   std::stringstream out;
   std::stringstream err;
-  int rc = run_helper("test/test-models/temp-bar.stan", out, err);
-
+  int rc = run_helper("src/test/test-models/good/stanc_helper.stan", out, err);
+  
   EXPECT_EQ(0, rc)
     << "out=" << out.str() << std::endl << "err=" << err.str() << std::endl;
-  expect_find(out.str(), "Model name=temp_bar_model");
-  expect_find(out.str(), "Input file=test/test-models/temp-bar.stan");
-  expect_find(out.str(), "Output file=temp_bar_model.cpp");
+  expect_find(out.str(), "Model name=stanc_helper_model");
+  expect_find(out.str(), "Input file=src/test/test-models/good/stanc_helper.stan");
+  expect_find(out.str(), "Output file=stanc_helper_model.cpp");
   EXPECT_EQ(0, err.str().size())
     << "error=" << err.str() << std::endl;
-
-  delete_file(&err, "test/test-models/temp-bar.stan");
-  delete_file(&err, "bar_model.cpp");
 }
 
 TEST(commandStancHelper, failRC) {
-  std::string path = "test/test-models/temp-bar.stan";
-  std::string prog
-    = "parameters { real y; } model { y ~ ormal(0, 1); }";
-  if (!create_test_file(path, prog)) return;
-
   std::stringstream out;
   std::stringstream err;
-  int rc = run_helper(path, out, err);
+  int rc = run_helper("src/test/test-models/bad/stanc_helper.stan", out, err);
 
   // TODO(carpenter): This should be -2 but it's -3 so
   // I only tested that it's != 0 to contrast with earlier success
   EXPECT_TRUE(rc != 0);
-
-  delete_file(&err, "test/test-models/temp-bar.stan");
 }
     
