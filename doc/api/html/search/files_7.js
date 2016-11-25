e65d81ddbd6acd41ad99069bbed1af946cd06de7
Daniel Lee <bearlee@alum.mit.edu>
2016-11-25 11:43:43 -0500

    Removing built documentation.

--- doc/api/html/search/files_7.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['hessian_2ehpp',['hessian.hpp',['../hessian_8hpp.html',1,'']]],
-  ['hessian_5ftimes_5fvector_2ehpp',['hessian_times_vector.hpp',['../hessian__times__vector_8hpp.html',1,'']]]
-];
