a34128eda21bcb9ebe2f2519cf611042ffc7fc93
Daniel Lee <bearlee@alum.mit.edu>
2016-11-25 11:33:52 -0500

    release/v2.13.0: adding built documentation.

---
+++ doc/api/html/search/files_10.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['term_5fgrammar_2ehpp',['term_grammar.hpp',['../term__grammar_8hpp.html',1,'']]],
+  ['term_5fgrammar_5fdef_2ehpp',['term_grammar_def.hpp',['../term__grammar__def_8hpp.html',1,'']]],
+  ['term_5fgrammar_5finst_2ecpp',['term_grammar_inst.cpp',['../term__grammar__inst_8cpp.html',1,'']]],
+  ['test_5fgradients_2ehpp',['test_gradients.hpp',['../test__gradients_8hpp.html',1,'']]]
+];
