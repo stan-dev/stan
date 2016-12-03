ead62e5226899821829fa78880d216385dfc84d0
Daniel Lee <bearlee@alum.mit.edu>
2016-12-03 14:02:40 -0500

    release/v2.13.1: adding built documentation.

---
+++ doc/api/html/search/enumvalues_6.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['usage',['USAGE',['../structstan_1_1services_1_1error__codes.html#aba3bc56841fd28f036005851249eda92adf27074bf1305fb8b8e8d5bb4dce0988',1,'stan::services::error_codes']]]
+];
---
+++ doc/api/html/search/typedefs_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['function_5fsignature_5ft',['function_signature_t',['../namespacestan_1_1lang.html#a97d35e31b83b79f49db2a5d012d8bddf',1,'stan::lang']]]
+];
