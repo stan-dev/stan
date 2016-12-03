1fd345371fe3f6f1dbd124e31a992901f01565c2
Daniel Lee <bearlee@alum.mit.edu>
2016-12-03 14:50:51 -0500

    Removing built documentation.

--- doc/api/html/search/typedefs_b.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['u_5fint_5fargument',['u_int_argument',['../namespacestan_1_1services.html#aebe051cc60d447008cdcea043a26d870',1,'stan::services']]],
-  ['updatet',['UpdateT',['../classstan_1_1optimization_1_1_l_b_f_g_s_update.html#ac3387080b62310a57cde240ccd11f7e5',1,'stan::optimization::LBFGSUpdate']]]
-];
