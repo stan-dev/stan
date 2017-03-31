7513dd8f555c6c4d8437fb91587f46b066a6b075
Mitzi Morris <mitzi@panix.com>
2017-03-31 13:24:50 -0400

    code cleanup for var_context readers

---
+++ src/stan/services/util/read_dense_mass_matrix.hpp
@@ -0,0 +1,52 @@
+#ifndef STAN_SERVICES_UTIL_READ_DENSE_MASS_MATRIX_HPP
+#define STAN_SERVICES_UTIL_READ_DENSE_MASS_MATRIX_HPP
+
+#include <stan/callbacks/writer.hpp>
+#include <stan/io/var_context.hpp>
+#include <stan/math/prim/mat.hpp>
+#include <limits>
+#include <sstream>
+#include <string>
+#include <vector>
+
+namespace stan {
+  namespace services {
+    namespace util {
+
+      /**
+       * Extract dense mass matrix from a var_context object.
+       *
+       * @param[in] init_mass_matrix a var_context with array of initial values
+       * @param[in] num_params expected number of row, column elements
+       * @param[in,out] error_writer message writer
+       * @throws std::domain_error if cannot read the mass matrix
+       * @return mass_matrix
+       */
+      Eigen::MatrixXd
+      read_dense_mass_matrix(stan::io::var_context& init_mass_matrix,
+                                 size_t num_params,
+                                 stan::callbacks::writer& error_writer) {
+        try {
+          init_mass_matrix.read_dims("read dense mass matrix", "mass_matrix",
+                           "matrix",
+                           init_mass_matrix.to_vec(num_params, num_params));
+        } catch (const std::domain_error& e) {
+          error_writer("Cannot get mass matrix from input file");
+          throw std::domain_error("Initialization failure");
+        }
+        size_t num_elements = num_params * num_params;
+        std::vector<double> dense_vals =
+          init_mass_matrix.vals_r("mass_matrix");
+
+        Eigen::MatrixXd inv_mass_matrix(num_params, num_params);
+        inv_mass_matrix =
+          stan::math::to_matrix(dense_vals, num_params, num_params);
+
+        return inv_mass_matrix;
+      }
+
+    }
+  }
+}
+
+#endif
---
+++ src/stan/services/util/read_diag_mass_matrix.hpp
@@ -0,0 +1,50 @@
+#ifndef STAN_SERVICES_UTIL_READ_DIAG_MASS_MATRIX_HPP
+#define STAN_SERVICES_UTIL_READ_DIAG_MASS_MATRIX_HPP
+
+#include <stan/callbacks/writer.hpp>
+#include <stan/io/var_context.hpp>
+#include <stan/math/prim/scal.hpp>
+#include <limits>
+#include <sstream>
+#include <string>
+#include <vector>
+
+namespace stan {
+  namespace services {
+    namespace util {
+
+      /**
+       * Extract diagonal values for a mass matrix from a var_context object.
+       *
+       * @param[in] init_mass_matrix a var_context with initial values
+       * @param[in] num_params expected number of diagonal elements
+       * @param[in,out] error_writer message writer
+       * @throws std::domain_error if the mass matrix is invalid
+       * @return mass_matrix vector of diagonal values
+       */
+      Eigen::VectorXd
+      read_diag_mass_matrix(stan::io::var_context& init_mass_matrix,
+                            size_t num_params,
+                            stan::callbacks::writer& error_writer) {
+        try {
+          init_mass_matrix.read_dims("read diag mass matrix", "mass_matrix",
+                                     "vector_d",
+                                     init_mass_matrix.to_vec(num_params));
+        } catch (const std::domain_error& e) {
+          error_writer("Cannot get mass matrix from input file");
+          throw std::domain_error("Initialization failure");
+        }
+        std::vector<double> mass_matrix(num_params) =
+          init_mass_matrix.vals_r("mass_matrix");
+        Eigen::VectorXd inv_mass_matrix(num_params);
+        for (size_t i=0; i < num_params; i++) {
+          inv_mass_matrix(i) = mass_matrix[i];
+        }
+        return inv_mass_matrix;
+      }
+
+    }
+  }
+}
+
+#endif
