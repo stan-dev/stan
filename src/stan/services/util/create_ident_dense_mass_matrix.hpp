d42763a7c1a821eabfbd8c96a43cd65e93db2435
Mitzi Morris <mitzi@panix.com>
2017-04-02 17:51:21 -0400

    helper functions to create initial mass matrix for samplers

---
+++ src/stan/services/util/create_ident_dense_mass_matrix.hpp
@@ -0,0 +1,43 @@
+#ifndef STAN_SERVICES_UTIL_CREATE_IDENT_DENSE_MASS_MATRIX_HPP
+#define STAN_SERVICES_UTIL_CREATE_IDENT_DENSE_MASS_MATRIX_HPP
+
+#include <stan/io/dump.hpp>
+#include <Eigen/Dense>
+#include <sstream>
+
+namespace stan {
+  namespace services {
+    namespace util {
+
+      /**
+       * Create a stan::dump object which contains vector "mass_matrix"
+       * of specified size where all elements are ones.
+       *
+       * @param[in] num_params expected number of denseonal elements
+       * @return var_context 
+       */
+      stan::io::dump
+      create_ident_dense_mass_matrix(size_t num_params) {
+
+        Eigen::MatrixXd inv_mass_matrix(num_params, num_params);
+        inv_mass_matrix.setIdentity();
+        size_t num_elements = num_params * num_params;
+        std::stringstream txt;
+        txt << "mass_matrix <- structure(c(";
+        for (size_t i = 0; i < num_elements; i++) {
+          txt << inv_mass_matrix(i);
+          if (i < num_elements - 1)
+            txt << ", ";
+        }
+        txt << "),.Dim=c("
+            << num_params
+            << ", "
+            << num_params
+            << "))";
+        return stan::io::dump(txt);
+      }
+    }
+  }
+}
+
+#endif
---
+++ src/stan/services/util/create_ident_diag_mass_matrix.hpp
@@ -0,0 +1,34 @@
+#ifndef STAN_SERVICES_UTIL_CREATE_IDENT_DIAG_MASS_MATRIX_HPP
+#define STAN_SERVICES_UTIL_CREATE_IDENT_DIAG_MASS_MATRIX_HPP
+
+#include <stan/io/dump.hpp>
+#include <sstream>
+
+namespace stan {
+  namespace services {
+    namespace util {
+
+      /**
+       * Create a stan::dump object which contains vector "mass_matrix"
+       * of specified size where all elements are ones.
+       *
+       * @param[in] num_params expected number of diagonal elements
+       * @return var_context 
+       */
+      stan::io::dump
+      create_ident_diag_mass_matrix(size_t num_params) {
+        std::stringstream txt;
+        txt << "mass_matrix <- structure(c(";
+        for (size_t i = 0; i < num_params; ++i) {
+          txt << "1.0";
+          if (i < num_params - 1)
+            txt << ", ";
+        }
+        txt << "),.Dim=c(" << num_params << "))";
+        return stan::io::dump(txt);
+      }
+    }
+  }
+}
+
+#endif
