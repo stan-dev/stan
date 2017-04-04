72f12ab9f956fcdd74ebce20ec7522897a64f873
Mitzi Morris <mitzi@panix.com>
2017-04-04 14:53:34 -0400

    more unit tests

--- src/test/unit/lang/compiler_test.cpp
+++ src/test/unit/lang/compiler_test.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <stan/lang/compiler.hpp>
 #include <test/unit/util.hpp>
+#include <boost/algorithm/string.hpp>
 
 TEST(LangCompiler, compile) {
   std::stringstream msgs, stan_lang_in, cpp_out;
@@ -21,4 +22,24 @@ TEST(LangCompiler, emptyProgram) {
   
   EXPECT_EQ(1,count_matches("WARNING: empty program",msgs.str()));
   EXPECT_EQ(std::string::npos, cpp_out.str().find("int main("));
+}
+
+TEST(LangCompiler, equivEmpty) {
+  std::stringstream msgs, empty_prog_in, empty_model_in, empty_prog_out,
+    empty_model_out;
+  std::string model_name = "m";
+
+  empty_prog_in << "";
+  stan::lang::compile(&msgs, empty_prog_in, empty_prog_out, model_name);
+
+  empty_model_in << "model { }";
+  stan::lang::compile(&msgs, empty_model_in, empty_model_out, model_name);
+
+  // empty model contains extra newline, remove all for comparison
+  std::string emptyprog(empty_prog_out.str());
+  boost::algorithm::erase_all(emptyprog, "\n");
+  std::string emptymodel(empty_model_out.str());
+  boost::algorithm::erase_all(emptymodel, "\n");
+
+  EXPECT_EQ(emptyprog, emptymodel);
 }
