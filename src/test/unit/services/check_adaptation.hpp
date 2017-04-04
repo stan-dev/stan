5341fbb4533a116c06f382ad9895b061a628b91c
Mitzi Morris <mitzi@panix.com>
2017-04-04 16:47:19 -0400

    unit test helper function uses atof, stod problematic with c++11 compilers

--- src/test/unit/services/check_adaptation.hpp
+++ src/test/unit/services/check_adaptation.hpp
@@ -3,6 +3,8 @@
 
 #include <test/unit/services/instrumented_callbacks.hpp>
 #include <gtest/gtest.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <string>
 #include <vector>
 #include <boost/algorithm/string.hpp>
@@ -11,6 +13,12 @@ namespace stan {
   namespace test {
     namespace unit {
 
+      double stod(const std::string& val) {
+        char tmp[val.length()];
+        strcpy(tmp,val.c_str());
+        return atof(tmp);
+      }
+        
       void check_adaptation(const size_t& num_params,
                             const std::vector<double>& param_vals,
                             stan::test::unit::instrumented_writer& report,
@@ -25,10 +33,11 @@ namespace stan {
           }
         }
         std::vector<std::string> strs;
-        boost::split(strs, param_strings[offset], boost::is_any_of(", "), boost::token_compress_on);
+        boost::split(strs, param_strings[offset], boost::is_any_of(", "),
+                     boost::token_compress_on);
         EXPECT_EQ(num_params, strs.size());
         for (size_t i = 0; i < num_params; i++) {
-          ASSERT_NEAR(param_vals[i], std::stod(strs[i]), err_margin);
+          ASSERT_NEAR(param_vals[i], test::unit::stod(strs[i]), err_margin);
         }
       }
 
@@ -47,10 +56,11 @@ namespace stan {
         }
         for (size_t i = 0, ij = 0; i < num_rows; i++) {
           std::vector<std::string> strs;
-          boost::split(strs, param_strings[offset+i], boost::is_any_of(", "), boost::token_compress_on);
+          boost::split(strs, param_strings[offset+i], boost::is_any_of(", "),
+                       boost::token_compress_on);
           EXPECT_EQ(num_cols, strs.size());
           for (size_t j = 0; j < num_cols; j++, ij++) {
-            ASSERT_NEAR(param_vals[ij], std::stod(strs[j]), err_margin);
+            ASSERT_NEAR(param_vals[ij], test::unit::stod(strs[j]), err_margin);
           }
         }
       }
@@ -69,10 +79,11 @@ namespace stan {
           }
         }
         std::vector<std::string> strs;
-        boost::split(strs, param_strings[offset], boost::is_any_of(", "), boost::token_compress_on);
+        boost::split(strs, param_strings[offset], boost::is_any_of(", "),
+                     boost::token_compress_on);
         EXPECT_EQ(num_params, strs.size());
         for (size_t i = 0; i < num_params; i++) {
-          ASSERT_GT(fabs(param_vals[i]-std::stod(strs[i])), margin);
+            ASSERT_GT(fabs(param_vals[i]-test::unit::stod(strs[i])), margin);
         }
       }
 
@@ -91,10 +102,11 @@ namespace stan {
         }
         for (size_t i = 0, ij = 0; i < num_rows; i++) {
           std::vector<std::string> strs;
-          boost::split(strs, param_strings[offset+i], boost::is_any_of(", "), boost::token_compress_on);
+          boost::split(strs, param_strings[offset+i], boost::is_any_of(", "),
+                       boost::token_compress_on);
           EXPECT_EQ(num_cols, strs.size());
           for (size_t j = 0; j < num_cols; j++, ij++) {
-            ASSERT_GT(fabs(param_vals[ij]-std::stod(strs[j])), margin);
+            ASSERT_GT(fabs(param_vals[ij]-test::unit::stod(strs[j])), margin);
           }
         }
       }
