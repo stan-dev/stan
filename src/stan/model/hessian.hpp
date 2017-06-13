8a4e1091810572d4d32d723fa7cab616ef54202d
Bob Carpenter <carp@alias-i.com>
2017-06-13 19:55:10 -0400

    resolve hessian() in mcmc and model, fixes #2321

--- src/stan/model/hessian.hpp
+++ src/stan/model/hessian.hpp
@@ -15,8 +15,9 @@ namespace stan {
                  Eigen::Matrix<double, Eigen::Dynamic, 1>& grad_f,
                  Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic>& hess_f,
                  std::ostream* msgs = 0) {
-      stan::math::hessian(model_functional<M>(model, msgs),
-                          x, f, grad_f, hess_f);
+      stan::math::hessian<model_functional<M> >(model_functional<M>(model,
+                                                                    msgs),
+                                                x, f, grad_f, hess_f);
     }
 
   }
