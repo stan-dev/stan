814b3aad3a3619d75b217e13558afdbde31518ed
Mitzi Morris <mitzi@panix.com>
2017-06-13 19:55:02 -0400

    back out inadvertant changes

--- src/stan/lang/compiler.hpp
+++ src/stan/lang/compiler.hpp
@@ -27,8 +27,6 @@ namespace stan {
      * @param allow_undefined true if permits undefined functions
      * @param filename name of file or other source from which input
      *   stream was derived
-     * @param include_paths Ordered sequence of paths to search for
-     *   include files
      * @return <code>false</code> if code could not be generated due
      *   to syntax error in the Stan model; <code>true</code>
      *   otherwise.
@@ -37,17 +35,16 @@ namespace stan {
                  const std::string& name, const bool allow_undefined = false,
                  const std::string& filename = "unkown file name",
                  const std::vector<std::string>& include_paths
-                 = std::vector<std::string>()) {
+                  = std::vector<std::string>()) {
       io::program_reader reader(in, filename, include_paths);
       std::string s = reader.program();
       std::stringstream ss(s);
-      std::stringstream included_files;
       program prog;
       bool parse_succeeded = parse(msgs, ss, name, reader, prog,
                                    allow_undefined);
       if (!parse_succeeded)
         return false;
-      generate_cpp(prog, name, reader.history(), out, included_listing);
+      generate_cpp(prog, name, reader.history(), out);
       return true;
     }
 
