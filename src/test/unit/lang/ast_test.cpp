25fc0b84e4ebf0ebc4d7e92e6b8e35ef8b8e423e
Charles Margossian <charlesm@metrumrg.com>
2017-06-26 11:08:42 -0400

    remove spaces in ast_test.cpp

--- src/test/unit/lang/ast_test.cpp
+++ src/test/unit/lang/ast_test.cpp
@@ -856,7 +856,7 @@ TEST(StanLangAstFun, is_space) {
   EXPECT_TRUE(is_space('\n'));
   EXPECT_TRUE(is_space('\t'));
   EXPECT_TRUE(is_space('\r'));
-  
+
   EXPECT_FALSE(is_space('a'));
   EXPECT_FALSE(is_space('2'));
 }
@@ -867,7 +867,7 @@ TEST(StanLangAstFun, is_nonempty) {
   EXPECT_FALSE(is_nonempty("\t"));
   EXPECT_FALSE(is_nonempty("\r"));
   EXPECT_FALSE(is_nonempty("   \r  \t  "));
-  
+
   EXPECT_TRUE(is_nonempty("1"));
   EXPECT_TRUE(is_nonempty("  \r\n \n 1  \n"));
 }
