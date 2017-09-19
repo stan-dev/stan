a4f7e10f462893110739bcb31beef29baf2c3184
Mitzi Morris <mitzi@panix.com>
2017-09-19 11:21:04 -0400

    merge changes to function_signatures.h

--- src/stan/lang/function_signatures.h
+++ src/stan/lang/function_signatures.h
@@ -38,11 +38,11 @@ for (size_t i = 0; i < base_types.size(); ++i) {
   add("add", base_types[i], base_types[i]);
 }
 for (size_t i = 1; i < 8; ++i) {
-  add("append_array", expr_type(INT_T, i), expr_type(INT_T, i), expr_type(INT_T, i));
-  add("append_array", expr_type(DOUBLE_T, i), expr_type(DOUBLE_T, i), expr_type(DOUBLE_T, i));
-  add("append_array", expr_type(VECTOR_T, i), expr_type(VECTOR_T, i), expr_type(VECTOR_T, i));
-  add("append_array", expr_type(ROW_VECTOR_T, i), expr_type(ROW_VECTOR_T, i), expr_type(ROW_VECTOR_T, i));
-  add("append_array", expr_type(MATRIX_T, i), expr_type(MATRIX_T, i), expr_type(MATRIX_T, i));
+  add("append_array", expr_type(int_type(), i), expr_type(int_type(), i), expr_type(int_type(), i));
+  add("append_array", expr_type(double_type(), i), expr_type(double_type(), i), expr_type(double_type(), i));
+  add("append_array", expr_type(vector_type(), i), expr_type(vector_type(), i), expr_type(vector_type(), i));
+  add("append_array", expr_type(row_vector_type(), i), expr_type(row_vector_type(), i), expr_type(row_vector_type(), i));
+  add("append_array", expr_type(matrix_type(), i), expr_type(matrix_type(), i), expr_type(matrix_type(), i));
 }
 add_unary_vectorized("asin");
 add_unary_vectorized("asinh");
