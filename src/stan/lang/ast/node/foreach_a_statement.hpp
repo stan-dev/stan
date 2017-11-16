1fb3a91197f72a460f9e21fcedb7a275f922caed
MatthijsV <matthijsvakar@gmail.com>
2017-11-16 17:58:07 -0500

    for loops for arrays and matrices correctly implemented and passing tests, modulo type checking and keyword overloading

---
+++ src/stan/lang/ast/node/foreach_a_statement.hpp
@@ -0,0 +1,51 @@
+#ifndef STAN_LANG_AST_NODE_FOREACH_A_STATEMENT_HPP
+#define STAN_LANG_AST_NODE_FOREACH_A_STATEMENT_HPP
+
+#include <stan/lang/ast/node/expression.hpp>
+//#include <stan/lang/ast/node/array_expr.hpp>
+#include <stan/lang/ast/node/statement.hpp>
+#include <string>
+
+namespace stan {
+  namespace lang {
+
+    /**
+     * AST node for representing a foreach statement over an array.
+     */
+    struct foreach_a_statement {
+      /**
+       * Construct an uninitialized foreach statement.
+       */
+      foreach_a_statement();
+
+      /**
+       * Construct a foreach statement that loops the specified variable
+       * over the specified expression to execute the specified statement.
+       *
+       * @param[in] variable loop variable
+       * @param[in] expression value expression foreach loop variable
+       * @param[in] stmt body of the foreach loop
+       */
+      foreach_a_statement(const std::string& variable,
+                          const expression& expression, //FOREACHCHANGE: really, this should be an array_expr. where do we do the type checking?
+                          const statement& stmt);
+
+      /**
+       * The loop variable.
+       */
+      std::string variable_;
+
+      /**
+       * The expression of values for the loop variable.
+       */
+      expression expression_; //FOREACHCHANGE:  really, this should be an array_expr. where do we do the type checking?
+
+      /**
+       * The body of the foreach loop.
+       */
+      statement statement_;
+    };
+
+  }
+}
+#endif
