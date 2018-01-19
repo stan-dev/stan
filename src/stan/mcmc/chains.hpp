4756fa2c947ef529ea93a57d91a89ec470b01c20
Aki Vehtari <Aki.Vehtari@aalto.fi>
2018-01-19 22:24:38 +0200

    spacing and parens

--- src/stan/mcmc/chains.hpp
+++ src/stan/mcmc/chains.hpp
@@ -256,12 +256,11 @@ namespace stan {
           var_plus += variance(chain_mean);
         Eigen::VectorXd rho_hat_t(n_samples);
         rho_hat_t.setZero();
-        double rho_hat_even = 1;
-        double rho_hat_odd = 0;
         Eigen::VectorXd acov_t(chains);
         for (int chain = 0; chain < chains; chain++)
           acov_t(chain) = acov(chain)(1);
-        rho_hat_odd = 1 - (mean_var - mean(acov_t)) / var_plus;
+        double rho_hat_even = 1;
+        double rho_hat_odd = 1 - (mean_var - mean(acov_t)) / var_plus;
         rho_hat_t(1) = rho_hat_odd;
         // Geyer's initial positive sequence
         int max_t = 1;
@@ -269,22 +268,22 @@ namespace stan {
              (t < (n_samples - 2) && (rho_hat_even + rho_hat_odd) >= 0);
              t += 2) {
           for (int chain = 0; chain < chains; chain++)
-            acov_t(chain) = acov(chain)(t+1);
+            acov_t(chain) = acov(chain)(t + 1);
           rho_hat_even = 1 - (mean_var - mean(acov_t)) / var_plus;
           for (int chain = 0; chain < chains; chain++)
-            acov_t(chain) = acov(chain)(t+2);
+            acov_t(chain) = acov(chain)(t + 2);
           rho_hat_odd = 1 - (mean_var - mean(acov_t)) / var_plus;
           if ((rho_hat_even + rho_hat_odd) >= 0) {
-            rho_hat_t(t+1) = rho_hat_even;
-            rho_hat_t(t+2) = rho_hat_odd;
+            rho_hat_t(t + 1) = rho_hat_even;
+            rho_hat_t(t + 2) = rho_hat_odd;
           }
-          max_t = t+2;
+          max_t = t + 2;
         }
         // Geyer's initial monotone sequence
-        for (int t = 3; t <= (max_t - 2); t += 2) {
-          if ((rho_hat_t(t+1) + rho_hat_t(t+2)) > (rho_hat_t(t-1) + rho_hat_t(t))) {
-            rho_hat_t(t+1) = (rho_hat_t(t-1) + rho_hat_t(t)) / 2;
-            rho_hat_t(t+2) = rho_hat_t(t+1);
+        for (int t = 3; t <= max_t - 2; t += 2) {
+          if (rho_hat_t(t + 1) + rho_hat_t(t + 2) > rho_hat_t(t - 1) + rho_hat_t(t)) {
+            rho_hat_t(t + 1) = (rho_hat_t(t - 1) + rho_hat_t(t)) / 2;
+            rho_hat_t(t + 2) = rho_hat_t(t + 1);
           }
         }
         double ess = chains * n_samples;
