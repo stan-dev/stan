23b3a1731455d73aba1cde1281427667f0d5cce1
Mitzi Morris <mitzi@panix.com>
2018-03-26 14:56:17 -0400

    grammar tests for non-clang compilers

--- src/test/unit/lang/parser/bad_bare_type_grammar_test.cpp
+++ src/test/unit/lang/parser/bad_bare_type_grammar_test.cpp
@@ -9,35 +9,38 @@
 #include <boost/algorithm/string/predicate.hpp>
 
 TEST(Parser, parse_local_unknown) {
-  std::string input("unknown");
+  std::string input("functions {\n"
+                    "unknown bad_fun; \n");
   bool pass = false;
   std::stringstream msgs;
-  stan::lang::bare_expr_type bet;
-  bet = parse_bare_type(input, pass, msgs);
+  std::vector<stan::lang::function_decl_def> fns;
+  fns = parse_functions(input, pass, msgs);
 
   EXPECT_FALSE(pass);
   EXPECT_NE(msgs.str().find("PARSER FAILED TO PARSE INPUT"), std::string::npos);
 }
 
 TEST(Parser, parse_bare_unclosed_dim) {
-  std::string input("int[");
+  std::string input("functions {\n"
+                    "void bad_fun(int[ ); \n");
   bool pass = false;
   std::stringstream msgs;
-  stan::lang::bare_expr_type bet;
-  bet = parse_bare_type(input, pass, msgs);
+  std::vector<stan::lang::function_decl_def> fns;
+  fns = parse_functions(input, pass, msgs);
 
   EXPECT_FALSE(pass);
   EXPECT_NE(msgs.str().find("<comma to indicate more dimensions or ] "), std::string::npos);
 }
 
 TEST(Parser, parse_bare_unclosed_dim_2) {
-  std::string input("int[,,,");
+  std::string input("functions {\n"
+                    "void bad_fun(int[ , , , ); \n");
   bool pass = false;
-  std::stringstream err_msgs;
-  stan::lang::bare_expr_type bet;
-  bet = parse_bare_type(input, pass, err_msgs);
+  std::stringstream msgs;
+  std::vector<stan::lang::function_decl_def> fns;
+  fns = parse_functions(input, pass, msgs);
 
-  EXPECT_NE(err_msgs.str().find("<comma to indicate more dimensions or ] "), std::string::npos);
+  EXPECT_NE(msgs.str().find("<comma to indicate more dimensions or ] "), std::string::npos);
   EXPECT_FALSE(pass);
 
 }
