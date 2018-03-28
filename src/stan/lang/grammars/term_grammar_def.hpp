1bc6bac32cef30b74e067d855e214b5c8b207feb
Bob Carpenter <carp@alias-i.com>
2018-03-28 08:16:48 -0700

    end of symbol for algebra_solver, map_rect, fixes #2502

--- src/stan/lang/grammars/term_grammar_def.hpp
+++ src/stan/lang/grammars/term_grammar_def.hpp
@@ -255,6 +255,7 @@ namespace stan {
       algebra_solver_r.name("expression");
       algebra_solver_r
         %= lit("algebra_solver")
+        >> no_skip[!char_("a-zA-Z0-9_")]
         > lit('(')
         > identifier_r          // 1) system function name (function only)
         > lit(',')
@@ -272,6 +273,7 @@ namespace stan {
       map_rect_r.name("map_rect");
       map_rect_r
           %= lit("map_rect")
+          >> no_skip[!char_("a-zA-Z0-9_")]
           > lit('(')
           > identifier_r          // 1) mapped function name
           > lit(',')
