6e0a67a9405ad48506c0382246d19c172579ac02
Daniel Lee <daniel@generable.com>
2018-08-17 23:49:50 -0400

    updating test so it creates the directory first

--- src/test/unit/lang/stanc_helper_test.cpp
+++ src/test/unit/lang/stanc_helper_test.cpp
@@ -27,6 +27,8 @@ TEST(commandStancHelper, printStancHelp) {
 }
 
 bool create_test_file(const std::string& path, const std::string& program) {
+  std::string cmd_setup = "mkdir -p test/test-models";
+  system(cmd_setup.c_str());
   std::string cmd = "echo ";
   cmd += "\"";
   cmd += program;
@@ -147,4 +149,3 @@ TEST(commandStancHelper, readOnlyDirBadFile) {
   int rc = stanc_helper(argc, argv, &out, &err);
   EXPECT_TRUE(rc != 0);
 }
-
