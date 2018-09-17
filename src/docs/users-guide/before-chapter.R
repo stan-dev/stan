d6150090c530739bcb3bb0532b44e60ecc574f7d
Bob Carpenter <carp@alias-i.com>
2018-09-17 11:21:43 +0200

    broke book into chapters and parts

---
+++ src/docs/users-guide/before-chapter.R
@@ -0,0 +1,51 @@
+# LIBRARY CONFIGURATION
+
+library(arm)
+library(bayesplot)
+  # theme_set(bayesplot::theme_default())
+library(dplyr)
+library(ggplot2)
+library(knitr)
+  knitr::opts_chunk$set(
+    cache = TRUE,
+    cache.extra = packageVersion('tufte'),
+    collapse = TRUE,
+    comment = NA,
+    dev = "png",
+    dpi = 150,
+    echo = FALSE,
+    fig.align = "center",
+    fig.width = 6,
+    fig.asp = 0.618,  # 1 / phi
+    fig.show = "hold",
+    include = FALSE,
+    out.width = "70%",
+    tidy = FALSE
+  )
+library(lubridate)
+library(rstan)
+  rstan_options(auto_write = TRUE)
+
+
+# UTILITY FUNCTIONS
+
+printf <- function(pattern, ...) {
+  cat(sprintf(pattern, ...))
+}
+
+print_file <- function(file) {
+  cat(paste(readLines(file), "\n", sep=""), sep="")
+}
+
+extract_one_draw <- function(stanfit, chain = 1, iter = 1) {
+  x <- get_inits(stanfit, iter = iter)
+  x[[chain]]
+}
+
+# GENERAL R CONFIGURATION
+
+options(digits = 2)
+options(htmltools.dir.version = FALSE)
+options(mc.cores = parallel::detectCores(logical = FALSE))
+
+set.seed(1123) 
