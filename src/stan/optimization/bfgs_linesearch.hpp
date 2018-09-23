fdc33b45ede390337eeb1ee68f263b2b410862d5
Edward A. Roualdes <eroualdes@csuchico.edu>
2018-09-23 10:07:06 -0700

    limit number of line search iterations

--- src/stan/optimization/bfgs_linesearch.hpp
+++ src/stan/optimization/bfgs_linesearch.hpp
@@ -203,7 +203,7 @@ namespace stan {
      *
      * @param f0 Value of function at starting point, \f$ f(x_0) \f$.
      *
-     * @param gradx0 Value of function gradient at starting point, 
+     * @param gradx0 Value of function gradient at starting point,
      *    \f$ g(x_0) \f$.
      *
      * @param c1 Parameter of the Wolfe conditions. \f$ 0 < c_1 < c_2 < 1 \f$
@@ -214,6 +214,8 @@ namespace stan {
      *
      * @param minAlpha Smallest allowable step-size.
      *
+     * @param maxLSIts Maximum number line search iterations.
+     *
      * @return Returns zero on success, non-zero otherwise.
      **/
     template<typename FunctorType, typename Scalar, typename XType>
@@ -223,7 +225,7 @@ namespace stan {
                         const XType &p,
                         const XType &x0, const Scalar &f0, const XType &gradx0,
                         const Scalar &c1, const Scalar &c2,
-                        const Scalar &minAlpha) {
+                        const Scalar &minAlpha, const Scalar &maxLSIts) {
       const Scalar dfp(gradx0.dot(p));
       const Scalar c1dfp(c1*dfp);
       const Scalar c2dfp(c2*dfp);
@@ -242,7 +244,12 @@ namespace stan {
         x1.noalias() = x0 + alpha1 * p;
         ret = func(x1, f1, gradx1);
         if (ret != 0) {
+          if (nits >= maxLSIts) {
+            retCode = 1;
+            break;
+          }
           alpha1 = 0.5 * (alpha0 + alpha1);
+          nits++;
           continue;
         }
         newDFp = gradx1.dot(p);
