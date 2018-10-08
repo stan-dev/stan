a5d38f183360f01a0380cc63375cd4cda5228ef2
Mitzi Morris <mitzi@panix.com>
2018-10-08 13:14:10 -0400

    checkpointing

--- src/docs/functions/_build.sh
+++ src/docs/functions/_build.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
-
-cp _common.R.true _common.R
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-
-cp _common.R.false _common.R
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
-
-rm _common.R
