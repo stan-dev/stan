563e8d50d362432f287c08f0bc80e2490c8804bb
Mitzi Morris <mitzi@panix.com>
2018-10-24 13:37:05 -0400

    fix logic; unit tests

--- src/stan/lang/grammars/semantic_actions_def.cpp
+++ src/stan/lang/grammars/semantic_actions_def.cpp
@@ -1018,20 +1018,20 @@ namespace stan {
         pass = false;
         return;
       }
+      if (a.lhs_var_has_sliced_idx()) {
+        if (a.lhs_var_occurs_on_rhs()) {
+          // allow assignment but generate warning
+          error_msgs << "Info: assignment to variable name "
+                     << "\"" << name << "\","
+                     << " variable occurs on right-hand side of assignment, "
+                     << " causing inefficient deep copy to avoid aliasing."
+                     << std::endl;
+        }
+      }
       if (a.is_simple_assignment()) {
         if (!has_same_shape(lhs_type, a.rhs_, name, "assignment", error_msgs)) {
           pass = false;
           return;
-        }
-        if (a.lhs_var_has_sliced_idx()) {
-          if (a.lhs_var_occurs_on_rhs()) {
-            // allow assignment but generate warning
-            error_msgs << "Info: left-hand side variable"
-                       << " (name=" << name << ")"
-                       << " occurs on right-hand side of assignment, causing"
-                       << " inefficient deep copy to avoid aliasing."
-                       << std::endl;
-          }
         }
         pass = true;
         return;
--- src/test/unit/lang/generator/deep_copy_test.cpp
+++ src/test/unit/lang/generator/deep_copy_test.cpp
@@ -3,6 +3,15 @@
 #include <gtest/gtest.h>
 #include <test/unit/util.hpp>
 #include <test/unit/lang/utility.hpp>
+
+#include <stan/callbacks/stream_writer.hpp>
+#include <stan/io/dump.hpp>
+
+#include <test/test-models/good/parser-generator/deep-copy-warning.hpp>
+
+#include <boost/random/additive_combine.hpp>
+#include <fstream>
+#include <stdexcept>
 #include <sstream>
 #include <string>
 #include <iostream>
@@ -18,4 +27,20 @@ TEST(lang, deep_copy_hpp) {
                  "  }\n"
                  "}\n\n");
   expect_matches(1,m1,"stan::model::deep_copy");
+}
+
+TEST(lang, run_deep_copy) {
+  std::fstream empty_data_stream("");
+  stan::io::dump empty_data_context(empty_data_stream);
+  empty_data_stream.close();
+  std::stringstream model_output;
+
+  // instantiate good test model parser-generator/deep-copy-warning
+  stan_model model(empty_data_context, &model_output);
+
+  // check print stmts
+  EXPECT_EQ(1,count_matches("y: 0.1",model_output.str()));
+  EXPECT_EQ(1,count_matches("v: [2,2,3,4,5]",model_output.str()));
+  EXPECT_EQ(1,count_matches("v: [3,3,3,4,5]",model_output.str()));
+  EXPECT_EQ(1,count_matches("v: [3,7,7,8,5]",model_output.str()));
 }
