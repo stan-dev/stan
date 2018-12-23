b42c22a8b30a0aaf8f1ce4f36ecbcbab82e9baac
Matthijs Vákár <matthijsvakar@gmail.com>
2018-12-22 19:36:14 -0500

    Trying Jenkins again

--- src/stan/io/reader.hpp
+++ src/stan/io/reader.hpp
@@ -539,7 +539,8 @@ public:
    */
   template <typename TU> inline T scalar_ub(TU ub) {
     T x(scalar());
-    stan::math::check_less_or_equal("stan::io::scalar_ub", "Constrained scalar",
+    stan::math::check_less_or_equal("stan::io::scalar_ub",
+                                    "Constrained scalar",
                                     x, ub);
     return x;
   }
