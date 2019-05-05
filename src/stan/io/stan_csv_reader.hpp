c1add0af94dc981feebb5c46fafaa2a713e738d6
Andrew Johnson <andrew.r.johnson@postgrad.curtin.edu.au>
2019-05-05 16:50:02 +0800

    Update Eigen headers to allow compilation with new plugin methods

--- src/stan/io/stan_csv_reader.hpp
+++ src/stan/io/stan_csv_reader.hpp
@@ -2,7 +2,7 @@
 #define STAN_IO_STAN_CSV_READER_HPP
 
 #include <boost/algorithm/string.hpp>
-#include <Eigen/Dense>
+#include <stan/math/prim/mat/fun/Eigen.hpp>
 #include <istream>
 #include <iostream>
 #include <sstream>
