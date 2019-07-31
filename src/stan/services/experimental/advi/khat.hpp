661f78fc94816feccdbd50fef0c255102d805594
andre <andre@cluster-local.node.drelen>
2019-07-31 18:17:42 -0400

    add estimation for GPD params, untested

--- src/stan/services/experimental/advi/khat.hpp
+++ src/stan/services/experimental/advi/khat.hpp
@@ -1,10 +1,14 @@
 #ifndef STAN_SERVICES_EXPERIMENTAL_ADVI_KHAT_HPP
 #define STAN_SERVICES_EXPERIMENTAL_ADVI_KHAT_HPP
 
+#include <stan/math/prim/mat/fun/mean.hpp>
 #include <stan/math/rev/scal/fun/floor.hpp>
 #include <stan/math/rev/scal/fun/sqrt.hpp>
 #include <stan/math/rev/scal/fun/log.hpp>
+#include <stan/math/rev/scal/fun/exp.hpp>
 #include <Eigen/Dense>
+#include <std/sort>
+#include <type_traits>
 
 namespace stan {
 namespace math {
@@ -13,7 +17,49 @@ namespace experimental {
 namespace advi {
 
   <typename T_x>
-  int compute_khat(const std::vector<T_x>& x) {
+  int compute_khat(const std::vector<T_x>& x,
+                   const int& min_grid_points = 30,
+                   auto &k) {
+    size_t N = size_of(x);
+    vector<T_x> x_s(N);
+    x_s = x; x_s = std::sort(x_s.begin(), x_s.end());
+
+    double prior = 3.0;
+    auto M = min_grid_points + floor(sqrt(N));
+    int quart1 = floor(N / 4 + 0.5);
+    int x_star = x[quart1 - 1];
+
+    std::vector<T_x> theta(M);
+    for(size_t i = 0; i < M; ++i)
+      theta[i] = 1 / x_s[i] + (1 - sqrt(M / (jj[i] - 0.5))) / (prior * x_star);
+
+    std::vector<T_x> l_theta(M);
+    lx(l_theta, x);
+    l_theta = N * l_theta;
+
+    std::vector<T_x> w_theta(M);
+    for (size_t i = 0; i < M; ++i) {
+      w_theta[i] = 0;
+      for (size_t ii = 0; j < M; ++ii)
+        w_theta[i] = w_theta[i] + exp(l_theta[ii] - l_theta[j]);
+    }
+
+    T_x theta_hat = 0;
+    for (size_t i = 0; i < M; ++i)
+      theta_hat = theta_hat + theta[i] * w_theta[i];
+
+    std::vector<auto> k_vec(N);
+    for (size_t i = 0; i < N; ++i)
+      k_vec[i] = log(-theta_hat * x[i]);
+
+    auto k = mean(k_vec);
+    auto sigma = -k / theta_hat;
+
+    adjust_k_wip(k, N);
+    
+    if (std::nan == k)
+      k = std::numeric_limits<double>::infinity();
+
     return 0;
   }
 
