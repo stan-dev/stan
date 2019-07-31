6e518393ce3f7c8475a855e43dd73e5f745bd479
andre <andre@cluster-local.node.drelen>
2019-07-31 10:50:11 -0400

    add WIP

--- src/stan/services/experimental/advi/khat.hpp
+++ src/stan/services/experimental/advi/khat.hpp
@@ -13,16 +13,12 @@ namespace experimental {
 namespace advi {
 
   <typename T_x>
-  int compute_khat(const std::vector<T_x>& samples,
-                   const int &S, 
-                   int &M = 0,
-                   std::vector<double> khat) {    
+  int compute_khat(const std::vector<T_x>& x) {
     return 0;
   }
 
   <typename T_x>
-  int lx(std::vector<T_x>& a,
-         const Eigen::Matrix<T_x, -1, 1>& x) {
+  int lx(std::vector<T_x>& a, const Eigen::Matrix<T_x, -1, 1>& x) {
     size_t a_size = a.size();
     size_t x_rows = x.rows();
 
@@ -37,6 +33,14 @@ namespace advi {
     for (size_t i = 0; i < a_size; ++i)
       a[i] = log(a_temp[i] / k[i]) - k[i] - 1;
 
+    return 0;
+  }
+
+  typename<T_k>
+  int adjust_k_wip(T_k& k, const size_t& n) {
+    size_t a = 10;
+    size_t n_plus_a = n + a;
+    k = k * n / n_plus_a + a * 0.5 / n_plus_a;
     return 0;
   }
   
