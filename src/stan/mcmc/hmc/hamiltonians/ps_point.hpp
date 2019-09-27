425a268626312f5a52ace90fdb6a3d8d2be20989
stevo15025 <stevo15025@gmail.com>
2019-09-27 17:55:07 +0300

    cleanup ps_point to use emplace_back for strings and remove .at() for vector access to use []

--- src/stan/mcmc/hmc/hamiltonians/ps_point.hpp
+++ src/stan/mcmc/hmc/hamiltonians/ps_point.hpp
@@ -22,29 +22,30 @@ class ps_point {
   explicit ps_point(int n) : q(n), p(n), V(0), g(n) {}
 
 
+  double V;
   Eigen::VectorXd q;
   Eigen::VectorXd p;
-
-  double V;
   Eigen::VectorXd g;
 
   virtual void get_param_names(std::vector<std::string>& model_names,
                                std::vector<std::string>& names) {
+    names.reserve(q.size() + p.size() + g.size());
     for (int i = 0; i < q.size(); ++i)
-      names.push_back(model_names.at(i));
-    for (int i = 0; i < q.size(); ++i)
-      names.push_back(std::string("p_") + model_names.at(i));
-    for (int i = 0; i < q.size(); ++i)
-      names.push_back(std::string("g_") + model_names.at(i));
+      names.emplace_back(model_names[i]);
+    for (int i = 0; i < p.size(); ++i)
+      names.emplace_back(std::string("p_") + model_names[i]);
+    for (int i = 0; i < g.size(); ++i)
+      names.emplace_back(std::string("g_") + model_names[i]);
   }
 
   virtual void get_params(std::vector<double>& values) {
+    values.reserve(q.size() + p.size() + g.size());
     for (int i = 0; i < q.size(); ++i)
-      values.push_back(q(i));
-    for (int i = 0; i < q.size(); ++i)
-      values.push_back(p(i));
-    for (int i = 0; i < q.size(); ++i)
-      values.push_back(g(i));
+      values.push_back(q[i]);
+    for (int i = 0; i < p.size(); ++i)
+      values.push_back(p[i]);
+    for (int i = 0; i < g.size(); ++i)
+      values.push_back(g[i]);
   }
 
   /**
@@ -54,27 +55,6 @@ class ps_point {
    */
   virtual inline void write_metric(stan::callbacks::writer& writer) {}
 
- protected:
-  template <typename T>
-  static inline void fast_vector_copy_(
-      Eigen::Matrix<T, Dynamic, 1>& v_to,
-      const Eigen::Matrix<T, Dynamic, 1>& v_from) {
-    int sz = v_from.size();
-    v_to.resize(sz);
-    if (sz > 0)
-      std::memcpy(&v_to(0), &v_from(0), v_from.size() * sizeof(double));
-  }
-
-  template <typename T>
-  static inline void fast_matrix_copy_(
-      Eigen::Matrix<T, Dynamic, Dynamic>& v_to,
-      const Eigen::Matrix<T, Dynamic, Dynamic>& v_from) {
-    int nr = v_from.rows();
-    int nc = v_from.cols();
-    v_to.resize(nr, nc);
-    if (nr > 0 && nc > 0)
-      std::memcpy(&v_to(0), &v_from(0), v_from.size() * sizeof(double));
-  }
 };
 
 }  // namespace mcmc
