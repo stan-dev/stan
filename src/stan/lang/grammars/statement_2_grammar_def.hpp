bd2aaf6971f6d8d4f79d03560808a16c5e5c73a0
Stan Jenkins <mc.stanislaw@gmail.com>
2019-09-29 16:48:46 -0400

    [Jenkins] auto-formatting by clang-format version 6.0.0 (tags/google/stable/2017-11-14)

--- src/stan/lang/grammars/statement_2_grammar_def.hpp
+++ src/stan/lang/grammars/statement_2_grammar_def.hpp
@@ -27,10 +27,10 @@ statement_2_grammar<Iterator>::statement_2_grammar(
   using boost::spirit::qi::_pass;
   using boost::spirit::qi::_val;
   using boost::spirit::qi::char_;
-  using boost::spirit::qi::lit;
-  using boost::spirit::qi::no_skip;
   using boost::spirit::qi::labels::_r1;
   using boost::spirit::qi::labels::_r2;
+  using boost::spirit::qi::lit;
+  using boost::spirit::qi::no_skip;
 
   //   _r1 var_scope
   //   _r2 true if in loop (allowing break/continue)
@@ -43,12 +43,12 @@ statement_2_grammar<Iterator>::statement_2_grammar(
   conditional_statement_r
       = (lit("if") >> no_skip[!char_("a-zA-Z0-9_")]) > lit('(')
         > expression_g(_r1)[add_conditional_condition_f(
-            _val, _1, _pass, boost::phoenix::ref(error_msgs_))]
+              _val, _1, _pass, boost::phoenix::ref(error_msgs_))]
         > lit(')') > statement_g(_r1, _r2)[add_conditional_body_f(_val, _1)]
         > *(((lit("else") >> no_skip[!char_("a-zA-Z0-9_")])
              >> (lit("if") >> no_skip[!char_("a-zA-Z0-9_")]))
             > lit('(') > expression_g(_r1)[add_conditional_condition_f(
-                _val, _1, _pass, boost::phoenix::ref(error_msgs_))]
+                             _val, _1, _pass, boost::phoenix::ref(error_msgs_))]
             > lit(')')
             > statement_g(_r1, _r2)[add_conditional_body_f(_val, _1)])
         > -((lit("else") >> no_skip[!char_("a-zA-Z0-9_")])
