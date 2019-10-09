6a9f84a70a53a9021f08a4882d0073ed511f04ad
Steve Bronder <Stevo15025@gmail.com>
2019-10-09 10:22:07 +0300

    Revert "[WIP] Removed fast copy funcs from ps_point and dependents"

--- src/stan/mcmc/hmc/hamiltonians/dense_e_point.hpp
+++ src/stan/mcmc/hmc/hamiltonians/dense_e_point.hpp
@@ -28,6 +28,17 @@ class dense_e_point : public ps_point {
   }
 
   /**
+   * Copy constructor which does fast copy of inverse mass matrix.
+   *
+   * @param z point to copy
+   */
+  dense_e_point(const dense_e_point& z)
+      : ps_point(z),
+        inv_e_metric_(z.inv_e_metric_.rows(), z.inv_e_metric_.cols()) {
+    fast_matrix_copy_<double>(inv_e_metric_, z.inv_e_metric_);
+  }
+
+  /**
    * Set elements of mass matrix
    *
    * @param inv_e_metric initial mass matrix
