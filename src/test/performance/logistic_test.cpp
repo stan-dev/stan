f9ac6f1d429ab1d9985f7225973b317bc7c26edf
betanalpha <betanalpha@gmail.com>
2019-10-11 21:06:56 -0400

    Update performance test

--- src/test/performance/logistic_test.cpp
+++ src/test/performance/logistic_test.cpp
@@ -104,23 +104,23 @@ TEST_F(performance, values_from_tagged_version) {
       << "last tagged version, 2.17.0, had " << N_values << " elements";
 
   std::vector<double> first_run = last_draws_per_run[0];
-  EXPECT_FLOAT_EQ(-66.0868, first_run[0]) << "lp__: index 0";
+  EXPECT_FLOAT_EQ(-66.992599, first_run[0]) << "lp__: index 0";
 
-  EXPECT_FLOAT_EQ(0.958736, first_run[1]) << "accept_stat__: index 1";
+  EXPECT_FLOAT_EQ(0.97065002, first_run[1]) << "accept_stat__: index 1";
 
-  EXPECT_FLOAT_EQ(0.76969099, first_run[2]) << "stepsize__: index 2";
+  EXPECT_FLOAT_EQ(0.77005202, first_run[2]) << "stepsize__: index 2";
 
-  EXPECT_FLOAT_EQ(2, first_run[3]) << "treedepth__: index 3";
+  EXPECT_FLOAT_EQ(3, first_run[3]) << "treedepth__: index 3";
 
-  EXPECT_FLOAT_EQ(3, first_run[4]) << "n_leapfrog__: index 4";
+  EXPECT_FLOAT_EQ(7, first_run[4]) << "n_leapfrog__: index 4";
 
   EXPECT_FLOAT_EQ(0, first_run[5]) << "divergent__: index 5";
 
-  EXPECT_FLOAT_EQ(66.158401, first_run[6]) << "energy__: index 6";
+  EXPECT_FLOAT_EQ(67.834, first_run[6]) << "energy__: index 6";
 
-  EXPECT_FLOAT_EQ(1.23053, first_run[7]) << "beta.1: index 7";
+  EXPECT_FLOAT_EQ(0.915712, first_run[7]) << "beta.1: index 7";
 
-  EXPECT_FLOAT_EQ(-0.79178798, first_run[8]) << "beta.2: index 8";
+  EXPECT_FLOAT_EQ(-0.334984, first_run[8]) << "beta.2: index 8";
 
   matches_tagged_version = !HasNonfatalFailure();
 }
