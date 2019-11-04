3e4766f38b5f5af59eb62f6edee880eb2eb4d8a8
Stan Jenkins <mc.stanislaw@gmail.com>
2019-11-03 23:53:34 -0500

    [Jenkins] auto-formatting by clang-format version 6.0.0 (tags/google/stable/2017-11-14)

--- src/stan/mcmc/hmc/nuts_classic/base_nuts_classic.hpp
+++ src/stan/mcmc/hmc/nuts_classic/base_nuts_classic.hpp
@@ -39,8 +39,8 @@ class base_nuts_classic
   base_nuts_classic(const Model& model, BaseRNG& rng)
       : base_hmc<Model, Hamiltonian, Integrator, BaseRNG>(model, rng) {}
 
-  inline auto& max_depth() { return max_depth_;}
-  inline const auto& max_depth() const { return max_depth_;}
+  inline auto& max_depth() { return max_depth_; }
+  inline const auto& max_depth() const { return max_depth_; }
 
   inline auto& max_delta() { return max_delta_; }
   inline const auto& max_delta() const { return max_delta_; }
