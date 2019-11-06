d57e3d991b959b69a40a79f79b797df3151a2469
yiz <yiz@metrumrg.com>
2019-11-06 11:12:23 -0800

    inter chain and intra chain MPI communicators

--- src/stan/services/util/mpi.hpp
+++ src/stan/services/util/mpi.hpp
@@ -4,12 +4,24 @@
 #ifdef STAN_LANG_MPI
 
 #include <boost/mpi.hpp>
+#include <stan/callbacks/logger.hpp>
+#include <stan/mcmc/sample.hpp>
 #include <stan/math/prim/scal/err/check_greater.hpp>
+#include <stan/math/prim/scal/err/check_greater_or_equal.hpp>
 #include <stan/math/prim/scal/err/check_less.hpp>
 #include <stan/math/prim/mat/fun/Eigen.hpp>
 #include <sstream>
 #include <string>
 #include <vector>
+
+// default comm to world comm, in case stan needs to be
+// called as library.
+#define MPI_COMM_STAN MPI_COMM_WORLD
+
+// by default there are no warmup-pulling chains.
+#ifndef NUM_MPI_CHAINS
+#define NUM_MPI_CHAINS 1
+#endif
 
 namespace stan {
 namespace services {
@@ -20,31 +32,40 @@ namespace mpi {
    * MPI Evionment that initializes and finalizes the MPI
    */
   struct Envionment {
-    Envionment() {
-      init();
-    }
-    ~Envionment() {
-      finalize();
-    }
-
-    static void init() {
-#ifdef STAN_LANG_MPI
-      int flag;
-      MPI_Initialized(&flag);
-      if(!flag) {
-        MPI_Init(NULL, NULL);
+    struct Envionment_ {
+      Envionment_() {
+        init();
       }
-#endif
-    }
+      ~Envionment_() {
+        finalize();
+      }
 
-    static void finalize() {
+      static void init() {
 #ifdef STAN_LANG_MPI
-      int flag;
-      MPI_Finalized(&flag);
-      if(!flag) MPI_Finalize();
+        int flag;
+        MPI_Initialized(&flag);
+        if(!flag) {
+          int provided;
+          MPI_Init_thread(NULL, NULL, MPI_THREAD_SINGLE, &provided);
+          // print provided when needed
+        }
 #endif
-    }
+      }
+
+      static void finalize() {
+#ifdef STAN_LANG_MPI
+        int flag;
+        MPI_Finalized(&flag);
+        if(!flag) MPI_Finalize();
+#endif
+      }
+    };
+
+    static const Envionment_ env;
   };
+
+  // out-of-line initilization
+  const Envionment::Envionment_ Envionment::env;
 
   /*
    * MPI Communicators. With default constructor disabled,
@@ -54,8 +75,6 @@ namespace mpi {
   private:
     Communicator();
 
-    const Envionment& env_;
-
   public:
     MPI_Comm comm;
     int size;
@@ -64,18 +83,20 @@ namespace mpi {
     /*
      * communicator constructor using @c Envionment and @c MPI_Comm
      */
-    Communicator(const Envionment& env, MPI_Comm other) :
-      env_(env), comm(MPI_COMM_NULL) {
-      MPI_Comm_dup(other, &comm);
-      MPI_Comm_size(comm, &size);
-      MPI_Comm_rank(comm, &rank);        
+    explicit Communicator(MPI_Comm other) :
+      comm(MPI_COMM_NULL), size(0), rank(-1) {
+      if (other != MPI_COMM_NULL) {
+        MPI_Comm_dup(other, &comm);
+        MPI_Comm_size(comm, &size);
+        MPI_Comm_rank(comm, &rank);        
+      }
     }
 
     /*
      * copy constructor is deep
      */
     explicit Communicator(const Communicator& other) :
-      Communicator(other.env_, other.comm)
+      Communicator(other.comm)
     {}
 
     /*
@@ -89,55 +110,123 @@ namespace mpi {
      * destructor needs to free MPI_Comm
      */
     ~Communicator() {
-      MPI_Comm_free(&comm);
+      if (comm != MPI_COMM_NULL) {
+        MPI_Comm_free(&comm);
+      }
     }
   };
   
-#define NUM_STAN_LANG_MPI_COMM 1
-#define STAN_LANG_MPI_COMM_WARMUP 0
+  MPI_Comm inter_chain_comm(int num_mpi_chains) {
+    Envionment::env.init();
 
-  /*
-   * MPI communicator wrapper for RAII. Note that no
-   * MPI's predfined comm such as @c MPI_COMM_WOLRD are allowed.
-   */
-  template<int N_comm>
+    int world_size;
+    MPI_Comm_size(MPI_COMM_STAN, &world_size);
+    stan::math::check_greater_or_equal("MPI inter-chain session",
+                                       "number of procs", world_size,
+                                       num_mpi_chains);
+
+    MPI_Group stan_group, new_group;
+    MPI_Comm_group(MPI_COMM_STAN, &stan_group);
+    int num_chain_with_extra_proc = world_size % num_mpi_chains;
+    int num_proc_per_chain = world_size / num_mpi_chains;
+    std::vector<int> ranks(num_mpi_chains);
+    if (num_chain_with_extra_proc == 0) {
+      for (int i = 0, j = 0; i < world_size; i += num_proc_per_chain, ++j) {
+        ranks[j] = i;
+      }
+    } else {
+      num_proc_per_chain++;
+      int i = 0;
+      for (int j = 0; j < num_chain_with_extra_proc; ++j) {
+        ranks[j] = i;
+        i += num_proc_per_chain;
+      }
+      num_proc_per_chain--;
+      for (int j = num_chain_with_extra_proc; j < num_mpi_chains; ++j) {
+        ranks[j] = i;
+        i += num_proc_per_chain;
+      }
+    }
+
+    MPI_Group_incl(stan_group, num_mpi_chains, ranks.data(), &new_group);
+    MPI_Comm new_inter_comm, new_intra_comm;
+    MPI_Comm_create_group(MPI_COMM_STAN, new_group, 99, &new_inter_comm);
+    MPI_Group_free(&new_group);
+    MPI_Group_free(&stan_group);
+    return new_inter_comm;
+  }
+
+  MPI_Comm intra_chain_comm(int num_mpi_chains) {
+    Envionment::env.init();
+
+    int world_size, world_rank, color;
+    MPI_Comm_size(MPI_COMM_STAN, &world_size);
+    MPI_Comm_rank(MPI_COMM_STAN, &world_rank);
+
+    int num_chain_with_extra_proc = world_size % num_mpi_chains;
+    const int n_proc = world_size / num_mpi_chains;
+    if (num_chain_with_extra_proc == 0) {
+      color = world_rank / n_proc;
+    } else {
+      int i = 0;
+      for (int j = 0; j < num_mpi_chains; ++j) {
+        const int n = j < num_chain_with_extra_proc ? (n_proc + 1) : n_proc;
+        if (world_rank >= i && world_rank < i + n) {
+          color = i;
+          break;
+        }
+        i += n;
+      }
+    }
+
+    MPI_Comm new_intra_comm;
+    MPI_Comm_split(MPI_COMM_STAN, color, world_rank, &new_intra_comm);
+    return new_intra_comm;
+  }
+
+   // * MPI communicator wrapper for RAII. Note that no
+   // * MPI's predfined comm such as @c MPI_COMM_WOLRD are allowed.
+  template<int num_mpi_chains>
   struct Session {
-    static Envionment env;
-    static std::vector<Communicator> comms;
+    static const Communicator stan_comm;
+    static const MPI_Comm MPI_COMM_INTER_CHAIN;
+    static const MPI_Comm MPI_COMM_INTRA_CHAIN;
   };
 
-  template<int N_comm>
-  Envionment Session<N_comm>::env;
+  template<int num_mpi_chains>
+  const Communicator Session<num_mpi_chains>::stan_comm(MPI_COMM_WORLD);
 
-  template<int N_comm>
-  std::vector<Communicator> Session<N_comm>::comms(N_comm, Communicator(Session<N_comm>::env, MPI_COMM_WORLD));
+  template<int num_mpi_chains>
+  const MPI_Comm Session<num_mpi_chains>::
+  MPI_COMM_INTER_CHAIN(inter_chain_comm(num_mpi_chains));
+
+  template<int num_mpi_chains>
+  const MPI_Comm Session<num_mpi_chains>::
+  MPI_COMM_INTRA_CHAIN(intra_chain_comm(num_mpi_chains));
 
   /**
    * Dynamic loader that manages master & slave
    * communication and data assembly.
    */
-  struct warmup_dynamic_loader_base {
-    static const int work_tag      = 1;
-    static const int err_tag       = 2;
-    static const int adapt_tag     = 3;
+  struct mpi_loader_base {
+    static const int work_tag = 1;
+    static const int err_tag  = 2;
+    static const int done_tag = 3;
 
     //! communicator wrapper for warmup
-    const Communicator& warmup_comm;
+    const Communicator& comm;
     //! MPI communicator
-    const MPI_Comm comm;
-    //! communication interval
-    int interval;
+    const MPI_Comm mpi_comm;
     //! double workspace
     Eigen::MatrixXd workspace_r;
 
     //! construct loader given MPI communicator
-    warmup_dynamic_loader_base(const Communicator& comm_in, int inter) :
-      warmup_comm(comm_in), comm(warmup_comm.comm),
-      interval(inter)
+    mpi_loader_base(const Communicator& comm_in) :
+      comm(comm_in), mpi_comm(comm.comm)
     {
       // make sure there are slave chains.
-      static const char* caller = "warmup_dynamic_loader";
-      stan::math::check_greater(caller, "MPI comm size", warmup_comm.size, 1);      
+      static const char* caller = "MPI load balance initialization";
+      stan::math::check_greater(caller, "MPI comm size", comm.size, 1);      
     }
   };
 
@@ -147,12 +236,19 @@ namespace mpi {
    * in order to improve the quality of adaptation, before
    * sending the improved adapt info to slave chains.
    */
-  struct warmup_dynamic_loader_master : warmup_dynamic_loader_base {
+  struct warmup_dynamic_loader_master : mpi_loader_base {
+    MPI_Request bcast_req;
+    int interval;
+
     //! construct loader given MPI communicator
-    warmup_dynamic_loader_master(const Communicator& comm_in,
-                                 int inter) :
-      warmup_dynamic_loader_base(comm_in, inter)
+    warmup_dynamic_loader_master(const Communicator& comm_in, int inter) :
+      mpi_loader_base(comm_in), interval(inter)
     {}
+
+    //! during destruction ensure MPI request is fulfilled.
+    ~warmup_dynamic_loader_master() {
+      MPI_Wait(&bcast_req, MPI_STATUS_IGNORE);
+    }
 
     /*
      * helper function to master node (rank = 0) to recv
@@ -183,12 +279,12 @@ namespace mpi {
      * available tasks to vacant slaves.
      */
     template<typename Sampler, typename Model,
-             typename Send_processor, typename Recv_processor,
-             typename Post_processor>
+             typename Send_processor, typename Recv_processor>
     void operator()(Sampler& sampler, Model& model,
+                    const stan::mcmc::sample& sample,
+                    stan::callbacks::logger& logger,
                     Send_processor& ensemble_func,
-                    Recv_processor& chain_func,
-                    Post_processor& post_func) {
+                    Recv_processor& chain_func) {
       static const char* caller = "warmup_dynamic_loader_master::master";
       stan::math::check_less(caller, "MPI comm rank", warmup_comm.rank, 1);
 
@@ -210,8 +306,6 @@ namespace mpi {
         irecve++;
       }
 
-      MPI_Request bcast_req;
-
       if (is_invalid) {
         for (int i = 1; i < warmup_comm.size; ++i) {
           MPI_Send(workspace_r.data(), 0, MPI_DOUBLE, i, err_tag, comm);
@@ -226,27 +320,24 @@ namespace mpi {
         throw std::runtime_error(chain_adapt_fail_msg.str());
       } else {
         for (int i = 1; i < warmup_comm.size; ++i) {
-          MPI_Send(workspace_r.data(), 0, MPI_DOUBLE, i, adapt_tag, comm);
+          MPI_Send(workspace_r.data(), 0, MPI_DOUBLE, i, done_tag, comm);
         }
         while (recved != nslave) {
           int index, flag = 0;
           MPI_Testany(nslave, req.data(), &index, &flag, MPI_STATUS_IGNORE);
           if(flag) {
             recved++;
-            chain_func(sampler, model, workspace_r, index + 1);
+            chain_func(sampler, model, sample, logger, workspace_r, index + 1);
           }
         }
-        ensemble_func(sampler, model, workspace_r);
+        ensemble_func(sampler, model, sample, logger, workspace_r);
         MPI_Ibcast(workspace_r.data(), ensemble_func.send_size(sampler, model),
                    MPI_DOUBLE, 0, comm, &bcast_req);
       }
-
-      post_func(sampler, model, workspace_r);
-      MPI_Wait(&bcast_req, MPI_STATUS_IGNORE);
     }
   };
 
-  struct warmup_dynamic_loader_slave : warmup_dynamic_loader_base {
+  struct warmup_dynamic_loader_slave : mpi_loader_base {
     //! construct loader given MPI communicator
     warmup_dynamic_loader_slave(const Communicator& comm_in,
                                 int inter) :
@@ -259,6 +350,8 @@ namespace mpi {
      */
     template<typename Sampler, typename Model, typename Send_processor, typename Recv_processor>
     void operator()(Sampler& sampler, Model& model,
+                    const stan::mcmc::sample& sample,
+                    stan::callbacks::logger& logger,
                     Send_processor& chain_func,
                     Recv_processor& adapt_func) {
       using Eigen::MatrixXd;
@@ -268,7 +361,7 @@ namespace mpi {
       stan::math::check_greater(caller, "MPI comm rank", warmup_comm.rank, 0);
 
       // process adapt info before sending out to master
-      chain_func(sampler, model, workspace_r, warmup_comm.rank);
+      chain_func(sampler, model, sample, logger, workspace_r, warmup_comm.rank);
       MPI_Send(workspace_r.data(), chain_func.send_size(sampler, model), MPI_DOUBLE, 0, work_tag, comm);
 
       MPI_Status stat;
@@ -278,12 +371,12 @@ namespace mpi {
         std::ostringstream chain_adapt_fail_msg;
         chain_adapt_fail_msg << "Invalid adaptation data in ensemble";
         throw std::runtime_error(chain_adapt_fail_msg.str());
-      } else if (stat.MPI_TAG == adapt_tag) {
+      } else if (stat.MPI_TAG == done_tag) {
         workspace_r.resize(adapt_func.recv_size(sampler, model), 1);
         MPI_Ibcast(workspace_r.data(), adapt_func.recv_size(sampler, model),
                    MPI_DOUBLE, 0, comm, &bcast_req);
         MPI_Wait(&bcast_req, MPI_STATUS_IGNORE);
-        adapt_func(sampler, model, workspace_r);
+        adapt_func(sampler, model, sample, logger, workspace_r);
       }
     }
   };
--- src/test/unit/services/util/mpi_warmup_test.cpp
+++ src/test/unit/services/util/mpi_warmup_test.cpp
@@ -22,6 +22,8 @@ struct dummy_master_ensemble_processor {
 
   template<typename Sampler, typename Model>
   void operator()(Sampler& sampler, Model& model,
+                  const stan::mcmc::sample& sample,
+                  stan::callbacks::logger& logger,
                   Eigen::MatrixXd& workspace_r) {
     for (int i = 0; i < workspace_r.cols(); ++i) {
       workspace_r(i, 0) = workspace_r(i + 1, i);
@@ -37,15 +39,10 @@ struct dummy_master_chain_processor {
 
   template<typename Sampler, typename Model>
   void operator()(Sampler& sampler, Model& model,
+                  const stan::mcmc::sample& sample,
+                  stan::callbacks::logger& logger,
                   Eigen::MatrixXd& workspace_r, int index) {
     workspace_r(index, index - 1) *= 2.5;
-  }
-};
-
-struct dummy_master_post_processor {
-  template<typename Sampler, typename Model>
-  void operator()(Sampler& sampler, Model& model,
-                  Eigen::MatrixXd& workspace_r) {
   }
 };
 
@@ -57,6 +54,8 @@ struct dummy_slave_chain_processor {
 
   template<typename Sampler, typename Model>
   void operator()(Sampler& sampler, Model& model,
+                  const stan::mcmc::sample& sample,
+                  stan::callbacks::logger& logger,
                   Eigen::MatrixXd& workspace_r, int index) {
     workspace_r.resize(send_size(sampler, model), 1);
     workspace_r.setZero();
@@ -72,41 +71,168 @@ struct dummy_slave_adapt_processor {
 
   template<typename Sampler, typename Model>
   void operator()(Sampler& sampler, Model& model,
+                  const stan::mcmc::sample& sample,
+                  stan::callbacks::logger& logger,
                   Eigen::MatrixXd& workspace_r) {
     workspace_r *= 1.5;
   }
 };
 
-TEST(mpi_warmup_test, mpi_session) {
-  const Communicator& warmup_comm =
-    Session<NUM_STAN_LANG_MPI_COMM>::comms[0];
-
-  warmup_dynamic_loader_base load(warmup_comm, 10);
-}
-
-TEST(mpi_warmup_test, mpi_master_slave) {
-  const Communicator& warmup_comm =
-    Session<NUM_STAN_LANG_MPI_COMM>::comms[0];
-
-  dummy_sampler sampler;
-  dummy_model model;
-
-  if (warmup_comm.rank == 0) {
-    warmup_dynamic_loader_master master(warmup_comm, 10);
-    dummy_master_ensemble_processor f;
-    dummy_master_chain_processor g;
-    dummy_master_post_processor h;
-    EXPECT_NO_THROW(master(sampler, model, f, g, h));
-    EXPECT_FLOAT_EQ(master.workspace_r(0), 2.5);
-    EXPECT_FLOAT_EQ(master.workspace_r(1), 5.0);
-  } else {
-    warmup_dynamic_loader_slave slave(warmup_comm, 10);    
-    dummy_slave_chain_processor f;
-    dummy_slave_adapt_processor g;
-    EXPECT_NO_THROW(slave(sampler, model, f, g));
-    EXPECT_FLOAT_EQ(slave.workspace_r(0), 3.75);
-    EXPECT_FLOAT_EQ(slave.workspace_r(1), 7.50);
+TEST(mpi_warmup_test, mpi_inter_intra_comms) {
+  const Communicator world_comm(MPI_COMM_STAN);
+  const Communicator inter_comm(Session<3>::MPI_COMM_INTER_CHAIN);
+  const Communicator intra_comm(Session<3>::MPI_COMM_INTRA_CHAIN);
+  if (world_comm.size == 3) {
+    switch (world_comm.rank) {
+    case 0:
+      EXPECT_EQ(inter_comm.rank, 0);
+      EXPECT_EQ(intra_comm.rank, 0);
+      break;
+    case 1:
+      EXPECT_EQ(inter_comm.rank, 1);
+      EXPECT_EQ(intra_comm.rank, 0);
+      break;
+    case 2:
+      EXPECT_EQ(inter_comm.rank, 2);
+      EXPECT_EQ(intra_comm.rank, 0);
+      break;
+    }
+  } else if (world_comm.size == 4) {
+    switch (world_comm.rank) {
+    case 0:
+      EXPECT_EQ(inter_comm.rank, 0);
+      EXPECT_EQ(intra_comm.rank, 0);
+      break;
+    case 1:
+      EXPECT_EQ(inter_comm.rank, -1);
+      EXPECT_EQ(intra_comm.rank, 1);
+      break;
+    case 2:
+      EXPECT_EQ(inter_comm.rank, 1);
+      EXPECT_EQ(intra_comm.rank, 0);
+      break;
+    case 3:
+      EXPECT_EQ(inter_comm.rank, 2);
+      EXPECT_EQ(intra_comm.rank, 0);
+      break;
+    }
+  } else if (world_comm.size == 5) {
+    switch (world_comm.rank) {
+    case 0:
+      EXPECT_EQ(inter_comm.rank, 0);
+      EXPECT_EQ(intra_comm.rank, 0);
+      break;
+    case 1:
+      EXPECT_EQ(inter_comm.rank, -1);
+      EXPECT_EQ(intra_comm.rank, 1);
+      break;
+    case 2:
+      EXPECT_EQ(inter_comm.rank, 1);
+      EXPECT_EQ(intra_comm.rank, 0);
+      break;
+    case 3:
+      EXPECT_EQ(inter_comm.rank, -1);
+      EXPECT_EQ(intra_comm.rank, 1);
+      break;
+    case 4:
+      EXPECT_EQ(inter_comm.rank, 2);
+      EXPECT_EQ(intra_comm.rank, 0);
+      break;
+    }
+  } else if (world_comm.size == 6) {
+    switch (world_comm.rank) {
+    case 0:
+      EXPECT_EQ(inter_comm.rank, 0);
+      EXPECT_EQ(intra_comm.rank, 0);
+      break;
+    case 1:
+      EXPECT_EQ(inter_comm.rank, -1);
+      EXPECT_EQ(intra_comm.rank, 1);
+      break;
+    case 2:
+      EXPECT_EQ(inter_comm.rank, 1);
+      EXPECT_EQ(intra_comm.rank, 0);
+      break;
+    case 3:
+      EXPECT_EQ(inter_comm.rank, -1);
+      EXPECT_EQ(intra_comm.rank, 1);
+      break;
+    case 4:
+      EXPECT_EQ(inter_comm.rank, 2);
+      EXPECT_EQ(intra_comm.rank, 0);
+      break;
+    case 5:
+      EXPECT_EQ(inter_comm.rank, -1);
+      EXPECT_EQ(intra_comm.rank, 1);
+      break;
+    }
+  } else if (world_comm.size == 7) {
+    switch (world_comm.rank) {
+    case 0:
+      EXPECT_EQ(inter_comm.rank, 0);
+      EXPECT_EQ(intra_comm.rank, 0);
+      break;
+    case 1:
+      EXPECT_EQ(inter_comm.rank, -1);
+      EXPECT_EQ(intra_comm.rank, 1);
+      break;
+    case 2:
+      EXPECT_EQ(inter_comm.rank, -1);
+      EXPECT_EQ(intra_comm.rank, 2);
+      break;
+    case 3:
+      EXPECT_EQ(inter_comm.rank, 1);
+      EXPECT_EQ(intra_comm.rank, 0);
+      break;
+    case 4:
+      EXPECT_EQ(inter_comm.rank, -1);
+      EXPECT_EQ(intra_comm.rank, 1);
+      break;
+    case 5:
+      EXPECT_EQ(inter_comm.rank, 2);
+      EXPECT_EQ(intra_comm.rank, 0);
+      break;
+    case 6:
+      EXPECT_EQ(inter_comm.rank, -1);
+      EXPECT_EQ(intra_comm.rank, 1);
+      break;
+    }
   }
 }
+
+// TEST(mpi_warmup_test, mpi_warmup) {
+//   const Communicator world_comm(MPI_COMM_STAN);
+//   const Communicator inter_comm(Session<3>::MPI_COMM_INTER_CHAIN);
+//   const Communicator intra_comm(Session<3>::MPI_COMM_INTRA_CHAIN);
+//   // 
+//   // warmup_dynamic_loader_base load(warmup_comm, 10);
+
+// }
+
+// TEST(mpi_warmup_test, mpi_master_slave) {
+//   const Communicator& warmup_comm =
+//     Session<NUM_STAN_LANG_MPI_COMM>::comms[0];
+
+//   dummy_sampler sampler;
+//   dummy_model model;
+//   stan::mcmc::sample sample(Eigen::VectorXd(0), 0, 0);
+//   stan::callbacks::logger logger;
+
+//   if (warmup_comm.rank == 0) {
+//     warmup_dynamic_loader_master master(warmup_comm, 10);
+//     dummy_master_ensemble_processor f;
+//     dummy_master_chain_processor g;
+//     EXPECT_NO_THROW(master(sampler, model, sample, logger, f, g));
+//     EXPECT_FLOAT_EQ(master.workspace_r(0), 2.5);
+//     EXPECT_FLOAT_EQ(master.workspace_r(1), 5.0);
+//   } else {
+//     warmup_dynamic_loader_slave slave(warmup_comm, 10);    
+//     dummy_slave_chain_processor f;
+//     dummy_slave_adapt_processor g;
+//     EXPECT_NO_THROW(slave(sampler, model, sample, logger, f, g));
+//     EXPECT_FLOAT_EQ(slave.workspace_r(0), 3.75);
+//     EXPECT_FLOAT_EQ(slave.workspace_r(1), 7.50);
+//   }
+// }
 
 #endif
