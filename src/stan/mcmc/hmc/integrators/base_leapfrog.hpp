e5ad2f658a9acaa03faf7f7d28a21ecc3c5c55f3
Steve Bronder <stevo15025@gmail.com>
2019-11-11 01:56:40 -0500

    add back verbose_evolve

--- src/stan/mcmc/hmc/integrators/base_leapfrog.hpp
+++ src/stan/mcmc/hmc/integrators/base_leapfrog.hpp
@@ -31,6 +31,83 @@ class base_leapfrog
     return static_cast<Derived const&>(*this);
   }
 
+
+  inline void verbose_evolve(point_type& z,
+                      Hamiltonian& hamiltonian, const double epsilon,
+                      callbacks::logger& logger) {
+    std::stringstream msg;
+    msg.precision(6);
+
+    int width = 14;
+    int nColumn = 4;
+
+    msg << "Verbose Hamiltonian Evolution, Step Size = " << epsilon << ":";
+    logger.info(msg);
+
+    msg.str("");
+    msg << "    " << std::setw(nColumn * width) << std::setfill('-') << ""
+        << std::setfill(' ');
+    logger.info(msg);
+
+    msg.str("");
+    msg << "    " << std::setw(width) << std::left << "Poisson"
+        << std::setw(width) << std::left << "Initial" << std::setw(width)
+        << std::left << "Current" << std::setw(width) << std::left << "DeltaH";
+    logger.info(msg);
+
+    msg.str("");
+    msg << "    " << std::setw(width) << std::left << "Operator"
+        << std::setw(width) << std::left << "Hamiltonian" << std::setw(width)
+        << std::left << "Hamiltonian" << std::setw(width) << std::left
+        << "/ Stepsize^{2}";
+    logger.info(msg);
+
+    msg.str("");
+    msg << "    " << std::setw(nColumn * width) << std::setfill('-') << ""
+        << std::setfill(' ');
+    logger.info(msg);
+
+    double H0 = hamiltonian.H(z);
+
+    begin_update_p(z, hamiltonian, 0.5 * epsilon, logger);
+
+    double H1 = hamiltonian.H(z);
+
+    msg.str("");
+    msg << "    " << std::setw(width) << std::left << "hat{V}/2"
+        << std::setw(width) << std::left << H0 << std::setw(width) << std::left
+        << H1 << std::setw(width) << std::left
+        << (H1 - H0) / (epsilon * epsilon);
+    logger.info(msg);
+
+    update_q(z, hamiltonian, epsilon, logger);
+
+    double H2 = hamiltonian.H(z);
+
+    msg.str("");
+    msg << "    " << std::setw(width) << std::left << "hat{T}"
+        << std::setw(width) << std::left << H0 << std::setw(width) << std::left
+        << H2 << std::setw(width) << std::left
+        << (H2 - H0) / (epsilon * epsilon);
+    logger.info(msg);
+
+    end_update_p(z, hamiltonian, 0.5 * epsilon, logger);
+
+    double H3 = hamiltonian.H(z);
+
+    msg.str("");
+    msg << "    " << std::setw(width) << std::left << "hat{V}/2"
+        << std::setw(width) << std::left << H0 << std::setw(width) << std::left
+        << H3 << std::setw(width) << std::left
+        << (H3 - H0) / (epsilon * epsilon);
+    logger.info(msg);
+
+    msg.str("");
+    msg << "    " << std::setw(nColumn * width) << std::setfill('-') << ""
+        << std::setfill(' ');
+    logger.info(msg);
+  }
+
   inline void evolve(point_type& z, hamiltonian_type& hamiltonian,
                      const double epsilon, callbacks::logger& logger) {
     this->derived().begin_update_p(z, hamiltonian, 0.5 * epsilon, logger);
