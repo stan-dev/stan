4d8774f8459f1f60a2387526d769fda7f90f6783
rok-cesnovar <rok.cesnovar@fri.uni-lj.si>
2020-04-27 21:47:36 +0200

    cleanup templating

--- src/stan/model/indexing/lvalue.hpp
+++ src/stan/model/indexing/lvalue.hpp
@@ -118,7 +118,7 @@ inline void assign(Eigen::Matrix<T, 1, Eigen::Dynamic>& x,
  * the indexed size.
  */
 template <typename T, typename I, typename U>
-inline typename std::enable_if<!std::is_same<I, index_uni>::value, void>::type
+inline typename std::enable_if_t<!std::is_same<I, index_uni>::value>::type
 assign(Eigen::Matrix<T, Eigen::Dynamic, 1>& x,
        const cons_index_list<I, nil_index_list>& idxs,
        const Eigen::Matrix<U, Eigen::Dynamic, 1>& y, const char* name = "ANON",
@@ -153,7 +153,7 @@ assign(Eigen::Matrix<T, Eigen::Dynamic, 1>& x,
  * the indexed size.
  */
 template <typename T, typename I, typename U>
-inline typename std::enable_if<!std::is_same<I, index_uni>::value, void>::type
+inline typename std::enable_if_t<!std::is_same<I, index_uni>::value>::type
 assign(Eigen::Matrix<T, 1, Eigen::Dynamic>& x,
        const cons_index_list<I, nil_index_list>& idxs,
        const Eigen::Matrix<U, 1, Eigen::Dynamic>& y, const char* name = "ANON",
@@ -218,7 +218,7 @@ void assign(Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>& x,
  * matrix and right-hand side matrix do not match.
  */
 template <typename T, typename I, typename U>
-inline typename std::enable_if<!std::is_same<I, index_uni>::value, void>::type
+inline typename std::enable_if_t<!std::is_same<I, index_uni>::value>::type
 assign(Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>& x,
        const cons_index_list<I, nil_index_list>& idxs,
        const Eigen::Matrix<U, Eigen::Dynamic, Eigen::Dynamic>& y,
@@ -284,7 +284,7 @@ void assign(Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>& x,
  * matrix and right-hand side row vector do not match.
  */
 template <typename T, typename I, typename U>
-inline typename std::enable_if<!std::is_same<I, index_uni>::value, void>::type
+inline typename std::enable_if_t<!std::is_same<I, index_uni>::value>::type
 assign(
     Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>& x,
     const cons_index_list<index_uni, cons_index_list<I, nil_index_list> >& idxs,
@@ -321,7 +321,7 @@ assign(
  * matrix and right-hand side vector do not match.
  */
 template <typename T, typename I, typename U>
-inline typename std::enable_if<!std::is_same<I, index_uni>::value, void>::type
+inline typename std::enable_if_t<!std::is_same<I, index_uni>::value>::type
 assign(
     Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>& x,
     const cons_index_list<I, cons_index_list<index_uni, nil_index_list> >& idxs,
@@ -359,9 +359,8 @@ assign(
  * matrix and value matrix do not match.
  */
 template <typename T, typename I1, typename I2, typename U>
-inline typename std::enable_if_t<std::is_same<I1, index_uni>::value
-                                     || std::is_same<I2, index_uni>::value,
-                                 void>::type
+inline typename std::enable_if_t<!std::is_same<I1, index_uni>::value
+                                     && !std::is_same<I2, index_uni>::value>::type
 assign(Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>& x,
        const cons_index_list<I1, cons_index_list<I2, nil_index_list> >& idxs,
        const Eigen::Matrix<U, Eigen::Dynamic, Eigen::Dynamic>& y,
@@ -440,7 +439,7 @@ inline void assign(std::vector<T>& x, const cons_index_list<index_uni, L>& idxs,
  * the recursive tail assignment dimensions do not match.
  */
 template <typename T, typename I, typename L, typename U>
-typename std::enable_if<std::is_same<I, index_uni>::value, void>::
+typename std::enable_if_t<!std::is_same<I, index_uni>::value>::
     type inline assign(std::vector<T>& x, const cons_index_list<I, L>& idxs,
                        const std::vector<U>& y, const char* name = "ANON",
                        int depth = 0) {
