e388852982a4601ee6a21cd7078fab9a9b701820
rok-cesnovar <rok.cesnovar@fri.uni-lj.si>
2020-05-29 16:07:15 +0200

    fix sampling time measure

--- src/stan/services/util/run_sampler.hpp
+++ src/stan/services/util/run_sampler.hpp
@@ -63,7 +63,7 @@ void run_sampler(stan::mcmc::base_mcmc& sampler, Model& model,
                              num_warmup + num_samples, num_thin, refresh, true,
                              false, writer, s, model, rng, interrupt, logger);
   auto end_sample = std::chrono::steady_clock::now();
-  std::chrono::duration<double> sample_delta_t = end_warm - start_warm;
+  std::chrono::duration<double> sample_delta_t = end_sample - start_sample;
 
   writer.write_timing(warm_delta_t.count(), sample_delta_t.count());
 }
