ed9b288b194a6b135f565c4ea666856a376bfeaf
rok-cesnovar <rok.cesnovar@fri.uni-lj.si>
2020-06-07 16:20:27 +0200

    fix util test

--- src/test/unit/util_test.cpp
+++ src/test/unit/util_test.cpp
@@ -2,13 +2,14 @@
 #include <test/unit/util.hpp>
 
 TEST(TestUnitUtil, streams) {
+  stan::test::capture_std_streams();
   std::cout << "foo";
   std::cerr << "bar";
-  stan::test::capture_std_streams();
   EXPECT_EQ("foo", stan::test::cout_ss.str());
   EXPECT_EQ("bar", stan::test::cerr_ss.str());
 
   stan::test::reset_std_streams();
+  stan::test::capture_std_streams();
   EXPECT_EQ("", stan::test::cout_ss.str());
   EXPECT_EQ("", stan::test::cerr_ss.str());
 }
