5346a97296762d1dbccb5d8531cad748ab2e3fa1
Stan Jenkins <mc.stanislaw@gmail.com>
2020-10-05 06:46:58 -0400

    [Jenkins] auto-formatting by clang-format version 6.0.1-14 (tags/RELEASE_601/final)

--- src/test/integration/standalone_functions_test.cpp
+++ src/test/integration/standalone_functions_test.cpp
@@ -2,13 +2,13 @@
 #include <test/test-models/good-standalone-functions/basic.hpp>
 
 TEST(standalone_functions, int_only_multiplication) {
-  EXPECT_EQ(int_only_multiplication(2,5), 10);
+  EXPECT_EQ(int_only_multiplication(2, 5), 10);
   EXPECT_NEAR(my_log1p_exp(1), 1.3132, 1E-4);
   Eigen::Matrix<double, -1, 1> a(6), correct(6), res(6);
-  a << 1,2,3,4,5,6;
+  a << 1, 2, 3, 4, 5, 6;
   res = my_vector_mul_by_5(a);
-  correct << 5,10,15,20,25,30;
-  for(int i = 0; i< a.size(); i++) {
+  correct << 5, 10, 15, 20, 25, 30;
+  for (int i = 0; i < a.size(); i++) {
     EXPECT_EQ(res(i), correct(i));
   }
 }
