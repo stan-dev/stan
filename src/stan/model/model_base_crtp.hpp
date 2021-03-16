c378aff707dd6f9089562a617e1dde7ecc8b022d
Steve Bronder <stevo15025@gmail.com>
2021-03-16 18:31:58 -0400

    add const override to transform_inits()

--- src/stan/model/model_base_crtp.hpp
+++ src/stan/model/model_base_crtp.hpp
@@ -201,15 +201,9 @@ class model_base_crtp : public stan::model::model_base {
   }
 
   void transform_inits(const io::var_context& context,
-                       Eigen::VectorXd& params_r, std::ostream* msgs) const {
+                       Eigen::VectorXd& params_r, std::ostream* msgs) const override {
     return static_cast<const M*>(this)->template transform_inits(
         context, params_r, msgs);
-  }
-
-  void transform_inits(Eigen::VectorXd& input_r, Eigen::VectorXd& params_r,
-                       std::ostream* msgs) const {
-    return static_cast<const M*>(this)->template transform_inits(
-        input_r, params_r, msgs);
   }
 };
 
