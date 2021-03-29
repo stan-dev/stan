0758320a5b720f6ae44e239ca0ca2fd3a414e4cd
Steve Bronder <stevo15025@gmail.com>
2021-03-29 00:31:18 -0400

    adds function to initialize data on a local stack allocator

--- src/stan/io/initialize_data.hpp
+++ src/stan/io/initialize_data.hpp
@@ -7,19 +7,34 @@
 namespace stan {
   namespace io {
     namespace internal {
+      /**
+       * Turn a type into an Eigen Map.
+       */
       template <typename T, typename = void>
       struct convert_to_map;
-
+      /**
+       * No-op for scalar types
+       */
       template <typename T>
       struct convert_to_map<T, require_arithmetic_t<T>> {
         using type = T;
       };
 
+      /**
+       * Convert an Eigen type to an `Eigen::Map`
+       * @tparam An Eigen type to be wrapped in Map.
+       */
       template <typename T>
       struct convert_to_map<T, require_eigen_t<T>> {
-        using type = Eigen::Map<T>;
+        using type = Eigen::Map<plain_type_t<T>>;
       };
 
+      /**
+       * Convert an Eigen type and the inner type of std::vectors to Maps.
+       * @tparam For Scalars this is a no-op, for Eigen types this holds
+       *  a Map of the Eigen type's `plain_type`. For std vectors holding
+       *  Eigen types this converts the inner Eigen type into a Map.
+       */
       template <typename T>
       using convert_to_map_t = typename convert_to_map<std::decay_t<T>>::type;
 
@@ -28,26 +43,54 @@ namespace stan {
         using type = std::vector<convert_to_map_t<value_type_t<T>>>;
       };
     }
+    /**
+     * Construct an object onto Stan's stack allocator. For scalars this
+     * only returns an NaN.
+     * @tparam Ret A user specified return type.
+     */
     template <typename Ret, require_arithmetic_t<Ret>* = nullptr>
-    auto initialize_data(const std::string& name, stan::math::stack_alloc& /* allocator */) {
+    inline auto initialize_data(stan::math::stack_alloc& /* allocator */) {
       return std::numeric_limits<Ret>::quiet_NaN();
     }
+    /**
+     * Construct an `Eigen::Map<Vector>` with data allocated on the given allocator.
+     * @tparam Ret A user specified return type with a compile time row or column value of 1.
+     * @param allocator The allocator to allocate memory from.
+     * @param size The dynamic size of the vector.
+     */
     template <typename Ret, require_eigen_vector_t<Ret>* = nullptr>
-    auto initialize_data(const std::string& name, stan::math::stack_alloc& allocator, size_t size) {
+    inline auto initialize_data(stan::math::stack_alloc& allocator, size_t size) {
       return Eigen::Map<Ret>(allocator.alloc_array<double>(size), size);
     }
 
+    /**
+     * Construct an `Eigen::Map<Matrix>` with data allocated on the given allocator.
+     * @tparam Ret A user specified return type with Dynamic compile time rows and columns.
+     * @param allocator The allocator to allocate memory from.
+     * @param size The dynamic size of the vector.
+     */
     template <typename Ret, require_eigen_matrix_dynamic_t<Ret>* = nullptr>
-    auto initialize_data(const std::string& name, stan::math::stack_alloc& allocator, size_t rows, size_t cols) {
+    inline auto initialize_data(stan::math::stack_alloc& allocator, size_t rows, size_t cols) {
       return Eigen::Map<Ret>(allocator.alloc_array<double>(rows * cols), rows, cols);
     }
 
+    /**
+     * Construct an std::vector with data possibly allocated on the given allocator.
+     *  For std vectors holding primitive types this will be allocated on the general
+     *   allocator. But for std vectors holding containers this will dynamically
+     *   allocate the elements memory on the given allocator.
+     * @tparam Ret A std vector.
+     * @tparam Dims Parameter pack of integral types.
+     * @param allocator The allocator to allocate memory from.
+     * @param vec_size The dynamic size of the vector.
+     * @param dims A parameter packing holding the sizes of the elements.
+     */
     template <typename Ret, require_std_vector_t<Ret>* = nullptr, typename... Dims>
-    auto initialize_data(const std::string& name, stan::math::stack_alloc& allocator, size_t vec_size, Dims... dims) {
+    inline auto initialize_data(stan::math::stack_alloc& allocator, size_t vec_size, Dims... dims) {
       internal::convert_to_map_t<Ret> ret_vec;
       ret_vec.reserve(vec_size);
       for (size_t i = 0; i < vec_size; ++i) {
-        ret_vec.push_back(initialize_data<value_type_t<Ret>>(name, allocator, dims...));
+        ret_vec.push_back(initialize_data<value_type_t<Ret>>(allocator, dims...));
       }
       return ret_vec;
     }
--- src/test/unit/io/initialize_data_test.cpp
+++ src/test/unit/io/initialize_data_test.cpp
@@ -3,20 +3,45 @@
 
 TEST(initializer, scalar) {
   stan::math::stack_alloc allocator;
-  double xx = stan::io::initialize_data<double>("blah", allocator);
+  double xx = stan::io::initialize_data<double>(allocator);
+  EXPECT_FALSE(allocator.in_stack(static_cast<void*>(&xx)));
 }
 
 TEST(initializer, eigen) {
   stan::math::stack_alloc allocator;
-  Eigen::Map<Eigen::VectorXd> x_vec = stan::io::initialize_data<Eigen::VectorXd>("blah", allocator, 3);
-  Eigen::Map<Eigen::RowVectorXd> x_rowvec = stan::io::initialize_data<Eigen::RowVectorXd>("blah", allocator, 3);
-  Eigen::Map<Eigen::MatrixXd> x_mat = stan::io::initialize_data<Eigen::MatrixXd>("blah", allocator, 3, 4);
+  Eigen::Map<Eigen::VectorXd> x_vec = stan::io::initialize_data<Eigen::VectorXd>(allocator, 3);
+  EXPECT_EQ(3, x_vec.size());
+  EXPECT_TRUE(allocator.in_stack(static_cast<void*>(x_vec.data())));
+  Eigen::Map<Eigen::RowVectorXd> x_rowvec = stan::io::initialize_data<Eigen::RowVectorXd>(allocator, 3);
+  EXPECT_EQ(3, x_rowvec.size());
+  EXPECT_TRUE(allocator.in_stack(static_cast<void*>(x_rowvec.data())));
+  Eigen::Map<Eigen::MatrixXd> x_mat = stan::io::initialize_data<Eigen::MatrixXd>(allocator, 3, 4);
+  EXPECT_TRUE(allocator.in_stack(static_cast<void*>(x_mat.data())));
+  EXPECT_EQ(3, x_mat.rows());
+  EXPECT_EQ(4, x_mat.cols());
 }
 
 TEST(initializer, std_vector) {
   stan::math::stack_alloc allocator;
-  std::vector<double> xx = stan::io::initialize_data<std::vector<double>>("blah", allocator, 3);
-  std::vector<Eigen::Map<Eigen::VectorXd>> x_vec = stan::io::initialize_data<std::vector<Eigen::VectorXd>>("blah", allocator, 4, 3);
-  std::vector<Eigen::Map<Eigen::RowVectorXd>> x_rowvec = stan::io::initialize_data<std::vector<Eigen::RowVectorXd>>("blah", allocator, 4, 3);
-  std::vector<Eigen::Map<Eigen::MatrixXd>> x_mat = stan::io::initialize_data<std::vector<Eigen::MatrixXd>>("blah", allocator, 4, 3, 4);
+  std::vector<double> xx = stan::io::initialize_data<std::vector<double>>(allocator, 3);
+  std::vector<Eigen::Map<Eigen::VectorXd>> x_vec = stan::io::initialize_data<std::vector<Eigen::VectorXd>>(allocator, 4, 3);
+  EXPECT_EQ(4, x_vec.size());
+  for (int i = 0; i < x_vec.size(); ++i) {
+    EXPECT_EQ(3, x_vec[i].size());
+    EXPECT_TRUE(allocator.in_stack(static_cast<void*>(x_vec[i].data())));
+  }
+
+  std::vector<Eigen::Map<Eigen::RowVectorXd>> x_rowvec = stan::io::initialize_data<std::vector<Eigen::RowVectorXd>>(allocator, 2, 3);
+  EXPECT_EQ(2, x_rowvec.size());
+  for (int i = 0; i < x_rowvec.size(); ++i) {
+    EXPECT_EQ(3, x_rowvec[i].size());
+    EXPECT_TRUE(allocator.in_stack(static_cast<void*>(x_rowvec[i].data())));
+  }
+  std::vector<Eigen::Map<Eigen::MatrixXd>> x_mat = stan::io::initialize_data<std::vector<Eigen::MatrixXd>>(allocator, 4, 3, 4);
+  EXPECT_EQ(4, x_mat.size());
+  for (int i = 0; i < x_mat.size(); ++i) {
+    EXPECT_EQ(3, x_mat[i].rows());
+    EXPECT_EQ(4, x_mat[i].cols());
+    EXPECT_TRUE(allocator.in_stack(static_cast<void*>(x_mat[i].data())));
+  }
 }
