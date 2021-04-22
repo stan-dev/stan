323adb97fd222e2645c8d0902c86514fec3f28d2
Steve Bronder <stevo15025@gmail.com>
2021-04-22 13:55:22 -0400

    update to reflect parallel chain design doc

--- src/stan/services/sample/hmc_nuts_dense_e_adapt.hpp
+++ src/stan/services/sample/hmc_nuts_dense_e_adapt.hpp
@@ -157,8 +157,8 @@ int hmc_nuts_dense_e_adapt(
 }
 
 /**
- * Runs multiple chains of HMC with NUTS with adaptation using diagonal
- Euclidean metric with a pre-specified Euclidean metric.
+ * Runs multiple chains of NUTS with adaptation using dense Euclidean metric
+ * with a pre-specified Euclidean metric.
  *
  * @tparam Model Model class
  * @tparam InitContext A type derived from `stan::io::var_context`
@@ -172,7 +172,7 @@ int hmc_nuts_dense_e_adapt(
  * @param[in] init_inv_metric An std vector of var contexts exposing an initial
  diagonal inverse Euclidean metric for each chain (must be positive definite)
  * @param[in] random_seed random seed for the random number generator
- * @param[in] chain chain id to advance the pseudo random number generator
+ * @param[in] init_chain_id first chain id. The pseudo random number generator will advance by for each chain by an integer sequence from `init_chain_id` to `num_chains`
  * @param[in] init_radius radius to initialize
  * @param[in] num_warmup Number of warmup samples
  * @param[in] num_samples Number of samples
@@ -196,7 +196,7 @@ int hmc_nuts_dense_e_adapt(
  * @param[in,out] sample_writer std vector of Writers for draws of each chain.
  * @param[in,out] diagnostic_writer std vector of Writers for diagnostic
  information of each chain.
- * @param[in] n_chain The number of chains to run in parallel. `init`,
+ * @param[in] num_chains The number of chains to run in parallel. `init`,
  `init_inv_metric`, `init_writer`, `sample_writer`, and `diagnostic_writer` must
  be the same length as this value.
  * @return error_codes::OK if successful
@@ -206,7 +206,7 @@ template <class Model, typename InitContext, typename InitInvContext,
 int hmc_nuts_dense_e_adapt(
     Model& model, const std::vector<InitContext>& init,
     const std::vector<InitInvContext>& init_inv_metric,
-    unsigned int random_seed, unsigned int chain, double init_radius,
+    unsigned int random_seed, unsigned int init_chain_id, double init_radius,
     int num_warmup, int num_samples, int num_thin, bool save_warmup,
     int refresh, double stepsize, double stepsize_jitter, int max_depth,
     double delta, double gamma, double kappa, double t0,
@@ -214,26 +214,26 @@ int hmc_nuts_dense_e_adapt(
     callbacks::interrupt& interrupt, callbacks::logger& logger,
     std::vector<InitWriter>& init_writer,
     std::vector<SampleWriter>& sample_writer,
-    std::vector<DiagnosticWriter>& diagnostic_writer, size_t n_chain) {
+    std::vector<DiagnosticWriter>& diagnostic_writer, size_t num_chains) {
   using util::get_underlying;
-  if (n_chain == 1) {
+  if (num_chains == 1) {
     return hmc_nuts_dense_e_adapt(
         model, get_underlying(init[0]), get_underlying(init_inv_metric[0]),
-        random_seed, chain, init_radius, num_warmup, num_samples, num_thin,
+        random_seed, init_chain_id, init_radius, num_warmup, num_samples, num_thin,
         save_warmup, refresh, stepsize, stepsize_jitter, max_depth, delta,
         gamma, kappa, t0, init_buffer, term_buffer, window, interrupt, logger,
         init_writer[0], sample_writer[0], diagnostic_writer[0]);
   } else {
     using sample_t = stan::mcmc::adapt_dense_e_nuts<Model, boost::ecuyer1988>;
     std::vector<boost::ecuyer1988> rngs;
-    rngs.reserve(n_chain);
+    rngs.reserve(num_chains);
     std::vector<std::vector<double>> cont_vectors;
-    cont_vectors.reserve(n_chain);
+    cont_vectors.reserve(num_chains);
     std::vector<sample_t> samplers;
-    samplers.reserve(n_chain);
+    samplers.reserve(num_chains);
     try {
-      for (int i = 0; i < n_chain; ++i) {
-        rngs.emplace_back(util::create_rng(random_seed, chain + i));
+      for (int i = 0; i < num_chains; ++i) {
+        rngs.emplace_back(util::create_rng(random_seed, init_chain_id + i));
         cont_vectors.emplace_back(
             util::initialize(model, get_underlying(init[i]), rngs[i],
                              init_radius, true, logger, init_writer[i]));
@@ -259,8 +259,8 @@ int hmc_nuts_dense_e_adapt(
       return error_codes::CONFIG;
     }
     tbb::parallel_for(
-        tbb::blocked_range<size_t>(0, n_chain, 1),
-        [num_warmup, num_samples, num_thin, refresh, save_warmup, n_chain,
+        tbb::blocked_range<size_t>(0, num_chains, 1),
+        [num_warmup, num_samples, num_thin, refresh, save_warmup, num_chains,
          &samplers, &model, &rngs, &interrupt, &logger, &sample_writer,
          &cont_vectors,
          &diagnostic_writer](const tbb::blocked_range<size_t>& r) {
@@ -268,7 +268,7 @@ int hmc_nuts_dense_e_adapt(
             util::run_adaptive_sampler(
                 samplers[i], model, cont_vectors[i], num_warmup, num_samples,
                 num_thin, refresh, save_warmup, rngs[i], interrupt, logger,
-                sample_writer[i], diagnostic_writer[i], i + 1, n_chain);
+                sample_writer[i], diagnostic_writer[i], i + 1, num_chains);
           }
         },
         tbb::simple_partitioner());
@@ -277,11 +277,49 @@ int hmc_nuts_dense_e_adapt(
   }
 }
 
+/**
+ * Runs multiple chains of NUTS with adaptation using dense Euclidean metric,
+ * with identity matrix as initial inv_metric.
+ *
+ * @tparam Model Model class
+ * @param[in] model Input model to test (with data already instantiated)
+ * @param[in] init An std vector of init var contexts for initialization of each
+ * chain.
+ * @param[in] random_seed random seed for the random number generator
+ * @param[in] init_chain_id first chain id. The pseudo random number generator will advance by for each chain by an integer sequence from `init_chain_id` to `num_chains`
+ * @param[in] init_radius radius to initialize
+ * @param[in] num_warmup Number of warmup samples
+ * @param[in] num_samples Number of samples
+ * @param[in] num_thin Number to thin the samples
+ * @param[in] save_warmup Indicates whether to save the warmup iterations
+ * @param[in] refresh Controls the output
+ * @param[in] stepsize initial stepsize for discrete evolution
+ * @param[in] stepsize_jitter uniform random jitter of stepsize
+ * @param[in] max_depth Maximum tree depth
+ * @param[in] delta adaptation target acceptance statistic
+ * @param[in] gamma adaptation regularization scale
+ * @param[in] kappa adaptation relaxation exponent
+ * @param[in] t0 adaptation iteration offset
+ * @param[in] init_buffer width of initial fast adaptation interval
+ * @param[in] term_buffer width of final fast adaptation interval
+ * @param[in] window initial width of slow adaptation interval
+ * @param[in,out] interrupt Callback for interrupts
+ * @param[in,out] logger Logger for messages
+ * @param[in,out] init_writer std vector of Writer callbacks for unconstrained
+ * inits of each chain.
+ * @param[in,out] sample_writer std vector of Writers for draws of each chain.
+ * @param[in,out] diagnostic_writer std vector of Writers for diagnostic
+ * information of each chain.
+ * @param[in] num_chains The number of chains to run in parallel. `init`,
+ * `init_writer`, `sample_writer`, and `diagnostic_writer` must be the same
+ * length as this value.
+ * @return error_codes::OK if successful
+ */
 template <class Model, typename InitContext, typename InitWriter,
           typename SampleWriter, typename DiagnosticWriter>
 int hmc_nuts_dense_e_adapt(
     Model& model, const std::vector<InitContext>& init,
-    unsigned int random_seed, unsigned int chain, double init_radius,
+    unsigned int random_seed, unsigned int init_chain_id, double init_radius,
     int num_warmup, int num_samples, int num_thin, bool save_warmup,
     int refresh, double stepsize, double stepsize_jitter, int max_depth,
     double delta, double gamma, double kappa, double t0,
@@ -289,28 +327,28 @@ int hmc_nuts_dense_e_adapt(
     callbacks::interrupt& interrupt, callbacks::logger& logger,
     std::vector<InitWriter>& init_writer,
     std::vector<SampleWriter>& sample_writer,
-    std::vector<DiagnosticWriter>& diagnostic_writer, size_t n_chain = 1) {
+    std::vector<DiagnosticWriter>& diagnostic_writer, size_t num_chains = 1) {
   using util::get_underlying;
-  if (n_chain == 1) {
+  if (num_chains == 1) {
     return hmc_nuts_dense_e_adapt(
-        model, get_underlying(init[0]), random_seed, chain, init_radius,
+        model, get_underlying(init[0]), random_seed, init_chain_id, init_radius,
         num_warmup, num_samples, num_thin, save_warmup, refresh, stepsize,
         stepsize_jitter, max_depth, delta, gamma, kappa, t0, init_buffer,
         term_buffer, window, interrupt, logger, init_writer[0],
         sample_writer[0], diagnostic_writer[0]);
   } else {
     std::vector<stan::io::dump> unit_e_metrics;
-    unit_e_metrics.reserve(n_chain);
-    for (size_t i = 0; i < n_chain; ++i) {
+    unit_e_metrics.reserve(num_chains);
+    for (size_t i = 0; i < num_chains; ++i) {
       unit_e_metrics.emplace_back(
           util::create_unit_e_dense_inv_metric(model.num_params_r()));
     }
     return hmc_nuts_dense_e_adapt(
-        model, init, unit_e_metrics, random_seed, chain, init_radius,
+        model, init, unit_e_metrics, random_seed, init_chain_id, init_radius,
         num_warmup, num_samples, num_thin, save_warmup, refresh, stepsize,
         stepsize_jitter, max_depth, delta, gamma, kappa, t0, init_buffer,
         term_buffer, window, interrupt, logger, init_writer, sample_writer,
-        diagnostic_writer, n_chain);
+        diagnostic_writer, num_chains);
   }
 }
 
