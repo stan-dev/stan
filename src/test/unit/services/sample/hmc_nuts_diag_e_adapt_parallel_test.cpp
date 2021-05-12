e1e37a45004e16743a74c7cff0617684716eeef3
Stan Jenkins <mc.stanislaw@gmail.com>
2021-05-12 16:26:42 +0000

    [Jenkins] auto-formatting by clang-format version 6.0.0-1ubuntu2~16.04.1 (tags/RELEASE_600/final)

--- src/test/unit/services/sample/hmc_nuts_diag_e_adapt_parallel_test.cpp
+++ src/test/unit/services/sample/hmc_nuts_diag_e_adapt_parallel_test.cpp
@@ -51,10 +51,10 @@ TEST_F(ServicesSampleHmcNutsDiagEAdaptPar, call_count) {
   EXPECT_EQ(interrupt.call_count(), 0);
 
   int return_code = stan::services::sample::hmc_nuts_diag_e_adapt(
-      model, num_chains, context, random_seed, chain, init_radius, num_warmup, num_samples,
-      num_thin, save_warmup, refresh, stepsize, stepsize_jitter, max_depth,
-      delta, gamma, kappa, t0, init_buffer, term_buffer, window, interrupt,
-      logger, init, parameter, diagnostic);
+      model, num_chains, context, random_seed, chain, init_radius, num_warmup,
+      num_samples, num_thin, save_warmup, refresh, stepsize, stepsize_jitter,
+      max_depth, delta, gamma, kappa, t0, init_buffer, term_buffer, window,
+      interrupt, logger, init, parameter, diagnostic);
 
   EXPECT_EQ(0, return_code);
 
@@ -91,10 +91,10 @@ TEST_F(ServicesSampleHmcNutsDiagEAdaptPar, parameter_checks) {
   EXPECT_EQ(interrupt.call_count(), 0);
 
   int return_code = stan::services::sample::hmc_nuts_diag_e_adapt(
-      model, num_chains, context, random_seed, chain, init_radius, num_warmup, num_samples,
-      num_thin, save_warmup, refresh, stepsize, stepsize_jitter, max_depth,
-      delta, gamma, kappa, t0, init_buffer, term_buffer, window, interrupt,
-      logger, init, parameter, diagnostic);
+      model, num_chains, context, random_seed, chain, init_radius, num_warmup,
+      num_samples, num_thin, save_warmup, refresh, stepsize, stepsize_jitter,
+      max_depth, delta, gamma, kappa, t0, init_buffer, term_buffer, window,
+      interrupt, logger, init, parameter, diagnostic);
 
   for (size_t i = 0; i < num_chains; ++i) {
     std::vector<std::vector<std::string>> parameter_names;
@@ -155,10 +155,10 @@ TEST_F(ServicesSampleHmcNutsDiagEAdaptPar, output_regression) {
   EXPECT_EQ(interrupt.call_count(), 0);
 
   stan::services::sample::hmc_nuts_diag_e_adapt(
-      model, num_chains, context, random_seed, chain, init_radius, num_warmup, num_samples,
-      num_thin, save_warmup, refresh, stepsize, stepsize_jitter, max_depth,
-      delta, gamma, kappa, t0, init_buffer, term_buffer, window, interrupt,
-      logger, init, parameter, diagnostic);
+      model, num_chains, context, random_seed, chain, init_radius, num_warmup,
+      num_samples, num_thin, save_warmup, refresh, stepsize, stepsize_jitter,
+      max_depth, delta, gamma, kappa, t0, init_buffer, term_buffer, window,
+      interrupt, logger, init, parameter, diagnostic);
 
   for (auto&& init_it : init) {
     std::vector<std::string> init_values;
