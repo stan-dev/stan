20f907b85bf9ef33a875a400a3e4dce8ff5428a6
Steve Bronder <stevo15025@gmail.com>
2021-05-20 20:33:32 -0400

    use no check return macro in deserializer and make a specialization for reading in scalar vectors

--- src/stan/io/deserializer.hpp
+++ src/stan/io/deserializer.hpp
@@ -52,7 +52,8 @@ class deserializer {
    * @param m Number of reals to read
    * @throws std::runtime_error if there aren't at least m reals left
    */
-  void check_r_capacity(size_t m) const {
+  inline void check_r_capacity(size_t m) const {
+    STAN_NO_RANGE_CHECKS_RETURN;
     if (pos_r_ + m > r_size_) {
       []() STAN_COLD_PATH {
         throw std::runtime_error("no more scalars to read");
@@ -65,7 +66,8 @@ class deserializer {
    * @param m Number of integers to read
    * @throws std::runtime_error if there aren't at least m integers left
    */
-  void check_i_capacity(size_t m) const {
+  inline void check_i_capacity(size_t m) const {
+    STAN_NO_RANGE_CHECKS_RETURN;
     if (pos_i_ + m > i_size_) {
       []() STAN_COLD_PATH {
         throw std::runtime_error("no more integers to read");
@@ -324,7 +326,8 @@ class deserializer {
    * `read` functions.
    */
   template <typename Ret, typename... Sizes,
-            require_std_vector_t<Ret>* = nullptr>
+            require_std_vector_t<Ret>* = nullptr,
+            require_not_same_t<value_type_t<Ret>, T>* = nullptr>
   inline auto read(Eigen::Index m, Sizes... dims) {
     if (unlikely(m == 0)) {
       return Ret();
@@ -333,6 +336,29 @@ class deserializer {
       ret_vec.reserve(m);
       for (size_t i = 0; i < m; ++i) {
         ret_vec.emplace_back(this->read<value_type_t<Ret>>(dims...));
+      }
+      return ret_vec;
+    }
+  }
+
+  /**
+   * Return an `std::vector`
+   * @tparam Ret The type to return.
+   * @tparam Sizes integral types.
+   * @param m The size of the vector.
+   * @param dims a possible set of inner container sizes passed to subsequent
+   * `read` functions.
+   */
+  template <typename Ret, typename... Sizes,
+            require_std_vector_t<Ret>* = nullptr,
+            require_same_t<value_type_t<Ret>, T>* = nullptr>
+  inline auto read(Eigen::Index m, Sizes... dims) {
+    if (unlikely(m == 0)) {
+      return Ret();
+    } else {
+      std::decay_t<Ret> ret_vec(m);
+      for (size_t i = 0; i < m; ++i) {
+        ret_vec[i] = this->read<value_type_t<Ret>>(dims...);
       }
       return ret_vec;
     }
