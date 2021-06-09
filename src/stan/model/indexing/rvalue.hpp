f72065c80940e4617c139f770d4176acc43df8a7
Tadej Ciglarič <tadej.c@gmail.com>
2021-06-09 09:46:25 +0200

    nullaryExpr

--- src/stan/model/indexing/rvalue.hpp
+++ src/stan/model/indexing/rvalue.hpp
@@ -119,16 +119,16 @@ inline auto rvalue(Vec&& v, const char* name, index_uni idx) {
  * the indexed size.
  */
 template <typename EigVec, require_eigen_vector_t<EigVec>* = nullptr>
-inline plain_type_t<EigVec> rvalue(EigVec&& v, const char* name,
+inline auto rvalue(EigVec&& v, const char* name,
                                    const index_multi& idx) {
-  const auto v_size = v.size();
   const auto& v_ref = stan::math::to_ref(v);
-  plain_type_t<EigVec> ret_v(idx.ns_.size());
-  for (int i = 0; i < idx.ns_.size(); ++i) {
-    math::check_range("vector[multi] indexing", name, v_ref.size(), idx.ns_[i]);
-    ret_v.coeffRef(i) = v_ref.coeff(idx.ns_[i] - 1);
-  }
-  return ret_v;
+  return plain_type_t<EigVec>::NullaryExpr(
+      idx.ns_.size(),
+      [name, &idx, &v_ref](Eigen::Index i) {
+        math::check_range("vector[multi] indexing", name, v_ref.size(),
+                          idx.ns_[i]);
+        return v_ref.coeff(idx.ns_[i] - 1);
+      });
 }
 
 /**
@@ -229,14 +229,15 @@ inline auto rvalue(Mat&& x, const char* name, index_uni idx) {
 template <typename EigMat, require_eigen_dense_dynamic_t<EigMat>* = nullptr>
 inline plain_type_t<EigMat> rvalue(EigMat&& x, const char* name,
                                    const index_multi& idx) {
-  const auto& x_ref = stan::math::to_ref(x);
-  plain_type_t<EigMat> x_ret(idx.ns_.size(), x.cols());
   for (int i = 0; i < idx.ns_.size(); ++i) {
-    const int n = idx.ns_[i];
-    math::check_range("matrix[multi] row indexing", name, x_ref.rows(), n);
-    x_ret.row(i) = x_ref.row(n - 1);
+    math::check_range("matrix[multi] row indexing", name, x.rows(), idx.ns_[i]);
   }
-  return x_ret;
+  return plain_type_t<EigMat>::NullaryExpr(
+      idx.ns_.size(), x.cols(),
+      [name, &idx, &x_ref = stan::math::to_ref(x)](Eigen::Index i,
+                                                   Eigen::Index j) {
+        return x_ref.coeff(idx.ns_[i] - 1, j);
+      });
 }
 
 /**
@@ -403,15 +404,14 @@ inline Eigen::Matrix<value_type_t<EigMat>, 1, Eigen::Dynamic> rvalue(
     const index_multi& col_idx) {
   math::check_range("matrix[uni, multi] row indexing", name, x.rows(),
                     row_idx.n_);
-  const auto& x_ref = stan::math::to_ref(x);
-  Eigen::Matrix<value_type_t<EigMat>, 1, Eigen::Dynamic> x_ret(
-      1, col_idx.ns_.size());
-  for (int i = 0; i < col_idx.ns_.size(); ++i) {
-    math::check_range("matrix[uni, multi] column indexing", name, x.cols(),
-                      col_idx.ns_[i]);
-    x_ret.coeffRef(i) = x_ref.coeff(row_idx.n_ - 1, col_idx.ns_[i] - 1);
-  }
-  return x_ret;
+
+  return Eigen::Matrix<value_type_t<EigMat>, 1, Eigen::Dynamic>::NullaryExpr(
+      col_idx.ns_.size(), [name, row_i = row_idx.n_ - 1, &col_idx,
+                           &x_ref = stan::math::to_ref(x)](Eigen::Index i) {
+        math::check_range("matrix[uni, multi] column indexing", name, x_ref.cols(),
+                          col_idx.ns_[i]);
+        return x_ref.coeff(row_i, col_idx.ns_[i] - 1);
+      });
 }
 
 /**
@@ -433,15 +433,14 @@ inline Eigen::Matrix<value_type_t<EigMat>, Eigen::Dynamic, 1> rvalue(
     index_uni col_idx) {
   math::check_range("matrix[multi, uni] column indexing", name, x.cols(),
                     col_idx.n_);
-  const auto& x_ref = stan::math::to_ref(x);
-  Eigen::Matrix<value_type_t<EigMat>, Eigen::Dynamic, 1> x_ret(
-      row_idx.ns_.size());
-  for (int i = 0; i < row_idx.ns_.size(); ++i) {
-    math::check_range("matrix[multi, uni] row indexing", name, x_ref.rows(),
-                      row_idx.ns_[i]);
-    x_ret.coeffRef(i) = x_ref.coeff(row_idx.ns_[i] - 1, col_idx.n_ - 1);
-  }
-  return x_ret;
+
+  return Eigen::Matrix<value_type_t<EigMat>, Eigen::Dynamic, 1>::NullaryExpr(
+      row_idx.ns_.size(), [name, &row_idx, col_i = col_idx.n_ - 1,
+                           &x_ref = stan::math::to_ref(x)](Eigen::Index i) {
+        math::check_range("matrix[multi, uni] row indexing", name, x_ref.rows(),
+                          row_idx.ns_[i]);
+        return x_ref.coeff(row_idx.ns_[i] - 1, col_i);
+      });
 }
 
 /**
