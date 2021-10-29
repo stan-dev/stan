a750fddebab9428f28316fa311ee0b7c3cd5b4db
stevebronder <stevo15025@gmail.com>
2021-10-29 18:12:23 -0400

    cleanup

--- src/stan/services/pathfinder/single.hpp
+++ src/stan/services/pathfinder/single.hpp
@@ -19,76 +19,291 @@ namespace stan {
 namespace services {
 namespace optimize {
 
-/**
- *
- */
-struct pathfinder_lbfgs_iter_t {
-  // The parameters for an iteration of LBFGS
-  Eigen::VectorXd params_;
-  // Gradients of parameters from an iteration of LBFGS
-  Eigen::VectorXd grads_;
-  // Vector of diagonal elements of $alpha$
-  Eigen::VectorXd alpha_;
-  // KxN matrix ($beta$)
-  Eigen::MatrixXd beta_;
-  // K sized vector $gamma$
-  Eigen::VectorXd gamma_;
+template <typename T1, typename T2>
+inline auto crossprod(T1&& x, T2&& y) {
+  return x.transpose() * y;
+}
+
+template <typename T1, typename T2>
+inline auto tcrossprod(T1&& x, T2&& y) {
+  return x * y.transpose();
+}
+
+inline bool is_nan(double x) {
+  return x == std::numeric_limits<double>::quiet_NaN();
+}
+
+inline bool is_infinite(double x) {
+  return x == std::numeric_limits<double>::infinity();
+}
+
+inline auto form_init_diag(const Eigen::Array<double, -1, 1>& E0,
+                           const Eigen::Array<double, -1, 1>& Yk,
+                           const Eigen::Array<double, -1, 1>& Sk) {
+  double Dk = (Yk * Sk).sum();
+  auto yk_sq = Yk.square().eval();
+  double thetak = yk_sq.sum() / Dk;
+  double a = ((E0 * yk_sq).sum() / Dk);
+  return 1.0
+         / (a / E0 + yk_sq / Dk
+            - a * (Sk / E0).square() / (Sk.square() / E0).sum());
+}
+
+struct sample_pkg_t {
+  std::string label;
+  Eigen::VectorXd x_center;
+  Eigen::MatrixXd cholHk;
+  Eigen::MatrixXd Qk;
+  Eigen::VectorXd theta_D;
+  Eigen::MatrixXd Rktilde;
+  double logdetcholHk;
 };
 
-template <typename BaseRNG>
-inline auto bfgs_sample(const pathfinder_lbfgs_iter_t& lbfgs_iter, BaseRNG& rng,
-                        size_t num_elbo_draws) {
-  auto&& params = lbfgs_iter.params_;
-  auto&& grads = lbfgs_iter.grads_;
-  auto&& alpha = lbfgs_iter.alpha_;
-  auto&& beta = lbfgs_iter.beta_;
-  auto&& gamma = lbfgs_iter.gamma_;
-  const auto N = alpha.size();
-  // determinant of a diagonal matrix is the product of elements of its diagonal
-  auto alpha_det = alpha[0];
-  for (Eigen::Index i = 1; i < N; ++i) {
-    alpha_det *= alpha[i];
-  }
-  Eigen::HouseholderQR<matrix_t> qr(m.rows(), m.cols());
-  qr.compute(m);
-  const auto min_size = std::min(m.rows(), m.cols());
-  Eigen::MatrixXd R = qr.matrixQR().topLeftCorner(min_size, m.cols());
-  for (int i = 0; i < min_size; i++) {
-    for (int j = 0; j < i; j++) {
-      R.coeffRef(i, j) = 0.0;
+template <typename Generator>
+inline auto calc_u_u2(Generator& rnorm, const sample_full_pkg& sample_pkg,
+                      const Eigen::VectorXd& alpha, size_t m) {
+  u = rnorm(m);
+  u2 = crossprod(sample_pkg.L_approx, u) + sample_pkg.x_center;
+  return std::forward_as_tuple(std::move(u), std::move(u2))
+}
+
+template <typename Generator>
+inline auto calc_u_u2(Generator& rnorm, const sample_sparse_pkg& sample_pkg,
+                      const Eigen::VectorXd& alpha, size_t m) {
+  u = rnorm(m);
+  Eigen::VectorXd u1 = crossprod(sample_pkg.Qk, u);
+  u2 = alpha.array().inverse().sqrt().matrix().asDiagonal()
+           * (sample_pkg.Qk * crossprod(sample_pkg.Rktilde, u1)
+              + (u - sample_pkg.Qk * u1))
+       + sample_pkg.x_center;
+  return std::forward_as_tuple(std::move(u), std::move(u2))
+}
+
+template <typename SamplePkg, typename F, typename BaseRNG>
+auto est_DIV(const SamplePkg& sample_pkg, size_t N_sam,
+             const Eigen::VectorXd& alpha, F&& fn, std::string label,
+             BaseRNG& rng) {
+  const auto D = sample_pkg.x_center.size();
+  // Should fill with Inf!
+  Eigen::VectorXd fn_draws = Eigen::VectorXd::Zero(N_sam);
+  Eigen::VectorXd lp_approx_draws = Eigen::VectorXd::Zero(N_sam);
+  Eigen::MatrixXd repeat_draws = Eigen::MatrixXd::Zero(D, N_sam);
+  int draw_ind = 1;
+  int fn_calls_DIV = 0;
+  int f_test_DIV = -std::numeric_limits<int>::infinity();
+  boost::variate_generator<BaseRNG&, boost::normal_distribution<>>
+      rand_unit_gaus(rng, boost::normal_distribution<>());
+  std::tuple<Eigen::VectorXd, Eigen::VectorXd> tuple_u;
+  auto rnorm = [&rand_unit_gaus](auto D) {
+    return Eigen::VectorXd::NullaryExpr(
+        D, [&rand_unit_gaus]() { return rand_unit_gaus(); });
+  };
+  for (int l = 0; l < (2 * N_sam); ++l) {
+    auto tuple_u = calc_u_u2(rnorm, sample_pkg, alpha, D);
+    auto&& u = std::get<0>(tuple_u);
+    auto&& u2 = std::get<1>(tuple_u);
+    // skip bad samples
+    bool skip_flag = false;
+    double f_test_DIV;
+    try {
+      f_test_DIV = fn(u2);
+    } catch (...) {
+      // TODO: Actually catch errors
+      skip_flag = true;
     }
-    if (R(i, i) < 0) {
-      R.row(i) *= -1.0;
+    if (f_test_DIV == std::numeric_limits<double>::quiet_NaN()) {
+      skip_flag = true;
     }
-  }
-  Eigen::MatrixXd Q
-      = qr.householderQ() * matrix_t::Identity(m.rows(), min_size);
-  for (int i = 0; i < min_size; i++) {
-    if (qr.matrixQR().coeff(i, i) < 0) {
-      Q.col(i) *= -1.0;
+    if (skip_flag) {
+      continue;
+    } else {
+      fn_draws[draw_ind] = f_test_DIV;
+      lp_approx_draws[draw_ind]
+          = -sample_pkg.logdetcholHk - 0.5 * u.array().square().sum()
+            - 0.5 * D * log(2 * 3.14);  // NOTE THIS NEEDS TO BE pi()
+      repeat_draws(0, draw_ind) = u2;
+      draw_ind = draw_ind + 1;
+    }
+    fn_calls_DIV = fn_calls_DIV + 1;
+    if (draw_ind == N_sam + 1) {
+      break;
     }
   }
 
-  auto L = (Eigen::MatrixXd::Identity(N, N) + R * gamma * R.transpose())
-               .llt()
-               .matrixL();
-  auto log_det_sigma = std::log(alpha_det) + 2 * L.logAbsDeterminant() auto mu
-      = params + alpha.asDiagonal() * grads + beta * gamma * beta * grads;
-  boost::variate_generator<BaseRNG&, boost::normal_distribution<> >
-      rand_unit_gaus(rng, boost::normal_distribution<>());
-  Eigen::VectorXd u = Eigen::VectorXd::NullaryExpr(
-      N, [&rand_unit_gaus]() { return rand_unit_gaus(); });
-  Eigen::MatrixXd approx_draws(N, num_elbo_draws);
-  Eigen::MatrixXd log_density_draws(num_elbo_draws);
-  for (int m = 0; m < num_elbo_draws; ++m) {
-      approx_draws.col(i) = mu + alpha.array().sqrt().asDiagonal() * (Q * L * Q.transpose() * u.col(m) + u.col(m)) - Q * Q.transpose() * u.col(m));
-      log_density_draws.coeffRef(i) = -.5 * log_det_sigma
-                                      + u.col(m).transpose() * u.col(m)
-                                      + N * log(2 * stan::math::pi());
-      u = Eigen::VectorXd::NullaryExpr(
-          N, [&rand_unit_gaus]() { return rand_unit_gaus(); });
+  //### Divergence estimation ###
+  double ELBO = -fn_draws.mean() - lp_approx_draws.mean();
+  ;
+  if (is_nan(ELBO)) {
+    ELBO = -std::numeric_limits<double>::infinity();
   }
-  return std::make_tuple(approx_draws, log_density_draws);
+  double DIV;
+  if (label == "ELBO") {
+    DIV = ELBO;
+  } else if (label == "lIKL") {
+    // log Inclusive-KL E[p(x_i)/q(x_k)]
+    DIV = ELBO
+          + log((-fn_draws.array() - lp_approx_draws.array() - ELBO)
+                    .exp()
+                    .mean());
+  } else if (label == "lADIV") {
+    // log alpha-divergence E[(p(x_i)/q(x_k))^alpha], e.g. with 1/2
+    DIV = 0.5 * ELBO
+          + log(((0.5 * (-fn_draws.array() - lp_approx_draws.array() - ELBO))
+                     .exp()
+                     .mean()));
+  } else if (label == "lCDIV") {
+    // log Chi^2-divergence is alpha-divergence with alpha=2
+    DIV = 2.0 * ELBO
+          + log(((2.0
+                  * (-fn_draws.array() - lp_approx_draws.array() - ELBO)
+                        .exp()
+                        .mean())));
+  } else {
+    throw std::domain_error("The divergence is misspecified");
+  }
+
+  if (is_nan(DIV)) {
+    DIV = -std::numeric_limits<double>::infinity();
+  }
+  if (is_infinite(DIV)) {
+    DIV = -std::numeric_limits<double>::infinity();
+  }
+
+  if ((fn_draws.array() == std::numeric_limits<double>::infinity()).any()) {
+    fn_draws[0] = std::numeric_limits<double>::infinity();
+  }
+  /*
+  return(list(DIV = DIV,
+              repeat_draws = repeat_draws, fn_draws = fn_draws,
+              lp_approx_draws = lp_approx_draws, fn_calls_DIV = fn_calls_DIV))
+    */
+}
+
+template <typename SamplePkg, typename BaseRNG>
+inline auto Sam_N_apx(const SamplePkg& sample_pkg, size_t N_sam,
+                      BaseRNG&& rng) {
+  const Eigen::Index D = sample_pkg.x_center.size();
+  Eigen::VectorXd lp_approx_draws = Eigen::VectorXd::Zero(N_sam);
+  Eigen::MatrixXd repeat_draws = Eigen::VectorXd::Zero(D, N_sam);
+
+  boost::variate_generator<BaseRNG&, boost::normal_distribution<>>
+      rand_unit_gaus(rng, boost::normal_distribution<>());
+  auto rnorm = [&rand_unit_gaus](auto D) {
+    return Eigen::VectorXd::NullaryExpr(
+        D, [&rand_unit_gaus]() { return rand_unit_gaus(); });
+  };
+  auto tuple_u = calc_u_u2(rnorm, sample_pkg, D * N_sam);
+  using map_t = Eigen::Map<Eigen::MatrixXd> Eigen::VectorXd lp_apx_draws
+      = -sample_pkg.logdetcholHk
+        - 0.5
+              * map_t(std::get<0>(tuple_u).data(), D, N_sam)
+                    .array()
+                    .square()
+                    .colwise()
+                    .sum()
+        - 0.5 * D* log(2 * 3.14);  // TODO: PUT BACK pi()
+  return std::make_tuple(std::get<1>(tuple_u), lp_apx_draws);
+}
+
+inline auto form_n_apx_taylor_full(const Eigen::MatrixXd& Ykt_mat,
+                                   const Eigen::VectorXd& alpha,
+                                   const Eigen::VectorXd& Dk,
+                                   const Eigen::MatrixXd& ninvRST,
+                                   const Eigen::VectorXd& point_est,
+                                   const Eigen::VectorXd& grad_est) {
+  Eigen::MatrixXd ykt_mat_cprod
+      = Eigen::MatrixXd(Ykt_mat.rows(), Ykt_mat.cols())
+            .setZero()
+            .template selfadjointView<Eigen::Upper>()
+            .rankUpdate(Ykt_mat * alpha.array().sqrt().matrix().asDiagonal());
+  ykt_mat_cprod += Dk.asDiagonal();
+  Eigen::MatrixXd Hk = ((Ykt_mat * alpha.asDiagonal()).transpose() * ninvRST)
+                       + (ninvRST.transpose() * Ykt_mat * alpha.asDiagonal())
+                       + (ninvRST.transpose() * (ykt_mat_cprod)*ninvRST);
+  Hk += alpha.asDiagonal();
+  Eigen::MatrixXd cholHk = Hk.llt().matrixL();
+  auto logdetcholHk = 2.0 * std::log(cholHk.determinant());
+
+  Eigen::VectorXd x_center = point_est - Hk * grad_est;
+  return 1;
+}
+
+inline auto form_n_apx_taylor_sparse(const Eigen::MatrixXd& Ykt_mat,
+                                     const Eigen::VectorXd& alpha,
+                                     const Eigen::VectorXd& Dk,
+                                     const Eigen::MatrixXd& ninvRST,
+                                     const Eigen::VectorXd& point_est,
+                                     const Eigen::VectorXd& grad_est) {
+  const Eigen::Index m = Ykt_mat.rows();
+  Eigen::MatrixXd Wkbart(Ykt_mat.rows() + ninvRST.rows(), Ykt_mat.cols());
+  Wkbart.topRows(Ykt_mat.rows())
+      = Ykt_mat * alpha.array().sqrt().matrix().asDiagonal();
+  Wkbart.bottomRows(ninvRST.rows())
+      = ninvRST * alpha.array().inverse().sqrt().matrix().asDiagonal();
+
+  Eigen::MatrixXd Mkbar(2 * m, 2 * m);
+  Mkbar.topLeftCorner(m, m).setZero();
+  Mkbar.topRightCorner(m, m) = Eigen::DiagonalMatrix<double, -1, -1>(m);
+  Mkbar.bottomLeftCorner(m, m) = Eigen::DiagonalMatrix<double, -1, -1>(m);
+  Eigen::MatrixXd ykt_mat_cprod
+      = Eigen::MatrixXd(Ykt_mat.rows(), Ykt_mat.cols())
+            .setZero()
+            .template selfadjointView<Eigen::Upper>()
+            .rankUpdate(Ykt_mat * alpha.array().sqrt().matrix().asDiagonal());
+  ykt_mat_cprod += Dk.asDiagonal();
+  Mkbar.bottomRightCorner(m, m) = ykt_mat_cprod;
+
+  Eigen::HouseholderQR<Eigen::MatrixXd> qr(Wkbart.rows(), Wkbart.cols());
+  qr.compute(Wkbart);
+  const auto min_size = std::min(Wkbart.rows(), Wkbart.cols());
+  Eigen::MatrixXd Rkbar = qr.matrixQR().topLeftCorner(min_size, Wkbart.cols());
+  for (int i = 0; i < min_size; i++) {
+    for (int j = 0; j < i; j++) {
+      Rkbar.coeffRef(i, j) = 0.0;
+    }
+    if (Rkbar(i, i) < 0) {
+      Rkbar.row(i) *= -1.0;
+    }
+  }
+  Eigen::MatrixXd Qk
+      = qr.householderQ() * Eigen::MatrixXd::Identity(Wkbart.rows(), min_size);
+  for (int i = 0; i < min_size; i++) {
+    if (qr.matrixQR().coeff(i, i) < 0) {
+      Qk.col(i) *= -1.0;
+    }
+  }
+  Eigen::MatrixXd Rktilde
+      = (Rkbar * Mkbar * Rkbar.transpose()
+         + Eigen::MatrixXd::Identity(Rkbar.rows(), Rkbar.rows()))
+            .llt()
+            .matrixL();
+  double logdetcholHk = Rktilde.diagonal().array().log().sum()
+                        + 0.5 * alpha.array().log().sum();
+  Eigen::VectorXd ninvRSTg = ninvRST * grad_est;
+  Eigen::VectorXd x_center_tmp
+      = (alpha.array() * grad_est.array()).matrix()
+        + (alpha.array() * (Ykt_mat.transpose() * ninvRSTg).array()).matrix()
+        + ninvRSTg.transpose() * Ykt_mat
+              * (alpha.array() * grad_est.array()).matrix()
+        + ninvRSTg.transpose() * ykt_mat_cprod * ninvRSTg.transpose();
+  Eigen::VectorXd x_center = point_est - x_center_tmp;
+
+  return std::make_tuple();
+}
+
+inline bool check_cond(const Eigen::VectorXd& Yk, const Eigen::VectorXd& Sk) {
+  double Dk = (Yk.array() * Sk.array()).sum();
+  if (Dk == 0) {
+    return false;
+  } else {
+    double thetak = Yk.array().square().sum() / Dk;
+    // curvature checking
+    if ((Dk <= 0 || std::abs(thetak) > 1e12)) {  // 2.2*e^{-16}
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
 
 /**
@@ -185,7 +400,7 @@ inline int pathfinder_lbfgs_single(
    * 2. Run L-BFGS to return optimization path for parameters, gradients of
    * objective function, and factorization of covariance estimation
    */
-  std::vector<pathfinder_lbfgs_iter_t> lbfgs_iters;
+  std::vector<std::tuple<Eigen::VectorXd, Eigen::VectorXd>> lbfgs_iters;
   lbfgs_iters.reserve(max_iter);
   int ret = 0;
 
@@ -230,48 +445,107 @@ inline int pathfinder_lbfgs_single(
       logger.info(lbfgs_ss);
       lbfgs_ss.str("");
     }
+    std::vector<double> values;
+    std::stringstream msg;
+    model.write_array(rng, cont_vector, disc_vector, values, true, true, &msg);
+    if (msg.str().length() > 0) {
+      logger.info(msg);
+    }
+
+    lbfgs_iters.emplace_back(values, lbfgs.curr_g());
 
     if (save_iterations) {
-      std::vector<double> values;
-      std::stringstream msg;
-      model.write_array(rng, cont_vector, disc_vector, values, true, true,
-                        &msg);
-      if (msg.str().length() > 0)
-        logger.info(msg);
-
       values.insert(values.begin(), lp);
       parameter_writer(values);
     }
   }
 
   // 3. For each L-BFGS iteration `num_iterations`
-  Eigen::VectorXd lambda(num_iterations);
-  for (size_t l = 0; l < num_iterations; l++) {
+  Eigen::VectorXd lambda(lbfgs_iters.size());
+  Eigen::VectorXd E = Eigen::VectorXd::Ones();
+  boost::circular_buffer<Eigen::VectorXd> Ykt_h(history_size);
+  boost::circular_buffer<Eigen::VectorXd> Skt_h(history_size);
+  double div_max = std::numeric_limits<double>::min();
+  for (size_t iter = 0; iter < lbfgs_iters; iter++) {
+    Eigen::VectorXd Ykt
+        = std::get<1>(lbfgs_iters[i]) - std::get<1>(lbfgs_iters[i + 1]);
+    Eigen::VectorXd Skt
+        = std::get<0>(lbfgs_iters[i]) - std::get<0>(lbfgs_iters[i + 1]);
+    if (check_cond(Ykt, Skt)) {
+      // initial estimate of diagonal inverse Hessian
+      E = form_init_diag(E.array(), Ykt.array(), Skt.array());
+      // update Y and S matrix
+      Ykt_h.push_back(Ykt);
+      Skt_h.push_back(Skt);
+    }
+    const auto param_size = Ykt.size();
+    const auto m = Ykt_h.size();
+    Eigen::VectorXd Dk(m);
+    Eigen::VectorXd thetak(m);
+    for (Eigen::Index i = 0; i < m; i++) {
+      Dk[i] = Ykt_h[i].dot(Skt_h[i]);
+      thetak[i]
+          = Ykt_h[i].array().square().sum() / Dk[i];  // curvature checking
+    }
+
+    Eigen::MatrixXd Rk = Eigen::MatrixXd::Zero(m, m);
+    for (Eigen::Index s = 0; s > m; s++) {
+      for (Eigen::Index i = 0; i > s; i++) {
+        Rk(i, s) = Skt_h[i].dot(Ykt_h[s]);
+      }
+    }
+    Eigen::MatrixXd ninvRST;
+
+    {
+      Eigen::MatrixXd Skt_mat(param_size, m);
+      for (Eigen::Index i = 0; i < m; ++i) {
+        Skt_mat.col(i) = Skt_h[i];
+      }
+      ninvRST = -(Rk.template triangularView<Eigen::Upper>().solve(Skt_mat));
+    }
+
+    Eigen::MatrixXd Ykt_mat(param_size, m);
+    for (Eigen::Index i = 0; i < m; ++i) {
+      Ykt_mat.col(i) = Ykt_h[i];
+    }
+    bool ill_dist = false;
     /**
      * 3a. Run BFGS-Sample to get `num_elbo_draws` draws from normal
      * approximation and log density of draws in the approximate normal
      * distribution
      */
-    auto&& lfbgs_ret_l = lfbgs_ret[l];
-    std::tuple<Eigen::MatrixXd, Eigen::VectorXd> phi_and_lq
-        = bfgs_sample(lfbgs_ret_l, num_elbo_draws);
-    /**
-     * 3b. Calculate a vector of size `num_elbo_draws` joint log probability
-     * given normal approximation
-     */
-    Eigen::VectorXd lp_calcs = lp_per_k_calc(std::get<0>(phi_and_lq));
-    // 3c. Calculate ELBO given 3a and 3b
-    lambda[l] = (lp_calcs - std::get<1>(phi_and_lq)).sum();
+
+    if (2 * m >= param_size) {
+      full_ret_t tayler_appx_tuple form_n_apx_taylor_full(
+          Ykt_mat, alpha, Dk, ninvRST, std::get<0>(lbfgs_iters[i]),
+          std::get<1>(lbfgs_iters[i]));
+    } else {
+      sparse_ret_t tayler_appx_tuple form_n_apx_taylor_sparse(
+          Ykt_mat, alpha, Dk, ninvRST, std::get<1>(lbfgs_iters[i]),
+          std::get<1>(lbfgs_iters[i]));
+    }
+    if (ill_distr) {
+      continue;
+    }
+    if (is_na(sample_pkg[1])) {
+      continue;
+    }
+    DIV_fit = est_DIV(sample_pkg, N_sam_DIV, fn,
+                      label = "ELBO")  //#lCDIV #lADIV  #lIKL #ELBO
+        // TODO: Calculate total function calls
+        // fn_call = fn_call + DIV_fit$fn_calls_DIV
+        // DIV_ls = c(DIV_ls, DIV_fit$DIV)
+        //  4. Find $l \in L$ that maximizes ELBO $l^* = arg max_l ELBO^(l)$.
+        if (DIV_fit.DIV_ > DIV_max) {
+      DIV_max = DIV_fit$DIV DIV_fit_pick = DIV_fit sample_pkg_pick = sample_pkg
+    }
   }
-  //  4. Find $l \in L$ that maximizes ELBO $l^* = arg max_l ELBO^(l)$.
-  Eigen::Index max_l = get_max_idx(lambda);
-  auto&& lbfgs_ret_l = lfbgs_ret[max_l];
   /**
    * 5. Run bfgs-Sample to return `num_draws` draws from ELBO-maximizing normal
    * approx and log density of draws in ELBO-maximizing normal approximation.
    */
-  std::tuple<Eigen::MatrixXd, Eigen::VectorXd> bfgs_sample(lbfgs_ret_l,
-                                                           num_draws);
+  std::tuple<Eigen::MatrixXd, Eigen::VectorXd> final_samples
+      = Sam_N_apx(sample_pkg_pick, num_draws, rng);
   return return_code;
 }
 
