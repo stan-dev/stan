18fe1790f57d575fe9d84250b1ca6de7eba0d61c
Steve Bronder <stevo15025@gmail.com>
2021-11-28 13:31:26 -0500

    update

--- src/stan/services/pathfinder/multi.hpp
+++ src/stan/services/pathfinder/multi.hpp
@@ -1,5 +1,5 @@
-#ifndef STAN_SERVICES_PATHFINDER_SINGLE_HPP
-#define STAN_SERVICES_PATHFINDER_SINGLE_HPP
+#ifndef STAN_SERVICES_PATHFINDER_MULTI_HPP
+#define STAN_SERVICES_PATHFINDER_MULTI_HPP
 
 #include <stan/callbacks/interrupt.hpp>
 #include <stan/callbacks/logger.hpp>
@@ -7,6 +7,8 @@
 #include <stan/io/var_context.hpp>
 #include <stan/optimization/bfgs.hpp>
 #include <stan/optimization/lbfgs_update.hpp>
+#include <stan/services/pathfinder/single.hpp>
+#include <stan/services/psis/psis.hpp>
 #include <stan/services/error_codes.hpp>
 #include <stan/services/util/initialize.hpp>
 #include <stan/services/util/create_rng.hpp>
@@ -32,36 +34,41 @@ inline int pathfinder_lbfgs_multi(
     size_t num_multi_draws, size_t num_threads, size_t num_paths,
     callbacks::logger& logger, callbacks::writer& init_writer,
     ParamWriter& parameter_writer, DiagnosticWriter& diagnostic_writer) {
-  Eigen::Array<double, -1, 1> lp_ratios(num_paths * num_draws);
+  Eigen::Array<double, -1, 1> lp_ratios(num_draws * num_paths);
   size_t num_params = 0;
-  Eigen::Array<double, -1, 1> samples(num_paths * num_draws, num_params);
+  Eigen::Array<double, -1, -1> samples(num_paths * num_draws, num_params);
   tbb::parallel_for(tbb::blocked_range<int>(0, num_paths),
-                    [&](tbb::blocked_range<int> r) {
-                      for (int iter = r.begin(); iter < r.end(); ++iter) {
-                        auto pathfinder_ret
-                            = stan::services::optimize::pathfinder_lbfgs_single(
-                                model, empty_context, seed, chain, init_radius,
-                                6, 0.001, 1e-12, 10000, 1e-8, 10000000, 1e-8,
-                                2000, save_iterations, refresh, callback, 100,
-                                100, 1, logger, init, parameter, diagnostics);
-                        Eigen::Array<double, -1, 1> lp_ratio
-                            = std::get<0>(lp_vals) - std::get<1>(lp_vals);
-                        // logic for writing to lp_ratios and draws
-                      }
-                    });
-  // Wonder how I can do relative effective sample sizes?
-  Eigen::Array<double, -1, 1> weight_vals = get_psis_weights(lp_ratios, 1);
+    [&](tbb::blocked_range<int> r) {
+      for (int iter = r.begin(); iter < r.end(); ++iter) {
+        auto pathfinder_ret
+            = stan::services::optimize::pathfinder_lbfgs_single<true>(
+                model, init, random_seed, path + iter, init_radius,
+                history_size, init_alpha, tol_obj, tol_rel_obj, tol_grad, tol_rel_grad,
+                tol_param, num_iterations, save_iterations, refresh,
+                interrupt, num_elbo_draws, num_draws, num_threads,
+                logger, init_writer, parameter_writer, diagnostic_writer);
+        Eigen::Array<double, -1, 1> lp_ratio = std::get<0>(pathfinder_ret);
+        // logic for writing to lp_ratios and draws
+        lp_ratios.segment(iter * num_draws, num_draws) = -lp_ratio;
+        samples.middleRows(iter * num_draws, num_draws) = std::get<1>(pathfinder_ret);
+      }
+    });
+  const auto tail_len = std::min(0.2 * samples.rows(), 3 * std::sqrt(samples.rows()));
+  Eigen::Array<double, -1, 1> weight_vals = stan::services::psis::get_psis_weights(lp_ratios, tail_len);
   // Figure out if I can use something in boost and not a std::vector
   std::vector<double> lp_weights(num_paths * num_draws);
   for (size_t i = 0; i < weight_vals.size(); ++i) {
     lp_weights[i] = weight_vals[i];
   }
+  boost::ecuyer1988 rng
+      = util::create_rng<boost::ecuyer1988>(random_seed, path);
+
   boost::variate_generator<boost::ecuyer1988&,
-                           boost::discrete_distribution<Eigen::Index, double>>
+                           boost::random::discrete_distribution<Eigen::Index, double>>
       rand_psis_idx(
-          rng, boost::discrete_distribution<Eigen::Index, double>(lp_weights));
+          rng, boost::random::discrete_distribution<Eigen::Index, double>(lp_weights));
   for (size_t i = 0; i < num_multi_draws; ++i) {
-    param_writer(samples.col(rand_psis_idx()));
+    parameter_writer(samples.row(rand_psis_idx()));
   }
   return 1;
 }
--- src/stan/services/psis/psis.hpp
+++ src/stan/services/psis/psis.hpp
@@ -24,115 +24,206 @@ namespace stan {
 namespace services {
 namespace psis {
 
-inline Eigen::Array<double, -1, 1> lx(const Eigen::Array<double, -1, 1>& a,
-                                      const Eigen::Array<double, -1, 1>& x) {
-  double k = ((-a) * x).log1p().mean();
-  return (-a / k).log() - k - 1;
+template <typename T>
+inline bool is_finite(T x) {
+    return x != std::numeric_limits<T>::infinity();
 }
 
-auto gpdfit(const Eigen::Array<double, -1, 1>& x, bool wip = true,
+template <typename T>
+inline auto log_sum_exp(const T& v) {
+    if (v.size() == 0) {
+        return -std::numeric_limits<double>::infinity();
+    }
+    const auto& v_ref = v;
+    const double max = v_ref.maxCoeff();
+    if (!std::isfinite(max)) {
+        return max;
+    }
+    return max + std::log((v_ref.array() - max).exp().sum());
+}
+
+inline Eigen::Array<double, -1, 1> lx(const Eigen::Array<double, -1, 1>& a,
+                                      const Eigen::Array<double, -1, 1>& x) {
+    Eigen::Array<double, -1, 1> k = ((-a).matrix() * x.matrix().transpose()).array().log1p().matrix().rowwise().mean().array();
+    return (-a / k).log() - k - 1;
+}
+
+inline auto gpdfit(const Eigen::Array<double, -1, 1>& x, bool wip = true,
             size_t min_grid_pts = 30) {
-  // See section 4 of Zhang and Stephens (2009)
-  const auto N = x.size();
-  constexpr auto prior = 3.0;
-  const auto M = min_grid_pts + std::floor(std::sqrt(N));
-  Array<double, -1, 1> jj
-      = Array<double, -1, 1>::LinSpaced(1.0, 0, static_cast<double>(M));
-  double xstar = x[std::floor(N / 4 + 0.5)];  // first quartile of sample
-  auto theta = 1.0 / x[N] + (1.0 - (M / (jj - 0.5)).sqrt()) / prior / xstar;
-  auto l_theta = static_cast<double>(N) * lx(theta, x);  // profile log-lik
-  auto w_theta
-      = (l_theta - stan::math::log_sum_exp(l_theta)).exp();  // normalize
-  auto theta_hat = (theta * w_theta).sum();
-  auto k = (-theta_hat * x).log1p().mean();
-  auto sigma = -k / theta_hat;
-  // auto k = adjust_k_wip(k, n = N);
-  constexpr double a = 10;
-  const double n_plus_a = N + a;
-  k = k * N / n_plus_a + a * 0.5 / n_plus_a;
-  return std::make_tuple(k, sigma)
+    // See section 4 of Zhang and Stephens (2009)
+    const auto N = x.size();
+    constexpr auto prior = 3.0;
+    const auto M = min_grid_pts + std::floor(std::sqrt(N));
+    Eigen::Array<double, -1, 1> jj =
+        Eigen::Array<double, -1, 1>::LinSpaced(M, 1, static_cast<double>(M));
+    double xstar = x[std::floor(N / 4 + 0.5) - 1];  // first quartile of sample
+//    std::cout << "\nx: \n" << x;
+//    std::cout << "\njj: \n" << jj;
+
+    auto theta = (1.0 / x[N - 1] + (1.0 - (M / (jj - 0.5)).sqrt()) / prior / xstar).eval();
+    auto l_theta = (static_cast<double>(N) * lx(theta, x)).eval();   // profile log-lik
+//    std::cout << "\nx[N - 1]: " << x[N - 1] << "\n";
+//    std::cout << "\nx_star: " << xstar << "\n";
+//    std::cout << "\nl_theta: \n" << l_theta;
+    auto w_theta = (l_theta - log_sum_exp(l_theta)).exp().eval();  // normalize
+//    std::cout << "\ntheta: \n" << theta;
+//    std::cout << "\nw_theta: \n" << w_theta << "\n";
+    auto theta_hat = (theta * w_theta).sum();
+    auto k = (-theta_hat * x).log1p().mean();
+    auto sigma = -k / theta_hat;
+    // auto k = adjust_k_wip(k, n = N);
+    constexpr double a = 10;
+    const double n_plus_a = N + a;
+    k = k * N / n_plus_a + a * 0.5 / n_plus_a;
+//    std::cout << "\nk: " << k;
+//    std::cout << "\nsigma: " << sigma;
+    return std::make_tuple(k, sigma);
 }
 
 inline auto qgpd(const Eigen::Array<double, -1, 1>& p, double k, double sigma) {
-  /*
-    if (is.nan(sigma) || sigma <= 0) {
-      return(rep(NaN, length(p)))
-    }
-  */
-  return (sigma * (-k * log1p(-p)).expm1() / k).eval();
+    /*
+      if (is.nan(sigma) || sigma <= 0) {
+        return(rep(NaN, length(p)))
+      }
+    */
+    // TODO: Replace with expm1()
+    return (sigma * stan::math::expm1(-k * (-p).log1p()) / k).eval();
 }
 
 inline auto psis_smooth_tail(const Eigen::Array<double, -1, 1>& x,
                              double cutoff) {
-  const auto x_size = x.size();
-  const auto exp_cutoff = std::exp(cutoff);
+    const auto x_size = x.size();
+    const auto exp_cutoff = std::exp(cutoff);
 
-  // save time not sorting since x already sorted
-  auto fit = gpdfit(x.array().exp() - exp_cutoff);
-  double k = std::get<0>(fit);
-  double sigma = std::get<1>(fit);
-  if (is_finite(k)) {
-    auto p = (seq_len(len) - 0.5) / x_size;
-    auto qq = qgpd(p, k, sigma) + exp_cutoff;
-    return std::make_tuple(qq.log().eval(), k);
-  } else {
-    return std::make_tuple(x, k);
-  }
-}
-
-inline auto max_n_element(const Eigen::Array<double, -1, 1>& lw_i,
-                          size_t tail_len_i) {
-  Eigen::Array<double, -1, 1> top_n = lw_i.head(tail_len_i);
-  Eigen::Array<Eigen::Index, -1, 1> top_n_idx(tail_len_idx);
-  return std::make_tuple(std::move(top_n), std::move(top_n_idx));
-}
-
-inline auto get_psis_weights(const Eigen::VectorXd& log_ratios_i,
-                             size_t tail_len_i) {
-  const auto S = log_ratios_i.size();
-  // shift log ratios for safer exponentation
-  const double max_log_ratio = log_ratios_i.maxCoeff();
-  Eigen::Array<double, -1, 1> lw_i = log_ratios_i - max_log_ratio;
-  double khat = 0;
-
-  if (tail_len_i >= 5) {
-    /*
-    ord = sort.int(lw_i, index.return = TRUE)
-    tail_ids = seq(S - tail_len_i + 1, S)
-    lw_tail = ord$x[tail_ids]
-    */
-    // Get back tail + smallest but not on tail in ascending order
-    auto max_n = max_n_elements(lw_i, tail_len_i + 1);
-    Eigen::Array<double, -1, 1> lw_tail = std::get<0>(max_n);
-    if (abs(max(lw_tail) - min(lw_tail))
-        <= std::numeric_limits<double>::min() * 10) {
-      /*
-      warning(
-        "Can't fit generalized Pareto distribution ",
-        "because all tail values are the same.",
-        call. = FALSE
-      )
-      */
+    // save time not sorting since x already sorted
+    auto fit = gpdfit(x.array().exp() - exp_cutoff);
+    double k = std::get<0>(fit);
+    double sigma = std::get<1>(fit);
+    std::cout << "\n k: " << k << "\n";
+    std::cout << "\n sigma: " << sigma << "\n";
+    if (is_finite(k)) {
+        auto p =
+            (Eigen::Array<double, -1, 1>::LinSpaced(x_size, 1, x_size) - 0.5) /
+            x_size;
+        Eigen::Array<double, -1, 1> blah = qgpd(p, k, sigma);
+        std::cout << "\nblah: \n" << blah << "\n";
+        auto qq = blah + exp_cutoff;
+        std::cout << "\n p: \n" << p << "\n";
+        std::cout << "\n qq: \n" << qq << "\n";
+        return std::make_tuple(qq.log().eval(), k);
     } else {
-      // cutoff = ord$x[min(tail_ids) - 1] // largest value smaller than tail
-      // values
-      auto smoothed = psis_smooth_tail(lw_tail, lw_tail.head(1));
-      auto khat = std::get<1>(smoothed);
-      //         lw_i[ord$ix[tail_ids]] = smoothed$tail
-      insert_smooth_to_tail(lw_i, std::get<1>(max_n), std::get<0>(smoothed));
+        return std::make_tuple(x, k);
     }
-  }
-
-  // truncate at max of raw wts (i.e., 0 since max has been subtracted)
-  for (Eigen::Index i = 0; i < lw_i.size(); ++i) {
-    if (lw_i(i) < 0) {
-      lw_i(i) = 0.0;
-    }
-    // shift log weights back so that the smallest log weights remain unchanged
-    // lw_i = lw_i + max(log_ratios_i)
-  }
-  return (lw_i - math::log_sum_exp(lw_i + max_log_ratio)).exp().eval();
 }
+
+void bubble_sort(Eigen::Array<double, -1, 1>& arr,
+                 Eigen::Array<Eigen::Index, -1, 1>& arr_idx) {
+    const auto n = arr.size();
+    Eigen::Index i, j;
+    for (i = 0; i < arr.size(); i++) {
+        // Last i elements are already in place
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                std::swap(arr[j], arr[j + 1]);
+                std::swap(arr_idx[j], arr_idx[j + 1]);
+            }
+        }
+    }
+}
+
+inline auto max_n_elements(const Eigen::Array<double, -1, 1>& lw_i,
+                           size_t tail_len_i) {
+    Eigen::Array<double, -1, 1> top_n = lw_i.head(tail_len_i);
+    Eigen::Array<Eigen::Index, -1, 1> top_n_idx =
+        Eigen::Array<Eigen::Index, -1, 1>::LinSpaced(tail_len_i, 0, tail_len_i);
+    bubble_sort(top_n, top_n_idx);
+    for (Eigen::Index i = tail_len_i; i < lw_i.size(); ++i) {
+        for (Eigen::Index j = top_n.size() - 1; j >= 0; --j) {
+            if (top_n[j] <= lw_i[i]) {
+                //              std::cout << "j: " << j;
+                for (Eigen::Index k = 1; k <= j; ++k) {
+                    //                  std::cout << " k: " << k;
+                    top_n[k - 1] = top_n[k];
+                    top_n_idx[k - 1] = top_n_idx[k];
+                    //                std::cout << " top_n: " <<
+                    //                top_n.transpose();
+                }
+                top_n[j] = lw_i[i];
+                top_n_idx[j] = i;
+                //              std::cout << "\n";
+                break;
+            }
+        }
+    }
+    return std::make_tuple(std::move(top_n), std::move(top_n_idx));
+}
+
+inline void insert_smooth_to_tail(Eigen::Array<double, -1, 1>& lw_i,
+                                  const Eigen::Array<Eigen::Index, -1, 1>& idx,
+                                  const Eigen::Array<double, -1, 1>& smoothed) {
+    for (Eigen::Index i = 0; i < idx.size(); ++i) {
+        lw_i[idx[i]] = smoothed[i];
+    }
+}
+
+inline auto get_psis_weights(const Eigen::Array<double, -1, 1>& log_ratios_i,
+                             size_t tail_len_i) {
+    const auto S = log_ratios_i.size();
+    // shift log ratios for safer exponentation
+    const double max_log_ratio = log_ratios_i.maxCoeff();
+    Eigen::Array<double, -1, 1> lw_i = log_ratios_i - max_log_ratio;
+    std::cout << "\nlw_i: \n" << lw_i << "\n";
+    double khat = 0;
+
+    if (tail_len_i >= 5) {
+        /*
+        ord = sort.int(lw_i, index.return = TRUE)
+        tail_ids = seq(S - tail_len_i + 1, S)
+        lw_tail = ord$x[tail_ids]
+        */
+        // Get back tail + smallest but not on tail in ascending order
+        auto max_n = max_n_elements(lw_i, tail_len_i + 1);
+        Eigen::Array<double, -1, 1> lw_tail = std::get<0>(max_n).tail(tail_len_i);
+        double cutoff = std::get<0>(max_n)(0);
+        std::cout << "\ncutoff: " << cutoff << "\n";
+        std::cout << "\ntop_n: \n" << lw_tail << "\n";
+        if (lw_tail.maxCoeff() - lw_tail.minCoeff() <=
+            std::numeric_limits<double>::min() * 10) {
+            /*
+            warning(
+              "Can't fit generalized Pareto distribution ",
+              "because all tail values are the same.",
+              call. = FALSE
+            )
+            */
+        } else {
+            // cutoff = ord$x[min(tail_ids) - 1] // largest value smaller than
+            // tail values
+            auto smoothed = psis_smooth_tail(lw_tail, cutoff);
+            std::cout << "\n smoothed: \n" << std::get<0>(smoothed) << "\n";
+            auto khat = std::get<1>(smoothed);
+            std::cout << "\nkhat: " << khat << "\n";
+            //         lw_i[ord$ix[tail_ids]] = smoothed$tail
+            std::cout << "\nidx_n: \n" << std::get<1>(max_n).tail(tail_len_i) << "\n";
+            insert_smooth_to_tail(lw_i, std::get<1>(max_n).tail(tail_len_i),
+                                  std::get<0>(smoothed));
+            std::cout << "\nnew lw_i: \n" << lw_i << "\n";
+        }
+    }
+
+    // truncate at max of raw wts (i.e., 0 since max has been subtracted)
+    for (Eigen::Index i = 0; i < lw_i.size(); ++i) {
+        if (lw_i(i) > 0) {
+            lw_i(i) = 0.0;
+        }
+        // shift log weights back so that the smallest log weights remain
+        // unchanged lw_i = lw_i + max(log_ratios_i)
+    }
+    auto max_adj = (lw_i + max_log_ratio).eval();
+    std::cout << "\nmax_adj: \n" << max_adj << "\n";
+    return (max_adj - log_sum_exp(max_adj)).exp().eval();
+}
+
 }  // namespace psis
 }  // namespace services
 }  // namespace stan
