1c3c425607e31f945c8e979bab13e53308475790
Stan BuildBot <mc.stanislaw@gmail.com>
2022-03-10 11:07:52 -0500

    [Jenkins] auto-formatting by clang-format version 10.0.0-4ubuntu1

--- src/stan/services/sample/hmc_nuts_diag_e_adapt.hpp
+++ src/stan/services/sample/hmc_nuts_diag_e_adapt.hpp
@@ -261,21 +261,21 @@ int hmc_nuts_diag_e_adapt(
   } catch (const std::domain_error& e) {
     return error_codes::CONFIG;
   }
-  tbb::parallel_for(tbb::blocked_range<size_t>(0, num_chains, 1),
-                    [num_warmup, num_samples, num_thin, refresh, save_warmup,
-                     num_chains, init_chain_id, &samplers, &model, &rngs,
-                     &interrupt, &logger, &sample_writer, &cont_vectors,
-                     &diagnostic_writer](const tbb::blocked_range<size_t>& r) {
-                      for (size_t i = r.begin(); i != r.end(); ++i) {
-                        util::run_adaptive_sampler(
-                            samplers[i], model, cont_vectors[i], num_warmup,
-                            num_samples, num_thin, refresh, save_warmup,
-                            rngs[i], interrupt, logger, sample_writer[i],
-                            diagnostic_writer[i], init_chain_id + i,
-                            num_chains);
-                      }
-                    },
-                    tbb::simple_partitioner());
+  tbb::parallel_for(
+      tbb::blocked_range<size_t>(0, num_chains, 1),
+      [num_warmup, num_samples, num_thin, refresh, save_warmup, num_chains,
+       init_chain_id, &samplers, &model, &rngs, &interrupt, &logger,
+       &sample_writer, &cont_vectors,
+       &diagnostic_writer](const tbb::blocked_range<size_t>& r) {
+        for (size_t i = r.begin(); i != r.end(); ++i) {
+          util::run_adaptive_sampler(samplers[i], model, cont_vectors[i],
+                                     num_warmup, num_samples, num_thin, refresh,
+                                     save_warmup, rngs[i], interrupt, logger,
+                                     sample_writer[i], diagnostic_writer[i],
+                                     init_chain_id + i, num_chains);
+        }
+      },
+      tbb::simple_partitioner());
   return error_codes::OK;
 }
 
