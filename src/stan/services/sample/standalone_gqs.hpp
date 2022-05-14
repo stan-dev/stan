bba74629b6b67dce70595fb38dc45bcaea5f6205
Hamada S. Badr <hamada.s.badr@gmail.com>
2022-05-14 11:40:44 -0400

    Fix warning: unused variable `num_params`

--- src/stan/services/sample/standalone_gqs.hpp
+++ src/stan/services/sample/standalone_gqs.hpp
@@ -44,7 +44,6 @@ void get_model_parameters(const Model &model,
   }
   std::vector<std::string> all_param_names;
   model.get_param_names(all_param_names);
-  size_t num_params = param_names.size();
   std::vector<std::vector<size_t>> dimss;
   model.get_dims(dimss);
   for (size_t i = 0; i < param_names.size(); i++) {
