8277d4ded752b68429dceb129926d00dc2bfc28f
Bob Carpenter <bcarpenter@flatironinstitute.org>
2022-10-19 16:27:45 +0200

    refresh sensitive logging

--- src/stan/services/optimize/laplace_sample.hpp
+++ src/stan/services/optimize/laplace_sample.hpp
@@ -8,6 +8,7 @@
 #include <stan/services/error_codes.hpp>
 #include <stan/services/util/create_rng.hpp>
 #include <string>
+#include <type_traits>
 #include <vector>
 
 namespace stan {
@@ -20,7 +21,10 @@ namespace services {
  * approximation to the sample writer and writing messages to the
  * logger, returning a return code of 0 if successful.
  *
- * Interrupts are called 
+ * Interrupts are called between every compute-intensive operation.
+ *
+ * To turn off all console messages sent to the logger, set refresh to
+ * 0. 
  *
  * @tparam jacobian `true` to include Jacobian adjustment for
  * constrained parameters
@@ -32,7 +36,7 @@ namespace services {
  * @param[in] random_seed seed for generating random numbers in the
  * Stan program and in sampling  
  * @param[in] refresh period between iterations at which updates are
- * given 
+ * given, with a value of 0 turning off all messages
  * @param[in] interrupt callback for interrupting sampling
  * @param[in,out] logger callback for writing console messages from
  * sampler and from Stan programs
@@ -50,10 +54,11 @@ int laplace_sample(const Model& model, const Eigen::VectorXd& theta_hat,
 		   callbacks::writer& sample_writer) {
   // validate number of draws >= 0
   if (draws <= 0) {
-    std::stringstream msg;
-    msg << "Number of draws must be > 0, found " << draws << std::endl;
-    std::string msg_str = msg.str();
-    logger.error(msg_str);
+    if (refresh >= 0) {
+      logger.error("Number of draws must be > 0, found ");
+      logger.error(std::to_string(draws));
+      logger.error("\n");
+    }
     return error_codes::DATAERR;
   }
 
@@ -62,13 +67,14 @@ int laplace_sample(const Model& model, const Eigen::VectorXd& theta_hat,
   model.unconstrained_param_names(unc_param_names, false, false);
   int num_unc_params = unc_param_names.size();
   if (theta_hat.size() != num_unc_params) {
-    std::stringstream msg;
-    msg << "Specified mode is wrong size; expected "
-	<< num_unc_params << " unconstrained parameters"
-	<< ", but specified mode has size " << theta_hat.size()
-	<< std::endl;
-    std::string msg_str = msg.str();
-    logger.error(msg_str);
+    if (refresh > 0) {
+      logger.error("Specified mode is wrong size; expected ");
+      logger.error(std::to_string(num_unc_params));
+      logger.error(" unconstrained parameters,");
+      logger.error(" but specified mode has size ");
+      logger.error(std::to_string(theta_hat.size()));
+      logger.error("\n");
+    }
     return error_codes::DATAERR;
   }
 
@@ -95,38 +101,50 @@ int laplace_sample(const Model& model, const Eigen::VectorXd& theta_hat,
   };
 
   // calculate inverse negative Hessian's Cholesky factor
-  logger.info("Calculating Hessian\n");
+  if (refresh > 0) {
+    logger.info("Calculating Hessian");
+    logger.info("\n");
+  }
   double log_p;  // dummy
   Eigen::VectorXd grad;  // dummy
   Eigen::MatrixXd hessian;
   interrupt();
   math::internal::finite_diff_hessian_auto(log_density_fun, theta_hat, log_p,
 					   grad, hessian);
-  std::string log_density_msgs_str = log_density_msgs.str();
-  logger.info(log_density_msgs_str);
+  if (refresh > 0) {
+    logger.info(log_density_msgs);
+    logger.info("\n");
+  }
 
   // calculate Cholesky factor and inverse
   interrupt();
-  logger.info("\nCalculating inverse of Cholesky factor\n");
+  if (refresh > 0) {
+    logger.info("Calculating inverse of Cholesky factor");
+    logger.info("\n");
+  }
   Eigen::MatrixXd L_neg_hessian = (-hessian).llt().matrixL();
   interrupt();
   Eigen::MatrixXd inv_sqrt_neg_hessian = L_neg_hessian.inverse();
   interrupt();
   Eigen::MatrixXd neg_half_inv = -0.5 * inv_sqrt_neg_hessian * inv_sqrt_neg_hessian.transpose();
+
   // don't need log-determinant because log_q is unnormalized
   // double log_det = 2 * L_neg_hessian.diagonal().array().log().sum();
 
-  // generate draws
+  // generate draws and output to sample writer
   interrupt();
-  logger.info("\nGenerating draws\n");
+  if (refresh > 0) {
+    logger.info("Generating draws");
+    logger.info("\n");
+  }
   boost::ecuyer1988 rng = util::create_rng(random_seed, 0);
   Eigen::VectorXd draw_vec;  // declare draw_vec, msgs here to avoid re-alloc
   for (int m = 0; m < draws; ++m) {
     interrupt();  // allow interpution each iteration
     if (refresh > 0 && m % refresh == 0) {
-      std::stringstream log_msg;
-      log_msg << "iteration " << m << std::endl;
-      logger.info(log_msg);
+      logger.info("iteration: ");
+      logger.info(std::to_string(m));
+      logger.info("\n");
     }
     Eigen::VectorXd z(num_unc_params);
     for (int n = 0; n < num_unc_params; ++n) {
@@ -141,7 +159,10 @@ int laplace_sample(const Model& model, const Eigen::VectorXd& theta_hat,
     
     std::stringstream msgs;
     model.write_array(rng, unc_draw, draw_vec, include_tp, include_gq, &msgs);
-    logger.info(msgs);
+    if (refresh > 0) {
+      logger.info(msgs);
+      logger.info("\n");
+    }
     std::vector<double> draw(&draw_vec(0), &draw_vec(0) + draw_size);
     draw.push_back(log_p);
     draw.push_back(log_q);
