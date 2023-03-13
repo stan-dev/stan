20970439fab6b0a515268a42a70284de7685ebd7
Nicusor Serban <nserban@dymeng.com>
2023-03-13 19:26:19 +0100

    Added performance tests back

---
+++ src/test/performance/logistic_test.cpp
@@ -0,0 +1,218 @@
+/**
+ * Performance test: logistic.
+ *
+ * This test runs a simple logistic model 100 times with a fixed
+ * seed. The test records to a csv file information about the current
+ * build, whether the values match the expected known values, whether
+ * values are identical between runs, and the runtime in seconds for
+ * each of the 100 individual runs.
+ *
+ * The output file is a csv file located at
+ * test/performance/performance.csv.  If the file is empty, a header
+ * line will be written.  If the file is not empty and the header line
+ * of the file matches, a data row will be appended.  If the file is
+ * not empty and the header line is different, a header row and a data
+ * row will be appended.
+ *
+ * Below are the test routines in this test.
+ * 1. run: Runs the model 100 times. This test should always
+ *    succeed. Test failure indicates that the model could not be run.
+ * 2. values_from_tagged_version: Compares the first run's final
+ *    iteration against known values. Test will succeed if the values
+ *    match the known values; the test will fail otherwise.
+ * 3. values_same_run_to_run: Compares the first run's final iteration
+ *    against all runs' final iterations. Test will succeed if all the
+ *    values match; the test will fail otherwise.
+ * 4. write_results_to_disk: Writes the results to disk. Test will
+ *    succeed if writing to an empty file or to one with a matching
+ *    header; the test will fail if the header in the existing file
+ *    does not match the current header.
+ *
+ * Note: the values_from_tagged_version will need to be updated
+ * if Stan's RNG changes, the seeding mechanism changes, or the
+ * algorithm changes. The other routines should be fine.
+ *
+ * Each run of this test produces a single, csv row with 106 columns
+ * in the output file:
+ *   1. current date. Formatted using std::ctime: "Www Mmm dd hh:mm:ss yyyy"
+ *   2. git hash. The current 40-character SHA-1 git hash or "NA".
+ *   3. git date. The date of the last commit or "NA".
+ *   4. model name. "logistic"
+ *   5. matches tagged version. Either "yes" or "no".
+ *   6. all values the same. Either "yes" or "no".
+ *   7--106. run time (in seconds).
+ */
+
+#include <gtest/gtest.h>
+#include <test/test-models/performance/logistic.hpp>
+#include <stan/version.hpp>
+#include <ctime>
+#include <test/performance/utility.hpp>
+#include <boost/algorithm/string/trim.hpp>
+
+class performance : public ::testing::Test {
+ public:
+  static void SetUpTestCase() {
+    N = 100;
+    seconds_per_run.resize(N);
+    last_draws_per_run.resize(N);
+    matches_tagged_version = false;
+    all_values_same = false;
+  }
+
+  static int N;
+  static std::vector<double> seconds_per_run;
+  static std::vector<std::vector<double> > last_draws_per_run;
+  static bool matches_tagged_version;
+  static bool all_values_same;
+};
+
+int performance::N;
+std::vector<double> performance::seconds_per_run;
+std::vector<std::vector<double> > performance::last_draws_per_run;
+bool performance::matches_tagged_version;
+bool performance::all_values_same;
+
+using stan::test::performance::get_date;
+using stan::test::performance::get_git_date;
+using stan::test::performance::get_git_hash;
+using stan::test::performance::get_last_iteration_from_file;
+using stan::test::performance::quote;
+using stan::test::performance::run_command;
+using stan::test::performance::run_command_output;
+
+TEST_F(performance, run) {
+  clock_t t;
+  for (int n = 0; n < N; ++n) {
+    std::cout << "iteration: " << n << " / " << N << std::endl;
+    t = clock();  // start timer
+    stan::test::performance::command<stan_model>(
+        1000, 10000, "src/test/test-models/performance/logistic.data.R",
+        "test/performance/logistic_output.csv", 0U);
+    t = clock() - t;  // end timer
+    seconds_per_run[n] = static_cast<double>(t) / CLOCKS_PER_SEC;
+    last_draws_per_run[n]
+        = get_last_iteration_from_file("test/performance/logistic_output.csv");
+  }
+  SUCCEED();
+}
+
+// evaluate
+TEST_F(performance, values_from_tagged_version) {
+  int N_values = 9;
+  ASSERT_EQ(N_values, last_draws_per_run[0].size())
+      << "last tagged version, 2.17.0, had " << N_values << " elements";
+
+  std::vector<double> first_run = last_draws_per_run[0];
+  EXPECT_FLOAT_EQ(-65.216301, first_run[0]) << "lp__: index 0";
+
+  EXPECT_FLOAT_EQ(0.91851199, first_run[1]) << "accept_stat__: index 1";
+
+  EXPECT_FLOAT_EQ(0.76885802, first_run[2]) << "stepsize__: index 2";
+
+  EXPECT_FLOAT_EQ(2, first_run[3]) << "treedepth__: index 3";
+
+  EXPECT_FLOAT_EQ(3, first_run[4]) << "n_leapfrog__: index 4";
+
+  EXPECT_FLOAT_EQ(0, first_run[5]) << "divergent__: index 5";
+
+  EXPECT_FLOAT_EQ(66.696503, first_run[6]) << "energy__: index 6";
+
+  EXPECT_FLOAT_EQ(1.3577, first_run[7]) << "beta.1: index 7";
+
+  EXPECT_FLOAT_EQ(-0.51189202, first_run[8]) << "beta.2: index 8";
+
+  matches_tagged_version = !HasNonfatalFailure();
+}
+
+TEST_F(performance, values_same_run_to_run) {
+  int N_values = last_draws_per_run[0].size();
+
+  for (int i = 0; i < N_values; i++) {
+    double expected_value = last_draws_per_run[0][i];
+    for (int n = 1; n < N; n++) {
+      EXPECT_FLOAT_EQ(expected_value, last_draws_per_run[n][i])
+          << "expecting run to run values to be the same. Found run " << n
+          << " to have different values than the 0th run for "
+          << "index: " << i;
+    }
+  }
+  all_values_same = !HasNonfatalFailure();
+}
+
+TEST_F(performance, check_output_is_same) {
+  std::ifstream file_stream;
+  file_stream.open("test/performance/logistic_output.csv", std::ios_base::in);
+  ASSERT_TRUE(file_stream.good());
+
+  std::string line, expected;
+
+  getline(file_stream, line);
+  ASSERT_EQ(
+      "lp__,accept_stat__,stepsize__,treedepth__,n_leapfrog__,divergent__,"
+      "energy__,beta.1,beta.2",
+      line);
+  ASSERT_TRUE(file_stream.good());
+
+  getline(file_stream, line);
+  ASSERT_EQ("# Adaptation terminated", line);
+  ASSERT_TRUE(file_stream.good());
+
+  file_stream.close();
+}
+
+TEST_F(performance, write_results_to_disk) {
+  std::stringstream header;
+  std::stringstream line;
+
+  // current date / time
+  header << quote("date");
+  line << quote(get_date());
+
+  // git hash
+  header << "," << quote("git hash") << "," << quote("git date");
+  line << "," << quote(get_git_hash()) << "," << quote(get_git_date());
+
+  // model name: "logistic"
+  header << "," << quote("model name");
+  line << "," << quote("logistic");
+
+  // matches tagged values
+  header << "," << quote("matches tagged version");
+  line << "," << quote(matches_tagged_version ? "yes" : "no");
+
+  // all values same
+  header << "," << quote("all values same");
+  line << "," << quote(all_values_same ? "yes" : "no");
+
+  // N times
+  for (int n = 0; n < N; n++) {
+    std::stringstream ss;
+    ss << "run " << n + 1;
+    header << "," << quote(ss.str());
+    line << "," << seconds_per_run[n];
+  }
+
+  // append output to: test/performance/performance.csv
+  bool write_header = false;
+  std::fstream file_stream;
+
+  file_stream.open("test/performance/performance.csv", std::ios_base::in);
+  if (file_stream.peek() == std::fstream::traits_type::eof()) {
+    write_header = true;
+  } else {
+    std::string file_header;
+    std::getline(file_stream, file_header);
+
+    EXPECT_EQ(file_header, header.str()) << "header of file is different";
+    if (file_header != header.str())
+      write_header = true;
+  }
+  file_stream.close();
+
+  file_stream.open("test/performance/performance.csv", std::ios_base::app);
+  if (write_header)
+    file_stream << header.str() << std::endl;
+  file_stream << line.str() << std::endl;
+  file_stream.close();
+}
---
+++ src/test/performance/utility.hpp
@@ -0,0 +1,241 @@
+#ifndef TEST__PERFORMANCE__UTILITY_HPP
+#define TEST__PERFORMANCE__UTILITY_HPP
+
+#include <stdexcept>
+#include <boost/algorithm/string.hpp>
+#include <boost/date_time/posix_time/posix_time_types.hpp>
+#include <boost/date_time/posix_time/posix_time_types.hpp>
+#include <boost/math/special_functions/fpclassify.hpp>
+#include <boost/random/additive_combine.hpp>  // L'Ecuyer RNG
+#include <boost/random/uniform_real_distribution.hpp>
+#include <stan/model/gradient.hpp>
+#include <stan/services/sample/hmc_nuts_diag_e_adapt.hpp>
+#include <stan/io/empty_var_context.hpp>
+#include <stan/callbacks/interrupt.hpp>
+#include <stan/callbacks/stream_logger.hpp>
+#include <stan/callbacks/stream_writer.hpp>
+#include <stan/callbacks/writer.hpp>
+
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+namespace stan {
+namespace test {
+namespace performance {
+
+struct run_command_output {
+  std::string command;
+  std::string output;
+  long time;
+  int err_code;
+  bool hasError;
+  std::string header;
+  std::string body;
+
+  run_command_output(const std::string command, const std::string output,
+                     const long time, const int err_code)
+      : command(command),
+        output(output),
+        time(time),
+        err_code(err_code),
+        hasError(err_code != 0),
+        header(),
+        body() {
+    size_t end_of_header = output.find("\n\n");
+    if (end_of_header == std::string::npos)
+      end_of_header = 0;
+    else
+      end_of_header += 2;
+    header = output.substr(0, end_of_header);
+    body = output.substr(end_of_header);
+  }
+
+  run_command_output()
+      : command(),
+        output(),
+        time(0),
+        err_code(0),
+        hasError(false),
+        header(),
+        body() {}
+};
+
+std::ostream& operator<<(std::ostream& os, const run_command_output& out) {
+  os << "run_command output:"
+     << "\n"
+     << "- command:   " << out.command << "\n"
+     << "- output:    " << out.output << "\n"
+     << "- time (ms): " << out.time << "\n"
+     << "- err_code:  " << out.err_code << "\n"
+     << "- hasError:  " << (out.hasError ? "true" : "false") << "\n"
+     << "- header:    " << out.header << "\n"
+     << "- body:      " << out.body << std::endl;
+  return os;
+}
+
+/**
+ * Runs the command provided and returns the system output
+ * as a string.
+ *
+ * @param command A command that can be run from the shell
+ * @return the system output of the command
+ */
+run_command_output run_command(std::string command) {
+  using boost::posix_time::microsec_clock;
+  using boost::posix_time::ptime;
+
+  FILE* in;
+  std::string new_command = command + " 2>&1";
+  // captures both cout amd err
+
+  in = popen(command.c_str(), "r");
+
+  if (!in) {
+    std::string err_msg;
+    err_msg = "Fatal error with popen; could not execute: \"";
+    err_msg += command;
+    err_msg += "\"";
+    throw std::runtime_error(err_msg.c_str());
+  }
+
+  std::string output;
+  char buf[1024];
+  size_t count;
+  ptime time_start(microsec_clock::universal_time());  // start timer
+  while ((count = fread(&buf, 1, 1024, in)) > 0)
+    output += std::string(&buf[0], &buf[count]);
+  ptime time_end(microsec_clock::universal_time());  // end timer
+
+  // bits 15-8 is err code, bit 7 if core dump, bits 6-0 is signal number
+  int err_code = pclose(in);
+  // on Windows, err code is the return code.
+  if (err_code != 0 && (err_code >> 8) > 0)
+    err_code >>= 8;
+
+  return run_command_output(
+      command, output, (time_end - time_start).total_milliseconds(), err_code);
+}
+
+std::vector<double> get_last_iteration_from_file(const char* filename) {
+  std::vector<double> draw;
+  const char comment = '#';
+
+  std::ifstream file_stream(filename);
+  std::string line;
+  std::string last_values;
+  while (std::getline(file_stream, line)) {
+    if (line.length() > 0 && line[0] != comment)
+      last_values = line;
+  }
+
+  std::stringstream values_stream(last_values);
+  std::vector<std::string> values;
+  std::string value;
+  while (std::getline(values_stream, value, ','))
+    values.push_back(value);
+
+  draw.resize(values.size());
+  for (size_t n = 0; n < draw.size(); ++n) {
+    draw[n] = atof(values[n].c_str());
+  }
+
+  return draw;
+}
+
+template <typename T>
+std::string quote(const T& val) {
+  std::stringstream quoted_val;
+  quoted_val << "\"" << val << "\"";
+  return quoted_val.str();
+}
+
+std::string get_git_hash() {
+  run_command_output git_hash = run_command("git rev-parse HEAD");
+  if (git_hash.hasError)
+    return "NA";
+  boost::trim(git_hash.body);
+  return git_hash.body;
+}
+
+std::string get_git_date() {
+  run_command_output git_date_command = run_command("git log --format=%ct -1");
+  if (git_date_command.hasError)
+    return "NA";
+  boost::trim(git_date_command.body);
+
+  long timestamp = atol(git_date_command.body.c_str());
+  std::time_t git_date(timestamp);
+
+  std::stringstream date_ss;
+  date_ss << std::ctime(&git_date);
+
+  std::string date;
+  date = date_ss.str();
+
+  boost::trim(date);
+  return date;
+}
+
+std::string get_date() {
+  std::time_t curr_date;
+  time(&curr_date);
+
+  std::stringstream date_ss;
+  date_ss << std::ctime(&curr_date);
+
+  std::string date;
+  date = date_ss.str();
+
+  boost::trim(date);
+  return date;
+}
+
+template <class Model>
+int command(int num_warmup, int num_samples, const std::string data_file,
+            const std::string output_file, unsigned int random_seed) {
+  // Data input
+  std::fstream data_stream(data_file.c_str(), std::fstream::in);
+  stan::io::dump data_var_context(data_stream);
+  data_stream.close();
+
+  // Sample output
+  callbacks::writer init_writer;
+  callbacks::stream_logger logger(std::cout, std::cout, std::cout, std::cerr,
+                                  std::cerr);
+  std::fstream output_stream(output_file.c_str(), std::fstream::out);
+  callbacks::stream_writer sample_writer(output_stream, "# ");
+  callbacks::writer diagnostic_writer;
+  callbacks::interrupt interrupt;
+
+  Model model(data_var_context, 0, &std::cout);
+  stan::io::empty_var_context init_context;
+  double init_radius = 0;
+  int id = 0;
+  int num_thin = 1;
+  bool save_warmup = false;
+  int refresh = num_samples;
+  double stepsize = 1.0;
+  double stepsize_jitter = 0.0;
+  int max_depth = 10;
+  double delta = 0.8;
+  double gamma = 0.05;
+  double kappa = 0.75;
+  double t0 = 10;
+  int init_buffer = 75;
+  int term_buffer = 50;
+  int window = 25;
+  return stan::services::sample::hmc_nuts_diag_e_adapt(
+      model, init_context, random_seed, id, init_radius, num_warmup,
+      num_samples, num_thin, save_warmup, refresh, stepsize, stepsize_jitter,
+      max_depth, delta, gamma, kappa, t0, init_buffer, term_buffer, window,
+      interrupt, logger, init_writer, sample_writer, diagnostic_writer);
+}
+
+}  // namespace performance
+}  // namespace test
+}  // namespace stan
+#endif
