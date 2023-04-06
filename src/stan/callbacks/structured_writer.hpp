38737abce151b4f5ac098196d96c659c1278bc1a
Mitzi Morris <mitzi@panix.com>
2023-04-06 17:00:36 -0400

    cleanup

--- src/stan/callbacks/structured_writer.hpp
+++ src/stan/callbacks/structured_writer.hpp
@@ -1,6 +1,10 @@
 #ifndef STAN_CALLBACKS_STRUCTURED_WRITER_HPP
 #define STAN_CALLBACKS_STRUCTURED_WRITER_HPP
 
+#include <stan/math/prim/fun/Eigen.hpp>
+#include <vector>
+#include <string>
+
 namespace stan {
 namespace callbacks {
 
@@ -26,6 +30,23 @@ class structured_writer {
    *
    */
   virtual void end_token() {}
+
+  virtual void keyed_begin(const std::string& key) {}
+  virtual void keyed_null() {}
+  virtual void keyed_bool(const std::string& key, bool value) {}
+
+  virtual void keyed_value(const std::string& key, int value) {}
+  virtual void keyed_value(const std::string& key, double value) {}
+  virtual void keyed_value(const std::string& key,
+    const std::tuple<Eigen::VectorXd, Eigen::VectorXd>& value) {}
+
+  virtual void keyed_values(const std::string& key,
+                            const std::vector<double> value) {}
+  virtual void keyed_values(const std::string& key,
+    const std::vector<std::tuple<Eigen::VectorXd, Eigen::VectorXd>>& values) {}
+
+  virtual void reset() {}
+
 };
 
 }  // namespace callbacks
