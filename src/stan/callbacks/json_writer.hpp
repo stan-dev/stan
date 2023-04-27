a8c68451ee8453a9bec8bfa07849df588140f137
Steve Bronder <sbronder@flatironinstitute.org>
2023-04-27 15:28:50 -0400

    fix off by one error in precess_string for json reader

--- src/stan/callbacks/json_writer.hpp
+++ src/stan/callbacks/json_writer.hpp
@@ -7,6 +7,7 @@
 #include <string>
 #include <vector>
 #include <memory>
+#include <cstring>
 
 namespace stan {
 namespace callbacks {
@@ -60,26 +61,36 @@ class json_writer final : public structured_writer {
    * @return The processed string.
    */
   std::string process_string(const std::string& value) {
-    static constexpr std::array<char, 10> chars_to_escape
-        = {'\\', '"', '/', '\b', '\f', '\n', '\r', '\t', '\v', '\a'};
+    static constexpr std::array<char, 11> chars_to_escape
+        = {'\\', '"', '/', '\b', '\f', '\n', '\r', '\t', '\v', '\a', '\0'};
     static constexpr std::array<const char*, 10> chars_to_replace = {
         "\\\\", "\\\"", "\\/", "\\b", "\\f", "\\n", "\\r", "\\t", "\\v", "\\a"};
-    static constexpr const char* chars_escape = "\\\"/\b\f\n\r\t\v\a";
-    std::string new_value = value;
+    // Replacing every value leads to 2x the size 
+    std::string new_value(value.size() * 2, 'x');
     std::size_t pos = 0;
     std::size_t count = 0;
-    while ((pos = value.find_first_of(chars_escape, pos, 10))
+    std::size_t prev_pos = 0;
+    while ((pos = value.find_first_of(chars_to_escape.data(), pos, 10))
            != std::string::npos) {
-      for (int i = 0; i < 11; ++i) {
-        if (value[pos] == chars_to_escape[i]) {
-          new_value.replace(pos + count, 1, chars_to_replace[i], 2);
-          pos += 1;
-          count++;
-        }
+      for (int i = prev_pos; i < pos; ++i) {
+        new_value[i + count] = value[i];
       }
+      const auto iter_pos = std::find(chars_to_escape.begin(), chars_to_escape.end(), value[pos]);
+      const int idx = std::distance(chars_to_escape.begin(), iter_pos);
+      new_value[pos + count] = chars_to_replace[idx][0];
+      new_value[pos + count + 1] = chars_to_replace[idx][1];
+      pos += 1;
+      count++;
+      prev_pos = pos;
     }
+    for (int i = prev_pos; i < value.size(); ++i) {
+      new_value[i + count] = value[i];
+    }
+    // Shrink any unused space
+    new_value.resize(value.size() + count);
     return new_value;
   }
+
 
   /**
    * Writes key plus colon for key-value pair.
