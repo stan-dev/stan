5d046f6dff3da6cff4932c21be781941975113a1
Brian Ward <bward@flatironinstitute.org>
2023-05-04 15:42:35 -0400

    Clean up standalone_gq code now that cmdstan is updated

--- src/stan/services/sample/standalone_gqs.hpp
+++ src/stan/services/sample/standalone_gqs.hpp
@@ -17,34 +17,6 @@ namespace stan {
 namespace services {
 
 /**
- * Find the names, dimensions of the model parameters.
- * Assembles vectors of name, dimensions for the variables
- * declared in the parameters block.
- *
- * @tparam Model type of model
- * @param[in] model model to query
- * @param[in, out] param_names sequence of parameter names
- * @param[in, out] param_dimss sequence of variable dimensionalities
- */
-template <class Model>
-void get_model_parameters(const Model &model,
-                          std::vector<std::string> &param_names,
-                          std::vector<std::vector<size_t>> &param_dimss) {
-  std::vector<std::string> all_param_names;
-  model.get_param_names(all_param_names, false, false);
-  std::vector<std::vector<size_t>> dimss;
-  model.get_dims(dimss, false, false);
-  // remove zero-size
-  for (size_t i = 0; i < all_param_names.size(); i++) {
-    auto &v = dimss[i];
-    if (std::find(v.begin(), v.end(), 0) == v.end()) {
-      param_names.emplace_back(all_param_names[i]);
-      param_dimss.emplace_back(dimss[i]);
-    }
-  }
-}
-
-/**
  * Given a set of draws from a fitted model, generate corresponding
  * quantities of interest which are written to callback writer.
  * Matrix of draws consists of one row per draw, one column per parameter.
