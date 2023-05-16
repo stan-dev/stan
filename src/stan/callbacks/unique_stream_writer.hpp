97912743d5571eb2bb570edbe2f449bc21494113
Mitzi Morris <mitzi@panix.com>
2023-05-16 18:18:42 -0400

    adding in changes to callbacks from branch multi-path

--- src/stan/callbacks/unique_stream_writer.hpp
+++ src/stan/callbacks/unique_stream_writer.hpp
@@ -12,28 +12,28 @@ namespace stan {
 namespace callbacks {
 
 /**
- * <code>unique_stream_writer</code> is an implementation
- * of <code>writer</code> that holds a unique pointer to the stream it is
+ * `unique_stream_writer` is an implementation
+ * of `writer` that holds a unique pointer to the stream it is
  * writing to.
  * @tparam Stream A type with with a valid `operator<<(std::string)`
+ * @tparam Deleter A class with a valid `operator()` method for deleting the
+ * output stream
  */
-template <typename Stream>
+template <typename Stream, typename Deleter = std::default_delete<Stream>>
 class unique_stream_writer final : public writer {
  public:
   /**
    * Constructs a unique stream writer with an output stream
    * and an optional prefix for comments.
    *
-   * @param[in, out] A unique pointer to a type inheriting from `std::ostream`
+   * @param[in, out] output A unique pointer to a type inheriting from
+   * `std::ostream`
    * @param[in] comment_prefix string to stream before each comment line.
    *  Default is "".
    */
-  explicit unique_stream_writer(std::unique_ptr<Stream>&& output,
+  explicit unique_stream_writer(std::unique_ptr<Stream, Deleter>&& output,
                                 const std::string& comment_prefix = "")
-      : output_(std::move(output)), comment_prefix_(comment_prefix) {
-    if (output_ == nullptr)
-      throw std::invalid_argument("output writer cannot be null");
-  }
+      : output_(std::move(output)), comment_prefix_(comment_prefix) {}
 
   unique_stream_writer();
   unique_stream_writer(unique_stream_writer& other) = delete;
@@ -60,40 +60,50 @@ class unique_stream_writer final : public writer {
   }
 
   /**
+   * Get the underlying stream
+   */
+  inline auto& get_stream() noexcept { return *output_; }
+
+  /**
    * Writes a set of values in csv format followed by a newline.
    *
    * Note: the precision of the output is determined by the settings
    *  of the stream on construction.
    *
-   * @param[in] state Values in a std::vector
+   * @param[in] values Values in a std::vector
    */
-  void operator()(const std::vector<double>& state) {
+  void operator()(const std::vector<double>& values) {
     if (output_ == nullptr)
       return;
-    write_vector(state);
+    write_vector(values);
   }
 
-  void operator()(const std::tuple<Eigen::VectorXd, Eigen::VectorXd>& state) {
+  /**
+   * Writes multiple rows and columns of values in csv format.
+   *
+   * Note: the precision of the output is determined by the settings
+   *  of the stream on construction.
+   *
+   * @param[in] values A matrix of values. The input is expected to have
+   * parameters in the rows and samples in the columns. The matrix is then
+   * transposed for the output.
+   */
+  void operator()(const Eigen::MatrixXd& values) {
     if (output_ == nullptr)
       return;
     Eigen::IOFormat CommaInitFmt(Eigen::StreamPrecision, Eigen::DontAlignCols,
                                  ", ", "", "", "\n", "", "");
-    *output_ << std::get<0>(state).transpose().eval();
-    *output_ << std::get<1>(state).transpose().eval();
-  }
-
-  void operator()(const Eigen::MatrixXd& states) {
-    if (output_ == nullptr)
-      return;
-    Eigen::IOFormat CommaInitFmt(Eigen::StreamPrecision, Eigen::DontAlignCols,
-                                 ", ", "", "", "\n", "", "");
-    *output_ << states.transpose().format(CommaInitFmt);
+    *output_ << values.transpose().format(CommaInitFmt);
   }
 
   /**
    * Writes the comment_prefix to the stream followed by a newline.
    */
-  void operator()() { *output_ << comment_prefix_ << std::endl; }
+  void operator()() {
+    if (output_ == nullptr)
+      return;
+    *output_ << comment_prefix_ << std::endl;
+  }
 
   /**
    * Writes the comment_prefix then the message followed by a newline.
@@ -106,16 +116,11 @@ class unique_stream_writer final : public writer {
     *output_ << comment_prefix_ << message << std::endl;
   }
 
-  /**
-   * Get the underlying stream
-   */
-  inline auto& get_stream() noexcept { return *output_; }
-
  private:
   /**
    * Output stream
    */
-  std::unique_ptr<Stream> output_;
+  std::unique_ptr<Stream, Deleter> output_;
 
   /**
    * Comment prefix to use when printing comments: strings and blank lines
