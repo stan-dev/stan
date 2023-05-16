0e1313ac100a821ba53bf23ed4025afe65d08290
Mitzi Morris <mitzi@panix.com>
2023-05-16 14:46:04 -0400

    bring code into line w/ multi-path branch

--- src/stan/callbacks/json_writer.hpp
+++ src/stan/callbacks/json_writer.hpp
@@ -1,154 +1,94 @@
 #ifndef STAN_CALLBACKS_JSON_WRITER_HPP
 #define STAN_CALLBACKS_JSON_WRITER_HPP
 
-#include <stan/callbacks/structured_writer.hpp>
 #include <stan/math/prim/fun/Eigen.hpp>
 #include <ostream>
 #include <string>
 #include <vector>
+#include <memory>
+#include <cstring>
 
 namespace stan {
 namespace callbacks {
 
 /**
- * <code>json_writer</code> is an implementation of
- * <code>structured_writer</code> that writes JSON format data to a stream.
+ * `json_writer` methods output JSON data to a stream piecewise,
+ * allowing for streaming outputs of structured data.
  * @tparam Stream A type with with a valid `operator<<(std::string)`
+ * @tparam Deleter A class with a valid `operator()` method for deleting the
+ * output stream
  */
 template <typename Stream, typename Deleter = std::default_delete<Stream>>
-class json_writer final : public structured_writer {
- public:
-  /**
-   * Constructs a json writer with an output stream.
-   *
-   * @param[in, out] A unique pointer to a type inheriting from `std::ostream`
-   */
-  explicit json_writer(std::unique_ptr<Stream, Deleter>&& output)
-      : output_(std::move(output)) {
-    if (output_ == nullptr)
-      throw std::invalid_argument("writer cannot be null");
-  }
-
-  json_writer();
-  json_writer(json_writer& other) = delete;
-  json_writer(json_writer&& other)
-      : output_(std::move(other.output_)) {}
-
-  /**
-   * Virtual destructor.
-   */
-  virtual ~json_writer() {}
-
-  /**
-   * Writes "{", initial token of a JSON object.
-   */
-  void begin() {
-    *output_ << "{" << std::endl;
-    is_rest_ = false;
-  }
-
-  /**
-   * Writes object member key followed by ": {"
-   *
-   */
-  void write_begin(const std::string& key) {
-    write_key(key);
-    begin();
-  }
-
-  /**
-   * Writes "}", final token of a JSON object.
-   */
-  void end() { *output_ << "}"; }
-
-  void write_string(const std::string& key, const std::string& value) {
-    write_sep();
-    write_key(key);
-    *output_ << "\"" << value << "\"";
-  }
-
-  void write_null(const std::string& key) {
-    write_sep();
-    write_key(key);
-    *output_ << "\"null\" ";
-  }
-
-  void write_bool(const std::string& key, bool value) {
-    write_sep();
-    write_key(key);
-    *output_ << "\"" << (value ? "true" : "false") << "\" :" << value;
-  }
-
-  void write_value(const std::string& key, int value) {
-    write_sep();
-    write_key(key);
-    *output_ << value;
-  }
-
-  void write_value(const std::string& key, double value) {
-    write_sep();
-    write_key(key);
-    *output_ << value;
-  }
-
-  void write_value(const std::string& key,
-                    const std::tuple<Eigen::VectorXd, Eigen::VectorXd>& state) {
-    write_sep();
-    write_key(key);
-    Eigen::IOFormat CommaInitFmt(Eigen::StreamPrecision, Eigen::DontAlignCols,
-                                 ", ", "", "", "\n", "", "");
-    *output_ << std::get<0>(state).transpose().eval();
-    *output_ << std::get<1>(state).transpose().eval();
-  }
-
-  // complex numbers are objects?
-  //  void write_scalar(const std::string& key, double value) {
-  //  write_sep();
-  //  write_key(key);
-  //    *output_ << "\"" << key << "\" :" << value;
-  //  }
-
-  void write_values(const std::string& key, const std::vector<double>& values) {
-    write_sep();
-    write_key(key);
-    write_vector(values);
-  }
-
-  void write_values(const std::string& key, const Eigen::MatrixXd& states) {
-    write_sep();
-    write_key(key);
-    *output_ << "\"" << key << "\" : \"";
-    Eigen::IOFormat CommaInitFmt(Eigen::StreamPrecision, Eigen::DontAlignCols,
-                                 ", ", "", "", "\n", "", "");
-    *output_ << states.transpose().format(CommaInitFmt);
-  }
-
-  /**
-   * Reset state
-   */
-  void reset() {
-    is_rest_ = false;
-  }
-
+class json_writer {
  private:
-  /**
-   * Output stream
-   */
-  std::unique_ptr<Stream, Deleter> output_;
+  // Output stream
+  std::unique_ptr<Stream, Deleter> output_{nullptr};
+  // Whether or not the record's current object needs a comma separator
+  bool record_internal_needs_comma_ = false;
+  // Depth of records (used to determine whether or not to print comma
+  // separator)
+  int record_depth_ = 0;
+  // Whether or not the record's parent object needs a comma separator
+  bool record_needs_comma_;
 
   /**
-   * State - first member or rest of object?
+   * Writes a comma separator for the record's parent object if needed.
    */
-  bool is_rest_;
+  void write_record_comma_if_needed() {
+    if (record_depth_ > 0 && record_needs_comma_) {
+      *output_ << ",";
+    }
+  }
 
   /**
-   * Determines whether or not output requires comma separator
+   * Determines whether a record's internal object requires a comma separator
    */
   void write_sep() {
-    if (is_rest_)
+    if (record_internal_needs_comma_) {
       *output_ << ", ";
-    else
-      is_rest_ = true;
+    } else {
+      record_internal_needs_comma_ = true;
+    }
+  }
+
+  /**
+   * Process a string to escape special characters.
+   * Valid json strings cannot contain any of the special characters
+   * `'\\', '"', '/', '\b', '\f', '\n', '\r', '\t', '\v', '\a', '\0'`.
+   * In order to print these characters, they must be escaped.
+   * @param value The string to process.
+   * @return The processed string.
+   */
+  std::string process_string(const std::string& value) {
+    static constexpr std::array<char, 11> chars_to_escape
+        = {'\\', '"', '/', '\b', '\f', '\n', '\r', '\t', '\v', '\a', '\0'};
+    static constexpr std::array<const char*, 11> chars_to_replace
+        = {"\\\\", "\\\"", "\\/", "\\b", "\\f", "\\n",
+           "\\r",  "\\t",  "\\v", "\\a", "\\0"};
+    // Replacing every value leads to 2x the size
+    std::string new_value(value.size() * 2, 'x');
+    std::size_t pos = 0;
+    std::size_t count = 0;
+    std::size_t prev_pos = 0;
+    while ((pos = value.find_first_of(chars_to_escape.data(), pos, 10))
+           != std::string::npos) {
+      for (int i = prev_pos; i < pos; ++i) {
+        new_value[i + count] = value[i];
+      }
+      int idx
+          = strchr(chars_to_escape.data(), value[pos]) - chars_to_escape.data();
+      new_value[pos + count] = chars_to_replace[idx][0];
+      new_value[pos + count + 1] = chars_to_replace[idx][1];
+      pos += 1;
+      count++;
+      prev_pos = pos;
+    }
+    for (int i = prev_pos; i < value.size(); ++i) {
+      new_value[i + count] = value[i];
+    }
+    // Shrink any unused space
+    new_value.resize(value.size() + count);
+    return new_value;
   }
 
   /**
@@ -157,6 +97,24 @@ class json_writer final : public structured_writer {
    * @param[in] key member name.
    */
   void write_key(const std::string& key) { *output_ << "\"" << key << "\" : "; }
+  /**
+   * Writes a set of comma separated strings.
+   * Strings are cleaned to escape special characters.
+   *
+   * @param[in] v Values in a std::vector
+   */
+  void write_vector(const std::vector<std::string>& v) {
+    if (v.empty()) {
+      return;
+    }
+    *output_ << "[ ";
+    auto last = v.end();
+    --last;
+    for (auto it = v.begin(); it != last; ++it) {
+      *output_ << process_string(*it) << ", ";
+    }
+    *output_ << v.back() << " ]";
+  }
 
   /**
    * Writes a set of comma separated values.
@@ -175,6 +133,240 @@ class json_writer final : public structured_writer {
       *output_ << *it << ", ";
     }
     *output_ << v.back() << " ]";
+  }
+
+ public:
+  json_writer() : output_(nullptr) {}
+  /**
+   * Constructs a json writer with an output stream.
+   *
+   * @param[in, out] output unique pointer to a type inheriting from
+   * `std::ostream`
+   */
+  explicit json_writer(std::unique_ptr<Stream, Deleter>&& output)
+      : output_(std::move(output)) {
+    if (output_ == nullptr)
+      throw std::invalid_argument("writer cannot be null");
+  }
+
+  json_writer(json_writer& other) = delete;
+  json_writer(json_writer&& other) : output_(std::move(other.output_)) {}
+
+  ~json_writer() {}
+
+  /**
+   * Writes "{", initial token of a JSON record.
+   */
+  void begin_record() {
+    write_record_comma_if_needed();
+    *output_ << "{";
+    record_needs_comma_ = false;
+    record_depth_++;
+  }
+
+  /**
+   * Writes "\"key\" : {", initial token of a named JSON record.
+   * @param[in] key The name of the record.
+   */
+  void begin_record(const std::string& key) {
+    write_record_comma_if_needed();
+    *output_ << "\"" << key << "\": {";
+    record_needs_comma_ = false;
+    record_depth_++;
+  }
+  /**
+   * Writes "}", final token of a JSON record.
+   */
+  void end_record() {
+    if (record_depth_ > 0) {
+      *output_ << "}";
+      record_depth_--;
+      if (record_depth_ > 0) {
+        record_needs_comma_ = true;
+      }
+      record_internal_needs_comma_ = false;
+    } else {
+      throw std::runtime_error(
+          "Attempted to end record, but there is no open record.");
+    }
+  }
+
+  /**
+   * Writes "[", initial token of a JSON list.
+   */
+  void begin_list() {
+    *output_ << "[";
+    record_internal_needs_comma_ = false;
+  }
+  /**
+   * Writes "]", final token of a JSON list.
+   */
+  void end_list() {
+    *output_ << "]";
+    write_sep();
+    *output_ << "\n";
+  }
+
+  /**
+   * Write a key-value pair to the output stream with a value of null as the
+   * value.
+   * @param key Name of the value pair
+   */
+  void write(const std::string& key) {
+    write_sep();
+    write_key(key);
+    *output_ << "\"null\" ";
+  }
+
+  /**
+   * Write a key-value pair where the value is a string.
+   * @param key Name of the value pair
+   * @param value string to write.
+   */
+  void write(const std::string& key, const std::string& value) {
+    std::string processsed_string = process_string(value);
+    write_sep();
+    write_key(key);
+    *output_ << "\"" << processsed_string << "\"";
+  }
+
+  /**
+   * Write a key-value pair where the value is a const char*.
+   * @param key Name of the value pair
+   * @param value pointer to chars to write.
+   */
+  void write(const std::string& key, const char* value) {
+    std::string processsed_string = process_string(value);
+    write_sep();
+    write_key(key);
+    *output_ << "\"" << processsed_string << "\"";
+  }
+
+  /**
+   * Write a key-value pair where the value is a bool.
+   * @param key Name of the value pair
+   * @param value bool to write.
+   */
+  void write(const std::string& key, bool value) {
+    write_sep();
+    write_key(key);
+    *output_ << (value ? "true" : "false");
+  }
+
+  /**
+   * Write a key-value pair where the value is an int.
+   * @param key Name of the value pair
+   * @param value int to write.
+   */
+  void write(const std::string& key, int value) {
+    write_sep();
+    write_key(key);
+    *output_ << value;
+  }
+
+  /**
+   * Write a key-value pair where the value is an `std::size_t`.
+   * @param key Name of the value pair
+   * @param value `std::size_t` to write.
+   */
+  void write(const std::string& key, std::size_t value) {
+    write_sep();
+    write_key(key);
+    *output_ << value;
+  }
+
+  /**
+   * Write a key-value pair where the value is a double.
+   * @param key Name of the value pair
+   * @param value double to write.
+   */
+  void write(const std::string& key, double value) {
+    write_sep();
+    write_key(key);
+    *output_ << value;
+  }
+
+  /**
+   * Write a key-value pair where the value is a complex value.
+   * @param key Name of the value pair
+   * @param value complex value to write.
+   */
+  void write(const std::string& key, const std::complex<double>& value) {
+    write_sep();
+    write_key(key);
+    *output_ << "\"" << key << "\" : [" << value.real() << ", " << value.imag()
+             << "]";
+  }
+
+  /**
+   * Write a key-value pair where the value is a vector to be made a list.
+   * @param key Name of the value pair
+   * @param values vector to write.
+   */
+  void write(const std::string& key, const std::vector<double>& values) {
+    write_sep();
+    write_key(key);
+    write_vector(values);
+  }
+
+  /**
+   * Write a key-value pair where the value is a vector of strings to be made a
+   * list.
+   * @param key Name of the value pair
+   * @param values vector of strings to write.
+   */
+  void write(const std::string& key, const std::vector<std::string>& values) {
+    write_sep();
+    write_key(key);
+    write_vector(values);
+  }
+
+  /**
+   * Write a key-value pair where the value is an Eigen Matrix.
+   * @param key Name of the value pair
+   * @param mat Eigen Matrix to write.
+   */
+  void write(const std::string& key, const Eigen::MatrixXd& mat) {
+    write_sep();
+    write_key(key);
+    Eigen::IOFormat json_format(Eigen::StreamPrecision, Eigen::DontAlignCols,
+                                ", ", ", ", "[", "]", "[", "]");
+    *output_ << mat.format(json_format);
+  }
+
+  /**
+   * Write a key-value pair where the value is an Eigen Vector.
+   * @param key Name of the value pair
+   * @param vec Eigen Vector to write.
+   */
+  void write(const std::string& key, const Eigen::VectorXd& vec) {
+    write_sep();
+    write_key(key);
+    Eigen::IOFormat json_format(Eigen::StreamPrecision, Eigen::DontAlignCols,
+                                ", ", "", "", "", "[", "]");
+    *output_ << vec.transpose().format(json_format);
+  }
+
+  /**
+   * Write a key-value pair where the value is a Eigen RowVector.
+   * @param key Name of the value pair
+   * @param vec Eigen RowVector to write.
+   */
+  void write(const std::string& key, const Eigen::RowVectorXd& vec) {
+    write_sep();
+    write_key(key);
+    Eigen::IOFormat json_format(Eigen::StreamPrecision, Eigen::DontAlignCols,
+                                ", ", "", "", "", "[", "]");
+    *output_ << vec.format(json_format);
+  }
+
+  /**
+   * Reset state
+   */
+  void reset() {
+    record_internal_needs_comma_ = false;
+    record_needs_comma_ = false;
+    record_depth_ = 0;
   }
 };
 
--- src/test/unit/callbacks/json_writer_test.cpp
+++ src/test/unit/callbacks/json_writer_test.cpp
@@ -3,8 +3,8 @@
 #include <stan/callbacks/json_writer.hpp>
 
 struct deleter_noop {
-    template <typename T>
-    constexpr void operator()(T* arg) const {}
+  template <typename T>
+  constexpr void operator()(T* arg) const {}
 };
 class StanInterfaceCallbacksJsonWriter : public ::testing::Test {
  public:
@@ -16,18 +16,16 @@ class StanInterfaceCallbacksJsonWriter : public ::testing::Test {
     ss.clear();
   }
 
-  void TearDown() {
-    writer.reset();
-  }
+  void TearDown() { writer.reset(); }
 
   std::stringstream ss;
   stan::callbacks::json_writer<std::stringstream, deleter_noop> writer;
 };
 
-TEST_F(StanInterfaceCallbacksJsonWriter, begin_end) {
-  EXPECT_NO_THROW(writer.begin());
-  EXPECT_NO_THROW(writer.end());
-  EXPECT_EQ("{\n}", ss.str());
+TEST_F(StanInterfaceCallbacksJsonWriter, begin_record_end) {
+  EXPECT_NO_THROW(writer.begin_record());
+  EXPECT_NO_THROW(writer.end_record());
+  EXPECT_EQ("{}", ss.str());
 }
 
 TEST_F(StanInterfaceCallbacksJsonWriter, write_double_vector) {
@@ -37,49 +35,51 @@ TEST_F(StanInterfaceCallbacksJsonWriter, write_double_vector) {
   for (int n = 0; n < N; ++n)
     x.push_back(n);
 
-  EXPECT_NO_THROW(writer.write_values(key, x));
-  EXPECT_EQ("\"key\" : [ 0, 1, 2, 3, 4 ]",
-            ss.str());
+  EXPECT_NO_THROW(writer.write(key, x));
+  EXPECT_EQ("\"key\" : [ 0, 1, 2, 3, 4 ]", ss.str());
 
-  EXPECT_NO_THROW(writer.write_values(key, x));
-  EXPECT_EQ("\"key\" : [ 0, 1, 2, 3, 4 ]"
-            ", \"key\" : [ 0, 1, 2, 3, 4 ]",
-            ss.str());
+  EXPECT_NO_THROW(writer.write(key, x));
+  EXPECT_EQ(
+      "\"key\" : [ 0, 1, 2, 3, 4 ]"
+      ", \"key\" : [ 0, 1, 2, 3, 4 ]",
+      ss.str());
 
-  EXPECT_NO_THROW(writer.write_values(key, x));
-  EXPECT_EQ("\"key\" : [ 0, 1, 2, 3, 4 ]"
-            ", \"key\" : [ 0, 1, 2, 3, 4 ]"
-            ", \"key\" : [ 0, 1, 2, 3, 4 ]",
-            ss.str());
+  EXPECT_NO_THROW(writer.write(key, x));
+  EXPECT_EQ(
+      "\"key\" : [ 0, 1, 2, 3, 4 ]"
+      ", \"key\" : [ 0, 1, 2, 3, 4 ]"
+      ", \"key\" : [ 0, 1, 2, 3, 4 ]",
+      ss.str());
 }
 
 TEST_F(StanInterfaceCallbacksJsonWriter, single_member) {
   std::string key("key");
   std::string value("value");
-  EXPECT_NO_THROW(writer.begin());
-  EXPECT_NO_THROW(writer.write_string(key, value));
-  EXPECT_NO_THROW(writer.end());
-  EXPECT_EQ("{\n\"key\" : \"value\"}",
-            ss.str());
+  EXPECT_NO_THROW(writer.begin_record());
+  EXPECT_NO_THROW(writer.write(key, value));
+  EXPECT_NO_THROW(writer.end_record());
+  EXPECT_EQ("{\"key\" : \"value\"}", ss.str());
 }
 
 TEST_F(StanInterfaceCallbacksJsonWriter, more_members) {
   std::string key("key");
   std::string value("value");
-  EXPECT_NO_THROW(writer.begin());
-  EXPECT_NO_THROW(writer.write_string(key, value));
-  EXPECT_NO_THROW(writer.write_string(key, value));
+  EXPECT_NO_THROW(writer.begin_record());
+  EXPECT_NO_THROW(writer.write(key, value));
+  EXPECT_NO_THROW(writer.write(key, value));
 
-  EXPECT_EQ("{\n\"key\" : \"value\""
-            ", \"key\" : \"value\"",
-            ss.str());
+  EXPECT_EQ(
+      "{\"key\" : \"value\""
+      ", \"key\" : \"value\"",
+      ss.str());
 
-  EXPECT_NO_THROW(writer.write_string(key, value));
-  EXPECT_NO_THROW(writer.end());
-  EXPECT_EQ("{\n\"key\" : \"value\""
-            ", \"key\" : \"value\""
-            ", \"key\" : \"value\"}",
-            ss.str());
+  EXPECT_NO_THROW(writer.write(key, value));
+  EXPECT_NO_THROW(writer.end_record());
+  EXPECT_EQ(
+      "{\"key\" : \"value\""
+      ", \"key\" : \"value\""
+      ", \"key\" : \"value\"}",
+      ss.str());
 }
 
 TEST_F(StanInterfaceCallbacksJsonWriter, write_double_vector_precision2) {
@@ -87,37 +87,46 @@ TEST_F(StanInterfaceCallbacksJsonWriter, write_double_vector_precision2) {
   std::string key("key");
   const int N = 5;
   std::vector<double> x{1.23456789, 2.3456789, 3.45678910, 4.567890123};
-  EXPECT_NO_THROW(writer.write_values(key, x));
+  EXPECT_NO_THROW(writer.write(key, x));
   EXPECT_EQ("\"key\" : [ 1.2, 2.3, 3.5, 4.6 ]", ss.str());
 }
 
-// TEST_F(StanInterfaceCallbacksJsonWriter, write_double_vector_precision3) {
-//   const int N = 5;
-//   std::vector<double> x{1.23456789, 2.3456789, 3.45678910, 4.567890123};
-//   writer.get_stream().precision(3);
-//   EXPECT_NO_THROW(writer(x));
-//   EXPECT_EQ("1.23,2.35,3.46,4.57\n",
-//             ss.str());
-// }
+TEST_F(StanInterfaceCallbacksJsonWriter, write_string_special_characters) {
+  ss << std::setprecision(2);
+  std::string key("key");
+  std::string x(
+      "the\\quick\"brown/\bfox\fjumped\nover\rthe\tlazy\vdog\atwotimes");
+  EXPECT_NO_THROW(writer.write(key, x));
+  EXPECT_EQ(
+      "\"key\" : "
+      "\"the\\\\quick\\\"brown\\/"
+      "\\bfox\\fjumped\\nover\\rthe\\tlazy\\vdog\\atwotimes\"",
+      ss.str());
+}
+TEST_F(StanInterfaceCallbacksJsonWriter, write_double_vector_precision3) {
+  const int N = 5;
+  std::vector<double> x{1.23456789, 2.3456789, 3.45678910, 4.567890123};
+  ss.precision(3);
+  EXPECT_NO_THROW(writer.write("key", x));
+  EXPECT_EQ("\"key\" : [ 1.23, 2.35, 3.46, 4.57 ]", ss.str());
+}
 
-// TEST_F(StanInterfaceCallbacksJsonWriter, write_string_vector) {
-//   const int N = 5;
-//   std::vector<std::string> x;
-//   for (int n = 0; n < N; ++n)
-//     x.push_back(boost::lexical_cast<std::string>(n));
+TEST_F(StanInterfaceCallbacksJsonWriter, write_string_vector) {
+  const int N = 5;
+  std::vector<std::string> x;
+  for (int n = 0; n < N; ++n)
+    x.push_back(boost::lexical_cast<std::string>(n));
 
-//   EXPECT_NO_THROW(writer(x));
-//   EXPECT_EQ("0,1,2,3,4\n",
-//             ss.str());
-// }
+  EXPECT_NO_THROW(writer.write("key", x));
+  EXPECT_EQ("\"key\" : [ 0, 1, 2, 3, 4 ]", ss.str());
+}
 
-// TEST_F(StanInterfaceCallbacksJsonWriter, write_null) {
-//   EXPECT_NO_THROW(writer());
-//   EXPECT_EQ("\n", ss.str());
-// }
+TEST_F(StanInterfaceCallbacksJsonWriter, write_null) {
+  EXPECT_NO_THROW(writer.write("message"));
+  EXPECT_EQ("\"message\" : \"null\" ", ss.str());
+}
 
-// TEST_F(StanInterfaceCallbacksJsonWriter, write_string) {
-//   EXPECT_NO_THROW(writer("message"));
-//   EXPECT_EQ("message\n",
-//             ss.str());
-// }
+TEST_F(StanInterfaceCallbacksJsonWriter, write_string) {
+  EXPECT_NO_THROW(writer.write("key", "value"));
+  EXPECT_EQ("\"key\" : \"value\"", ss.str());
+}
