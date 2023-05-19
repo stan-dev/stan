70ec6e89a01c5cfb6bf005f28c8c24407a32cab6
Stan Jenkins <mc.stanislaw@gmail.com>
2023-05-19 12:45:43 -0400

    [Jenkins] auto-formatting by clang-format version 10.0.0-4ubuntu1

--- src/test/unit/callbacks/json_writer_test.cpp
+++ src/test/unit/callbacks/json_writer_test.cpp
@@ -224,5 +224,5 @@ TEST_F(StanInterfaceCallbacksJsonWriter, no_op_writer) {
   std::string key("key");
   std::string value("value");
   stan::callbacks::json_writer<std::stringstream, deleter_noop> writer;
-  EXPECT_NO_THROW(writer.write(key,value));
+  EXPECT_NO_THROW(writer.write(key, value));
 }
