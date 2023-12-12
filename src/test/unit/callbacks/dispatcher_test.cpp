83a9b0625a0efb93b35b1d65de6fbe1c760724a2
Mitzi Morris <mitzi@panix.com>
2023-12-12 15:21:40 -0500

    checkpointing unit test

--- src/test/unit/callbacks/dispatcher_test.cpp
+++ src/test/unit/callbacks/dispatcher_test.cpp
@@ -16,7 +16,8 @@ struct deleter_noop {
 class CallbacksDispatcher : public ::testing::Test {
  public:
   CallbacksDispatcher()
-      : ss_metric(), ss_draws_cnstrn(),
+      : ss_metric(),
+        ss_draws_cnstrn(),
         writer_metric(std::unique_ptr<std::stringstream, deleter_noop>(&ss_metric)),
         writer_draws_cnstrn(std::unique_ptr<std::stringstream, deleter_noop>(&ss_draws_cnstrn)) {
   }
@@ -26,10 +27,10 @@ class CallbacksDispatcher : public ::testing::Test {
     ss_metric.clear();
     ss_draws_cnstrn.str(std::string());
     ss_draws_cnstrn.clear();
-    std::shared_ptr<stan::callbacks::structured_writer> writer_metric_ptr =
-        std::make_shared<stan::callbacks::json_writer<std::stringstream, deleter_noop>>(std::move(writer_metric));
-    std::shared_ptr<stan::callbacks::structured_writer> writer_draws_cnstrn_ptr =
-        std::make_shared<stan::callbacks::csv_writer<std::stringstream, deleter_noop>>(std::move(writer_draws_cnstrn));
+    std::shared_ptr<stan::callbacks::structured_writer> writer_metric_ptr
+        = std::make_shared<stan::callbacks::json_writer<std::stringstream, deleter_noop>>(std::move(writer_metric));
+    std::shared_ptr<stan::callbacks::structured_writer> writer_draws_cnstrn_ptr
+        = std::make_shared<stan::callbacks::csv_writer<std::stringstream, deleter_noop>>(std::move(writer_draws_cnstrn));
     dp.addWriter(stan::callbacks::info_type::METRIC, std::move(writer_metric_ptr));
     dp.addWriter(stan::callbacks::info_type::DRAW_CONSTRAINED, std::move(writer_draws_cnstrn_ptr));
   }
@@ -73,5 +74,21 @@ TEST_F(CallbacksDispatcher, write_noops) {
   EXPECT_NO_THROW(dp.begin_record(stan::callbacks::info_type::CONFIG));
   EXPECT_NO_THROW(dp.end_record(stan::callbacks::info_type::CONFIG));
   EXPECT_NO_THROW(dp.write(stan::callbacks::info_type::CONFIG, "foo", "bar"));
+  ASSERT_TRUE(ss_metric.str().empty());
+  ASSERT_TRUE(ss_draws_cnstrn.str().empty());
 }
 
+TEST_F(CallbacksDispatcher, csv_no_header) {
+  std::vector<double> values = {1, 2, 3};
+  dp.table_row(stan::callbacks::info_type::DRAW_CONSTRAINED, values);
+  ASSERT_TRUE(ss_draws_cnstrn.str().empty());
+}
+
+TEST_F(CallbacksDispatcher, csv_mult_header) {
+  std::vector<std::string> header = {"mu", "sigma", "theta"};
+  std::vector<double> values = {1, 2, 3};
+  dp.table_header(stan::callbacks::info_type::DRAW_CONSTRAINED, header);
+  dp.table_header(stan::callbacks::info_type::DRAW_CONSTRAINED, header);
+  dp.table_row(stan::callbacks::info_type::DRAW_CONSTRAINED, values);
+  EXPECT_EQ("mu, sigma, theta\n1, 2, 3\n", ss_draws_cnstrn.str());
+}
