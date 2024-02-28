51ad447d6965a59da24c6365c078450bdaebea9b
Stan Jenkins <mc.stanislaw@gmail.com>
2024-02-28 13:16:43 -0500

    [Jenkins] auto-formatting by clang-format version 10.0.0-4ubuntu1

--- src/stan/analyze/mcmc/compute_potential_scale_reduction.hpp
+++ src/stan/analyze/mcmc/compute_potential_scale_reduction.hpp
@@ -116,7 +116,7 @@ inline double compute_potential_scale_reduction_rank(
   for (int chain = 1; chain < num_chains; ++chain) {
     num_draws = std::min(num_draws, sizes[chain]);
   }
-  
+
   // check if chains are constant; all equal to first draw's value
   bool are_all_const = false;
   Eigen::VectorXd init_draw = Eigen::VectorXd::Zero(num_chains);
