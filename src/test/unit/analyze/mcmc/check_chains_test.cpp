f67a14241ccac890cfbfabe67787d49e60b748bc
Stan Jenkins <mc.stanislaw@gmail.com>
2024-10-07 16:55:36 -0400

    [Jenkins] auto-formatting by clang-format version 10.0.0-4ubuntu1

--- src/test/unit/analyze/mcmc/check_chains_test.cpp
+++ src/test/unit/analyze/mcmc/check_chains_test.cpp
@@ -43,6 +43,6 @@ TEST(CheckChains, good_and_bad) {
 
   // above test shows that column 7 is - make it non-finite
   chain_1.col(0) = eight_schools_1.samples.col(7);
-  chain_1(0,0) = std::numeric_limits<double>::infinity();
+  chain_1(0, 0) = std::numeric_limits<double>::infinity();
   EXPECT_FALSE(stan::analyze::is_finite_and_varies(chain_1));
 }
