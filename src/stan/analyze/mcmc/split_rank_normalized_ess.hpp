ada840722a41530e896669e790343fe0fd578534
Mitzi Morris <mitzi212@gmail.com>
2024-10-17 19:48:27 -0400

    ess use stan::analyze::autocovariance, not stan::math

--- src/stan/analyze/mcmc/split_rank_normalized_ess.hpp
+++ src/stan/analyze/mcmc/split_rank_normalized_ess.hpp
@@ -2,6 +2,7 @@
 #define STAN_ANALYZE_MCMC_SPLIT_RANK_NORMALIZED_ESS_HPP
 
 #include <stan/math/prim.hpp>
+#include <stan/analyze/mcmc/autocovariance.hpp>
 #include <stan/analyze/mcmc/check_chains.hpp>
 #include <stan/analyze/mcmc/rank_normalization.hpp>
 #include <stan/analyze/mcmc/split_chains.hpp>
@@ -38,11 +39,12 @@ double ess(const Eigen::MatrixXd& chains) {
 
   // compute the per-chain autocovariance
   for (size_t i = 0; i < num_chains; ++i) {
-    Eigen::Map<const Eigen::VectorXd> chain_col(chains.col(i).data(),
+    chain_mean(i) = chains.col(i).mean();
+    Eigen::Map<const Eigen::VectorXd> draw_col(chains.col(i).data(),
                                                 num_draws);
-    Eigen::Map<Eigen::VectorXd> cov_col(acov.col(i).data(), num_draws);
-    stan::math::autocovariance<double>(chain_col, cov_col);
-    chain_mean(i) = chain_col.mean();
+    Eigen::VectorXd cov_col(num_draws);
+    autocovariance<double>(draw_col, cov_col);
+    acov.col(i) = cov_col;
     chain_var(i) = cov_col(0) * num_draws / (num_draws - 1);
   }
 
@@ -56,9 +58,12 @@ double ess(const Eigen::MatrixXd& chains) {
   // Geyer's initial positive sequence, eqn (11)
   Eigen::VectorXd rho_hat_t = Eigen::VectorXd::Zero(num_draws);
   Eigen::VectorXd acov_t(num_chains);
+  for (size_t i = 0; i < num_chains; ++i) {
+    acov_t(i) = acov(1, i);
+  }
   double rho_hat_even = 1.0;
   rho_hat_t(0) = rho_hat_even;  // lag 0
-  double rho_hat_odd = 1 - (w_chain_var - acov.row(1).mean()) / var_plus;
+  double rho_hat_odd = 1 - (w_chain_var - acov_t.mean()) / var_plus;
   rho_hat_t(1) = rho_hat_odd;  // lag 1
 
   // compute autocorrelation at lag t for pair (t, t+1)
--- src/test/unit/analyze/mcmc/split_rank_normalized_ess_test.cpp
+++ src/test/unit/analyze/mcmc/split_rank_normalized_ess_test.cpp
@@ -1,3 +1,4 @@
+#include <stan/analyze/mcmc/compute_effective_sample_size.hpp>
 #include <stan/analyze/mcmc/split_rank_normalized_ess.hpp>
 #include <stan/io/stan_csv_reader.hpp>
 #include <gtest/gtest.h>
@@ -6,43 +7,57 @@
 #include <string>
 #include <cmath>
 
-TEST(RankNormalizedEss, test_bulk_tail_basic_ess) {
+
+TEST(RankNormalizedEss, test_basic_bulk_tail_ess) {
   std::stringstream out;
-  std::ifstream eight_schools_1_stream, eight_schools_2_stream;
-  stan::io::stan_csv eight_schools_1, eight_schools_2;
-  eight_schools_1_stream.open(
-      "src/test/unit/mcmc/test_csv_files/eight_schools_1.csv",
-      std::ifstream::in);
-  eight_schools_1
-      = stan::io::stan_csv_reader::parse(eight_schools_1_stream, &out);
-  eight_schools_1_stream.close();
+  Eigen::MatrixXd chains_lp(1000, 4);
+  Eigen::MatrixXd chains_theta(1000, 4);
 
-  eight_schools_2_stream.open(
-      "src/test/unit/mcmc/test_csv_files/eight_schools_2.csv",
-      std::ifstream::in);
-  eight_schools_2
-      = stan::io::stan_csv_reader::parse(eight_schools_2_stream, &out);
-  eight_schools_2_stream.close();
+  std::vector<const double*> draws_theta(4);
+  std::vector<const double*> draws_lp(4);
+  std::vector<size_t> sizes(4);
 
-  // test against R implementation in pkg posterior (via cmdstanr)
-  Eigen::VectorXd ess_8_schools_bulk(10);
-  ess_8_schools_bulk << 348, 370, 600, 638, 765, 608, 629, 274, 517, 112;
-  Eigen::VectorXd ess_8_schools_tail(10);
-  ess_8_schools_tail << 845, 858, 874, 726, 620, 753, 826, 628, 587, 108;
-  Eigen::VectorXd ess_8_schools_basic(10);
-  ess_8_schools_basic << 351, 403, 569, 646, 788, 602, 634, 262, 520, 223;
-
-  Eigen::MatrixXd chains(eight_schools_1.samples.rows(), 2);
-  for (size_t i = 0; i < 10; ++i) {
-    chains.col(0) = eight_schools_1.samples.col(i + 7);
-    chains.col(1) = eight_schools_2.samples.col(i + 7);
-    auto ess = stan::analyze::split_rank_normalized_ess(chains);
-    EXPECT_NEAR(ess.first, ess_8_schools_bulk(i), 5);
-    EXPECT_NEAR(ess.second, ess_8_schools_tail(i), 5);
-    auto ess_basic = stan::analyze::ess(chains);
-    EXPECT_NEAR(ess_basic, ess_8_schools_basic(i), 5);
+  for (size_t i = 0; i < 4; ++i) {
+    std::stringstream fname;
+    fname << "src/test/unit/analyze/mcmc/test_csv_files/bern" << (i + 1) << ".csv"; 
+    std::ifstream bern_stream(fname.str(), std::ifstream::in);
+    stan::io::stan_csv bern_csv = stan::io::stan_csv_reader::parse(bern_stream, &out);
+    bern_stream.close();
+    chains_lp.col(i) = bern_csv.samples.col(0);
+    chains_theta.col(i) = bern_csv.samples.col(7);
+    draws_lp[i] = chains_lp.col(i).data();
+    draws_theta[i] = chains_theta.col(i).data();
+    sizes[i] = 1000;
   }
+  double ess_lp_expect = 1335.4137;
+  double ess_lp_bulk_expect = 1512.7684;
+  double ess_lp_tail_expect = 1591.9707;
+
+  double ess_theta_expect = 1377.503;
+  double ess_theta_bulk_expect = 1407.5124;
+  double ess_theta_tail_expect = 1291.7131;
+  
+  auto ess_basic_lp = stan::analyze::ess(chains_lp);
+  auto old_ess_basic_lp = stan::analyze::compute_effective_sample_size(draws_lp, sizes);
+  auto ess_lp = stan::analyze::split_rank_normalized_ess(chains_lp);
+
+  auto ess_basic_theta = stan::analyze::ess(chains_theta);
+  auto old_ess_basic_theta = stan::analyze::compute_effective_sample_size(draws_theta, sizes);
+  auto ess_theta = stan::analyze::split_rank_normalized_ess(chains_theta);
+
+  EXPECT_NEAR(ess_lp_expect, ess_basic_lp, 0.001);
+  EXPECT_NEAR(ess_theta_expect, ess_basic_theta, 0.001);
+
+  EXPECT_NEAR(old_ess_basic_lp, ess_basic_lp, 0.00001);
+  EXPECT_NEAR(old_ess_basic_theta, ess_basic_theta, 0.00001);
+
+  EXPECT_NEAR(ess_lp_bulk_expect, ess_lp.first, 0.001);
+  EXPECT_NEAR(ess_lp_tail_expect, ess_lp.second, 0.001);
+
+  EXPECT_NEAR(ess_theta_bulk_expect, ess_theta.first, 0.001);
+  EXPECT_NEAR(ess_theta_tail_expect, ess_theta.second, 0.001);
 }
+
 
 TEST(RankNormalizedEss, short_chains_fail) {
   std::stringstream out;
