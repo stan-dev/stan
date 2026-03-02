069f5923212ae4c4c86c35f3472d86c6a914db17
Brian Ward <bward@flatironinstitute.org>
2026-03-02 15:24:43 -0500

    Update standalone_gqs_test.cpp

--- src/test/unit/services/sample/standalone_gqs_test.cpp
+++ src/test/unit/services/sample/standalone_gqs_test.cpp
@@ -55,7 +55,7 @@ TEST_F(ServicesStandaloneGQ, genDraws_bernoulli) {
   EXPECT_EQ(return_code, stan::services::error_codes::OK);
   EXPECT_EQ(count_matches("mu", sample_ss.str()), 1);
   EXPECT_EQ(count_matches("y_rep", sample_ss.str()), 10);
-  EXPECT_EQ(count_matches("\n", sample_ss.str()), 1001);
+  EXPECT_EQ(count_matches("\n", sample_ss.str()), 1004);
   match_csv_columns(bern_csv.samples, sample_ss.str(), 1000, 1, 8);
 }
 
